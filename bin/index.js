a321b38a00f3a0fac65d86567aaffb16384bf075
Enes Dindaş <37349027+enesdindas@users.noreply.github.com>
2020-11-21 16:56:08 +0300

    fix

--- bin/index.js
+++ bin/index.js
@@ -17,8 +17,8 @@ function startXmysql(sqlConfig) {
   let app = express();
   app.use(morgan("tiny"));
   app.use(cors());
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  app.use(bodyParser.json({limit: '50mb', extended: true, parameterLimit:50000}));
+  app.use(express.json({limit: '50mb', extended: true, parameterLimit:50000}));
   /**************** END : setup express ****************/
 
   /**************** START : setup mysql ****************/

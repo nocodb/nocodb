470942b9b5c7054101dc31af77310075068b6279
SebGTx <8062146+SebGTx@users.noreply.github.com>
2021-06-12 16:37:55 +0200

    Update i18n.js

--- packages/nc-gui/plugins/i18n.js
+++ packages/nc-gui/plugins/i18n.js
@@ -11,7 +11,7 @@ export default ({app, store}) => {
   // This way we can use it globally in our components through this.$i18n
   app.i18n = new VueI18n({
     // Set the initial locale
-    locale: "en",
+    locale: "fr",
 
     // Set the fallback locale in case the current locale can't be found
     fallbackLocale: "en",

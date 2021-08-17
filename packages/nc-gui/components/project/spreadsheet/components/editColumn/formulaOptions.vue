c0e44ba457d2ab90aa8ab9501f2f928d7cc59d6c
Pranav C <pranavxc@gmail.com>
2021-08-17 19:58:02 +0530

    fix: Missing operation added
    added <
    
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui/components/project/spreadsheet/components/editColumn/formulaOptions.vue
+++ packages/nc-gui/components/project/spreadsheet/components/editColumn/formulaOptions.vue
@@ -73,7 +73,7 @@ export default {
     formula: {},
     // formulas: ['AVERAGE()', 'COUNT()', 'COUNTA()', 'COUNTALL()', 'SUM()', 'MIN()', 'MAX()', 'AND()', 'OR()', 'TRUE()', 'FALSE()', 'NOT()', 'XOR()', 'ISERROR()', 'IF()', 'LEN()', 'MID()', 'LEFT()', 'RIGHT()', 'FIND()', 'CONCATENATE()', 'T()', 'VALUE()', 'ARRAYJOIN()', 'ARRAYUNIQUE()', 'ARRAYCOMPACT()', 'ARRAYFLATTEN()', 'ROUND()', 'ROUNDUP()', 'ROUNDDOWN()', 'INT()', 'EVEN()', 'ODD()', 'MOD()', 'LOG()', 'EXP()', 'POWER()', 'SQRT()', 'CEILING()', 'FLOOR()', 'ABS()', 'RECORD_ID()', 'CREATED_TIME()', 'ERROR()', 'BLANK()', 'YEAR()', 'MONTH()', 'DAY()', 'HOUR()', 'MINUTE()', 'SECOND()', 'TODAY()', 'NOW()', 'WORKDAY()', 'DATETIME_PARSE()', 'DATETIME_FORMAT()', 'SET_LOCALE()', 'SET_TIMEZONE()', 'DATESTR()', 'TIMESTR()', 'TONOW()', 'FROMNOW()', 'DATEADD()', 'WEEKDAY()', 'WEEKNUM()', 'DATETIME_DIFF()', 'WORKDAY_DIFF()', 'IS_BEFORE()', 'IS_SAME()', 'IS_AFTER()', 'REPLACE()', 'REPT()', 'LOWER()', 'UPPER()', 'TRIM()', 'SUBSTITUTE()', 'SEARCH()', 'SWITCH()', 'LAST_MODIFIED_TIME()', 'ENCODE_URL_COMPONENT()', 'REGEX_EXTRACT()', 'REGEX_MATCH()', 'REGEX_REPLACE()']
     availableFunctions: formulaList,
-    availableBinOps: ['+', '-', '*', '/', '>', '==', '<=', '>=', '!='],
+    availableBinOps: ['+', '-', '*', '/', '>', '<', '==', '<=', '>=', '!='],
     autocomplete: false,
     suggestion: null,
     wordToComplete: '',

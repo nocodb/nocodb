865b4aea7e2fabc8099f736cc5df6e56e65ca9c3
Pranav C <pranavxc@gmail.com>
2021-08-17 09:43:08 +0530

    chore(publish): v0.11.19
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui/nuxt.config.js
+++ packages/nc-gui/nuxt.config.js
@@ -19,7 +19,7 @@ export default {
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'static',
+  target: 'server',
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head

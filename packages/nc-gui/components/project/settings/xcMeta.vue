e6a26615aa5fef73259dfa8ae177b40ea0ffb1af
Naveen MR <oof1lab@gmail.com>
2021-09-16 13:28:26 +0100

    chore : Comment out additional options in meta management.
    Let it be simple export and import available.
    
    Signed-off-by: Naveen MR <oof1lab@gmail.com>

--- packages/nc-gui/components/project/settings/xcMeta.vue
+++ packages/nc-gui/components/project/settings/xcMeta.vue
@@ -12,52 +12,52 @@
       <!--      </tr>-->
       <!--      </thead>-->
       <tbody>
-        <tr>
-          <td>
-            <!-- Export all metadata from the meta tables to meta directory. -->
-            {{ $t('management.meta.export_to_file.desc') }}
-          </td>
-          <td>
-            <v-btn
-              min-width="150"
-              color="primary"
-              small
-              outlined
-              :loading="loading === 'export-file'"
-              @click="exportMeta"
-            >
-              <v-icon small>
-                mdi-export
-              </v-icon>&nbsp;
-              <!-- Export to file -->
-              {{ $t('management.meta.export_to_file.title') }}
-            </v-btn>
-          </td>
-        </tr>
+<!--        <tr>-->
+<!--          <td>-->
+<!--            &lt;!&ndash; Export all metadata from the meta tables to meta directory. &ndash;&gt;-->
+<!--            {{ $t('management.meta.export_to_file.desc') }}-->
+<!--          </td>-->
+<!--          <td>-->
+<!--            <v-btn-->
+<!--              min-width="150"-->
+<!--              color="primary"-->
+<!--              small-->
+<!--              outlined-->
+<!--              :loading="loading === 'export-file'"-->
+<!--              @click="exportMeta"-->
+<!--            >-->
+<!--              <v-icon small>-->
+<!--                mdi-export-->
+<!--              </v-icon>&nbsp;-->
+<!--              &lt;!&ndash; Export to file &ndash;&gt;-->
+<!--              {{ $t('management.meta.export_to_file.title') }}-->
+<!--            </v-btn>-->
+<!--          </td>-->
+<!--        </tr>-->
 
-        <tr>
-          <td>
-            <!-- Import all metadata from the meta directory to meta tables. -->
-            {{ $t('management.meta.import.desc') }}
-          </td>
-          <td>
-            <v-btn
-              :loading="loading === 'import-file'"
-              min-width="150"
-              color="info"
-              small
-              outlined
-              @click="importMeta"
-            >
-              <v-icon small>
-                mdi-import
-              </v-icon>&nbsp;
+<!--        <tr>-->
+<!--          <td>-->
+<!--            &lt;!&ndash; Import all metadata from the meta directory to meta tables. &ndash;&gt;-->
+<!--            {{ $t('management.meta.import.desc') }}-->
+<!--          </td>-->
+<!--          <td>-->
+<!--            <v-btn-->
+<!--              :loading="loading === 'import-file'"-->
+<!--              min-width="150"-->
+<!--              color="info"-->
+<!--              small-->
+<!--              outlined-->
+<!--              @click="importMeta"-->
+<!--            >-->
+<!--              <v-icon small>-->
+<!--                mdi-import-->
+<!--              </v-icon>&nbsp;-->
 
-              <!-- Import -->
-              {{ $t('management.meta.import.title') }}
-            </v-btn>
-          </td>
-        </tr>
+<!--              &lt;!&ndash; Import &ndash;&gt;-->
+<!--              {{ $t('management.meta.import.title') }}-->
+<!--            </v-btn>-->
+<!--          </td>-->
+<!--        </tr>-->
 
         <tr>
           <td>
@@ -112,28 +112,28 @@
             >
           </td>
         </tr>
-        <tr>
-          <td>
-            <!-- Clear all metadata from meta tables. -->
-            {{ $t('management.meta.reset.desc') }}
-          </td>
-          <td>
-            <v-btn
-              :loading="loading === 'reset-metadata'"
-              min-width="150"
-              color="error"
-              small
-              outlined
-              @click="resetMeta"
-            >
-              <v-icon small>
-                mdi-delete-variant
-              </v-icon>&nbsp;
-              <!-- Reset -->
-              {{ $t('management.meta.reset.title') }}
-            </v-btn>
-          </td>
-        </tr>
+<!--        <tr>-->
+<!--          <td>-->
+<!--            &lt;!&ndash; Clear all metadata from meta tables. &ndash;&gt;-->
+<!--            {{ $t('management.meta.reset.desc') }}-->
+<!--          </td>-->
+<!--          <td>-->
+<!--            <v-btn-->
+<!--              :loading="loading === 'reset-metadata'"-->
+<!--              min-width="150"-->
+<!--              color="error"-->
+<!--              small-->
+<!--              outlined-->
+<!--              @click="resetMeta"-->
+<!--            >-->
+<!--              <v-icon small>-->
+<!--                mdi-delete-variant-->
+<!--              </v-icon>&nbsp;-->
+<!--              &lt;!&ndash; Reset &ndash;&gt;-->
+<!--              {{ $t('management.meta.reset.title') }}-->
+<!--            </v-btn>-->
+<!--          </td>-->
+<!--        </tr>-->
       </tbody>
     </v-simple-table>
 

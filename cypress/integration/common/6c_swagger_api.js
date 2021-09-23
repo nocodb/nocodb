c2d88e5c18ee880cf3a9541233fd5db6728a9ed5
Raju Udava <sivadstala@gmail.com>
2021-09-23 12:36:05 +0530

    test: [cypress] Swagger API validation
    Signed-off-by: Raju Udava <sivadstala@gmail.com>

--- cypress/integration/common/6c_swagger_api.js
+++ cypress/integration/common/6c_swagger_api.js
@@ -1,6 +1,7 @@
 
 import { loginPage } from "../../support/page_objects/navigation"
 import { mainPage } from "../../support/page_objects/mainPage"
+import { roles } from "../../support/page_objects/projectConstants"
 
 describe(`Swagger API - Test preparation`, () => {
     before(()=> {
@@ -33,6 +34,8 @@ describe(`Swagger page, base verification`, () => {
         return cy.get(`#operations-tag-${tag}`).next().find('.opblock').eq(idx).find(`button:contains(${desc})`)
     }
 
+    let Token
+
     // basic authentication tag verification
     //
     it("Swagger URL access & basic validation", () => {
@@ -40,12 +43,12 @@ describe(`Swagger page, base verification`, () => {
         //
         cy.readFile("shared.json").then((jsonPayload) => {
             let URL = jsonPayload.SWAGGER_URL
-            let Token = jsonPayload.AUTH_TOKEN
+            Token = jsonPayload.AUTH_TOKEN
 
             cy.visit(URL)
             cy.log(Token)
 
-            // validate
+            // validate; API order assumed
             cy.get('#operations-tag-Authentication').next().find('.opblock').should('has.length', 9)
             getSwaggerButton("Authentication", 0, "User login").should('exist')
             getSwaggerButton("Authentication", 1, "User signup").should('exist')
@@ -59,7 +62,101 @@ describe(`Swagger page, base verification`, () => {
         })
     })
 
-    // clean-up created file
+    it("Authorize success: Valid token", ()=> {
+        // authorize button, feed token, click authorize
+        cy.get('[class="btn authorize unlocked"]').click()
+        cy.get('input').type(Token)
+        cy.get('.auth-btn-wrapper > .authorize').click()
+
+        // Response: "Authorized" should exist on DOM
+        cy.get('.auth-container').contains('Authorized').should('exist')        
+        cy.get('.btn-done').click()
+
+        // Authorize button is LOCKED now
+        cy.get('[class="btn authorize locked"]').should('exist')
+    })
+
+    it("Execute Authentication (valid token case) > GET: User details API", ()=> {  
+        // Auth> User details API  
+        getSwaggerButton("Authentication", 6, "User details").click()
+
+        // "Try it out" button, followed by "Execute"
+        cy.get('.try-out > .btn').click()
+        cy.get('.execute-wrapper > .btn').click()
+
+        // check response: validate email credentials
+        cy.get('.highlight-code > .microlight').contains('email').should('exist')
+        cy.get('.highlight-code > .microlight').contains(roles.owner.credentials.username).should('exist')
+
+        // reset operations (clear, cancel, windback User details tab)
+        cy.get('.btn-clear').click()
+        cy.get('.try-out > .btn').click()
+        getSwaggerButton("Authentication", 6, "User details").click()
+    })
+
+    it("Logout post authorization", ()=> {
+        // authorize button, logout
+        cy.get('[class="btn authorize locked"]').click()
+        cy.get('.auth-btn-wrapper > button:contains("Logout")').click()
+        cy.get('.btn-done').click()
+
+        // Authorize button is UNLOCKED now
+        cy.get('[class="btn authorize unlocked"]').should('exist')
+    })    
+
+    it("Execute Authentication (logout case) > GET: User details API", ()=> {    
+        // Auth> User details API  
+        getSwaggerButton("Authentication", 6, "User details").click()
+
+        // "Try it out" button, followed by "Execute"
+        cy.get('.try-out > .btn').click()
+        cy.get('.execute-wrapper > .btn').click()
+
+        // check response: email credentials shouldnt exist. should display 'guest:true'
+        cy.get('.highlight-code > .microlight').contains('guest').should('exist')
+        cy.get('.highlight-code > .microlight').contains('email').should('not.exist')
+        cy.get('.highlight-code > .microlight').contains(roles.owner.credentials.username).should('not.exist')
+
+        // reset operations (clear, cancel, windback User details tab)
+        cy.get('.btn-clear').click()
+        cy.get('.try-out > .btn').click()
+        getSwaggerButton("Authentication", 6, "User details").click()        
+    })
+
+    it("Authorize failure: invalid token", ()=> {
+        // authorize button, feed *invalid* token, click authorize
+        cy.get('[class="btn authorize unlocked"]').click()
+        cy.get('input').type('xyz')
+        cy.get('.auth-btn-wrapper > .authorize').click()
+
+        // Response: "Authorized" should *not* exist on DOM
+        // TBD: cy.get('.auth-container').contains('Authorized').should('not.exist')        
+        cy.get('.btn-done').click()
+
+        // Authorize button should be UNLOCKED now
+        // TBD: cy.get('[class="btn authorize unlocked"]').should('exist')
+    })    
+
+    it("Execute Authentication (invalid token case) > GET: User details API", ()=> {    
+        // Auth> User details API  
+        getSwaggerButton("Authentication", 6, "User details").click()
+
+        // "Try it out" button, followed by "Execute"
+        cy.get('.try-out > .btn').click()
+        cy.get('.execute-wrapper > .btn').click()
+
+        // check response: email credentials shouldnt exist. should display 'guest:true'
+        cy.get('.highlight-code > .microlight').contains('guest').should('exist')
+        cy.get('.highlight-code > .microlight').contains('email').should('not.exist')
+        cy.get('.highlight-code > .microlight').contains(roles.owner.credentials.username).should('not.exist')
+
+        // reset operations (clear, cancel, windback User details tab)
+        cy.get('.btn-clear').click()
+        cy.get('.try-out > .btn').click()
+        getSwaggerButton("Authentication", 6, "User details").click()         
+    })
+
+    // clean-up created file (shared.json)
     after(() => {
         cy.exec("del shared.json").then(()=> {
             cy.log("file cleaned up!")

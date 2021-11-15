6860ad1d80f9b7167e712dffd9ec516dfa043ef1
Pranav C <pranavxc@gmail.com>
2021-11-15 19:54:56 +0530

    test(cypress): remove whitespace from url
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- scripts/cypress/integration/common/6f_attachments.js
+++ scripts/cypress/integration/common/6f_attachments.js
@@ -1,112 +1,111 @@
-
-import { mainPage } from "../../support/page_objects/mainPage"
-import { loginPage } from "../../support/page_objects/navigation"
-import { isTestSuiteActive } from "../../support/page_objects/projectConstants"
+import {mainPage} from "../../support/page_objects/mainPage"
+import {loginPage} from "../../support/page_objects/navigation"
+import {isTestSuiteActive} from "../../support/page_objects/projectConstants"
 
 export const genTest = (type, xcdb) => {
-    if(!isTestSuiteActive(type, xcdb)) return;
+  if (!isTestSuiteActive(type, xcdb)) return;
 
-    describe(`${type.toUpperCase()} Columns of type attachment`, () => {
-        before(() => {
-            loginPage.loginAndOpenProject(type)
-            cy.openTableTab('Country');
-        })
+  describe(`${type.toUpperCase()} Columns of type attachment`, () => {
+    before(() => {
+      loginPage.loginAndOpenProject(type)
+      cy.openTableTab('Country');
+    })
 
-        after(() => {
-            mainPage.deleteColumn('testAttach')
+    after(() => {
+      mainPage.deleteColumn('testAttach')
 
-            // clean up newly added rows into Country table operations
-            // this auto verifies successfull addition of rows to table as well
-            mainPage.getPagination(5).click()
-            cy.wait(3000)
-            mainPage.getRow(10).find('.mdi-checkbox-blank-outline').click({ force: true })
-              
-            mainPage.getCell("Country", 10).rightclick()
-            cy.getActiveMenu().contains('Delete Selected Row').click()
-            
-            cy.closeTableTab('Country')          
-        })
-        
-        it(`Add column of type attachments`, () => {
-            mainPage.addColumnWithType('testAttach', 'Attachment')
-            cy.wait(4000)
-            for (let i = 1; i <= 2; i++) {
-                let filepath = `sampleFiles/${i}.json`
-                mainPage.getCell('testAttach', i).click().find('input[type="file"]').attachFile(filepath)
-                cy.wait(4000)      
-            }
-        })
+      // clean up newly added rows into Country table operations
+      // this auto verifies successfull addition of rows to table as well
+      mainPage.getPagination(5).click()
+      cy.wait(3000)
+      mainPage.getRow(10).find('.mdi-checkbox-blank-outline').click({force: true})
 
-        it(`Form view with Attachment field- Submit & verify`, () => {
-            // create form-view
-            cy.get(`.nc-create-form-view`).click();
-            cy.getActiveModal().find('button:contains(Submit)').click()
+      mainPage.getCell("Country", 10).rightclick()
+      cy.getActiveMenu().contains('Delete Selected Row').click()
 
-            cy.get(`.nc-view-item.nc-form-view-item`).contains('Country1').click()
-            cy.get('.v-navigation-drawer__content > .container')
-                .find('.v-list > .v-list-item')
-                .contains('Share View')
-                .click()
+      cy.closeTableTab('Country')
+    })
 
-            // copy link text, visit URL
-            cy.getActiveModal().find('.share-link-box')
-                .contains('/nc/form/', { timeout: 10000 })
-                .then(($obj) => {
+    it(`Add column of type attachments`, () => {
+      mainPage.addColumnWithType('testAttach', 'Attachment')
+      cy.wait(4000)
+      for (let i = 1; i <= 2; i++) {
+        let filepath = `sampleFiles/${i}.json`
+        mainPage.getCell('testAttach', i).click().find('input[type="file"]').attachFile(filepath)
+        cy.wait(4000)
+      }
+    })
 
-                    let linkText = $obj.text()
-                    cy.log(linkText)
-                    cy.visit(linkText)
-            
-                    // wait for share view page to load!
-                    cy.wait(5000)
+    it(`Form view with Attachment field- Submit & verify`, () => {
+      // create form-view
+      cy.get(`.nc-create-form-view`).click();
+      cy.getActiveModal().find('button:contains(Submit)').click()
 
-                    cy.get('#data-table-form-Country').type('_abc')
-                    cy.get('#data-table-form-LastUpdate').click()
-                    cy.getActiveModal().find('button').contains('19').click()
-                    cy.getActiveModal().find('button').contains('OK').click()
+      cy.get(`.nc-view-item.nc-form-view-item`).contains('Country1').click()
+      cy.get('.v-navigation-drawer__content > .container')
+        .find('.v-list > .v-list-item')
+        .contains('Share View')
+        .click()
 
-                    cy.get('.nc-field-editables')
-                        .last()
-                        .find('input[type="file"]')
-                        .attachFile(`sampleFiles/1.json`)
+      // copy link text, visit URL
+      cy.getActiveModal().find('.share-link-box')
+        .contains('/nc/form/', {timeout: 10000})
+        .then(($obj) => {
 
-                    // submit button & validate
-                    cy.get('.nc-form').find('button').contains('Submit').click()
-                    cy.wait(3000)
-                })
-        })
+          let linkText = $obj.text().trim()
+          cy.log(linkText)
+          cy.visit(linkText)
 
-        it(`Filter column which contain only attachments, download CSV`, () => {
-            // come back to main window
-            loginPage.loginAndOpenProject(type)
-            cy.openTableTab('Country');
-            
-            mainPage.filterField('testAttach', 'is not null', null)
-            cy.wait(1000)
-            mainPage.hideUnhideField('LastUpdate')
+          // wait for share view page to load!
+          cy.wait(5000)
 
-            const verifyCsv = (retrievedRecords) => {
-                let storedRecords = [
-                    `Country,Country => City,testAttach`,
-                    `Afghanistan,Kabul,1.json(http://localhost:8080/dl/externalrest_5agd/db/country/testAttach_VWk3fz_1.json)`
-                ]
-                
-                expect(retrievedRecords[0]).to.be.equal(storedRecords[0])
-                for (let i = 1; i < storedRecords.length; i++) {
-                    const columns = retrievedRecords[i].split(',')
-                    expect(columns[2]).to.contain('.json(http://localhost:8080/dl/external')
-                }
-                
-                cy.log(retrievedRecords[109])
-                cy.log(retrievedRecords[110])
-                cy.log(retrievedRecords[111])
-            }
+          cy.get('#data-table-form-Country').type('_abc')
+          cy.get('#data-table-form-LastUpdate').click()
+          cy.getActiveModal().find('button').contains('19').click()
+          cy.getActiveModal().find('button').contains('OK').click()
 
-            mainPage.downloadAndVerifyCsv(`Country_exported_1.csv`, verifyCsv)
-            mainPage.hideUnhideField('LastUpdate')
-            mainPage.filterReset()
+          cy.get('.nc-field-editables')
+            .last()
+            .find('input[type="file"]')
+            .attachFile(`sampleFiles/1.json`)
+
+          // submit button & validate
+          cy.get('.nc-form').find('button').contains('Submit').click()
+          cy.wait(3000)
         })
     })
+
+    it(`Filter column which contain only attachments, download CSV`, () => {
+      // come back to main window
+      loginPage.loginAndOpenProject(type)
+      cy.openTableTab('Country');
+
+      mainPage.filterField('testAttach', 'is not null', null)
+      cy.wait(1000)
+      mainPage.hideUnhideField('LastUpdate')
+
+      const verifyCsv = (retrievedRecords) => {
+        let storedRecords = [
+          `Country,Country => City,testAttach`,
+          `Afghanistan,Kabul,1.json(http://localhost:8080/dl/externalrest_5agd/db/country/testAttach_VWk3fz_1.json)`
+        ]
+
+        expect(retrievedRecords[0]).to.be.equal(storedRecords[0])
+        for (let i = 1; i < storedRecords.length; i++) {
+          const columns = retrievedRecords[i].split(',')
+          expect(columns[2]).to.contain('.json(http://localhost:8080/dl/external')
+        }
+
+        cy.log(retrievedRecords[109])
+        cy.log(retrievedRecords[110])
+        cy.log(retrievedRecords[111])
+      }
+
+      mainPage.downloadAndVerifyCsv(`Country_exported_1.csv`, verifyCsv)
+      mainPage.hideUnhideField('LastUpdate')
+      mainPage.filterReset()
+    })
+  })
 }
 
 /**

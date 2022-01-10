352367a83611ab66a45318a21a2645605c182a5b
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-01-10 15:13:06 +0800

    fix: page title
    Signed-off-by: Wing-Kam Wong <wingkwong.code@gmail.com>

--- packages/nocodb/src/lib/noco/rest/ui/auth/signin.ts
+++ packages/nocodb/src/lib/noco/rest/ui/auth/signin.ts
@@ -1,7 +1,7 @@
 export default `<!DOCTYPE html>
 <html>
 <head>
-    <title>Welcome to Vue</title>
+    <title>NocoDB - Sign In</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">

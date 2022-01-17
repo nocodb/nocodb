1610467fe5100416b2254cae4282abd707a91918
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-01-17 23:50:17 +0800

    fix: use to boolean instead of string
    Signed-off-by: Wing-Kam Wong <wingkwong.code@gmail.com>

--- packages/nocodb/src/plugins/mino/Minio.ts
+++ packages/nocodb/src/plugins/mino/Minio.ts
@@ -60,7 +60,7 @@ export default class Minio implements IStorageAdapter {
   public async init(): Promise<any> {
     // todo:  update in ui(checkbox and number field)
     this.input.port = +this.input.port || 9000;
-    this.input.useSSL = this.input.useSSL ==='true';
+    this.input.useSSL = this.input.useSSL === true;
     this.input.accessKey = this.input.access_key;
     this.input.secretKey = this.input.access_secret;
 

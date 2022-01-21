9dae25d141bd288f900706cae1573eb1348efcf4
Pranav C <pranavxc@gmail.com>
2022-01-21 11:57:48 +0530

    feat: add rows count in footer
    re #946
    
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui/components/project/spreadsheet/components/pagination.vue
+++ packages/nc-gui/components/project/spreadsheet/components/pagination.vue
@@ -1,32 +1,41 @@
 <template>
-  <v-pagination
-    v-if="count !== Infinity"
-    v-model="page"
-    style="max-width: 100%"
-    :length="Math.ceil(count / size)"
-    :total-visible="8"
-    color="primary lighten-2"
-    class="nc-pagination"
-    @input="$emit('input',page)"
-  />
-  <div v-else class="mx-auto d-flex align-center mt-n1 " style="max-width:250px">
-    <span class="caption" style="white-space: nowrap"> Change page:</span>
-    <v-text-field
+  <div class="d-flex align-center">
+    <v-spacer />
+
+    <span v-if="count && count > 1 && count !== Infinity" class="caption ml-2">Total {{ count }} rows found</span>
+    <v-spacer />
+    <v-pagination
+      v-if="count !== Infinity"
       v-model="page"
-      class="ml-1 caption"
-      :full-width="false"
-      outlined
-      dense
-      hide-details
-      type="number"
-      @keydown.enter="$emit('input',page)"
-    >
-      <template #append>
-        <x-icon tooltip="Change page" small icon.class="mt-1" @click="$emit('input',page)">
-          mdi-keyboard-return
-        </x-icon>
-      </template>
-    </v-text-field>
+      style="max-width: 100%"
+      :length="Math.ceil(count / size)"
+      :total-visible="8"
+      color="primary lighten-2"
+      class="nc-pagination"
+      @input="$emit('input',page)"
+    />
+    <div v-else class="mx-auto d-flex align-center mt-n1 " style="max-width:250px">
+      <span class="caption" style="white-space: nowrap"> Change page:</span>
+      <v-text-field
+        v-model="page"
+        class="ml-1 caption"
+        :full-width="false"
+        outlined
+        dense
+        hide-details
+        type="number"
+        @keydown.enter="$emit('input',page)"
+      >
+        <template #append>
+          <x-icon tooltip="Change page" small icon.class="mt-1" @click="$emit('input',page)">
+            mdi-keyboard-return
+          </x-icon>
+        </template>
+      </v-text-field>
+    </div>
+
+    <v-spacer />
+    <v-spacer />
   </div>
 </template>
 

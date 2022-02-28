bac9cae3ad5ac5783ca5c27ff15d8d86c28ede4f
Vijay Kumar Rathore <professional.vijay8492@gmail.com>
2022-02-28 12:18:47 +0100

    add terraform action

--- scripts/upgradeNcGui.js
+++ scripts/upgradeNcGui.js
@@ -7,7 +7,7 @@ const execSync = require('child_process').execSync;
 const ncLibPackage = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'packages', 'nc-lib-gui', 'package.json')))
 
 // upgrade nc-lib-gui version in nocodb
-execSync(`cd packages/nocodb && npm install --save --save-exact nc-lib-gui@${ncLibPackage.version}`, {});
+execSync(`cd packages/nocodb && npm install --save --save-exact nc-lib-gui@npm:finn-nc-lib-gui@${ncLibPackage.version}`, {});
 
 // const nocodbPackageFilePath = path.join(__dirname, '..', 'packages', 'nocodb', 'package.json')
 // const nocoLibPackage = JSON.parse(fs.readFileSync(nocodbPackageFilePath))

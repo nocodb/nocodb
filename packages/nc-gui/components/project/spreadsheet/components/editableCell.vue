277a031d26f4be333bc6dcc950a8caa161c239ba
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-03-19 18:29:26 +0800

    fix: remove unnecessary update listener - ref: #1312
    Signed-off-by: Wing-Kam Wong <wingkwong.code@gmail.com>

--- packages/nc-gui/components/project/spreadsheet/components/editableCell.vue
+++ packages/nc-gui/components/project/spreadsheet/components/editableCell.vue
@@ -203,9 +203,6 @@ export default {
         $listeners.cancel = this.$listeners.cancel
       }
 
-      if (this.$listeners.update) {
-        $listeners.update = this.$listeners.update
-      }
 
       return $listeners
     }

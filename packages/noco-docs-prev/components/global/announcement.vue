b13839ee2d4f71755f4e8b06dc7575707111d2ab
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-04-04 16:20:54 +0800

    docs: rm link break
    Signed-off-by: Wing-Kam Wong <wingkwong.code@gmail.com>

--- packages/noco-docs-prev/components/global/announcement.vue
+++ packages/noco-docs-prev/components/global/announcement.vue
@@ -1,8 +1,6 @@
 <template>
     <alert type="warning">
-    If you are using NocoDB with version 0.90 onwards, please check out the latest documentation <a href="#" target="_blank">here</a> instead. <br/> 
-    The documentation for previous versions is no longer maintained and will be completely removed on 30 Jun 2022. <br/> 
-    See <a href="https://github.com/nocodb/nocodb/issues/1564" target="_blank">v0.90 Important Announcement</a> for more.
+    If you are using NocoDB with version 0.90 onwards, please check out the latest documentation <a href="#" target="_blank">here</a> instead. The documentation for previous versions is no longer maintained and will be completely removed on 30 Jun 2022. See <a href="https://github.com/nocodb/nocodb/issues/1564" target="_blank">v0.90 Important Announcement</a> for more.
     </alert>
 </template>
 

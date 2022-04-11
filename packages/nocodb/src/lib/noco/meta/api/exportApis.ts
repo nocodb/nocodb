4f76ebffcd6f6428e5f11b620480c59f5c9c00bc
Pranav C <pranavxc@gmail.com>
2022-04-11 17:21:26 +0530

    wip
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb/src/lib/noco/meta/api/exportApis.ts
+++ packages/nocodb/src/lib/noco/meta/api/exportApis.ts
@@ -1,15 +1,10 @@
 import { Request, Response, Router } from 'express';
 import View from '../../../noco-models/View';
-import Model from '../../../noco-models/Model';
-import Base from '../../../noco-models/Base';
-import NcConnectionMgrv2 from '../../common/NcConnectionMgrv2';
-import { nocoExecute } from 'nc-help';
 import papaparse from 'papaparse';
-import { isSystemColumn, UITypes } from 'nocodb-sdk';
+import { isSystemColumn } from 'nocodb-sdk';
 import Column from '../../../noco-models/Column';
-import LinkToAnotherRecordColumn from '../../../noco-models/LinkToAnotherRecordColumn';
-import LookupColumn from '../../../noco-models/LookupColumn';
 import ncMetaAclMw from '../helpers/ncMetaAclMw';
+import { extractCsvData } from './dataApis/helpers';
 
 async function exportCsv(req: Request, res: Response, next) {
   const view = await View.get(req.params.viewId);
@@ -26,67 +21,7 @@ async function exportCsv(req: Request, res: Response, next) {
     .filter(column => !isSystemColumn(column) || view.show_system_fields);
 
   if (!model) return next(new Error('Table not found'));
-
-  const base = await Base.get(model.base_id);
-  const baseModel = await Model.getBaseModelSQL({
-    id: model.id,
-    viewId: view?.id,
-    dbDriver: NcConnectionMgrv2.get(base)
-  });
-
-  const key = `${model.title}List`;
-  const requestObj = {
-    [key]: await baseModel.defaultResolverReq(req.query, false, false)
-  };
-
-  let offset = +req.query.offset || 0;
-  const limit = 100;
-  // const size = +process.env.NC_EXPORT_MAX_SIZE || 1024;
-  const timeout = +process.env.NC_EXPORT_MAX_TIMEOUT || 5000;
-  const csvRows = [];
-  const startTime = process.hrtime();
-  let elapsed, temp;
-
-  for (
-    elapsed = 0;
-    elapsed < timeout;
-    offset += limit,
-      temp = process.hrtime(startTime),
-      elapsed = temp[0] * 1000 + temp[1] / 1000000
-  ) {
-    console.time('nocoExecute');
-    const rows = (
-      await nocoExecute(
-        requestObj,
-        {
-          [key]: async args => {
-            return await baseModel.list({ ...args, offset, limit });
-          }
-        },
-        {},
-        { nested: { [key]: req.query } }
-      )
-    )?.[key];
-
-    if (!rows?.length) {
-      offset = -1;
-      break;
-    }
-
-    for (const row of rows) {
-      const csvRow = { ...row };
-
-      for (const column of view.model.columns) {
-        if (isSystemColumn(column) && !view.show_system_fields) continue;
-        csvRow[column.title] = await serializeCellValue({
-          value: row[column.title],
-          column
-        });
-      }
-      csvRows.push(csvRow);
-    }
-    console.timeEnd('nocoExecute');
-  }
+  const { offset, csvRows, elapsed } = await extractCsvData(model, view, req);
 
   const data = papaparse.unparse(
     {
@@ -105,71 +40,6 @@ async function exportCsv(req: Request, res: Response, next) {
     'Content-Disposition': `attachment; filename="${view.title}-export.csv"`
   });
   res.send(data);
-}
-
-async function serializeCellValue({
-  value,
-  column
-}: {
-  column?: Column;
-  value: any;
-}) {
-  if (!column) {
-    return value;
-  }
-
-  if (!value) return value;
-
-  switch (column?.uidt) {
-    case UITypes.Attachment: {
-      let data = value;
-      try {
-        if (typeof value === 'string') {
-          data = JSON.parse(value);
-        }
-      } catch {}
-
-      return (data || []).map(
-        attachment =>
-          `${encodeURI(attachment.title)}(${encodeURI(attachment.url)})`
-      );
-    }
-    case UITypes.Lookup:
-      {
-        const colOptions = await column.getColOptions<LookupColumn>();
-        const lookupColumn = await colOptions.getLookupColumn();
-        return (
-          await Promise.all(
-            [...(Array.isArray(value) ? value : [value])].map(async v =>
-              serializeCellValue({
-                value: v,
-                column: lookupColumn
-              })
-            )
-          )
-        ).join(', ');
-      }
-      break;
-    case UITypes.LinkToAnotherRecord:
-      {
-        const colOptions = await column.getColOptions<
-          LinkToAnotherRecordColumn
-        >();
-        const relatedModel = await colOptions.getRelatedTable();
-        await relatedModel.getColumns();
-        return [...(Array.isArray(value) ? value : [value])]
-          .map(v => {
-            return v[relatedModel.primaryValue?.title];
-          })
-          .join(', ');
-      }
-      break;
-    default:
-      if (value && typeof value === 'object') {
-        return JSON.stringify(value);
-      }
-      return value;
-  }
 }
 
 const router = Router({ mergeParams: true });

74ee83b94ce9c4bfefbb3b9b01e2f4790c87341e
Raju Udava <86527202+dstala@users.noreply.github.com>
2022-04-13 10:11:14 +0530

    sync: migration from nocodb/nc
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

---
+++ packages/nocodb/tests/sync/source.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+  title: 'source_type',
+
+  version: '',
+
+  init: function (src, dest) {
+
+  },
+
+  TablesGet: function () {
+
+  },
+
+  ColumnsGet: function () {
+
+  },
+
+  RelationalColumnsGet: function () {
+
+  },
+
+  VirtualColumnsGet: function () {
+
+  },
+
+  SchemaGet: function () {
+
+  },
+
+  SchemaCreate: function () {
+
+  },
+
+  DataExport: function () {
+
+  },
+
+  DataWatch: function () {
+    /**
+     * Fresh load = schema + data
+     * Reload = clean the data + reload data
+     * Real time sync
+     *    airtable to nocodb
+     *    nocodb to airtable
+     */
+  }
+
+
+
+}

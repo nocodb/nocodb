255e62da4b64164f208c49a5ffadfa9068ccfca4
mertmit <mertmit99@gmail.com>
2022-04-16 03:58:58 +0300

    feat: add default currency locale and code
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/components/project/spreadsheet/components/cell/currencyCell.vue
+++ packages/nc-gui/components/project/spreadsheet/components/cell/currencyCell.vue
@@ -10,8 +10,8 @@ export default {
   computed: {
     currency() {
       try {
-        return new Intl.NumberFormat(this.column.colOptions.currency_locale,
-          { style: 'currency', currency: this.column.colOptions.currency_code }).format(this.value)
+        return new Intl.NumberFormat(this.column.colOptions?.currency_locale || 'en-US',
+          { style: 'currency', currency: this.column.colOptions?.currency_code || 'USD' }).format(this.value)
       } catch (e) {
         return this.value
       }

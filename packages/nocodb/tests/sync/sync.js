01e81b52e7143451e49d0e2ac74ce7c781efbf4a
Raju Udava <86527202+dstala@users.noreply.github.com>
2022-04-21 22:35:07 +0530

    sync: ltar (wip)
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- packages/nocodb/tests/sync/sync.js
+++ packages/nocodb/tests/sync/sync.js
@@ -13,7 +13,7 @@ const api = new Api({
   baseURL: 'http://localhost:8080',
   headers: {
     'xc-auth':
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAbm9jb2RiLmNvbSIsImZpcnN0bmFtZSI6bnVsbCwibGFzdG5hbWUiOm51bGwsImlkIjoidXNfYXZmd2Z4bmNsaGQzcG8iLCJyb2xlcyI6InVzZXIsc3VwZXIiLCJpYXQiOjE2NTAyODIxMzh9.WRXsAjEk9-tF_TGk5gOLhD8S-4TURAZZtPDAumj4M7c'
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAbm9jb2RiLmNvbSIsImZpcnN0bmFtZSI6bnVsbCwibGFzdG5hbWUiOm51bGwsImlkIjoidXNfN2NuZmpxMGt5NjczaXkiLCJyb2xlcyI6InVzZXIsc3VwZXIiLCJpYXQiOjE2NTA0NzM3MTl9.VVZKink3FSpajxnfaTVPn2iuCNH3lTjepNQNb4Q8VOE'
   }
 });
 
@@ -21,7 +21,9 @@ const api = new Api({
 let aTblSchema = {};
 
 function getAtableSchema() {
-  let file = jsonfile.readFileSync('./t0v0.json');
+  // let file = jsonfile.readFileSync('./t0v0.json');
+  let file = jsonfile.readFileSync('./ltar.json');
+
   // store copy of atbl schema globally
   aTblSchema = file.tableSchemas;
   return file;
@@ -77,8 +79,37 @@ function aTbl_getColumnName(colId) {
   }
 }
 
-// Nc: retrieve column datatype from column-name
+// nc dump schema
 //
+async function nc_DumpTableSchema() {
+  console.log('[')
+  let ncTblList = await api.dbTable.list(ncCreatedProjectSchema.id)
+  for(let i=0; i<ncTblList.list.length; i++) {
+    let ncTbl = await api.dbTable.read(ncTblList.list[i].id)
+    console.log(JSON.stringify(ncTbl, null, 2))
+    console.log(',')
+  }
+  console.log(']')
+}
+
+// retrieve nc column schema from using aTbl field ID as reference
+//
+async function nc_getColumnSchema(aTblFieldId) {
+  let ncTblList = await api.dbTable.list(ncCreatedProjectSchema.id);
+  let aTblField = aTbl_getColumnName(aTblFieldId);
+  let ncTblId = ncTblList.list.filter(x => x.title === aTblField.tn)[0].id;
+  let ncTbl = await api.dbTable.read(ncTblId);
+  let ncCol = ncTbl.columns.find(x => x.title === aTblField.cn);
+  return ncCol;
+}
+
+// retrieve nc table schema using table name
+async function nc_getTableSchema(tableName) {
+  let ncTblList = await api.dbTable.list(ncCreatedProjectSchema.id);
+  let ncTblId = ncTblList.list.filter(x => x.title === tableName)[0].id;
+  let ncTbl = await api.dbTable.read(ncTblId);
+  return ncTbl;
+}
 
 
 // map UIDT
@@ -100,57 +131,26 @@ function getNocoType(col) {
     case 'number':
       // kludge: currency validation error with decimal places
       if (col.typeOptions?.format === 'percentV2') ncType = UITypes.Percent;
-      else if (col.typeOptions?.format === 'duration') ncType = UITypes.Duration;
-      else if (col.typeOptions?.format === 'currency') ncType = UITypes.Currency;
+      else if (col.typeOptions?.format === 'duration')
+        ncType = UITypes.Duration;
+      else if (col.typeOptions?.format === 'currency')
+        ncType = UITypes.Currency;
       break;
 
     case 'formula':
-      if (col.typeOptions?.formulaTextParsed === 'CREATED_TIME()') ncType = UITypes.CreateTime;
-      else if (col.typeOptions?.formulaTextParsed === 'LAST_MODIFIED_TIME()') ncType = UITypes.LastModifiedTime;
+      if (col.typeOptions?.formulaTextParsed === 'CREATED_TIME()')
+        ncType = UITypes.CreateTime;
+      else if (col.typeOptions?.formulaTextParsed === 'LAST_MODIFIED_TIME()')
+        ncType = UITypes.LastModifiedTime;
       break;
-    /**
-    case 'foreignKey':
-      break;
-    case 'multilineText':
-      break;
-    case 'multipleAttachment':
-      break;
-    case 'checkbox':
-      break;
-    case 'multiSelect':
-      break;
-    case 'select':
-      break;
-    case 'collaborator':
-      break;
-    case 'date':
-      break;
-    case 'phone':
-      break;
-    case 'rating':
-      break;
-    case 'rollup':
-      break;
-    case 'count':
-      break;
-    case 'lookup':
-      break;
-    case 'computation':
-      break;
-    case 'autoNumber':
-      break;
-    case 'barcode':
-      break;
-    case 'button':
-      break;
- **/
   }
 
   return ncType;
 }
 
+// retrieve additional options associated with selected data types
+//
 function getNocoTypeOptions(col) {
-
   switch (col.type) {
     case 'select':
     case 'multiSelect':
@@ -163,24 +163,6 @@ function getNocoTypeOptions(col) {
       }
       let csvOpt = "'" + opt.join("','") + "'";
       return { type: 'select', data: csvOpt };
-
-    // case `foreignKey`:
-    //   return {
-    //     type: 'manyToMany',
-    //     data: aTbl_getTableName(tOpt.foreignTableId)
-    //   };
-    // case `lookup`:
-    //   return {
-    //     type: 'lookup',
-    //     data: aTbl_getColumnName(tOpt.foreignTableRollupColumnId)
-    //   };
-    // case `rollup`:
-    //   let fnValue = findRollupAggregatorMap(tOpt.formulaTextParsed);
-    //   return {
-    //     type: 'rollup',
-    //     data: aTbl_getColumnName(tOpt.foreignTableRollupColumnId),
-    //     fn: fnValue
-    //   };
 
     default:
       return { type: undefined };
@@ -198,14 +180,19 @@ function tablesPrepare(tblSchema) {
     table.table_name = tblSchema[i].name;
 
     // insert record_id of type ID by default
-    table.columns = [{
-      title: 'record_id',
-      column_name: 'record_id',
-      uidt: UITypes.ID
-    }];
+    table.columns = [
+      {
+        title: 'record_id',
+        column_name: 'record_id',
+        uidt: UITypes.ID
+      }
+    ];
 
     for (let j = 0; j < tblSchema[i].columns.length; j++) {
       let col = tblSchema[i].columns[j];
+
+      // skip link, lookup, rollup fields in this iteration
+      if (['foreignKey', 'lookup', 'rollup'].includes(col.type)) continue;
 
       // base column schema
       // kludge: error observed in Nc with space around column-name
@@ -235,9 +222,9 @@ function tablesPrepare(tblSchema) {
   return tables;
 }
 
-// async function tablesCreate() {}
 
 async function nocoCreateSchema(srcSchema) {
+  // base schema preparation: exclude
   let tables = tablesPrepare(srcSchema.tableSchemas);
 
   // debug
@@ -247,7 +234,6 @@ async function nocoCreateSchema(srcSchema) {
 
 //////////  Data processing
 
-
 // https://www.airtable.com/app1ivUy7ba82jOPn/api/docs#javascript/metadata
 let Airtable = require('airtable');
 let base = new Airtable({ apiKey: syncDB.airtable.apiKey }).base(
@@ -255,30 +241,25 @@ let base = new Airtable({ apiKey: syncDB.airtable.apiKey }).base(
 );
 
 async function nocoReadData(table) {
-
-  // console.log(table)
-
   base(table.title)
     .select({
       pageSize: 2,
-      maxRecords:2,
+      maxRecords: 2,
       view: 'Grid view'
     })
     .eachPage(
       function page(records, fetchNextPage) {
+        // console.log(JSON.stringify(records, null, 2));
 
-        console.log(`>>>>>>>>>>>>>>>>>>>>>>>>>>>`)
-        console.log(JSON.stringify(records, null, 2));
         // This function (`page`) will get called for each page of records.
         records.forEach(function(record) {
-
-          (async()=>{
-            let rec = record.fields
+          (async () => {
+            let rec = record.fields;
 
             // kludge -
             // trim spaces on either side of column name
             // leads to error in NocoDB
-            Object.keys(rec).forEach((key) => {
+            Object.keys(rec).forEach(key => {
               let replacedKey = key.trim();
               if (key !== replacedKey) {
                 rec[replacedKey] = rec[key];
@@ -288,20 +269,23 @@ async function nocoReadData(table) {
 
             // post-processing on the record
             for (const [key, value] of Object.entries(rec)) {
-
               // retrieve datatype
-              let dt = table.columns.find(x => x.title === key).uidt
+              let dt = table.columns.find(x => x.title === key).uidt;
 
               // https://www.npmjs.com/package/validator
               // default value: digits_after_decimal: [2]
               // if currency, set decimal place to 2
               //
-              if(dt === 'Currency')
-                rec[key] = value.toFixed(2)
+              if (dt === 'Currency') rec[key] = value.toFixed(2);
             }
 
             // bulk Insert
-            let returnValue = await api.dbTableRow.bulkCreate('nc', 'sample-4', table.title, [rec])
+            let returnValue = await api.dbTableRow.bulkCreate(
+              'nc',
+              'sample-4',
+              table.title,
+              [rec]
+            );
           })().catch(e => {
             console.log(e);
           });
@@ -321,45 +305,327 @@ async function nocoReadData(table) {
 }
 
 //////////
+// holds response for all created tables
+let ncCreatedProjectSchema = [];
+let ncLinkMappingTable = [];
+
+function nc_isLinkExists(atblFieldId) {
+  if(ncLinkMappingTable.find(x => x.aTbl.typeOptions.symmetricColumnId === atblFieldId))
+    return true;
+  return false;
+}
 
 (async () => {
   // read schema file
   const schema = getAtableSchema();
+  let aTblSchema = schema.tableSchemas;
+
+  // create empty project (XC-DB)
+  ncCreatedProjectSchema = await api.project.create({
+    title: 'sample-4'
+  });
 
   // prepare table schema
   let ncTblSchema = await nocoCreateSchema(schema);
 
-  // create empty project (XC-DB)
-  let project = await api.project.create({
-    title: 'sample-4'
-  });
-
-  // console.log(project)
-  // console.log(project.id, project.bases[0].id);
-
-  // for each table schema,
-  //  a. create table
-  //  b. populate data using airtable data APIs
+  // for each table schema, create nc table
   for (let idx = 0; idx < ncTblSchema.length; idx++) {
     let table = await api.dbTable.create(
-      project.id,
+      ncCreatedProjectSchema.id,
       ncTblSchema[idx]
     );
-    // console.log(table);
-
-    // read data
-    await nocoReadData(table)
   }
+
+  // Link to another RECORD
+  for (let idx = 0; idx < aTblSchema.length; idx++) {
+    let aTblLinkColumns = aTblSchema[idx].columns.filter(
+      x => x.type === 'foreignKey'
+    );
+
+    // Link columns exist
+    //
+    if (aTblLinkColumns.length) {
+      for (let i = 0; i < aTblLinkColumns.length; i++) {
+
+        // check if link already established?
+        if(!nc_isLinkExists(aTblLinkColumns[i].id)) {
+
+          // parent table ID
+          let srcTableId = (await nc_getTableSchema(aTblSchema[idx].name)).id;
+
+          // find child table name from symmetric column ID specified
+          let childTable = aTbl_getColumnName(
+            aTblLinkColumns[i].typeOptions.symmetricColumnId
+          );
+
+          // retrieve child table ID (nc) from table name
+          let childTableId = (await nc_getTableSchema(childTable.tn)).id;
+
+          // create link
+          let column = await api.dbTableColumn.create(srcTableId, {
+            uidt: 'LinkToAnotherRecord',
+            title: aTblLinkColumns[i].name,
+            parentId: srcTableId,
+            childId: childTableId,
+            type: aTblLinkColumns[i].typeOptions.relationship === 'many'
+                ? 'mm'
+                : 'hm'
+          });
+
+          // clean up
+          let link = {
+            nc: {
+              title: aTblLinkColumns[i].name,
+              parentId: srcTableId,
+              childId: childTableId,
+              type: 'hm'
+            },
+            aTbl: {
+              tblId: aTblSchema[idx].id,
+              ...aTblLinkColumns[i]
+            }
+          };
+
+          ncLinkMappingTable.push(link);
+        } else {
+
+          // extract associated link information
+          let x = ncLinkMappingTable.findIndex(
+            x =>
+              x.aTbl.tblId === aTblLinkColumns[i].typeOptions.foreignTableId &&
+              x.aTbl.id ===
+                aTblLinkColumns[i].typeOptions.symmetricColumnId
+          );
+
+          let childTblSchema = await api.dbTable.read(
+            ncLinkMappingTable[x].nc.childId
+          );
+          let parentTblSchema = await api.dbTable.read(
+            ncLinkMappingTable[x].nc.parentId
+          );
+
+          let parentLinkColumn = parentTblSchema.columns.find(
+            col => col.title === ncLinkMappingTable[x].nc.title
+          );
+
+          let childLinkColumn = {};
+          if (parentLinkColumn.colOptions.type == 'hm') {
+            childLinkColumn = childTblSchema.columns.find(
+              col =>
+                col.uidt === 'LinkToAnotherRecord' &&
+                col.colOptions.fk_child_column_id ===
+                  parentLinkColumn.colOptions.fk_child_column_id &&
+                col.colOptions.fk_parent_column_id ===
+                  parentLinkColumn.colOptions.fk_parent_column_id
+            );
+          } else {
+            childLinkColumn = childTblSchema.columns.find(
+              col =>
+                col.uidt === 'LinkToAnotherRecord' &&
+                col.colOptions.fk_child_column_id ===
+                  parentLinkColumn.colOptions.fk_parent_column_id &&
+                col.colOptions.fk_parent_column_id ===
+                  parentLinkColumn.colOptions.fk_child_column_id
+            );
+          }
+
+          await api.dbTableColumn.update(childLinkColumn.id, {
+            ...childLinkColumn,
+            title: aTblLinkColumns[i].name
+          });
+        }
+      }
+    }
+  }
+
+  // LookUps
+  for (let idx = 0; idx < aTblSchema.length; idx++) {
+    let aTblColumns = aTblSchema[idx].columns.filter(x => x.type === 'lookup');
+
+    // parent table ID
+    let srcTableId = (await nc_getTableSchema(aTblSchema[idx].name)).id;
+
+    if (aTblColumns.length) {
+      // Lookup
+      for (let i = 0; i < aTblColumns.length; i++) {
+        let ncRelationColumn = await nc_getColumnSchema(
+          aTblColumns[i].typeOptions.relationColumnId
+        );
+        let ncLookupColumn = await nc_getColumnSchema(
+          aTblColumns[i].typeOptions.foreignTableRollupColumnId
+        );
+
+        // console.log('xxx', ncRelationColumnId, ncLookupColumnId)
+
+        let lookupColumn = await api.dbTableColumn.create(srcTableId, {
+          uidt: 'Lookup',
+          title: aTblColumns[i].name,
+          fk_relation_column_id: ncRelationColumn.id,
+          fk_lookup_column_id: ncLookupColumn.id
+        });
+      }
+    }
+  }
+
+  // Rollups
+  for (let idx = 0; idx < aTblSchema.length; idx++) {
+    let aTblColumns = aTblSchema[idx].columns.filter(x => x.type === 'rollup');
+
+    // parent table ID
+    let srcTableId = (await nc_getTableSchema(aTblSchema[idx].name)).id;
+
+    if (aTblColumns.length) {
+      // rollup exist
+      for (let i = 0; i < aTblColumns.length; i++) {
+        let ncRelationColumn = await nc_getColumnSchema(
+          aTblColumns[i].typeOptions.relationColumnId
+        );
+        let ncRollupColumn = await nc_getColumnSchema(
+          aTblColumns[i].typeOptions.foreignTableRollupColumnId
+        );
+
+        let lookupColumn = await api.dbTableColumn.create(srcTableId, {
+          uidt: 'Rollup',
+          title: aTblColumns[i].name,
+          fk_relation_column_id: ncRelationColumn.id,
+          fk_rollup_column_id: ncRollupColumn.id,
+          rollup_function: 'sum' // fix me: hardwired
+        });
+      }
+    }
+  }
+
+  // await nc_DumpTableSchema();
+
+  // read data
+  // await nocoReadData(table)
 
 })().catch(e => {
   console.log(e);
 });
 
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 // Scratch pad
+
+
 // await api.dbTableRow.bulkInsert('nc', 'x', 'x', [{Title: 'abc'}, {Title: 'abc'}, {Title: 'abc'}])
 // await api.data.bulkInsert();
 // let column = await api.meta.columnCreate('md_vnesap07k24lku', {
 //   uidt: UITypes.SingleLineText,
 //   cn: 'col-1',
 // })
+
+
+// // t0 schema
+// let t0 = {
+//   table_name: 't0',
+//   columns: [
+//     {
+//       title: 'record_id',
+//       column_name: 'record_id',
+//       uidt: 'ID'
+//     },
+//     {
+//       title: 'Segment1',
+//       column_name: 'Segment',
+//       uidt: 'LongText'
+//     }
+//   ]
+// };
+// let t1 = {
+//   table_name: 't1',
+//   columns: [
+//     {
+//       title: 'record_id',
+//       column_name: 'record_id',
+//       uidt: 'ID'
+//     },
+//     {
+//       title: 'Segment2',
+//       column_name: 'Segment',
+//       uidt: 'LongText'
+//     }
+//   ]
+// };
+
+// (async () => {
+//   return;
+//
+//   // create empty project (XC-DB)
+//   let project = await api.project.create({
+//     title: 'sample-a'
+//   });
+//
+//   let table_1 = await api.dbTable.create(project.id, t0);
+//
+//   let table_2 = await api.dbTable.create(project.id, t1);
+//
+//   // console.log(project, table_1, table_2)
+//
+//   let LTAR = await api.dbTableColumn.create(table_1.id, {
+//     uidt: 'LinkToAnotherRecord',
+//     title: 'LinkField',
+//     parentId: table_1.id,
+//     childId: table_2.id,
+//     type: 'hm'
+//   });
+//
+//   // console.log(LTAR)
+//
+//   let lookupColumn = await api.dbTableColumn.create(table_1.id, {
+//     uidt: 'Lookup',
+//     title: 'LookUP Field',
+//     fk_relation_column_id: LTAR.columns.find(o => o.title === 'LinkField').id,
+//     fk_lookup_column_id: table_2.columns.find(o => o.title === 'Segment2').id
+//   });
+//
+//   let rollupColumn = await api.dbTableColumn.create(table_1.id, {
+//     uidt: 'Rollup',
+//     title: 'RollUP Field',
+//     fk_relation_column_id: LTAR.columns.find(o => o.title === 'LinkField').id,
+//     fk_rollup_column_id: table_2.columns.find(o => o.title === 'Segment2').id,
+//     rollup_function: 'count'
+//   });
+//
+//   await api.dbTableRow.create('noco', project.title, table_2.title, {
+//     Segment2: 'tbl-0 record 1'
+//   });
+//   await api.dbTableRow.create('noco', project.title, table_2.title, {
+//     Segment2: 'tbl-0 record 2'
+//   });
+//   await api.dbTableRow.create('noco', project.title, table_2.title, {
+//     Segment2: 'tbl-0 record 3'
+//   });
+//
+//   await api.dbTableRow.create('noco', project.title, table_1.title, {
+//     Segment1: 'tbl-1 record 1'
+//   });
+//   await api.dbTableRow.nestedAdd(
+//     'noco',
+//     project.title,
+//     table_1.title,
+//     '1',
+//     'mm',
+//     'LinkField',
+//     '1'
+//   );
+//   await api.dbTableRow.nestedAdd(
+//     'noco',
+//     project.title,
+//     table_1.title,
+//     '1',
+//     'mm',
+//     'LinkField',
+//     '2'
+//   );
+// })().catch(e => {
+//   console.log(e);
+// });

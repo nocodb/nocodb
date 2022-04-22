d77a5f1c6d1f7ee686383e3ddaf0dcee5b09f8d8
mertmit <mertmit99@gmail.com>
2022-04-22 20:08:00 +0300

    fix: pg date formatting
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/components/project/spreadsheet/components/virtualCell/formulaCell.vue
+++ packages/nc-gui/components/project/spreadsheet/components/virtualCell/formulaCell.vue
@@ -11,19 +11,27 @@
   </v-tooltip>
   <div v-else-if="urls" v-html="urls" />
   <div v-else>
-    {{ row[column.title] }}
+    {{ result }}
   </div>
 </template>
 
 <script>
+import dayjs from 'dayjs'
+
 export default {
   name: 'FormulaCell',
-  props: { column: Object, row: Object },
+  props: { column: Object, row: Object, client: String },
   computed: {
+    result() {
+      if (this.client === 'pg') {
+        return this.handleTZ(this.row[this.column.title])
+      }
+      return this.row[this.column.title]
+    },
     urls() {
       if (!this.row[this.column.title]) { return }
 
-      const rawText = this.row[this.column.title].toString()
+      const rawText = this.result.toString()
       let found = false
       const out = rawText.replace(/URI::\((.*?)\)/g, (_, url) => {
         found = true
@@ -35,6 +43,15 @@ export default {
       })
 
       return found && out
+    }
+  },
+  methods: {
+    // handle date returned from PostgreSQL
+    handleTZ(val) {
+      if (!val) { return }
+      return val.replace(/((?:-?(?:[1-9][0-9]*)?[0-9]{4})-(?:1[0-2]|0[1-9])-(?:3[01]|0[1-9]|[12][0-9])T(?:2[0-3]|[01][0-9]):(?:[0-5][0-9]):(?:[0-5][0-9])(?:\.[0-9]+)?(?:Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9]))/g, (i, v) => {
+        return dayjs(v).format('YYYY-MM-DD HH:mm')
+      })
     }
   }
 }

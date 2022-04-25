cdef167078cfcb57e1595fedb6f6616850720a07
Pranav C <pranavxc@gmail.com>
2022-04-26 01:20:33 +0530

    fix: ignore date formatting if type is not string
    re #1864
    
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui/components/project/spreadsheet/components/virtualCell/formulaCell.vue
+++ packages/nc-gui/components/project/spreadsheet/components/virtualCell/formulaCell.vue
@@ -49,6 +49,7 @@ export default {
     // handle date returned from PostgreSQL
     handleTZ(val) {
       if (!val) { return }
+      if (typeof val !== 'string') { return val }
       return val.replace(/((?:-?(?:[1-9][0-9]*)?[0-9]{4})-(?:1[0-2]|0[1-9])-(?:3[01]|0[1-9]|[12][0-9])T(?:2[0-3]|[01][0-9]):(?:[0-5][0-9]):(?:[0-5][0-9])(?:\.[0-9]+)?(?:Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9]))/g, (i, v) => {
         return dayjs(v).format('YYYY-MM-DD HH:mm')
       })

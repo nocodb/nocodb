2ec643102c7b5676dd93b3805b2b3e2b6861e270
Pranav C <pranavxc@gmail.com>
2022-05-12 23:46:31 +0530

    chore: add migration to convert column types
    re #2012
    
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb/src/lib/noco/common/XcMigrationSourcev2.ts
+++ packages/nocodb/src/lib/noco/common/XcMigrationSourcev2.ts
@@ -1,5 +1,6 @@
 import * as nc_011 from '../migrationsv2/nc_011';
 import * as nc_012_alter_column_data_types from '../migrationsv2/nc_012_alter_column_data_types';
+import * as nc_013_alter_column_data_types from '../migrationsv2/nc_013_alter_column_data_types';
 import * as nc_013_sync_source from '../migrationsv2/nc_013_sync_source';
 
 // Create a custom migration source class
@@ -12,7 +13,8 @@ export default class XcMigrationSourcev2 {
     return Promise.resolve([
       'nc_011',
       'nc_012_alter_column_data_types',
-      'nc_013_sync_source'
+      'nc_013_sync_source',
+      'nc_013_alter_column_data_types'
     ]);
   }
 
@@ -28,6 +30,8 @@ export default class XcMigrationSourcev2 {
         return nc_012_alter_column_data_types;
       case 'nc_013_sync_source':
         return nc_013_sync_source;
+      case 'nc_013_alter_column_data_types':
+        return nc_013_alter_column_data_types;
     }
   }
 }

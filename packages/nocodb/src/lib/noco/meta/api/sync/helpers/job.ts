2ad653a2a35c9662398bee232fbce91f21020b20
Raju Udava <86527202+dstala@users.noreply.github.com>
2022-05-19 20:03:33 +0530

    fix: replace special characters in column name with an _
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- packages/nocodb/src/lib/noco/meta/api/sync/helpers/job.ts
+++ packages/nocodb/src/lib/noco/meta/api/sync/helpers/job.ts
@@ -162,16 +162,8 @@ export default async (
   //
 
   function nc_sanitizeName(name) {
-    // knex complains use of '?' in field name
-    // good to replace all special characters by _ in one go
-
-    // https://stackoverflow.com/questions/18862256/how-to-detect-emoji-using-javascript
-    const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
-
-    return name
-      .replace(/[?.\s]/g, '_')
-      .replace(regex, '_')
-      .trim();
+    // replace all special characters by _
+    return name.replace(/\W+/g, '_').trim();
   }
 
   function nc_getSanitizedColumnName(table, name) {

1631594aafa4214277a7ff557af33438ac1f7338
mertmit <mertmit99@gmail.com>
2022-05-24 09:21:18 +0300

    feat/fix: currency column (#1687)
    * feat: currency column supporting multiple locales and units
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * feat: add default currency locale and code
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * feat: change default length and scale for currency
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * feat: add proper validations for currency fields
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * fix: use UITypes instead of raw uidt
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * feat: dropdown for currency properties
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * fix: add proper v2 migrations
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * fix: add custom validators
    
    Signed-off-by: Mert Ersoy <mertmit99@gmail.com>
    
    * feat: handle currency data from meta column
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * fix: move currency specific options out
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * fix: pass colMeta by value instead of reference
    
    Signed-off-by: mertmit <mertmit99@gmail.com>

---
+++ packages/nc-gui/components/project/spreadsheet/components/cell/CurrencyCell.vue
@@ -0,0 +1,37 @@
+<template>
+  <a v-if="value">{{ currency }}</a>
+  <span v-else />
+</template>
+
+<script>
+export default {
+  name: 'CurrencyCell',
+  props: {
+    column: Object,
+    value: [String, Number]
+  },
+  computed: {
+    currency() {
+      try {
+        return new Intl.NumberFormat(this.currencyMeta.currency_locale || 'en-US',
+          { style: 'currency', currency: this.currencyMeta.currency_code || 'USD' }).format(this.value)
+      } catch (e) {
+        return this.value
+      }
+    },
+    currencyMeta() {
+      return {
+        currency_locale: 'en-US',
+        currency_code: 'USD',
+        ...(this.column && this.column.meta
+          ? this.column.meta
+          : {})
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

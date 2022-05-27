4c5c4803c2af4c483d6a86c071a33db901879a0f
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-05-27 16:24:42 +0800

    feat: add duration helper

---
+++ packages/nc-gui/helpers/durationHelper.js
@@ -0,0 +1,29 @@
+export const durationOptions = [
+    {id: 0, title: 'h:mm', example: '(e.g. 1:23)'},
+    {id: 1, title: 'h:mm:ss', example: '(e.g. 3:45, 1:23:40)'},
+    {id: 2, title: 'h:mm:ss.s', example: '(e.g. 3:34.6, 1:23:40.0)'},
+    {id: 3, title: 'h:mm:ss.ss', example: '(e.g. 3.45.67, 1:23:40.00)'},
+    {id: 4, title: 'h:mm:ss.sss', example: '(e.g. 3.45.678, 1:23:40.000)'},
+]
+
+/**
+ * @copyright Copyright (c) 2021, Xgene Cloud Ltd
+ *
+ * @author Wing-Kam Wong <wingkwong.code@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */

8f3f437c304e6e7ddf1a86ce73c6fb25986cf214
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-05-27 12:54:46 +0800

    fix: use id & title structure for duration option

--- packages/nc-gui/components/project/spreadsheet/components/editColumn/DurationOptions.vue
+++ packages/nc-gui/components/project/spreadsheet/components/editColumn/DurationOptions.vue
@@ -11,18 +11,20 @@
       label="Duration Format"
       dense
       outlined
+      item-value="id"
+      item-text="title"
       :items="durationOptionList"
     >
       <template #selection="{ item }">
         <div>
           <span class="caption grey--text text--darken-4">
-            {{ item }}
+            {{ item.title }}
           </span>
         </div>
       </template>
       <template #item="{ item }">
         <div class="caption">
-          {{ item }}
+          {{ item.title }}
         </div>
       </template>
     </v-autocomplete>
@@ -35,12 +37,15 @@ export default {
   props: ['column', 'meta', 'value'],
   data: () => ({
     durationOptionList: [
-      'h:mm (e.g. 1:23)',
-      'h:mm:ss (e.g. 3:45, 1:23:40)',
-      'h:mm:ss.s (e.g. 3:34.6, 1:23:40,0)',
-      'h:mm:ss.ss (e.g. 3.45.67, 1:23:40,00)',
-      'h:mm:ss.sss (e.g. 3.45.678, 1:23:40.000)'
-    ]
+      {id: 0, title: 'h:mm (e.g. 1:23)'},
+      {id: 1, title: 'h:mm:ss (e.g. 3:45, 1:23:40)'},
+      {id: 2, title: 'h:mm:ss.s (e.g. 3:34.6, 1:23:40,0)'},
+      {id: 3, title: 'h:mm:ss.ss (e.g. 3.45.67, 1:23:40,00)'},
+      {id: 4, title: 'h:mm:ss.sss (e.g. 3.45.678, 1:23:40.000)'},
+    ],
+    colMeta: {
+        duration: 0
+    }
   }),
   watch: {
     value() {

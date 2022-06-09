0938532be8f90eb9c417b5d35d43e4af931983e3
Vijay Kumar Rathore <professional.vijay8492@gmail.com>
2022-06-09 10:00:40 +0200

    keep alive timeout to 65 sec

--- packages/nocodb/docker/index.js
+++ packages/nocodb/docker/index.js
@@ -12,8 +12,8 @@ server.set('view engine', 'ejs');
   const httpServer = server.listen(process.env.PORT || 8080, () => {
     console.log(`App started successfully.\nVisit -> ${Noco.dashboardUrl}`);
   })
-  server.keepAliveTimeout = 125000; // Ensure all inactive connections are terminated by the ALB, by setting this a few seconds higher than the ALB idle timeout
-  server.headersTimeout = 126000; 
+  server.keepAliveTimeout = 65000; // Ensure all inactive connections are terminated by the ALB, by setting this a few seconds higher than the ALB idle timeout
+  server.headersTimeout = 66000; 
   server.use(await Noco.init({}, httpServer, server));
 })().catch(e => console.log(e))
 

98d22169e627e99e04e1ac15dc1f1ed982a5ed31
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-06-11 14:16:32 +0800

    cypress: t3e duration test draft

---
+++ scripts/cypress/integration/common/3e_duration_column.js
@@ -0,0 +1,29 @@
+// import { mainPage } from "../../support/page_objects/mainPage";
+import { isTestSuiteActive } from "../../support/page_objects/projectConstants";
+
+export const genTest = (apiType, dbType) => {
+    if (!isTestSuiteActive(apiType, dbType)) return;
+    // TODO
+};
+
+/**
+ * @copyright Copyright (c) 2021, Xgene Cloud Ltd
+ *
+ * @author Wing-Kam Wong <wingkwong@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */

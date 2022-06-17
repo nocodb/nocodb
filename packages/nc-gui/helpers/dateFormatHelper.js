6aef05481d683efba551df3e0031f25e4a4ba081
elvus <lvssgv@gmail.com>
2022-06-17 16:00:53 -0400

    Change the text field to autocomplete and add dateformat list

--- packages/nc-gui/helpers/dateFormatHelper.js
+++ packages/nc-gui/helpers/dateFormatHelper.js
@@ -1,7 +1,7 @@
 export const dateFormat = [
     'DD-MM-YYYY', "MM-DD-YYYY", "YYYY-MM-DD",
     'DD/MM/YYYY', "MM/DD/YYYY", "YYYY/MM/DD",
-    'DD MM YYYY', "MM DD YYYY", "YYYY MM DD",
+    'DD MM YYYY', "MM DD YYYY", "YYYY MM DD"
 ]
 
 export function validatedateFormat(v) {

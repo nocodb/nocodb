1822b22e4bc300e57c78956559c188933569c39e
Raju Udava <86527202+dstala@users.noreply.github.com>
2022-06-20 12:55:09 +0530

    fix: hm symmetric column creation
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- packages/nocodb/tests/export-import/exportSchema.js
+++ packages/nocodb/tests/export-import/exportSchema.js
@@ -1,19 +1,22 @@
 const Api = require('nocodb-sdk').Api;
 const { UITypes } = require('nocodb-sdk');
-const jsonfile = require("jsonfile");
+const jsonfile = require('jsonfile');
 
-let ncMap = {}
-let tblSchema = []
-let api = {}
-let viewStore = {columns: {}, sort: {}, filter: {}}
+const GRID = 3, GALLERY = 2, FORM = 1;
+
+let ncMap = { /* id: name <string> */ };
+let tblSchema = [];
+let api = {};
+let viewStore = { columns: {}, sort: {}, filter: {} };
 
 const ncConfig = {
-  projectName: "sample",
-  baseURL: "http://localhost:8080",
+  projectName: 'sample',
+  baseURL: 'http://localhost:8080',
   headers: {
-    'xc-auth': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAbm9jb2RiLmNvbSIsImZpcnN0bmFtZSI6bnVsbCwibGFzdG5hbWUiOm51bGwsImlkIjoidXNfdDNkb2ppNXdtbDJ3bHIiLCJyb2xlcyI6InVzZXIsc3VwZXIiLCJpYXQiOjE2NTMzODY4NTN9.EvMwhhWJTM4QdEpgpmK1wSxsy7QiP7-sHWVDuTyOXSI"
+    'xc-auth':
+      ''
   }
-}
+};
 
 // helper routines
 // remove objects containing 0/ false/ null
@@ -21,110 +24,147 @@ const ncConfig = {
 function removeEmpty(obj) {
   return Object.fromEntries(
     Object.entries(obj)
-      .filter(([_, v]) => ((v != null) && (v != 0) && (v != false)))
+      .filter(([_, v]) => v != null && v != 0 && v != false)
       .map(([k, v]) => [k, v === Object(v) ? removeEmpty(v) : v])
   );
 }
 
-// let linksAdded = []
-// function isLinkExists(pId, cId) {
-//   let idx = linksAdded.findIndex(a => a.child === pId && a.parent === cId)
-//   if(idx === -1) {
-//     linksAdded.push({ child: cId, parent: pId })
-//     return false;
-//   }
-//   return true;
-// }
-
-function ncGetColData(cId) {
-
-}
-
 function addColumnSpecificData(c) {
   // pick required fields to proceed further
-  let col = removeEmpty((({ id, title, column_name, uidt, dt, pk, pv, rqd, dtxp, system }) =>
-    ({ id, title, column_name, uidt, dt, pk, pv, rqd, dtxp, system }))(c))
+  let col = removeEmpty(
+    (({ id, title, column_name, uidt, dt, pk, pv, rqd, dtxp, system }) => ({
+      id,
+      title,
+      column_name,
+      uidt,
+      dt,
+      pk,
+      pv,
+      rqd,
+      dtxp,
+      system
+    }))(c)
+  );
 
-  let colOptions = null
-  switch(c.uidt) {
+  switch (c.uidt) {
     case UITypes.Formula:
-      colOptions = {
-        formula: c.colOptions.formula,
-        formula_raw: c.colOptions.formula_raw
-      }
-      break
+      col.formula = c.colOptions.formula;
+      col.formula_raw = c.colOptions.formula_raw;
+      break;
     case UITypes.LinkToAnotherRecord:
-      colOptions = {
+      col[`colOptions`] = {
         fk_model_id: c.fk_model_id,
         fk_related_model_id: c.colOptions.fk_related_model_id,
         fk_child_column_id: c.colOptions.fk_child_column_id,
         fk_parent_column_id: c.colOptions.fk_parent_column_id,
         type: c.colOptions.type
-      }
+      };
       break;
     case UITypes.Lookup:
-      colOptions = {
+      col[`colOptions`] = {
         fk_model_id: c.fk_model_id,
         fk_relation_column_id: c.colOptions.fk_relation_column_id,
-        fk_lookup_column_id: c.colOptions.fk_lookup_column_id,
-      }
+        fk_lookup_column_id: c.colOptions.fk_lookup_column_id
+      };
       break;
     case UITypes.Rollup:
-      colOptions = {
+      col[`colOptions`] = {
         fk_model_id: c.fk_model_id,
         fk_relation_column_id: c.colOptions.fk_relation_column_id,
         fk_rollup_column_id: c.colOptions.fk_rollup_column_id,
         rollup_function: c.colOptions.rollup_function
-      }
+      };
       break;
   }
 
-  // colOptions not required for formula
-  if (c.uidt === UITypes.Formula) {
-    col.formula = c.colOptions.formula;
-    col.formula_raw = c.colOptions.formula_raw;
-  }
-  else if(colOptions) col[`colOptions`] = colOptions;
   return col;
 }
 
 function addViewDetails(v) {
   // pick required fields to proceed further
-  let view = (({ id, title, type, show_system_fields, lock_type, order }) => (
-    { id, title, type, show_system_fields, lock_type, order }))(v);
+  let view = (({ id, title, type, show_system_fields, lock_type, order }) => ({
+    id,
+    title,
+    type,
+    show_system_fields,
+    lock_type,
+    order
+  }))(v);
 
   // form view
-  if(v.type === 1) {
-    view.property = (({ heading, subheading, success_msg, redirect_after_secs, email, submit_another_form, show_blank_form }) => (
-      { heading, subheading, success_msg, redirect_after_secs, email, submit_another_form, show_blank_form }))(v.view);
+  if (v.type === FORM) {
+    view.property = (({
+      heading,
+      subheading,
+      success_msg,
+      redirect_after_secs,
+      email,
+      submit_another_form,
+      show_blank_form
+    }) => ({
+      heading,
+      subheading,
+      success_msg,
+      redirect_after_secs,
+      email,
+      submit_another_form,
+      show_blank_form
+    }))(v.view);
   }
 
   // gallery view
-  else if (v.type === 2) {
-    view.property = {fk_cover_image_col_id: ncMap[v.view.fk_cover_image_col_id]}
+  else if (v.type === GALLERY) {
+    view.property = {
+      fk_cover_image_col_id: ncMap[v.view.fk_cover_image_col_id]
+    };
   }
 
   // gallery view doesn't share column information in api yet
-  if(v.type !== 2) {
-    if(v.type === 3)
-    view.columns = viewStore.columns[v.id].map(a => (({ id, width, order, show }) => (
-      { id, width, order, show }))(a))
-    if(v.type === 1)
-      view.columns = viewStore.columns[v.id].map(a => (({ id, order, show, label, help, description, required }) => (
-        { id, order, show, label, help, description, required }))(a))
+  if (v.type !== GALLERY) {
+    if (v.type === GRID)
+      view.columns = viewStore.columns[v.id].map(a =>
+        (({ id, width, order, show }) => ({ id, width, order, show }))(a)
+      );
+    if (v.type === FORM)
+      view.columns = viewStore.columns[v.id].map(a =>
+        (({ id, order, show, label, help, description, required }) => ({
+          id,
+          order,
+          show,
+          label,
+          help,
+          description,
+          required
+        }))(a)
+      );
 
     for (let i = 0; i < view.columns?.length; i++)
-      view.columns[i].title = ncMap[viewStore.columns[v.id][i].id]
+      view.columns[i].title = ncMap[viewStore.columns[v.id][i].id];
 
-    view.columns = view.columns.filter(a => a.title.includes('_nc_m2m_') === false)
+    // skip hm & mm columns
+    view.columns = view.columns
+      ?.filter(a => a.title?.includes('_nc_m2m_') === false)
+      .filter(a => a.title?.includes('nc_') === false);
   }
 
   // filter & sort configurations
-  if(v.type !== 1) {
-    view.sort = viewStore.sort[v.id].map(a => (({ fk_column_id, direction, order }) => (
-      { fk_column_id, direction, order }))(a))
-    view.filter = viewStore.filter[v.id].map(a => (({ fk_column_id, logical_op, comparison_op, value, order }) => (
-      { fk_column_id, logical_op, comparison_op, value, order }))(a))
+  if (v.type !== FORM) {
+    view.sort = viewStore.sort[v.id].map(a =>
+      (({ fk_column_id, direction, order }) => ({
+        fk_column_id,
+        direction,
+        order
+      }))(a)
+    );
+    view.filter = viewStore.filter[v.id].map(a =>
+      (({ fk_column_id, logical_op, comparison_op, value, order }) => ({
+        fk_column_id,
+        logical_op,
+        comparison_op,
+        value,
+        order
+      }))(a)
+    );
   }
   return view;
 }
@@ -132,53 +172,54 @@ function addViewDetails(v) {
 // view data stored as is for quick access
 async function storeViewDetails(tableId) {
   // read view data for each table
-  let viewList = await api.dbView.list(tableId)
-  for(let j=0; j<viewList.list.length; j++) {
-    let v = viewList.list[j]
-    let viewDetails = []
+  let viewList = await api.dbView.list(tableId);
+  for (let j = 0; j < viewList.list.length; j++) {
+    let v = viewList.list[j];
+    let viewDetails = [];
 
     // invoke view specific read to populate columns information
-    if (v.type === 1) viewDetails = (await api.dbView.formRead(v.id)).columns;
-    else if (v.type === 2) viewDetails = await api.dbView.galleryRead(v.id);
-    else if (v.type === 3) viewDetails = await api.dbView.gridColumnsList(v.id);
+    if (v.type === FORM) viewDetails = (await api.dbView.formRead(v.id)).columns;
+    else if (v.type === GALLERY) viewDetails = await api.dbView.galleryRead(v.id);
+    else if (v.type === GRID) viewDetails = await api.dbView.gridColumnsList(v.id);
     viewStore.columns[v.id] = viewDetails;
 
     // populate sort information
     let vSort = await api.dbTableSort.list(v.id);
-    viewStore.sort[v.id] = vSort.sorts.list
+    viewStore.sort[v.id] = vSort.sorts.list;
 
-    let vFilter = await api.dbTableFilter.read(v.id)
-    viewStore.filter[v.id] = vFilter
+    let vFilter = await api.dbTableFilter.read(v.id);
+    viewStore.filter[v.id] = vFilter;
   }
 }
 
 // mapping table for quick information access
 // store maps for tableId, columnId, viewColumnId & viewId to their names
 async function generateMapTbl(pId) {
-  const tblList = await api.dbTable.list(pId)
+  const tblList = await api.dbTable.list(pId);
 
-  for(let i=0; i<tblList.list.length; i++) {
-    let tblId = tblList.list[i].id
-    let tbl = await api.dbTable.read(tblId)
+  for (let i = 0; i < tblList.list.length; i++) {
+    let tblId = tblList.list[i].id;
+    let tbl = await api.dbTable.read(tblId);
 
     // table ID <> name
     ncMap[tblId] = tbl.title;
 
     // column ID <> name
-    tbl.columns.map(x => ncMap[x.id] = x.title )
+    tbl.columns.map(x => (ncMap[x.id] = x.title));
 
     // view ID <> name
-    tbl.views.map(x => ncMap[x.id] = x.tn )
+    tbl.views.map(x => (ncMap[x.id] = x.tn));
 
-    for(let i=0; i<tbl.views.length; i++) {
-      let x = tbl.views[i]
-      let viewColumns = []
-      if (x.type === 1) viewColumns = (await api.dbView.formRead(x.id)).columns;
-      else if (x.type === 2) viewColumns = (await api.dbView.galleryRead(x.id)).columns;
-      else viewColumns = await api.dbView.gridColumnsList(x.id);
+    for (let i = 0; i < tbl.views.length; i++) {
+      let x = tbl.views[i];
+      let viewColumns = [];
+      if (x.type === FORM) viewColumns = (await api.dbView.formRead(x.id)).columns;
+      else if (x.type === GALLERY)
+        viewColumns = (await api.dbView.galleryRead(x.id)).columns;
+      else if (x.type === GRID) viewColumns = await api.dbView.gridColumnsList(x.id);
 
       // view column ID <> name
-      viewColumns?.map(a => ncMap[a.id] = ncMap[a.fk_column_id])
+      viewColumns?.map(a => (ncMap[a.id] = ncMap[a.fk_column_id]));
     }
   }
 }
@@ -186,21 +227,23 @@ async function generateMapTbl(pId) {
 // main
 //
 async function exportSchema() {
-  api = new Api( ncConfig );
+  api = new Api(ncConfig);
 
   // fetch project details (id et.al)
   const x = await api.project.list();
   const p = x.list.find(a => a.title === ncConfig.projectName);
 
-  await generateMapTbl(p.id)
+  await generateMapTbl(p.id);
 
   // read project
-  const tblList = await api.dbTable.list(p.id)
-  for(let i=0; i<tblList.list.length; i++) {
-    let tblId = tblList.list[i].id
-    await storeViewDetails(tblId)
+  const tblList = await api.dbTable.list(p.id);
 
-    let tbl = await api.dbTable.read(tblId)
+  // for each table
+  for (let i = 0; i < tblList.list.length; i++) {
+    let tblId = tblList.list[i].id;
+    await storeViewDetails(tblId);
+
+    let tbl = await api.dbTable.read(tblId);
 
     // prepare schema
     let tSchema = {
@@ -208,15 +251,46 @@ async function exportSchema() {
       title: tbl.title,
       table_name: tbl?.table_name,
       columns: [...tbl.columns.map(c => addColumnSpecificData(c))]
-        .filter(a => a.title.includes('_nc_m2m_') === false)
-        .filter(a => !(a?.system===1 && a.uidt===UITypes.LinkToAnotherRecord )),
+        .filter(a => a.title.includes('_nc_m2m_') === false)        // mm
+        .filter(a => a.title.includes(p.prefix) === false)          // hm
+        .filter(
+          a => !(a?.system === 1 && a.uidt === UITypes.LinkToAnotherRecord)
+        ),
       views: [...tbl.views.map(v => addViewDetails(v))]
-    }
-    tblSchema.push(tSchema)
+    };
+    tblSchema.push(tSchema);
   }
 }
 
-(async() => {
-  await exportSchema()
-  jsonfile.writeFileSync(`${ncConfig.projectName.replace(/ /g, '_')}.json`, tblSchema, { spaces: 2 })
-})().catch(e => {console.log(e)})
+(async () => {
+  await exportSchema();
+  jsonfile.writeFileSync(
+    `${ncConfig.projectName.replace(/ /g, '_')}.json`,
+    tblSchema,
+    { spaces: 2 }
+  );
+})().catch(e => {
+  console.log(e);
+});
+
+/**
+ * @copyright Copyright (c) 2021, Xgene Cloud Ltd
+ *
+ * @author Raju Udava <sivadstala@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */

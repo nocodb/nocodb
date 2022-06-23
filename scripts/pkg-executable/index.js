36cfb2c1c2204c62ecb7928647563a44b75fbf2a
Pranav C <pranavxc@gmail.com>
2022-06-23 11:49:55 +0530

    chore: action for releasing
    Signed-off-by: Pranav C <pranavxc@gmail.com>

---
+++ scripts/pkg-executable/index.js
@@ -0,0 +1,11 @@
+(async () => {
+  try {
+    const app = require('express')();
+    const {Noco} = require("nocodb");
+    const httpServer = app.listen(process.env.PORT || 8080);
+    app.use(await Noco.init({}, httpServer, app));
+    console.log(`Visit : localhost:${process.env.PORT}/dashboard`)
+  } catch(e) {
+    console.log(e)
+  }
+})()

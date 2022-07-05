bb190e2b2c167a1c9a6d2c6468c4625a4a2366e7
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-07-05 16:16:52 +0800

    fix: change from boolean to number

--- packages/nocodb/src/lib/meta/helpers/PagedResponse.ts
+++ packages/nocodb/src/lib/meta/helpers/PagedResponse.ts
@@ -7,15 +7,15 @@ export class PagedResponseImpl<T> {
       limit = 25,
       offset = 0,
       count = null,
-      shuffle = false,
+      shuffle = 0,
     }: {
       limit?: number;
       offset?: number;
       count?: number;
-      shuffle?: boolean;
+      shuffle?: number;
     } = {}
   ) {
-    if (shuffle) {
+    if (+shuffle) {
       for (let i = list.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [list[i], list[j]] = [list[j], list[i]];

eb3f44fed2aa5ce34dae0c1b6978552aa2fb6de2
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-05 18:24:53 +0200

    chore(gui-v2): update nuxt.config.ts to use i18n plugin

--- packages/nc-gui-v2/nuxt.config.ts
+++ packages/nc-gui-v2/nuxt.config.ts
@@ -1,26 +1,41 @@
+import path from 'path'
 import { defineNuxtConfig } from 'nuxt'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['nuxt3-store'],
+  modules: ['nuxt3-store', '@vueuse/nuxt'],
+
   ssr: false,
+
   css: ['vuetify/lib/styles/main.sass'],
+
   build: {
     transpile: ['vuetify'],
   },
 
-  meta: {
+  head: {
     title: 'NocoDB',
     description: 'NocoDB GUI V2',
-    titleTemplate: (titleChunk) => {
+    titleTemplate: (titleChunk: string) => {
       // If undefined or blank then we don't need the hyphen
       return titleChunk ? `${titleChunk} - NocoDB` : 'NocoDB'
     },
   },
 
   vite: {
+    plugins: [
+      vueI18n({
+        include: path.resolve(__dirname, './lang'),
+      }),
+    ],
     define: {
       'process.env.DEBUG': 'false',
     },
+  },
+
+  experimental: {
+    reactivityTransform: true,
+    viteNode: false,
   },
 })

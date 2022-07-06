c89729a90f1c62d58c92a8877645d43d59bad212
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-07-06 12:18:36 +0800

    feat: percent helper

---
+++ packages/nc-gui/helpers/percentHelper.js
@@ -0,0 +1,46 @@
+export const percentOptions = [
+  { id: 0, title: '1' },
+  { id: 1, title: '1.0' },
+  { id: 2, title: '1.00' },
+  { id: 3, title: '1.000' },
+  { id: 4, title: '1.0000' },
+  { id: 5, title: '1.00000' },
+  { id: 6, title: '1.000000' },
+  { id: 7, title: '1.0000000' },
+  { id: 8, title: '1.00000000' }
+]
+
+export const renderPercent = (value, percentOption) => {
+  if (!value) { return value }
+  return padPercentSymbol(Number(value).toFixed(percentOption))
+}
+
+export const isValidPercent = (val) => {
+  return /^\d{1,20}(\.\d+)?$/.test(val)
+}
+
+const padPercentSymbol = (value) => {
+  return value ? value + '%' : value
+}
+
+/**
+ * @copyright Copyright (c) 2021, Xgene Cloud Ltd
+ *
+ * @author Wing-Kam Wong <wingkwong.code@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */

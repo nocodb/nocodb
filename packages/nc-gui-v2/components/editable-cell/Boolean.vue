49336616ebe29e7032e58f59798f43b109b3898f
Pranav C <pranavxc@gmail.com>
2022-07-06 17:43:46 +0530

    feat: migrate boolean cell
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui-v2/components/editable-cell/Boolean.vue
+++ packages/nc-gui-v2/components/editable-cell/Boolean.vue
@@ -16,10 +16,10 @@ const checkboxMeta = computed(() => {
       unchecked: "mdi-checkbox-blank-circle-outline"
     },
     color: "primary",
-    // ...(column?.meta || {})
+    ...(column?.meta || {})
   };
 });
-const localState = $computed({
+const localState = computed({
   get() {
     return value;
   }, set(val) {
@@ -28,8 +28,15 @@ const localState = $computed({
 });
 
 const toggle = () =>  {
-  localState = !localState
+  localState.value = !localState.value
 }
+
+// const checkedIcon = computed(() => {
+//   return defineAsyncComponent( ()=>import('~icons/material-symbols/'+checkboxMeta?.value?.icon?.checked))
+// });
+// const uncheckedIcon = computed(() => {
+//   return defineAsyncComponent(()=>import('~icons/material-symbols/'+checkboxMeta?.value?.icon?.unchecked))
+// });
 
 
 /*export default {
@@ -81,34 +88,11 @@ const toggle = () =>  {
 <template>
   <div
     class="d-flex align-center" :class="{ 'justify-center': !isForm, 'nc-cell-hover-show': !localState }">
-    <v-icon small :color="checkboxMeta.color" @click="toggle">
-      {{ localState ? checkedIcon : uncheckedIcon }}
-    </v-icon>
+<!--    <span :is="localState ? checkedIcon : uncheckedIcon" small :color="checkboxMeta.color" @click="toggle">-->
+<!--      {{ localState ? checkedIcon : uncheckedIcon }}-->
+<!--    </span>-->
+
+    <input type="checkbox"
+      v-model="localState">
   </div>
 </template>
-
-<style scoped></style>
-<!--
-/**
- * @copyright Copyright (c) 2021, Xgene Cloud Ltd
- *
- * @author Naveen MR <oof1lab@gmail.com>
- * @author Pranav C Balan <pranavxc@gmail.com>
- *
- * @license GNU AGPL version 3 or any later version
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- */
--->

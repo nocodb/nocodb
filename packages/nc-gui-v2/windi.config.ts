76d8f913ac818336e35443ca211c6aee570e51f7
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-06 16:58:39 +0200

    feat(gui-v2): add plugins to windi config
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/nc-gui-v2/windi.config.ts
+++ packages/nc-gui-v2/windi.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'windicss/helpers'
+import formsPlugin from 'windicss/plugin/forms'
+import typographyPlugin from 'windicss/plugin/typography'
+import aspectRatioPlugin from 'windicss/plugin/aspect-ratio'
+import lineClampPlugin from 'windicss/plugin/line-clamp'
+import windiColors from 'windicss/colors'
 // @ts-expect-error no types for plugin-scrollbar
 import scrollbar from '@windicss/plugin-scrollbar'
-import colors from './utils/colorsUtils'
+import colors, { themeColors } from './utils/colorsUtils'
 
 export default defineConfig({
   extract: {
@@ -12,11 +17,18 @@ export default defineConfig({
   attributify: true,
   darkMode: 'class',
 
-  plugins: [scrollbar],
+  plugins: [scrollbar, formsPlugin, typographyPlugin, aspectRatioPlugin, lineClampPlugin],
 
   theme: {
+    fontFamily: {
+      sans: ['Vazirmatn', 'sans-serif'],
+      serif: ['Vazirmatn', 'serif'],
+      mono: ['Roboto', 'mono'],
+    },
     extend: {
       colors: {
+        ...windiColors,
+        ...themeColors,
         dark: colors.dark,
         light: colors.light,
       },

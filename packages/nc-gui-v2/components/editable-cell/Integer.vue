1d52a65efd6d25ddb0413ebf3252b8d2b3718023
Pranav C <pranavxc@gmail.com>
2022-07-06 23:41:27 +0530

    feat(gui-v2): add single/multi select
    Signed-off-by: Pranav C <pranavxc@gmail.com>

---
+++ packages/nc-gui-v2/components/editable-cell/Integer.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed } from "@vue/reactivity";
+import { onMounted } from "vue";
+
+const { modelValue: value } = defineProps<{ modelValue: any }>()
+
+const emit = defineEmits(['update:modelValue'])
+
+const root = ref<HTMLInputElement>()
+
+const localState = computed({
+  get() {
+    return value
+  },
+  set(val) {
+    emit('update:modelValue', val)
+  },
+})
+
+onMounted(() => {
+  root.value?.focus()
+})
+
+</script>
+
+<template>
+  <input ref="root" v-model="localState" type="number" />
+</template>
+
+<style scoped>
+input {
+  outline: none;
+  width: 100%;
+  height: 100%;
+  color: var(--v-textColor-base);
+}
+</style>

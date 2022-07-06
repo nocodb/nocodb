6f4a8ab16d1ba68e5dd4a01035a29a9f8208f3a6
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-06 12:41:38 +0200

    chore(gui-v2): wrap watcher in scope

--- packages/nc-gui-v2/composables/useColors.ts
+++ packages/nc-gui-v2/composables/useColors.ts
@@ -1,25 +1,32 @@
 import type { MaybeRef } from '@vueuse/core'
 import { unref } from '@vue/reactivity'
+import { tryOnScopeDispose } from '#build/imports'
 import { useNuxtApp } from '#app'
 import theme from '~/utils/colorsUtils'
 
 export default function useColors(darkMode?: MaybeRef<boolean>) {
+  const scope = effectScope()
+
   let mode = $ref(unref(darkMode))
   const { $state } = useNuxtApp()
 
   if (!mode) mode = $state.value.darkMode
 
-  watch(
-    () => $state.value.darkMode,
-    (newMode) => {
-      mode = newMode
-    },
-  )
+  scope.run(() => {
+    watch(
+      () => $state.value.darkMode,
+      (newMode) => {
+        mode = newMode
+      },
+    )
 
-  watchEffect(() => {
-    const newMode = unref(darkMode)
-    if (newMode) mode = newMode
+    watchEffect(() => {
+      const newMode = unref(darkMode)
+      if (newMode) mode = newMode
+    })
   })
+
+  tryOnScopeDispose(() => scope.stop())
 
   const colors = computed(() => (mode ? theme.dark : theme.light))
 

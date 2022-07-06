22012c9ab85e584bd1da1917606bd3a47328da67
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-06 20:32:10 +0200

    feat(gui-v2): add page transitions
    # What's changed?
    
    * transition on page/layout change
    * move v2 styles to separate file
    * refactor layouts
    
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/nc-gui-v2/components/general/Sponsors.vue
+++ packages/nc-gui-v2/components/general/Sponsors.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import MdiHeartsCard from '~icons/mdi/cards-heart'
+
 interface Props {
   nav?: boolean
 }
@@ -9,20 +11,18 @@ const { nav = false } = defineProps<Props>()
 <template>
   <v-card :rounded="0" max-width="300" class="pb-3" href="https://github.com/sponsors/nocodb" target="_blank">
     <v-img src="/ants-leaf-cutter.jpeg" :cover="true" :aspect-ratio="1" :height="nav ? 80 : ''" class="mt-0" />
-    <!-- You can help us! -->
-    <v-card-title v-if="!nav" class="pb-2" :class="{ 'body-2 justify-center mt-n2': nav }">
+
+    <v-card-title v-if="!nav" class="pb-2">
       {{ $t('msg.info.sponsor.header') }}
     </v-card-title>
+
     <v-card-text v-if="!nav" class="pb-0">
-      <p class="caption text-left body-1 textColor--text text--lighten-1">
-        <!-- We are a tiny team working full time to make NocoDB open source. We believe a tool like NocoDB should be
-         available freely to every problem solver on internet. -->
-        {{ $t('msg.info.sponsor.message') }}
-      </p>
+      {{ $t('msg.info.sponsor.message') }}
     </v-card-text>
+
     <v-card-actions class="justify-center">
-      <v-btn color="primary" :class="{ 'mt-n2': !nav, 'mt-2 mb-n1': nav }" small outlined>
-        <v-icon small color="red" class="mr-2"> mdi-cards-heart </v-icon>
+      <v-btn color="primary" class="mt-2">
+        <MdiHeartsCard class="text-red-500 mr-2" />
         {{ $t('activity.sponsorUs') }}
       </v-btn>
     </v-card-actions>

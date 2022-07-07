2eb5acdef1b77d888d36d5907d2ded1b9cf5e998
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-07 13:59:27 +0200

    feat(gui-v2): add signIn action to global state
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/nc-gui-v2/composables/useGlobalState.ts
+++ packages/nc-gui-v2/composables/useGlobalState.ts
@@ -1,6 +1,7 @@
 import { usePreferredDark, usePreferredLanguages, useStorage } from '@vueuse/core'
+import { navigateTo } from '#app'
 import { computed, toRefs } from '#build/imports'
-import type { GlobalState, State } from '~/lib/types'
+import type { Actions, Getters, GlobalState, State } from '~/lib/types'
 
 const storageKey = 'nocodb-gui-v2'
 
@@ -18,13 +19,22 @@ export const useGlobalState = (): GlobalState => {
   const storage = useStorage<State>(storageKey, initialState)
 
   // getters
-  const signedIn = computed(() => storage.value.token !== null && storage.value.token !== '' && storage.value.user !== null)
+  const signedIn: Getters['signedIn'] = computed(
+    () => storage.value.token !== null && storage.value.token !== '' && storage.value.user !== null,
+  )
 
   // actions
-  function signOut() {
+  const signOut: Actions['signOut'] = () => {
     storage.value.token = null
     storage.value.user = null
+    navigateTo('/signin')
   }
 
-  return { ...toRefs(storage.value), signedIn, signOut }
+  const signIn: Actions['signIn'] = (user, token) => {
+    storage.value.token = token
+    storage.value.user = user
+    navigateTo('/')
+  }
+
+  return { ...toRefs(storage.value), signedIn, signOut, signIn }
 }

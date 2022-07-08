314ad7ebd18c4c1859699ba37dc5f36b01ddafea
Pranav C <pranavxc@gmail.com>
2022-07-08 19:39:14 +0530

    feat(gui-v2): add view icons
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui-v2/utils/viewUtils.ts
+++ packages/nc-gui-v2/utils/viewUtils.ts
@@ -1,11 +1,19 @@
 import { ViewTypes } from 'nocodb-sdk'
 
+import MdiGridIcon from '~icons/mdi/grid-large'
+import MdiFormIcon from '~icons/mdi/form-select'
+import MdiCalendarIcon from '~icons/mdi/calendar'
+import MdiGalleryIcon from '~icons/mdi/camera-image'
+import MdiKanbanIcon from '~icons/mdi/tablet-dashboard'
+import MdiEyeIcon from '~icons/mdi/eye-circle-outline'
+
+
 export const viewIcons = {
-  [ViewTypes.GRID]: { icon: 'mdi-grid-large', color: 'blue' },
+  [ViewTypes.GRID]: { icon: MdiGridIcon, color: 'blue' },
   // [ViewTypes.GRID]: { icon: "mdi-grid-large", color: "blue" },
-  [ViewTypes.FORM]: { icon: 'mdi-form-select', color: 'pink' },
-  calendar: { icon: 'mdi-calendar', color: 'purple' },
-  [ViewTypes.GALLERY]: { icon: 'mdi-camera-image', color: 'orange' },
-  [ViewTypes.KANBAN]: { icon: 'mdi-tablet-dashboard', color: 'green' },
-  view: { icon: 'mdi-eye-circle-outline', color: 'blue' },
+  [ViewTypes.FORM]: { icon: MdiFormIcon, color: 'pink' },
+  calendar: { icon: MdiCalendarIcon, color: 'purple' },
+  [ViewTypes.GALLERY]: { icon: MdiGalleryIcon, color: 'orange' },
+  [ViewTypes.KANBAN]: { icon: MdiKanbanIcon, color: 'green' },
+  view: { icon: MdiEyeIcon, color: 'blue' },
 }

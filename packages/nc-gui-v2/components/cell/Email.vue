b0d5f4580d71057a94640f8cc7965f79a047fbde
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-07-13 13:46:52 +0800

    feat(gui-v2): Email Cell

--- packages/nc-gui-v2/components/cell/Email.vue
+++ packages/nc-gui-v2/components/cell/Email.vue
@@ -1,15 +1,23 @@
 <script lang="ts" setup>
 import { computed } from '#imports'
-
 import { isEmail } from '~/utils/validation'
+
+const editEnabled = inject<boolean>('editEnabled')
 
 interface Props {
   modelValue: string
 }
 
-const { modelValue } = defineProps<Props>()
+const { modelValue: value } = defineProps<Props>()
 
-const validEmail = computed(() => isEmail(modelValue))
+const emit = defineEmits(['update:modelValue'])
+const root = ref<HTMLInputElement>()
+const localState = computed({
+  get: () => value,
+  set: (val) => emit('update:modelValue', val),
+})
+
+const validEmail = computed(() => isEmail(value))
 </script>
 
 <script lang="ts">
@@ -19,6 +27,14 @@ export default {
 </script>
 
 <template>
-  <a v-if="validEmail" :href="`mailto:${modelValue}`" target="_blank">{{ modelValue }}</a>
-  <span v-else>{{ modelValue }}</span>
+  <input v-if="editEnabled" ref="root" v-model="localState" />
+  <a
+    v-else-if="validEmail"
+    class="caption py-2 text-primary underline hover:opacity-75"
+    :href="`mailto:${value}`"
+    target="_blank"
+  >
+    {{ value }}
+  </a>
+  <span v-else>{{ value }}</span>
 </template>

a699feb3a33bcf0f40ac61f58ef3161d107130b6
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-18 14:51:57 +0200

    feat(gui-v2): add noco icon component

---
+++ packages/nc-gui-v2/components/general/NocoIcon.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+interface Props {
+  width?: number
+  height?: number
+}
+
+const { width = 90, height = 90 } = defineProps<Props>()
+</script>
+
+<template>
+  <div :style="{ left: `calc(50% - ${width / 2}px)`, top: `-${height * 0.6}px` }" class="absolute rounded-lg bg-primary">
+    <img :width="width" :height="height" alt="NocoDB" src="~/assets/img/icons/512x512-trans.png" />
+  </div>
+</template>

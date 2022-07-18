915c34e1b07ec5d28f1dc1474b9fd7e170963397
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-18 12:55:54 +0200

    feat(gui-v2): add reset-password page

--- packages/nc-gui-v2/pages/reset-password.vue
+++ packages/nc-gui-v2/pages/reset-password.vue
@@ -7,7 +7,7 @@ import { isEmail } from '~/utils/validation'
 import MdiLogin from '~icons/mdi/login'
 import MaterialSymbolsWarning from '~icons/material-symbols/warning'
 
-const { $api, $state } = $(useNuxtApp())
+const { $api } = $(useNuxtApp())
 
 const { t } = useI18n()
 
@@ -34,7 +34,6 @@ const formValidator = ref()
 
 const form = reactive({
   email: '',
-  password: '',
 })
 
 const formRules = {
@@ -44,18 +43,12 @@ const formRules = {
     // E-mail must be valid format
     (v: string) => isEmail(v) || t('msg.error.signUpRules.emailInvalid'),
   ],
-  password: [
-    // Password is required
-    (v: string) => !!v || t('msg.error.signUpRules.passwdRequired'),
-  ],
 }
 
-const signIn = async () => {
+const resetPassword = async () => {
   error = null
   try {
-    const { token } = await $api.auth.signin(form)
-    $state.signIn(token!)
-    await navigateTo('/projects')
+    await $api.auth.passwordForgot(form)
   } catch (e: any) {
     // todo: errors should not expose what was wrong (i.e. do not show "Password is wrong" messages)
     error = await extractSdkResponseErrorMsg(e)
@@ -74,8 +67,8 @@ const resetError = () => {
     <v-form
       ref="formValidator"
       v-model="valid"
-      class="h-[calc(100%_+_90px)] min-h-[600px] flex justify-center items-center"
-      @submit.prevent="signIn"
+      class="h-full min-h-[600px] flex justify-center items-center"
+      @submit.prevent="resetPassword"
     >
       <div class="h-full w-full flex flex-col flex-wrap justify-center items-center">
         <div
@@ -83,12 +76,12 @@ const resetError = () => {
         >
           <div
             style="left: -moz-calc(50% - 45px); left: -webkit-calc(50% - 45px); left: calc(50% - 45px)"
-            class="absolute top-12 md:top-[-10%] rounded-lg bg-primary"
+            class="absolute top-12 md:top-[-17%] rounded-lg bg-primary"
           >
             <img width="90" height="90" src="~/assets/img/icons/512x512-trans.png" />
           </div>
 
-          <h1 class="prose-2xl font-bold self-center my-4">{{ $t('general.signIn') }}</h1>
+          <h1 class="prose-2xl font-bold self-center my-4">Reset Password</h1>
 
           <Transition name="layout">
             <div v-if="error" class="self-center mb-4 bg-red-500 text-white rounded-lg w-3/4 p-1">
@@ -108,24 +101,6 @@ const resetError = () => {
             @focus="resetError"
           />
 
-          <v-text-field
-            id="password"
-            v-model="form.password"
-            class="bg-white dark:!bg-gray-900"
-            :rules="formRules.password"
-            :label="$t('labels.password')"
-            :placeholder="$t('labels.password')"
-            :persistent-placeholder="true"
-            type="password"
-            @focus="resetError"
-          />
-
-          <div class="hidden md:block self-end mx-8">
-            <nuxt-link class="prose-sm" to="/reset-password">
-              {{ $t('msg.info.signUp.forgotPassword') }}
-            </nuxt-link>
-          </div>
-
           <div class="self-center flex flex-wrap gap-4 items-center mt-4 md:mx-8 md:justify-between justify-center w-full">
             <button
               :disabled="!valid"
@@ -134,20 +109,14 @@ const resetError = () => {
                   ? '!opacity-50 !cursor-default'
                   : 'shadow-md hover:(text-primary bg-primary/10 dark:text-white dark:!bg-primary/50)',
               ]"
-              class="ml-1 border-1 border-solid border-gray-300 color-transition rounded-lg p-4 bg-gray-100/50"
+              class="ml-1 border-1 border-solid border-gray-300 transition-color duration-100 ease-in rounded-lg p-4 bg-gray-100/50"
               type="submit"
             >
-              <span class="flex items-center gap-2"><MdiLogin /> {{ $t('general.signIn') }}</span>
+              <span class="flex items-center gap-2"><MdiLogin /> Reset Password</span>
             </button>
             <div class="text-end prose-sm">
-              {{ $t('msg.info.signUp.dontHaveAccount') }}
-              <nuxt-link to="/signup">{{ $t('general.signUp') }}</nuxt-link>
-            </div>
-
-            <div class="prose-sm md:hidden">
-              <nuxt-link class="prose-sm text-primary underline hover:opacity-75" to="/reset-password">
-                {{ $t('msg.info.signUp.forgotPassword') }}
-              </nuxt-link>
+              {{ $t('msg.info.signUp.alreadyHaveAccount') }}
+              <nuxt-link class="text-primary underline hover:opacity-75" to="/signin">{{ $t('general.signIn') }}</nuxt-link>
             </div>
           </div>
         </div>

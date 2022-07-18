160e54a44a397d94cf4f425fc4975865b29e7eff
Pranav C <pranavxc@gmail.com>
2022-07-19 00:02:20 +0530

    feat: add settings modal
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui-v2/components/dashboard/TreeView.vue
+++ packages/nc-gui-v2/components/dashboard/TreeView.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import useProject from '~/composables/useProject'
 import useTabs from '~/composables/useTabs'
+import MdiSettingIcon from '~icons/mdi/cog'
 
 const { tables } = useProject()
 const { addTab } = useTabs()
+
+const settingsDlg = ref(false)
 </script>
 
 <template>
   <div class="nc-treeview-container flex flex-column">
-    <a-menu>
+    <a-menu class="flex-1 overflow-y-auto">
       <a-menu-item
         v-for="table in tables"
         :key="table.id"
@@ -18,6 +21,12 @@ const { addTab } = useTabs()
         {{ table.title }}
       </a-menu-item>
     </a-menu>
+    <div class="cursor-pointer nc-team-settings pa-4 flex align-center hover:bg-gray-200/20" @click="settingsDlg = true">
+      <MdiSettingIcon class="mr-2" />
+      <span> {{ $t('title.teamAndSettings') }}</span>
+    </div>
+
+    <a-modal v-model:visible="settingsDlg" width="max(90vw, 600px)"> Team and settings </a-modal>
   </div>
 </template>
 

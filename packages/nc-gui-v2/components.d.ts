39b640259d659e37118327814aa0b218b4a4b83d
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-07-20 16:21:19 +0800

    feat(gui-v2): add AForm & AFormItem

--- packages/nc-gui-v2/components.d.ts
+++ packages/nc-gui-v2/components.d.ts
@@ -14,6 +14,8 @@ declare module '@vue/runtime-core' {
     ACollapsePanel: typeof import('ant-design-vue/es')['CollapsePanel']
     ADivider: typeof import('ant-design-vue/es')['Divider']
     ADropdown: typeof import('ant-design-vue/es')['Dropdown']
+    AForm: typeof import('ant-design-vue/es')['Form']
+    AFormItem: typeof import('ant-design-vue/es')['FormItem']
     AInput: typeof import('ant-design-vue/es')['Input']
     ALayout: typeof import('ant-design-vue/es')['Layout']
     ALayoutContent: typeof import('ant-design-vue/es')['LayoutContent']

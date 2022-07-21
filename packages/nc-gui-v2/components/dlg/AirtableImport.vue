80dafd6b878e785aeb7f7830c6b8c5bc34f10982
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-07-21 23:28:10 +0800

    init(gui-v2): airtable import dialog

---
+++ packages/nc-gui-v2/components/dlg/AirtableImport.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { UploadChangeParam } from 'ant-design-vue'
+
+const { modelValue = false } = defineProps<{ modelValue?: boolean }>()
+
+const emit = defineEmits(['update:modelValue'])
+
+const dialogShow = computed({
+  get() {
+    return modelValue
+  },
+  set(v) {
+    emit('update:modelValue', v)
+  },
+})
+</script>
+
+<template>
+  <v-dialog
+    v-model="dialogShow"
+    persistent
+    max-width="550"
+    @keydown.esc="dialogShow = false"
+    @keydown.enter="$emit('create', table)"
+  >
+    <v-card class="elevation-1 backgroundColor">
+      <!-- TODO  -->
+    </v-card>
+  </v-dialog>
+</template>
+
+<style scoped lang="scss"></style>

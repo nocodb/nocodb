20ca94d0b20e8d470d5ecf0a18f303c0680d3e06
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-22 15:57:24 +0200

    refactor(gui-v2): accept projectId as arg in `useProject`

--- packages/nc-gui-v2/composables/useProject.ts
+++ packages/nc-gui-v2/composables/useProject.ts
@@ -1,37 +1,46 @@
 import { SqlUiFactory } from 'nocodb-sdk'
 import type { ProjectType, TableType } from 'nocodb-sdk'
+import type { MaybeRef } from '@vueuse/core'
 import { useNuxtApp, useState } from '#app'
 import { USER_PROJECT_ROLES } from '~/lib/constants'
+import type { Role } from '~/lib/enums'
 
-export default (reloadOnRouteChange = false) => {
-  const projectRoles = useState<Record<string, boolean>>(USER_PROJECT_ROLES, () => ({}))
+interface UseProjectProps {
+  projectId?: MaybeRef<string>
+  reloadOnRouteChange?: MaybeRef<boolean>
+}
+
+type ProjectRoles = Record<Role, boolean>
+
+export default ({ projectId, reloadOnRouteChange = false }: UseProjectProps) => {
+  const projectRoles = useState<ProjectRoles>(USER_PROJECT_ROLES, () => ({} as ProjectRoles))
   const project = useState<ProjectType | null>('project')
   const tables = useState<TableType[]>('tables', () => [] as TableType[])
 
   const { $api } = useNuxtApp()
   const route = useRoute()
 
-  const projectId = $computed<string>(() => route.params?.projectId as string)
+  const _projectId = $computed(() => unref(projectId) ?? (route.params?.projectId as string))
 
   async function loadProjectRoles() {
-    projectRoles.value = {}
+    projectRoles.value = {} as ProjectRoles
 
     if (projectId) {
-      const user = await $api.auth.me({ project_id: projectId })
+      const user = await $api.auth.me({ project_id: _projectId })
       projectRoles.value = user.roles
     }
   }
 
   async function loadTables() {
     if (projectId) {
-      const tablesResponse = await $api.dbTable.list(projectId)
+      const tablesResponse = await $api.dbTable.list(_projectId)
       if (tablesResponse.list) tables.value = tablesResponse.list
     }
   }
 
   async function loadProject() {
     if (projectId) {
-      project.value = await $api.project.read(projectId)
+      project.value = await $api.project.read(_projectId)
       await loadProjectRoles()
     }
   }
@@ -42,21 +51,18 @@ export default (reloadOnRouteChange = false) => {
   const isPg = computed(() => projectBaseType === 'pg')
   const sqlUi = computed(() => SqlUiFactory.create({ client: projectBaseType }))
 
-  if (reloadOnRouteChange) {
-    watch(
-      () => route.params?.projectId,
-      async (id) => {
-        if (id) {
-          await loadProject()
-          await loadTables()
-        } else {
-          project.value = null
-          tables.value = []
-        }
-      },
-      { immediate: true },
-    )
-  }
+  watchEffect(async () => {
+    if (route.params?.projectId !== _projectId && unref(reloadOnRouteChange)) {
+      await loadProject()
+      await loadTables()
+    } else if (_projectId) {
+      await loadProject()
+      await loadTables()
+    } else {
+      project.value = null
+      tables.value = []
+    }
+  })
 
   return { project, tables, loadProjectRoles, loadProject, loadTables, isMysql, isPg, sqlUi }
 }

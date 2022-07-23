c553d955eec214acf3d38b7bfdf08799d21c0e40
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-07-23 19:02:30 +0800

    chore(gui-v2): remove quickImportType from ExcelUrlTemplateAdapter

--- packages/nc-gui-v2/utils/parsers/ExcelUrlTemplateAdapter.ts
+++ packages/nc-gui-v2/utils/parsers/ExcelUrlTemplateAdapter.ts
@@ -3,14 +3,12 @@ const { $api } = useNuxtApp()
 
 export default class ExcelUrlTemplateAdapter extends ExcelTemplateAdapter {
   url: string
-  quickImportType: string
   excelData: any
 
-  constructor(url: string, parserConfig: Record<string, any>, quickImportType: string) {
+  constructor(url: string, parserConfig: Record<string, any>) {
     const name = url.split('/').pop()
     super(name, parserConfig)
     this.url = url
-    this.quickImportType = quickImportType
     this.excelData = null
   }
 

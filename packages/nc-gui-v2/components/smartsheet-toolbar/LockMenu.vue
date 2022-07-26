ca3738e2777fe8102ff5c0013da2ed28dfe0cffc
Pranav C <pranavxc@gmail.com>
2022-07-27 00:07:50 +0530

    chore(gui-v2): lint
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui-v2/components/smartsheet-toolbar/LockMenu.vue
+++ packages/nc-gui-v2/components/smartsheet-toolbar/LockMenu.vue
@@ -96,7 +96,6 @@ export default {
         </div>
       </div>
     </template>
-
   </a-dropdown>
 </template>
 
--- packages/nc-gui-v2/components/smartsheet-toolbar/MoreActions.vue
+++ packages/nc-gui-v2/components/smartsheet-toolbar/MoreActions.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
-import MdiFlashIcon from "~icons/mdi/flash-outline";
-import MdiMenuDownIcon from "~icons/mdi/menu-down";
-import MdiDownloadIcon from "~icons/mdi/download-outline";
-import MdiUploadIcon from "~icons/mdi/upload-outline";
-import MdiHookIcon from "~icons/mdi/hook";
-import MdiViewListIcon from "~icons/mdi/view-list-outline";
+import MdiFlashIcon from '~icons/mdi/flash-outline'
+import MdiMenuDownIcon from '~icons/mdi/menu-down'
+import MdiDownloadIcon from '~icons/mdi/download-outline'
+import MdiUploadIcon from '~icons/mdi/upload-outline'
+import MdiHookIcon from '~icons/mdi/hook'
+import MdiViewListIcon from '~icons/mdi/view-list-outline'
 </script>
 
 <template>
@@ -13,29 +13,29 @@ import MdiViewListIcon from "~icons/mdi/view-list-outline";
       <div class="flex gap-1 align-center">
         <MdiFlashIcon class="text-grey" />
         <!-- More -->
-        {{ $t("general.more") }}
+        {{ $t('general.more') }}
         <MdiMenuDownIcon class="text-grey" />
       </div>
     </a-button>
     <template #overlay>
-      <div class=" bg-white shadow">
+      <div class="bg-white shadow">
         <div>
           <div class="nc-menu-item" @click.stop>
             <MdiDownloadIcon />
             <!-- Download as CSV -->
-            {{ $t("activity.downloadCSV") }}
+            {{ $t('activity.downloadCSV') }}
           </div>
-          <div class="nc-menu-item"  @click.stop>
+          <div class="nc-menu-item" @click.stop>
             <MdiUploadIcon />
             <!-- Upload CSV -->
-            {{ $t("activity.uploadCSV") }}
+            {{ $t('activity.uploadCSV') }}
           </div>
-          <div class="nc-menu-item"  @click.stop>
+          <div class="nc-menu-item" @click.stop>
             <MdiViewListIcon />
             <!-- Shared View List -->
-            {{ $t("activity.listSharedView") }}
+            {{ $t('activity.listSharedView') }}
           </div>
-          <div class="nc-menu-item"  @click.stop>
+          <div class="nc-menu-item" @click.stop>
             <MdiHookIcon />
             <!-- todo: i18n -->
             Webhook

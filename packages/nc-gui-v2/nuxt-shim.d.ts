82e8f7fae974426c727e7ad48685b21c751f332a
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-26 12:20:29 +0200

    fix(gui-v2): shim API functions (swagger outdated)

--- packages/nc-gui-v2/nuxt-shim.d.ts
+++ packages/nc-gui-v2/nuxt-shim.d.ts
@@ -1,13 +1,12 @@
-import type { RemovableRef } from '@vueuse/core'
-import type { Api } from 'nocodb-sdk'
-import type { I18n } from 'vue-i18n'
-import type { GlobalState } from '~/lib/types'
-
 import type messages from '@intlify/vite-plugin-vue-i18n/messages'
+import type { RemovableRef } from '@vueuse/core'
+import type { Api, FormType, GalleryType, GridType, RequestParams } from 'nocodb-sdk'
+import type { I18n } from 'vue-i18n'
+import type { GlobalState } from './src/lib/types'
 
 declare module '#app/nuxt' {
   interface NuxtApp {
-    $api: Api<any>;
+    $api: Api<any>
     $tele: {
       emit: (event: string, data: any) => void
     }
@@ -20,5 +19,14 @@ declare module '#app/nuxt' {
 declare module '@vue/runtime-core' {
   interface App {
     i18n: I18n<messages, unknown, unknown, false>['global']
+  }
+}
+
+declare module 'nocodb-sdk' {
+  interface Api<T> {
+    // todo: update swagger to accept correct data type
+    gridCreate: (tableId: string, data: GridType & { copy_from_id: string }, params?: RequestParams) => Promise<GridType>
+    formCreate: (tableId: string, data: FormType, params?: RequestParams) => Promise<FormType>
+    galleryCreate: (tableId: string, data: GalleryType, params?: RequestParams) => Promise<any>
   }
 }

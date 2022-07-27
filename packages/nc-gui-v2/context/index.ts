aca9b94bcfa68d21ec060e730bed35d55271502e
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-27 18:28:03 +0200

    chore(gui-v2): lint files

--- packages/nc-gui-v2/context/index.ts
+++ packages/nc-gui-v2/context/index.ts
@@ -1,4 +1,4 @@
-import type { ColumnType, FormType, GalleryType, GridType, KanbanType, TableType, ViewType } from 'nocodb-sdk'
+import type { ColumnType, FormType, GalleryType, GridType, KanbanType, TableType } from 'nocodb-sdk'
 import type { ComputedRef, InjectionKey, Ref } from 'vue'
 import type { EventHook } from '@vueuse/core'
 import type { useViewData } from '#imports'

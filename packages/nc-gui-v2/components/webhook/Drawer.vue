364ea59e22d6b70426e63194c704ad766ee7af12
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-07-27 17:30:48 +0800

    feat(gui-v2): include back to list

--- packages/nc-gui-v2/components/webhook/Drawer.vue
+++ packages/nc-gui-v2/components/webhook/Drawer.vue
@@ -25,7 +25,7 @@ function editHook(hook: Record<string, any>) {
 
 <template>
   <a-drawer v-model:visible="dialogShow" :closable="false" placement="right" width="700px" @keydown.esc="dialogShow = false">
-    <WebhookEditor v-if="editOrAdd" />
+    <WebhookEditor v-if="editOrAdd" @back-to-list="editOrAdd = false" />
     <WebhookList v-else @edit="editHook" @add="editOrAdd = true" />
     <div class="self-center flex flex-column flex-wrap gap-4 items-center mt-4 md:mx-8 md:justify-between justify-center">
       <a-button v-t="['e:hiring']" href="https://angel.co/company/nocodb" target="_blank" size="large">

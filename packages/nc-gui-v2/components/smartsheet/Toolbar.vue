0dd73e7535da668625976377be72f4121805e78d
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-27 12:34:04 +0200

    refactor(gui-v2): reduce toolbar height to header height

--- packages/nc-gui-v2/components/smartsheet/Toolbar.vue
+++ packages/nc-gui-v2/components/smartsheet/Toolbar.vue
@@ -1,22 +1,37 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div dense class="nc-table-toolbar w-100 p-1 flex gap-1 align-center" style="z-index: 7">
+  <div class="nc-table-toolbar w-100 p-1 flex gap-1 align-center" style="z-index: 7">
     <SmartsheetToolbarSearchData class="flex-shrink" />
+
     <SmartsheetToolbarFieldsMenu :show-system-fields="false" />
+
     <SmartsheetToolbarColumnFilterMenu />
+
     <SmartsheetToolbarSortListMenu />
+
     <SmartsheetToolbarShareView />
+
     <SmartsheetToolbarMoreActions />
+
     <div class="flex-1" />
+
     <SmartsheetToolbarLockMenu />
+
     <div class="dot" />
+
     <SmartsheetToolbarReload />
+
     <div class="dot" />
+
     <SmartsheetToolbarAddRow />
+
     <div class="dot" />
+
     <SmartsheetToolbarDeleteTable />
+
     <div class="dot" />
+
     <SmartsheetToolbarToggleDrawer class="mr-2" />
   </div>
 </template>

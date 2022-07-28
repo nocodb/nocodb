4b2834af0a273aab5d3c37a675762ef58a2475c8
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-28 09:23:05 +0200

    chore(gui-v2): update drawer toggle styles and add tooltip

--- packages/nc-gui-v2/components/smartsheet-toolbar/ToggleDrawer.vue
+++ packages/nc-gui-v2/components/smartsheet-toolbar/ToggleDrawer.vue
@@ -8,7 +8,13 @@ const Icon = computed(() => (navDrawerOpened.value ? MdiDoorOpenIcon : MdiDoorCl
 </script>
 
 <template>
-  <Icon class="text-grey" @click="navDrawerOpened = !navDrawerOpened" />
+  <a-tooltip>
+    <template #title> {{ $t('tooltip.toggleNavDraw') }} </template>
+    <Icon
+      class="rounded text-xl p-1 text-gray-500 hover:(text-white bg-pink-500 shadow)"
+      @click="navDrawerOpened = !navDrawerOpened"
+    />
+  </a-tooltip>
 </template>
 
 <style scoped></style>

07a11916e010255b7c43a24ea9eb4b0f1590186b
Pranav C <pranavxc@gmail.com>
2022-08-01 20:45:58 +0530

    refactor(gui-v2): code cleanup
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui-v2/components/smartsheet/Toolbar.vue
+++ packages/nc-gui-v2/components/smartsheet/Toolbar.vue
@@ -13,24 +13,11 @@
     <SmartsheetToolbarMoreActions />
 
     <div class="flex-1" />
-    <!--    <SmartsheetToolbarLockMenu />
-    <div class="dot" />
-    <SmartsheetToolbarReload />
-    <div class="dot" />
-    <SmartsheetToolbarAddRow />
-    <div class="dot" />
-    <SmartsheetToolbarDeleteTable />
-    <div class="dot" />
-    <SmartsheetToolbarToggleDrawer class="mr-2" />
- -->
   </div>
 </template>
 
 <style scoped>
 :deep(.nc-toolbar-btn) {
   @apply border-0 !text-xs font-semibold px-2;
-} /*
-.dot {
-  @apply w-[3px] h-[3px] bg-gray-300 mx-1 rounded-full;
-}*/
+}
 </style>

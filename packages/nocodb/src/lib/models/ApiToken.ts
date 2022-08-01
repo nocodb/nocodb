9d741b6bc70d59993273d02ad485e7db42f3dab1
Vijay Kumar Rathore <professional.vijay8492@gmail.com>
2022-08-01 17:30:11 +0200

    remove caching from api tokens

--- packages/nocodb/src/lib/models/ApiToken.ts
+++ packages/nocodb/src/lib/models/ApiToken.ts
@@ -1,12 +1,6 @@
-import {
-  CacheDelDirection,
-  CacheGetType,
-  CacheScope,
-  MetaTable
-} from '../utils/globals';
+import { MetaTable } from '../utils/globals';
 import Noco from '../Noco';
 import { nanoid } from 'nanoid';
-import NocoCache from '../cache/NocoCache';
 
 export default class ApiToken {
   project_id?: string;
@@ -31,42 +25,22 @@ export default class ApiToken {
       description: apiToken.description,
       token
     });
-    await NocoCache.appendToList(
-      CacheScope.API_TOKEN,
-      [],
-      `${CacheScope.API_TOKEN}:${token}`
-    );
     return this.getByToken(token);
   }
 
   static async list(ncMeta = Noco.ncMeta) {
-    let tokens = await NocoCache.getList(CacheScope.API_TOKEN, []);
-    if (!tokens.length) {
-      tokens = await ncMeta.metaList(null, null, MetaTable.API_TOKENS);
-      await NocoCache.setList(CacheScope.API_TOKEN, [], tokens);
-    }
+    const tokens = await ncMeta.metaList(null, null, MetaTable.API_TOKENS);
     return tokens?.map(t => new ApiToken(t));
   }
+
   static async delete(token, ncMeta = Noco.ncMeta) {
-    await NocoCache.deepDel(
-      CacheScope.API_TOKEN,
-      `${CacheScope.API_TOKEN}:${token}`,
-      CacheDelDirection.CHILD_TO_PARENT
-    );
     return await ncMeta.metaDelete(null, null, MetaTable.API_TOKENS, { token });
   }
 
   static async getByToken(token, ncMeta = Noco.ncMeta) {
-    let data =
-      token &&
-      (await NocoCache.get(
-        `${CacheScope.API_TOKEN}:${token}`,
-        CacheGetType.TYPE_OBJECT
-      ));
-    if (!data) {
-      data = await ncMeta.metaGet(null, null, MetaTable.API_TOKENS, { token });
-      await NocoCache.set(`${CacheScope.API_TOKEN}:${token}`, data);
-    }
+    const data = await ncMeta.metaGet(null, null, MetaTable.API_TOKENS, {
+      token,
+    });
     return data && new ApiToken(data);
   }
 }

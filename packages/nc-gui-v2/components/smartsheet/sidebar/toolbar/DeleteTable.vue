f6bfdd262daa99dcc245e8199b0e5250b9739763
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-08-02 09:15:17 +0200

    refactor(gui-v2): show tooltip in bottomEnd when sidebar is open

--- packages/nc-gui-v2/components/smartsheet/sidebar/toolbar/DeleteTable.vue
+++ packages/nc-gui-v2/components/smartsheet/sidebar/toolbar/DeleteTable.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-import { inject, useTable } from '#imports'
-import { MetaInj } from '~/context'
+import { inject, ref, useTable } from '#imports'
+import { MetaInj, RightSidebarInj } from '~/context'
 import MdiDeleteIcon from '~icons/mdi/delete-outline'
 
 const meta = inject(MetaInj)
 
 const { deleteTable } = useTable()
+
+const sidebarOpen = inject(RightSidebarInj, ref(true))
 </script>
 
 <template>
-  <a-tooltip placement="left">
+  <a-tooltip :placement="sidebarOpen ? 'bottomRight' : 'left'">
     <template #title> {{ $t('activity.deleteTable') }} </template>
 
     <div class="nc-sidebar-right-item hover:after:bg-red-500 group">

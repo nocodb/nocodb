af9b9d7c070fd710bdde989f9ef1880da6d2a87c
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-08-02 15:13:58 +0800

    wip(gui-v2): add count draft

---
+++ packages/nc-gui-v2/components/virtual-cell/Count.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// TODO: wait for Count Column implementation
+</script>
+
+<template>
+  <span class="prose-sm"></span>
+</template>
+
+<style scoped lang="scss">
+</style>

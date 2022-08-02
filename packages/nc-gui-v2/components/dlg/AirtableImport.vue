6d02903efadcce4786217fe273602ac3c6c66814
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-08-02 11:35:56 +0800

    chore(gui-v2): fix formatting

--- packages/nc-gui-v2/components/dlg/AirtableImport.vue
+++ packages/nc-gui-v2/components/dlg/AirtableImport.vue
@@ -335,8 +335,7 @@ onBeforeUnmount(() => {
           </div>
         </a-card>
         <div class="flex justify-center items-center">
-          <a-button v-if="showGoToDashboardButton" class="mt-4" size="large" @click="dialogShow = false"
-            >Go to Dashboard</a-button
+          <a-button v-if="showGoToDashboardButton" class="mt-4" size="large" @click="dialogShow = false">Go to Dashboard</a-button
           >
         </div>
       </div>

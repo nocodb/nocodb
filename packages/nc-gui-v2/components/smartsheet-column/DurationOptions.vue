290d3d36b1f8a4d67778c03046b9d84e38fd1702
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-08-03 16:13:47 +0800

    fix(gui-v2): remove unnecessary computed & type

--- packages/nc-gui-v2/components/smartsheet-column/DurationOptions.vue
+++ packages/nc-gui-v2/components/smartsheet-column/DurationOptions.vue
@@ -4,13 +4,12 @@ import { durationOptions } from '@/utils'
 
 const { formState, validateInfos, setAdditionalValidations, sqlUi, onDataTypeChange, onAlter } = useColumnCreateStoreOrThrow()
 
-const durationOptionList = computed(() =>
-  durationOptions.map((o: Record<string, any>) => ({
+const durationOptionList =
+  durationOptions.map((o) => ({
     ...o,
     // h:mm:ss (e.g. 3:45, 1:23:40)
     title: `${o.title} ${o.example}`,
-  })),
-)
+  })) || []
 </script>
 
 <template>
@@ -21,7 +20,7 @@ const durationOptionList = computed(() =>
     <a-col :span="24">
       <a-form-item label="Duration Format">
         <a-select v-model:value="formState.meta.duration" size="small" class="w-52">
-          <a-select-option v-for="(duration, i) in durationOptionList ?? []" :key="i" :value="duration.id">
+          <a-select-option v-for="(duration, i) of durationOptionList" :key="i" :value="duration.id">
             {{ duration.title }}
           </a-select-option>
         </a-select>

9e5cb492f9c55d9c490b1ffcfc037cfdea881831
Pranav C <pranavxc@gmail.com>
2022-08-03 16:20:02 +0530

    refactor(gui-v2): remove unused import
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui-v2/components/smartsheet-header/Cell.vue
+++ packages/nc-gui-v2/components/smartsheet-header/Cell.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { Modal } from 'ant-design-vue'
 import type { ColumnType, TableType } from 'nocodb-sdk'
 import type { Ref } from 'vue'
 import { inject } from 'vue'

c0a12602beb29acfb589361e7865e040889e542f
Pranav C <pranavxc@gmail.com>
2022-08-06 12:01:04 +0530

    feat(gui-v2): add save/update action
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui-v2/components/smartsheet/expanded-form/Header.vue
+++ packages/nc-gui-v2/components/smartsheet/expanded-form/Header.vue
@@ -1,24 +1,22 @@
 <script lang="ts" setup>
-import { computed } from '#imports'
-import { useExpandedFormStoreOrThrow, useSmartsheetStoreOrThrow, useUIPermission } from '#imports'
+import { computed, useExpandedFormStoreOrThrow, useSmartsheetStoreOrThrow, useUIPermission } from '#imports'
 import MdiDoorOpen from '~icons/mdi/door-open'
 import MdiDoorClosed from '~icons/mdi/door-closed'
 
-const {meta} = useSmartsheetStoreOrThrow()
+const { meta } = useSmartsheetStoreOrThrow()
 const { commentsDrawer, row, primaryValue, save } = useExpandedFormStoreOrThrow()
-const { isUIAllowed} =useUIPermission()
+const { isUIAllowed } = useUIPermission()
 
 const drawerToggleIcon = computed(() => (commentsDrawer.value ? MdiDoorOpen : MdiDoorClosed))
 
 // todo: accept as a prop / inject
-const iconColor =  '#1890ff'
-
+const iconColor = '#1890ff'
 </script>
 
 <template>
   <div class="flex p-2 align-center gap-2">
-    <h5 class="text-md font-weight-bold flex align-center gap-1 mb-0">
-      <mdi-table-arrow-right :style="{color:iconColor}"/>
+    <h5 class="text-lg font-weight-medium flex align-center gap-1 mb-0">
+      <mdi-table-arrow-right :style="{ color: iconColor }" />
 
       <template v-if="meta">
         {{ meta.title }}
@@ -29,14 +27,14 @@ const iconColor =  '#1890ff'
       <template v-if="primaryValue">: {{ primaryValue }}</template>
     </h5>
     <div class="flex-grow" />
-    <mdi-reload/>
+    <mdi-reload />
     <component :is="drawerToggleIcon" class="" @click="commentsDrawer = !commentsDrawer" />
     <a-button size="small" class="!text">
       <!-- Cancel -->
       {{ $t('general.cancel') }}
     </a-button>
     <a-button size="small" :disabled="!isUIAllowed('tableRowUpdate')" type="primary" @click="save">
-      <!--Save Row-->
+      <!-- Save Row -->
       {{ $t('activity.saveRow') }}
     </a-button>
   </div>

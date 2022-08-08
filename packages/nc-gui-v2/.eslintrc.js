d704c37dbf1d3ed676bd495f4295ff39eb9a50ad
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-08-08 19:37:01 +0200

    chore(gui-v2): add eslint rule to error unused vars

--- packages/nc-gui-v2/.eslintrc.js
+++ packages/nc-gui-v2/.eslintrc.js
@@ -2,6 +2,7 @@ const baseRules = {
   'vue/no-setup-props-destructure': 0,
   'no-console': 0,
   'antfu/if-newline': 0,
+  'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   'prettier/prettier': ['error', {}, { usePrettierrc: true }],
 }
 

9131ab6c419a7605f5120e387722fc9e40fca6af
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-08-08 20:03:04 +0200

    chore(gui-v2): fix types in Reload

--- packages/nc-gui-v2/components/smartsheet/sidebar/toolbar/Reload.vue
+++ packages/nc-gui-v2/components/smartsheet/sidebar/toolbar/Reload.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import { ReloadViewDataHookInj, RightSidebarInj } from '~/context'
-import MdiReloadIcon from '~icons/mdi/reload'
 import { inject, ref } from '#imports'
 
-const reloadTri = inject(ReloadViewDataHookInj)
+const reloadTri = inject(ReloadViewDataHookInj)!
 
 const sidebarOpen = inject(RightSidebarInj, ref(true))
 </script>
@@ -13,9 +12,7 @@ const sidebarOpen = inject(RightSidebarInj, ref(true))
     <template #title> {{ $t('general.reload') }} </template>
 
     <div class="nc-sidebar-right-item hover:after:bg-green-500 group">
-      <MdiReloadIcon class="group-hover:(!text-white)" @click="reloadTri.trigger()" />
+      <MdiReload class="group-hover:(!text-white)" @click="reloadTri.trigger(null)" />
     </div>
   </a-tooltip>
 </template>
-
-<style scoped></style>

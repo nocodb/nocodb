b92f8263adfb4f976f0b0ab7a9b8f4c97fcb0b38
Muhammed Mustafa <mustafa010199@gmail.com>
2022-08-12 14:35:52 +0530

    refactor/gui-v2-Share view minor cleanup

--- packages/nc-gui-v2/composables/useViewSorts.ts
+++ packages/nc-gui-v2/composables/useViewSorts.ts
@@ -31,7 +31,6 @@ export function useViewSorts(
 
   const loadSorts = async () => {
     if (isPublic.value) {
-      console.log('load sorts public', sharedView.value?.sorts)
       const sharedSorts = sharedView.value?.sorts || []
       sorts.value = [...sharedSorts]
       return

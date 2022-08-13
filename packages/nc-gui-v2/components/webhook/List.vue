bab571e2765fe987ed0f498d1fe6a0a62443c230
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-08-13 17:11:37 +0800

    fix(gui-v2): add scrollbar for listing

--- packages/nc-gui-v2/components/webhook/List.vue
+++ packages/nc-gui-v2/components/webhook/List.vue
@@ -58,7 +58,11 @@ onMounted(() => {
     </div>
     <a-divider />
     <div v-if="hooks.length">
-      <a-list item-layout="horizontal" :data-source="hooks" class="cursor-pointer pl-5 pr-5 pt-2 pb-2">
+      <a-list
+        item-layout="horizontal"
+        :data-source="hooks"
+        class="cursor-pointer max-h-[75vh] overflow-y-auto scrollbar-thin-primary"
+      >
         <template #renderItem="{ item, index }">
           <a-list-item class="pa-2" @click="emit('edit', item)">
             <a-list-item-meta>

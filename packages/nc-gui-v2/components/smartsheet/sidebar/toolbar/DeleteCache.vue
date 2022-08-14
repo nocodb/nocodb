ca1b670c0f11fbaf1c2babedb14f6e6facf1cadf
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-08-14 17:41:53 +0800

    feat(gui-v2): split into components to make toolbar clean

---
+++ packages/nc-gui-v2/components/smartsheet/sidebar/toolbar/DeleteCache.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { message } from 'ant-design-vue'
+const { api } = useApi()
+
+async function deleteCache() {
+  try {
+    await api.utils.cacheDelete()
+    message.info('Deleted Cache Successfully')
+  } catch (e: any) {
+    message.error(e.message)
+  }
+}
+</script>
+
+<template>
+  <a-tooltip>
+    <template #title>
+      <span> Delete Cache </span>
+    </template>
+    <mdi-delete class="cursor-pointer mx-3" @click="deleteCache" />
+  </a-tooltip>
+</template>
---
+++ packages/nc-gui-v2/components/smartsheet/sidebar/toolbar/ExportCache.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { message } from 'ant-design-vue'
+import FileSaver from 'file-saver'
+
+const { api } = useApi()
+
+async function exportCache() {
+  try {
+    const data = await api.utils.cacheGet()
+    if (!data) {
+      message.info('Cache is empty')
+      return
+    }
+    const blob = new Blob([JSON.stringify(data)], {
+      type: 'text/plain;charset=utf-8',
+    })
+    FileSaver.saveAs(blob, 'cache_exported.json')
+    message.info('Exported Cache Successfully')
+  } catch (e: any) {
+    message.error(e.message)
+  }
+}
+</script>
+
+<template>
+  <!-- Export Cache -->
+  <a-tooltip>
+    <template #title>
+      <span> Export Cache </span>
+    </template>
+    <mdi-export class="cursor-pointer mx-3" @click="exportCache" />
+  </a-tooltip>
+</template>

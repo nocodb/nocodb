8818227606d5a5769b66bdfa74e0d06e0e684ec1
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-08-15 18:59:47 +0800

    feat(gui-v2): add debug meta

---
+++ packages/nc-gui-v2/components/smartsheet/sidebar/toolbar/DebugMeta.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { MetaInj } from '#imports'
+
+const editorOpen = ref(false)
+
+const meta = $(inject(MetaInj))
+</script>
+
+<template>
+  <a-tooltip>
+    <template #title>
+      <span> Debug Meta </span>
+    </template>
+    <mdi-bug-outline class="cursor-pointer" @click="editorOpen = true" />
+  </a-tooltip>
+
+  <a-modal v-model:visible="editorOpen" :footer="null" width="80%" :title="meta.title">
+    <MonacoEditor ref="jsonEditorRef" v-model="meta" class="h-max-[70vh]" />
+  </a-modal>
+</template>

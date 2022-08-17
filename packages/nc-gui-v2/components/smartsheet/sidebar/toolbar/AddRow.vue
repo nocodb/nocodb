41b392f3ff7d03f9b7d5203013b1c97532041817
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-08-17 18:27:13 +0800

    feat(gui-v2): disable add row in form view

--- packages/nc-gui-v2/components/smartsheet/sidebar/toolbar/AddRow.vue
+++ packages/nc-gui-v2/components/smartsheet/sidebar/toolbar/AddRow.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 const emits = defineEmits(['addRow'])
 const { isOpen } = useSidebar({ storageKey: 'nc-right-sidebar' })
+const isLocked = inject(IsLockedInj)
 </script>
 
 <template>
   <a-tooltip :placement="isOpen ? 'bottomRight' : 'left'">
     <template #title> {{ $t('activity.addRow') }} </template>
-
-    <div class="nc-sidebar-right-item hover:after:bg-primary/75 group nc-sidebar-add-row">
-      <MdiPlusOutline class="cursor-pointer group-hover:(!text-white)" @click="emits('addRow')" />
+    <div
+      :class="{ 'hover:after:bg-primary/75 group': !isLocked, 'disabled-ring': isLocked }"
+      class="nc-sidebar-right-item nc-sidebar-add-row"
+    >
+      <MdiPlusOutline
+        :class="{ 'cursor-pointer group-hover:(!text-white)': !isLocked, 'disabled': isLocked }"
+        @click="!isLocked ? emits('addRow') : {}"
+      />
     </div>
   </a-tooltip>
 </template>

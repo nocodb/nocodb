6dea33e1e3e3ee2be2f1f1f88478d6e62bad7831
Pranav C <pranavxc@gmail.com>
2022-08-20 19:36:20 +0530

    chore(gui-v2): remove duplicate and unused components
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui-v2/utils/viewUtils.ts
+++ packages/nc-gui-v2/utils/viewUtils.ts
@@ -1,4 +1,5 @@
 import { ViewTypes } from 'nocodb-sdk'
+import { themeV2Colors } from '~/utils'
 
 import MdiGridIcon from '~icons/mdi/grid-large'
 import MdiFormIcon from '~icons/mdi/form-select'
@@ -9,7 +10,7 @@ import MdiEyeIcon from '~icons/mdi/eye-circle-outline'
 
 export const viewIcons = {
   [ViewTypes.GRID]: { icon: MdiGridIcon, color: '#8f96f2' },
-  [ViewTypes.FORM]: { icon: MdiFormIcon, color: '#ec4899' },
+  [ViewTypes.FORM]: { icon: MdiFormIcon, color: themeV2Colors.pink['500'] },
   calendar: { icon: MdiCalendarIcon, color: 'purple' },
   [ViewTypes.GALLERY]: { icon: MdiGalleryIcon, color: 'orange' },
   [ViewTypes.KANBAN]: { icon: MdiKanbanIcon, color: 'green' },

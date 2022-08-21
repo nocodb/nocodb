55335300c43f46d39ea67a2c68746d1a61fb0ea2
Raju Udava <86527202+dstala@users.noreply.github.com>
2022-08-21 22:34:46 +0530

    fix: isUIAllwoed check for add new table btn
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- packages/nc-gui-v2/components/dashboard/TreeView.vue
+++ packages/nc-gui-v2/components/dashboard/TreeView.vue
@@ -232,6 +232,7 @@ function openTableCreateDialog() {
         <div class="flex-1">
           <div
             class="group flex items-center gap-2 pl-5 pr-3 py-2 text-primary/70 hover:(text-primary/100) cursor-pointer select-none"
+            v-if="isUIAllowed('table-create')"
             @click="openTableCreateDialog"
           >
             <MdiPlus />

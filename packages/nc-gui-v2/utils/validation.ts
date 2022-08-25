c0523f36b69dbabf459b47271bc96ff028ea4f58
mertmit <mertmit99@gmail.com>
2022-08-25 13:12:10 +0300

    fix: connection interfaces for dataSource.connection
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui-v2/utils/validation.ts
+++ packages/nc-gui-v2/utils/validation.ts
@@ -129,7 +129,7 @@ export const importExcelUrlValidator = {
 }
 
 export const extraParameterValidator = {
-  validator: (rule: any, value: any) => {
+  validator: (_: unknown, value: any) => {
     return new Promise((resolve, reject) => {
       for (const param of value) {
         if (param.key === '') {

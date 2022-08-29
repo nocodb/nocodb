4246fefab65249b560c5c9e5019977f1a28cff63
mertmit <mertmit99@gmail.com>
2022-08-29 18:46:50 +0300

    fix: use function scope instead of vue's effect scope
    by @bcakmakoglu
    
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui-v2/composables/useInjectionState/index.ts
+++ packages/nc-gui-v2/composables/useInjectionState/index.ts
@@ -1,22 +1,20 @@
-import type { EffectScope, InjectionKey } from 'vue'
-import { getCurrentScope } from 'vue'
-
-type ExtendedScope<T> = { [key: symbol]: T } & EffectScope
+import type { InjectionKey } from 'vue'
+import { inject, provide, tryOnScopeDispose } from '#imports'
 
 export function useInjectionState<Arguments extends any[], Return>(
   composable: (...args: Arguments) => Return,
   keyName = 'InjectionState',
 ): readonly [useInjectionState: (...args: Arguments) => Return, useInjectedState: () => Return | undefined] {
-  const keySymbol = Symbol(keyName)
-  const key: string | InjectionKey<Return> = keySymbol
+  const key: string | InjectionKey<Return> = Symbol(keyName)
+
+  let providableState: Return | undefined
 
   const useProvidingState = (...args: Arguments) => {
     const providedState = composable(...args)
 
-    const currentScope = getCurrentScope() as ExtendedScope<Return>
-    currentScope[keySymbol] = providedState
-
     provide(key, providedState)
+
+    providableState = providedState
 
     return providedState
   }
@@ -25,12 +23,15 @@ export function useInjectionState<Arguments extends any[], Return>(
     let injection = inject(key, undefined)
 
     if (typeof injection === 'undefined') {
-      const currentScope = getCurrentScope() as ExtendedScope<Return>
-      injection = currentScope[keySymbol]
+      injection = providableState
     }
 
     return injection
   }
+
+  tryOnScopeDispose(() => {
+    providableState = undefined
+  })
 
   return [useProvidingState, useInjectedState]
 }

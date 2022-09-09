a16ba2e05f4ae28113889df18cea7bd7d70adc30
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-09-09 12:07:01 +0800

    feat(nc-gui): add kanban view in menu bottom

--- packages/nc-gui/components/smartsheet/sidebar/MenuBottom.vue
+++ packages/nc-gui/components/smartsheet/sidebar/MenuBottom.vue
@@ -95,6 +95,28 @@ function onOpenModal(type: ViewTypes, title = '') {
         </a-tooltip>
       </a-menu-item>
 
+      <a-menu-item
+        key="kanban"
+        class="group !flex !items-center !my-0 nc-create-kanban-view"
+        @click="onOpenModal(ViewTypes.KANBAN)"
+      >
+        <a-tooltip :mouse-enter-delay="1" placement="left">
+          <template #title>
+            {{ $t('msg.info.addView.kanban') }}
+          </template>
+
+          <div class="nc-project-menu-item text-xs flex items-center h-full w-full gap-2">
+            <component :is="viewIcons[ViewTypes.KANBAN].icon" :style="{ color: viewIcons[ViewTypes.KANBAN].color }" />
+
+            <div>{{ $t('objects.viewType.kanban') }}</div>
+
+            <div class="flex-1" />
+
+            <mdi-plus class="group-hover:text-primary" />
+          </div>
+        </a-tooltip>
+      </a-menu-item>
+
       <div class="w-full h-4" />
     </div>
   </a-menu>

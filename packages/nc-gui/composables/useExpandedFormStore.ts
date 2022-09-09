20a4af26b0577f8d13ff1a46555f0536ac8f5b61
Pranav C <pranavxc@gmail.com>
2022-09-09 23:16:45 +0530

    fix(gui): avoid linking if new record
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui/composables/useExpandedFormStore.ts
+++ packages/nc-gui/composables/useExpandedFormStore.ts
@@ -30,7 +30,6 @@ const [useProvideExpandedFormStore, useExpandedFormStore] = useInjectionState((m
   const { project } = useProject()
   const rowStore = useProvideSmartsheetRowStore(meta, row)
   const { sharedView } = useSharedView() as Record<string, any>
-  const reloadTrigger = inject(ReloadViewDataHookInj)!
 
   // todo
   // const activeView = inject(ActiveViewInj)
@@ -170,7 +169,6 @@ const [useProvideExpandedFormStore, useExpandedFormStore] = useInjectionState((m
       message.success(`${primaryValue.value || 'Row'} updated successfully.`)
 
       changedColumns.value = new Set()
-      reloadTrigger?.trigger()
     } catch (e: any) {
       message.error(`${t('msg.error.rowUpdateFailed')}: ${await extractSdkResponseErrorMsg(e)}`)
     }

16b95bc717c4fb8038d05bba2d2b0b5a47b1bad5
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-09-14 18:26:30 +0800

    feat(nc-gui): add nc-kanban-btn & refactor handleSubmit

--- packages/nc-gui/components/smartsheet-toolbar/KanbanStackEditOrAdd.vue
+++ packages/nc-gui/components/smartsheet-toolbar/KanbanStackEditOrAdd.vue
@@ -24,33 +24,34 @@ const isLocked = inject(IsLockedInj, ref(false))
 
 const addOrEditStackDropdown = ref(false)
 
+const handleSubmit = async () => {
+  addOrEditStackDropdown.value = false
+  await loadKanbanMeta()
+  await loadKanbanData()
+}
+
 provide(IsKanbanInj, ref(true))
 </script>
 
 <template>
   <a-dropdown v-if="isUIAllowed('edit-column')" v-model:visible="addOrEditStackDropdown" :trigger="['click']">
-    <a-button v-t="['c:kanban-stack-edit-or-add']" class="nc-fields-menu-btn nc-toolbar-btn" :disabled="isLocked">
-      <div class="flex items-center gap-1">
-        <mdi-plus-circle-outline />
-        <span class="text-capitalize !text-sm font-weight-normal">
-          <!-- TODO: i18n -->
-          Add / Edit Stack
-        </span>
-        <MdiMenuDown class="text-grey" />
-      </div>
-    </a-button>
-
+    <div class="nc-kanban-btn">
+      <a-button v-t="['c:kanban-stack-edit-or-add']" class="nc-fields-menu-btn nc-toolbar-btn" :disabled="isLocked">
+        <div class="flex items-center gap-1">
+          <mdi-plus-circle-outline />
+          <span class="text-capitalize !text-sm font-weight-normal">
+            <!-- TODO: i18n -->
+            Add / Edit Stack
+          </span>
+          <MdiMenuDown class="text-grey" />
+        </div>
+      </a-button>
+    </div>
     <template #overlay>
       <SmartsheetColumnEditOrAddProvider
         v-if="addOrEditStackDropdown"
         :column="groupingFieldColumn"
-        @submit="
-          ;async () => {
-            addOrEditStackDropdown = false
-            await loadKanbanMeta()
-            await loadKanbanData()
-          }
-        "
+        @submit="handleSubmit"
         @cancel="addOrEditStackDropdown = false"
         @click.stop
         @keydown.stop

9d7cc03ffd87c3545da57d3512697204c3609671
elvus <lvssgv@gmail.com>
2022-09-13 21:17:29 -0400

    comments added

--- packages/nc-gui/components/smartsheet-toolbar/SharedViewList.vue
+++ packages/nc-gui/components/smartsheet-toolbar/SharedViewList.vue
@@ -103,7 +103,6 @@ const deleteLink = async (id: string) => {
       :locale="{
         emptyText: $t('labels.noData'),
       }"
-      :scroll="{ x: 400 }"
     >
       <template #emptyText>
         <a-empty :image="Empty.PRESENTED_IMAGE_SIMPLE" :description="$t('labels.noData')" />

ed2d640da4861d49dbddb068d65dac2f8387dc05
Pranav C <pranavxc@gmail.com>
2022-09-17 00:50:20 +0530

    enhancement(gui): add not found page for handling invalid base url
    re #3648
    
    Signed-off-by: Pranav C <pranavxc@gmail.com>

---
+++ packages/nc-gui/pages/404.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { definePageMeta } from '#imports'
+
+definePageMeta({
+  title: 'Page Not Found',
+  public: true,
+})
+</script>
+
+<template>
+  <div class="w-full h-[300px] flex justify-center items-center text-4xl">
+    <h1 class="text-gray-400">Page Not Found</h1>
+  </div>
+</template>

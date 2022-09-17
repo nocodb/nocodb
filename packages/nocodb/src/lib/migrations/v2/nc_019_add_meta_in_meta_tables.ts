aa376101ffba5f234ee1bd95a2439030fcfb4597
Pranav C <pranavxc@gmail.com>
2022-09-17 11:48:29 +0530

    feat(api): add meta in grid and gallery view
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb/src/lib/migrations/v2/nc_019_add_meta_in_meta_tables.ts
+++ packages/nocodb/src/lib/migrations/v2/nc_019_add_meta_in_meta_tables.ts
@@ -8,6 +8,12 @@ const up = async (knex: Knex) => {
   await knex.schema.alterTable(MetaTable.FORM_VIEW_COLUMNS, (table) => {
     table.text('meta');
   });
+  await knex.schema.alterTable(MetaTable.GRID_VIEW, (table) => {
+    table.text('meta');
+  });
+  await knex.schema.alterTable(MetaTable.GALLERY_VIEW, (table) => {
+    table.text('meta');
+  });
 };
 
 const down = async (knex) => {
@@ -15,6 +21,12 @@ const down = async (knex) => {
     table.dropColumns('meta');
   });
   await knex.schema.alterTable(MetaTable.FORM_VIEW_COLUMNS, (table) => {
+    table.dropColumns('meta');
+  });
+  await knex.schema.alterTable(MetaTable.GRID_VIEW, (table) => {
+    table.dropColumns('meta');
+  });
+  await knex.schema.alterTable(MetaTable.GALLERY_VIEW, (table) => {
     table.dropColumns('meta');
   });
 };

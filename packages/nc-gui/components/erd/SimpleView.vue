1d19e4822e96894fceccfc9b4e3511783979dbf5
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-09-17 21:20:17 +0200

    chore(nc-gui): lint

--- packages/nc-gui/components/erd/SimpleView.vue
+++ packages/nc-gui/components/erd/SimpleView.vue
@@ -24,6 +24,8 @@ const { metasWithIdAsKey } = useMetas()
 
 const { $destroy, fitView } = useVueFlow()
 
+const isTransitioning = ref(true)
+
 const nodes = ref<Node[]>([])
 const edges = ref<Edge[]>([])
 
@@ -193,35 +195,41 @@ init()
 onScopeDispose($destroy)
 
 watch([() => tables, () => config], () => init(true), { deep: true, flush: 'pre' })
+
+useEventListener('transitionend', () => {
+  isTransitioning.value = false
+})
 </script>
 
 <template>
-  <VueFlow :nodes="nodes" :edges="edges" fit-view-on-init elevate-edges-on-select>
-    <Controls class="!left-auto right-2 !top-3.5 !bottom-auto" :show-fit-view="false" :show-interactive="false" />
+  <Transition name="layout" mode="out-in">
+    <VueFlow v-if="!isTransitioning" :nodes="nodes" :edges="edges" fit-view-on-init elevate-edges-on-select>
+      <Controls class="!left-auto right-2 !top-3.5 !bottom-auto" :show-fit-view="false" :show-interactive="false" />
 
-    <template #node-custom="props">
-      <TableNode :data="props.data" />
-    </template>
+      <template #node-custom="props">
+        <TableNode :data="props.data" />
+      </template>
 
-    <template #edge-custom="props">
-      <RelationEdge v-bind="props" />
-    </template>
+      <template #edge-custom="props">
+        <RelationEdge v-bind="props" />
+      </template>
 
-    <Background />
+      <Background />
 
-    <div
-      v-if="!config.singleTableMode"
-      class="absolute bottom-0 right-0 flex flex-col text-xs bg-white px-2 py-1 border-1 rounded-md border-gray-200"
-      style="font-size: 0.6rem"
-    >
-      <div class="flex flex-row items-center space-x-1 border-b-1 pb-1 border-gray-100">
-        <MdiTableLarge class="text-primary" />
-        <div>{{ $t('objects.table') }}</div>
+      <div
+        v-if="!config.singleTableMode"
+        class="absolute bottom-0 right-0 flex flex-col text-xs bg-white px-2 py-1 border-1 rounded-md border-gray-200 z-50 nc-erd-histogram"
+        style="font-size: 0.6rem"
+      >
+        <div class="flex flex-row items-center space-x-1 border-b-1 pb-1 border-gray-100">
+          <MdiTableLarge class="text-primary" />
+          <div>{{ $t('objects.table') }}</div>
+        </div>
+        <div class="flex flex-row items-center space-x-1 pt-1">
+          <MdiEyeCircleOutline class="text-primary" />
+          <div>{{ $t('objects.sqlVIew') }}</div>
+        </div>
       </div>
-      <div class="flex flex-row items-center space-x-1 pt-1">
-        <MdiEyeCircleOutline class="text-primary" />
-        <div>{{ $t('objects.sqlVIew') }}</div>
-      </div>
-    </div>
-  </VueFlow>
+    </VueFlow>
+  </Transition>
 </template>

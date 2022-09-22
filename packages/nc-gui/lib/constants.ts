e6d7d7f9044e4be1ee173999661345d7490d5df4
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-09-22 17:35:55 +0200

    chore(nc-gui): remove unused constant

--- packages/nc-gui/lib/constants.ts
+++ packages/nc-gui/lib/constants.ts
@@ -1,4 +1,3 @@
 export const NOCO = 'noco'
-export const USER_PROJECT_ROLES = 'user_project_roles'
 export const SYSTEM_COLUMNS = ['id', 'title', 'created_at', 'updated_at']
 export const BASE_URL = process.env.NC_BACKEND_URL || (process.env.NODE_ENV === 'production' ? '..' : 'http://localhost:8080')

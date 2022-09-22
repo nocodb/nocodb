40264e0c7a2ff18d3789ccfde54d46706a03961f
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-09-23 00:34:55 +0200

    revert(nc-gui): constant for base url

--- packages/nc-gui/lib/constants.ts
+++ packages/nc-gui/lib/constants.ts
@@ -4,8 +4,7 @@ export const NOCO = 'noco'
 
 export const SYSTEM_COLUMNS = ['id', 'title', 'created_at', 'updated_at']
 
-export const BASE_URL =
-  process.env.NC_BACKEND_URL || (process.env.NODE_ENV === 'production' ? 'http://localhost:8080' : 'http://localhost:8080')
+export const BASE_URL = process.env.NC_BACKEND_URL || (process.env.NODE_ENV === 'production' ? '..' : 'http://localhost:8080')
 
 export const rolePermissions = {
   // general role permissions

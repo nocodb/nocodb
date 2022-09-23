844c4dda0f00516d21419a5b507a1015973097ac
Muhammed Mustafa <mustafa010199@gmail.com>
2022-09-23 17:52:35 +0530

    fix(cypress): Added ERD test with pg and sqlite

--- scripts/cypress/support/page_objects/projectConstants.js
+++ scripts/cypress/support/page_objects/projectConstants.js
@@ -1,19 +1,19 @@
 export const defaultDbParams = {
-  databaseType: 0, // MySQL
-  hostAddress: "localhost",
-  portNumber: "3306",
-  username: "root",
-  password: "password",
-  databaseName: "sakila",
+    databaseType: 0, // MySQL
+    hostAddress: "localhost",
+    portNumber: "3306",
+    username: "root",
+    password: "password",
+    databaseName: "sakila",
 };
 
 export const defaultPgDbParams = {
-  databaseType: 1, // Postgres
-  hostAddress: "localhost",
-  portNumber: "5432",
-  username: "postgres",
-  password: "password",
-  databaseName: "postgres",
+    databaseType: 1, // Postgres
+    hostAddress: "localhost",
+    portNumber: "5432",
+    username: "postgres",
+    password: "password",
+    databaseName: "postgres",
 };
 
 // database
@@ -24,162 +24,157 @@ export const defaultPgDbParams = {
 //          editComment: add comment
 //          shareView: right navigation bar (share options)
 export const roles = {
-  owner: {
-    name: "owner",
-    credentials: { username: "user@nocodb.com", password: "Password123." },
-    validations: {
-      advSettings: true,
-      editSchema: true,
-      editData: true,
-      editComment: true,
-      shareView: true,
+    owner: {
+        name: "owner",
+        credentials: { username: "user@nocodb.com", password: "Password123." },
+        validations: {
+            advSettings: true,
+            editSchema: true,
+            editData: true,
+            editComment: true,
+            shareView: true,
+        },
     },
-  },
-  creator: {
-    name: "creator",
-    credentials: {
-      username: "creator@nocodb.com",
-      password: "Password123.",
+    creator: {
+        name: "creator",
+        credentials: {
+            username: "creator@nocodb.com",
+            password: "Password123.",
+        },
+        validations: {
+            advSettings: true,
+            editSchema: true,
+            editData: true,
+            editComment: true,
+            shareView: true,
+        },
     },
-    validations: {
-      advSettings: true,
-      editSchema: true,
-      editData: true,
-      editComment: true,
-      shareView: true,
+    editor: {
+        name: "editor",
+        credentials: {
+            username: "editor@nocodb.com",
+            password: "Password123.",
+        },
+        validations: {
+            advSettings: false,
+            editSchema: false,
+            editData: true,
+            editComment: true,
+            shareView: false,
+        },
     },
-  },
-  editor: {
-    name: "editor",
-    credentials: {
-      username: "editor@nocodb.com",
-      password: "Password123.",
+    commenter: {
+        name: "commenter",
+        credentials: {
+            username: "commenter@nocodb.com",
+            password: "Password123.",
+        },
+        validations: {
+            advSettings: false,
+            editSchema: false,
+            editData: false,
+            editComment: true,
+            shareView: false,
+        },
     },
-    validations: {
-      advSettings: false,
-      editSchema: false,
-      editData: true,
-      editComment: true,
-      shareView: false,
+    viewer: {
+        name: "viewer",
+        credentials: {
+            username: "viewer@nocodb.com",
+            password: "Password123.",
+        },
+        validations: {
+            advSettings: false,
+            editSchema: false,
+            editData: false,
+            editComment: false,
+            shareView: false,
+        },
     },
-  },
-  commenter: {
-    name: "commenter",
-    credentials: {
-      username: "commenter@nocodb.com",
-      password: "Password123.",
-    },
-    validations: {
-      advSettings: false,
-      editSchema: false,
-      editData: false,
-      editComment: true,
-      shareView: false,
-    },
-  },
-  viewer: {
-    name: "viewer",
-    credentials: {
-      username: "viewer@nocodb.com",
-      password: "Password123.",
-    },
-    validations: {
-      advSettings: false,
-      editSchema: false,
-      editData: false,
-      editComment: false,
-      shareView: false,
-    },
-  },
 };
 
 // default projects
 //
 export const staticProjects = {
-  sampleREST: {
-    basic: { dbType: "none", apiType: "REST", name: "sampleREST" },
-    config: {},
-  },
-  sampleGQL: {
-    basic: { dbType: "none", apiType: "GQL", name: "sampleGQL" },
-    config: {},
-  },
-  externalREST: {
-    basic: { dbType: "external", apiType: "REST", name: "externalREST" },
-    config: defaultDbParams,
-  },
-  externalGQL: {
-    basic: { dbType: "external", apiType: "GQL", name: "externalGQL" },
-    config: defaultDbParams,
-  },
-  pgExternalREST: {
-    basic: { dbType: "external", apiType: "REST", name: "pgExtREST" },
-    config: defaultPgDbParams,
-  },
-  pgExternalGQL: {
-    basic: { dbType: "external", apiType: "GQL", name: "pgExternalGQL" },
-    config: defaultPgDbParams,
-  },
+    sampleREST: {
+        basic: { dbType: "none", apiType: "REST", name: "sampleREST" },
+        config: {},
+    },
+    sampleGQL: {
+        basic: { dbType: "none", apiType: "GQL", name: "sampleGQL" },
+        config: {},
+    },
+    externalREST: {
+        basic: { dbType: "external", apiType: "REST", name: "externalREST" },
+        config: defaultDbParams,
+    },
+    externalGQL: {
+        basic: { dbType: "external", apiType: "GQL", name: "externalGQL" },
+        config: defaultDbParams,
+    },
+    pgExternalREST: {
+        basic: { dbType: "external", apiType: "REST", name: "pgExtREST" },
+        config: defaultPgDbParams,
+    },
+    pgExternalGQL: {
+        basic: { dbType: "external", apiType: "GQL", name: "pgExternalGQL" },
+        config: defaultPgDbParams,
+    },
 };
 
 // return TRUE if test suite specified is activated from env-variables
 //
 export const isTestSuiteActive = (apiType, dbType) => {
-  const env = Cypress.env("testMode");
-  return env.some(
-    (element) => element.apiType === apiType && element.dbType === dbType
-  );
+    const env = Cypress.env("testMode");
+    return env.some(
+        (element) => element.apiType === apiType && element.dbType === dbType
+    );
 };
 
 let currentTestMode = { apiType: null, dbType: null };
 let xcdbProjectString = ``;
 export function setCurrentMode(apiType, dbType) {
-  currentTestMode = { apiType: apiType, dbType: dbType };
+    currentTestMode = { apiType: apiType, dbType: dbType };
 }
 
 export function getCurrentMode() {
-  return currentTestMode;
+    return currentTestMode;
 }
 
 export function isXcdb() {
-  return currentTestMode.dbType === "xcdb";
+    return currentTestMode.dbType === "xcdb";
 }
 
 export function isPostgres() {
-  return currentTestMode.dbType === "postgres";
+    return currentTestMode.dbType === "postgres";
 }
 
 export function setProjectString(projStr) {
-  xcdbProjectString = projStr;
+    xcdbProjectString = projStr;
 }
 
 export function getProjectString() {
-  return xcdbProjectString;
+    return xcdbProjectString;
 }
 
-const sakilaTables = [
-  "actor",
-  "address",
-  "category",
-  "city",
-  "country",
-  "customer",
-  "film",
-  "film_text",
-  "language",
-  "payment",
-  "rental",
-  "staff",
-];
+const mysqlSakilaTables = [
+    'actor', 'address', 'category', 'city', 'country', 'customer', 'film', 'film_text', 'language', 'payment', 'rental', 'staff'
+]
 
-const sakilaSqlViews = [
-  "actor_info",
-  "customer_list",
-  "film_list",
-  "nicer_but_slower_film_list",
-  "sales_by_film_category",
-  "sales_by_store",
-  "staff_list",
-];
+const mysqlSakilaSqlViews = [
+    'actor_info', 'customer_list', 'film_list', 'nicer_but_slower_film_list', 'sales_by_film_category', 'sales_by_store', 'staff_list'
+]
 
-export { sakilaTables, sakilaSqlViews };
+const pgSakilaTables = [
+    'actor', 'address', 'category', 'city', 'country', 'customer', 'film', 'language', 'payment', 'payment_p2007_01', 'payment_p2007_02', 'payment_p2007_03', 'payment_p2007_04', 'payment_p2007_05', 'payment_p2007_06', 'rental', 'staff'
+]
+
+const pgSakilaSqlViews = [
+    'actor_info', 'customer_list', 'film_list', 'nicer_but_slower_film_list', 'sales_by_film_category', 'sales_by_store', 'staff_list'
+]
+
+const sqliteSakilaSqlViews = [
+    'actor_info', 'customer_list', 'film_list', 'nice_but_slower_film_list', 'sales_by_film_category', 'sales_by_store', 'staff_list'
+]
+
+export { mysqlSakilaTables, mysqlSakilaSqlViews, pgSakilaTables, pgSakilaSqlViews, sqliteSakilaSqlViews }

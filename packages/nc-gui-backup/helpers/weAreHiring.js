71dca317b4abb2e471513af652761882f9599af0
Naveen MR <oof1lab@gmail.com>
2022-09-24 01:48:51 +0100

    chore: remove nc-gui-backup

--- packages/nc-gui-backup/helpers/weAreHiring.js
+++
@@ -1,7 +0,0 @@
-export default function weAreHiring() {
-  const fn = () => {
-    console.log('%c🚀 We are Hiring!!! 🚀%c\n%cJoin the forces http://careers.nocodb.com', 'color:#1348ba;font-size:3rem;padding:20px;', 'display:none', 'font-size:1.5rem;padding:20px')
-  }
-  fn()
-  setInterval(fn, 300000)
-}

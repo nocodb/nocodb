eaeaf6b1fc79bb8248e9db377a1822e9ad537e5a
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-09-30 10:05:10 +0200

    feat(nc-gui): add isLTAR utility

--- packages/nc-gui/utils/virtualCell.ts
+++ packages/nc-gui/utils/virtualCell.ts
@@ -1,14 +1,17 @@
 import type { ColumnType, LinkToAnotherRecordType } from 'nocodb-sdk'
 import { RelationTypes, UITypes } from 'nocodb-sdk'
 
+export const isLTAR = (uidt: string, colOptions: unknown): colOptions is LinkToAnotherRecordType =>
+  uidt === UITypes.LinkToAnotherRecord
+
 export const isHm = (column: ColumnType) =>
-  column.uidt === UITypes.LinkToAnotherRecord && (<LinkToAnotherRecordType>column.colOptions).type === RelationTypes.HAS_MANY
+  isLTAR(column.uidt, column.colOptions) && column.colOptions.type === RelationTypes.HAS_MANY
 
 export const isMm = (column: ColumnType) =>
-  column.uidt === UITypes.LinkToAnotherRecord && (<LinkToAnotherRecordType>column.colOptions).type === RelationTypes.MANY_TO_MANY
+  isLTAR(column.uidt, column.colOptions) && column.colOptions.type === RelationTypes.MANY_TO_MANY
 
 export const isBt = (column: ColumnType) =>
-  column.uidt === UITypes.LinkToAnotherRecord && (<LinkToAnotherRecordType>column.colOptions).type === RelationTypes.BELONGS_TO
+  isLTAR(column.uidt, column.colOptions) && column.colOptions.type === RelationTypes.BELONGS_TO
 
 export const isLookup = (column: ColumnType) => column.uidt === UITypes.Lookup
 export const isRollup = (column: ColumnType) => column.uidt === UITypes.Rollup

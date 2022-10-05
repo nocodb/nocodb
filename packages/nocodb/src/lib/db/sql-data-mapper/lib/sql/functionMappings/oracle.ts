57e253cf052b718bfb381637b1bdd656d1bccef6
Pranav C <61551451+pranavxc@users.noreply.github.com>
2022-10-05 13:14:58 +0530

    feat(api): oracledb - add formula dateadd function support

--- packages/nocodb/src/lib/db/sql-data-mapper/lib/sql/functionMappings/oracle.ts
+++ packages/nocodb/src/lib/db/sql-data-mapper/lib/sql/functionMappings/oracle.ts
@@ -10,39 +10,59 @@ const pg = {
   POWER: 'POWER',
   SQRT: 'SQRT',
   SEARCH: 'INSTR',
+  MID: 'SUBSTR',
   NOW: (args: MapFnArgs) => {
     return args.knex.raw(`CURRENT_DATE${args.colAlias}`);
   },
   REPEAT: (args: MapFnArgs) => {
-    const arg1 = args.fn(args.pt.arguments[0]);
-    const arg2 = args.fn(args.pt.arguments[1]);
+    const arg1 = args.fn(args.pt.arguments[0]).toQuery();
+    const arg2 = args.fn(args.pt.arguments[1]).toQuery();
     return args.knex.raw(
       `RPAD(${arg1}, ${arg2}*LENGTH(${arg1}), ${arg1})${args.colAlias}`
     );
   },
-  // INT(args: MapFnArgs) {
-  //   // todo: correction
-  //   return args.knex.raw(
-  //     `REGEXP_REPLACE(COALESCE(${args.fn(
-  //       args.pt.arguments[0]
-  //     )}::character varying, '0'), '[^0-9]+|\\.[0-9]+' ,'')${args.colAlias}`
-  //   );
-  // },
-  // MID: 'SUBSTR',
-  // FLOAT: ({ fn, knex, pt, colAlias }: MapFnArgs) => {
-  //   return knex
-  //     .raw(`CAST(${fn(pt.arguments[0])} as DOUBLE PRECISION)${colAlias}`)
-  //     .wrap('(', ')');
-  // },
-  // DATEADD: ({ fn, knex, pt, colAlias }: MapFnArgs) => {
-  //   return knex.raw(
-  //     `${fn(pt.arguments[0])} + (${fn(pt.arguments[1])} ||
-  //     '${String(fn(pt.arguments[2])).replace(
-  //       /["']/g,
-  //       ''
-  //     )}')::interval${colAlias}`
-  //   );
-  // },
+  INT: (args: MapFnArgs) => {
+    return args.knex.raw(
+      `CASE WHEN TRIM(TRANSLATE(${args
+        .fn(args.pt.arguments[0])
+        .toQuery()}, '0123456789-,.', ' ')) is null
+      THEN FLOOR(TO_NUMBER(${args
+        .fn(args.pt.arguments[0])
+        .toQuery()})) ELSE 0 END${args.colAlias}`
+    );
+  },
+  LEFT: (args: MapFnArgs) => {
+    return args.knex.raw(
+      `SUBSTR(${args.fn(args.pt.arguments[0])},1,${args.fn(
+        args.pt.arguments[1]
+      )})${args.colAlias}`
+    );
+  },
+  RIGHT: (args: MapFnArgs) => {
+    return args.knex.raw(
+      `SUBSTR(${args.fn(args.pt.arguments[0])},-1 * ${args.fn(
+        args.pt.arguments[1]
+      )})${args.colAlias}`
+    );
+  },
+  DATEADD: ({ fn, knex, pt, colAlias }: MapFnArgs) => {
+    return knex.raw(
+      `CASE
+      WHEN ${fn(pt.arguments[2])} = 'year' THEN ADD_MONTHS(${fn(
+        pt.arguments[0]
+      ).toQuery()},12 * ${fn(pt.arguments[1])})
+      WHEN ${fn(pt.arguments[2])} = 'month' THEN ADD_MONTHS(${fn(
+        pt.arguments[0]
+      ).toQuery()},${fn(pt.arguments[1])})
+      WHEN ${fn(pt.arguments[2])} = 'day' THEN ${fn(
+        pt.arguments[0]
+      ).toQuery()} + ${fn(pt.arguments[1])}
+      WHEN ${fn(pt.arguments[2])} = 'week' THEN ${fn(
+        pt.arguments[0]
+      ).toQuery()} + 7 * ${fn(pt.arguments[1])}
+      END${colAlias}`
+    );
+  },
   // WEEKDAY: ({ fn, knex, pt, colAlias }: MapFnArgs) => {
   //   // isodow: the day of the week as Monday (1) to Sunday (7)
   //   // WEEKDAY() returns an index from 0 to 6 for Monday to Sunday

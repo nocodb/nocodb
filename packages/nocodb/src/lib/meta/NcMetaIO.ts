685b57fe59ee62510fa234b69277c47b634297f3
Pranav C <pranavxc@gmail.com>
2022-10-06 19:57:01 +0530

    feat(api): add metaCount method for getting records count
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb/src/lib/meta/NcMetaIO.ts
+++ packages/nocodb/src/lib/meta/NcMetaIO.ts
@@ -162,6 +162,16 @@ export default abstract class NcMetaIO {
     }
   ): Promise<any[]>;
 
+  public abstract metaCount(
+    project_id: string,
+    base_id: string,
+    target: string,
+    args?: {
+      condition?: { [key: string]: any };
+      xcCondition?: XcCondition;
+    }
+  ): Promise<number>;
+
   public abstract metaPaginatedList(
     project_id: string,
     dbAlias: string,

5fe1ee53dcf3d56a98bc5f1b81ad18620bcf04f4
Muhammed Mustafa <mustafa010199@gmail.com>
2022-10-07 10:49:49 +0530

    feat(testing): Fixed CI config for playwright

--- packages/nocodb/src/lib/services/test/TestResetService/setupSakilaSqlite.ts
+++ packages/nocodb/src/lib/services/test/TestResetService/setupSakilaSqlite.ts
@@ -2,29 +2,20 @@ import fs from 'fs';
 import Knex from 'knex';
 
 const setupSakilaSqlite = async (metaKnex: Knex) => {
-  const config = metaKnex.client.config;
-  await metaKnex.destroy();
-  const rootDir = __dirname.replace(
-    'packages/nocodb/src/lib/services/test/TestResetService',
-    ''
-  );
-  if (fs.existsSync(`${rootDir}/packages/nocodb/test_noco.db`)) {
-    fs.unlinkSync(`${rootDir}/packages/nocodb/test_noco.db`);
-  }
-  fs.copyFileSync(
-    `${rootDir}/scripts/cypress/fixtures/sqlite-sakila/sakila.db`,
-    `${rootDir}/packages/nocodb/test_noco.db`
+  const testsDir = __dirname.replace(
+    '/src/lib/services/test/TestResetService',
+    '/tests'
   );
 
-  metaKnex = Knex(config);
-  console.log('config', config);
+  const schemaFile = fs
+    .readFileSync(`${testsDir}/pg-sakila-db/01-sqlite-sakila-schema.sql`)
+    .toString();
+  const dataFile = fs
+    .readFileSync(`${testsDir}/pg-sakila-db/02-sqlite-sakila-insert-data.sql`)
+    .toString();
 
-  // await metaKnex.client.destroy();
-  // console.log('destroyed');
-  // await metaKnex.client.initializePool(config);
-  // console.log('initializePool');
-  // await metaKnex.client.acquireConnection();
-  // console.log('acquireConnection');
+  await metaKnex.raw(schemaFile);
+  await metaKnex.raw(dataFile);
 };
 
 export default setupSakilaSqlite;

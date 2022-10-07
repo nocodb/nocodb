2258aa45138f0c06fffc2f70f6918f244477f88a
Muhammed Mustafa <mustafa010199@gmail.com>
2022-10-07 11:21:54 +0530

    feat(testing): Fixed CI config for playwright

--- scripts/playwright/playwright.config.ts
+++ scripts/playwright/playwright.config.ts
@@ -96,7 +96,7 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: './output',
+  outputDir: './playwright-report',
 
   /* Run your local dev server before starting the tests */
   // webServer: {

6d7cc2ef07bd637cfded0e1be9720e368ac60264
Vijay Kumar Rathore <professional.vijay8492@gmail.com>
2022-10-19 12:59:29 +0530

    reset validation if column is not to be validated

--- packages/nc-gui/composables/useColumnCreateStore.ts
+++ packages/nc-gui/composables/useColumnCreateStore.ts
@@ -179,6 +179,9 @@ const [useProvideColumnCreateStore, useColumnCreateStore] = createInjectionState
         formState.value.table_name = meta.value?.table_name
         // formState.value.title = formState.value.column_name
         if (column.value) {
+          if (!columnToValidate.includes(formState.value.uidt)) {
+            formState.value.validate = ''
+          }
           await $api.dbTableColumn.update(column.value?.id as string, formState.value)
           // Column updated
           message.success(t('msg.success.columnUpdated'))

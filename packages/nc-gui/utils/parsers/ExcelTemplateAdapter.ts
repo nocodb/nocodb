fd262e86600c96548a2f8fdc4d3400f195b45d86
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-10-22 13:42:29 +0800

    feat(nc-gui): fallback to SingleLineText if there is some data not numeric

--- packages/nc-gui/utils/parsers/ExcelTemplateAdapter.ts
+++ packages/nc-gui/utils/parsers/ExcelTemplateAdapter.ts
@@ -159,7 +159,7 @@ export default class ExcelTemplateAdapter extends TemplateGenerator {
                   }
                 } else if (column.uidt === UITypes.Number) {
                   if (
-                    rows.slice(1, this.config.maxRowsToParse).some((v: any) => {
+                    rows.slice(1, this.config.maxRowsToParse).every((v: any) => {
                       return v && v[col] && parseInt(v[col]) !== +v[col]
                     })
                   ) {
@@ -178,6 +178,14 @@ export default class ExcelTemplateAdapter extends TemplateGenerator {
                     })
                   ) {
                     column.uidt = UITypes.Currency
+                  }
+                  if (
+                    rows.slice(1, this.config.maxRowsToParse).some((v: any) => {
+                      return !(!isNaN(Number(v)) && !isNaN(parseFloat(v)))
+                    })
+                  ) {
+                    // fallback to SingleLineText
+                    column.uidt = UITypes.SingleLineText
                   }
                 } else if (column.uidt === UITypes.DateTime) {
                   // TODO(import): centralise

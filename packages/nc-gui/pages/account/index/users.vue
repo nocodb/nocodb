b7fb043504b2bd4ce42d0cce2d22d110f5a23bd0
Pranav C <pranavxc@gmail.com>
2022-10-25 11:21:43 +0530

    refactor(gui): manage nested users tab route based
    Signed-off-by: Pranav C <pranavxc@gmail.com>

---
+++ packages/nc-gui/pages/account/index/users.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="h-full overflow-y-scroll scrollbar-thin-dull pt-4">
+    <a-tabs v-model:active-key="selectedTabKey" :open-keys="[]" mode="horizontal" class="nc-auth-tabs">
+      <a-tab-pane v-for="tab of tabs" :key="tab.key" class="select-none">
+        <template #tab>
+          <span>
+            {{ tab.label }}
+          </span>
+        </template>
+      </a-tab-pane>
+    </a-tabs>
+    <NuxtPage :tab-key="selectedTabKey"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useUIPermission } from '~/composables/useUIPermission'
+
+const { isUIAllowed } = useUIPermission()
+
+const tabs = [
+  ...(isUIAllowed('superAdminUserManagement')
+    ? [
+      { label: 'Users', key: 'list' },
+      { label: 'Settings', key: 'settings' },
+    ]
+    : []),
+  { label: 'Reset Password', key: 'password-reset' },
+]
+
+const $route = useRoute()
+
+const selectedTabKey = computed({
+  get() {
+    return tabs.find(t => t.key === $route.params.tabType)?.key ?? tabs[0].key
+  },
+  set(val) {
+    navigateTo(`/account/users/${val}`)
+  },
+})
+</script>
---
+++ packages/nc-gui/pages/account/index/users/[[tabType]].vue
@@ -0,0 +1,18 @@
+<template>
+  <template v-if="props.tabKey === 'list'">
+    <LazyAccountUserList />
+  </template>
+  <template v-else-if="props.tabKey === 'settings'">
+    <LazyAccountSignupSettings />
+  </template>
+  <template v-else-if="props.tabKey === 'password-reset'">
+    <LazyAccountResetPassword />
+  </template>
+</template>
+<script setup lang="ts">
+import { defineProps } from '#imports'
+
+const props = defineProps<{
+  tabKey: string
+}>()
+</script>

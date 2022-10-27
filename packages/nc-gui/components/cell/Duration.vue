1d89c7c8d3f8bced6f67e5acb8d1f3a78ad61829
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-10-27 16:56:05 +0800

    fix(nc-gui): duration focus issue

--- packages/nc-gui/components/cell/Duration.vue
+++ packages/nc-gui/components/cell/Duration.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { VNodeRef } from '@vue/runtime-core'
 import {
   ColumnInj,
   EditModeInj,
@@ -67,13 +68,15 @@ const submitDuration = () => {
   }
   isEdited.value = false
 }
+
+const focus: VNodeRef = (el) => (el as HTMLInputElement)?.focus()
 </script>
 
 <template>
   <div class="duration-cell-wrapper">
     <input
       v-if="editEnabled"
-      ref="durationInput"
+      :ref="focus"
       v-model="localState"
       class="w-full !border-none p-0"
       :class="{ '!px-2': editEnabled }"

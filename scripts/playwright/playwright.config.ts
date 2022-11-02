c81ef83c18c976e15b15146ca6cf5e93a7d14ae9
Muhammed Mustafa <mustafa010199@gmail.com>
2022-11-02 16:06:37 +0530

    feat(testing): Stability

--- scripts/playwright/playwright.config.ts
+++ scripts/playwright/playwright.config.ts
@@ -19,7 +19,7 @@ const config: PlaywrightTestConfig = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 13000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,

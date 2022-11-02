24f98f26f4755a043ff4ad4874dd12c883d18da3
Muhammed Mustafa <mustafa010199@gmail.com>
2022-11-02 21:21:18 +0530

    feat(testing): Added eslint and git pre commit hook

--- scripts/playwright/playwright.config.ts
+++ scripts/playwright/playwright.config.ts
@@ -19,7 +19,7 @@ const config: PlaywrightTestConfig = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 13000
+    timeout: 13000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -39,7 +39,7 @@ const config: PlaywrightTestConfig = {
     baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: process.env.CI ? 'on-first-retry': process.env.TRACE ? 'on' : 'off',
+    trace: process.env.CI ? 'on-first-retry' : process.env.TRACE ? 'on' : 'off',
     screenshot: 'only-on-failure',
   },
   /* Configure projects for major browsers */

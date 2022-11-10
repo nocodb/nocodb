aac0ce28c24d4e65d5ea214ec9ea4a2b73d1133d
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-10 10:22:45 +0800

    fix(nocodb): wrap the second argument in formula RIGHT

--- packages/nocodb/src/lib/db/sql-data-mapper/lib/sql/functionMappings/mysql.ts
+++ packages/nocodb/src/lib/db/sql-data-mapper/lib/sql/functionMappings/mysql.ts
@@ -28,9 +28,9 @@ const mysql2 = {
   },
   RIGHT: (args: MapFnArgs) => {
     return args.knex.raw(
-      `SUBSTR(${args.fn(args.pt.arguments[0])},-${args.fn(
+      `SUBSTR(${args.fn(args.pt.arguments[0])}, -(${args.fn(
         args.pt.arguments[1]
-      )})${args.colAlias}`
+      )}))${args.colAlias}`
     );
   },
   MID: 'SUBSTR',

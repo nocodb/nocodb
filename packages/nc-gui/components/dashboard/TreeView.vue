3085d7283b3e1fea50cba73c4f82ecb19e0f7370
mertmit <mertmit99@gmail.com>
2022-11-13 22:01:39 +0300

    fix: make base name bold
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/components/dashboard/TreeView.vue
+++ packages/nc-gui/components/dashboard/TreeView.vue
@@ -496,7 +496,7 @@ useEventListener(document, 'keydown', async (e: KeyboardEvent) => {
                 >
                   <a-collapse-panel :key="`collapse-${base.id}`">
                     <template #header>
-                      <div v-if="index !== '0'" class="flex items-center gap-2 text-gray-500 font-weightd">
+                      <div v-if="index !== '0'" class="flex items-center gap-2 text-gray-500 font-bold">
                         <GeneralBaseLogo :base-type="base.type" />
                         {{ base.alias || '' }}
                       </div>

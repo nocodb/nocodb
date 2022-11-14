6f60d1236448057504e8ea8db843bd15b6dcf5f8
Pranav C <pranavxc@gmail.com>
2022-11-14 18:28:52 +0530

    test(cypress): select cell if not active
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- tests/playwright/pages/Dashboard/common/Cell/SelectOptionCell.ts
+++ tests/playwright/pages/Dashboard/common/Cell/SelectOptionCell.ts
@@ -25,7 +25,14 @@ export class SelectOptionCellPageObject extends BasePage {
     option: string;
     multiSelect?: boolean;
   }) {
-    await this.get({ index, columnHeader }).click();
+    const selectCell = this.get({ index, columnHeader });
+
+    // check if cell active
+    if (!(await selectCell.getAttribute('class')).includes('active')) {
+      await selectCell.click();
+    }
+
+    await selectCell.click();
 
     await this.rootPage.getByTestId(`select-option-${columnHeader}-${index}`).getByText(option).click();
 

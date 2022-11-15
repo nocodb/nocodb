f9cf48f624fe286172088320c62cbda65face32d
Daniel Spaude <daniel@spaude.de>
2022-11-15 09:58:24 +0300

    QR Code: add basic unit tests

--- packages/nocodb/tests/unit/rest/index.test.ts
+++ packages/nocodb/tests/unit/rest/index.test.ts
@@ -1,6 +1,7 @@
 import 'mocha';
 import authTests from './tests/auth.test';
 import projectTests from './tests/project.test';
+import columnTypeSpecificTests from './tests/columnTypeSpecific.test';
 import tableTests from './tests/table.test';
 import tableRowTests from './tests/tableRow.test';
 import viewRowTests from './tests/viewRow.test';
@@ -11,6 +12,7 @@ function restTests() {
   tableTests();
   tableRowTests();
   viewRowTests();
+  columnTypeSpecificTests();
 }
 
 export default function () {

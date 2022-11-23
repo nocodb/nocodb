78508444820d8353cdb9fe966eadaac81ecd4e89
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-23 20:21:07 +0800

    feat(nc-gui): AntDropdownAutoDestroy

---
+++ packages/nc-gui/components/general/AntDropdownAutoDestroy.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { dropdownProps } from 'ant-design-vue/es/dropdown'
+const props = defineProps(dropdownProps())
+</script>
+
+<template>
+  <a-dropdown v-bind="props" :destroy-popup-on-hide="true">
+    <slot />
+    <template #overlay>
+      <slot name="overlay" />
+    </template>
+  </a-dropdown>
+</template>

baed9a7ed7ac0907dfe213629d7fb69eb6384f30
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-24 13:31:13 +0800

    feat(nc-gui): keyboard manoeuvre - Alt + I

--- packages/nc-gui/components/general/ShareBaseButton.vue
+++ packages/nc-gui/components/general/ShareBaseButton.vue
@@ -3,22 +3,35 @@ import { useRoute, useUIPermission } from '#imports'
 
 const route = useRoute()
 
-const showUserModal = $ref(false)
+const showUserModal = ref(false)
 
 const { isUIAllowed } = useUIPermission()
+
+const isShareBaseAllowed =
+  isUIAllowed('newUser') &&
+  route.name !== 'index' &&
+  route.name !== 'index-index-create' &&
+  route.name !== 'index-index-create-external' &&
+  route.name !== 'index-user-index'
+
+useEventListener(document, 'keydown', async (e: KeyboardEvent) => {
+  if (e.altKey) {
+    switch (e.keyCode) {
+      case 73: {
+        // ALT + I
+        if (isShareBaseAllowed) {
+          showUserModal.value = true
+        }
+        break
+      }
+    }
+  }
+})
 </script>
 
 <template>
   <div class="flex items-center w-full pl-3 hover:(text-primary bg-primary bg-opacity-5)" @click="showUserModal = true">
-    <div
-      v-if="
-        isUIAllowed('newUser') &&
-        route.name !== 'index' &&
-        route.name !== 'index-index-create' &&
-        route.name !== 'index-index-create-external' &&
-        route.name !== 'index-user-index'
-      "
-    >
+    <div v-if="isShareBaseAllowed">
       <div class="flex items-center space-x-1">
         <MdiAccountPlusOutline class="mr-1 nc-share-base" />
 

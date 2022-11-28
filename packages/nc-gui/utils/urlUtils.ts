1ff1e2099a28fc7ac49706333ac4fa5aa6562d89
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-28 16:09:26 +0800

    fix(nc-gui): use validator/lib/isURL

--- packages/nc-gui/utils/urlUtils.ts
+++ packages/nc-gui/utils/urlUtils.ts
@@ -1,3 +1,5 @@
+import isURL from 'validator/lib/isURL'
+
 export const replaceUrlsWithLink = (text: string): boolean | string => {
   if (!text) {
     return false
@@ -17,12 +19,8 @@ export const replaceUrlsWithLink = (text: string): boolean | string => {
   return found && out
 }
 
-// ref : https://stackoverflow.com/a/5717133
 export const isValidURL = (str: string) => {
-  const pattern =
-    /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00A1-\uFFFF0-9]-*)*[a-z\u00A1-\uFFFF0-9]+)(?:\.(?:[a-z\u00A1-\uFFFF0-9]-*)*[a-z\u00A1-\uFFFF0-9]+)*(?:\.(?:[a-z\u00A1-\uFFFF]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
-
-  return pattern.test(str)
+  return isURL(str)
 }
 
 export const openLink = (path: string, baseURL?: string, target = '_blank') => {

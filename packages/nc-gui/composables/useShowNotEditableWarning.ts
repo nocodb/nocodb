9722e65ee2cc73dad122e78b0aa5ccb18ac98d99
Daniel Spaude <daniel@spaude.de>
2022-11-29 22:23:11 +0300

    qrcode: cleanup

--- packages/nc-gui/composables/useShowNotEditableWarning.ts
+++ packages/nc-gui/composables/useShowNotEditableWarning.ts
@@ -1,12 +1,6 @@
-// import type { Ref } from 'vue'
-// import type { ViewType } from 'nocodb-sdk'
-
 const timeout = 3000 // in ms
 
-// export default function useShowNotEditableWarning(view: Ref<ViewType | undefined>) {
 export default function useShowNotEditableWarning() {
-  //   console.log(view)
-
   const showEditNonEditableFieldWarning = refAutoReset(false, timeout)
   const showClearNonEditableFieldWarning = refAutoReset(false, timeout)
 

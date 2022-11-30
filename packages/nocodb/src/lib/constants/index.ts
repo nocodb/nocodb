94d9417e309d6cad97907b0dda635d5e875375de
Muhammed Mustafa <mustafa@nocodb.com>
2022-11-30 14:27:55 +0530

    fix(playwright): NC_ATTACHMENT_FIELD_SIZE env variable now used for setting attachment field size and field size now defaulted to 20MB from 150MB

--- packages/nocodb/src/lib/constants/index.ts
+++ packages/nocodb/src/lib/constants/index.ts
@@ -1,5 +1,5 @@
 export const NC_LICENSE_KEY = 'nc-license-key';
 export const NC_APP_SETTINGS = 'nc-app-settings';
-export const NC_FILE_FIELD_SIZE = process.env['NC_FILE_FIELD_SIZE']
-  ? Number(process.env['NC_FILE_FIELD_SIZE'])
-  : 250 * 1024 * 1024; // 250 MB
+export const NC_FILE_FIELD_SIZE = process.env['NC_ATTACHMENT_FIELD_SIZE']
+  ? Number(process.env['NC_ATTACHMENT_FIELD_SIZE'])
+  : 20 * 1024 * 1024; // 20 MB

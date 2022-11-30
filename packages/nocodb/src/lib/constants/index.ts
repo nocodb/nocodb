5d234732e20650a845317e3137673d9ba2ac02a5
Muhammed Mustafa <mustafa@nocodb.com>
2022-11-30 11:50:00 +0530

    fix(test): Made defealt file field size limit to 250MB and is configurable using NC_FILE_FIELD_SIZE env variable

--- packages/nocodb/src/lib/constants/index.ts
+++ packages/nocodb/src/lib/constants/index.ts
@@ -1,2 +1,5 @@
 export const NC_LICENSE_KEY = 'nc-license-key';
 export const NC_APP_SETTINGS = 'nc-app-settings';
+export const NC_FILE_FIELD_SIZE = process.env['NC_FILE_FIELD_SIZE']
+  ? Number(process.env['NC_FILE_FIELD_SIZE'])
+  : 250 * 1024 * 1024; // 250 MB

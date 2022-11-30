e3fc9b2b00f9329756a468c9bb9c432c0f488fe9
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-30 12:10:20 +0800

    fix(nc-gui): use ant-drawer-open instead

--- packages/nc-gui/utils/browserUtils.ts
+++ packages/nc-gui/utils/browserUtils.ts
@@ -1,4 +1,3 @@
 // refer - https://stackoverflow.com/a/11752084
 export const isMac = () => /Mac/i.test(navigator.platform)
-// .ant-drawer will be destroyed when closed. no active class is required.
-export const isDrawerOrModalExist = () => document.querySelector('.ant-modal.active, .ant-drawer')
+export const isDrawerOrModalExist = () => document.querySelector('.ant-modal.active, .ant-drawer-open')

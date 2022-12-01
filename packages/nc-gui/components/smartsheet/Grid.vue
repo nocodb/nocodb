c6403b62f8dc0e5bfe25a1c6306fb7040ca447ae
Pranav C <pranavxc@gmail.com>
2022-12-01 19:33:19 +0530

    fix: verify property exist in row object
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui/components/smartsheet/Grid.vue
+++ packages/nc-gui/components/smartsheet/Grid.vue
@@ -534,7 +534,7 @@ const saveOrUpdateRecords = async (args: { metaValue?: TableType; viewMetaValue?
       currentRow.rowMeta.changed = false
       for (const field of (args.metaValue || meta.value)?.columns ?? []) {
         if (isVirtualCol(field)) continue
-        if (currentRow.row[field.title!] !== currentRow.oldRow[field.title!]) {
+        if (field.title! in currentRow.row && currentRow.row[field.title!] !== currentRow.oldRow[field.title!]) {
           await updateOrSaveRow(currentRow, field.title!, {}, args)
         }
       }

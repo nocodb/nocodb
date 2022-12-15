868ec59895b3d52f684e59efe401497f1c6ec21c
Pranav C <pranavxc@gmail.com>
2022-12-15 10:06:11 +0300

    refactor: rename migration sequence number

--- packages/nocodb/src/lib/migrations/XcMigrationSourcev2.ts
+++ packages/nocodb/src/lib/migrations/XcMigrationSourcev2.ts
@@ -13,7 +13,7 @@ import * as nc_022_qr_code_column_type from './v2/nc_022_qr_code_column_type';
 import * as nc_023_multiple_source from './v2/nc_023_multiple_source';
 import * as nc_024_add_type_to_project from './v2/nc_024_add_type_to_project';
 import * as nc_025_create_nocodoc_page from './v2/nc_025_create_nocodoc_page';
-import * as nc_025_workspace from './v2/nc_025_workspace';
+import * as nc_026_workspace from './v2/nc_026_workspace';
 
 // Create a custom migration source class
 export default class XcMigrationSourcev2 {
@@ -38,7 +38,7 @@ export default class XcMigrationSourcev2 {
       'nc_023_multiple_source',
       'nc_024_add_type_to_project',
       'nc_025_create_nocodoc_page',
-      'nc_025_workspace',
+      'nc_026_workspace',
     ]);
   }
 
@@ -78,8 +78,8 @@ export default class XcMigrationSourcev2 {
         return nc_024_add_type_to_project;
       case 'nc_025_create_nocodoc_page':
         return nc_025_create_nocodoc_page;
-      case 'nc_025_workspace':
-        return nc_025_workspace;
+      case 'nc_026_workspace':
+        return nc_026_workspace;
     }
   }
 }

0b1709050bc9fae20640fa269808732ca5457bef
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-23 21:38:55 +0800

    chore(nc-gui): autosize -> auto-size

--- packages/nc-gui/components/gpt/Prompt.vue
+++ packages/nc-gui/components/gpt/Prompt.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-// TODO: 
+// TODO:
 const data = ref('')
 </script>
 
 <template>
   <div class="h-min-[calc(100%_-_160px] w-full overflow-y-auto scrollbar-thin-dull">
-    <a-textarea v-model:value="data" :autosize="{ minRows: 20 }" />
+    <a-textarea v-model:value="data" :auto-size="{ minRows: 20 }" />
   </div>
 </template>

385747378a05fe2cdc68b0fd2c50c97faad0e312
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-23 20:00:50 +0800

    feat(nc-gui): gpt slug page

---
+++ packages/nc-gui/pages/[projectType]/gpt/[projectId]/[...slugs].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  key: 'true',
+  hideHeader: true,
+  layout: 'gpt',
+})
+</script>
+
+<template>
+  <NuxtLayout id="content" class="flex">
+    <template #header> <GptHeader /> </template>
+    <template #left> <GptInput /> </template>
+    <template #right> <GptOutput /> </template>
+    <GptPage />
+  </NuxtLayout>
+</template>

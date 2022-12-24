c5f44daeaedfe8ef9d460980c9ccaf157f75c1d7
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-24 15:05:17 +0800

    wip(nc-gui): useGPTStore

---
+++ packages/nc-gui/composables/useGPTStore.ts
@@ -0,0 +1,63 @@
+import type { ProjectType, TableInfoType, ViewType } from 'nocodb-sdk'
+import { ViewTypes } from 'nocodb-sdk'
+import { useNuxtApp, useViews } from '#imports'
+
+const [useProvideGPTStore, useGPTStore] = useInjectionState((projectId: string) => {
+  const gptProject = ref<ProjectType | null>()
+
+  const gptTable = ref<TableInfoType | null>()
+
+  const gptView = ref<ViewType | null>()
+
+  const { $api } = useNuxtApp()
+
+  async function loadGPTProject() {
+    gptProject.value = await $api.project.read(projectId)
+    console.log(gptProject.value)
+  }
+
+  async function loadGPTTable() {
+    const firstTable = (await $api.dbTable.list(projectId)).list?.[0]
+    gptTable.value = await $api.dbTable.read(firstTable!.id!)
+    console.log(gptTable.value)
+    await loadGPTView()
+  }
+
+  async function loadGPTView() {
+    const { views, loadViews } = useViews(gptTable.value as TableInfoType)
+    await loadViews()
+    if (views.value.length === 1) {
+      // no form view, create one
+      await $api.dbView.formCreate(gptTable.value!.id!, {
+        type: ViewTypes.FORM,
+        title: `${gptTable.value!.title}_form`,
+      } as ViewType)
+      await loadViews()
+    }
+    gptView.value = views.value[1]
+    console.log(gptView.value)
+  }
+
+  watch(
+    gptProject,
+    async (project) => {
+      if (project) return
+      await Promise.all([loadGPTProject(), loadGPTTable()])
+    },
+    { immediate: true },
+  )
+
+  return {
+    gptProject,
+    gptTable,
+    gptView,
+  }
+})
+
+export { useProvideGPTStore }
+
+export function useGPTStoreOrThrow() {
+  const state = useGPTStore()
+  if (!state) throw new Error('Please call `useProvideGPTStore` on the appropriate parent component')
+  return state
+}

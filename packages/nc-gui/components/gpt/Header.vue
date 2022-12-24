a3ff081a7ce86b8db9518b52ad1b1acc7e4b6571
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-24 13:13:12 +0800

    feat(nc-gui): use gptProject

--- packages/nc-gui/components/gpt/Header.vue
+++ packages/nc-gui/components/gpt/Header.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-import { useProject } from '#imports'
-const { project, loadProject } = useProject()
-
-onMounted(loadProject)
+import { useGPTStoreOrThrow } from '#imports'
+const { gptProject } = useGPTStoreOrThrow()
 </script>
 
 <template>
@@ -11,10 +9,10 @@ onMounted(loadProject)
     <img src="https://dummyimage.com/48x48/000/fff" class="h-20 w-20 p-2" />
     <div class="flex min-w-0 w-200 text-base">
       <div class="nc-gpt-project mx-2 text-sm py-4">
-        <div class="font-bold">{{ project.title }}</div>
+        <div class="font-bold">{{ gptProject.title }}</div>
         <div class="text-gray">
           <!-- TODO: handle super long description -->
-          {{ project.description }}
+          {{ gptProject.description }}
         </div>
       </div>
     </div>

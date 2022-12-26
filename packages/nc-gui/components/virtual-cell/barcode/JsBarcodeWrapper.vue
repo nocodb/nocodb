3150b986bbf7233dcd2147267bb0a6fb68f32af4
flisowna <ewa.agnieszka.flis@gmail.com>
2022-12-26 22:12:58 +0100

    barcode: formatting

--- packages/nc-gui/components/virtual-cell/barcode/JsBarcodeWrapper.vue
+++ packages/nc-gui/components/virtual-cell/barcode/JsBarcodeWrapper.vue
@@ -34,6 +34,6 @@ onMounted(generate)
 </script>
 
 <template>
-  <svg class="w-full" data-testid="barcode" v-show="!errorForCurrentInput" ref="barcodeSvgRef" @click="onBarcodeClick"></svg>
+  <svg v-show="!errorForCurrentInput" ref="barcodeSvgRef" class="w-full" data-testid="barcode" @click="onBarcodeClick"></svg>
   <slot v-if="errorForCurrentInput" name="barcodeRenderError" />
 </template>

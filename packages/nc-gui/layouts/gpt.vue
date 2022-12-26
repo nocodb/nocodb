40f0b86dd68d6949eb28e424b197aea1d92c69b4
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-26 17:54:47 +0800

    feat(nc-gui): GPT layout & remove unused code

--- packages/nc-gui/layouts/gpt.vue
+++ packages/nc-gui/layouts/gpt.vue
@@ -1,33 +1,5 @@
 <script lang="ts" setup>
-import { computed, navigateTo, ref, useGlobal, useNuxtApp, useRoute, useSidebar } from '#imports'
-
-const { signOut, signedIn, isLoading, user, currentVersion } = useGlobal()
-
-useSidebar('nc-left-sidebar', { hasSidebar: false })
-
-const route = useRoute()
-
-const email = computed(() => user.value?.email ?? '---')
-
-const hasSider = ref(false)
-
-const sidebar = ref<HTMLDivElement>()
-
-const logout = () => {
-  signOut()
-  navigateTo('/signin')
-}
-
-const { hooks } = useNuxtApp()
-
-const isDashboard = computed(() => !!route.params.projectType)
-
-/** when page suspensions have finished, check if a sidebar element was teleported into the layout */
-hooks.hook('page:finish', () => {
-  if (sidebar.value) {
-    hasSider.value = sidebar.value?.children.length > 0
-  }
-})
+//
 </script>
 
 <script lang="ts">
@@ -37,33 +9,32 @@ export default {
 </script>
 
 <template>
-  <!-- TODO: make header as a component -->
-  <a-layout-header class="!px-2">
-    <div class="flex w-full h-full items-center">
-      <div class="flex-1 min-w-0 w-50">
-        <img src="~/assets/img/brand/nocodb-full-color.png" class="h-12" />
-      </div>
-
-      <div class="flex-1 min-w-0 flex justify-end gap-2">
-        <div class="nc-quick-action-wrapper">
-          <MaterialSymbolsSearch class="nc-quick-action-icon" />
-          <input class="" placeholder="Quick Actions" />
-
-          <span class="nc-quick-action-shortcut">⌘ K</span>
-        </div>
-
-        <div class="flex items-center">
-          <MdiBellOutline class="text-xl" />
-          <MaterialSymbolsKeyboardArrowDownRounded />
-        </div>
-        <div class="flex items-center gap-1">
-          <div class="h-14 w-14 rounded-full bg-primary flex items-center justify-center font-weight-bold text-white">AB</div>
-          <MaterialSymbolsKeyboardArrowDownRounded />
-        </div>
-      </div>
-    </div>
-  </a-layout-header>
   <a-layout>
+    <a-layout-header class="!px-2">
+      <div class="flex w-full h-full items-center">
+        <div class="flex-1 min-w-0 w-50">
+          <img src="~/assets/img/brand/nocodb-full-color.png" class="h-12" />
+        </div>
+
+        <div class="flex-1 min-w-0 flex justify-end gap-2">
+          <div class="nc-quick-action-wrapper">
+            <MaterialSymbolsSearch class="nc-quick-action-icon" />
+            <input class="" placeholder="Quick Actions" />
+
+            <span class="nc-quick-action-shortcut">⌘ K</span>
+          </div>
+
+          <div class="flex items-center">
+            <MdiBellOutline class="text-xl" />
+            <MaterialSymbolsKeyboardArrowDownRounded />
+          </div>
+          <div class="flex items-center gap-1">
+            <div class="h-14 w-14 rounded-full bg-primary flex items-center justify-center font-weight-bold text-white">AB</div>
+            <MaterialSymbolsKeyboardArrowDownRounded />
+          </div>
+        </div>
+      </div>
+    </a-layout-header>
     <a-layout-header class="!bg-white !p-2">
       <slot name="header" />
     </a-layout-header>

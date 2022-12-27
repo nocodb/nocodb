dfb2d9fca4cb4511bd10fe55617c36f0ce6f7c04
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-27 14:17:20 +0800

    feat(nc-gui): add GPT Grid

---
+++ packages/nc-gui/components/gpt/Grid.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import type { ColumnType, TableType, ViewType } from 'nocodb-sdk'
+import type { Ref } from 'vue'
+import { ActiveViewInj, MetaInj, useGPTStoreOrThrow, useProvideSmartsheetStore } from '#imports'
+
+const { gptTable, gptGridView } = useGPTStoreOrThrow()
+
+const fields = ref<ColumnType[]>([])
+
+const reloadEventHook = createEventHook<void | boolean>()
+
+const reloadViewMetaEventHook = createEventHook<void | boolean>()
+
+const openNewRecordFormHook = createEventHook<void>()
+
+provide(MetaInj, gptTable as Ref<TableType>)
+
+provide(ActiveViewInj, gptGridView)
+
+provide(IsFormInj, ref(false))
+
+provide(FieldsInj, fields)
+
+provide(ReloadViewDataHookInj, reloadEventHook)
+
+provide(ReloadViewMetaHookInj, reloadViewMetaEventHook)
+
+provide(OpenNewRecordFormHookInj, openNewRecordFormHook)
+
+useProvideSmartsheetStore(gptGridView as Ref<ViewType>, gptTable as Ref<TableType>)
+</script>
+
+<template>
+  <div class="nc-container flex h-full">
+    <div class="flex flex-col h-full flex-1 min-w-0">
+      <LazySmartsheetToolbar />
+      <Transition name="layout" mode="out-in">
+        <template v-if="gptTable">
+          <div class="flex flex-1 min-h-0">
+            <div v-if="gptGridView" class="h-full flex-1 min-w-0 min-h-0 bg-gray-50">
+              <LazySmartsheetGrid />
+            </div>
+          </div>
+        </template>
+      </Transition>
+    </div>
+  </div>
+</template>

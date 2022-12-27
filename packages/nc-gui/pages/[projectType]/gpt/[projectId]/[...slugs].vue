40d49db548030ba7b39987901e6f42054a96d2c5
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-27 13:18:10 +0800

    feat(nc-gui): add GptGrid

--- packages/nc-gui/pages/[projectType]/gpt/[projectId]/[...slugs].vue
+++ packages/nc-gui/pages/[projectType]/gpt/[projectId]/[...slugs].vue
@@ -5,7 +5,7 @@ const route = useRoute()
 
 const { loadProject } = useProject()
 
-useProvideGPTStore(route.params.projectId as string)
+const { gptLayout } = useProvideGPTStore(route.params.projectId as string)
 
 loadProject(true, route.params.projectId as string)
 
@@ -19,7 +19,8 @@ definePageMeta({
 <template>
   <NuxtLayout id="content" class="flex">
     <template #header> <GptHeader /> </template>
-    <template #left> <GptInput /> </template>
-    <template #right> <GptOutput /> </template>
+    <template v-if="gptLayout === 'form'" #left> <GptInput /> </template>
+    <template v-if="gptLayout === 'form'" #right> <GptOutput /> </template>
+    <template v-if="gptLayout === 'grid'" #middle> <GptGrid /> </template>
   </NuxtLayout>
 </template>

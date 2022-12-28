c85c74901dfa78f774093f22fba43203e5fd621f
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-28 18:56:58 +0800

    feat(nc-gui): revise cowriter output layout

--- packages/nc-gui/components/cowriter/Output.vue
+++ packages/nc-gui/components/cowriter/Output.vue
@@ -1,27 +1,36 @@
 <script setup lang="ts">
-// TODO:
+import { useCowriterStoreOrThrow } from '#imports'
+const { cowriterOutputList, cowriterOutputActiveKey, clearCowriterOutput } = useCowriterStoreOrThrow()
 </script>
 
 <template>
-  <div>
-    <div class="flex items-center gap-1 p-[7px]">
-      <a-button class="!rounded-md !border-none">
-        New Outputs
+  <a-tabs v-model:activeKey="cowriterOutputActiveKey" class="nc-cowriter-tabs">
+    <a-tab-pane key="cowriter-output">
+      <template #tab>
+        Output
         <span class="bg-gray-200 ml-1 px-2 rounded-md">
-          <!-- TODO: -->
-          1
+          {{ cowriterOutputList.length }}
         </span>
-      </a-button>
-      <a-button class="!rounded-md !bg-[#F8FAFD] !border-none"> History </a-button>
-    </div>
-  </div>
-  <!-- TODO: -->
-  <div v-for="i of [1, 2, 3]" :key="i" class="bg-[#EEF2FF] border-b-1 border-gray-200">
-    <div class="p-[24px] pb-0">
-      -NESCAFE Original is made with 100% coffee beans for a rich, full flavor <br />
-      -The hinged lid with Click-to-close technology keeps your coffee fresh and easy to acccess <br />
-      -Space efficient tin is perfect for small kitchens or on the go <br />
-    </div>
-    <div class="flex justify-end pr-3 pb-3">3 mins ago</div>
-  </div>
+      </template>
+      <CowriterList />
+    </a-tab-pane>
+    <a-tab-pane key="cowriter-history" tab="History">
+      <CowriterList />
+    </a-tab-pane>
+    <template #rightExtra>
+      <div class="flex items-center gap-1 px-4">
+        <a-button class="!rounded-md" @click="clearCowriterOutput"> Clear </a-button>
+      </div>
+    </template>
+  </a-tabs>
 </template>
+
+<style scoped lang="scss">
+:deep(.ant-tabs-nav) {
+  @apply !mb-0;
+}
+
+:deep(.ant-tabs-nav-wrap) {
+  @apply !ml-[30px];
+}
+</style>

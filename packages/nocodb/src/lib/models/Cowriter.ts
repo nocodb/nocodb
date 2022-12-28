99ab968ad39c9efba3c0d9fcc5404d208550bccd
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-28 14:07:45 +0800

    feat(nocodb): add Cowriter model

---
+++ packages/nocodb/src/lib/models/Cowriter.ts
@@ -0,0 +1,82 @@
+import { extractProps } from '../meta/helpers/extractProps';
+import Noco from '../Noco';
+import { MetaTable } from '../utils/globals';
+
+import { CowriterType } from 'nocodb-sdk';
+
+export default class Cowriter implements CowriterType {
+  id?: string;
+  fk_model_id?: string;
+  prompt_statement?: string;
+  prompt_statement_template?: string;
+  output?: string;
+  time_taken?: number;
+  created_by?: string;
+
+  constructor(cowriter: Cowriter | CowriterType) {
+    Object.assign(this, cowriter);
+  }
+
+  public static async get(cowriterId: string, ncMeta = Noco.ncMeta) {
+    const cowriter = await ncMeta.metaGet2(
+      null,
+      null,
+      MetaTable.COWRITER,
+      cowriterId
+    );
+    return cowriter && new Cowriter(cowriter);
+  }
+
+  public static async insert(
+    cowriter: Partial<Cowriter>,
+    ncMeta = Noco.ncMeta
+  ) {
+    // extract props which is allowed to be inserted
+    const insertObject = extractProps(cowriter, [
+      'id',
+      'fk_model_id',
+      'prompt_statement',
+      'prompt_statement_template',
+      'output',
+      'time_taken',
+      'created_by',
+    ]);
+
+    const { id } = await ncMeta.metaInsert2(
+      null,
+      null,
+      MetaTable.COWRITER,
+      insertObject
+    );
+    return this.get(id);
+  }
+
+  static async list(
+    {
+      fk_model_id,
+    }: {
+      fk_model_id: string;
+    },
+    ncMeta = Noco.ncMeta
+  ) {
+    const cowriterList = await ncMeta.metaList2(
+      null,
+      null,
+      MetaTable.COWRITER,
+      {
+        condition: {
+          fk_model_id,
+        },
+        orderBy: {
+          order: 'asc',
+        },
+      }
+    );
+
+    return Promise.all(
+      cowriterList.map(async (cowriter) => {
+        return new Cowriter(cowriter);
+      })
+    );
+  }
+}

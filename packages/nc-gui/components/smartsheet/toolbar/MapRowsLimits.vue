5e6ba2dc111cc1a3b29ee3341db8859c76f5373e
flisowna <ewa.agnieszka.flis@gmail.com>
2022-12-28 11:17:55 +0100

    WIP info button on toolbar about record limits in map view

---
+++ packages/nc-gui/components/smartsheet/toolbar/MapRowsLimits.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// import type { MapType } from 'nocodb-sdk'
+// import { UITypes } from 'nocodb-sdk'
+// import type { SelectProps } from 'ant-design-vue'
+import { PaginationDataInj, computed, inject } from '#imports'
+const { count } = useSmartsheetStoreOrThrow()
+</script>
+
+<template>
+  <a-tooltip placement="bottom">
+    <template #title> You're getting close to the limit. The limit of markers in a Map View is 1000 records. </template>
+
+    <div class="nc-warning-info flex min-w-32px h-32px items-center gap-1 px-2">
+      <div>{{ count }} records</div>
+      <mdi-map-marker-alert />
+    </div>
+  </a-tooltip>
+</template>

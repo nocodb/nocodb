9b5c794e96421757ed3d7be47251e20dc7974c89
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-28 16:19:05 +0800

    chore(nc-gui): promptStatement -> promptStatementTemplate

--- packages/nc-gui/components/cowriter/Prompt.vue
+++ packages/nc-gui/components/cowriter/Prompt.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { useCowriterStoreOrThrow } from '#imports'
 
-const { promptStatement } = useCowriterStoreOrThrow()
+const { promptStatementTemplate } = useCowriterStoreOrThrow()
 </script>
 
 <template>
   <div class="h-min-[calc(100%_-_160px] w-full overflow-y-auto scrollbar-thin-dull">
-    <a-textarea v-model:value="promptStatement" :auto-size="{ minRows: 20 }" />
+    <a-textarea v-model:value="promptStatementTemplate" :auto-size="{ minRows: 20 }" />
   </div>
 </template>

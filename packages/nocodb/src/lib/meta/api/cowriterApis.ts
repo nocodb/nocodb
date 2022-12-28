eb31afa4fd1fa2dff04faa9b727a25cbd0f13492
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-28 19:13:04 +0800

    feat(nc-gui): integrate with open ai createCompletion

--- packages/nocodb/src/lib/meta/api/cowriterApis.ts
+++ packages/nocodb/src/lib/meta/api/cowriterApis.ts
@@ -1,17 +1,43 @@
 import { Request, Response, Router } from 'express';
 import ncMetaAclMw from '../helpers/ncMetaAclMw';
+import { NcError } from '../helpers/catchError';
 import { CowriterListType, CowriterType } from 'nocodb-sdk';
 import Cowriter from '../../models/Cowriter';
 import { PagedResponseImpl } from '../helpers/PagedResponse';
+
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 export async function cowriterCreate(
   req: Request<any, CowriterType, CowriterType>,
   res: Response<CowriterType>
 ) {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: req.body.prompt_statement,
+    temperature: 0.7,
+    max_tokens: 2048,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  if (response.data.choices.length === 0) {
+    NcError.badRequest('Failed to generate output');
+  }
+
+  const output = response.data.choices[0].text;
+
   const cowriter = await Cowriter.insert({
     ...req.body,
     fk_model_id: req.params.tableId,
     created_by: (req as any).user.id,
+    output,
   });
   res.json(cowriter);
 }

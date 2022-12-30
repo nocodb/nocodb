ee976b3087356c0528f8517ab901e8fe224d60ce
Muhammed Mustafa <mustafa@nocodb.com>
2022-12-30 14:35:21 +0530

    feat(nocodocs): Synced migration number and cleaned up a duplicate migration

--- packages/nocodb/src/lib/migrations/XcMigrationSourcev2.ts
+++ packages/nocodb/src/lib/migrations/XcMigrationSourcev2.ts
@@ -11,9 +11,9 @@ import * as nc_020_kanban_view from './v2/nc_020_kanban_view';
 import * as nc_021_add_fields_in_token from './v2/nc_021_add_fields_in_token';
 import * as nc_022_qr_code_column_type from './v2/nc_022_qr_code_column_type';
 import * as nc_023_multiple_source from './v2/nc_023_multiple_source';
-import * as nc_024_add_type_to_project from './v2/nc_024_add_type_to_project';
-import * as nc_025_create_books from './v2/nc_025_create_books';
-import * as nc_026_workspace from './v2/nc_026_workspace';
+import * as nc_025_add_type_to_project from './v2/nc_025_add_type_to_project';
+import * as nc_026_create_books from './v2/nc_026_create_books';
+import * as nc_027_workspace from './v2/nc_027_workspace';
 
 // Create a custom migration source class
 export default class XcMigrationSourcev2 {
@@ -74,12 +74,12 @@ export default class XcMigrationSourcev2 {
         return nc_022_qr_code_column_type;
       case 'nc_023_multiple_source':
         return nc_023_multiple_source;
-      case 'nc_024_add_type_to_project':
-        return nc_024_add_type_to_project;
-      case 'nc_025_create_books':
-        return nc_025_create_books;
-      case 'nc_026_workspace':
-        return nc_026_workspace;
+      case 'nc_025_add_type_to_project':
+        return nc_025_add_type_to_project;
+      case 'nc_026_create_books':
+        return nc_026_create_books;
+      case 'nc_027_workspace':
+        return nc_027_workspace;
     }
   }
 }

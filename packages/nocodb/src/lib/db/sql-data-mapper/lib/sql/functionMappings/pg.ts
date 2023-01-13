a04d62ab495b84767b77db1f998251f6a3b528b3
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-13 18:13:37 +0800

    fix(nocodb): postgres datetime_diff year sql

--- packages/nocodb/src/lib/db/sql-data-mapper/lib/sql/functionMappings/pg.ts
+++ packages/nocodb/src/lib/db/sql-data-mapper/lib/sql/functionMappings/pg.ts
@@ -91,7 +91,7 @@ const pg = {
                     DATE_PART('year', AGE(${datetime_expr2}, '1900/01/01')) * 4) - 1)`;
         break;
       case 'year':
-        sql = `DATE_PART('year', ${datetime_expr1}::TIMESTAMP) - DATE_PART('year', ${datetime_expr2}::TIMESTAMP)`;
+        sql = `DATE_PART('year', age(${datetime_expr1}, ${datetime_expr2}))`;
         break;
       case 'day':
         sql = `DATE_PART('day', ${datetime_expr1}::TIMESTAMP - ${datetime_expr2}::TIMESTAMP)`;

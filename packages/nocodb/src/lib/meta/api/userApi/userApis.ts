5ceb0777877b45b75920437a7245f8121486ea51
Pranav C <pranavxc@gmail.com>
2023-01-13 14:42:17 +0530

    Update userApis.ts
    Ignore email from update

--- packages/nocodb/src/lib/meta/api/userApi/userApis.ts
+++ packages/nocodb/src/lib/meta/api/userApi/userApis.ts
@@ -383,7 +383,6 @@ async function passwordForgot(req: Request<any, any>, res): Promise<any> {
   if (user) {
     const token = uuidv4();
     await User.update(user.id, {
-      email: user.email,
       reset_password_token: token,
       reset_password_expires: new Date(Date.now() + 60 * 60 * 1000),
       token_version: null,

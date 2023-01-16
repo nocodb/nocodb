88e648c5a97213b9ef08e6f4fbb8ac1f36126598
Pranav C <pranavxc@gmail.com>
2023-01-16 15:14:41 +0530

    fix(gui): avoid opening expanded form in shared view
    re #4580
    
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui/components/smartsheet/Grid.vue
+++ packages/nc-gui/components/smartsheet/Grid.vue
@@ -201,7 +201,7 @@ const { isCellSelected, activeCell, handleMouseDown, handleMouseOver, handleCell
       const cmdOrCtrl = isMac() ? e.metaKey : e.ctrlKey
       const altOrOptionKey = e.altKey
       if (e.key === ' ') {
-        if (activeCell.row != null && !editEnabled) {
+        if (activeCell.row != null && !editEnabled && hasEditPermission?.value) {
           e.preventDefault()
           clearSelectedRange()
           const row = data.value[activeCell.row]

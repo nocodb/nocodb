17dd51fd73905158dd5a4e0a811e410b27e4c0c2
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-04 16:21:14 +0800

    feat(nocodb): numeric-based columns filter migration

--- packages/nocodb/src/lib/version-upgrader/ncFilterUpgrader_0104003.ts
+++ packages/nocodb/src/lib/version-upgrader/ncFilterUpgrader_0104003.ts
@@ -9,6 +9,7 @@ import { UITypes } from 'nocodb-sdk';
 // this upgrader is to remove those unsupported filters / migrate to the correct filter
 // changes:
 // - remove `>`, `<`, `>=`, `<=` for text-based columns
+// - remove `like`, `null`, and `empty` for numeric-based columns - migrate to `blank` from `null` and `empty`
 
 export default async function ({ ncMeta }: NcUpgraderCtx) {
   const filters = await ncMeta.metaList2(null, null, MetaTable.FILTER_EXP);
@@ -31,6 +32,27 @@ export default async function ({ ncMeta }: NcUpgraderCtx) {
       // remove `>`, `<`, `>=`, `<=` for text-based columns
       if (['gt', 'lt', 'gte', 'lte'].includes(filter.comparison_op)) {
         actions.push(await Filter.delete(filter, ncMeta));
+      }
+    } else if (
+      [
+        UITypes.Duration,
+        UITypes.Currency,
+        UITypes.Percent,
+        UITypes.Number,
+        UITypes.Decimal,
+        UITypes.Rating,
+        UITypes.Rollup,
+      ].includes(col.uidt)
+    ) {
+      if (['like', 'nlike'].includes(filter.comparison_op)) {
+        // remove `is like`, `is not like`
+        actions.push(await Filter.delete(filter, ncMeta));
+      } else if (
+        ['null', 'notnull', 'empty', 'notempty'].includes(filter.comparison_op)
+      ) {
+        // remove `is null`, `is not null`, `is empty`, `is not empty`
+        actions.push(await Filter.delete(filter, ncMeta));
+        // TODO: migrate to blank / not blank
       }
     }
   }

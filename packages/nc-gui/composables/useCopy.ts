99a7bcbd5b0508bc8c2616085c36f08735b59954
Pranav C <pranavxc@gmail.com>
2023-02-09 13:18:47 +0530

    fix(gui): if copy failed show error message and print content in console
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui/composables/useCopy.ts
+++ packages/nc-gui/composables/useCopy.ts
@@ -2,14 +2,27 @@ import { useClipboard } from '#imports'
 
 export const useCopy = () => {
   /** fallback for copy if clipboard api is not supported */
-  const copyFallback = (text: string) => {
-    const textAreaEl = document.createElement('textarea')
-    textAreaEl.innerHTML = text
-    document.body.appendChild(textAreaEl)
-    textAreaEl.select()
-    const result = document.execCommand('copy')
-    document.body.removeChild(textAreaEl)
-    return result
+  const copyFallback = async (text: string, retryCount = 0): Promise<boolean> => {
+    try {
+      const textAreaEl = document.createElement('textarea')
+      textAreaEl.innerHTML = text
+      document.body.appendChild(textAreaEl)
+      textAreaEl.select()
+      const result = document.execCommand('copy')
+      document.body.removeChild(textAreaEl)
+      if (!result && retryCount < 3) {
+        // retry if copy failed
+        return new Promise((resolve) => setTimeout(() => resolve(copyFallback(text, retryCount + 1)), 100))
+      }
+
+      if (!result) {
+        throw new Error('failed')
+      }
+      return result
+    } catch (e) {
+      throw new Error('Clipboard copy failed, please copy it from console log')
+      console.log(text)
+    }
   }
 
   const { copy: _copy, isSupported } = useClipboard()
@@ -18,11 +31,12 @@ export const useCopy = () => {
     try {
       if (isSupported.value) {
         await _copy(text)
+        return true
       } else {
-        copyFallback(text)
+        return copyFallback(text)
       }
     } catch (e) {
-      copyFallback(text)
+      return copyFallback(text)
     }
   }
 

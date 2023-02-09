59e6cd147d9bc23d146ae6e735f59e718770ba3e
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-09 14:28:42 +0800

    feat(nc-gui): AttachmentImage

---
+++ packages/nc-gui/components/cell/attachment/Image.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  src: string[]
+  alt?: string
+}
+
+const props = defineProps<Props>()
+
+const index = ref(0)
+
+const onError = () => index.value++
+</script>
+
+<template>
+  <LazyNuxtImg
+    v-if="index < props.src.length"
+    :src="props.src[index]"
+    :onerror="onError"
+    :alt="props?.alt || ''"
+    placeholder
+    quality="75"
+  />
+  <img v-else src="~/assets/img/file-not-found.png" />
+</template>

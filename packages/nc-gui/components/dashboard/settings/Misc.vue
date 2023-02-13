fed62a3e009d343857fb59eb7f130e5dbb55fbd7
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-13 18:59:44 +0800

    feat(nc-gui): add desc for misc items

--- packages/nc-gui/components/dashboard/settings/Misc.vue
+++ packages/nc-gui/components/dashboard/settings/Misc.vue
@@ -14,22 +14,22 @@ watch(includeM2M, async () => await loadTables())
       <div class="flex flex-row items-center w-full mb-4 gap-2">
         <!-- Show M2M Tables -->
         <a-checkbox v-model:checked="includeM2M" v-e="['c:themes:show-m2m-tables']" class="nc-settings-meta-misc">
-          {{ $t('msg.info.showM2mTables') }} <br/>
-          <span class="text-gray-500">{{ $t('msg.info.showM2mTablesDesc')}}</span>
+          {{ $t('msg.info.showM2mTables') }} <br />
+          <span class="text-gray-500">{{ $t('msg.info.showM2mTablesDesc') }}</span>
         </a-checkbox>
       </div>
       <div class="flex flex-row items-center w-full mb-4 gap-2">
         <!-- Show NULL -->
         <a-checkbox v-model:checked="showNull" v-e="['c:settings:show-null']" class="nc-settings-show-null">
-          {{ $t('msg.info.showNullInCells')}} <br/>
-          <span class="text-gray-500">{{ $t('msg.info.showNullInCellsDesc')}}</span>
+          {{ $t('msg.info.showNullInCells') }} <br />
+          <span class="text-gray-500">{{ $t('msg.info.showNullInCellsDesc') }}</span>
         </a-checkbox>
       </div>
       <div class="flex flex-row items-center w-full mb-4 gap-2">
         <!-- Show NULL and EMPTY in Filters -->
         <a-checkbox v-model:checked="showNullAndEmptyInFilter" v-e="['c:settings:show-null']" class="nc-settings-show-null">
-          {{ $t('msg.info.showNullAndEmptyInFilter')}} <br/>
-          <span class="text-gray-500">{{ $t('msg.info.showNullAndEmptyInFilterDesc')}}</span>
+          {{ $t('msg.info.showNullAndEmptyInFilter') }} <br />
+          <span class="text-gray-500">{{ $t('msg.info.showNullAndEmptyInFilterDesc') }}</span>
         </a-checkbox>
       </div>
     </div>

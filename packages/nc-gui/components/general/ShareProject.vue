bb27767c1336a30444b6aa21005ccca558d40fd8
Muhammed Mustafa <mustafa@nocodb.com>
2023-02-13 11:08:36 +0530

    feat(nocodocs): Removed share project button tooltip

--- packages/nc-gui/components/general/ShareProject.vue
+++ packages/nc-gui/components/general/ShareProject.vue
@@ -25,32 +25,14 @@ useEventListener(document, 'keydown', async (e: KeyboardEvent) => {
 </script>
 
 <template>
-  <a-tooltip placement="bottom" color="#F2F4F7" overlay-class-name="share-status-tootltip" :mouse-enter-delay="0.25">
-    <template #title>
-      <div class="flex flex-col text-black px-1 py-0.5">
-        <template v-if="visibility === 'public'">
-          <div :style="{ fontSize: '0.8rem' }" style="font-weight: 500">Page Edits</div>
-          <div :style="{ fontSize: '0.65rem' }">Page edits will immediately be updated to public link</div>
-          <div :style="{ fontSize: '0.8rem' }" class="pt-1" style="font-weight: 500">New Page</div>
-          <div :style="{ fontSize: '0.65rem' }">New pages created will be published</div>
-        </template>
-        <template v-else>
-          <div :style="{ fontSize: '0.8rem' }" style="font-weight: 500">Page Edits</div>
-          <div :style="{ fontSize: '0.65rem' }">Page edits will be saved to your project</div>
-          <div :style="{ fontSize: '0.8rem' }" class="pt-1" style="font-weight: 500">New Page</div>
-          <div :style="{ fontSize: '0.65rem' }">New pages created will be saved to your project</div>
-        </template>
-      </div>
-    </template>
-    <div
-      class="my-auto h-7.5 flex flex-row items-center gap-x-1.5 bg-primary text-white hover:bg-opacity-80 py-1.5 px-2.5 rounded-md mr-2 cursor-pointer"
-      @click="showModal = true"
-    >
-      <MaterialSymbolsPublic v-if="visibility === 'public'" class="h-3.5" />
-      <MaterialSymbolsLockOutline v-else-if="visibility === 'private'" class="h-3.5" />
-      <div class="flex">Share</div>
-    </div>
-  </a-tooltip>
+  <div
+    class="my-auto h-7.5 flex flex-row items-center gap-x-1.5 bg-primary text-white hover:bg-opacity-80 py-1.5 px-2.5 rounded-md mr-2 cursor-pointer"
+    @click="showModal = true"
+  >
+    <MaterialSymbolsPublic v-if="visibility === 'public'" class="h-3.5" />
+    <MaterialSymbolsLockOutline v-else-if="visibility === 'private'" class="h-3.5" />
+    <div class="flex">Share</div>
+  </div>
 
   <LazyDlgShareAndCollaborate v-model:model-value="showModal" />
 </template>

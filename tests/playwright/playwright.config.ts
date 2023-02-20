15a1ade24359845b26b3967b80ffd5f5d839f3a9
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-02-20 21:29:52 +0530

    test: experiment with wider viewport
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -33,6 +33,7 @@ const config: PlaywrightTestConfig = {
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    viewport: { width: 1920, height: 1080 },
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 10000,
     /* Base URL to use in actions like `await page.goto('/')`. */

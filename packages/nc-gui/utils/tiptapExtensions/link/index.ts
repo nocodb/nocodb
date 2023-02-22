a3325244d9f0d5e766b244c4c34918e60c390287
Muhammed Mustafa <mustafa@nocodb.com>
2023-02-22 17:40:31 +0530

    fix(nocodocs): On admin view only cmd+click will open the link while in public view link click behavior is the same

--- packages/nc-gui/utils/tiptapExtensions/link/index.ts
+++ packages/nc-gui/utils/tiptapExtensions/link/index.ts
@@ -1,5 +1,33 @@
 import TiptapLink from '@tiptap/extension-link'
+import { getAttributes } from '@tiptap/core'
+import { Plugin } from 'prosemirror-state'
 
-export const Link = TiptapLink.configure({
+export const Link = TiptapLink.extend({
+  addProseMirrorPlugins() {
+    return [
+      new Plugin({
+        props: {
+          handleClick(view, pos, event) {
+            const attrs = getAttributes(view.state, 'link')
+
+            if (view.editable && !event.metaKey) {
+              return true
+            }
+
+            const link = (event.target as HTMLElement)?.closest('a')
+
+            if (link && attrs.href) {
+              window.open(attrs.href, attrs.target)
+
+              return true
+            }
+
+            return false
+          },
+        },
+      }),
+    ]
+  },
+}).configure({
   openOnClick: false,
 })

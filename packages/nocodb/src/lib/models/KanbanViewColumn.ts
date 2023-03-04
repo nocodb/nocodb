f950cfeda1e951ca12ced2e232022f40217eb089
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-03-04 19:54:31 +0800

    fix(nocodb): change to BoolType

--- packages/nocodb/src/lib/models/KanbanViewColumn.ts
+++ packages/nocodb/src/lib/models/KanbanViewColumn.ts
@@ -3,11 +3,12 @@ import { CacheGetType, CacheScope, MetaTable } from '../utils/globals';
 import View from './View';
 import NocoCache from '../cache/NocoCache';
 import { extractProps } from '../meta/helpers/extractProps';
+import { BoolType } from 'nocodb-sdk';
 
 export default class KanbanViewColumn {
   id: string;
   title?: string;
-  show?: boolean;
+  show?: BoolType;
   order?: number;
 
   fk_view_id: string;

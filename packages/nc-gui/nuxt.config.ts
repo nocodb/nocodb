4fad28bb3e154045ccce16fba5f7a0a91578aceb
Pranav C <pranavxc@gmail.com>
2023-03-06 19:28:40 +0530

    chore: update cdn url

--- packages/nc-gui/nuxt.config.ts
+++ packages/nc-gui/nuxt.config.ts
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
     layoutTransition: false,
 
     /** In production build we need to load assets using relative path, to achieve the result we are using cdnURL */
-    cdnURL: process.env.NODE_ENV === 'production' ? 'https://d13mcbt6a41d0f.cloudfront.net' : undefined,
+    cdnURL: process.env.NODE_ENV === 'production' ? `https://d13mcbt6a41d0f.cloudfront.net/${process.env.VERSION}` : undefined,
   },
 
   css: [

1447e4da438f27b59cf968af547a9ff36c89587c
Pranav C <pranavxc@gmail.com>
2023-03-06 18:16:37 +0530

    refactor: move public docs api handler logic to corresponding service file

--- packages/nocodb/src/lib/services/public/index.ts
+++ packages/nocodb/src/lib/services/public/index.ts
@@ -1,5 +1,11 @@
 import * as publicDataService from './publicData.svs';
 import * as publicDataExportApis from './publicDataExport.svs';
 import * as publicMetaService from './publicMeta.svs';
+import * as publicDocsPagesService from './publicDocsPages.svs';
 
-export { publicDataService, publicDataExportApis, publicMetaService };
+export {
+  publicDataService,
+  publicDataExportApis,
+  publicMetaService,
+  publicDocsPagesService,
+};
---
+++ packages/nocodb/src/lib/services/public/publicDocsPages.svs.ts
@@ -0,0 +1,82 @@
+import Page from '../../models/Page';
+import {NcError} from '../../meta/helpers/catchError';
+import Project from '../../models/Project';
+
+export  async function get(
+ param:{
+   projectId:string,
+   pageId:string,
+   nestedPageId:string
+ }
+) {
+    const project = await Project.getWithInfo(param.projectId as string);
+    const projectMeta = (typeof project.meta === 'string' ? JSON.parse(project.meta as string) : project.meta);
+
+    const page = await Page.get({
+      id: param.pageId,
+      projectId: param?.projectId,
+    });
+
+    if (!page) NcError.notFound('Page not found');
+
+    // if page and project is not public, check if the given parent page is published with nested pages
+    if (!projectMeta?.isPublic && !page.is_published) {
+      const parentPage = await Page.get({
+        id: param.nestedPageId ,
+        projectId: param.projectId ,
+      });
+
+      if (!parentPage?.is_nested_published) NcError.notFound('Page not found');
+
+      if (
+        page.id !== parentPage.id &&
+        !Page.isParent({
+          parentId: parentPage.id,
+          pageId: page.id,
+          projectId: param.projectId
+        })
+      ) {
+        NcError.notFound('Page is not found.');
+      }
+    }
+
+    return page;
+}
+
+export async function list(
+param:{
+  projectId:string,
+  parentPageId:string
+
+}
+) {
+  try {
+    const project = await Project.getWithInfo(param.projectId as string);
+    const projectMeta = JSON.parse(project.meta as string);
+
+    if (!projectMeta?.isPublic && !param.parentPageId)
+      throw new Error('Project is not found');
+
+    if (projectMeta?.isPublic) {
+      return await Page.nestedList({
+          projectId: param.projectId as string,
+        })
+    }
+
+    const page = await Page.get({
+      id: param.parentPageId as string,
+      projectId: param.projectId as string,
+    });
+    if (!page) NcError.notFound('Page not found');
+
+    if (page.is_published && page.is_nested_published) {
+      return  await Page.nestedList({
+          projectId: param.projectId as string,
+          parent_page_id: param.parent_page_id as string,
+          fetchAll: false,
+        })
+
+    }
+
+    return [page]
+}

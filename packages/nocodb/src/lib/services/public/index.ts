04dd4410b30ab7d0d6aa881da6f7906b49caab8d
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-03-07 17:22:23 +0800

    refactor(nocodb): svs -> svc

--- packages/nocodb/src/lib/services/public/index.ts
+++ packages/nocodb/src/lib/services/public/index.ts
@@ -1,5 +1,5 @@
-import * as publicDataService from './publicData.svs';
-import * as publicDataExportApis from './publicDataExport.svs';
-import * as publicMetaService from './publicMeta.svs';
+import * as publicDataService from './publicData.svc';
+import * as publicDataExportApis from './publicDataExport.svc';
+import * as publicMetaService from './publicMeta.svc';
 
 export { publicDataService, publicDataExportApis, publicMetaService };

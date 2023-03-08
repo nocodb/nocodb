f9894aea446040bd1453df86c9c5157fc8575908
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-03-08 12:28:33 +0800

    chore(nocodb): remove setTokenCookie from user.ctl.ts

--- packages/nocodb/src/lib/controllers/user/user.ctl.ts
+++ packages/nocodb/src/lib/controllers/user/user.ctl.ts
@@ -135,15 +135,6 @@ async function oidcSignin(req, res, next) {
   )(req, res, next);
 }
 
-function setTokenCookie(res: Response, token): void {
-  // create http only cookie with refresh token that expires in 7 days
-  const cookieOptions = {
-    httpOnly: true,
-    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-  };
-  res.cookie('refresh_token', token, cookieOptions);
-}
-
 async function me(req, res): Promise<any> {
   res.json(req?.session?.passport?.user ?? {});
 }

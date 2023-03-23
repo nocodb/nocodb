ef6ca5ea99f2fde7f5d26781cd3385163b418824
Muhammed Mustafa <mustafa@nocodb.com>
2023-03-23 15:13:05 +0530

    feat(nocodocs): Integrated SMTP to cloud

--- packages/nocodb/src/lib/services/plugin/populateCloudPlugins.ts
+++ packages/nocodb/src/lib/services/plugin/populateCloudPlugins.ts
@@ -1,4 +1,6 @@
 import { MetaTable } from '../../utils/globals';
+import S3PluginConfig from '../../plugins/s3';
+import SMTPPluginConfig from '../../plugins/smtp';
 import Noco from '../..';
 
 export const populatePluginsForCloud = async ({ ncMeta = Noco.ncMeta }) => {
@@ -12,7 +14,7 @@ export const populatePluginsForCloud = async ({ ncMeta = Noco.ncMeta }) => {
   }
 
   const s3PluginData = await ncMeta.metaGet2(null, null, MetaTable.PLUGIN, {
-    title: 'S3',
+    title: S3PluginConfig.title,
   });
 
   if (s3PluginData) {
@@ -33,5 +35,48 @@ export const populatePluginsForCloud = async ({ ncMeta = Noco.ncMeta }) => {
     );
   } else {
     throw new Error('S3 plugin not found');
+  }
+
+  // SMTP
+  if (
+    !process.env.NC_ClOUD_SMTP_FROM ||
+    !process.env.NC_ClOUD_SMTP_HOST ||
+    !process.env.NC_ClOUD_SMTP_PORT ||
+    !process.env.NC_ClOUD_SMTP_SECURE ||
+    !process.env.NC_ClOUD_SMTP_IGNORE_TLS ||
+    !process.env.NC_ClOUD_SMTP_REJECT_UNAUTHORIZED ||
+    !process.env.NC_ClOUD_SMTP_USERNAME ||
+    !process.env.NC_ClOUD_SMTP_PASSWORD
+  ) {
+    throw new Error('SMTP env variables not found');
+  }
+
+  const smtpPluginData = await ncMeta.metaGet2(null, null, MetaTable.PLUGIN, {
+    title: SMTPPluginConfig.title,
+  });
+
+  if (smtpPluginData) {
+    await ncMeta.metaUpdate(
+      null,
+      null,
+      MetaTable.PLUGIN,
+      {
+        input: JSON.stringify({
+          from: process.env.NC_ClOUD_SMTP_FROM,
+          host: process.env.NC_ClOUD_SMTP_HOST,
+          port: process.env.NC_ClOUD_SMTP_PORT,
+          secure: process.env.NC_ClOUD_SMTP_SECURE === 'true',
+          ignoreTLS: process.env.NC_ClOUD_SMTP_IGNORE_TLS === 'true',
+          rejectUnauthorized:
+            process.env.NC_ClOUD_SMTP_REJECT_UNAUTHORIZED === 'true',
+          username: process.env.NC_ClOUD_SMTP_USERNAME,
+          password: process.env.NC_ClOUD_SMTP_PASSWORD,
+        }),
+        active: true,
+      },
+      smtpPluginData.id
+    );
+  } else {
+    throw new Error('SMTP plugin not found');
   }
 };

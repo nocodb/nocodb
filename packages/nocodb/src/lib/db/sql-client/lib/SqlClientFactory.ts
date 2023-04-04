2bdb598ac8ebb3ae8d51c351beb92d462f5b2346
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-04-04 12:50:41 +0800

    refactor(nocodb): lint

--- packages/nocodb/src/lib/db/sql-client/lib/SqlClientFactory.ts
+++ packages/nocodb/src/lib/db/sql-client/lib/SqlClientFactory.ts
@@ -48,21 +48,27 @@ export default class {
       typeof connectionConfig.connection.ssl === 'object'
     ) {
       if (connectionConfig.connection.ssl.caFilePath) {
-        connectionConfig.connection.ssl.ca = (await promisify(fs.readFile)(
-          connectionConfig.connection.ssl.caFilePath
-        )).toString();
+        connectionConfig.connection.ssl.ca = (
+          await promisify(fs.readFile)(
+            connectionConfig.connection.ssl.caFilePath
+          )
+        ).toString();
         delete connectionConfig.connection.ssl.caFilePath;
       }
       if (connectionConfig.connection.ssl.keyFilePath) {
-        connectionConfig.connection.ssl.key = (await promisify(fs.readFile)(
-          connectionConfig.connection.ssl.keyFilePath
-        )).toString();
+        connectionConfig.connection.ssl.key = (
+          await promisify(fs.readFile)(
+            connectionConfig.connection.ssl.keyFilePath
+          )
+        ).toString();
         delete connectionConfig.connection.ssl.keyFilePath;
       }
       if (connectionConfig.connection.ssl.certFilePath) {
-        connectionConfig.connection.ssl.cert = (await promisify(fs.readFile)(
-          connectionConfig.connection.ssl.certFilePath
-        )).toString();
+        connectionConfig.connection.ssl.cert = (
+          await promisify(fs.readFile)(
+            connectionConfig.connection.ssl.certFilePath
+          )
+        ).toString();
         delete connectionConfig.connection.ssl.certFilePath;
       }
     }

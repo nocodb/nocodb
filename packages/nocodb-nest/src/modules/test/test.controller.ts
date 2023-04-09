0b64c1baf226930cb7eccd59474de771b5caf2a7
Pranav C <pranavxc@gmail.com>
2023-04-10 02:11:08 +0530

    feat: test apis
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb-nest/src/modules/test/test.controller.ts
+++ packages/nocodb-nest/src/modules/test/test.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
 import { TestService } from './test.service';
+import { TestResetService } from './TestResetService';
 
 @Controller('test')
 export class TestController {
   constructor(private readonly testService: TestService) {}
+
+  @Post('/api/v1/meta/test/reset')
+  async reset(@Req() req) {
+    const service = new TestResetService({
+      parallelId: req.body.parallelId,
+      dbType: req.body.dbType,
+      isEmptyProject: req.body.isEmptyProject,
+      workerId: req.body.workerId,
+    });
+
+    return await service.process();
+  }
 }

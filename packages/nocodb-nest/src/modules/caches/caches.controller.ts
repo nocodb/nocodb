884b0c84a55ad5328c8a718f5200138d5b41d2dd
Pranav C <pranavxc@gmail.com>
2023-04-10 02:05:03 +0530

    feat: cache apis
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb-nest/src/modules/caches/caches.controller.ts
+++ packages/nocodb-nest/src/modules/caches/caches.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get } from '@nestjs/common';
+import { Acl } from '../../middlewares/extract-project-id/extract-project-id.middleware';
 import { CachesService } from './caches.service';
 
 @Controller('caches')
 export class CachesController {
   constructor(private readonly cachesService: CachesService) {}
+
+  @Get('/api/v1/db/meta/cache')
+  @Acl('cacheGet')
+  async cacheGet(_, res) {
+    const data = await this.cachesService.cacheGet();
+    res.set({
+      'Content-Type': 'application/json',
+      'Content-Disposition': `attachment; filename="cache-export.json"`,
+    });
+    return JSON.stringify(data);
+  }
+
+  @Delete('/api/v1/db/meta/cache')
+  @Acl('cacheDelete')
+  async cacheDelete() {
+    return await this.cachesService.cacheDelete();
+  }
 }

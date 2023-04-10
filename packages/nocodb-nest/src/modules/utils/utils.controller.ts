fa6f20957d93cc1fba429a667d7752a1e65c296d
Pranav C <pranavxc@gmail.com>
2023-04-10 23:06:27 +0530

    fix: attachment path correction
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb-nest/src/modules/utils/utils.controller.ts
+++ packages/nocodb-nest/src/modules/utils/utils.controller.ts
@@ -1,20 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
 import { UtilsService } from './utils.service';
 
 @Controller()
 export class UtilsController {
   constructor(private readonly utilsService: UtilsService) {}
 
-
   @Get('/api/v1/db/meta/nocodb/info')
-  info() {
-    return this.utilsService.info();
+  info(@Request() req) {
+    return this.utilsService.info({
+      ncSiteUrl: req.ncSiteUrl,
+    });
   }
-
 
   @Get('/api/v1/version')
   version() {
     return this.utilsService.versionInfo();
   }
-
 }

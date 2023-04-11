f909f131679d88e4173fe8e43d6a735ba00af47d
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-04-11 13:00:10 +0800

    feat: OldDatasController - dataList

--- packages/nocodb-nest/src/modules/datas/old-datas/old-datas.controller.ts
+++ packages/nocodb-nest/src/modules/datas/old-datas/old-datas.controller.ts
@@ -1,7 +1,35 @@
-import { Controller, UseGuards } from '@nestjs/common'
-import { ExtractProjectIdMiddleware } from '../../../middlewares/extract-project-id/extract-project-id.middleware'
-import { AuthGuard } from '@nestjs/passport'
+import {
+  Controller,
+  UseGuards,
+  Body,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Request,
+} from '@nestjs/common';
+import {
+  Acl,
+  ExtractProjectIdMiddleware,
+} from '../../../middlewares/extract-project-id/extract-project-id.middleware';
+import { AuthGuard } from '@nestjs/passport';
+import { DatasService } from '../datas.service';
 
 @Controller()
 @UseGuards(ExtractProjectIdMiddleware, AuthGuard('jwt'))
-export class OldDatasController {}
+export class OldDatasController {
+  constructor(private readonly datasService: DatasService) {}
+
+  @Get('/nc/:projectId/api/v1/:tableName')
+  @Acl('dataList')
+  async dataList(@Request() req) {
+    const { model, view } =
+      await this.datasService.getViewAndModelFromRequestByAliasOrId(req);
+    return await this.datasService.getDataList({
+      model,
+      view,
+      query: req.query,
+    });
+  }
+}

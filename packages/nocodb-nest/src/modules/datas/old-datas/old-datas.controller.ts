518c21ceffef8e5e8c5f808091650fa8f6664a75
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-04-11 13:06:58 +0800

    feat: OldDatasController - dataCount

--- packages/nocodb-nest/src/modules/datas/old-datas/old-datas.controller.ts
+++ packages/nocodb-nest/src/modules/datas/old-datas/old-datas.controller.ts
@@ -8,12 +8,14 @@ import {
   Patch,
   Post,
   Request,
+  Response,
 } from '@nestjs/common';
 import {
   Acl,
   ExtractProjectIdMiddleware,
 } from '../../../middlewares/extract-project-id/extract-project-id.middleware';
 import { AuthGuard } from '@nestjs/passport';
+import { Project } from 'src/models';
 import { DatasService } from '../datas.service';
 
 @Controller()
@@ -31,5 +33,24 @@ export class OldDatasController {
       view,
       query: req.query,
     });
+  }
+
+  @Get('/nc/:projectId/api/v1/:tableName/count')
+  @Acl('dataCount')
+  async dataCount(
+    @Request() req,
+    @Response() res,
+    @Param('tableName') tableName: string,
+  ) {
+    const { model, view } =
+      await this.datasService.getViewAndModelFromRequestByAliasOrId(req);
+    const project = await Project.get(model.project_id);
+    const countResult = await this.datasService.dataCount({
+      query: req.query,
+      projectName: project.title,
+      tableName: tableName,
+      viewName: view.title,
+    });
+    res.json(countResult);
   }
 }

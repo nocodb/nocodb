c868a7f5cf993107e5d3cbeb2cd334da8de9278f
Pranav C <pranavxc@gmail.com>
2023-04-13 19:37:25 +0530

    fix: remove missing dependency
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb-nest/src/run/docker.ts
+++ packages/nocodb-nest/src/run/docker.ts
@@ -1,9 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 import cors from 'cors';
 import express from 'express';
-import Noco from '../lib/Noco';
-import { User } from '../models'
-import nocobuild from '../nocobuild'
+import nocobuild from '../nocobuild';
 
 const server = express();
 server.enable('trust proxy');
@@ -12,17 +10,16 @@ server.disable('x-powered-by');
 server.use(
   cors({
     exposedHeaders: 'xc-db-response',
-  })
+  }),
 );
 
 server.set('view engine', 'ejs');
 
 process.env[`DEBUG`] = 'xc*';
 
-
 (async () => {
   await nocobuild(server);
   const httpServer = server.listen(process.env.PORT || 8080, async () => {
-     console.log('Server started')
+    console.log('Server started');
   });
 })().catch((e) => console.log(e));

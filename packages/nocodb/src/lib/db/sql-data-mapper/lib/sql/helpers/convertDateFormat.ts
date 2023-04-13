02fd36589d1d446d9ca5c2ed5cb33802a1fa09ae
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-04-13 16:30:00 +0800

    fix(nocodb): sqlite -> sqlite3

--- packages/nocodb/src/lib/db/sql-data-mapper/lib/sql/helpers/convertDateFormat.ts
+++ packages/nocodb/src/lib/db/sql-data-mapper/lib/sql/helpers/convertDateFormat.ts
@@ -1,22 +1,22 @@
 export function convertDateFormat(date_format: string, type: string) {
   if (date_format === 'YYYY-MM-DD') {
-    if (type === 'mysql2' || type === 'sqlite') return '%Y-%m-%d';
+    if (type === 'mysql2' || type === 'sqlite3') return '%Y-%m-%d';
   } else if (date_format === 'YYYY/MM/DD') {
-    if (type === 'mysql2' || type === 'sqlite') return '%Y/%m/%d';
+    if (type === 'mysql2' || type === 'sqlite3') return '%Y/%m/%d';
   } else if (date_format === 'DD-MM-YYYY') {
-    if (type === 'mysql2' || type === 'sqlite') return '%d/%m/%Y';
+    if (type === 'mysql2' || type === 'sqlite3') return '%d/%m/%Y';
   } else if (date_format === 'MM-DD-YYYY') {
-    if (type === 'mysql2' || type === 'sqlite') return '%d-%m-%Y';
+    if (type === 'mysql2' || type === 'sqlite3') return '%d-%m-%Y';
   } else if (date_format === 'DD/MM/YYYY') {
-    if (type === 'mysql2' || type === 'sqlite') return '%d/%m/%Y';
+    if (type === 'mysql2' || type === 'sqlite3') return '%d/%m/%Y';
   } else if (date_format === 'MM/DD/YYYY') {
-    if (type === 'mysql2' || type === 'sqlite') return '%m-%d-%Y';
+    if (type === 'mysql2' || type === 'sqlite3') return '%m-%d-%Y';
   } else if (date_format === 'DD MM YYYY') {
-    if (type === 'mysql2' || type === 'sqlite') return '%d %m %Y';
+    if (type === 'mysql2' || type === 'sqlite3') return '%d %m %Y';
   } else if (date_format === 'MM DD YYYY') {
-    if (type === 'mysql2' || type === 'sqlite') return '%m %d %Y';
+    if (type === 'mysql2' || type === 'sqlite3') return '%m %d %Y';
   } else if (date_format === 'YYYY MM DD') {
-    if (type === 'mysql2' || type === 'sqlite') return '%Y %m %d';
+    if (type === 'mysql2' || type === 'sqlite3') return '%Y %m %d';
   }
   // pg / mssql
   return date_format;

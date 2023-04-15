aa2d64520c45f759bcf574c642d9d7cff4c27c3c
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-04-16 00:23:31 +0800

    feat: add MulterModule.register

--- packages/nocodb-nest/src/modules/public-datas/public-datas.module.ts
+++ packages/nocodb-nest/src/modules/public-datas/public-datas.module.ts
@@ -1,8 +1,19 @@
+import multer from 'multer';
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { NC_ATTACHMENT_FIELD_SIZE } from '../../constants';
 import { PublicDatasService } from './public-datas.service';
 import { PublicDatasController } from './public-datas.controller';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      storage: multer.diskStorage({}),
+      limits: {
+        fileSize: NC_ATTACHMENT_FIELD_SIZE,
+      },
+    }),
+  ],
   controllers: [PublicDatasController],
   providers: [PublicDatasService],
 })

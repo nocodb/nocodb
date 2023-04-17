e691eced499039544895313ccc07867581ed97d2
Pranav C <pranavxc@gmail.com>
2023-04-17 12:09:25 +0530

    chore: add docker and npm build script
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb-nest/src/controllers/hook-filters.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { HookFiltersService } from '../services/hook-filters.service';
-
-@Controller()
-export class HookFiltersController {
-  constructor(private readonly hookFiltersService: HookFiltersService) {}
-}

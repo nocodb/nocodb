5d195f72cd7c21e488c80d78056d4a0d13e3b3e3
Pranav C <pranavxc@gmail.com>
2023-04-18 11:24:54 +0530

    feat: add middleware to serve static file and redirect root to dashboard
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb-nest/src/app.module.ts
+++ packages/nocodb-nest/src/app.module.ts
@@ -1,12 +1,11 @@
-import { join } from 'path';
 import { Module, RequestMethod } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { Connection } from './connection/connection';
 import { GlobalExceptionFilter } from './filters/global-exception/global-exception.filter';
 import NcPluginMgrv2 from './helpers/NcPluginMgrv2';
 import { GlobalMiddleware } from './middlewares/global/global.middleware';
 import { GuiMiddleware } from './middlewares/gui/gui.middleware';
+import { PublicMiddleware } from './middlewares/public/public.middleware'
 import { DatasModule } from './modules/datas/datas.module';
 import { AuthService } from './services/auth.service';
 import { UsersModule } from './modules/users/users.module';
@@ -17,7 +16,6 @@ import { GlobalModule } from './modules/global/global.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AuthTokenStrategy } from './strategies/authtoken.strategy/authtoken.strategy';
 import { BaseViewStrategy } from './strategies/base-view.strategy/base-view.strategy';
-import { GoogleStrategy } from './strategies/google.strategy/google.strategy';
 import NcUpgrader from './version-upgrader/NcUpgrader';
 import { MetasModule } from './modules/metas/metas.module';
 import type {
@@ -28,9 +26,6 @@ import NocoCache from './cache/NocoCache';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), 'public'),
-    }),
     GlobalModule,
     UsersModule,
     ...(process.env['PLAYWRIGHT_TEST'] === 'true' ? [TestModule] : []),
@@ -59,6 +54,8 @@ export class AppModule implements OnApplicationBootstrap {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(GuiMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.GET })
+      .apply(PublicMiddleware)
       .forRoutes({ path: '*', method: RequestMethod.GET })
       .apply(GlobalMiddleware)
       .forRoutes({ path: '*', method: RequestMethod.ALL });

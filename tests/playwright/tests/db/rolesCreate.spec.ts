04444c60ab33bdeec798ac11acfb013a5298995e
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-04-20 11:06:48 +0530

    chore: folder restructure
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

---
+++ tests/playwright/tests/db/rolesCreate.spec.ts
@@ -0,0 +1,127 @@
+import { test } from '@playwright/test';
+import { DashboardPage } from '../../pages/Dashboard';
+import setup from '../../setup';
+import { SettingsPage, SettingTab } from '../../pages/Dashboard/Settings';
+import { SignupPage } from '../../pages/SignupPage';
+import { ProjectsPage } from '../../pages/ProjectsPage';
+
+const roleDb = [
+  { email: 'creator@nocodb.com', role: 'creator', url: '' },
+  { email: 'editor@nocodb.com', role: 'editor', url: '' },
+  { email: 'commenter@nocodb.com', role: 'commenter', url: '' },
+  { email: 'viewer@nocodb.com', role: 'viewer', url: '' },
+];
+
+test.describe('User roles', () => {
+  let dashboard: DashboardPage;
+  let settings: SettingsPage;
+  let signupPage: SignupPage;
+  let projectsPage: ProjectsPage;
+  let context: any;
+
+  test.beforeEach(async ({ page }) => {
+    context = await setup({ page });
+    dashboard = new DashboardPage(page, context.project);
+    settings = dashboard.settings;
+    signupPage = new SignupPage(page);
+    projectsPage = new ProjectsPage(page);
+  });
+
+  test('Create role', async () => {
+    test.slow();
+
+    // close 'Team & Auth' tab
+    await dashboard.closeTab({ title: 'Team & Auth' });
+    await dashboard.gotoSettings();
+    await settings.selectTab({ tab: SettingTab.TeamAuth });
+    for (let i = 0; i < roleDb.length; i++) {
+      roleDb[i].url = await settings.teams.invite({
+        email: roleDb[i].email,
+        role: roleDb[i].role,
+      });
+      await settings.teams.closeInvite();
+    }
+    await settings.close();
+
+    // configure access control
+    await dashboard.gotoSettings();
+    await settings.selectTab({
+      tab: SettingTab.DataSources,
+    });
+    await settings.dataSources.openAcl();
+    await settings.dataSources.acl.toggle({ table: 'Language', role: 'editor' });
+    await settings.dataSources.acl.toggle({ table: 'Language', role: 'commenter' });
+    await settings.dataSources.acl.toggle({ table: 'Language', role: 'viewer' });
+    await settings.dataSources.acl.toggle({ table: 'CustomerList', role: 'editor' });
+    await settings.dataSources.acl.toggle({ table: 'CustomerList', role: 'commenter' });
+    await settings.dataSources.acl.toggle({ table: 'CustomerList', role: 'viewer' });
+    await settings.dataSources.acl.save();
+    await settings.close();
+
+    // Role test
+    for (let i = 0; i < roleDb.length; i++) {
+      await roleTest(i);
+    }
+  });
+
+  async function roleTest(roleIdx: number) {
+    await roleSignup(roleIdx);
+    await dashboard.validateProjectMenu({
+      role: roleDb[roleIdx].role,
+    });
+
+    await dashboard.treeView.openTable({ title: 'Country' });
+
+    await dashboard.viewSidebar.validateRoleAccess({
+      role: roleDb[roleIdx].role,
+    });
+
+    await dashboard.grid.toolbar.validateRoleAccess({
+      role: roleDb[roleIdx].role,
+    });
+
+    await dashboard.treeView.validateRoleAccess({
+      role: roleDb[roleIdx].role,
+    });
+
+    await dashboard.grid.validateRoleAccess({
+      role: roleDb[roleIdx].role,
+    });
+
+    await dashboard.grid.openExpandedRow({ index: 0 });
+    await dashboard.expandedForm.validateRoleAccess({
+      role: roleDb[roleIdx].role,
+    });
+
+    // Access control validation
+    await dashboard.treeView.verifyTable({
+      title: 'Language',
+      exists: roleDb[roleIdx].role === 'creator' ? true : false,
+    });
+    await dashboard.treeView.verifyTable({
+      title: 'CustomerList',
+      exists: roleDb[roleIdx].role === 'creator' ? true : false,
+    });
+  }
+
+  async function roleSignup(roleIdx: number) {
+    await dashboard.signOut();
+
+    await dashboard.rootPage.goto(roleDb[roleIdx].url);
+    await signupPage.signUp({
+      email: roleDb[roleIdx].email,
+      password: 'Password123.',
+    });
+
+    await projectsPage.openProject({
+      title: context.project.title,
+      waitForAuthTab: roleDb[roleIdx].role === 'creator',
+      withoutPrefix: true,
+    });
+
+    // close 'Team & Auth' tab
+    if (roleDb[roleIdx].role === 'creator') {
+      await dashboard.closeTab({ title: 'Team & Auth' });
+    }
+  }
+});
---
+++ tests/playwright/tests/db/rolesPreview.spec.ts
@@ -0,0 +1,105 @@
+import { test } from '@playwright/test';
+import { DashboardPage } from '../../pages/Dashboard';
+import setup from '../../setup';
+import { ToolbarPage } from '../../pages/Dashboard/common/Toolbar';
+import { SettingsPage, SettingTab } from '../../pages/Dashboard/Settings';
+
+const roles = ['Editor', 'Commenter', 'Viewer'];
+
+test.describe('Preview Mode', () => {
+  test.slow();
+
+  let dashboard: DashboardPage;
+  let toolbar: ToolbarPage;
+  let settings: SettingsPage;
+
+  let context: any;
+
+  test.beforeEach(async ({ page }) => {
+    context = await setup({ page });
+    dashboard = new DashboardPage(page, context.project);
+    toolbar = dashboard.grid.toolbar;
+    settings = dashboard.settings;
+  });
+
+  test('Preview Mode', async () => {
+    // close 'Team & Auth' tab
+    await dashboard.closeTab({ title: 'Team & Auth' });
+
+    // configure ACL
+    // configure access control
+    await dashboard.gotoSettings();
+    await settings.selectTab({
+      tab: SettingTab.DataSources,
+    });
+    await settings.dataSources.openAcl();
+    await settings.dataSources.acl.toggle({ table: 'Language', role: 'editor' });
+    await settings.dataSources.acl.toggle({ table: 'Language', role: 'commenter' });
+    await settings.dataSources.acl.toggle({ table: 'Language', role: 'viewer' });
+    await settings.dataSources.acl.toggle({ table: 'CustomerList', role: 'editor' });
+    await settings.dataSources.acl.toggle({ table: 'CustomerList', role: 'commenter' });
+    await settings.dataSources.acl.toggle({ table: 'CustomerList', role: 'viewer' });
+    await settings.dataSources.acl.save();
+    await settings.close();
+
+    // Role test
+    for (let i = 0; i < roles.length; i++) {
+      await roleTest(roles[i]);
+    }
+  });
+
+  async function roleTest(role: string) {
+    await dashboard.grid.projectMenu.toggle();
+    await dashboard.grid.projectMenu.click({
+      menu: 'Preview as',
+      subMenu: role,
+    });
+
+    // wait for preview mode to be enabled
+    await dashboard.rootPage.locator('.nc-preview-btn-exit-to-app').waitFor();
+    // todo: Otherwise grid will be stuck at loading even tho the data is loaded
+    await dashboard.rootPage.waitForTimeout(2500);
+
+    await dashboard.validateProjectMenu({
+      role: role.toLowerCase(),
+    });
+
+    await dashboard.rootPage.waitForTimeout(1500);
+
+    await dashboard.treeView.openTable({ title: 'Country', networkResponse: false });
+
+    await dashboard.viewSidebar.validateRoleAccess({
+      role: role.toLowerCase(),
+    });
+
+    await toolbar.validateRoleAccess({
+      role: role.toLowerCase(),
+    });
+
+    await dashboard.treeView.validateRoleAccess({
+      role: role.toLowerCase(),
+    });
+
+    await dashboard.grid.validateRoleAccess({
+      role: role.toLowerCase(),
+    });
+
+    await dashboard.grid.openExpandedRow({ index: 0 });
+    await dashboard.expandedForm.validateRoleAccess({
+      role: role.toLowerCase(),
+    });
+
+    // Access control validation
+    await dashboard.treeView.verifyTable({
+      title: 'Language',
+      exists: role.toLowerCase() === 'creator' ? true : false,
+    });
+    await dashboard.treeView.verifyTable({
+      title: 'CustomerList',
+      exists: role.toLowerCase() === 'creator' ? true : false,
+    });
+
+    // close preview mode
+    await dashboard.rootPage.locator('.nc-preview-btn-exit-to-app').click();
+  }
+});

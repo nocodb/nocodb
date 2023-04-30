144fdfac3e764b2e7828ab5a4683cbd3b6659964
Muhammed Mustafa <mustafa@nocodb.com>
2023-04-30 22:20:37 +0530

    feat(nocodocs): On backspace for link to page node, If the current node is empty, delete the current node and select the previous section, If cursor is at the beginning of the node, select the previous section AND If the selected node is link-to-page, open the page on enter

--- packages/nc-gui/utils/tiptapExtensions/linkToPage/index.ts
+++ packages/nc-gui/utils/tiptapExtensions/linkToPage/index.ts
@@ -2,7 +2,7 @@ import { Node, mergeAttributes } from '@tiptap/core'
 import { VueNodeViewRenderer } from '@tiptap/vue-3'
 import { TiptapNodesTypes } from 'nocodb-sdk'
 import { Plugin } from 'prosemirror-state'
-import { nodeTypesContainingSection } from '../helper'
+import { getPositionOfSection, nodeTypesContainingSection } from '../helper'
 import LinkToPageComponent from './link-to-page.vue'
 
 export const LinkToPage = Node.create({
@@ -39,6 +39,55 @@ export const LinkToPage = Node.create({
 
   addNodeView() {
     return VueNodeViewRenderer(LinkToPageComponent)
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      Backspace: () => {
+        const state = this.editor.state
+        if (!state.selection.empty) return false
+
+        const prevSectionPos = getPositionOfPreviousSection(state)
+        if (!prevSectionPos) return false
+
+        const prevSection = state.doc.nodeAt(prevSectionPos)
+        if (prevSection?.firstChild?.type.name !== TiptapNodesTypes.linkToPage) {
+          return false
+        }
+
+        const currentNode = state.selection.$from.node()
+
+        // If cursor is not at the beginning of the node, and the node is not empty, do nothing
+        if (state.selection.from !== state.selection.$from.start() && currentNode.textContent.length !== 0) {
+          return false
+        }
+
+        // If the current node is empty, delete the current node and select the previous section
+        if (state.selection.from === state.selection.$from.start() && currentNode.textContent.length === 0) {
+          this.editor.chain().deleteActiveSection().run()
+        }
+        // If cursor is at the beginning of the node, select the previous section
+        return this.editor.chain().selectPrevSection().selectPrevSection().selectNextSection().run()
+      },
+      Enter: () => {
+        // If the selected node is link-to-page, open the page on enter
+        const state = this.editor.state
+        const currentSectionPos = getPositionOfSection(state)
+        const currentNode = state.selection.$from.node()
+        const linkToPageNode = currentNode.firstChild
+        if (linkToPageNode?.type.name !== TiptapNodesTypes.linkToPage) {
+          return false
+        }
+
+        const linkDom = document.querySelector(
+          `.ProseMirror [pos='${currentSectionPos}'] .link-to-page-wrapper a`,
+        ) as HTMLAnchorElement
+
+        if (!linkDom || !linkDom.click) return false
+
+        linkDom.click()
+      },
+    }
   },
 
   addProseMirrorPlugins() {

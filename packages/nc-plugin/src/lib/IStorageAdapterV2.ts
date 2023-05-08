eee2ad7da3266b2065bb747e8ca0b8bf46ad9fd7
mertmit <mertmit99@gmail.com>
2023-05-08 19:02:55 +0300

    chore: comment for IStorageAdapterV2 change
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-plugin/src/lib/IStorageAdapterV2.ts
+++ packages/nc-plugin/src/lib/IStorageAdapterV2.ts
@@ -1,5 +1,17 @@
-import IStorageAdapter from './IStorageAdapter';
 import { Readable } from 'stream';
+
+import IStorageAdapter from './IStorageAdapter';
+
+/*
+  #ref: https://github.com/nocodb/nocodb/pull/5608
+    fileCreateByStream: write file from a readable stream to the storage
+    fileReadByStream: read file from the storage to a readable stream
+    getDirectoryList: get files available in a directory
+
+    These methods are added to support export/import without buffering all the data in memory.
+    The methods are only available for Local storage adapter for now, and will be implemented for other adapters later.
+    The methods are not used in current codebase, but will be used in future.
+*/
 
 export default interface IStorageAdapterV2 extends IStorageAdapter {
   fileCreateByUrl(

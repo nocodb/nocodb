1e43a10e4503f1e957856e657d6929d4c53c209a
Oluwaseun <oluwaseunus@gmail.com>
2023-05-12 12:14:35 +0100

    fix(mapdefaultdisplayvalue.ts): use id as default display value

--- packages/nocodb/src/lib/meta/helpers/mapDefaultDisplayValue.ts
+++ packages/nocodb/src/lib/meta/helpers/mapDefaultDisplayValue.ts
@@ -6,25 +6,12 @@ export default function mapDefaultDisplayValue<T extends ColumnType>(
   if (!columnsArr.some((column) => column.pv)) {
     const pkIndex = columnsArr.findIndex((column) => column.pk);
 
-    // if PK is at the end of table
-    if (pkIndex === columnsArr.length - 1) {
-      if (pkIndex > 0) {
-        columnsArr[pkIndex - 1].pv = true;
-        return columnsArr[pkIndex - 1];
-      } else if (columnsArr.length > 0) {
-        columnsArr[0].pv = true;
-        return columnsArr[0];
-      }
-      // pk is not at the end of table
-    } else if (pkIndex > -1) {
-      columnsArr[pkIndex + 1].pv = true;
-      return columnsArr[pkIndex + 1];
-      //  no pk at all
-    } else {
-      if (columnsArr.length > 0) {
-        columnsArr[0].pv = true;
-        return columnsArr[0];
-      }
+    if (pkIndex === -1) {
+      columnsArr[0].pv = true;
+      return columnsArr[0];
     }
+
+    columnsArr[pkIndex].pv = true;
+    return columnsArr[pkIndex];
   }
 }

724270ccb2fa21181c8a4007c95bff64078a4217
Pranav C <pranavxc@gmail.com>
2023-05-12 19:27:47 +0530

    fix: use NcConfigFactory for parsing url

--- packages/nocodb-ingest-service/src/clickhouse/clickhouse.service.ts
+++ packages/nocodb-ingest-service/src/clickhouse/clickhouse.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ClickHouse } from 'clickhouse';
 import * as nc_001_notification from './migrations/nc_001_notification';
 import * as nc_002_page from './migrations/nc_002_page';
 import * as nc_003_api_count from './migrations/nc_003_api_count';
 import * as nc_004_api_exec from './migrations/nc_004_api_calls';
 import type { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import NcConfigFactory from '../helpers/NcConfigFactory';
 
 const knownQueryParams = [
   {
@@ -48,6 +49,8 @@ const knownQueryParams = [
 @Injectable()
 export class ClickhouseService implements OnModuleInit, OnModuleDestroy {
   private client: ClickHouse;
+  private logger: Logger = new Logger(ClickhouseService.name);
+
   private config: {
     username?: string;
     password?: string;
@@ -71,55 +74,38 @@ export class ClickhouseService implements OnModuleInit, OnModuleDestroy {
     //   return;
     // }
 
-    const { connection, protocol } = await ClickhouseService.metaUrlToDbConfig(
+    const { connection, client } = await NcConfigFactory.metaUrlToDbConfig(
       process.env.NC_CLICKHOUSE,
     );
 
     this.config = {
-      host: `${protocol ?? 'http'}://${connection.host ?? 'localhost'}` ?? 'http://localhost',
+      host:
+        `${client ?? 'http'}://${connection.host ?? 'localhost'}` ??
+        'http://localhost',
       port: connection.port ?? 8123,
       username: connection.user,
       password: connection.password,
       database: connection.database ?? 'nc',
     };
-
-    // Create a new ClickHouse client instance
-    const clickhouse = new ClickHouse({ ...this.config, database: undefined });
-    for (const { up } of [
-      nc_001_notification,
-      nc_002_page,
-      nc_003_api_count,
-      nc_004_api_exec,
-    ]) {
-      await up(clickhouse, this.config);
-    }
-
-    this.client = new ClickHouse(this.config);
-  }
-
-  static metaUrlToDbConfig(urlString) {
-    const url = new URL(urlString);
-
-    const parsedQuery: Record<string, any> = {};
-    for (const [key, value] of url.searchParams.entries()) {
-      const fnd = knownQueryParams.find(
-        (param) => param.parameter === key || param.aliases.includes(key),
-      );
-      if (fnd) {
-        parsedQuery[fnd.parameter] = value;
-      } else {
-        parsedQuery[key] = value;
+    try {
+      // Create a new ClickHouse client instance
+      const clickhouse = new ClickHouse({
+        ...this.config,
+        database: undefined,
+      });
+      for (const { up } of [
+        nc_001_notification,
+        nc_002_page,
+        nc_003_api_count,
+        nc_004_api_exec,
+      ]) {
+        await up(clickhouse, this.config);
       }
+
+      this.client = new ClickHouse(this.config);
+    } catch (e) {
+      this.logger.error(e);
+      process.exit(1);
     }
-
-    return {
-      protocol: url.protocol.replace(':', ''),
-      connection: {
-        ...parsedQuery,
-        host: url.hostname,
-        port: +url.port,
-      },
-    } as any
-
   }
 }

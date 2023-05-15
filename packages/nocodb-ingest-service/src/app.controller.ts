9c8760a8d9ae9b0735d9dfa0e2fa116fd29b4862
Pranav C <pranavxc@gmail.com>
2023-05-15 23:35:29 +0530

    feat: add consumer handler for topic

--- packages/nocodb-ingest-service/src/app.controller.ts
+++ packages/nocodb-ingest-service/src/app.controller.ts
@@ -73,4 +73,56 @@ export class AppController {
       this.logger.log(e);
     }
   }
+
+  @MessagePattern('cloud-telemetry')
+  async teleEvent(@Payload() messages: any[]) {
+    try {
+      const rows = [];
+
+      // Process the batch of messages
+      messages.forEach((data) => {
+        if (!data.url) return;
+
+        // Extract the necessary data from the message
+        const {
+          workspace_id,
+          user_id,
+          project_id,
+          url,
+          method,
+          status,
+          exec_time,
+          timestamp,
+          ip,
+
+          props,
+        } = data;
+
+        let ipv4 = 'NULL';
+        let ipv6 = 'NULL';
+
+        if (/^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/.test(ip)) {
+          ipv6 = `'${ip}'`;
+        } else if (/^([0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip)) {
+          ipv4 = `'${ip}'`;
+        }
+
+        rows.push(
+          `(${
+            Math.round(timestamp / 1000) ?? 'NOW()'
+          }, '${workspace_id}', '${user_id}', '${project_id}', '${url}', '${method}', ${exec_time}, ${
+            status ?? 'NULL'
+          }, ${ipv4}, ${ipv6})`,
+        );
+      });
+
+      // Generate the ClickHouse insert query
+      const insertQuery = `INSERT INTO api_calls (timestamp, workspace_id, user_id, project_id, url, method, exec_time, status, req_ipv4, req_ipv6, props) 
+                         VALUES ${rows.join(',')}`;
+
+      await this.clickhouseService.execute(insertQuery);
+    } catch (e) {
+      this.logger.log(e);
+    }
+  }
 }
--- packages/nocodb/src/services/clickhouse/migrations/5_telemetry.sql
+++ packages/nocodb/src/services/clickhouse/migrations/5_telemetry.sql
@@ -1,19 +1,18 @@
 CREATE TABLE IF NOT EXISTS telemetry (
     id UUID,
     timestamp DateTime,
+    event String,
+    url String,
+
     workspace_id String,
   	user_id String,
     project_id String,
-    event String,
-    url String,
-    method String,
-    status UInt16,
-    exec_time UInt64,
     req_ipv4 IPv4,
     req_ipv6 IPv6,
     package_id String,
     client_id String,
 
-    props Nullable(Nested)
-  ) ENGINE = MergeTree
-  ORDER BY timestamp;
+    properties JSON,
+
+    PRIMARY KEY (id)
+  ) ENGINE = MergeTree;

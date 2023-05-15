82ed9a7cd51c9cc217acf59a7c1a6edc7aca7f23
Muhammed Mustafa <mustafa@nocodb.com>
2023-05-15 21:49:40 +0530

    fix(nocodocs): Added toggle of text selection inside a table cell

--- packages/nc-gui/utils/tiptapExtensions/listItem/helper/toggleItem.ts
+++ packages/nc-gui/utils/tiptapExtensions/listItem/helper/toggleItem.ts
@@ -1,6 +1,8 @@
+import { TextSelection } from 'prosemirror-state'
 import type { EditorState } from 'prosemirror-state'
 import type { ChainedCommands } from '@tiptap/core'
 import { Slice } from 'prosemirror-model'
+import { TiptapNodesTypes } from 'nocodb-sdk'
 import { nodeTypesContainingSection } from '../../helper'
 import type { ListNodeType } from '.'
 
@@ -56,22 +58,97 @@ const toggleListItemInSliceJson = ({ content, type }: { content: any[]; type: Li
 }
 
 export const toggleItem = ({ chain, type, state }: { chain: () => ChainedCommands; state: EditorState; type: ListNodeType }) => {
-  const currentSecNodePos = getPositionOfSection(state)
-  const nextSecNodeEndPos = getPositionOfSection(state, state.selection.to - 1, 'end')
+  const selection = state.selection
+  const _currentSecNodePos = getPositionOfSection(state)
+  const _nextSecNodeEndPos = getPositionOfSection(state, state.selection.to - 1, 'end')
 
-  const sliceJson = state.doc.slice(currentSecNodePos, nextSecNodeEndPos, false).toJSON()
+  const _currentTableCellPos = getPositionOfTableCell(state)
+  const _nextTableCellEndPos = getPositionOfTableCell(state, state.selection.to - 1, 'end')
+
+  let isTableCellSelected = false
+  let sliceStart: number
+  let sliceEnd: number
+  if (
+    selection instanceof TextSelection &&
+    !selection.empty &&
+    _currentTableCellPos &&
+    _nextTableCellEndPos &&
+    _currentTableCellPos > _currentSecNodePos
+  ) {
+    sliceStart = _currentTableCellPos
+    sliceEnd = _nextTableCellEndPos
+    isTableCellSelected = true
+  } else {
+    sliceStart = _currentSecNodePos
+    sliceEnd = _nextSecNodeEndPos
+  }
+
+  const sliceJson = state.doc.slice(sliceStart, sliceEnd, false).toJSON()
 
   // Toggle a list items under `sec` nodes in slice and reset the prevOrderedListNodeNumber
   prevOrderedListNodeNumber = 0
-  toggleListItemInSliceJson({ content: sliceJson.content, type })
+  if (isTableCellSelected) {
+    sliceJson.content.forEach((node: any) => {
+      node.type = type
+      node.attrs = {
+        ...(node.attrs ?? {}),
+        number: String(prevOrderedListNodeNumber + 1),
+      }
+
+      prevOrderedListNodeNumber = prevOrderedListNodeNumber + 1
+    })
+  } else {
+    toggleListItemInSliceJson({ content: sliceJson.content, type })
+  }
 
   const newSlice = Slice.fromJSON(state.schema, sliceJson)
 
   return chain()
     .command(() => {
-      state.tr.replaceRange(currentSecNodePos, nextSecNodeEndPos, newSlice)
+      state.tr.replaceRange(sliceStart, sliceEnd, newSlice)
 
       return true
     })
-    .setTextSelection(currentSecNodePos + newSlice.size - 1)
+    .setTextSelection(sliceStart + newSlice.size - 1)
+}
+
+/**
+ * Table cell list toggle helper
+ */
+
+const nodeTypesContainingTableCell = [
+  TiptapNodesTypes.doc,
+  TiptapNodesTypes.sec,
+  TiptapNodesTypes.collapsable,
+  TiptapNodesTypes.collapsableHeader,
+  TiptapNodesTypes.collapsableContent,
+  TiptapNodesTypes.column,
+  TiptapNodesTypes.columnContent,
+  TiptapNodesTypes.table,
+  TiptapNodesTypes.tableRow,
+  TiptapNodesTypes.tableCell,
+]
+
+function getPositionOfTableCell(state: EditorState, pos?: number, type: 'start' | 'end' | undefined = 'start') {
+  const searchStopPos = pos ?? state.selection.$from.pos
+  let sectionPos = 0
+  state.doc.descendants((node, _pos) => {
+    if (node.type.name === TiptapNodesTypes.tableCell && _pos < searchStopPos) {
+      sectionPos = _pos
+    }
+
+    if (nodeTypesContainingTableCell.includes(node.type.name as TiptapNodesTypes)) {
+      return true
+    }
+
+    return false
+  })
+
+  if (type === 'end') {
+    const sectionNode = state.doc.nodeAt(sectionPos)!
+
+    return sectionPos + sectionNode?.nodeSize - 1
+  }
+
+  return sectionPos
 }

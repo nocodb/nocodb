722deb5e5e16a27cd52d58dccac0661489ee936d
Pranav C <pranavxc@gmail.com>
2023-05-19 17:48:27 +0530

    fix: add auth guard to tele endpoint

--- packages/nocodb/src/controllers/telemetry.controller.ts
+++ packages/nocodb/src/controllers/telemetry.controller.ts
@@ -1,13 +1,17 @@
-import {Body, Controller, Post, Req} from '@nestjs/common';
-import {TelemetryService} from '../services/telemetry.service';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { TelemetryService } from '../services/telemetry.service';
+import { GlobalGuard } from '../guards/global/global.guard';
 
 @Controller()
+@UseGuards(GlobalGuard)
 export class TelemetryController {
-  constructor(private readonly telemetryService: TelemetryService) {
-  }
+  constructor(private readonly telemetryService: TelemetryService) {}
 
   @Post('/api/v1/tele')
-  async trackEvents(@Body() body: { clientId: string; events: any[] }, @Req() req) {
-    await this.telemetryService.trackEvents({ body, req});
+  async trackEvents(
+    @Body() body: { clientId: string; events: any[] },
+    @Req() req,
+  ) {
+    await this.telemetryService.trackEvents({ body, req });
   }
 }

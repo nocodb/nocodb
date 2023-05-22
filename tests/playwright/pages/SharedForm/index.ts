37331cd803aae5c3763eead89b03bf45e94090f6
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-05-22 15:51:12 +0530

    test: LTAR verification in shared form
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- tests/playwright/pages/SharedForm/index.ts
+++ tests/playwright/pages/SharedForm/index.ts
@@ -29,4 +29,39 @@ export class SharedFormPage extends BasePage {
       })
     ).toBeVisible();
   }
+
+  async clickLinkToChildList() {
+    await this.get().locator('button[data-testid="nc-child-list-button-link-to"]').click();
+  }
+
+  async verifyChildList(cardTitle?: string[]) {
+    await this.get().locator('.nc-modal-link-record').waitFor();
+    const linkRecord = await this.get();
+
+    // DOM element validation
+    //    title: Link Record
+    //    button: Add new record
+    //    icon: reload
+    await expect(this.get().locator(`.ant-modal-title`)).toHaveText(`Link record`);
+
+    // add new record option is not available for shared form
+    await expect(await linkRecord.locator(`button:has-text("Add new record")`).isVisible()).toBeFalsy();
+
+    await expect(await linkRecord.locator(`.nc-reload`).isVisible()).toBeTruthy();
+    // placeholder: Filter query
+    await expect(await linkRecord.locator(`[placeholder="Filter query"]`).isVisible()).toBeTruthy();
+
+    {
+      const childList = linkRecord.locator(`.ant-card`);
+      const childCards = await childList.count();
+      await expect(childCards).toEqual(cardTitle.length);
+      for (let i = 0; i < cardTitle.length; i++) {
+        await expect(await childList.nth(i).textContent()).toContain(cardTitle[i]);
+      }
+    }
+  }
+
+  async selectChildList(cardTitle: string) {
+    await this.get().locator(`.ant-card:has-text("${cardTitle}"):visible`).click();
+  }
 }

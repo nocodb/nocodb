433e57b03ecdcc6f400d0864f9998e728b828893
Daniel Spaude <daniel@spaude.de>
2023-05-22 13:10:17 +0200

    Dashboard - MVP - 'Create Dashboard project' overlay - WIP

--- packages/nc-gui/components/workspace/CreateDashboardProjectDlg.vue
+++ packages/nc-gui/components/workspace/CreateDashboardProjectDlg.vue
@@ -3,6 +3,9 @@ import type { RuleObject } from 'ant-design-vue/es/form'
 import type { Form, Input } from 'ant-design-vue'
 import type { VNodeRef } from '@vue/runtime-core'
 import { computed } from '@vue/reactivity'
+import type { ComputedRef } from 'nuxt/dist/app/compat/capi'
+import Fuse from 'fuse.js'
+import type { IdAndTitle } from '../dashboards/types'
 import { NcProjectType, extractSdkResponseErrorMsg } from '~/utils'
 import { ref, useVModel } from '#imports'
 import { useWorkspace } from '~/store/workspace'
@@ -19,6 +22,7 @@ const dialogShow = useVModel(props, 'modelValue', emit)
 const projectsStore = useProjects()
 
 const workspaceStore = useWorkspace()
+const input: VNodeRef = ref<typeof Input>()
 
 const { createProject: _createProject } = projectsStore
 
@@ -36,24 +40,21 @@ const formState = reactive({
   title: '',
 })
 
-const route = useRoute()
+// const route = useRoute()
 const creating = ref(false)
 
-const createProject = async () => {
+const createDashboardProject = async () => {
   creating.value = true
   try {
     const project = await _createProject({
-      type: props.type,
+      type: NcProjectType.DASHBOARD,
       title: formState.title,
       workspaceId: workspaceStore.workspace!.id!,
     })
 
     await workspaceStore.loadProjects()
-    navigateToProject({
-      projectId: project.id!,
-      workspaceId: workspaceStore.workspace!.id!,
-      type: props.type,
-    })
+    navigateTo(`/ws/${workspaceStore.workspace!.id!}/project/${project.id!}`)
+
     dialogShow.value = false
   } catch (e: any) {
     message.error(await extractSdkResponseErrorMsg(e))
@@ -61,20 +62,6 @@ const createProject = async () => {
     creating.value = false
   }
 }
-
-// todo: move to utils
-function navigateToProject(param: { projectId: string; workspaceId: string; type: NcProjectType }) {
-  switch (param.type) {
-    case NcProjectType.DOCS:
-      navigateTo(`/ws/${param.workspaceId}/nc/${param.projectId}/doc`)
-      break
-    default:
-      navigateTo(`/ws/${param.workspaceId}/project/${param.projectId}`)
-      break
-  }
-}
-
-const input: VNodeRef = ref<typeof Input>()
 
 watch(dialogShow, async (n, o) => {
   if (n === o && !n) return
@@ -84,15 +71,108 @@ watch(dialogShow, async (n, o) => {
   input.value?.$el?.select()
 })
 
-const typeLabel = computed(() => {
-  switch (props.type) {
-    case NcProjectType.DOCS:
-      return 'Book'
-    case NcProjectType.DB:
-      return 'Database'
-    default:
-      return ''
+// const { $e } = useNuxtApp()
+
+// const { loadProject } = useProject()
+
+// const { refreshCommandPalette } = useCommandPalette()
+
+// const { api, isLoading } = useApi({ useGlobalInstance: true })
+
+useSidebar('nc-left-sidebar', { hasSidebar: false })
+
+const { loadWorkspaceList } = workspaceStore
+const { projects } = storeToRefs(workspaceStore)
+const availableDbProjects: ComputedRef<Array<IdAndTitle>> = computed(() => {
+  return (
+    projects.value
+      ?.filter((project) => project.type === 'database')
+      .map((project) => ({
+        id: project.id!,
+        title: project.title || 'unknown',
+      })) || []
+  )
+})
+
+type ToggableDBProject = Array<IdAndTitle & { isToggle: boolean }>
+
+const dbProjectSearchTerm = ref('')
+// const fuse = ref<ToggableDBProject | null>(null)
+const fuse = ref<Fuse<{
+  id: string
+  title: string
+  isToggle: boolean
+}> | null>(null)
+
+const dbProjectsWithToggleStatus = ref<ToggableDBProject>([])
+
+watch(availableDbProjects, (projects) => {
+  dbProjectsWithToggleStatus.value = projects.map((project) => ({
+    ...project,
+    isToggle: false,
+  }))
+  fuse.value = new Fuse(dbProjectsWithToggleStatus.value, {
+    keys: ['title'],
+    includeScore: true,
+    threshold: 0.0,
+  })
+})
+
+const filteredDbProjects = computed(() => {
+  if (!dbProjectSearchTerm.value) {
+    return dbProjectsWithToggleStatus.value
   }
+  const results = fuse.value?.search(dbProjectSearchTerm.value)
+  return results?.map((result) => result.item) || []
+})
+
+// const createProject = async () => {
+//   $e('a:project:create:xcdb')
+//   try {
+//     // pick a random color from array and assign to project
+//     const color = projectThemeColors[Math.floor(Math.random() * 1000) % projectThemeColors.length]
+//     const tcolor = tinycolor(color)
+
+//     const complement = tcolor.complement()
+
+//     creating.value = true
+
+//     const result = (await api.project.create({
+//       title: formState.title,
+//       fk_workspace_id: route.query.workspaceId,
+//       linked_db_project_ids: filteredDbProjects.value.filter((project) => project.isToggle).map((project) => project.id),
+//       type: NcProjectType.DASHBOARD,
+//       color,
+//       meta: JSON.stringify({
+//         theme: {
+//           primaryColor: color,
+//           accentColor: complement.toHex8String(),
+//         },
+//         ...(route.query.type === NcProjectType.COWRITER && { prompt_statement: '' }),
+//       }),
+//     })) as Partial<ProjectType>
+
+//     refreshCommandPalette()
+
+//     switch (route.query.type) {
+//       case NcProjectType.DASHBOARD:
+//         await loadProject(true, result.id)
+//         await navigateTo(`/ws/${route.query.workspaceId}/nc/${result.id}/layout`)
+//         break
+//     }
+//   } catch (e: any) {
+//     message.error(await extractSdkResponseErrorMsg(e))
+//   } finally {
+//     creating.value = false
+//   }
+// }
+
+onMounted(async () => {
+  await loadWorkspaceList()
+  formState.title = await generateUniqueName()
+  await nextTick()
+  input.value?.$el?.focus()
+  input.value?.$el?.select()
 })
 </script>
 
@@ -117,7 +197,7 @@ const typeLabel = computed(() => {
         class="lg:max-w-3/4 w-full !mx-auto"
         no-style
         autocomplete="off"
-        @finish="createProject"
+        @finish="createDashboardProject"
       >
         <h2 class="prose-2xl font-bold self-center my-4">{{ $t('dashboards.create_new_dashboard_project') }}</h2>
         <a-form-item :label="$t('labels.projName')" name="title" :rules="nameValidationRules" class="m-10">
@@ -152,7 +232,7 @@ const typeLabel = computed(() => {
         :disabled="creating"
         size="large"
         type="primary"
-        @click="createProject"
+        @click="createDashboardProject"
         >{{ $t('general.create') }}
       </a-button>
 

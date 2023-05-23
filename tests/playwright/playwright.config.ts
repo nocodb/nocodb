0c673e39bf67d3bc9ff5e2a15d0e9826518a4c55
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-05-23 21:37:23 +0530

    test: disable parallel threads
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -28,7 +28,8 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : 4,
+  // workers: process.env.CI ? 2 : 4,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */

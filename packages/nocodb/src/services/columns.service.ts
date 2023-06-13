7433af41860ab617ddbc647678448bb90fefe9fb
Pranav C <pranavxc@gmail.com>
2023-06-13 12:49:15 +0530

    fix: hm relation creation
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nocodb/src/services/columns.service.ts
+++ packages/nocodb/src/services/columns.service.ts
@@ -1619,8 +1619,8 @@ export class ColumnsService {
         (param.column as LinkToAnotherColumnReqType).title,
         foreignKeyName,
         (param.column as LinkToAnotherColumnReqType).virtual,
-        param.column['meta'],
         null,
+        param.column['meta'],
         isLinks,
       );
     } else if ((param.column as LinkToAnotherColumnReqType).type === 'mm') {

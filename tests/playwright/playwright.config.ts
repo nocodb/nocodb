4c0ae3652b1d7a93a9d88478f40fb24aee47367c
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-19 17:27:38 +0800

    test(playwright): try higher timeout

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 13000,
+    timeout: 30000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,

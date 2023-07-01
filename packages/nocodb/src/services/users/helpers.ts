7af6c0887364026cb7248e657173ffb058b40dd8
Pranav C <pranavxc@gmail.com>
2023-07-01 11:44:08 +0530

    feat: share refresh token across subdomains

--- packages/nocodb/src/services/users/helpers.ts
+++ packages/nocodb/src/services/users/helpers.ts
@@ -1,8 +1,8 @@
 import crypto from 'crypto';
 import * as jwt from 'jsonwebtoken';
 import type User from '../../models/User';
-import type { NcConfig } from '../../interface/config';
-import type { Response } from 'express';
+import type {NcConfig} from '../../interface/config';
+import type {Response} from 'express';
 
 export function genJwt(user: User, config: NcConfig) {
   return jwt.sign(
@@ -17,7 +17,7 @@ export function genJwt(user: User, config: NcConfig) {
     },
     config.auth.jwt.secret,
     // todo: better typing
-    { expiresIn: '10h', ...(config.auth.jwt.options as any) },
+    {expiresIn: '10h', ...(config.auth.jwt.options as any)},
   );
 }
 
@@ -30,6 +30,7 @@ export function setTokenCookie(res: Response, token): void {
   const cookieOptions = {
     httpOnly: true,
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    domain: process.env.NC_BASE_HOST_NAME || undefined,
   };
   res.cookie('refresh_token', token, cookieOptions);
 }

6236082c714cbfc84efc3885e333412f4e9087af
Muhammed Mustafa <mustafa@nocodb.com>
2023-07-01 17:54:23 +0530

    fix(nocohub): Improved webhooks and sidebar toggle button

--- packages/nc-gui/components/smartsheet/sidebar/toolbar/Developer.vue
+++ packages/nc-gui/components/smartsheet/sidebar/toolbar/Developer.vue
@@ -7,38 +7,28 @@ const isPublicView = inject(IsPublicInj, ref(false))
 
 const showApiSnippetDrawer = ref(false)
 const showErd = ref(false)
-const showWebhookDrawer = ref(false)
 </script>
 
 <template>
-  <div class="flex flex-col px-3">
+  <div class="flex flex-col px-3 nc-sidebar-md">
     <div
       v-if="!isMobileMode"
       v-e="['c:erd:open']"
       class="py-2 flex gap-2 items-center nc-view-action-erd button"
       @click="showErd = true"
     >
-      <component :is="iconMap.erd" class="text-gray-500" />
+      <component :is="iconMap.erd" class="text-gray-600" />
       {{ $t('title.erdView') }}
     </div>
     <div v-e="['c:snippet:open']" class="py-2 flex gap-2 items-center button" @click="showApiSnippetDrawer = true">
-      <component :is="iconMap.snippet" class="text-gray-500" />
+      <component :is="iconMap.snippet" class="text-gray-600" />
       <!-- Get API Snippet -->
       {{ $t('activity.getApiSnippet') }}
     </div>
-    <div
-      v-if="isUIAllowed('webhook') && !isPublicView"
-      v-e="['c:actions:webhook']"
-      class="py-2 flex gap-2 items-center button"
-      @click="showWebhookDrawer = true"
-    >
-      <component :is="iconMap.hook" class="text-gray-500" />
-      {{ $t('objects.webhooks') }}
-    </div>
+    <SmartsheetSidebarToolbarWebhook v-if="isUIAllowed('webhook') && !isPublicView" />
 
     <LazySmartsheetToolbarErd v-model="showErd" />
     <LazySmartsheetApiSnippet v-model="showApiSnippetDrawer" />
-    <LazyWebhookDrawer v-if="showWebhookDrawer" v-model="showWebhookDrawer" />
   </div>
 </template>
 

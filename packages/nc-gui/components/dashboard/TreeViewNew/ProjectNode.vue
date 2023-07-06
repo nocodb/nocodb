9b8bd5070671f791fe7365cd29e9d23805b5e14b
Muhammed Mustafa <mustafa@nocodb.com>
2023-07-06 12:12:35 +0530

    feat(nocohub): Added base import options

--- packages/nc-gui/components/dashboard/TreeViewNew/ProjectNode.vue
+++ packages/nc-gui/components/dashboard/TreeViewNew/ProjectNode.vue
@@ -38,7 +38,7 @@ const {
 const { projects } = storeToRefs(projectsStore)
 
 const { loadProjectTables } = useTablesStore()
-const { projectTables, activeTable } = storeToRefs(useTablesStore())
+const { activeTable } = storeToRefs(useTablesStore())
 
 const { addNewLayout } = useDashboardStore()
 
@@ -73,6 +73,7 @@ const { projectUrl: docsProjectUrl } = useDocStore()
 const { $e } = useNuxtApp()
 
 const isOptionsOpen = ref(false)
+const isBasesOptionsOpen = ref<Record<string, boolean>>({})
 
 const contextMenuTarget = reactive<{ type?: 'project' | 'base' | 'table' | 'main' | 'layout'; value?: any }>({})
 const activeKey = ref<string[]>([])
@@ -324,79 +325,6 @@ const reloadTables = async () => {
   // await loadTables()
 }
 
-function openSchemaMagicDialog(baseId?: string) {
-  $e('c:table:create:navdraw')
-
-  const isOpen = ref(true)
-
-  const { close } = useDialog(resolveComponent('DlgSchemaMagic'), {
-    'modelValue': isOpen,
-    'baseId': baseId,
-    'onUpdate:modelValue': closeDialog,
-  })
-
-  function closeDialog() {
-    isOpen.value = false
-
-    close(1000)
-  }
-}
-
-function openQuickImportDialog(type: string, baseId?: string) {
-  $e(`a:actions:import-${type}`)
-
-  const isOpen = ref(true)
-
-  const { close } = useDialog(resolveComponent('DlgQuickImport'), {
-    'modelValue': isOpen,
-    'importType': type,
-    'baseId': baseId,
-    'onUpdate:modelValue': closeDialog,
-  })
-
-  function closeDialog() {
-    isOpen.value = false
-
-    close(1000)
-  }
-}
-
-function openAirtableImportDialog(baseId?: string) {
-  $e('a:actions:import-airtable')
-
-  const isOpen = ref(true)
-
-  const { close } = useDialog(resolveComponent('DlgAirtableImport'), {
-    'modelValue': isOpen,
-    'baseId': baseId,
-    'onUpdate:modelValue': closeDialog,
-  })
-
-  function closeDialog() {
-    isOpen.value = false
-
-    close(1000)
-  }
-}
-
-function openTableCreateMagicDialog(baseId?: string) {
-  $e('c:table:create:navdraw')
-
-  const isOpen = ref(true)
-
-  const { close } = useDialog(resolveComponent('DlgTableMagic'), {
-    'modelValue': isOpen,
-    'baseId': baseId,
-    'onUpdate:modelValue': closeDialog,
-  })
-
-  function closeDialog() {
-    isOpen.value = false
-
-    close(1000)
-  }
-}
-
 watch(
   () => activeTable.value?.id,
   async () => {
@@ -417,6 +345,10 @@ watch(
 onKeyStroke('Escape', () => {
   if (isOptionsOpen.value) {
     isOptionsOpen.value = false
+  }
+
+  for (const key of Object.keys(isBasesOptionsOpen.value)) {
+    isBasesOptionsOpen.value[key] = false
   }
 })
 </script>
@@ -599,138 +531,7 @@ onKeyStroke('Escape', () => {
                 <!--          </a-menu> -->
 
                 <template v-if="project?.bases?.[0]?.enabled && !project?.bases?.slice(1).filter((el) => el.enabled)?.length">
-                  <a-menu-item-group class="!px-0 !mx-0">
-                    <template #title>
-                      <div class="flex items-center">
-                        Noco
-                        <GeneralIcon icon="magic" class="ml-1 text-orange-400" />
-                      </div>
-                    </template>
-                    <a-menu-item key="table-magic" @click="openTableCreateMagicDialog(project.bases[0].id)">
-                      <div class="color-transition nc-project-menu-item group">
-                        <GeneralIcon icon="magic1" class="group-hover:text-accent" />
-                        Create table
-                      </div>
-                    </a-menu-item>
-                    <a-menu-item key="schema-magic" @click="openSchemaMagicDialog(project.bases[0].id)">
-                      <div class="color-transition nc-project-menu-item group">
-                        <GeneralIcon icon="magic1" class="group-hover:text-accent" />
-                        Create schema
-                      </div>
-                    </a-menu-item>
-                  </a-menu-item-group>
-
-                  <a-menu-divider class="my-0" />
-
-                  <!-- Quick Import From -->
-                  <a-menu-item-group :title="$t('title.quickImportFrom')" class="!px-0 !mx-0">
-                    <a-menu-item
-                      v-if="isUIAllowed('airtableImport', false, projectRole)"
-                      key="quick-import-airtable"
-                      @click="openAirtableImportDialog(project.bases[0].id)"
-                    >
-                      <div class="color-transition nc-project-menu-item group">
-                        <GeneralIcon icon="airtable" class="group-hover:text-accent" />
-                        Airtable
-                      </div>
-                    </a-menu-item>
-
-                    <a-menu-item
-                      v-if="isUIAllowed('csvImport', false, projectRole)"
-                      key="quick-import-csv"
-                      @click="openQuickImportDialog('csv', project.bases[0].id)"
-                    >
-                      <div class="color-transition nc-project-menu-item group">
-                        <GeneralIcon icon="csv" class="group-hover:text-accent" />
-                        CSV file
-                      </div>
-                    </a-menu-item>
-
-                    <a-menu-item
-                      v-if="isUIAllowed('jsonImport', false, projectRole)"
-                      key="quick-import-json"
-                      @click="openQuickImportDialog('json', project.bases[0].id)"
-                    >
-                      <div class="color-transition nc-project-menu-item group">
-                        <GeneralIcon icon="json" class="group-hover:text-accent" />
-                        JSON file
-                      </div>
-                    </a-menu-item>
-
-                    <a-menu-item
-                      v-if="isUIAllowed('excelImport', false, projectRole)"
-                      key="quick-import-excel"
-                      @click="openQuickImportDialog('excel', project.bases[0].id)"
-                    >
-                      <div class="color-transition nc-project-menu-item group">
-                        <GeneralIcon icon="excel" class="group-hover:text-accent" />
-                        Microsoft Excel
-                      </div>
-                    </a-menu-item>
-                  </a-menu-item-group>
-
-                  <a-menu-divider class="my-0" />
-
-                  <a-menu-item-group title="Connect to new datasource" class="!px-0 !mx-0">
-                    <a-menu-item
-                      key="connect-new-source"
-                      @click="toggleDialog(true, 'dataSources', ClientType.MYSQL, project.id)"
-                    >
-                      <div class="color-transition nc-project-menu-item group">
-                        <LogosMysqlIcon class="group-hover:text-accent" />
-                        MySQL
-                      </div>
-                    </a-menu-item>
-                    <a-menu-item key="connect-new-source" @click="toggleDialog(true, 'dataSources', ClientType.PG, project.id)">
-                      <div class="color-transition nc-project-menu-item group">
-                        <LogosPostgresql class="group-hover:text-accent" />
-                        Postgres
-                      </div>
-                    </a-menu-item>
-                    <a-menu-item
-                      key="connect-new-source"
-                      @click="toggleDialog(true, 'dataSources', ClientType.SQLITE, project.id)"
-                    >
-                      <div class="color-transition nc-project-menu-item group">
-                        <VscodeIconsFileTypeSqlite class="group-hover:text-accent" />
-                        SQLite
-                      </div>
-                    </a-menu-item>
-                    <a-menu-item
-                      key="connect-new-source"
-                      @click="toggleDialog(true, 'dataSources', ClientType.MSSQL, project.id)"
-                    >
-                      <div class="color-transition nc-project-menu-item group">
-                        <SimpleIconsMicrosoftsqlserver class="group-hover:text-accent" />
-                        MSSQL
-                      </div>
-                    </a-menu-item>
-                    <a-menu-item
-                      v-if="appInfo.ee"
-                      key="connect-new-source"
-                      @click="toggleDialog(true, 'dataSources', ClientType.SNOWFLAKE, project.id)"
-                    >
-                      <div class="color-transition nc-project-menu-item group">
-                        <LogosSnowflakeIcon class="group-hover:text-accent" />
-                        Snowflake
-                      </div>
-                    </a-menu-item>
-                  </a-menu-item-group>
-
-                  <a-menu-divider class="my-0" />
-
-                  <a-menu-item v-if="isUIAllowed('importRequest', false, projectRole)" key="add-new-table" class="py-1 rounded-b">
-                    <a
-                      v-e="['e:datasource:import-request']"
-                      href="https://github.com/nocodb/nocodb/issues/2052"
-                      target="_blank"
-                      class="prose-sm hover:(!text-primary !opacity-100) color-transition nc-project-menu-item group after:(!rounded-b)"
-                    >
-                      <GeneralIcon icon="openInNew" class="group-hover:text-accent" />
-                      <!-- Request a data source you need? -->
-                      {{ $t('labels.requestDataSource') }}
-                    </a>
-                  </a-menu-item>
+                  <DashboardTreeViewNewBaseOptions v-model:project="project" :base="project.bases[0]" />
                 </template>
               </a-menu>
             </template>
@@ -813,11 +614,37 @@ onKeyStroke('Escape', () => {
                             <GeneralBaseLogo :base-type="base.type" />
                             {{ base.alias || '' }}
                           </div>
-                          <div
-                            class="flex h-full items-center invisible nc-sidebar-base-node-btns hover:bg-gray-200 mt-0.5 text-gray-500 hover:text-gray-700 rounded p-0.25"
-                            @click.stop="openTableCreateDialog(baseIndex)"
-                          >
-                            <component :is="iconMap.plus" class="text-inherit" />
+                          <div class="flex flex-row items-center gap-x-1">
+                            <a-dropdown
+                              :visible="isBasesOptionsOpen[base!.id!]"
+                              trigger="click"
+                              @update:visible="isBasesOptionsOpen[base!.id!] = $event"
+                            >
+                              <MdiDotsHorizontal
+                                class="invisible nc-sidebar-base-node-btns hover:text-black py-0.25 h-5.5 w-5 px-0.5 mt-0.25 rounded hover:bg-gray-200 text-gray-600"
+                                :class="{ '!text-black !opacity-100': isBasesOptionsOpen[base!.id!] }"
+                                @click.stop="isBasesOptionsOpen[base!.id!] = !isBasesOptionsOpen[base!.id!]"
+                              />
+                              <template #overlay>
+                                <a-menu
+                                  class="nc-sidebar-md"
+                                  :style="{
+                                    maxHeight: '70vh',
+                                    overflow: 'overlay',
+                                  }"
+                                  @click="isBasesOptionsOpen[base!.id!] = false"
+                                >
+                                  <DashboardTreeViewNewBaseOptions v-model:project="project" :base="base" />
+                                </a-menu>
+                              </template>
+                            </a-dropdown>
+
+                            <div
+                              class="flex h-full items-center invisible nc-sidebar-base-node-btns hover:bg-gray-200 mt-0.25 text-gray-600 hover:text-gray-700 rounded p-0.25"
+                              @click.stop="openTableCreateDialog(baseIndex)"
+                            >
+                              <component :is="iconMap.plus" class="text-inherit" />
+                            </div>
                           </div>
                         </div>
                       </template>
@@ -905,7 +732,7 @@ onKeyStroke('Escape', () => {
   @apply !mx-0 !pl-7.25 !pr-1 !py-0.75 hover:bg-gray-100 !rounded-md;
 }
 
-.nc-sidebar-base-node:hover .nc-sidebar-base-node-btns {
+:deep(.ant-collapse-header:hover .nc-sidebar-base-node-btns) {
   @apply visible;
 }
 </style>

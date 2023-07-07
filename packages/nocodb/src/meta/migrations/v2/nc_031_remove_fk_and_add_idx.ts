0323c4d557840deafaace080b432922d961b5521
Pranav C <pranavxc@gmail.com>
2023-07-07 15:52:44 +0530

    refactor: add log for fk removal migration

--- packages/nocodb/src/meta/migrations/v2/nc_031_remove_fk_and_add_idx.ts
+++ packages/nocodb/src/meta/migrations/v2/nc_031_remove_fk_and_add_idx.ts
@@ -3,23 +3,30 @@ import { MetaTable } from '../../meta.service';
 import type { Knex } from 'knex';
 
 const up = async (knex: Knex) => {
+  console.time(MetaTable.BASES);
   await knex.schema.alterTable(MetaTable.BASES, (table) => {
     table.dropForeign('project_id');
     table.index('project_id');
   });
+  console.timeEnd(MetaTable.BASES);
 
+  console.time(MetaTable.MODELS);
   await knex.schema.alterTable(MetaTable.MODELS, (table) => {
     table.dropForeign('base_id');
     table.index('base_id');
     table.dropForeign('project_id');
     table.index('project_id');
   });
+  console.timeEnd(MetaTable.MODELS);
 
+  console.time(MetaTable.COLUMNS);
   await knex.schema.alterTable(MetaTable.COLUMNS, (table) => {
     table.dropForeign('fk_model_id');
     table.index('fk_model_id');
   });
+  console.timeEnd(MetaTable.COLUMNS);
 
+  console.time(MetaTable.COL_RELATIONS);
   await knex.schema.alterTable(MetaTable.COL_RELATIONS, (table) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
@@ -36,12 +43,16 @@ const up = async (knex: Knex) => {
 
     table.dropForeign('fk_mm_parent_column_id');
   });
+  console.timeEnd(MetaTable.COL_RELATIONS);
 
+  console.time(MetaTable.COL_SELECT_OPTIONS);
   await knex.schema.alterTable(MetaTable.COL_SELECT_OPTIONS, (table) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
   });
+  console.timeEnd(MetaTable.COL_SELECT_OPTIONS);
 
+  console.time(MetaTable.COL_LOOKUP);
   await knex.schema.alterTable(MetaTable.COL_LOOKUP, (table) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
@@ -50,26 +61,34 @@ const up = async (knex: Knex) => {
 
     table.dropForeign('fk_lookup_column_id');
   });
+  console.timeEnd(MetaTable.COL_LOOKUP);
 
+  console.time(MetaTable.COL_QRCODE);
   await knex.schema.alterTable(MetaTable.COL_QRCODE, (table) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
 
     table.dropForeign('fk_qr_value_column_id');
   });
+  console.timeEnd(MetaTable.COL_QRCODE);
 
+  console.time(MetaTable.COL_BARCODE);
   await knex.schema.alterTable(MetaTable.COL_BARCODE, (table) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
 
     table.dropForeign('fk_barcode_value_column_id');
   });
+  console.timeEnd(MetaTable.COL_BARCODE);
 
+  console.time(MetaTable.COL_FORMULA);
   await knex.schema.alterTable(MetaTable.COL_FORMULA, (table) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
   });
+  console.timeEnd(MetaTable.COL_FORMULA);
 
+  console.time(MetaTable.COL_ROLLUP);
   await knex.schema.alterTable(MetaTable.COL_ROLLUP, (table) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
@@ -78,17 +97,23 @@ const up = async (knex: Knex) => {
 
     table.dropForeign('fk_rollup_column_id');
   });
+  console.timeEnd(MetaTable.COL_ROLLUP);
 
+  console.time(MetaTable.VIEWS);
   await knex.schema.alterTable(MetaTable.VIEWS, (table) => {
     table.dropForeign('fk_model_id');
     table.index('fk_model_id');
   });
+  console.timeEnd(MetaTable.VIEWS);
 
+  console.time(MetaTable.HOOKS);
   await knex.schema.alterTable(MetaTable.HOOKS, (table) => {
     table.dropForeign('fk_model_id');
     table.index('fk_model_id');
   });
+  console.timeEnd(MetaTable.HOOKS);
 
+  console.time(MetaTable.FILTER_EXP);
   await knex.schema.alterTable(MetaTable.FILTER_EXP, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
@@ -101,7 +126,9 @@ const up = async (knex: Knex) => {
     table.dropForeign('fk_parent_id');
     table.index('fk_parent_id');
   });
+  console.timeEnd(MetaTable.FILTER_EXP);
 
+  console.time(MetaTable.SORT);
   await knex.schema.alterTable(MetaTable.SORT, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
@@ -109,17 +136,23 @@ const up = async (knex: Knex) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
   });
+  console.timeEnd(MetaTable.SORT);
 
+  console.time(MetaTable.SHARED_VIEWS);
   await knex.schema.alterTable(MetaTable.SHARED_VIEWS, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
   });
+  console.timeEnd(MetaTable.SHARED_VIEWS);
 
+  console.time(MetaTable.FORM_VIEW);
   await knex.schema.alterTable(MetaTable.FORM_VIEW, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
   });
+  console.timeEnd(MetaTable.FORM_VIEW);
 
+  console.time(MetaTable.FORM_VIEW_COLUMNS);
   await knex.schema.alterTable(MetaTable.FORM_VIEW_COLUMNS, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
@@ -127,14 +160,18 @@ const up = async (knex: Knex) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
   });
+  console.timeEnd(MetaTable.FORM_VIEW_COLUMNS);
 
+  console.time(MetaTable.GALLERY_VIEW);
   await knex.schema.alterTable(MetaTable.GALLERY_VIEW, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
 
     table.dropForeign('fk_cover_image_col_id');
   });
+  console.timeEnd(MetaTable.GALLERY_VIEW);
 
+  console.time(MetaTable.GALLERY_VIEW_COLUMNS);
   await knex.schema.alterTable(MetaTable.GALLERY_VIEW_COLUMNS, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
@@ -142,12 +179,16 @@ const up = async (knex: Knex) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
   });
+  console.timeEnd(MetaTable.GALLERY_VIEW_COLUMNS);
 
+  console.time(MetaTable.GRID_VIEW);
   await knex.schema.alterTable(MetaTable.GRID_VIEW, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
   });
+  console.timeEnd(MetaTable.GRID_VIEW);
 
+  console.time(MetaTable.GRID_VIEW_COLUMNS);
   await knex.schema.alterTable(MetaTable.GRID_VIEW_COLUMNS, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
@@ -155,7 +196,9 @@ const up = async (knex: Knex) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
   });
+  console.timeEnd(MetaTable.GRID_VIEW_COLUMNS);
 
+  console.time(MetaTable.MAP_VIEW);
   await knex.schema.alterTable(MetaTable.MAP_VIEW, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
@@ -166,7 +209,9 @@ const up = async (knex: Knex) => {
     table.dropForeign('base_id');
     table.dropForeign('project_id');
   });
+  console.timeEnd(MetaTable.MAP_VIEW);
 
+  console.time(MetaTable.MAP_VIEW_COLUMNS);
   await knex.schema.alterTable(MetaTable.MAP_VIEW_COLUMNS, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
@@ -174,7 +219,9 @@ const up = async (knex: Knex) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
   });
+  console.timeEnd(MetaTable.MAP_VIEW_COLUMNS);
 
+  console.time(MetaTable.KANBAN_VIEW);
   await knex.schema.alterTable(MetaTable.KANBAN_VIEW, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
@@ -184,7 +231,9 @@ const up = async (knex: Knex) => {
 
     table.dropForeign('fk_cover_image_col_id');
   });
+  console.timeEnd(MetaTable.KANBAN_VIEW);
 
+  console.time(MetaTable.KANBAN_VIEW_COLUMNS);
   await knex.schema.alterTable(MetaTable.KANBAN_VIEW_COLUMNS, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
@@ -192,7 +241,9 @@ const up = async (knex: Knex) => {
     table.dropForeign('fk_column_id');
     table.index('fk_column_id');
   });
+  console.timeEnd(MetaTable.KANBAN_VIEW_COLUMNS);
 
+  console.time(MetaTable.PROJECT_USERS);
   await knex.schema.alterTable(MetaTable.PROJECT_USERS, (table) => {
     table.dropForeign('project_id');
     table.index('project_id');
@@ -200,7 +251,9 @@ const up = async (knex: Knex) => {
     table.dropForeign('fk_user_id');
     table.index('fk_user_id');
   });
+  console.timeEnd(MetaTable.PROJECT_USERS);
 
+  console.time(MetaTable.AUDIT);
   await knex.schema.alterTable(MetaTable.AUDIT, (table) => {
     table.dropForeign('project_id');
     table.index('project_id');
@@ -208,12 +261,16 @@ const up = async (knex: Knex) => {
     table.dropForeign('fk_model_id');
     table.index('fk_model_id');
   });
+  console.timeEnd(MetaTable.AUDIT);
 
+  console.time(MetaTable.MODEL_ROLE_VISIBILITY);
   await knex.schema.alterTable(MetaTable.MODEL_ROLE_VISIBILITY, (table) => {
     table.dropForeign('fk_view_id');
     table.index('fk_view_id');
   });
+  console.timeEnd(MetaTable.MODEL_ROLE_VISIBILITY);
 
+  console.time(MetaTable.SYNC_SOURCE);
   await knex.schema.alterTable(MetaTable.SYNC_SOURCE, (table) => {
     table.dropForeign('project_id');
     table.index('project_id');
@@ -221,11 +278,14 @@ const up = async (knex: Knex) => {
     table.dropForeign('base_id');
     table.index('base_id');
   });
+  console.timeEnd(MetaTable.SYNC_SOURCE);
 
+  console.time(MetaTable.API_TOKENS);
   await knex.schema.alterTable(MetaTable.API_TOKENS, (table) => {
     table.dropForeign('fk_user_id');
     table.index('fk_user_id');
   });
+  console.timeEnd(MetaTable.API_TOKENS);
 };
 
 const down = async (knex: Knex) => {

59cf77866f2e0d48703c22f77630cebe415f875a
Pranav C <pranavxc@gmail.com>
2023-07-12 13:04:43 +0530

    refactor: revert file size limit

--- packages/nc-gui/components/dlg/QuickImport.vue
+++ packages/nc-gui/components/dlg/QuickImport.vue
@@ -320,9 +320,9 @@ const customReqCbk = (customReqArgs: { file: any; onSuccess: () => void }) => {
 
 /** check if the file size exceeds the limit */
 const beforeUpload = (file: UploadFile) => {
-  const exceedLimit = file.size! / 1024 / 1024 > 10
+  const exceedLimit = file.size! / 1024 / 1024 > 5
   if (exceedLimit) {
-    message.error(`File ${file.name} is too big. The accepted file size is less than 10MB.`)
+    message.error(`File ${file.name} is too big. The accepted file size is less than 5MB.`)
   }
   return !exceedLimit || Upload.LIST_IGNORE
 }

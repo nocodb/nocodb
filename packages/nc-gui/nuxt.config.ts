6cf51de3022f0fd7ec225a3ed2e07dce38e46b51
Pranav C <pranavxc@gmail.com>
2023-07-13 19:24:33 +0530

    chore: enable minify

--- packages/nc-gui/nuxt.config.ts
+++ packages/nc-gui/nuxt.config.ts
@@ -87,7 +87,7 @@ export default defineNuxtConfig({
       commonjsOptions: {
         ignoreTryCatch: true,
       },
-      minify: false,
+      minify: true,
       rollupOptions: {},
     },
     plugins: [

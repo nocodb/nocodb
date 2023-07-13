f7c0b8603c1434933dde880d5adf1b9cbcc37aec
Pranav C <pranavxc@gmail.com>
2023-07-13 19:06:27 +0530

    chore: improved github action to deploy directly to staging/prod

---
+++ scripts/release/production.sh
@@ -0,0 +1,1 @@
+aws ecs update-service --cluster nocohub-001-a --service nocohub-nocodb_ai_main --force-new-deployment --region=us-east-2
---
+++ scripts/release/staging.sh
@@ -0,0 +1,3 @@
+aws ecs update-service --cluster nocodb-staging --service nocohub-wj1dqpgolupckbi --force-new-deployment --region=us-east-2
+
+aws ecs update-service --cluster nocodb-staging --service nocohub-noco_to_main --force-new-deployment --region=us-east-2

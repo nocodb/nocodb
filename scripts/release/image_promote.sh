2e225a3c7e106d38c832f7f4c33773bd220e8950
starbirdtech383 <starbirdtech@gmail.com>
2023-07-13 19:45:05 +0530

    update staging and production script

--- scripts/release/image_promote.sh
+++ scripts/release/image_promote.sh
@@ -8,7 +8,7 @@
 #
 
 STAGE_TAG=${1:-"ws-pre-release"}
-PROD_TAG=${2:-"ws-ready-for-prod"}
+PROD_TAG=${2:-"ws-prod-ready"}
 
 NC_MANIFEST=$(aws ecr batch-get-image --region us-east-2 --repository-name nocohub --image-ids imageTag=${STAGE_TAG} --output text --query images[].imageManifest) 
 aws ecr put-image --repository-name nocohub --image-tag "${PROD_TAG}" --image-manifest "${NC_MANIFEST}"
--- scripts/release/staging.sh
+++ scripts/release/staging.sh
@@ -1,3 +1,11 @@
-aws ecs update-service --cluster nocodb-staging --service nocohub-wj1dqpgolupckbi --force-new-deployment --region=us-east-2
+#!/bin/bash
+# caution: This script is for staging. 
+# forces ecs to relaunch the tasks/instances 
+# when relaunched a new docker image will be pulled
+# resulting in rolling out a software/config or just 
+# restart.  
+# 
 
+# TODO: prewarm ASG to have additional instances. update only desired 
 aws ecs update-service --cluster nocodb-staging --service nocohub-noco_to_main --force-new-deployment --region=us-east-2
+aws ecs update-service --cluster nocodb-staging --service nocohub-wj1dqpgolupckbi --force-new-deployment --region=us-east-2

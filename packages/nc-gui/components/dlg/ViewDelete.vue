ee2a12df7cdaf5ad5bdb1333a39e90ef18508a01
Muhammed Mustafa <mustafa@nocodb.com>
2023-07-14 15:21:30 +0530

    feat(nocohub): Fixed view delete modal UI acc to design

--- packages/nc-gui/components/dlg/ViewDelete.vue
+++ packages/nc-gui/components/dlg/ViewDelete.vue
@@ -24,6 +24,8 @@ const props = defineProps<Props>()
 
 const emits = defineEmits<Emits>()
 
+const { view } = props
+
 const { t } = useI18n()
 
 const vModel = useVModel(props, 'modelValue', emits)
@@ -57,23 +59,43 @@ async function onDelete() {
 </script>
 
 <template>
-  <a-modal
-    v-model:visible="vModel"
-    centered
-    :class="{ active: vModel }"
-    :confirm-loading="isLoading"
-    wrap-class-name="nc-modal-view-delete"
-  >
-    <template #title> {{ $t('general.delete') }} {{ $t('objects.view') }} </template>
+  <GeneralModal v-model:visible="vModel" centered :class="{ active: vModel }" :confirm-loading="isLoading">
+    <div class="flex flex-col p-6">
+      <div class="flex flex-row pb-2 mb-4 font-medium text-lg border-b-1 border-gray-50 text-gray-800">
+        {{ $t('general.delete') }} {{ $t('objects.view') }}
+      </div>
 
-    {{ $t('msg.info.deleteViewConfirmation') }}
+      <div class="mb-3 text-gray-800">Are you sure you want to delete the following view?</div>
 
-    <template #footer>
-      <a-button key="back" class="!rounded-md" @click="vModel = false">{{ $t('general.cancel') }}</a-button>
+      <div class="flex flex-row items-center py-2 px-3 bg-gray-50 rounded-lg text-gray-700 mb-4">
+        <GeneralViewIcon :meta="props.view" class="nc-view-icon"></GeneralViewIcon>
+        <div
+          class="capitalize text-ellipsis overflow-hidden select-none w-full pl-3"
+          :style="{ wordBreak: 'keep-all', whiteSpace: 'nowrap', display: 'inline' }"
+        >
+          {{ view.title }}
+        </div>
+      </div>
 
-      <a-button key="submit" class="!rounded-md" danger html-type="submit" :loading="isLoading" @click="onDelete">
-        {{ $t('general.submit') }}
-      </a-button>
-    </template>
-  </a-modal>
+      <div class="flex flex-row items-center py-2 px-3 border-1 border-gray-100 rounded-lg text-gray-700">
+        <GeneralIcon icon="warning" class="text-orange-500"></GeneralIcon>
+        <div class="pl-2">This action cannot be undone</div>
+      </div>
+
+      <div class="flex flex-row gap-x-2 mt-2.5 pt-2.5 border-t-1 border-gray-50 justify-end">
+        <a-button key="back" class="!rounded-md !font-medium" @click="vModel = false">{{ $t('general.cancel') }}</a-button>
+
+        <a-button
+          key="submit"
+          class="!rounded-md !font-medium"
+          type="danger"
+          html-type="submit"
+          :loading="isLoading"
+          @click="onDelete"
+        >
+          {{ $t('general.submit') }}
+        </a-button>
+      </div>
+    </div>
+  </GeneralModal>
 </template>

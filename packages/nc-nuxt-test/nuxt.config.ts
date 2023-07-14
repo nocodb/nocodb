27a00f2a380b4033f45618e8940b328ab701997b
Pranav C <pranavxc@gmail.com>
2023-07-14 17:37:56 +0530

    chore: remove sample project

--- packages/nc-nuxt-test/nuxt.config.ts
+++
@@ -1,4 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
--- packages/nc-nuxt-test/utils/parsers/CSVImporter.ts
+++
@@ -1,5 +0,0 @@
-export default class CSVImporter{
-  constructor() {
-    console.log('CSVImporter constructor')
-  }
-}

b09beb51d0341e292b3abfd99c11b7fddece3bce
Pranav C <pranavxc@gmail.com>
2023-07-18 13:35:21 +0530

    chore: cleanup

--- packages/nocodb/src/meta/migrations/v2/nc_011.ts
+++ packages/nocodb/src/meta/migrations/v2/nc_011.ts
@@ -1,17 +1,5 @@
 import { MetaTable, orderedMetaTables } from '../../meta.service';
 
-// CREATE TABLE IF NOT EXISTS nc_notification (
-//           id String(20) NOT NULL,
-//           body Text,
-//           type String,
-//           is_read UInt8 DEFAULT 0,
-//           is_deleted UInt8 DEFAULT 0,
-//           fk_user_id String(20),
-//           created_at DateTime DEFAULT now(),
-//           PRIMARY KEY (id)
-//       ) ENGINE = ReplacingMergeTree
-//       ORDER BY id;
-
 const up = async (knex) => {
   await knex.schema.createTable(MetaTable.PROJECT, (table) => {
     table.string('id', 128).primary();

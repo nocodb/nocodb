1798a4fed282cf66b4536cc84eec57ea104b1883
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-07-20 20:35:13 +0800

    chore: revise local docker bulid script

--- build-local-docker-image.sh
+++ build-local-docker-image.sh
@@ -26,7 +26,7 @@ function remove_image() {
 function build_sdk(){
     # build nocodb-sdk
     cd ${SCRIPT_DIR}/packages/nocodb-sdk
-    pnpm install --prod || ERROR="sdk build failed"
+    pnpm install --no-frozen-lockfile || ERROR="sdk build failed"
     pnpm run build || ERROR="sdk build failed"
 }
 
@@ -35,7 +35,7 @@ function build_gui(){
     export NODE_OPTIONS="--max_old_space_size=16384"
     # generate static build of nc-gui
     cd ${SCRIPT_DIR}/packages/nc-gui
-    pnpm install --prod || ERROR="gui build failed"
+    pnpm install --no-frozen-lockfile || ERROR="gui build failed"
     pnpm run generate || ERROR="gui build failed"
 }
 
@@ -48,7 +48,7 @@ function package_nocodb(){
     #build nocodb
     # build nocodb ( pack nocodb-sdk and nc-gui )
     cd ${SCRIPT_DIR}/packages/nocodb
-    pnpm install --prod || ERROR="package_nocodb failed"
+    pnpm install --no-frozen-lockfile || ERROR="package_nocodb failed"
     EE=true ./node_modules/.bin/webpack --config webpack.local.config.js || ERROR="package_nocodb failed"
 }
 

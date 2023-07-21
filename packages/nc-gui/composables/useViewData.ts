d78b541ed0e55333f4e1b1b1b058c5f274f4b076
Pranav C <pranavxc@gmail.com>
2023-07-21 13:12:31 +0530

    refactor: enable optimised query by default

--- packages/nc-gui/composables/useViewData.ts
+++ packages/nc-gui/composables/useViewData.ts
@@ -36,6 +36,7 @@ import {
   useSharedView,
   useSmartsheetStoreOrThrow,
   useUIPermission,
+  useState
 } from '#imports'
 import type { Row, UndoRedoAction } from '~/lib'
 
@@ -57,7 +58,7 @@ export function useViewData(
 
   const { t } = useI18n()
 
-  const optimisedQuery = ref(false)
+  const optimisedQuery = useState('optimisedQuery', () => true)
 
   const { api, isLoading, error } = useApi()
 

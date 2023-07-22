9d73074e8c04691d0da5229f74cf8fd2fcbb04ff
Pranav C <pranavxc@gmail.com>
2023-07-22 23:04:43 +0530

    fix: on column update clear all single query cache

--- packages/nocodb/src/models/Column.ts
+++ packages/nocodb/src/models/Column.ts
@@ -1127,6 +1127,9 @@ export default class Column<T = any> implements ColumnType {
       colId,
     );
     await this.insertColOption(column, colId, ncMeta);
+
+    // on column update, delete any optimised single query cache
+    await NocoCache.delAll(CacheScope.SINGLE_QUERY, `${oldCol.fk_model_id}:*`);
   }
 
   static async updateAlias(

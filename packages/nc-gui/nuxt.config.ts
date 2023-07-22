a5fd861683087d6d779362151ed800c76c621595
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-07-22 12:26:24 +0800

    fix(nc-gui): move meta and link to head

--- packages/nc-gui/nuxt.config.ts
+++ packages/nc-gui/nuxt.config.ts
@@ -31,6 +31,27 @@ export default defineNuxtConfig({
         },
     /** In production build we need to load assets using relative path, to achieve the result we are using cdnURL */
     cdnURL: process.env.NODE_ENV === 'production' ? '.' : undefined,
+    head: {
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: process.env.npm_package_description || '',
+        },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: './favicon.ico',
+        },
+      ],
+    }
   },
 
   css: [
@@ -45,28 +66,6 @@ export default defineNuxtConfig({
     public: {
       ncBackendUrl: '',
     },
-  },
-
-  meta: {
-    link: [
-      {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: './favicon.ico',
-      },
-    ],
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
-    ],
   },
 
   build: {

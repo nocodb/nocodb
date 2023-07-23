edbc948e7f2439edc52da7e4f07f4a0003dbe711
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-07-23 18:34:45 +0530

    test: class name for view icon
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- packages/nc-gui/components/smartsheet/sidebar/RenameableMenuItem.vue
+++ packages/nc-gui/components/smartsheet/sidebar/RenameableMenuItem.vue
@@ -179,6 +179,7 @@ function onStopEdit() {
     <div v-e="['a:view:open', { view: vModel.type }]" class="text-xs flex items-center w-full gap-1" data-testid="view-item">
       <div class="flex min-w-6" :data-testid="`view-sidebar-drag-handle-${vModel.alias || vModel.title}`">
         <GeneralEmojiPicker
+          class="nc-table-icon"
           :emoji="props.view?.meta?.icon"
           size="small"
           :clearable="true"

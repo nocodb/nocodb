f93e6e3feacf55d38d259dae34b6b9462d7634c0
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-07-26 11:33:09 +0530

    test: kanban menu change fix
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- tests/playwright/pages/Dashboard/common/Toolbar/Fields.ts
+++ tests/playwright/pages/Dashboard/common/Toolbar/Fields.ts
@@ -65,25 +65,25 @@ export class ToolbarFieldsPage extends BasePage {
     await this.toolbar.parent.waitLoading();
   }
 
-  async hideAll({ isLocallySaved }: { isLocallySaved?: boolean } = {}) {
+  async toggleShowAllFields({ isLocallySaved }: { isLocallySaved?: boolean } = {}) {
     await this.toolbar.clickFields();
     await this.waitForResponse({
-      uiAction: () => this.get().locator(`button:has-text("Hide all")`).click(),
+      uiAction: () => this.get().locator(`button.nc-switch`).first().click(),
       requestUrlPathToMatch: isLocallySaved ? '/api/v1/db/public/' : '/api/v1/db/data/noco/',
       httpMethodsToMatch: ['GET'],
     });
     await this.toolbar.clickFields();
   }
 
-  async showAll({ isLocallySaved }: { isLocallySaved?: boolean } = {}) {
-    await this.toolbar.clickFields();
-    await this.waitForResponse({
-      uiAction: () => this.get().locator(`button:has-text("Show all")`).click(),
-      requestUrlPathToMatch: isLocallySaved ? '/api/v1/db/public/' : '/api/v1/db/data/noco/',
-      httpMethodsToMatch: ['GET'],
-    });
-    await this.toolbar.clickFields();
-  }
+  // async showAll({ isLocallySaved }: { isLocallySaved?: boolean } = {}) {
+  //   await this.toolbar.clickFields();
+  //   await this.waitForResponse({
+  //     uiAction: () => this.get().locator(`button.nc-switch`).first().click(),
+  //     requestUrlPathToMatch: isLocallySaved ? '/api/v1/db/public/' : '/api/v1/db/data/noco/',
+  //     httpMethodsToMatch: ['GET'],
+  //   });
+  //   await this.toolbar.clickFields();
+  // }
 
   async toggleShowSystemFields({ isLocallySaved }: { isLocallySaved?: boolean } = {}) {
     await this.toolbar.clickFields();

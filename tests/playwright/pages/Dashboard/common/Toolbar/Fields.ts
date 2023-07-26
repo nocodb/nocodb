23f7a2fa6decb55244b3570b24bb8cb53418d2bd
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-07-26 11:11:00 +0530

    test: fields menu re-org
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- tests/playwright/pages/Dashboard/common/Toolbar/Fields.ts
+++ tests/playwright/pages/Dashboard/common/Toolbar/Fields.ts
@@ -30,7 +30,7 @@ export class ToolbarFieldsPage extends BasePage {
     await this.rootPage.waitForTimeout(100);
 
     const toggleColumn = () =>
-      this.get().locator(`[data-testid="nc-fields-menu-${title}"]`).locator('input[type="checkbox"]').click();
+      this.get().locator(`[data-testid="nc-fields-menu-${title}"]`).locator('.nc-switch').click();
 
     if (validateResponse) {
       await this.waitForResponse({
@@ -47,7 +47,7 @@ export class ToolbarFieldsPage extends BasePage {
   }
 
   async verify({ title, checked }: { title: string; checked?: boolean }) {
-    const checkbox = this.get().locator(`[data-testid="nc-fields-menu-${title}"]`).locator('input[type="checkbox"]');
+    const checkbox = this.get().locator(`[data-testid="nc-fields-menu-${title}"]`).locator('.nc-switch');
 
     if (checked) {
       await expect(checkbox).toBeChecked();
@@ -58,8 +58,7 @@ export class ToolbarFieldsPage extends BasePage {
 
   async click({ title, isLocallySaved }: { title: string; isLocallySaved?: boolean }) {
     await this.waitForResponse({
-      uiAction: () =>
-        this.get().locator(`[data-testid="nc-fields-menu-${title}"]`).locator('input[type="checkbox"]').click(),
+      uiAction: () => this.get().locator(`[data-testid="nc-fields-menu-${title}"]`).locator('.nc-switch').click(),
       requestUrlPathToMatch: isLocallySaved ? '/api/v1/db/public/' : '/api/v1/db/data/noco/',
       httpMethodsToMatch: ['GET'],
     });

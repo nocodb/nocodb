32632e3c10be0e3ba73de0b8ace27bfc31530135
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-07-28 18:46:19 +0530

    Fixed phonenumber regex

--- packages/nc-gui/utils/parsers/CSVTemplateAdapter.ts
+++ packages/nc-gui/utils/parsers/CSVTemplateAdapter.ts
@@ -83,7 +83,7 @@ export default class CSVTemplateAdapter {
   detectInitialUidt(v: string) {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
     const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/
-    const phoneRegex = /\b(?:\+?\d{1,3}(?:[\s-]+\(\d+\))?)?[\s-]*(?:\d[\s-]*){6,14}\b/
+    const phoneRegex = /\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/
     if (phoneRegex.test(v)) return UITypes.PhoneNumber
     if (emailRegex.test(v)) return UITypes.Email
     if (urlRegex.test(v)) return UITypes.URL

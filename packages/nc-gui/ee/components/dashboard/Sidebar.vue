9a8d1e39b4c403cdbf77f99e75f245a524febbc8
Pranav C <pranavxc@gmail.com>
2023-08-03 00:34:36 +0530

    chore: remove command+k from ce

--- packages/nc-gui/ee/components/dashboard/Sidebar.vue
+++ packages/nc-gui/ee/components/dashboard/Sidebar.vue
@@ -8,7 +8,7 @@ const route = $(router.currentRoute)
 
 const workspaceStore = useWorkspace()
 
-const { activeWorkspace } = storeToRefs(workspaceStore)
+const { activeWorkspace, isWorkspaceLoading } = storeToRefs(workspaceStore)
 
 const { isUIAllowed } = useUIPermission()
 
@@ -58,9 +58,9 @@ const navigateToHome = () => {
       outlineWidth: '1px',
     }"
   >
-    <div style="height: var(--sidebar-top-height)">
+    <div style="min-height: var(--sidebar-top-height)">
       <div style="border-bottom-width: 1px" class="flex items-center px-1 nc-sidebar-header !border-0 py-1.25 pl-2">
-        <div class="flex flex-row flex-grow hover:bg-gray-100 pl-2 pr-1 py-0.5 rounded-md max-w-full">
+        <div v-if="!isWorkspaceLoading" class="flex flex-row flex-grow hover:bg-gray-200 pl-2 pr-1 py-0.5 rounded-md max-w-full">
           <a
             v-if="isSharedBase"
             class="w-[40px] min-w-[40px] transition-all duration-200 p-1 cursor-pointer transform hover:scale-105"
@@ -71,7 +71,7 @@ const navigateToHome = () => {
               <template #title>
                 {{ currentVersion }}
               </template>
-              <img width="25" alt="NocoDB" src="~/assets/img/icons/512x512-trans.png" />
+              <img width="25" alt="NocoDB" src="~/assets/img/icons/256x256-trans.png" />
             </a-tooltip>
           </a>
 
@@ -83,63 +83,89 @@ const navigateToHome = () => {
                 data-testid="nc-noco-brand-icon"
                 class="w-[29px] min-w-[29px] nc-noco-brand-icon"
               >
-                <img width="25" class="mr-0" alt="NocoDB" src="~/assets/img/icons/512x512.png" />
+                <img width="25" class="mr-0" alt="NocoDB" src="~/assets/img/icons/256x256.png" />
               </div>
             </template>
           </WorkspaceMenu>
+        </div>
+        <div v-else class="flex flex-row items-center w-full mt-2 ml-2.5 gap-x-3">
+          <a-skeleton-input :active="true" class="!w-6 !h-6 !rounded overflow-hidden" />
+          <a-skeleton-input :active="true" class="!w-40 !h-4 !rounded overflow-hidden" />
         </div>
       </div>
 
       <div class="w-full mt-2"></div>
 
-      <div role="button" class="nc-sidebar-top-button" data-testid="nc-sidebar-home-btn" @click="navigateToHome">
-        <MaterialSymbolsHomeOutlineRounded class="!h-3.9" />
-        <div>Home</div>
-      </div>
-      <div role="button" class="nc-sidebar-top-button" data-testid="nc-sidebar-search-btn" @click="commandPalette?.open()">
-        <MaterialSymbolsSearch class="!h-3.9" />
-        <div class="flex items-center gap-2">
-          Search
-          <div
-            class="inline-flex gap-1 justify-center text-xs px-[8px] py-[1px] uppercase border-1 border-solid rounded-md bg-slate-150 text-gray-500"
-          >
-            <kbd class="text-[16px] mt-[0.5px]">⌘</kbd>
-            <kbd>K</kbd>
+      <template v-if="isWorkspaceLoading">
+        <div class="flex flex-col w-full gap-y-3.25 ml-5.5 mt-5.75">
+          <div class="flex flex-row items-center w-full gap-x-3">
+            <a-skeleton-input :active="true" class="!w-4 !h-4 !rounded overflow-hidden" />
+            <a-skeleton-input :active="true" class="!w-40 !h-4 !rounded overflow-hidden" />
+          </div>
+          <div class="flex flex-row items-center w-full gap-x-3">
+            <a-skeleton-input :active="true" class="!w-4 !h-4 !rounded overflow-hidden" />
+            <a-skeleton-input :active="true" class="!w-40 !h-4 !rounded overflow-hidden" />
+          </div>
+          <div class="flex flex-row items-center w-full gap-x-3">
+            <a-skeleton-input :active="true" class="!w-4 !h-4 !rounded overflow-hidden" />
+            <a-skeleton-input :active="true" class="!w-40 !h-4 !rounded overflow-hidden" />
           </div>
         </div>
-      </div>
-      <WorkspaceCreateProjectBtn
-        v-if="isUIAllowed('createProject', false, activeWorkspace?.roles)"
-        v-model:is-open="isCreateProjectOpen"
-        modal
-        type="text"
-        class="!p-0 mx-1"
-        data-testid="nc-sidebar-create-project-btn"
-        :active-workspace-id="route.params.workspaceId"
-      >
-        <div
-          class="gap-x-2 flex flex-row w-full items-center nc-sidebar-top-button !my-0 !ml-0"
-          :class="{
-            'bg-gray-100': isCreateProjectOpen,
-          }"
-        >
-          <MdiPlus class="!h-4" />
-
-          <div class="flex">New Project</div>
+        <div class="mt-6 ml-4.5">
+          <a-skeleton-input :active="true" class="!w-40 !h-4 !rounded overflow-hidden" />
         </div>
-      </WorkspaceCreateProjectBtn>
-      <div v-else class="!h-7"></div>
-      <div class="text-gray-500 mx-5 font-medium mt-3 mb-1.5">Projects</div>
-      <div
-        class="w-full border-b-1"
-        :class="{
-          'border-gray-100': !isTreeViewOnScrollTop,
-          'border-transparent': isTreeViewOnScrollTop,
-        }"
-      ></div>
+      </template>
+      <template v-else>
+        <div role="button" class="nc-sidebar-top-button" data-testid="nc-sidebar-home-btn" @click="navigateToHome">
+          <MaterialSymbolsHomeOutlineRounded class="!h-3.9" />
+          <div>Home</div>
+        </div>
+        <div role="button" class="nc-sidebar-top-button" data-testid="nc-sidebar-search-btn" @click="commandPalette?.open()">
+          <MaterialSymbolsSearch class="!h-3.9" />
+          <div class="flex items-center gap-2">
+            Search
+            <div
+              class="inline-flex gap-1 justify-center text-xs px-[8px] py-[1px] uppercase border-1 border-gray-300 rounded-md bg-slate-150 text-gray-500"
+            >
+              <kbd class="text-[16px] mt-[0.5px]">⌘</kbd>
+              <kbd>K</kbd>
+            </div>
+          </div>
+        </div>
+        <WorkspaceCreateProjectBtn
+          v-if="isUIAllowed('createProject', false, activeWorkspace?.roles)"
+          v-model:is-open="isCreateProjectOpen"
+          modal
+          type="text"
+          class="!p-0 mx-1"
+          data-testid="nc-sidebar-create-project-btn"
+          :active-workspace-id="route.params.workspaceId"
+        >
+          <div
+            class="gap-x-2 flex flex-row w-full items-center nc-sidebar-top-button !my-0 !ml-0"
+            :class="{
+              'bg-gray-100': isCreateProjectOpen,
+            }"
+          >
+            <MdiPlus class="!h-4" />
+
+            <div class="flex">New Project</div>
+          </div>
+        </WorkspaceCreateProjectBtn>
+        <div v-else class="!h-7"></div>
+        <div class="text-gray-500 mx-5 font-medium mt-3 mb-1.5">Projects</div>
+        <div
+          class="w-full border-b-1"
+          :class="{
+            'border-gray-200': !isTreeViewOnScrollTop,
+            'border-transparent': isTreeViewOnScrollTop,
+          }"
+        ></div>
+      </template>
     </div>
     <LazyDashboardTreeViewNew
-      @create-base-dlg="toggleDialog(true, 'dataSources', null, projectId)"
+      v-if="!isWorkspaceLoading"
+      @create-base-dlg="toggleDialog(true, 'dataSources', undefined, projectId)"
       @on-scroll-top="onTreeViewScrollTop"
     />
   </div>
@@ -147,6 +173,6 @@ const navigateToHome = () => {
 
 <style lang="scss" scoped>
 .nc-sidebar-top-button {
-  @apply flex flex-row mx-1 px-3.5 rounded-md items-center py-0.75 my-0.5 gap-x-2 hover:bg-gray-100 cursor-pointer;
+  @apply flex flex-row mx-1 px-3.5 rounded-md items-center py-0.75 my-0.5 gap-x-2 hover:bg-gray-200 cursor-pointer;
 }
 </style>

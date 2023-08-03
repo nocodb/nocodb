4e11390164894223c2107d5953f4da8953138b05
Muhammed Mustafa <mustafa@nocodb.com>
2023-08-03 18:19:44 +0530

    fix(nocohub): Minor UI fixes

--- packages/nc-gui/components/smartsheet/Pagination.vue
+++ packages/nc-gui/components/smartsheet/Pagination.vue
@@ -57,7 +57,7 @@ const page = computed({
       <div
         class="nc-sidebar-left-toggle-icon hover:after:(bg-primary bg-opacity-75) hover:(bg-gray-100 border-gray-200) border-gray-200 group flex items-center justify-center rounded-md h-full px-1.75 h-7 cursor-pointer text-gray-500 hover:text-gray-700"
         :class="{
-          'bg-gray-50': !isLeftSidebarOpen,
+          'bg-gray-100': !isLeftSidebarOpen,
         }"
         @click="isLeftSidebarOpen = !isLeftSidebarOpen"
       >
@@ -114,7 +114,7 @@ const page = computed({
       <div
         class="flex flex-row items-center justify-center !rounded-md !p-1.75 border-gray-100 cursor-pointer bg-white hover:bg-gray-100 text-gray-500 hover:text-gray-700 nc-sidebar-right-toggle-icon"
         :class="{
-          'bg-gray-100': !isRightSidebarOpen,
+          '!bg-gray-100': !isRightSidebarOpen,
         }"
         type="ghost"
         @click="isRightSidebarOpen = !isRightSidebarOpen"

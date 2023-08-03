bfa19f4fbabba7ebc29baa41be3ef8040c6e3ddd
Pranav C <pranavxc@gmail.com>
2023-08-03 11:52:46 +0530

    refactor: move cowriter/docs components to ee

--- packages/nc-gui/core/pages/ws/[workspaceId]/[[projectType]]/[projectId]/index/index/doc/index/[pageId].vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const { project } = storeToRefs(useProject())
-
-const { openedPageId } = storeToRefs(useDocStore())
-</script>
-
-<template>
-  <LazyDocsPageView v-if="project?.id && openedPageId" />
-</template>

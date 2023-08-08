6799a07e41079aa88bf32f73e3543e011e79f129
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-08-08 18:57:32 +0530

    rebase develop
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- packages/nc-gui/ee/store/share.ts
+++ packages/nc-gui/ee/store/share.ts
@@ -6,6 +6,7 @@ export const useShare = defineStore('share', () => {
   const visibility = ref<'public' | 'private' | 'none' | 'hidden'>('none')
   const { project } = toRefs(useProject())
   const { openedPage, isEditAllowed } = toRefs(useDocStore())
+  const viewsStore = useViewsStore()
 
   const isProjectPublic = computed(() => {
     if (typeof project.value?.meta === 'string') {
@@ -32,9 +33,35 @@ export const useShare = defineStore('share', () => {
 
   const invitationUsersData = ref<Users>({ emails: undefined, role: ProjectRole.Viewer, invitationToken: undefined })
 
+  // watch(
+  //   [openedPage, isEditAllowed, isProjectPublic],
+  //   () => {
+  //     if (!isEditAllowed.value) {
+  //       visibility.value = 'hidden'
+  //       return
+  //     }
+  //
+  //     visibility.value = openedPage.value?.is_published || isProjectPublic.value ? 'public' : 'private'
+  //   },
+  //   { immediate: true, deep: true },
+  // )
+
+  watch(
+    () => viewsStore.activeView?.uuid,
+    (uuid) => {
+      if (project.value?.type !== 'database') return
+
+      visibility.value = uuid ? 'public' : 'private'
+    },
+    {
+      immediate: true,
+    },
+  )
+
   watch(
     [openedPage, isEditAllowed, isProjectPublic],
     () => {
+      if (project.value?.type !== 'documentation') return
       if (!isEditAllowed.value) {
         visibility.value = 'hidden'
         return

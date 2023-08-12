d2153b7ab945088720154d273907d0c523ca6a68
starbirdtech383 <starbirdtech@gmail.com>
2023-08-12 23:42:17 +0530

    configure pw tests to run on selfhosted github runners

--- packages/nocodb/src/controllers/test/TestResetService/resetMysqlSakilaProject.ts
+++ packages/nocodb/src/controllers/test/TestResetService/resetMysqlSakilaProject.ts
@@ -13,7 +13,7 @@ const config = {
     port: 3306,
     user: 'root',
     password: 'password',
-    database: 'sakila',
+    database: 'mysql',
     multipleStatements: true,
     dateStrings: true,
   },

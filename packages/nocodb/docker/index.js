a0003e975a2d6a70608444bf6a7f6e4eecb9f9a1
Harry Dao <khoa2031990@gmail.com>
2023-08-19 17:58:49 +0700

    be/aa-12752. Add script to run migration for docker

--- packages/nocodb/docker/index.js
+++ packages/nocodb/docker/index.js
@@ -1,16 +1,29 @@
-const Noco = require("../build/main/lib/Noco").default;
+const Noco = require('../build/main/lib/Noco').default;
+const {
+  createDatabaseConnection,
+  runMigrations,
+} = require('../build/main/databaseConnection');
+const { setupReusablesAndRoutes } = require('../build/main/reusables');
 const express = require('express');
 const cors = require('cors');
+const { getConnectionManager } = require('typeorm');
 const server = express();
 server.enable('trust proxy');
 server.use(cors());
 
-
 server.set('view engine', 'ejs');
 
-
 (async () => {
+  let connection;
+  if (!getConnectionManager().has('default')) {
+    connection = await createDatabaseConnection();
+  } else {
+    connection = getConnectionManager().get('default');
+  }
+  await runMigrations(connection);
+  await setupReusablesAndRoutes(server, connection);
+
   const httpServer = server.listen(process.env.PORT || 8080, async () => {
     server.use(await Noco.init({}, httpServer, server));
-  })
-})().catch(e => console.log(e))
+  });
+})().catch((e) => console.log(e));

be11190abb2d6007e9fd4fca36d7e649b800494e
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-08-19 16:20:45 +0530

    feat: groupby API Test

---
+++ packages/nocodb/tests/unit/rest/tests/groupby.test.ts
@@ -0,0 +1,87 @@
+import request from 'supertest';
+import { createProject, createSakilaProject } from '../../factory/project';
+import { getTable } from '../../factory/table';
+import { getView } from '../../factory/view';
+import init from '../../init';
+import type { Column, Model, Project, View } from '../../../../src/models';
+
+function groupByTests() {
+  let context;
+  let project: Project;
+  let sakilaProject: Project;
+  let filmTable: Model;
+  let filmColumns: Array<Column>;
+  let filmView: View;
+
+  before(async function () {
+    console.time('GroupBy Tests');
+    context = await init();
+
+    sakilaProject = await createSakilaProject(context);
+    project = await createProject(context);
+
+    filmTable = await getTable({
+      project: sakilaProject,
+      name: 'film',
+    });
+    filmView = await getView(context, { table: filmTable, name: 'Film' });
+    filmColumns = await filmTable.getColumns();
+  });
+
+  it('Check One GroupBy Column', async function () {
+    const lengthColumn = filmColumns.find((c) => c.column_name === 'length');
+    const response = await request(context.app)
+      .get(`/api/v1/db/data/noco/${sakilaProject.id}/${filmTable.id}/groupby`)
+      .set('xc-auth', context.token)
+      .query({
+        column_name: lengthColumn.column_name,
+      })
+      .expect(200);
+    if (
+      response.body.list[4]['length'] !== '68' &&
+      parseInt(response.body.list[4]['count']) !== 5
+    )
+      throw new Error('Invalid GroupBy');
+  });
+
+  it.only('Set GroupBy and Verify', async () => {
+    const lengthColumn = filmColumns.find((c) => c.column_name === 'length');
+    const rentalColumn = filmColumns.find(
+      (c) => c.column_name === 'rental_duration',
+    );
+
+    // Group By Length Column Ascending Order
+    await request(context.app)
+      .patch(`/api/v1/db/meta/grid-columns/${lengthColumn.id}`)
+      .set('xc-auth', context.token)
+      .send({
+        group_by: true,
+        group_by_sort: 'asc',
+        group_by_order: 1,
+      })
+      .expect(200);
+    // Group By RentalDuration Column Descending
+    await request(context.app)
+      .patch(`/api/v1/db/meta/grid-columns/${rentalColumn.id}`)
+      .set('xc-auth', context.token)
+      .send({
+        group_by: true,
+        group_by_sort: 'desc',
+        group_by_order: 2,
+      })
+      .expect(200);
+
+    const columns = (
+      await request(context.app)
+        .get(`/api/v1/db/meta/views/${filmView.id}/columns`)
+        .set('xc-auth', context.token)
+        .expect(200)
+    ).body.list;
+
+    //console.log(columns);
+  });
+}
+
+export default function () {
+  describe('GroupBy (Static)', groupByTests);
+}

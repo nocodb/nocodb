74127fdcc82d02608a541a4827f986f31e2d5759
Pranav C <pranavxc@gmail.com>
2023-08-21 18:01:42 +0530

    fix: handle if project not found

--- packages/nocodb/src/strategies/base-view.strategy/base-view.strategy.ts
+++ packages/nocodb/src/strategies/base-view.strategy/base-view.strategy.ts
@@ -16,7 +16,7 @@ export class BaseViewStrategy extends PassportStrategy(Strategy, 'base-view') {
         );
 
         // validate project id
-        if (req.ncProjectId !== sharedProject.id) {
+        if (!sharedProject || req.ncProjectId !== sharedProject.id) {
           return callback(new UnauthorizedException());
         }
 

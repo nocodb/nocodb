c67c9245de9afb5bf976856a3dbe6023fdd117bc
Harry Dao <khoa2031990@gmail.com>
2023-08-28 16:08:36 +0700

    be/aa-12752. QA Fix

--- packages/nocodb/docker/index.js
+++ packages/nocodb/docker/index.js
@@ -1,12 +1,9 @@
 const Noco = require('../build/main/lib/Noco').default;
-const {
-  createDatabaseConnection,
-  runMigrations,
-} = require('../build/main/databaseConnection');
-const { setupReusablesAndRoutes } = require('../build/main/reusables');
+const databaseConnection = require('../build/main/databaseConnection');
+const reusables = require('../build/main/reusables');
 const express = require('express');
 const cors = require('cors');
-const { getConnectionManager } = require('typeorm');
+const typeorm = require('typeorm');
 const server = express();
 server.enable('trust proxy');
 server.use(cors());
@@ -15,13 +12,13 @@ server.set('view engine', 'ejs');
 
 (async () => {
   let connection;
-  if (!getConnectionManager().has('default')) {
-    connection = await createDatabaseConnection();
+  if (!typeorm.getConnectionManager().has('default')) {
+    connection = await databaseConnection.createDatabaseConnection();
   } else {
-    connection = getConnectionManager().get('default');
+    connection = typeorm.getConnectionManager().get('default');
   }
-  await runMigrations(connection);
-  await setupReusablesAndRoutes(server, connection);
+  await databaseConnection.runMigrations(connection);
+  await reusables.setupReusablesAndRoutes(server, connection);
 
   const httpServer = server.listen(process.env.PORT || 8080, async () => {
     server.use(await Noco.init({}, httpServer, server));

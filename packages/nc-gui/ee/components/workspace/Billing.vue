4d2f60f344ce0602e7e78d3a48e25b591ba3cfb7
Pranav C <pranavxc@gmail.com>
2023-08-30 12:50:16 +0530

    fix: show spinner while upgrading workspace

--- packages/nc-gui/ee/components/workspace/Billing.vue
+++ packages/nc-gui/ee/components/workspace/Billing.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
-import { WorkspacePlan } from 'nocodb-sdk'
+import { WorkspacePlan, WorkspaceStatus } from 'nocodb-sdk'
 import { storeToRefs } from 'pinia'
+import { onMounted, onUnmounted } from '@vue/runtime-core'
 import { extractSdkResponseErrorMsg } from '#imports'
 
 const workspaceStore = useWorkspace()
-const { upgradeActiveWorkspace } = workspaceStore
+const { upgradeActiveWorkspace, loadWorkspaces } = workspaceStore
 const { activeWorkspace } = storeToRefs(workspaceStore)
 const isUpgrading = ref(false)
 
@@ -12,12 +13,38 @@ const upgradeWorkspace = async () => {
   isUpgrading.value = true
   try {
     await upgradeActiveWorkspace()
+    loadWorkspacesWithInterval()
   } catch (e: any) {
     message.error(await extractSdkResponseErrorMsg(e))
   } finally {
     isUpgrading.value = false
   }
 }
+
+let timerRef: any
+
+onUnmounted(() => {
+  if (timerRef) clearTimeout(timerRef)
+})
+
+// todo: do it in a better way
+function loadWorkspacesWithInterval() {
+  // keep checking for workspace status every 10 seconds if workspace is upgrading
+  timerRef = setTimeout(async () => {
+    if (
+      activeWorkspace.value &&
+      activeWorkspace.value.plan === WorkspacePlan.FREE &&
+      activeWorkspace.value.status !== WorkspaceStatus.CREATED
+    ) {
+      await loadWorkspaces()
+      loadWorkspacesWithInterval()
+    }
+  }, 10000)
+}
+
+onMounted(async () => {
+  loadWorkspacesWithInterval()
+})
 </script>
 
 <template>
@@ -30,6 +57,13 @@ const upgradeWorkspace = async () => {
           >Upgrade
         </a-button>
       </template>
+      <div
+        v-if="activeWorkspace && activeWorkspace.status !== WorkspaceStatus.CREATED"
+        class="h-full w-full flex flex-col gap-3 items-center justify-center"
+      >
+        <a-spin size="large" />
+        <div class="text-gray-300 text-lg">Please wait while we set up your workspace</div>
+      </div>
       <template v-else>
         <div class="flex text-xl font-medium">Your workspace is upgraded</div>
       </template>

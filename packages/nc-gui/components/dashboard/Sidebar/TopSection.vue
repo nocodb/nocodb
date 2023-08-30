66b32310afbbdbb0af95dd56236dafd56c755ad8
Muhammed Mustafa <mustafa@nocodb.com>
2023-08-30 16:33:47 +0530

    fix(nocohub): Split sidebar topsection so that search component is only in EE

--- packages/nc-gui/components/dashboard/Sidebar/TopSection.vue
+++ packages/nc-gui/components/dashboard/Sidebar/TopSection.vue
@@ -11,8 +11,6 @@ const { navigateToWorkspaceSettings } = workspaceStore
 
 const { isSharedBase } = storeToRefs(projectStore)
 
-const { commandPalette } = useCommandPalette()
-
 const { isUIAllowed } = useUIPermission()
 
 const isCreateProjectOpen = ref(false)
@@ -47,26 +45,7 @@ const navigateToSettings = () => {
   </template>
   <template v-else-if="!isSharedBase">
     <div class="flex flex-col p-1 gap-y-0.5 mt-0.25">
-      <NcButton
-        v-if="appInfo.ee"
-        type="text"
-        size="small"
-        class="nc-sidebar-top-button w-full !hover:bg-gray-200 !rounded-md"
-        data-testid="nc-sidebar-search-btn"
-        :centered="false"
-        @click="commandPalette?.open()"
-      >
-        <div class="flex items-center gap-2">
-          <MaterialSymbolsSearch class="!h-3.9" />
-          Search
-          <div
-            class="inline-flex gap-1 justify-center text-xs px-[8px] py-[1px] uppercase border-1 border-gray-300 rounded-md bg-slate-150 text-gray-500"
-          >
-            <kbd class="text-[16px] mt-[0.5px]">⌘</kbd>
-            <kbd>K</kbd>
-          </div>
-        </div>
-      </NcButton>
+      <DashboardSidebarTopSectionHeader />
 
       <NcButton
         v-if="isWorkspaceOwnerOrCreator"
---
+++ packages/nc-gui/ee/components/dashboard/Sidebar/TopSection/Header.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const { commandPalette } = useCommandPalette()
+</script>
+
+<template>
+  <NcButton
+    type="text"
+    size="small"
+    class="nc-sidebar-top-button w-full !hover:bg-gray-200 !rounded-md"
+    data-testid="nc-sidebar-search-btn"
+    :centered="false"
+    @click="commandPalette?.open()"
+  >
+    <div class="flex items-center gap-2">
+      <MaterialSymbolsSearch class="!h-3.9" />
+      Search
+      <div
+        class="inline-flex gap-1 justify-center text-xs px-[8px] py-[1px] uppercase border-1 border-gray-300 rounded-md bg-slate-150 text-gray-500"
+      >
+        <kbd class="text-[16px] mt-[0.5px]">⌘</kbd>
+        <kbd>K</kbd>
+      </div>
+    </div>
+  </NcButton>
+</template>

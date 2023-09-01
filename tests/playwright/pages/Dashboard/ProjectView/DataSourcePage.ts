bbd8563b88722d3870c3afdac6c3fab43d212429
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-09-01 09:38:40 +0530

    test: sync from CE
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- tests/playwright/pages/Dashboard/ProjectView/DataSourcePage.ts
+++ tests/playwright/pages/Dashboard/ProjectView/DataSourcePage.ts
@@ -2,17 +2,20 @@ import BasePage from '../../Base';
 import { ProjectViewPage } from './index';
 import { Locator } from '@playwright/test';
 import { MetaDataPage } from './Metadata';
+import { AuditPage } from './Audit';
 
 export class DataSourcePage extends BasePage {
   readonly projectView: ProjectViewPage;
   readonly databaseType: Locator;
   readonly metaData: MetaDataPage;
+  readonly audit: AuditPage;
 
   constructor(projectView: ProjectViewPage) {
     super(projectView.rootPage);
     this.projectView = projectView;
     this.databaseType = this.get().locator('.nc-extdb-db-type');
     this.metaData = new MetaDataPage(this);
+    this.audit = new AuditPage(this);
   }
 
   get() {
@@ -40,13 +43,18 @@ export class DataSourcePage extends BasePage {
   }
 
   async openERD({ rowIndex }: { rowIndex: number }) {
-    // hardwired
-    // await this.rootPage.locator('button.nc-action-btn:has-text("Relations")').click();
-
     // 0th offset for header
     const row = this.get()
       .locator('.ds-table-row')
       .nth(rowIndex + 1);
     await row.locator('button.nc-action-btn:has-text("Relations")').click();
+  }
+
+  async openAudit({ rowIndex }: { rowIndex: number }) {
+    // 0th offset for header
+    const row = this.get()
+      .locator('.ds-table-row')
+      .nth(rowIndex + 1);
+    await row.locator('button.nc-action-btn:has-text("Audit")').click();
   }
 }

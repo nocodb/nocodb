2660edfb6570613fa6b449e0722517f283f39f51
Muhammed Mustafa <mustafa@nocodb.com>
2023-09-01 18:39:23 +0530

    fix(nocohub): Added loader to data field of select mode

--- packages/nc-gui/components/smartsheet/topbar/SelectMode.vue
+++ packages/nc-gui/components/smartsheet/topbar/SelectMode.vue
@@ -15,7 +15,8 @@ const { onViewsTabChange } = useViewsStore()
       }"
       @click="onViewsTabChange('view')"
     >
-      <GeneralViewIcon class="tab-icon" :meta="{ type: activeView?.type }" ignore-color />
+      <GeneralViewIcon v-if="activeView?.type" class="tab-icon" :meta="{ type: activeView?.type }" ignore-color />
+      <GeneralLoader v-else class="tab-icon" />
       <div class="tab-title nc-tab">Data</div>
     </div>
     <div
@@ -45,6 +46,7 @@ const { onViewsTabChange } = useViewsStore()
 
 .tab-icon {
   font-size: 1.1rem !important;
+  @apply min-w-4.5;
 }
 .tab .tab-title {
   @apply min-w-0;

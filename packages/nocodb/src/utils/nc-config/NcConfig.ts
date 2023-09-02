928f04a2010df322f23a1304f4001372c200817e
Pranav C <pranavxc@gmail.com>
2023-09-02 11:44:09 +0530

    refactor: use same env variables

--- packages/nocodb/src/utils/nc-config/NcConfig.ts
+++ packages/nocodb/src/utils/nc-config/NcConfig.ts
@@ -146,7 +146,7 @@ export class NcConfig {
       port: process.env.NC_PORT,
       tryMode: !!process.env.NC_TRY,
       worker: !!process.env.NC_WORKER,
-      dashboardPath: process.env.NC_DASHBOARD_PATH ?? '/',
+      dashboardPath: process.env.NC_DASHBOARD_URL ?? '/',
       publicUrl: process.env.NC_PUBLIC_URL,
     });
   }

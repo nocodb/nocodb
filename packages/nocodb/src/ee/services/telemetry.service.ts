25768ec178b4d4f2d8c6dd6d63f91fdfdc4d876c
Pranav C <pranavxc@gmail.com>
2023-09-03 23:28:34 +0530

    feat: add posthog client in backend

--- packages/nocodb/src/ee/services/telemetry.service.ts
+++ packages/nocodb/src/ee/services/telemetry.service.ts
@@ -1,16 +1,20 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { packageInfo } from 'nc-help';
+import { PostHog } from 'posthog-node';
 import { Producer } from './producer/producer';
 
 @Injectable()
 export class TelemetryService {
   private logger: Logger = new Logger(TelemetryService.name);
   private defaultPayload: any;
+  private phClient: PostHog;
 
   constructor(@Inject(Producer) private producer: Producer) {
     this.defaultPayload = {
       package_id: packageInfo.version,
     };
+    if (process.env.NC_CLOUD_POSTHOG_API_KEY)
+      this.phClient = new PostHog(process.env.NC_CLOUD_POSTHOG_API_KEY);
   }
 
   public sendEvent({
@@ -49,11 +53,20 @@ export class TelemetryService {
         project_id: event?.pid ?? undefined,
         ip: param.req.clientIp,
         userId: param.req.user?.id,
+        email: param.req.user?.email,
         user_agent: param.req.headers['user-agent'],
         ...this.defaultPayload,
       };
 
       messages.push(JSON.stringify(payload));
+
+      this.phClient?.capture({
+        distinctId: payload.userId,
+        event: payload.evt_type,
+        properties: {
+          ...payload,
+        },
+      });
     }
 
     await this.producer.sendMessages('cloud-telemetry', messages);

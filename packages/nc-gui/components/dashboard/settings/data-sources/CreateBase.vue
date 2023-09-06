9b20269d08857eec30281c111ca4cae8f3affc7d
mertmit <mertmit99@gmail.com>
2023-09-06 16:41:50 +0300

    fix: warnings in ui
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/components/dashboard/settings/data-sources/CreateBase.vue
+++ packages/nc-gui/components/dashboard/settings/data-sources/CreateBase.vue
@@ -350,8 +350,8 @@ onMounted(async () => {
     // todo: replace setTimeout and follow better approach
     setTimeout(() => {
       const input = form.value?.$el?.querySelector('input[type=text]')
-      input.setSelectionRange(0, formState.value.title.length)
-      input.focus()
+      input?.setSelectionRange(0, formState.value.title.length)
+      input?.focus()
     }, 500)
   })
 })

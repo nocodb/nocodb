ff2f61ee1bdf1ffdd5c36a18649742d7dfacb10b
mertmit <mertmit99@gmail.com>
2023-09-20 19:11:42 +0000

    feat: new role badge and selector

---
+++ packages/nc-gui/components/roles/Selector.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import type { RoleLabels } from 'nocodb-sdk'
+import { toRef } from '#imports'
+
+const props = defineProps<{
+  role: keyof typeof RoleLabels
+  roles: (keyof typeof RoleLabels)[]
+  inherit?: string
+  onRoleChange: (role: keyof typeof RoleLabels) => void
+}>()
+
+const roleRef = toRef(props, 'role')
+const inheritRef = toRef(props, 'inherit')
+</script>
+
+<template>
+  <NcDropdown>
+    <RolesBadge class="border-1" :role="roleRef" clickable :inherit="inheritRef === role" />
+    <template #overlay>
+      <div class="flex flex-col gap-[4px] p-1">
+        <div class="flex flex-col gap-[4px]">
+          <NcDropdownItem
+            v-for="rl in props.roles"
+            :key="rl"
+            class="cursor-pointer"
+            :value="rl"
+            :selected="rl === roleRef"
+            @click="props.onRoleChange(rl)"
+          >
+            <RolesBadge class="!bg-white hover:!bg-gray-100" :role="rl" :inherit="inheritRef === rl" />
+          </NcDropdownItem>
+        </div>
+      </div>
+    </template>
+  </NcDropdown>
+</template>
+
+<style scoped lang="scss"></style>

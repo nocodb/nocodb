0fe8d8b97c4266fcc25b0b12c598a6ac269a4d92
Muhammed Mustafa <mustafa@nocodb.com>
2023-09-22 16:30:49 +0530

    fix: View icon fix

--- packages/nc-gui/components/smartsheet/toolbar/ViewInfo.vue
+++ packages/nc-gui/components/smartsheet/toolbar/ViewInfo.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import { ViewTypes } from 'nocodb-sdk'
-import { ActiveViewInj, inject } from '#imports'
 
-const selectedView = inject(ActiveViewInj)
-const { openedViewsTab } = storeToRefs(useViewsStore())
+const { openedViewsTab, activeView } = storeToRefs(useViewsStore())
 
 const { activeTable } = storeToRefs(useTablesStore())
 </script>
@@ -12,19 +10,13 @@ const { activeTable } = storeToRefs(useTablesStore())
   <div
     class="flex flex-row font-medium items-center border-gray-50 mt-0.5"
     :class="{
-      'min-w-2/5 max-w-2/5': selectedView?.type !== ViewTypes.KANBAN,
-      'min-w-1/4 max-w-1/4': selectedView?.type === ViewTypes.KANBAN,
+      'min-w-2/5 max-w-2/5': activeView?.type !== ViewTypes.KANBAN,
+      'min-w-1/4 max-w-1/4': activeView?.type === ViewTypes.KANBAN,
     }"
   >
     <LazyGeneralEmojiPicker :emoji="activeTable?.meta?.icon" readonly size="xsmall">
       <template #default>
-        <MdiTable
-          class="min-w-5 !text-gray-500"
-          :class="{
-            '!text-gray-500': !selectedView?.is_default,
-            '!text-gray-800 mr-0.25': selectedView?.is_default,
-          }"
-        />
+        <MdiTable class="min-w-5 !text-gray-500" />
       </template>
     </LazyGeneralEmojiPicker>
     <span
@@ -35,14 +27,14 @@ const { activeTable } = storeToRefs(useTablesStore())
     </span>
 
     <div class="px-2 text-gray-300">/</div>
-    <LazyGeneralEmojiPicker :emoji="selectedView?.meta?.icon" readonly size="xsmall">
+    <LazyGeneralEmojiPicker :emoji="activeView?.meta?.icon" readonly size="xsmall">
       <template #default>
-        <GeneralViewIcon :meta="{ type: selectedView?.type }" class="min-w-4.5 text-lg flex" />
+        <GeneralViewIcon :meta="{ type: activeView?.type }" class="min-w-4.5 text-lg flex" />
       </template>
     </LazyGeneralEmojiPicker>
 
     <span class="truncate pl-1.25 text-gray-700 max-w-28/100">
-      {{ selectedView?.is_default ? $t('title.defaultView') : selectedView?.title }}
+      {{ activeView?.is_default ? $t('title.defaultView') : activeView?.title }}
     </span>
 
     <LazySmartsheetToolbarReload v-if="openedViewsTab === 'view'" />

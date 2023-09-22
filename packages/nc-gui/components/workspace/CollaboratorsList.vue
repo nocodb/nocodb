95a4d4f4d6d1d89854512fabe9bcc282c228f579
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-09-22 07:57:40 +0000

    fix: added members to the input label

--- packages/nc-gui/components/workspace/CollaboratorsList.vue
+++ packages/nc-gui/components/workspace/CollaboratorsList.vue
@@ -48,7 +48,7 @@ onMounted(async () => {
     <div class="w-full h-1 border-t-1 border-gray-100 opacity-50 mt-6"></div>
     <div class="w-full flex flex-row justify-between items-baseline mt-6.5 mb-2 pr-0.25 ml-2">
       <div class="text-xl">Members</div>
-      <a-input v-model:value="userSearchText" class="!max-w-90 !rounded-md mr-4" placeholder="Search collaborators">
+      <a-input v-model:value="userSearchText" class="!max-w-90 !rounded-md mr-4" placeholder="Search members">
         <template #prefix>
           <PhMagnifyingGlassBold class="!h-3.5 text-gray-500" />
         </template>

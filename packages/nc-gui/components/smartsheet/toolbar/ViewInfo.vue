810e3fc5d185e99b0a18b5c8b05d3a568176b275
Muhammed Mustafa <mustafa@nocodb.com>
2023-09-23 19:17:15 +0530

    feat: Integrated View info with mobile view

--- packages/nc-gui/components/smartsheet/toolbar/ViewInfo.vue
+++ packages/nc-gui/components/smartsheet/toolbar/ViewInfo.vue
@@ -14,7 +14,7 @@ const { activeTable } = storeToRefs(useTablesStore())
     :class="{
       'min-w-2/5 max-w-2/5': !isMobileMode && activeView?.type !== ViewTypes.KANBAN,
       'min-w-1/4 max-w-1/4': !isMobileMode && activeView?.type === ViewTypes.KANBAN,
-      'min-w-2/3 max-w-2/3': isMobileMode,
+      'min-w-1/2 max-w-1/2': isMobileMode,
     }"
   >
     <template v-if="!(isMobileMode && !activeView?.is_default)">

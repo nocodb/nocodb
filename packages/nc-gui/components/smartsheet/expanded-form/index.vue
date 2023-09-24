360b045beb7834c80f8ce292ff7abfb33781926b
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-09-24 17:55:43 +0530

    feat: change record on delete

--- packages/nc-gui/components/smartsheet/expanded-form/index.vue
+++ packages/nc-gui/components/smartsheet/expanded-form/index.vue
@@ -320,8 +320,14 @@ const onDeleteRowClick = () => {
 const onConfirmDeleteRowClick = async () => {
   showDeleteRowModal.value = false
   await deleteRowById(primaryKey.value)
-  await reloadTrigger.trigger()
   message.success('Row deleted')
+  if (!props.lastRow) {
+    await onNext()
+  } else if (!props.firstRow) {
+    emits('prev')
+  } else {
+    onClose()
+  }
 }
 </script>
 

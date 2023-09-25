7a77988b2dd06ea185322474e9c47ed181c4a7b8
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-09-25 15:58:07 +0530

    docs: theme in mdxComponents.js file
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

---
+++ packages/noco-docs/src/theme/MDXComponents.js
@@ -0,0 +1,10 @@
+// import the original mapper
+import MDXComponents from "@theme-original/MDXComponents";
+
+export default {
+  // re-use the default mapping
+  ...MDXComponents,
+  // some custom components goes here ...
+  Tabs,
+  TabItem,
+};

4c0e429b76b64ad86b5071b94daf38ca88cd3e15
Pranav C <pranavxc@gmail.com>
2023-09-27 16:32:54 +0530

    test: playwright test correction
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- tests/playwright/tests/db/general/groupCRUD.spec.ts
+++ tests/playwright/tests/db/general/groupCRUD.spec.ts
@@ -30,7 +30,7 @@ test.describe('GroupBy CRUD Operations', () => {
   let context: any;
 
   test.beforeEach(async ({ page }) => {
-    context = await setup({ page, isEmptyProject: true });
+    context = await setup({ page, isEmptyProject: false });
     dashboard = new DashboardPage(page, context.project);
     toolbar = dashboard.grid.toolbar;
     topbar = dashboard.grid.topbar;
@@ -252,19 +252,17 @@ test.describe('GroupBy CRUD Operations', () => {
   test('Single GroupBy CRUD Operations - Links', async ({ page }) => {
     await dashboard.treeView.openTable({ title: 'Film' });
 
-    await toolbar.sort.add({ title: 'Actors', ascending: true, locallySaved: false });
-
     await toolbar.clickGroupBy();
 
     await toolbar.groupBy.add({ title: 'Actors', ascending: false, locallySaved: false });
 
-    await dashboard.grid.groupPage.openGroup({ indexMap: [0] });
+    await dashboard.grid.groupPage.openGroup({ indexMap: [2] });
 
-    await dashboard.grid.groupPage.addNewRow({
-      indexMap: [0],
-      index: 10,
-      columnHeader: 'Item',
-      value: 'Aaaaaaaaaaaaaaaaaaaa',
+    await dashboard.grid.groupPage.validateFirstRow({
+      indexMap: [2],
+      rowIndex: 0,
+      columnHeader: 'Title',
+      value: 'ARABIA DOGMA',
     });
   });
 });

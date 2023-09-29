57124940be23f23381d26a048d1deb5996f4fef4
Muhammed Mustafa <mustafa@nocodb.com>
2023-09-29 12:52:30 +0530

    fix: i18n for PgBinaryOption

--- packages/nc-gui/components/smartsheet/column/PgBinaryOptions.vue
+++ packages/nc-gui/components/smartsheet/column/PgBinaryOptions.vue
@@ -23,10 +23,14 @@ vModel.value.meta = {
 <template>
   <a-row class="my-2" gutter="8">
     <a-col :span="24">
-      <a-form-item v-bind="validateInfos['meta.format']" label="Binary encoding format">
-        <a-select v-model:value="vModel.meta.format" placeholder="Binary encoding format" class="!w-full nc-link-singular">
-          <a-select-option value="escape">Escape</a-select-option>
-          <a-select-option value="hex">Hex</a-select-option>
+      <a-form-item v-bind="validateInfos['meta.format']" :label="$t('labels.binaryEncodingFormat')">
+        <a-select
+          v-model:value="vModel.meta.format"
+          :placeholder="$t('labels.binaryEncodingFormat')"
+          class="!w-full nc-link-singular"
+        >
+          <a-select-option value="escape">{{ $t('general.escape') }}</a-select-option>
+          <a-select-option value="hex">{{ $t('general.hex') }}</a-select-option>
         </a-select>
       </a-form-item>
     </a-col>

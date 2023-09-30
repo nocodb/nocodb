f5f059a886974ff266b46f1dde5b5de601984291
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-09-30 15:57:38 +0530

    fix: lint

--- packages/nc-gui/components/smartsheet/grid/newFile.ts
+++ packages/nc-gui/components/smartsheet/grid/newFile.ts
@@ -1,4 +1,4 @@
-import { JsonExpandInj, provide } from '#imports';
-import { isJsonExpand } from './Table.vue';
+import { isJsonExpand } from './Table.vue'
+import { JsonExpandInj, provide } from '#imports'
 
-provide(JsonExpandInj, isJsonExpand);
+provide(JsonExpandInj, isJsonExpand)

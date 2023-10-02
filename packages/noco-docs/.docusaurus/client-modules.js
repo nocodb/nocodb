2b3d15cac09794a42150c5e01673156717b6f677
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-10-02 18:44:34 +0530

    docs: add .docusaurus folder
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

---
+++ packages/noco-docs/.docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require('/Users/rajuudava/NocoDB/nocodb/packages/noco-docs/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/rajuudava/NocoDB/nocodb/packages/noco-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/rajuudava/NocoDB/nocodb/packages/noco-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/rajuudava/NocoDB/nocodb/packages/noco-docs/src/css/custom.scss'),
+  require('/Users/rajuudava/NocoDB/nocodb/packages/noco-docs/node_modules/plugin-image-zoom/src/zoom'),
+  require('/Users/rajuudava/NocoDB/nocodb/packages/noco-docs/src/modules/tele.js'),
+];

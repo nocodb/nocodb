524e12d93c4c437e217215556700154c1d7fca28
Raju Udava <86527202+dstala@users.noreply.github.com>
2023-10-03 10:37:39 +0530

    fix: tooltip overlap
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- packages/nc-gui/components/smartsheet/header/VirtualCell.vue
+++ packages/nc-gui/components/smartsheet/header/VirtualCell.vue
@@ -137,7 +137,6 @@ const closeAddColumnDropdown = () => {
       <span
         class="name pl-1"
         :class="{ 'truncate': !isForm || !isExpandedForm, 'whitespace-pre-line': isForm || isExpandedForm }"
-        :title="column.title"
       >
         {{ column.title }}
       </span>

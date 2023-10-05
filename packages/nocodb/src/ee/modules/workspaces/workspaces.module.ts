0aa74c74c48c8a380f05fbcdeb22abaa5b341967
mertmit <mertmit99@gmail.com>
2023-10-05 23:13:11 +0530

    feat: prepopulated workspaces
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/ee/modules/workspaces/workspaces.module.ts
+++ packages/nocodb/src/ee/modules/workspaces/workspaces.module.ts
@@ -3,9 +3,10 @@ import { WorkspacesService } from './workspaces.service';
 import { WorkspacesController } from './workspaces.controller';
 import { MetasModule } from '~/modules/metas/metas.module';
 import { WorkspaceUsersModule } from '~/modules/workspace-users/workspace-users.module';
+import { JobsModule } from '~/modules/jobs/jobs.module';
 
 @Module({
-  imports: [MetasModule, forwardRef(() => WorkspaceUsersModule)],
+  imports: [MetasModule, JobsModule, forwardRef(() => WorkspaceUsersModule)],
   controllers: [WorkspacesController],
   providers: [WorkspacesService],
   exports: [WorkspacesService],

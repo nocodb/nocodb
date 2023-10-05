b4ac172cfd173c1b9c35f98ffcc5f84e4edaa7b9
Pranav C <pranavxc@gmail.com>
2023-10-05 13:53:40 +0530

    refactor: allow only one token per user

--- packages/nocodb/src/ee/services/org-tokens.service.ts
+++ packages/nocodb/src/ee/services/org-tokens.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { ApiTokensService as ApiTokensServiceCE } from 'src/services/api-tokens.service';
+import { OrgTokensService as OrgTokensServiceCE } from 'src/services/org-tokens.service';
+import type { User } from '~/models';
 import type { ApiTokenReqType } from 'nocodb-sdk';
 import { AppHooksService } from '~/services/app-hooks/app-hooks.service';
 import { ApiToken } from '~/models';
 import { NcError } from '~/helpers/catchError';
 
 @Injectable()
-export class ApiTokensService extends ApiTokensServiceCE {
+export class OrgTokensService extends OrgTokensServiceCE {
   constructor(protected readonly appHooksService: AppHooksService) {
     super(appHooksService);
   }
 
-  async apiTokenCreate(param: { userId: string; tokenBody: ApiTokenReqType }) {
-    if (await ApiToken.count({ fk_user_id: param.userId })) {
+  async apiTokenCreate(param: { user: User; apiToken: ApiTokenReqType }) {
+    if (await ApiToken.count({ fk_user_id: param.user.id })) {
       NcError.badRequest('Only one token per user is allowed');
     }
+
     return await super.apiTokenCreate(param);
   }
 }

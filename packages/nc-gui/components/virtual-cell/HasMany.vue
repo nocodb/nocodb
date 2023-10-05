693c389e2f36e0a3dc5c662befcf111c8a5a039b
mertmit <mertmit99@gmail.com>
2023-10-05 04:30:56 +0000

    fix: only render cell modals on demand

--- packages/nc-gui/components/virtual-cell/HasMany.vue
+++ packages/nc-gui/components/virtual-cell/HasMany.vue
@@ -136,9 +136,10 @@ useSelectedCellKeyupListener(inject(ActiveCellInj, ref(false)), (e: KeyboardEven
       />
     </div>
 
-    <LazyVirtualCellComponentsListItems v-model="listItemsDlg" :column="hasManyColumn" />
+    <LazyVirtualCellComponentsListItems v-if="listItemsDlg" v-model="listItemsDlg" :column="hasManyColumn" />
 
     <LazyVirtualCellComponentsListChildItems
+      v-if="childListDlg"
       v-model="childListDlg"
       :cell-value="localCellValue"
       :column="hasManyColumn"

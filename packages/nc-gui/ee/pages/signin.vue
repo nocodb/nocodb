8ef3e037f6c1e0e398b1092811587e481f856c89
Pranav C <pranavxc@gmail.com>
2023-10-06 23:25:15 +0530

    fix: hide logout

--- packages/nc-gui/ee/pages/signin.vue
+++ packages/nc-gui/ee/pages/signin.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import '@aws-amplify/ui-vue/styles.css';
+import '@aws-amplify/ui-vue/styles.css'
 import { Authenticator } from '@aws-amplify/ui-vue'
 import { definePageMeta } from '#imports'
 
@@ -11,15 +11,15 @@ definePageMeta({
 
 <template>
   <div class="py-10 flex justify-center">
-    <authenticator initial-state="signUp" :social-providers="['google']" >
-
-    <template v-slot="{ user, signOut }">
+    <Authenticator initial-state="signUp" :social-providers="['google']">
+      <!--
+      <template v-slot="{ user, signOut }">
         <div>
-        <h1>Hello {{ user.username }}!</h1>
-        <button @click="signOut">Sign Out</button>
+          <h1>Hello {{ user.username }}!</h1>
+          <button @click="signOut">Sign Out</button>
         </div>
-      </template>
-    </authenticator>
+      </template> -->
+    </Authenticator>
   </div>
 </template>
 

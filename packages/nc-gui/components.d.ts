a4314f513f6e59a3368e51afb34c959229111e4e
Pranav C <pranavxc@gmail.com>
2023-10-11 17:15:05 +0530

    chore: component.d.ts
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui/components.d.ts
+++ packages/nc-gui/components.d.ts
@@ -150,7 +150,6 @@ declare module '@vue/runtime-core' {
     MdiReload: typeof import('~icons/mdi/reload')['default']
     MdiRocketLaunchOutline: typeof import('~icons/mdi/rocket-launch-outline')['default']
     MdiScriptTextOutline: typeof import('~icons/mdi/script-text-outline')['default']
-    MdiShieldKeyOutline: typeof import('~icons/mdi/shield-key-outline')['default']
     MdiSlack: typeof import('~icons/mdi/slack')['default']
     MdiStar: typeof import('~icons/mdi/star')['default']
     MdiStarOutline: typeof import('~icons/mdi/star-outline')['default']

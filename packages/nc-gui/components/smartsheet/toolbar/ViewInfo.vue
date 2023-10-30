1e3f5811d66bf7cf79075e79ae887fd931f0c8bd
Muhammed Mustafa <mustafa@nocodb.com>
2023-10-30 05:26:27 +0000

    fix: Made project breadcrumb clickable and table node clickable only on default view case

--- packages/nc-gui/components/smartsheet/toolbar/ViewInfo.vue
+++ packages/nc-gui/components/smartsheet/toolbar/ViewInfo.vue
@@ -4,11 +4,21 @@ const { isMobileMode } = useGlobal()
 const { openedViewsTab, activeView } = storeToRefs(useViewsStore())
 
 const { base, isSharedBase } = storeToRefs(useBase())
+const { baseUrl } = useBase()
 
 const { activeTable } = storeToRefs(useTablesStore())
 const { tableUrl } = useTablesStore()
 
 const { isLeftSidebarOpen } = storeToRefs(useSidebarStore())
+
+const openedBaseUrl = computed(() => {
+  if (!base.value) return ''
+
+  return `${window.location.origin}/#${baseUrl({
+    id: base.value.id!,
+    type: 'database',
+  })}`
+})
 </script>
 
 <template>
@@ -22,35 +32,40 @@ const { isLeftSidebarOpen } = storeToRefs(useSidebarStore())
     }"
   >
     <template v-if="!isMobileMode">
-      <NcTooltip
-        class="ml-0.75 max-w-1/4 max-w-"
+      <NuxtLink
+        class="!hover:(text-black underline-gray-600) !underline-transparent ml-0.75 max-w-1/4"
         :class="{
           '!max-w-none': isSharedBase && !isMobileMode,
+          '!text-gray-500': activeTable,
+          '!text-gray-700': !activeTable,
         }"
+        :to="openedBaseUrl"
       >
-        <template #title>
-          {{ base?.title }}
-        </template>
-        <div class="flex flex-row items-center gap-x-1.5">
-          <GeneralProjectIcon
-            :meta="{ type: base?.type }"
-            class="!grayscale min-w-4"
-            :style="{
-              filter: 'grayscale(100%) brightness(115%)',
-            }"
-          />
-          <div
-            class="hidden !2xl:(flex truncate ml-1)"
-            :class="{
-              '!flex': isSharedBase && !isMobileMode,
-            }"
-          >
-            <span class="truncate text-gray-700">
-              {{ base?.title }}
-            </span>
+        <NcTooltip class="!text-inherit">
+          <template #title>
+            {{ base?.title }}
+          </template>
+          <div class="flex flex-row items-center gap-x-1.5">
+            <GeneralProjectIcon
+              :meta="{ type: base?.type }"
+              class="!grayscale min-w-4"
+              :style="{
+                filter: 'grayscale(100%) brightness(115%)',
+              }"
+            />
+            <div
+              class="hidden !2xl:(flex truncate ml-1)"
+              :class="{
+                '!flex': isSharedBase && !isMobileMode,
+              }"
+            >
+              <span class="truncate !text-inherit">
+                {{ base?.title }}
+              </span>
+            </div>
           </div>
-        </div>
-      </NcTooltip>
+        </NcTooltip>
+      </NuxtLink>
       <div class="px-1.5 text-gray-500">/</div>
     </template>
     <template v-if="!(isMobileMode && !activeView?.is_default)">
@@ -81,7 +96,7 @@ const { isLeftSidebarOpen } = storeToRefs(useSidebarStore())
             class="text-ellipsis overflow-hidden text-gray-500 xs:ml-2"
             :class="{
               'text-gray-500': !isMobileMode,
-              'text-gray-700 font-medium': isMobileMode || activeView?.is_default,
+              'text-gray-800 font-medium': isMobileMode || activeView?.is_default,
             }"
             :style="{
               wordBreak: 'keep-all',
@@ -90,7 +105,11 @@ const { isLeftSidebarOpen } = storeToRefs(useSidebarStore())
             }"
           >
             <NuxtLink
-              class="!hover:(text-black underline-gray-600) !text-inherit !underline-transparent"
+              class="!text-inherit !underline-transparent"
+              :class="{
+                '!hover:(underline-transparent text-gray-800)': activeView?.is_default,
+                '!hover:(text-black underline-gray-600)': !activeView?.is_default,
+              }"
               :to="tableUrl({ table: activeTable, completeUrl: true })"
             >
               {{ activeTable?.title }}

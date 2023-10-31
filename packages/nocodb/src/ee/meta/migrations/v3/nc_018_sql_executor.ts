bea4dd278aec0e220ca5a62fccda12b0629af92d
mertmit <mertmit99@gmail.com>
2023-10-31 11:36:34 +0300

    feat: priority and capacity for sql executor migration
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/ee/meta/migrations/v3/nc_018_sql_executor.ts
+++ packages/nocodb/src/ee/meta/migrations/v3/nc_018_sql_executor.ts
@@ -5,10 +5,19 @@ import { MetaTable } from '~/utils/globals';
 const nanoidv2 = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 14);
 
 const up = async (knex: Knex) => {
+  if (await knex.schema.hasTable(MetaTable.SQL_EXECUTOR)) {
+    await knex.schema.alterTable(MetaTable.BASES, (table) => {
+      table.dropColumn('fk_sql_executor_id');
+    });
+    await knex.schema.dropTable(MetaTable.SQL_EXECUTOR);
+  }
+
   await knex.schema.createTable(MetaTable.SQL_EXECUTOR, (table) => {
     table.string('id', 20).primary();
     table.string('domain', 50);
     table.string('status', 20);
+    table.integer('priority');
+    table.integer('capacity');
     table.timestamps(true, true);
   });
 
@@ -18,24 +27,23 @@ const up = async (knex: Knex) => {
       id: `nc${nanoidv2()}`,
       domain: `http://staging-se-${(i + 1).toString().padStart(5, '0')}`,
       status: 'inactive',
+      priority: i + 1,
+      capacity: 10,
     });
   }
 
   await knex(MetaTable.SQL_EXECUTOR).insert(predefinedSqlExecutors);
 
   await knex.schema.alterTable(MetaTable.BASES, (table) => {
-    table
-      .string('fk_sql_executor_id', 20)
-      .references('id')
-      .inTable(MetaTable.SQL_EXECUTOR);
+    table.string('fk_sql_executor_id', 20).index();
   });
 };
 
 const down = async (knex: Knex) => {
-  await knex.schema.dropTable(MetaTable.SQL_EXECUTOR);
   await knex.schema.alterTable(MetaTable.BASES, (table) => {
     table.dropColumn('fk_sql_executor_id');
   });
+  await knex.schema.dropTable(MetaTable.SQL_EXECUTOR);
 };
 
 export { up, down };

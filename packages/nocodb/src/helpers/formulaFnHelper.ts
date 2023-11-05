206703d32f195b3ab686de888324f86ae51bdd4c
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2023-11-05 13:35:06 +0800

    refactor(nocodb): move the common datetime functions to sdk

--- packages/nocodb/src/helpers/formulaFnHelper.ts
+++ packages/nocodb/src/helpers/formulaFnHelper.ts
@@ -1,11 +1,6 @@
-import dayjs from 'dayjs';
 import { UITypes } from 'nocodb-sdk';
 import { convertDateFormat } from './convertDateFormat';
 import Column from '~/models/Column';
-
-// todo: tobe fixed
-// import customParseFormat from 'dayjs/plugin/customParseFormat';
-// extend(customParseFormat);
 
 export function getWeekdayByText(v: string) {
   return {
@@ -29,31 +24,6 @@ export function getWeekdayByIndex(idx: number): string {
     5: 'saturday',
     6: 'sunday',
   }[idx || 0];
-}
-
-export function validateDateWithUnknownFormat(v: string) {
-  const dateFormats = [
-    'DD-MM-YYYY',
-    'MM-DD-YYYY',
-    'YYYY-MM-DD',
-    'DD/MM/YYYY',
-    'MM/DD/YYYY',
-    'YYYY/MM/DD',
-    'DD MM YYYY',
-    'MM DD YYYY',
-    'YYYY MM DD',
-  ];
-  for (const format of dateFormats) {
-    if (dayjs(v, format, true).isValid() as any) {
-      return true;
-    }
-    for (const timeFormat of ['HH:mm', 'HH:mm:ss', 'HH:mm:ss.SSS']) {
-      if (dayjs(v, `${format} ${timeFormat}`, true).isValid() as any) {
-        return true;
-      }
-    }
-  }
-  return false;
 }
 
 export async function convertDateFormatForConcat(

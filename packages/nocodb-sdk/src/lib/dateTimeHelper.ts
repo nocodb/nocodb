399b4c1c54928a6f47667bb4e18e851d90814948
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2023-11-05 14:55:25 +0800

    feat(sdk): add timeAgo

--- packages/nocodb-sdk/src/lib/dateTimeHelper.ts
+++ packages/nocodb-sdk/src/lib/dateTimeHelper.ts
@@ -109,3 +109,13 @@ export const handleTZ = (val: any) => {
 export function validateDateFormat(v: string) {
   return dateFormats.includes(v);
 }
+
+export const timeAgo = (date: any) => {
+  if (/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/.test(date)) {
+    // if there is no timezone info, consider as UTC
+    // e.g. 2023-01-01 08:00:00 (MySQL)
+    date += '+00:00';
+  }
+  // show in local time
+  return dayjs(date).fromNow();
+};

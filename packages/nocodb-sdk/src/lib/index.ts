bb37b993ed64d4f6a7b88a48d99ea63a7cc69353
Pranav C <pranavxc@gmail.com>
2023-11-07 02:28:07 +0530

    fix: missing types in nocodb-sdk

--- packages/nocodb-sdk/src/lib/index.ts
+++ packages/nocodb-sdk/src/lib/index.ts
@@ -14,7 +14,7 @@ export {
   isVirtualCol,
   isLinksOrLTAR,
 } from '~/lib/UITypes';
-export { default as CustomAPI } from '~/lib/CustomAPI';
+export { default as CustomAPI, FileType } from '~/lib/CustomAPI';
 export { default as TemplateGenerator } from '~/lib/TemplateGenerator';
 export * from '~/lib/passwordHelpers';
 export * from '~/lib/mergeSwaggerSchema';

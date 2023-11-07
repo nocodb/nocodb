3b5e486e89d7e06f7f1fcdce039307edc2b2fca5
mertmit <mertmit99@gmail.com>
2023-11-07 11:57:46 +0300

    temp: job status logs
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/modules/jobs/redis/jobs-event.service.ts
+++ packages/nocodb/src/modules/jobs/redis/jobs-event.service.ts
@@ -7,14 +7,18 @@ import {
 import { Job } from 'bull';
 import boxen from 'boxen';
 import { EventEmitter2 } from '@nestjs/event-emitter';
+import { Logger } from '@nestjs/common';
 import { JobEvents, JOBS_QUEUE, JobStatus } from '~/interface/Jobs';
 
 @Processor(JOBS_QUEUE)
 export class JobsEventService {
+  protected logger = new Logger(JobsEventService.name);
+
   constructor(private eventEmitter: EventEmitter2) {}
 
   @OnQueueActive()
   onActive(job: Job) {
+    this.logger.log(`Processing job ${job.id} of type ${job.name}`);
     this.eventEmitter.emit(JobEvents.STATUS, {
       id: job.id.toString(),
       status: JobStatus.ACTIVE,
@@ -23,6 +27,7 @@ export class JobsEventService {
 
   @OnQueueFailed()
   onFailed(job: Job, error: Error) {
+    this.logger.error(`Job ${job.id} failed with error ${error.message}`);
     console.error(
       boxen(
         `---- !! JOB FAILED !! ----\nid:${job.id}\nerror:${error.name} (${error.message})\n\nstack: ${error.stack}`,
@@ -47,6 +52,7 @@ export class JobsEventService {
 
   @OnQueueCompleted()
   onCompleted(job: Job, data: any) {
+    this.logger.log(`Job ${job.id} completed`);
     this.eventEmitter.emit(JobEvents.STATUS, {
       id: job.id.toString(),
       status: JobStatus.COMPLETED,

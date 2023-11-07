de551f740d6530757a45690683f28b29abcaa29e
Muhammed Mustafa <mustafa@nocodb.com>
2023-11-07 10:42:15 +0000

    feat: Added barcode download btn

--- packages/nc-gui/components/virtual-cell/barcode/Barcode.vue
+++ packages/nc-gui/components/virtual-cell/barcode/Barcode.vue
@@ -46,7 +46,12 @@ const rowHeight = inject(RowHeightInj, ref(undefined))
     :footer="null"
     @ok="handleModalOkClick"
   >
-    <JsBarcodeWrapper v-if="showBarcode" :barcode-value="barcodeValue" :barcode-format="barcodeMeta.barcodeFormat" />
+    <JsBarcodeWrapper
+      v-if="showBarcode"
+      :barcode-value="barcodeValue"
+      :barcode-format="barcodeMeta.barcodeFormat"
+      show-download
+    />
   </a-modal>
   <div
     class="flex ml-2 w-full items-center"

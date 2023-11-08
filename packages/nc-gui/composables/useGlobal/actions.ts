bbc8e40e3c4f1acea3277c36209c50013c3220ae
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2023-11-08 13:44:12 +0800

    chore(nc-gui): lint

--- packages/nc-gui/composables/useGlobal/actions.ts
+++ packages/nc-gui/composables/useGlobal/actions.ts
@@ -1,8 +1,7 @@
 import { getActivePinia } from 'pinia'
 import type { Actions, AppInfo, State } from './types'
 import type { NcProjectType } from '#imports'
-import { message, useNuxtApp } from '#imports'
-import { navigateTo } from '#imports'
+import { message, navigateTo, useNuxtApp } from '#imports'
 
 export function useGlobalActions(state: State): Actions {
   const setIsMobileMode = (isMobileMode: boolean) => {

5373d73e158195b31b5f2cbf310c54ab1c3d6f3b
mertmit <mertmit99@gmail.com>
2023-11-17 13:25:16 +0300

    chore: add planned todos
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/plugins/backblaze/Backblaze.ts
+++ packages/nocodb/src/plugins/backblaze/Backblaze.ts
@@ -54,6 +54,7 @@ export default class Backblaze implements IStorageAdapterV2 {
         .get(url, {
           httpAgent: useAgent(url, { stopPortScanningByUrlRedirection: true }),
           httpsAgent: useAgent(url, { stopPortScanningByUrlRedirection: true }),
+          // TODO - use stream instead of buffer
           responseType: 'arraybuffer',
         })
         .then((response) => {

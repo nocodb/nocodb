7f914701c17fc954a293ec25a370ee0499609e0f
musharaf <musharaf@nocodb.com>
2023-12-01 16:04:53 +0530

    fix :  removed unwanted truncate

--- packages/nc-gui/components/smartsheet/toolbar/SearchData.vue
+++ packages/nc-gui/components/smartsheet/toolbar/SearchData.vue
@@ -112,7 +112,7 @@ watch(columns, () => {
         @change="onPressEnter"
       >
         <a-select-option v-for="op of columns" :key="op.value" v-e="['c:search:field:select']" :value="op.value">
-          <div class="text-[0.75rem] flex items-center truncate -ml-1 gap-2">
+          <div class="text-[0.75rem] flex items-center -ml-1 gap-2">
             <SmartsheetHeaderIcon class="text-sm" :column="op.column" />
             <NcTooltip class="truncate" placement="top">
               <template #title>{{ op.label }}</template>

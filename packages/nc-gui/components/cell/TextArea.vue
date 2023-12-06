8c170a15238ee13bdc0e3e6ca3bd287726de06fa
Muhammed Mustafa <mustafa@nocodb.com>
2023-12-06 12:09:18 +0000

    fix: Made rich modal dragging smoother

--- packages/nc-gui/components/cell/TextArea.vue
+++ packages/nc-gui/components/cell/TextArea.vue
@@ -120,8 +120,8 @@ const onMouseMove = (e: MouseEvent) => {
   e.stopPropagation()
 
   position.value = {
-    top: e.clientY - 22,
-    left: e.clientX - 46,
+    top: e.clientY - 30,
+    left: e.clientX - 120,
   }
 }
 
@@ -149,7 +149,7 @@ watch(position, () => {
 
     dom.style.left = `${position.value.left}px`
     dom.style.top = `${position.value.top}px`
-  }, 100)
+  }, 1)
 })
 
 const dragStart = () => {

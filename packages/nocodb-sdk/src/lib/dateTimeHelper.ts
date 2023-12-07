314d36453385f6ca751f6fc63fc5fc801b57b270
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2023-12-07 17:01:16 +0000

    refactor(sdk): add comment to handleTZ regex

--- packages/nocodb-sdk/src/lib/dateTimeHelper.ts
+++ packages/nocodb-sdk/src/lib/dateTimeHelper.ts
@@ -98,6 +98,7 @@ export const handleTZ = (val: any) => {
     return val;
   }
   return val.replace(
+    // match and extract dates and times in the ISO 8601 format
     /((?:-?(?:[1-9][0-9]*)?[0-9]{4})-(?:1[0-2]|0[1-9])-(?:3[01]|0[1-9]|[12][0-9])T(?:2[0-3]|[01][0-9]):(?:[0-5][0-9]):(?:[0-5][0-9])(?:\.[0-9]+)?(?:Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9]))/g,
     (_, v) => {
       return dayjs(v).format('YYYY-MM-DD HH:mm');

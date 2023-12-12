6c0c89e16c56553ee3e59d4444ee533820b2a7ab
musharaf-nocodb <musharaf@nocodb.com>
2023-12-12 16:24:40 +0000

    fix : test case for dateoption

--- packages/nc-gui/components/smartsheet/column/DateOptions.vue
+++ packages/nc-gui/components/smartsheet/column/DateOptions.vue
@@ -17,9 +17,9 @@ if (!vModel.value.meta?.date_format) {
 
 <template>
   <a-form-item :label="$t('labels.dateFormat')">
-    <a-select v-model:value="vModel.meta.date_format" dropdown-class-name="nc-dropdown-date-format">
-      <a-select-option v-for="(format, i) of dateFormats" :key="i" :value="format">
-        <div class="flex gap-2 w-full justify-between items-center">
+    <a-select v-model:value="vModel.meta.date_format" class="nc-date-select" dropdown-class-name="nc-dropdown-date-format">
+      <a-select-option v-for="(format, i) of [...dateFormats, ...dateMonthFormats]" :key="i" :value="format">
+        <div class="flex gap-2 justify-between items-center" :data-testid="`${format}`">
           {{ format }}
           <component
             :is="iconMap.check"

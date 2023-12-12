4b8ee083f5689fd543f5a727c98a384e9f250525
musharaf-nocodb <musharaf@nocodb.com>
2023-12-12 14:44:25 +0530

    fix : removed unwanted spaces

--- packages/nc-gui/components/smartsheet/column/LookupOptions.vue
+++ packages/nc-gui/components/smartsheet/column/LookupOptions.vue
@@ -83,7 +83,7 @@ const cellIcon = (column: ColumnType) =>
       <a-form-item class="flex w-1/2 pb-2" :label="$t('labels.links')" v-bind="validateInfos.fk_relation_column_id">
         <a-select
           v-model:value="vModel.fk_relation_column_id"
-          dropdown-class-name="!w-64 !rounded-md nc-dropdown-relation-table "
+          dropdown-class-name="!w-64 !rounded-md nc-dropdown-relation-table"
           @change="onRelationColChange"
         >
           <a-select-option v-for="(table, i) of refTables" :key="i" :value="table.col.fk_column_id">

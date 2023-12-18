f34fe0d564264b3219e459d4ec624bc89a5afc6a
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2023-12-18 11:02:03 +0530

    fix: Expand icon for long text & rich text, should become visible on hover / on click. Not always #7210

--- packages/nc-gui/components/cell/TextArea.vue
+++ packages/nc-gui/components/cell/TextArea.vue
@@ -239,7 +239,7 @@ watch(editEnabled, () => {
       <NcTooltip
         v-if="!isVisible"
         placement="bottom"
-        class="!absolute right-0 bottom-1 nc-text-area-expand-btn"
+        class="!absolute right-0 bottom-1 hidden nc-text-area-expand-btn"
         :class="{ 'right-0 bottom-1': editEnabled, '!bottom-0': !isRichMode }"
       >
         <template #title>{{ $t('title.expand') }}</template>

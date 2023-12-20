a62f825c1c2dc6a5591adf2c44b7b15c6f8871a7
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-12-20 17:47:30 +0530

    feat(nc-gui): day view inital draft

---
+++ packages/nc-gui/components/smartsheet/calendar/DayView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import {UITypes} from "nocodb-sdk";
+
+const { pageDate, selectedDate, calDataType } = useCalendarViewStoreOrThrow();
+
+const emit = defineEmits(['expand-record']);
+
+const events = ref([
+  {
+    "Id": 1,
+    "Title": "Event 01",
+    "from_date_time": "2023-12-15",
+    "to_date_time": "2023-12-20"
+  },
+  {
+    "Id": 2,
+    "Title": "Event 02",
+    "from_date_time": "2023-12-20",
+    "to_date_time": "2023-12-25"
+  }
+])
+</script>
+
+<template>
+<div v-if="calDataType === UITypes.Date" class="flex flex-col px-2 gap-4 pt-4">
+  <LazySmartsheetCalendarRecordCard v-for="event in events" :name="event.Title" :date="event.from_date_time" color="blue" size="medium" @click="emit('expand-record', 'xxx')" />
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>

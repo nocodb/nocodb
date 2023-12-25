ffcf84752dd280d8e6e94cb0a209e30d944a46cf
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2023-12-25 23:02:18 +0530

    fix: number input up-down button should be hidden

--- packages/nc-gui/components/cell/Currency.vue
+++ packages/nc-gui/components/cell/Currency.vue
@@ -99,3 +99,17 @@ onMounted(() => {
   <!-- possibly unexpected string / null with showNull == false  -->
   <span v-else />
 </template>
+
+<style lang="scss" scoped>
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type='number'] {
+  -moz-appearance: textfield;
+}
+</style>

739caf1476531120db66e003c0421b243e1d5e69
Pranav C <pranavxc@gmail.com>
2023-12-29 16:39:32 +0530

    fix: set the default value using withDefault

--- packages/nc-gui/components/general/NocoIcon.vue
+++ packages/nc-gui/components/general/NocoIcon.vue
@@ -6,7 +6,7 @@ interface Props {
   animate?: boolean
 }
 
-const { size = 90, animate = false } = defineProps<Props>()
+const { size, animate } = withDefaults(defineProps<Props>(), { size: 90, animate: false })
 
 const ping = autoResetRef(false, 1000)
 

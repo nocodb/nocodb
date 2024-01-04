1a23741727b1caed2c1d59fd27c04bcb78f9e84e
Pranav C <pranavxc@gmail.com>
2024-01-04 13:19:27 +0000

    fix: add missing validation

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -2962,6 +2962,12 @@ class BaseModelSqlv2 {
           for (let i = 0; i < this.model.columns.length; ++i) {
             const col = this.model.columns[i];
 
+            if (col.title in d && isCreatedTimeOrUpdatedTimeCol(col)) {
+              NcError.badRequest(
+                `Column "${col.title}" is auto generated and cannot be updated`,
+              );
+            }
+
             // populate pk columns
             if (col.pk) {
               if (col.meta?.ag && !d[col.title]) {

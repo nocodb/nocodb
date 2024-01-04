214fb0dad986e8a350ddb6b9057fde8f06d4d3e8
Pranav C <pranavxc@gmail.com>
2024-01-04 13:19:21 +0000

    fix: add an overlay to block the interaction with datetimepicker

--- packages/nc-gui/components/cell/ReadOnlyDateTimePicker.vue
+++ packages/nc-gui/components/cell/ReadOnlyDateTimePicker.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ReadonlyInj, provide, ref, EditModeInj, ActiveCellInj } from '#imports'
+import { ActiveCellInj, EditModeInj, ReadonlyInj, provide, ref } from '#imports'
 
 interface Props {
   modelValue?: string | null
@@ -12,9 +12,11 @@ provide(ReadonlyInj, ref(true))
 provide(EditModeInj, ref(true))
 
 provide(ActiveCellInj, ref(true))
-
 </script>
 
 <template>
-  <LazyCellDateTimePicker :model-value="modelValue" :is-pk="false" />
+  <div class="relative">
+    <LazyCellDateTimePicker class="z-0" :model-value="modelValue" :is-pk="false" />
+    <div class="w-full h-full z-1 absolute top-0 left-0"></div>
+  </div>
 </template>

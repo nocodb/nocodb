7584350e7e00fffed6cf5d414608afbd0a9da334
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-01-17 23:33:38 +0530

    fix: small changes

--- packages/nc-gui/composables/useExpandedFormStore.ts
+++ packages/nc-gui/composables/useExpandedFormStore.ts
@@ -48,9 +48,7 @@ const [useProvideExpandedFormStore, useExpandedFormStore] = useInjectionState((m
   const { sharedView } = useSharedView()
 
   const row = ref<Row>(
-    sharedView.value?.type === ViewTypes.GALLERY || sharedView.value?.type === ViewTypes.KANBAN
-      ? _row.value
-      : _row.value.rowMeta.new
+    sharedView.value?.type === ViewTypes.GALLERY || sharedView.value?.type === ViewTypes.KANBAN || _row.value.rowMeta.new
       ? _row.value
       : ({ row: {}, oldRow: {}, rowMeta: {} } as Row),
   )

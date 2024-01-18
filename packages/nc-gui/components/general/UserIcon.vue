2f01d8798c39c178361622e4ca0913f6c9261cee
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-01-18 06:33:40 +0000

    fix(nc-gui): add circular badge with initials for user field options

--- packages/nc-gui/components/general/UserIcon.vue
+++ packages/nc-gui/components/general/UserIcon.vue
@@ -3,7 +3,7 @@ import { isColorDark, stringToColor } from '~/utils/colorsUtils'
 
 const props = withDefaults(
   defineProps<{
-    size?: 'small' | 'medium' | 'base' | 'large' | 'xlarge'
+    size?: 'small' | 'medium' | 'base' | 'large' | 'xlarge' | 'auto'
     name?: string
     email?: string
   }>(),
@@ -20,11 +20,7 @@ const email = computed(() => props?.email ?? '')
 
 const backgroundColor = computed(() => {
   // in comments we need to generate user icon from email
-  if (email.value.length) {
-    return stringToColor(email.value)
-  }
-
-  return email.value ? stringToColor(email.value) : '#FFFFFF'
+  return displayName.value ? stringToColor(displayName.value) : email.value ? stringToColor(email.value) : '#FFFFFF'
 })
 
 const usernameInitials = computed(() => {
@@ -46,6 +42,7 @@ const usernameInitials = computed(() => {
   <div
     class="flex nc-user-avatar font-bold"
     :class="{
+      'min-h-full aspect-square': size === 'auto',
       'min-w-4 min-h-4': size === 'small',
       'min-w-6 min-h-6': size === 'medium',
       'w-8 h-8 !text-md': size === 'base',
@@ -62,6 +59,6 @@ const usernameInitials = computed(() => {
 
 <style lang="scss" scoped>
 .nc-user-avatar {
-  @apply rounded-full text-xs flex items-center justify-center  uppercase;
+  @apply rounded-full text-xs flex items-center justify-center uppercase;
 }
 </style>

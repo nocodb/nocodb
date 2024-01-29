f5437cffacadaf5db7d0cd5a8100690b6ab3a812
Pranav C <pranavxc@gmail.com>
2024-01-30 02:08:13 +0530

    test: providers script for openid/saml playwright test

---
+++ scripts/ee/playwright/openid-provider/index.js
@@ -0,0 +1,50 @@
+// ref: https://github.com/magnolia-cms/mock-oidc-user-server/blob/master/server.js
+
+const assert = require('assert')
+const camelCase = require('camelcase')
+
+const Provider = require('oidc-provider')
+
+const host = process.env.HOST || 'localhost'
+const port = process.env.PORT || 3000
+
+const config = ['CLIENT_ID', 'CLIENT_SECRET', 'CLIENT_REDIRECT_URI', 'CLIENT_LOGOUT_REDIRECT_URI'].reduce((acc, v) => {
+  assert(process.env[v], `${v} config missing`)
+  acc[camelCase(v)] = process.env[v]
+  return acc
+}, {})
+
+const oidcConfig = {
+  claims: {
+    email: ['email'],
+    profile: ['name', 'preferred_username']
+  },
+  clients: [{
+    client_id: config.clientId,
+    client_secret: config.clientSecret,
+    redirect_uris: [config.clientRedirectUri],
+    post_logout_redirect_uris: [config.clientLogoutRedirectUri]
+  }],
+  async findAccount (ctx, id) {
+    return {
+      accountId: id,
+      async claims (use, scope) {
+        return {
+          sub: id,
+          email: 'test@test.ch',
+          name: 'test',
+          preferred_username: 'Test'
+        }
+      }
+    }
+  }
+
+}
+
+const oidc = new Provider(`http://${host}:${port}`, oidcConfig)
+
+oidc.callback()
+
+oidc.listen(port, () => {
+  console.log(`oidc-provider listening on port ${port}, check http://localhost:${port}/.well-known/openid-configuration`)
+})
---
+++ scripts/ee/playwright/saml-provider/index.js
@@ -0,0 +1,27 @@
+// ref: https://github.com/mcguinness/saml-idp
+
+const { runServer } = require('saml-idp')
+
+runServer({
+  acsUrl: `https://foo.okta.com/auth/saml20/assertion-consumer`,
+  audience: `https://foo.okta.com/auth/saml20/metadata`,
+  config: {
+    user: {
+      email: 'test@nocodb.com'
+    },
+    // The auth-service requires at least one AttributeStatement in the SAML assertion.
+    metadata: [{
+      id: 'email',
+      optional: false,
+      displayName: 'E-Mail Address',
+      description: 'The e-mail address of the user',
+      multiValue: false,
+    }, {
+      id: 'userType',
+      optional: true,
+      displayName: 'User Type',
+      description: 'The type of user',
+      options: ['Admin', 'Editor', 'Commenter'],
+    }],
+  },
+})

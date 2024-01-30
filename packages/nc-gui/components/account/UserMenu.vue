b88f63d90a37d4118823cf2db5336b0391b93b63
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-01-30 05:03:29 +0000

    fix(nc-gui): Failed to resolve component - Empty

--- packages/nc-gui/components/account/UserMenu.vue
+++ packages/nc-gui/components/account/UserMenu.vue
@@ -4,7 +4,7 @@ import type { UsersSortType } from '~/lib'
 
 const { field, direction, handleUserSort } = defineProps<{
   field: UsersSortType['field']
-  direction: UsersSortType['direction']
+  direction?: UsersSortType['direction']
   handleUserSort: Function
 }>()
 

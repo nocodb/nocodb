33132099a4cf2b67add30bed67595e25df6656c8
Pranav C <pranavxc@gmail.com>
2024-02-04 00:45:11 +0530

    test: wait until network idle on reload

--- tests/playwright/pages/SsoIdpPage/OpenIDLoginPage.ts
+++ tests/playwright/pages/SsoIdpPage/OpenIDLoginPage.ts
@@ -12,9 +12,7 @@ export class OpenIDLoginPage extends BasePage {
 
   async goto(title = 'test') {
     // reload page to get latest app info
-    await this.rootPage.reload();
-    await this.rootPage.waitForTimeout(1000);
-    await this.rootPage.goto('/#/signin/');
+    await this.rootPage.reload({ waitUntil: 'networkidle' });
     // click sign in with SAML
     await this.rootPage.locator(`button:has-text("Sign in with ${title}")`).click();
   }

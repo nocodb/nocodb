e1170c7826cd20338e0dfeaf74173d729a893baa
Pranav C <pranavxc@gmail.com>
2024-02-07 12:26:52 +0000

    test: remove unnecessary wait time

--- tests/playwright/pages/SsoIdpPage/OpenIDLoginPage.ts
+++ tests/playwright/pages/SsoIdpPage/OpenIDLoginPage.ts
@@ -13,8 +13,10 @@ export class OpenIDLoginPage extends BasePage {
   async goto(title = 'test') {
     // reload page to get latest app info
     await this.rootPage.reload();
-    await this.rootPage.waitForTimeout(1000);
+    await this.rootPage.waitForTimeout(500);
     await this.rootPage.goto('/#/signin/');
+    await this.rootPage.waitForTimeout(500);
+    await this.rootPage.reload();
     // click sign in with SAML
     await this.rootPage.locator(`button:has-text("Sign in with ${title}")`).click();
   }

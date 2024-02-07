ca9b62c3d66436e635d9571496a153adc3197b4d
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-02-07 20:21:19 +0000

    fic(nc-gui): set new record dropdown position to topLeft

--- packages/nc-gui/components/smartsheet/grid/Table.vue
+++ packages/nc-gui/components/smartsheet/grid/Table.vue
@@ -1893,11 +1893,11 @@ onKeyStroke('ArrowDown', onDown)
             <template #overlay>
               <div class="relative overflow-visible min-h-17 w-10">
                 <div
-                  class="absolute -top-19 flex flex-col min-h-34.5 w-70 p-1.5 bg-white rounded-lg border-1 border-gray-200 justify-start overflow-hidden"
+                  class="absolute -top-21 flex flex-col min-h-34.5 w-70 p-1.5 bg-white rounded-lg border-1 border-gray-200 justify-start overflow-hidden"
                   style="box-shadow: 0px 4px 6px -2px rgba(0, 0, 0, 0.06), 0px -12px 16px -4px rgba(0, 0, 0, 0.1)"
                   :class="{
-                    '-left-44': !isAddNewRecordGridMode,
-                    '-left-32': isAddNewRecordGridMode,
+                    '-left-32.5': !isAddNewRecordGridMode,
+                    '-left-21.5': isAddNewRecordGridMode,
                   }"
                 >
                   <div

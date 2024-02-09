6a13b1ee9a0817c76d10b4a7d39ad8959e53d8db
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-02-09 15:28:42 +0530

    test: playwright pages

---
+++ tests/playwright/pages/Dashboard/common/Toolbar/CalendarViewModel.ts
@@ -0,0 +1,19 @@
+import BasePage from '../../../Base';
+import { ToolbarPage } from './index';
+
+export class ToolbarCalendarViewModePage extends BasePage {
+  readonly toolbar: ToolbarPage;
+
+  constructor(toolbar: ToolbarPage) {
+    super(toolbar.rootPage);
+    this.toolbar = toolbar;
+  }
+
+  get() {
+    return this.rootPage.getByTestId('nc-calendar-view-mode');
+  }
+
+  changeCalendarView({ title }: { title: string }) {
+    await this.get().getByTestId(`nc-calendar-view-mode-${title}`).click();
+  }
+}

0717529215a80d6fcbd4e29aafbb36c902aabcdd
mertmit <mertmit99@gmail.com>
2024-02-10 16:12:43 +0300

    fix: alter column types for form view
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/meta/migrations/v2/nc_040_form_view_alter_column_types.ts
+++ packages/nocodb/src/meta/migrations/v2/nc_040_form_view_alter_column_types.ts
@@ -5,17 +5,24 @@ const up = async (knex: Knex) => {
   await knex.schema.alterTable(MetaTable.FORM_VIEW, (table) => {
     table.text('subheading').alter();
   });
-  await knex.schema.alterTable(MetaTable.FORM_VIEW, (table) => {
-    table.text('success_msg').alter();
-  });
   await knex.schema.alterTable(MetaTable.FORM_VIEW_COLUMNS, (table) => {
     table.text('label').alter();
   });
   await knex.schema.alterTable(MetaTable.FORM_VIEW_COLUMNS, (table) => {
-    table.text('description').alter();
+    table.text('help').alter();
   });
 };
 
-const down = async (knex: Knex) => {};
+const down = async (knex: Knex) => {
+  await knex.schema.alterTable(MetaTable.FORM_VIEW, (table) => {
+    table.string('subheading').alter();
+  });
+  await knex.schema.alterTable(MetaTable.FORM_VIEW_COLUMNS, (table) => {
+    table.string('label').alter();
+  });
+  await knex.schema.alterTable(MetaTable.FORM_VIEW_COLUMNS, (table) => {
+    table.string('help').alter();
+  });
+};
 
 export { up, down };

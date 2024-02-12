e9e3fec5aec89147a354fed49fcbebdaaa646143
Pranav C <pranavxc@gmail.com>
2024-02-12 20:19:49 +0530

    refactor: map CTA action

--- packages/nc-gui/ee/components/dashboard/Sidebar/Trial.vue
+++ packages/nc-gui/ee/components/dashboard/Sidebar/Trial.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { useGlobal } from '#imports'
+
 const { appInfo } = useGlobal()
 
 const numberOfDaysLeft = computed(() => {
@@ -16,18 +17,19 @@ const trialPercentage = computed(() => {
   const trialStartDate = new Date(appInfo.value.licenseIssuedTime * 1000)
   const trialEndDate = new Date(appInfo.value.licenseExpiryTime * 1000)
 
-  const timeDiff = today.getTime() - trialStartDate.getTime()
+  const timeDiff = trialEndDate - today.getTime()
   const timeDiffTotal = trialEndDate.getTime() - trialStartDate.getTime()
   return Math.ceil((timeDiff / timeDiffTotal) * 100)
 })
 
+// color of the progress bar based on the percentage
 const precentageColor = computed(() => {
-  if (trialPercentage.value < 50) {
-    return 'blue'
-  } else if (trialPercentage.value < 75) {
-    return 'orange'
+  if (trialPercentage.value > 50) {
+    return '#3366FF'
+  } else if (trialPercentage.value > 25) {
+    return '#FA8231'
   } else {
-    return 'red'
+    return '#FF4A3F'
   }
 })
 </script>
@@ -39,15 +41,17 @@ const precentageColor = computed(() => {
         <img width="25" alt="NocoDB" src="~/assets/img/icons/256x256.png" />
         Enterprise Trial License
       </div>
-      <a-progress :percent="trialPercentage" :strokeColor="precentageColor" :show-info="false" />
+      <a-progress :percent="trialPercentage" :stroke-color="precentageColor" :show-info="false" />
 
-      <div>Expires in: {{ numberOfDaysLeft }} day{{numberOfDaysLeft > 1 ? 's' : ''}}</div>
-      <nc-button size="small" class="text-center w-full">
-        <div class="flex gap-2 align-center justify-center">
-          <GeneralIcon icon="phoneCall" />
-          Talk to sales
-        </div>
-      </nc-button>
+      <div>Expires in: {{ numberOfDaysLeft }} day{{ numberOfDaysLeft > 1 ? 's' : '' }}</div>
+      <a no-prefetch no-rel href="https://calendly.com/nocodb-meeting" target="_blank">
+        <nc-button size="small" class="text-center w-full text-white">
+          <div class="flex gap-2 items-center justify-center">
+            <GeneralIcon icon="phoneCall" />
+            Talk to sales
+          </div>
+        </nc-button>
+      </a>
     </div>
   </a-card>
 </template>

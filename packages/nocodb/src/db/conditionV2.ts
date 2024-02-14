d57ff78e43473e763ba7e1f1a7209c19da6fe29d
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-02-14 20:09:41 +0530

    fix: small changes

--- packages/nocodb/src/db/conditionV2.ts
+++ packages/nocodb/src/db/conditionV2.ts
@@ -670,8 +670,7 @@ const parseConditionV2 = async (
         if (
           isNumericCol(column.uidt) &&
           typeof genVal === 'string' &&
-          !genVal.startsWith('%') &&
-          !genVal.endsWith('%')
+          !isNaN(+genVal)
         ) {
           // convert to number
           genVal = +genVal;

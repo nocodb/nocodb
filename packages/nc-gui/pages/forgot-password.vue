b14492ac178128598ac6eef60679f6a0fa4b5db9
Pranav C <pranavxc@gmail.com>
2024-02-15 19:38:12 +0000

    fix: wrap with div if root element is NuxtLayout to avoid navigation error

--- packages/nc-gui/pages/forgot-password.vue
+++ packages/nc-gui/pages/forgot-password.vue
@@ -61,61 +61,68 @@ function navigateSignIn() {
 </script>
 
 <template>
-  <NuxtLayout>
-    <div class="md:bg-primary bg-opacity-5 forgot-password h-full min-h-[600px] flex flex-col justify-center items-center">
-      <div
-        class="bg-white mt-[60px] relative flex flex-col justify-center gap-2 w-full max-w-[500px] mx-auto p-8 md:(rounded-lg border-1 border-gray-200 shadow-xl)"
-      >
-        <LazyGeneralNocoIcon class="color-transition hover:(ring ring-accent ring-opacity-100)" :animate="isLoading" />
+  <div>
+    <NuxtLayout>
+      <div class="md:bg-primary bg-opacity-5 forgot-password h-full min-h-[600px] flex flex-col justify-center items-center">
+        <div
+          class="bg-white mt-[60px] relative flex flex-col justify-center gap-2 w-full max-w-[500px] mx-auto p-8 md:(rounded-lg border-1 border-gray-200 shadow-xl)"
+        >
+          <LazyGeneralNocoIcon class="color-transition hover:(ring ring-accent ring-opacity-100)" :animate="isLoading" />
 
-        <div class="self-center flex flex-col justify-center items-center text-center gap-2">
-          <h1 class="prose-2xl font-bold my-4 w-full">{{ $t('title.resetPassword') }}</h1>
+          <div class="self-center flex flex-col justify-center items-center text-center gap-2">
+            <h1 class="prose-2xl font-bold my-4 w-full">{{ $t('title.resetPassword') }}</h1>
 
-          <template v-if="!success">
-            <div class="prose-sm">{{ $t('msg.info.passwordRecovery.message_1') }}</div>
-            <div class="prose-sm mb-4">{{ $t('msg.info.passwordRecovery.message_2') }}</div>
-          </template>
+            <template v-if="!success">
+              <div class="prose-sm">{{ $t('msg.info.passwordRecovery.message_1') }}</div>
+              <div class="prose-sm mb-4">{{ $t('msg.info.passwordRecovery.message_2') }}</div>
+            </template>
 
-          <template v-else>
-            <div class="prose-sm text-success flex items-center leading-8 gap-2">
-              {{ $t('msg.info.passwordRecovery.success') }} <ClaritySuccessLine />
-            </div>
+            <template v-else>
+              <div class="prose-sm text-success flex items-center leading-8 gap-2">
+                {{ $t('msg.info.passwordRecovery.success') }} <ClaritySuccessLine />
+              </div>
 
-            <nuxt-link @click="navigateSignIn">{{ $t('general.signIn') }}</nuxt-link>
-          </template>
-        </div>
+              <nuxt-link @click="navigateSignIn">{{ $t('general.signIn') }}</nuxt-link>
+            </template>
+          </div>
 
-        <a-form ref="formValidator" layout="vertical" :model="form" no-style @finish="resetPassword">
-          <Transition name="layout">
-            <div v-if="error" class="self-center mb-4 bg-red-500 text-white rounded-lg w-3/4 mx-auto p-1">
-              <div class="flex items-center gap-2 justify-center">
-                <MaterialSymbolsWarning />
-                <div class="break-words">{{ error }}</div>
+          <a-form ref="formValidator" layout="vertical" :model="form" no-style @finish="resetPassword">
+            <Transition name="layout">
+              <div v-if="error" class="self-center mb-4 bg-red-500 text-white rounded-lg w-3/4 mx-auto p-1">
+                <div class="flex items-center gap-2 justify-center">
+                  <MaterialSymbolsWarning />
+                  <div class="break-words">{{ error }}</div>
+                </div>
+              </div>
+            </Transition>
+
+            <a-form-item :label="$t('labels.email')" name="email" :rules="formRules.email">
+              <a-input
+                v-model:value="form.email"
+                size="large"
+                :placeholder="$t('msg.info.signUp.workEmail')"
+                @focus="resetError"
+              />
+            </a-form-item>
+
+            <div class="self-center flex flex-col gap-4 items-center justify-center w-full">
+              <button class="scaling-btn bg-opacity-100" type="submit">
+                <span class="flex items-center gap-2">
+                  <component :is="iconMap.signin" />
+                  {{ $t('activity.sendEmail') }}
+                </span>
+              </button>
+
+              <div class="text-end prose-sm">
+                {{ $t('msg.info.signUp.alreadyHaveAccount') }}
+                <nuxt-link @click="navigateSignIn">{{ $t('general.signIn') }}</nuxt-link>
               </div>
             </div>
-          </Transition>
-
-          <a-form-item :label="$t('labels.email')" name="email" :rules="formRules.email">
-            <a-input v-model:value="form.email" size="large" :placeholder="$t('msg.info.signUp.workEmail')" @focus="resetError" />
-          </a-form-item>
-
-          <div class="self-center flex flex-col gap-4 items-center justify-center w-full">
-            <button class="scaling-btn bg-opacity-100" type="submit">
-              <span class="flex items-center gap-2">
-                <component :is="iconMap.signin" />
-                {{ $t('activity.sendEmail') }}
-              </span>
-            </button>
-
-            <div class="text-end prose-sm">
-              {{ $t('msg.info.signUp.alreadyHaveAccount') }}
-              <nuxt-link @click="navigateSignIn">{{ $t('general.signIn') }}</nuxt-link>
-            </div>
-          </div>
-        </a-form>
+          </a-form>
+        </div>
       </div>
-    </div>
-  </NuxtLayout>
+    </NuxtLayout>
+  </div>
 </template>
 
 <style lang="scss">
--- packages/nc-gui/pages/projects/index.vue
+++ packages/nc-gui/pages/projects/index.vue
@@ -57,88 +57,90 @@ const deleteProject = (base: BaseType) => {
 </script>
 
 <template>
-  <NuxtLayout>
-    <template #sidebar>
-      <div class="flex flex-col h-full">
-        <div class="flex p-4">
-          <v-menu class="select-none">
-            <template #activator="{ props }">
-              <div
-                class="color-transition hover:(bg-gray-100) mr-auto select-none flex items-center gap-2 leading-8 cursor-pointer rounded-full border-1 border-gray-300 px-5 py-2 shadow prose-lg font-semibold"
-                @click="props.onClick"
-              >
-                <component :is="iconMap.plus" class="text-primary text-2xl" />
-                {{ $t('title.newProj') }}
-              </div>
-            </template>
+  <div>
+    <NuxtLayout>
+      <template #sidebar>
+        <div class="flex flex-col h-full">
+          <div class="flex p-4">
+            <v-menu class="select-none">
+              <template #activator="{ props }">
+                <div
+                  class="color-transition hover:(bg-gray-100) mr-auto select-none flex items-center gap-2 leading-8 cursor-pointer rounded-full border-1 border-gray-300 px-5 py-2 shadow prose-lg font-semibold"
+                  @click="props.onClick"
+                >
+                  <component :is="iconMap.plus" class="text-primary text-2xl" />
+                  {{ $t('title.newProj') }}
+                </div>
+              </template>
 
-            <v-list class="!py-0 flex flex-col bg-white rounded-lg shadow-md border-1 border-gray-300 mt-2 ml-2">
-              <div
-                class="grid grid-cols-12 cursor-pointer hover:bg-gray-200 flex items-center p-2"
-                @click="navigateTo('/base/create')"
-              >
-                <component :is="iconMap.plus" class="col-span-2 mr-1 mt-[1px] text-primary text-lg" />
-                <div class="col-span-10 text-sm xl:text-md">{{ $t('activity.createProject') }}</div>
+              <v-list class="!py-0 flex flex-col bg-white rounded-lg shadow-md border-1 border-gray-300 mt-2 ml-2">
+                <div
+                  class="grid grid-cols-12 cursor-pointer hover:bg-gray-200 flex items-center p-2"
+                  @click="navigateTo('/base/create')"
+                >
+                  <component :is="iconMap.plus" class="col-span-2 mr-1 mt-[1px] text-primary text-lg" />
+                  <div class="col-span-10 text-sm xl:text-md">{{ $t('activity.createProject') }}</div>
+                </div>
+                <div
+                  class="grid grid-cols-12 cursor-pointer hover:bg-gray-200 flex items-center p-2"
+                  @click="navigateTo('/base/create-external')"
+                >
+                  <component :is="iconMap.database" class="col-span-2 mr-1 mt-[1px] text-green-500 text-lg" />
+                  <div class="col-span-10 text-sm xl:text-md" v-html="$t('activity.createProjectExtended.extDB')" />
+                </div>
+              </v-list>
+            </v-menu>
+          </div>
+
+          <a-menu class="pr-4 flex-1 border-0">
+            <a-menu-item
+              v-for="(option, index) in navDrawerOptions"
+              :key="index"
+              class="!rounded-r-lg"
+              @click="activePage = option.title"
+            >
+              <div class="flex items-center gap-4">
+                <component :is="option.icon" />
+
+                <span class="font-semibold">
+                  {{ option.title }}
+                </span>
               </div>
-              <div
-                class="grid grid-cols-12 cursor-pointer hover:bg-gray-200 flex items-center p-2"
-                @click="navigateTo('/base/create-external')"
-              >
-                <component :is="iconMap.database" class="col-span-2 mr-1 mt-[1px] text-green-500 text-lg" />
-                <div class="col-span-10 text-sm xl:text-md" v-html="$t('activity.createProjectExtended.extDB')" />
-              </div>
-            </v-list>
-          </v-menu>
+            </a-menu-item>
+          </a-menu>
+
+          <general-social />
+
+          <general-sponsors :nav="true" />
+        </div>
+      </template>
+
+      <div class="flex-1 mb-12">
+        <div class="flex">
+          <div class="flex-1 text-2xl md:text-4xl font-bold text-gray-500 p-4">
+            {{ activePage }}
+          </div>
+
+          <div class="self-end flex text-4xl mb-1">
+            <MaterialSymbolsGridView
+              :class="route.name === 'index-index' ? '!text-primary' : ''"
+              class="cursor-pointer p-2 hover:bg-gray-300/50 rounded-full"
+              @click="navigateTo('/')"
+            />
+            <MaterialSymbolsFormatListBulletedRounded
+              :class="route.name === 'index-index-list' ? '!text-primary' : ''"
+              class="cursor-pointer p-2 hover:bg-gray-300/50 rounded-full"
+              @click="navigateTo('/list')"
+            />
+          </div>
         </div>
 
-        <a-menu class="pr-4 flex-1 border-0">
-          <a-menu-item
-            v-for="(option, index) in navDrawerOptions"
-            :key="index"
-            class="!rounded-r-lg"
-            @click="activePage = option.title"
-          >
-            <div class="flex items-center gap-4">
-              <component :is="option.icon" />
+        <a-divider class="!mb-4 lg:(!mb-8)" />
 
-              <span class="font-semibold">
-                {{ option.title }}
-              </span>
-            </div>
-          </a-menu-item>
-        </a-menu>
-
-        <general-social />
-
-        <general-sponsors :nav="true" />
+        <NuxtPage :bases="bases" @delete-base="deleteProject" />
       </div>
-    </template>
 
-    <div class="flex-1 mb-12">
-      <div class="flex">
-        <div class="flex-1 text-2xl md:text-4xl font-bold text-gray-500 p-4">
-          {{ activePage }}
-        </div>
-
-        <div class="self-end flex text-4xl mb-1">
-          <MaterialSymbolsGridView
-            :class="route.name === 'index-index' ? '!text-primary' : ''"
-            class="cursor-pointer p-2 hover:bg-gray-300/50 rounded-full"
-            @click="navigateTo('/')"
-          />
-          <MaterialSymbolsFormatListBulletedRounded
-            :class="route.name === 'index-index-list' ? '!text-primary' : ''"
-            class="cursor-pointer p-2 hover:bg-gray-300/50 rounded-full"
-            @click="navigateTo('/list')"
-          />
-        </div>
-      </div>
-
-      <a-divider class="!mb-4 lg:(!mb-8)" />
-
-      <NuxtPage :bases="bases" @delete-base="deleteProject" />
-    </div>
-
-    <a-modal></a-modal>
-  </NuxtLayout>
+      <a-modal></a-modal>
+    </NuxtLayout>
+  </div>
 </template>

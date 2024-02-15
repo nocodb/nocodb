84799a43a6c5314188b660036394b515770413a5
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-02-15 09:15:05 +0000

    refactor: form view field migration script

--- packages/nocodb/src/meta/migrations/v2/nc_039_form_view_filds.ts
+++ packages/nocodb/src/meta/migrations/v2/nc_039_form_view_filds.ts
@@ -4,11 +4,14 @@ import { MetaTable } from '~/utils/globals';
 const up = async (knex: Knex) => {
   await knex.schema.alterTable(MetaTable.FORM_VIEW, (table) => {
     table.text('subheading').alter();
+  });
+  await knex.schema.alterTable(MetaTable.FORM_VIEW, (table) => {
     table.text('success_msg').alter();
   });
-
   await knex.schema.alterTable(MetaTable.FORM_VIEW_COLUMNS, (table) => {
     table.text('label').alter();
+  });
+  await knex.schema.alterTable(MetaTable.FORM_VIEW_COLUMNS, (table) => {
     table.text('description').alter();
   });
 };

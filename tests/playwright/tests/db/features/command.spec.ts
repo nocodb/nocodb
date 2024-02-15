f6b502909d1c695ded57a01c22ae881ec81de833
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-02-16 00:17:02 +0530

    fix(nc-gui): cmd L

--- tests/playwright/tests/db/features/command.spec.ts
+++ tests/playwright/tests/db/features/command.spec.ts
@@ -88,7 +88,7 @@ test.describe('Command Shortcuts', () => {
 
     expect(await dashboard.cmdL.getActiveViewTitle()).toBe('Default View');
 
-    expect(await dashboard.cmdL.getActiveTableTitle()).toBe('City');
+    expect(await dashboard.cmdL.getActiveTableTitle()).toBe('Address');
 
     await dashboard.signOut();
 

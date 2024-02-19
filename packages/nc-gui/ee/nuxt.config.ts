67f6fda8e9e3b432703c1560c0fed304f08e6c8f
mertmit <mertmit99@gmail.com>
2024-02-19 10:57:48 +0300

    chore: bump nuxt version
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/ee/nuxt.config.ts
+++ packages/nc-gui/ee/nuxt.config.ts
@@ -206,7 +206,7 @@ export default defineNuxtConfig({
     define: {
       'process.env.DEBUG': 'false',
       'process.env.NC_ON_PREM': process.env.NC_ON_PREM ? JSON.stringify(process.env.NC_ON_PREM) : '"false"',
-      'process.nextTick': () => {},
+      'process.nextTick': 'globalThis.setImmediate',
       'process.env.ANT_MESSAGE_DURATION': process.env.ANT_MESSAGE_DURATION,
     },
     server: {
@@ -230,10 +230,6 @@ export default defineNuxtConfig({
         plugins: [NodeModulesPolyfillPlugin()],
       },
     },
-  },
-
-  experimental: {
-    reactivityTransform: true,
   },
 
   image: {

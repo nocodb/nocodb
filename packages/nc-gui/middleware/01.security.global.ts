d755b485143cd6b5ceed277617b0dc3c40e038fd
mertmit <mertmit99@gmail.com>
2024-02-19 16:34:22 +0300

    fix: PR requested change
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/middleware/01.security.global.ts
+++ packages/nc-gui/middleware/01.security.global.ts
@@ -2,9 +2,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
   // avoid non-embeddable paths within an iframe
   if (self !== top) {
     // allow for shared base
-    const embeddablePaths = ['/base/']
-    const embedRegex = new RegExp(`^(${embeddablePaths.join('|')})`)
-    if (embedRegex.test(to.path)) {
+    if (to.path.startsWith('/base/')) {
       return
     }
 

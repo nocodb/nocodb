c980e13e7df148151ca034785dfdaa4759982460
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-02-20 07:15:52 +0000

    fix(nc-gui): style day records based on range

--- packages/nc-gui/components/smartsheet/calendar/DayView.vue
+++ packages/nc-gui/components/smartsheet/calendar/DayView.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { UITypes } from 'nocodb-sdk'
+import dayjs from 'dayjs'
 import { type Row, computed, ref } from '#imports'
 
 interface Props {
@@ -21,6 +22,29 @@ const data = toRefs(props).data
 const displayField = computed(() => meta.value?.columns?.find((c) => c.pv && fields.value.includes(c)) ?? null)
 
 const { pageDate, selectedDate, calDataType, formattedData, calendarRange } = useCalendarViewStoreOrThrow()
+
+const getRecordPosition = (record: Row) => {
+  if (!calendarRange.value || !calendarRange.value[0]) return ''
+  const startCol = calendarRange.value[0].fk_from_col
+  const endCol = calendarRange.value[0].fk_to_col
+
+  if (!endCol && startCol) {
+    const startDate = dayjs(record.row[startCol.title])
+    return startDate.isSame(selectedDate.value, 'day') ? 'rounded' : ''
+  } else if (endCol && startCol) {
+    const startDate = dayjs(record.row[startCol.title])
+    const endDate = dayjs(record.row[endCol.title])
+    if (startDate.isSame(selectedDate.value, 'day') || endDate.isSame(selectedDate.value, 'day')) {
+      return 'rounded'
+    } else if (startDate.isBefore(selectedDate.value, 'day') && endDate.isAfter(selectedDate.value, 'day')) {
+      return 'none'
+    } else if (startDate.isSame(selectedDate.value, 'day') && endDate.isAfter(selectedDate.value, 'day')) {
+      return 'leftRounded'
+    } else if (startDate.isBefore(selectedDate.value, 'day') && endDate.isSame(selectedDate.value, 'day')) {
+      return 'rightRounded'
+    }
+  }
+}
 
 const hours = computed(() => {
   const hours = []
@@ -46,13 +70,14 @@ const renderData = computed(() => {
         'h-calc(100vh-10.8rem) overflow-y-auto nc-scrollbar-md': !props.isEmbed,
         'border-r-1 h-full border-gray-200 hover:bg-gray-50 ': props.isEmbed,
       }"
-      class="flex flex-col pt-3 gap-2 h-full w-full px-1"
+      class="flex flex-col pt-3 gap-2 h-full w-full"
     >
       <LazySmartsheetRow v-for="(record, rowIndex) in renderData" :key="rowIndex" :row="record">
         <LazySmartsheetCalendarRecordCard
           :key="rowIndex"
           :date="record.row[calendarRange[0].fk_from_col.title]"
           :name="record.row[displayField.title]"
+          :position="getRecordPosition(record)"
           color="blue"
           size="small"
           @click="emit('expand-record', record)"

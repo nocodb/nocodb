08f04c3c8251f821ca188153cd0405c094594612
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-02-20 13:29:00 +0530

    fix(nc-gui): add banner in editMode submitted form

--- packages/nc-gui/components/general/FormBanner.vue
+++ packages/nc-gui/components/general/FormBanner.vue
@@ -6,18 +6,24 @@ const { bannerImageUrl } = defineProps<Props>()
 </script>
 
 <template>
-  <div class="w-full aspect-{4/1} bg-white border-1 border-gray-200 rounded-3xl overflow-hidden">
-    <!-- Todo: cover image uploader and image cropper to crop image in fixed aspect ratio  -->
-    <LazyNuxtImg v-if="bannerImageUrl" class="h-full" :src="bannerImageUrl" alt="form-banner'" placeholder quality="75" />
-    <div v-else class="h-full flex items-stretch justify-between">
-      <div>
+  <div
+    class="w-full max-w-screen-xl mx-auto bg-white border-1 border-gray-200 rounded-3xl overflow-hidden"
+    :style="{
+      ...(bannerImageUrl
+        ? { 'background-image': `url(${bannerImageUrl})`, 'background-size': 'cover', 'background-position': 'center' }
+        : {}),
+    }"
+  >
+    <!-- Todo: aspect ratio and cover image uploader and image cropper to crop image in fixed aspect ratio  -->
+    <div v-if="!bannerImageUrl" class="h-full flex items-stretch justify-between">
+      <div class="flex">
         <img src="~assets/img/form-banner-left.png" alt="form-banner-left'" />
       </div>
 
-      <div class="w-[91px] grid place-items-center">
-        <img class="w-full" src="~assets/img/icons/256x256.png" alt="form-banner-logo'" />
+      <div class="w-[91px] flex justify-center">
+        <img class="max-h-full self-center" src="~assets/img/icons/256x256.png" alt="form-banner-logo'" />
       </div>
-      <div>
+      <div class="flex justify-end">
         <img src="~assets/img/form-banner-right.png" alt="form-banner-left'" />
       </div>
     </div>

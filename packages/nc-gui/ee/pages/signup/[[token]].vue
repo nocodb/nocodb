bc099944e69dd392e5681d5cd7d161e51f5dc475
Pranav C <pranavxc@gmail.com>
2024-02-21 12:40:30 +0530

    refactor: remove unnecessary div wrapping

--- packages/nc-gui/ee/pages/signup/[[token]].vue
+++ packages/nc-gui/ee/pages/signup/[[token]].vue
@@ -19,13 +19,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <template v-if="appInfo.cognito">
-      <AuthCognito v-if="appInfo.cognito.aws_user_pools_id" />
-      <NuxtLayout>
-        <AuthSignup />
-      </NuxtLayout>
-    </template>
-    <span v-else />
-  </div>
+  <template v-if="appInfo.cognito">
+    <AuthCognito v-if="appInfo.cognito.aws_user_pools_id" />
+    <NuxtLayout>
+      <AuthSignup />
+    </NuxtLayout>
+  </template>
+  <span v-else />
 </template>

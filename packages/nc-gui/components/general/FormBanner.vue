4be3674404abe66b962f86b0aae7842f14cb13db
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-02-28 12:04:46 +0530

    fix(nc-gui): remove border from form banner

--- packages/nc-gui/components/general/FormBanner.vue
+++ packages/nc-gui/components/general/FormBanner.vue
@@ -8,25 +8,21 @@ const { getPossibleAttachmentSrc } = useAttachment()
 </script>
 
 <template>
-  <div
-    class="nc-form-banner-wrapper w-full mx-auto bg-white border-1 border-gray-200 rounded-2xl overflow-hidden"
-    :style="{ aspectRatio: 4 / 1 }"
-  >
+  <div class="nc-form-banner-wrapper w-full mx-auto rounded-2xl overflow-hidden" :style="{ aspectRatio: 4 / 1 }">
     <LazyCellAttachmentImage
       v-if="bannerImageUrl"
       :srcs="getPossibleAttachmentSrc(parseProp(bannerImageUrl))"
       class="nc-form-banner-image object-cover w-full"
     />
-    <!-- Todo: aspect ratio and cover image uploader and image cropper to crop image in fixed aspect ratio  -->
-    <div v-else class="h-full flex items-stretch justify-between">
-      <div class="flex">
+    <div v-else class="h-full flex items-stretch justify-between bg-white">
+      <div class="flex -mt-1">
         <img src="~assets/img/form-banner-left.png" alt="form-banner-left'" />
       </div>
 
       <div class="w-[91px] flex justify-center">
-        <img class="max-h-full self-center" src="~assets/img/icons/256x256.png" alt="form-banner-logo'" />
+        <img class="max-h-full self-center" src="~assets/img/icons/256x256.png" alt="form-banner-logo" />
       </div>
-      <div class="flex justify-end">
+      <div class="flex justify-end -mb-1">
         <img src="~assets/img/form-banner-right.png" alt="form-banner-left'" />
       </div>
     </div>

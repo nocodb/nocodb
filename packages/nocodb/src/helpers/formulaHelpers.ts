ffc07e940db57a4e0294ba72be17e55e6a0d2a50
Pranav C <pranavxc@gmail.com>
2024-03-02 12:38:13 +0530

    chore: lint

--- packages/nocodb/src/helpers/formulaHelpers.ts
+++ packages/nocodb/src/helpers/formulaHelpers.ts
@@ -2,15 +2,18 @@ import jsep from 'jsep';
 import { UITypes } from 'nocodb-sdk';
 import type FormulaColumn from '../models/FormulaColumn';
 import type { Column } from '~/models';
-import Noco from "~/Noco";
+import Noco from '~/Noco';
 
-export async function getFormulasReferredTheColumn({
-  column,
-  columns,
-}: {
-  column: Column;
-  columns: Column[];
-}, ncMeta = Noco.ncMeta): Promise<Column[]> {
+export async function getFormulasReferredTheColumn(
+  {
+    column,
+    columns,
+  }: {
+    column: Column;
+    columns: Column[];
+  },
+  ncMeta = Noco.ncMeta,
+): Promise<Column[]> {
   const fn = (pt) => {
     if (pt.type === 'CallExpression') {
       return pt.arguments.some((arg) => fn(arg));

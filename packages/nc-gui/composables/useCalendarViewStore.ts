3d9b7b6f2d92c509b5336b7507bf8a4eb4c15827
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-03-06 10:46:36 +0530

    fix(nc-gui): minor fixes

--- packages/nc-gui/composables/useCalendarViewStore.ts
+++ packages/nc-gui/composables/useCalendarViewStore.ts
@@ -733,9 +733,8 @@ const [useProvideCalendarViewStore, useCalendarViewStore] = useInjectionState(
     watch(activeCalendarView, async (value, oldValue) => {
       if (oldValue === 'week') {
         pageDate.value = selectedDate.value
-        selectedDate.value = selectedDateRange.value.start
-        selectedMonth.value = selectedDateRange.value.start
-        selectedTime.value = selectedDateRange.value.start
+        selectedMonth.value = selectedDate.value ?? selectedDateRange.value.start
+        selectedTime.value = selectedDate.value ?? selectedDateRange.value.start
       } else if (oldValue === 'month') {
         selectedDate.value = selectedMonth.value
         pageDate.value = selectedDate.value

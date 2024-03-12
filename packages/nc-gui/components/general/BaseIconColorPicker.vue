95507c1896493dae58bb0d6a7eb1e481c9728113
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-03-12 06:45:01 +0000

    fix(nc-gui): base icon predefined color lightness issue

--- packages/nc-gui/components/general/BaseIconColorPicker.vue
+++ packages/nc-gui/components/general/BaseIconColorPicker.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import tinycolor from 'tinycolor2'
 
-import { NcProjectType } from '#imports'
+import { NcProjectType, baseIconColors } from '#imports'
 
 const props = withDefaults(
   defineProps<{

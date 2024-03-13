d89eee8bb72085cfe7c3bdce0e57e7bc2b5fe9ca
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-03-13 21:16:14 +0000

    fix(nc-gui):  Invalid prop: type check failed for prop "baseRole".

--- packages/nc-gui/components/dashboard/TreeView/ProjectWrapper.vue
+++ packages/nc-gui/components/dashboard/TreeView/ProjectWrapper.vue
@@ -2,15 +2,20 @@
 import type { BaseType } from 'nocodb-sdk'
 import { ProjectInj, ProjectRoleInj } from '#imports'
 
-const props = defineProps<{
-  baseRole: string | string[]
-  base: BaseType
-}>()
+const props = withDefaults(
+  defineProps<{
+    baseRole: string | string[]
+    base: BaseType
+  }>(),
+  {
+    baseRole: '',
+  },
+)
 
 const baseRole = toRef(props, 'baseRole')
 const base = toRef(props, 'base')
 
-provide(ProjectRoleInj, baseRole)
+provide(ProjectRoleInj, baseRole ?? '')
 provide(ProjectInj, base)
 </script>
 

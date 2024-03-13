4551d1c620a9ebdb0b9ab98c072ff5d5edc1fa3e
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-03-13 21:16:15 +0000

    fix(nc-gui): small changes

--- packages/nc-gui/components/dashboard/TreeView/ProjectWrapper.vue
+++ packages/nc-gui/components/dashboard/TreeView/ProjectWrapper.vue
@@ -15,7 +15,7 @@ const props = withDefaults(
 const baseRole = toRef(props, 'baseRole')
 const base = toRef(props, 'base')
 
-provide(ProjectRoleInj, baseRole ?? '')
+provide(ProjectRoleInj, baseRole)
 provide(ProjectInj, base)
 </script>
 

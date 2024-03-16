6cc07667b1c6923eeaf8adb1ab8c401e8f32722d
Raju Udava <86527202+dstala@users.noreply.github.com>
2024-03-16 16:24:46 +0530

    Revert "feat(nc-gui): new share ui (#2521)" (#2547)
    This reverts commit 4cfdac56da1a80b83e67dd1935e959e1c0d0b692.

--- packages/nc-gui/components/nc/Switch.vue
+++ packages/nc-gui/components/nc/Switch.vue
@@ -1,14 +1,11 @@
 <script lang="ts" setup>
-const props = withDefaults(
-  defineProps<{ checked: boolean; disabled?: boolean; size?: 'default' | 'small'; loading?: boolean }>(),
-  {
-    size: 'small',
-  },
-)
-const emit = defineEmits(['change', 'update:checked'])
-const checked = useVModel(props, 'checked', emit)
+const props = withDefaults(defineProps<{ checked: boolean; disabled?: boolean; size?: 'default' | 'small' }>(), {
+  size: 'small',
+})
 
-const loading = computed(() => props.loading)
+const emit = defineEmits(['change', 'update:checked'])
+
+const checked = useVModel(props, 'checked', emit)
 
 const onChange = (e: boolean) => {
   emit('change', e)
@@ -16,15 +13,7 @@ const onChange = (e: boolean) => {
 </script>
 
 <template>
-  <a-switch
-    v-model:checked="checked"
-    :disabled="disabled"
-    :loading="loading"
-    :size="size"
-    class="nc-switch"
-    v-bind="$attrs"
-    @change="onChange"
-  >
+  <a-switch v-model:checked="checked" :disabled="disabled" class="nc-switch" v-bind="$attrs" :size="size" @change="onChange">
   </a-switch>
   <span v-if="$slots.default" class="cursor-pointer pl-2" @click="checked = !checked">
     <slot />

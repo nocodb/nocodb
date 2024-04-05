681487813cf46aaec421d6bc0976c22d0cbf1c2c
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-04-05 04:57:41 +0000

    fix(nc-gui): revert use of escape any number of times should not make selected cell go away

--- packages/nc-gui/composables/useMultiSelect/index.ts
+++ packages/nc-gui/composables/useMultiSelect/index.ts
@@ -767,6 +767,9 @@ export function useMultiSelect(
           // Handle escape
           if (e.key === 'Escape') {
             selectedRange.clear()
+
+            activeCell.col = null
+            activeCell.row = null
           }
 
           if (unref(editEnabled) || e.ctrlKey || e.altKey || e.metaKey) {

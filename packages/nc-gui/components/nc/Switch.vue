b0ff3eff57e338428de07af67f71e07cc2f9bb65
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-04-08 09:16:29 +0000

    fix(nc-gui): use computed value for switch size

--- packages/nc-gui/components/nc/Switch.vue
+++ packages/nc-gui/components/nc/Switch.vue
@@ -7,13 +7,25 @@ const emit = defineEmits(['change', 'update:checked'])
 
 const checked = useVModel(props, 'checked', emit)
 
+const switchSize = computed(() => (['default', 'small'].includes(props.size) ? props.size : undefined))
+
 const onChange = (e: boolean) => {
   emit('change', e)
 }
 </script>
 
 <template>
-  <a-switch v-model:checked="checked" :disabled="disabled" class="nc-switch" v-bind="$attrs" :size="size" @change="onChange">
+  <a-switch
+    v-model:checked="checked"
+    :disabled="disabled"
+    class="nc-switch"
+    :class="{
+      'size-xsmall': size === 'xsmall',
+    }"
+    v-bind="$attrs"
+    :size="switchSize"
+    @change="onChange"
+  >
   </a-switch>
   <span v-if="$slots.default" class="cursor-pointer pl-2" @click="checked = !checked">
     <slot />

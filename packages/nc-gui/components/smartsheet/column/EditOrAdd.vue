fec720187d102fcb5c6e74abb0c6e8cfb1c3d735
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-04-12 12:37:50 +0530

    fix(nc-gui): On `Add Field`/`Edit Field` in form view - pre-select the placeholder field name (#2713)
    * fix(nc-gui): focus & select title text on addOrEdit modal open in form builder
    
    * refactor(nc-gui): focus add or edit field placeholder input code

--- packages/nc-gui/components/smartsheet/column/EditOrAdd.vue
+++ packages/nc-gui/components/smartsheet/column/EditOrAdd.vue
@@ -208,6 +208,13 @@ onMounted(() => {
         emit('add', formState.value)
       }
     }
+
+    if (isForm.value && !props.fromTableExplorer) {
+      setTimeout(() => {
+        antInput.value?.focus()
+        antInput.value?.select()
+      }, 100)
+    }
   })
 })
 

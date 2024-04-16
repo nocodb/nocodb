61eafe0227f5674f474e617615296fd3e66604bc
Pranav C <pranavxc@gmail.com>
2024-04-16 07:05:15 +0000

    chore: lint

--- packages/nocodb/src/models/GridViewColumn.ts
+++ packages/nocodb/src/models/GridViewColumn.ts
@@ -100,7 +100,7 @@ export default class GridViewColumn implements GridColumnType {
       insertObj.source_id = viewRef.source_id;
     }
 
-    const { id, fk_column_id } = await ncMeta.metaInsert2(
+    const { id } = await ncMeta.metaInsert2(
       null,
       null,
       MetaTable.GRID_VIEW_COLUMNS,

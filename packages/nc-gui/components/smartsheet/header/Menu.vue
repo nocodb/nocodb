be77f75d899a0a6fe6711b5c9b0610492c5e1482
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-04-17 21:36:42 +0530

    fix: removed unwanted change

--- packages/nc-gui/components/smartsheet/header/Menu.vue
+++ packages/nc-gui/components/smartsheet/header/Menu.vue
@@ -519,7 +519,7 @@ const filterOrGroupByThisField = (event: SmartsheetStoreEvents) => {
             {{ t('general.insertAfter') }}
           </div>
         </NcMenuItem>
-        <NcMenuItem v-if="!column?.pk" @click="onInsertBefore">
+        <NcMenuItem v-if="!column?.pv" @click="onInsertBefore">
           <div v-e="['a:field:insert:before']" class="nc-column-insert-before nc-header-menu-item">
             <component :is="iconMap.colInsertBefore" class="text-gray-600 !w-4.5 !h-4.5" />
             <!-- Insert Before -->

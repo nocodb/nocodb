7514de824fc897f0c5de122b99f9aca32625afb1
Raju Udava <86527202+dstala@users.noreply.github.com>
2024-04-26 07:14:00 +0000

    Update index.ts

--- tests/playwright/pages/Dashboard/ExpandedForm/index.ts
+++ tests/playwright/pages/Dashboard/ExpandedForm/index.ts
@@ -130,7 +130,6 @@ export class ExpandedFormPage extends BasePage {
     // removing focus from toast
     await this.rootPage.locator('.nc-modal').click();
     await this.get().press('Escape');
-    await this.get().press('Escape');
     await this.get().waitFor({ state: 'hidden' });
   }
 

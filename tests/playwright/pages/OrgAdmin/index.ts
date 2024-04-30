df81763a0387ff8e1653cbf570e0a03cb1416451
Pranav C <pranavxc@gmail.com>
2024-04-30 19:27:48 +0300

    test: playwright

--- tests/playwright/pages/OrgAdmin/index.ts
+++ tests/playwright/pages/OrgAdmin/index.ts
@@ -1,6 +1,9 @@
 import { Page } from '@playwright/test';
 import BasePage from '../Base';
 import { SSOPage } from './SSOPage';
+import { SidebarUserMenuObject } from '../Dashboard/Sidebar/UserMenu';
+import { SidebarPage } from '../Dashboard/Sidebar';
+import { DashboardPage } from '../Dashboard';
 
 export class AccountPage extends BasePage {
   readonly ssoPage: SSOPage;
@@ -22,5 +25,12 @@ export class AccountPage extends BasePage {
     await this.openAppMenu();
     await this.rootPage.locator('div.nc-account-dropdown-item:has-text("Sign Out"):visible').click();
     await this.rootPage.locator('[data-testid="nc-form-signin"]:visible').waitFor();
+  }
+
+  async goto() {
+    await this.rootPage.goto('/');
+
+    const dashboardPage = new DashboardPage(this.rootPage);
+    await dashboardPage.sidebar.userMenu.click();
   }
 }

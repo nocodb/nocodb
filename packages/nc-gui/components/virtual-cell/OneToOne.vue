03c1e429aaaa4e0b2ae99129555a480f2254abbc
Pranav C <pranavxc@gmail.com>
2024-05-06 06:39:21 +0000

    refactor: use variables to improve readability

--- packages/nc-gui/components/virtual-cell/OneToOne.vue
+++ packages/nc-gui/components/virtual-cell/OneToOne.vue
@@ -55,9 +55,9 @@ const value = computed(() => {
   if (cellValue?.value) {
     return cellValue?.value
   } else if (isNew.value) {
-    return Array.isArray(state?.value?.[column?.value.title as string])
-      ? state?.value?.[column?.value.title as string][0]
-      : state?.value?.[column?.value.title as string]
+    const columnTitle = column?.value.title as string
+    const columnValue = state?.value?.[columnTitle]
+    return Array.isArray(columnValue) ? columnValue[0] : columnValue
   }
   return null
 })

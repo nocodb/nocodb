76f390e06fe6163951696a100793636062f26055
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-05-08 10:49:11 +0000

    feat(nc-gui): resizable link record dropdown

--- packages/nc-gui/components/virtual-cell/components/LinkRecordDropdown.vue
+++ packages/nc-gui/components/virtual-cell/components/LinkRecordDropdown.vue
@@ -68,7 +68,7 @@ watch([ncLinksDropdownRef, isOpen], () => {
   >
     <slot />
     <template #overlay>
-      <div ref="ncLinksDropdownRef" class="h-[412px] w-[540px]" :class="`${randomClass}`">
+      <div ref="ncLinksDropdownRef" class="h-[412px] w-[540px] nc-links-dropdown-wrapper" :class="`${randomClass}`">
         <slot name="overlay" />
       </div>
     </template>
@@ -82,5 +82,15 @@ watch([ncLinksDropdownRef, isOpen], () => {
 }
 .nc-link-dropdown-root {
   z-index: 1000;
+}
+
+.nc-links-dropdown-wrapper {
+  @apply h-[412px] w-[540px];
+  overflow-y: auto;
+  overflow-x: hidden;
+  resize: vertical;
+  min-height: 412px;
+  max-height: 700px;
+  max-width: 540px;
 }
 </style>

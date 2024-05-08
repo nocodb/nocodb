7340262276d8995bbb71a1c4ea5448b0d462c7c2
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-05-08 10:49:10 +0000

    fix(nc-gui): update grid row hover & selected state bg color

--- packages/nc-gui/components/smartsheet/grid/Table.vue
+++ packages/nc-gui/components/smartsheet/grid/Table.vue
@@ -1892,6 +1892,7 @@ onKeyStroke('ArrowDown', onDown)
                       :class="{
                         'active-row': activeCell.row === rowIndex || selectedRange._start?.row === rowIndex,
                         'mouse-down': isGridCellMouseDown || isFillMode,
+                        'selected-row': row.rowMeta.selected,
                       }"
                       :style="{ height: rowHeight ? `${rowHeightInPx[`${rowHeight}`]}px` : `${rowHeightInPx['1']}px` }"
                       :data-testid="`grid-row-${rowIndex}`"
@@ -2072,22 +2073,7 @@ onKeyStroke('ArrowDown', onDown)
                         :data-col-index="colIndex"
                         @mousedown="handleMouseDown($event, rowIndex, colIndex)"
                         @mouseover="handleMouseOver($event, rowIndex, colIndex)"
-                        @click="
-                          ($event) => {
-                            handleCellClick($event, rowIndex, colIndex)
-                            console.log(
-                              'fdsa',
-                              colMeta[colIndex],
-                              (colMeta[colIndex].isLookup ||
-                                colMeta[colIndex].isRollup ||
-                                colMeta[colIndex].isFormula ||
-                                colMeta[colIndex].isCreatedOrLastModifiedTimeCol ||
-                                colMeta[colIndex].isCreatedOrLastModifiedByCol) &&
-                                hasEditPermission &&
-                                selectRangeMap[`${rowIndex}-${colIndex}`],
-                            )
-                          }
-                        "
+                        @click="handleCellClick($event, rowIndex, colIndex)"
                         @dblclick="makeEditable(row, columnObj)"
                         @contextmenu="showContextMenu($event, { row: rowIndex, col: colIndex })"
                       >
@@ -2445,7 +2431,7 @@ onKeyStroke('ArrowDown', onDown)
 
   td,
   th {
-    @apply border-gray-100 border-solid border-r bg-gray-50 p-0;
+    @apply border-gray-100 border-solid border-r bg-gray-100 p-0;
     min-height: 32px !important;
     height: 32px !important;
     position: relative;
@@ -2700,9 +2686,18 @@ onKeyStroke('ArrowDown', onDown)
       @apply !xs:hidden flex;
     }
 
+    &:not(.selected-row) {
+      td.nc-grid-cell:not(.active),
+      td:nth-child(2):not(.active) {
+        @apply !bg-gray-50 border-b-gray-200 border-r-gray-200;
+      }
+    }
+  }
+
+  &.selected-row {
     td.nc-grid-cell:not(.active),
     td:nth-child(2):not(.active) {
-      @apply !bg-gray-50 border-b-gray-200 border-r-gray-200;
+      @apply !bg-[#F0F3FF] border-b-gray-200 border-r-gray-200;
     }
   }
 }

5af03f906f62a32dab8cdb109519e51c1d823e56
Anbarasu <anbarasun123@gmail.com>
2024-05-09 22:00:47 +0530

    feat: sort group-by by count (#2816)
    * feat: group-by count sort
    
    * fix: refactor
    
    * fix: add new row requires manual pageload
    
    * fix: use + for ascending sort
    
    * fix: use tilde instead of count
    
    * fix: groupby sort count not working in shared view
    
    * fix: update group row count
    
    * fix: remove imports

--- packages/nc-gui/utils/sortUtils.ts
+++ packages/nc-gui/utils/sortUtils.ts
@@ -1,6 +1,13 @@
 import { UITypes } from 'nocodb-sdk'
 
-export const getSortDirectionOptions = (uidt: UITypes | string) => {
+export const getSortDirectionOptions = (uidt: UITypes | string, isGroupBy?: boolean) => {
+  const groupByOptions = isGroupBy
+    ? [
+        { text: 'Count (9 → 1)', value: 'count-desc' },
+        { text: 'Count (1 → 9)', value: 'count-asc' },
+      ]
+    : []
+
   switch (uidt) {
     case UITypes.Year:
     case UITypes.Number:
@@ -20,16 +27,16 @@ export const getSortDirectionOptions = (uidt: UITypes | string) => {
       return [
         { text: '1 → 9', value: 'asc' },
         { text: '9 → 1', value: 'desc' },
-      ]
+      ].concat(groupByOptions)
     case UITypes.Checkbox:
       return [
         { text: '▢ → ✓', value: 'asc' },
         { text: '✓ → ▢', value: 'desc' },
-      ]
+      ].concat(groupByOptions)
     default:
       return [
         { text: 'A → Z', value: 'asc' },
         { text: 'Z → A', value: 'desc' },
-      ]
+      ].concat(groupByOptions)
   }
 }

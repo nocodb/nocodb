5fb2fce18f2071bbf3341a96519690a47576b534
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-05-09 10:33:25 +0000

    fix(nc-gui): currency validation input cell prefix issue

--- packages/nc-gui/components/cell/Currency.vue
+++ packages/nc-gui/components/cell/Currency.vue
@@ -4,6 +4,7 @@ import type { VNodeRef } from '@vue/runtime-core'
 interface Props {
   modelValue: number | null | undefined
   placeholder?: string
+  hidePrefix?: boolean
 }
 
 const props = defineProps<Props>()
@@ -93,7 +94,7 @@ onMounted(() => {
 
 <template>
   <div
-    v-if="isForm && !isEditColumn"
+    v-if="isForm && !isEditColumn && !hidePrefix"
     class="nc-currency-code h-full !bg-gray-100 border-r border-gray-200 px-3 mr-1 flex items-center"
   >
     <span>

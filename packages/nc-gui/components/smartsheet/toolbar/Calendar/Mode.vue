35d2909c7a40f7a756dbff166ee58eab232d4782
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-05-14 04:50:02 +0000

    test: fix tests

--- packages/nc-gui/components/smartsheet/toolbar/Calendar/Mode.vue
+++ packages/nc-gui/components/smartsheet/toolbar/Calendar/Mode.vue
@@ -49,7 +49,7 @@ watch(activeCalendarView, () => {
 
   <div v-else>
     <NcDropdown :trigger="['click']">
-      <NcButton size="small" type="secondary">
+      <NcButton data-testid="nc-calendar-view-mode" size="small" type="secondary">
         {{ $t(`objects.${activeCalendarView}`) }}
         <component :is="iconMap.arrowDown" />
       </NcButton>
--- tests/playwright/pages/Dashboard/common/Toolbar/CalendarViewMode.ts
+++ tests/playwright/pages/Dashboard/common/Toolbar/CalendarViewMode.ts
@@ -12,7 +12,16 @@ export class ToolbarCalendarViewModePage extends BasePage {
     return this.rootPage.getByTestId('nc-calendar-view-mode');
   }
 
-  async changeCalendarView({ title }: { title: string }) {
-    await this.get().getByTestId(`nc-calendar-view-mode-${title}`).click();
+  async changeCalendarView({ title: _title }: { title: string }) {
+    await this.get().click();
+    await this.rootPage.waitForTimeout(500);
+
+    const title = _title[0].toUpperCase() + _title.slice(1);
+
+    await this.rootPage.locator(`.nc-menu-item-inner`).getByText(title).click({
+      force: true,
+    });
+    await this.rootPage.waitForTimeout(500);
+    // await this.get().getByTestId(`nc-calendar-view-mode-${title}`).click();
   }
 }

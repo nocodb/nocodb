ac7112963b3b24947dfb90007d29be8c9627e1f3
Salim B <git@salim.space>
2024-05-15 23:50:08 +0200

    feat: expose Litestream configuration

--- packages/nocodb/docker/start-litestream.sh
+++ packages/nocodb/docker/start-litestream.sh
@@ -6,21 +6,21 @@ if [ -n "${NC_TOOL_DIR}" ]; then
   mkdir -p "$NC_TOOL_DIR"
 fi
 
-if [ -n "${AWS_ACCESS_KEY_ID}" ] && [ -n "${AWS_SECRET_ACCESS_KEY}" ] && [ -n "${AWS_BUCKET}" ] && [ -n "${AWS_BUCKET_PATH}" ]; then
+if [ -n "${LITESTREAM_S3_ENDPOINT}" ] && [ -n "${LITESTREAM_S3_BUCKET}" ] && [ -n "${LITESTREAM_ACCESS_KEY_ID}" ] && [ -n "${LITESTREAM_SECRET_ACCESS_KEY}" ] ; then
 
-  if [ -f "${NC_TOOL_DIR}noco.db" ]
-  then
+  if [ -f "${NC_TOOL_DIR}noco.db" ] ; then
     rm "${NC_TOOL_DIR}noco.db"
     rm "${NC_TOOL_DIR}noco.db-shm"
     rm "${NC_TOOL_DIR}noco.db-wal"
   fi
 
-  litestream restore -o "${NC_TOOL_DIR}noco.db" "s3://$AWS_BUCKET/$AWS_BUCKET_PATH"
-  if [ ! -f "${NC_TOOL_DIR}noco.db" ]
-  then
+  litestream restore "${NC_TOOL_DIR}noco.db"
+
+  if [ ! -f "${NC_TOOL_DIR}noco.db" ] ; then
     touch "${NC_TOOL_DIR}noco.db"
   fi
-  litestream replicate "${NC_TOOL_DIR}noco.db" "s3://$AWS_BUCKET/$AWS_BUCKET_PATH" &
+
+  litestream replicate &
 fi
 
 node docker/main.js

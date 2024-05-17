4bf65e29d90ad92a98bafd5fdfdf69a8c1b4d67a
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-05-17 12:18:16 +0000

    fix(nc-gui): disable undo redo if expanded form is open

--- packages/nc-gui/utils/browserUtils.ts
+++ packages/nc-gui/utils/browserUtils.ts
@@ -2,6 +2,7 @@
 export const isMac = () => /Mac/i.test(navigator.platform)
 export const isDrawerExist = () => document.querySelector('.ant-drawer-open')
 export const isDrawerOrModalExist = () => document.querySelector('.ant-modal.active, .ant-drawer-open')
+export const isExpandedFormOpen = () => document.querySelector('.nc-drawer-expanded-form.active')
 export const isExpandedCellInputExist = () => document.querySelector('.expanded-cell-input')
 export const cmdKActive = () => document.querySelector('.cmdk-modal-active')
 

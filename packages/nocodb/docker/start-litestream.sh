ed84aedf2cbcaf62bf90212ddf45167f9fc2585a
Salim B <git@salim.space>
2024-05-19 02:02:27 +0200

    feat: support Litestream age encryption

--- packages/nocodb/docker/start-litestream.sh
+++ packages/nocodb/docker/start-litestream.sh
@@ -19,19 +19,38 @@ use_litestream() {
 
 if use_litestream ; then
 
+  # enable age encryption in Litestream config if indicated
+  LITESTREAM_CONFIG_PATH='/etc/litestream.yml'
+
+  if [ -n "${LITESTREAM_AGE_PUBLIC_KEY}" ] \
+  && [ -n "${LITESTREAM_AGE_SECRET_KEY}" ] \
+  && ! dasel --file "${LITESTREAM_CONFIG_PATH}" --read yaml 'dbs.first().replicas.first().age' > /dev/null 2>&1 ; then
+    # shellcheck disable=SC2016
+    dasel put --file "${LITESTREAM_CONFIG_PATH}" \
+              --read yaml \
+              --type json \
+              --value '{ "identities": [ "${LITESTREAM_AGE_SECRET_KEY}" ], "recipients": [ "${LITESTREAM_AGE_PUBLIC_KEY}" ] }' \
+              --selector 'dbs.first().replicas.first().age'
+  fi
+
+  # remove any possible local DB leftovers
   if [ -f "${NC_TOOL_DIR}noco.db" ] ; then
     rm "${NC_TOOL_DIR}noco.db"
     rm -f "${NC_TOOL_DIR}noco.db-shm"
     rm -f "${NC_TOOL_DIR}noco.db-wal"
   fi
 
+  # restore DB from Litestream replica
   litestream restore "${NC_TOOL_DIR}noco.db"
 
+  # create empty DB file if no Litestream replica exists
   if [ ! -f "${NC_TOOL_DIR}noco.db" ] ; then
     touch "${NC_TOOL_DIR}noco.db"
   fi
 
+  # start Litestream replication
   litestream replicate &
 fi
 
+# start NocoDB
 node docker/main.js

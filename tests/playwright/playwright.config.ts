4ccc2686c04ebff65b066e0c8597d0bcc1338518
Raju Udava <86527202+dstala@users.noreply.github.com>
2024-05-21 21:48:16 +0530

    test: enable fullyParallel mode
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     timeout: 14000,
   },
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,

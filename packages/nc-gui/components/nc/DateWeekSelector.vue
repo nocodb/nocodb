72e1e249980d3de4793caa8b097885063bee8101
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-05-23 11:23:54 +0000

    fix(nc-gui): small changes

--- packages/nc-gui/components/nc/DateWeekSelector.vue
+++ packages/nc-gui/components/nc/DateWeekSelector.vue
@@ -213,7 +213,9 @@ const paginate = (action: 'next' | 'prev') => {
         </span>
       </div>
       <div v-if="isCellInputField" class="flex items-center justify-center px-2 pb-2 pt-1">
-        <NcButton class="!h-7" size="small" type="secondary" @click="handleSelectDate(dayjs())"> Today </NcButton>
+        <NcButton class="!h-7" size="small" type="secondary" @click="handleSelectDate(dayjs())">
+          <span class="text-small"> {{ $t('labels.today') }} </span>
+        </NcButton>
       </div>
     </div>
   </div>

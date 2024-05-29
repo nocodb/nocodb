8122106ab00ca562c0a83680a75956a156767231
mertmit <mertmit99@gmail.com>
2024-05-29 11:41:22 +0300

    fix: use generic id for at imported bases
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/modules/jobs/jobs/at-import/at-import.processor.ts
+++ packages/nocodb/src/modules/jobs/jobs/at-import/at-import.processor.ts
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import { UITypes } from 'nocodb-sdk';
+import { SqlUiFactory, UITypes } from 'nocodb-sdk';
 import Airtable from 'airtable';
 import hash from 'object-hash';
 import dayjs from 'dayjs';
@@ -519,14 +519,18 @@ export class AtImportProcessor {
         table.table_name = uniqueTableNameGen(sanitizedName);
 
         table.columns = [];
+
+        const source = await Source.get(syncDB.sourceId);
+
+        const sqlUi = SqlUiFactory.create({ client: source.type });
+
         const sysColumns = [
+          ...sqlUi.getNewTableColumns().filter((c) => c.column_name === 'id'),
           {
             title: ncSysFields.id,
             column_name: ncSysFields.id,
-            uidt: UITypes.ID,
-            meta: {
-              ag: 'nc',
-            },
+            uidt: UITypes.SingleLineText,
+            system: true,
           },
           {
             title: ncSysFields.hash,
@@ -535,6 +539,8 @@ export class AtImportProcessor {
             system: true,
           },
         ];
+
+        table.columns.push(...sysColumns);
 
         for (let j = 0; j < tblSchema[i].columns.length; j++) {
           const col = tblSchema[i].columns[j];
@@ -605,8 +611,6 @@ export class AtImportProcessor {
           }
           table.columns.push(ncCol);
         }
-        table.columns.push(sysColumns[0]);
-        table.columns.push(sysColumns[1]);
 
         tables.push(table);
       }
@@ -2556,7 +2560,6 @@ export class AtImportProcessor {
           data: { error: e.message },
         });
         logger.log(e);
-        throw new Error(e.message);
       }
       throw e;
     }

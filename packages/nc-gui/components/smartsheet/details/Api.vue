12ed9a13c3efaa99aa9b9bf6e063994192eb980f
Raju Udava <86527202+dstala@users.noreply.github.com>
2024-05-30 19:26:06 +0530

    fix: review comments
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- packages/nc-gui/components/smartsheet/details/Api.vue
+++ packages/nc-gui/components/smartsheet/details/Api.vue
@@ -76,7 +76,7 @@ const snippet = computed(
       headers: [
         {
           name: 'xc-token',
-          value: `<CREATE YOUR API TOKEN FROM : ${location.origin + location.pathname}#/account/tokens>`,
+          value: `CREATE_YOUR_API_TOKEN_FROM ${location.origin + location.pathname}#/account/tokens`,
           comment: 'API token',
         },
       ],
@@ -101,7 +101,7 @@ const code = computed(() => {
 const api = new Api({
   baseURL: "${(appInfo.value && appInfo.value.ncSiteUrl) || '/'}",
   headers: {
-    "xc-token": "<CREATE YOUR API TOKEN FROM : ${location.origin + location.pathname}#/account/tokens>"
+    "xc-token": "CREATE_YOUR_API_TOKEN_FROM ${location.origin + location.pathname}#/account/tokens"
   }
 })
 

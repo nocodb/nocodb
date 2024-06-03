01a5f01853e4dda5aac3f06b0ac335a347cd489b
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-06-03 06:05:56 +0000

    fix(nc-gui): shared view download btn text color

--- packages/nc-gui/components/smartsheet/toolbar/Export.vue
+++ packages/nc-gui/components/smartsheet/toolbar/Export.vue
@@ -31,9 +31,9 @@ useProvideCalendarViewStore(meta, sharedView, true, nestedFilters)
 <template>
   <NcDropdown :trigger="['click']" overlay-class-name="nc-dropdown-actions-menu">
     <NcButton v-e="['c:actions']" class="nc-actions-menu-btn nc-toolbar-btn" size="xs" type="secondary">
-      <div class="flex gap-2 items-center">
-        <component :is="iconMap.download" class="group-hover:text-accent text-gray-500" />
-        <span class="text-capitalize !text-sm font-medium text-gray-500 xs:hidden">{{ $t('general.download') }}</span>
+      <div class="flex gap-2 items-center text-gray-700">
+        <component :is="iconMap.download" class="group-hover:text-accent" />
+        <span class="text-capitalize !text-sm font-medium xs:hidden">{{ $t('general.download') }}</span>
         <component :is="iconMap.arrowDown" class="text-grey" />
       </div>
     </NcButton>

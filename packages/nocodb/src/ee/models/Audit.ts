baaa56ea13234f01c4bdcbcabfdc3767524e1ba2
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-06-17 21:42:43 +0530

    feat(nocodb): add filter & search query params

--- packages/nocodb/src/ee/models/Audit.ts
+++ packages/nocodb/src/ee/models/Audit.ts
@@ -8,25 +8,69 @@ export default class Audit extends AuditCE {
     {
       limit = 25,
       offset = 0,
+      user,
+      base,
+      type,
+      subType,
+      startDate,
+      endDate,
+      search,
     }: {
       limit?: number;
       offset?: number;
       sourceId?: string;
+      user?: string;
+      base?: string;
+      type?: string;
+      subType?: string;
+      startDate?: string;
+      endDate?: string;
+      search?: string;
     },
   ) {
+    // Initialize the condition object
+    const condition: any = { fk_workspace_id: workspaceId };
+
+    // Build the condition dynamically
+    const additionalConditions = [
+      user && { user: user },
+      base && { base_id: base },
+      type && { op_type: type },
+      subType && { op_sub_type: subType },
+    ].filter(Boolean);
+
+    // Merge additional conditions into the main condition object
+    additionalConditions.forEach((cond) => Object.assign(condition, cond));
+
     return await Noco.ncMeta.metaList2(
       RootScopes.ROOT,
       RootScopes.ROOT,
       MetaTable.AUDIT,
       {
-        condition: {
-          fk_workspace_id: workspaceId,
-        },
-        orderBy: {
-          created_at: 'desc',
-        },
         limit,
         offset,
+        condition,
+        orderBy: { created_at: 'desc' },
+        xcCondition: {
+          _or: [
+            ...(search
+              ? [
+                  {
+                    user: {
+                      like: `%${search}%`,
+                    },
+                  },
+                  {
+                    description: {
+                      like: `%${search}%`,
+                    },
+                  },
+                ]
+              : []),
+            ...(startDate ? [{ created_at: { ge: startDate } }] : []),
+            ...(endDate ? [{ created_at: { le: endDate } }] : []),
+          ],
+        },
       },
     );
   }

ac07b53c916b584b676e01061fd374151963f9da
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-06-22 12:13:48 +0530

    fix: Formula modal UI/UX (#3207)
    * fix(nc-gui): small changes
    
    * fix(nc-gui): formula suggestion popover visibility issue in MFE
    
    * fix(nc-gui): console warnings
    
    * fix(nc-gui): formula field madal width issue
    
    * fix(nc-gui): formula fun or field empty state

--- packages/nc-gui/components/smartsheet/column/EditOrAdd.vue
+++ packages/nc-gui/components/smartsheet/column/EditOrAdd.vue
@@ -360,8 +360,8 @@ const isFullUpdateAllowed = computed(() => {
     :class="{
       'bg-white': !props.fromTableExplorer,
       'w-[384px]': !props.embedMode,
-      'min-w-500px': formState.uidt === UITypes.LinkToAnotherRecord || formState.uidt === UITypes.Links,
-      '!w-116 overflow-visible': formState.uidt === UITypes.Formula && !props.embedMode,
+      'min-w-[500px]': formState.uidt === UITypes.LinkToAnotherRecord || formState.uidt === UITypes.Links,
+      'overflow-visible': formState.uidt === UITypes.Formula,
       '!w-[600px]': formState.uidt === UITypes.LinkToAnotherRecord || formState.uidt === UITypes.Links,
       'shadow-lg border-1 border-gray-200 shadow-gray-300 rounded-xl p-5': !embedMode,
     }"

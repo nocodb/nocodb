acdd894b2c503a28cbf0ae3dda89cb5960e799b9
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-06-24 08:28:51 +0000

    feat(nc-gui): filter audit logs base on date range

--- packages/nc-gui/lib/types.ts
+++ packages/nc-gui/lib/types.ts
@@ -1,8 +1,20 @@
-import type { BaseType, ColumnType, FilterType, MetaType, PaginatedType, Roles, RolesObj, TableType, ViewTypes } from 'nocodb-sdk'
+import type {
+  AuditOperationSubTypes,
+  AuditOperationTypes,
+  BaseType,
+  ColumnType,
+  FilterType,
+  MetaType,
+  PaginatedType,
+  Roles,
+  RolesObj,
+  TableType,
+  ViewTypes,
+} from 'nocodb-sdk'
 import type { I18n } from 'vue-i18n'
 import type { Theme as AntTheme } from 'ant-design-vue/es/config-provider'
 import type { UploadFile } from 'ant-design-vue'
-import type { ImportSource, ImportType, PreFilledMode, TabType } from './enums'
+import type { AuditLogsDateRange, ImportSource, ImportType, PreFilledMode, TabType } from './enums'
 import type { rolePermissions } from './acl'
 
 interface User {
@@ -226,6 +238,23 @@ interface ImageCropperConfig {
   imageRestriction?: 'fill-area' | 'fit-area' | 'stencil' | 'none'
 }
 
+interface AuditLogsQuery {
+  type?: AuditOperationTypes
+  subType?: AuditOperationSubTypes
+  base?: string
+  user?: string
+  search?: string
+  sourceId?: string
+  startDate?: string
+  endDate?: string
+  dateRange?: AuditLogsDateRange
+  dateRangeLabel?: string
+  orderBy: {
+    created_at?: 'asc' | 'desc'
+    user?: 'asc' | 'desc'
+  }
+}
+
 export type {
   User,
   ProjectMetaInfo,
@@ -256,4 +285,5 @@ export type {
   CalendarRangeType,
   FormFieldsLimitOptionsType,
   ImageCropperConfig,
+  AuditLogsQuery,
 }

76c3d64728801c07f14eec65ec29a70249d220d2
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-06-28 12:00:49 +0000

    fix(nc-gui): audit record access issue for oss base creator

--- packages/nc-gui/composables/useExpandedFormStore.ts
+++ packages/nc-gui/composables/useExpandedFormStore.ts
@@ -174,7 +174,7 @@ const [useProvideExpandedFormStore, useExpandedFormStore] = useInjectionState((m
   }
 
   const loadAudits = async (_rowId?: string) => {
-    if (!isUIAllowed('auditList') || (!row.value && !_rowId)) return
+    if (!isUIAllowed('baseAuditList') || (!row.value && !_rowId)) return
 
     const rowId = _rowId ?? extractPkFromRow(row.value.row, meta.value.columns as ColumnType[])
 

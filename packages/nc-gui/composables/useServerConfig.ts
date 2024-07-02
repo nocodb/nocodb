3db3286a72e40a3127d58669a9e48ac98f74eac3
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-07-02 14:02:51 +0000

    feat: maintenance alert ui

---
+++ packages/nc-gui/composables/useServerConfig.ts
@@ -0,0 +1,15 @@
+const useServerConfig = () => {
+  const getConfig = async () => {}
+
+  const checkMaintenance = async () => {}
+
+  const dismissMaintenance = () => {}
+
+  return {
+    getConfig,
+    checkMaintenance,
+    dismissMaintenance,
+  }
+}
+
+export default useServerConfig

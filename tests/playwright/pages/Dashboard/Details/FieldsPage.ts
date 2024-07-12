711ef06f48ec3a7d9d85e7293178528a85eab1d8
Raju Udava <86527202+dstala@users.noreply.github.com>
2024-07-12 18:22:05 +0000

    test: fix mm-hm order

--- tests/playwright/pages/Dashboard/Details/FieldsPage.ts
+++ tests/playwright/pages/Dashboard/Details/FieldsPage.ts
@@ -208,6 +208,10 @@ export class FieldsPage extends BasePage {
         break;
       case 'Links':
         await this.addOrEditColumn.locator('.nc-ltar-relation-type').getByTestId(relationType).click();
+        // await this.addOrEditColumn
+        //   .locator('.nc-ltar-relation-type >> .ant-radio')
+        //   .nth(relationType === 'Has Many' ? 1 : 0)
+        //   .click();
         await this.addOrEditColumn.locator('.ant-select-single').nth(1).click();
         await this.rootPage.locator(`.nc-ltar-child-table >> input[type="search"]`).first().fill(childTable);
         await this.rootPage

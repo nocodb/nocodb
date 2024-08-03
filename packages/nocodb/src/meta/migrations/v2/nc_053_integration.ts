dabdcf6c2ca3523c16442f72e1a9327ecac9ea64
Pranav C <pranavxc@gmail.com>
2024-08-03 12:15:21 +0000

    feat: updated migration

--- packages/nocodb/src/meta/migrations/v2/nc_053_integration.ts
+++ packages/nocodb/src/meta/migrations/v2/nc_053_integration.ts
@@ -1,23 +1,70 @@
+import { ProjectRoles } from 'nocodb-sdk';
 import type { Knex } from 'knex';
 import { MetaTable } from '~/utils/globals';
 
 const up = async (knex: Knex) => {
-  await knex.schema.createTable(MetaTable.BASE_SOURCES, (table) => {
-    table.string('base_id', 120).index();
-    table.string('source_id', 20).index();
+  await knex.schema.createTable(MetaTable.INTEGRATIONS, (table) => {
+    table.string('id', 20).primary();
+    table.string('fk_workspace_id', 20).index();
+    table.text('config');
+    table.text('meta');
+    table.string('type', 20).index();
+
+    table.boolean('is_private').defaultTo(false);
+
+    table.string('created_by', 20).index();
+
     table.timestamps(true, true);
   });
-
-  // create source project mapping by using sql query
-  knex(MetaTable.BASE_SOURCES).insert(
-    knex.select('id as source_id', 'base_id as base_id').from(MetaTable.BASES),
-  );
 
   await knex.schema.alterTable(MetaTable.BASES, (table) => {
-    table.string('base_id', 120).index();
-    table.string('source_id', 20).index();
-    table.timestamps(true, true);
+    table.string('fk_integration_id', 20).index();
   });
+
+  // get all external sources, add them to integrations table and map them to bases
+  const sources = await knex(MetaTable.BASES)
+    .select(`${MetaTable.BASES}.*`)
+    .select(`${MetaTable.PROJECT_USERS}.user_id as created_by`)
+    .innerJoin(
+      MetaTable.PROJECT,
+      `${MetaTable.BASES}.base_id`,
+      `${MetaTable.PROJECT}.id`,
+    )
+    .where((qb) =>
+      qb
+        .where(`${MetaTable.BASES}.is_meta`, false)
+        .orWhereNull(`${MetaTable.BASES}.is_meta`),
+    )
+    .andWhere((qb) =>
+      qb
+        .where(`${MetaTable.BASES}.is_local`, false)
+        .orWhereNull(`${MetaTable.BASES}.is_local`),
+    )
+    .leftJoin(
+      MetaTable.PROJECT_USERS,
+      `${MetaTable.PROJECT}.id`,
+      `${MetaTable.PROJECT_USERS}.base_id`,
+    )
+    .on(`${MetaTable.PROJECT_USERS}.roles`, ProjectRoles.OWNER);
+
+  for (const source of sources) {
+    const integration = {
+      id: source.id,
+      fk_workspace_id: source.fk_workspace_id,
+      config: source.config,
+      meta: JSON.stringify(source.meta),
+      type: source.type,
+      is_private: source.is_private,
+      created_by: source.created_by,
+      created_at: source.created_at,
+      updated_at: source.updated_at,
+    };
+
+    await knex(MetaTable.INTEGRATIONS).insert(integration);
+    await knex(MetaTable.BASES)
+      .where('fk_source_id', source.id)
+      .update({ fk_integration_id: source.id });
+  }
 };
 
 const down = async (knex: Knex) => {

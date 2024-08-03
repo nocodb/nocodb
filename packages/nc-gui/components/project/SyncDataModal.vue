fc7c869b765a0b11ec72ab4e6e9b12fe8aecd9c6
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-08-03 12:15:33 +0000

    feat(nc-gui): search option in sync data modal

--- packages/nc-gui/components/project/SyncDataModal.vue
+++ packages/nc-gui/components/project/SyncDataModal.vue
@@ -9,6 +9,12 @@ const { syncDataUpvotes, updateSyncDataUpvotes } = useGlobal()
 
 const { $e } = useNuxtApp()
 
+const searchQuery = ref('')
+
+const filteredSyncDataTypes = computed(() =>
+  syncDataTypes.filter((s) => s.title.toLowerCase().includes(searchQuery.value.toLowerCase())),
+)
+
 const upvotesData = computed(() => {
   return new Set(syncDataUpvotes.value)
 })
@@ -36,28 +42,46 @@ const handleUpvote = (syncDataType: SyncDataType) => {
     <div class="h-full flex flex-col overflow-hidden">
       <div class="flex items-center justify-between gap-4 p-4 border-b-1 border-gray-200">
         <GeneralIcon icon="refresh" class="flex-none h-5 w-5 !text-blue-700" />
-        <div class="flex-1 flex items-center gap-3">
-          <h3 class="my-0 capitalize text-base font-weight-700">
-            {{ $t('labels.syncData') }}
-          </h3>
+        <div class="flex-1">
+          <div class="flex-1 flex items-center gap-3">
+            <h3 class="my-0 capitalize text-base font-weight-700">
+              {{ $t('labels.syncData') }}
+            </h3>
 
-          <NcBadge :border="false" class="text-brand-500 !h-6 bg-brand-50 text-sm px-2">{{
-            $t('msg.toast.futureRelease')
-          }}</NcBadge>
+            <NcBadge :border="false" class="text-brand-500 !h-5.5 bg-brand-50 text-sm px-2">{{
+              $t('msg.toast.futureRelease')
+            }}</NcBadge>
+          </div>
+          <div class="text-xs text-gray-600">{{ $t('labels.syncDataModalSubtitle') }}</div>
         </div>
         <NcButton type="text" size="xs" class="!px-0" @click="vOpen = false">
           <GeneralIcon icon="close" />
         </NcButton>
       </div>
-      <div class="flex-1 p-6 overflow-auto nc-scrollbar-thin">
-        <div class="flex flex-col gap-6 w-full max-w-[918px] mx-auto">
-          <div class="flex flex-col gap-3">
-            <div class="nc-sync-data-subtitle">
-              {{ $t('labels.syncDataModalSubtitle') }}
-            </div>
-          </div>
-          <div class="flex items-start gap-3 flex-wrap">
-            <div v-for="syncData of syncDataTypes" :key="syncData.value" class="nc-sync-data-card">
+      <div class="p-6">
+        <div class="flex items-center justify-end gap-3 max-w-[918px] mx-auto">
+          <a-input
+            v-model:value="searchQuery"
+            type="text"
+            class="nc-search-sync-data-input !min-w-[300px] !max-w-[300px] nc-input-sm flex-none"
+            placeholder="Search service"
+            allow-clear
+          >
+            <template #prefix>
+              <GeneralIcon icon="search" class="mr-2 h-4 w-4 text-gray-500" />
+            </template>
+          </a-input>
+        </div>
+      </div>
+      <div class="flex-1 px-6 flex overflow-auto nc-scrollbar-thin">
+        <div
+          class="flex flex-col gap-6 w-full max-w-[918px] mx-auto"
+          :class="{
+            'flex-1': !filteredSyncDataTypes.length,
+          }"
+        >
+          <div v-if="filteredSyncDataTypes.length" class="flex items-start gap-3 flex-wrap pb-6">
+            <div v-for="syncData of filteredSyncDataTypes" :key="syncData.value" class="nc-sync-data-card">
               <div class="card-icon-wrapper">
                 <component :is="syncData.icon" class="flex-none stroke-transparent" />
               </div>
@@ -81,6 +105,9 @@ const handleUpvote = (syncDataType: SyncDataType) => {
               </div>
             </div>
           </div>
+          <div v-else class="pt-8 flex-1 flex items-center justify-center">
+            <a-empty :image="Empty.PRESENTED_IMAGE_SIMPLE" :description="$t('labels.noData')" class="!my-0" />
+          </div>
         </div>
       </div>
     </div>
@@ -98,10 +125,12 @@ const handleUpvote = (syncDataType: SyncDataType) => {
   .nc-sync-data-title {
     @apply text-xl font-semibold;
   }
-  .nc-sync-data-subtitle {
-    @apply text-sm font-normal text-gray-600;
-  }
 
+  .ant-input-affix-wrapper.nc-search-sync-data-input {
+    &:not(:has(.ant-input-clear-icon-hidden)):has(.ant-input-clear-icon) {
+      @apply border-[var(--ant-primary-5)];
+    }
+  }
   .nc-sync-data-card {
     @apply p-3 flex items-center gap-4 rounded-xl border-1 border-gray-200 w-[298px] h-[76px];
 

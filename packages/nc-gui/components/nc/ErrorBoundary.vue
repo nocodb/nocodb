578626d3287cf50179756264cc30b4f0290978fe
Pranav C <pranavxc@gmail.com>
2024-08-05 21:04:07 +0000

    fix: global error boundary handling improvements

--- packages/nc-gui/components/nc/ErrorBoundary.vue
+++ packages/nc-gui/components/nc/ErrorBoundary.vue
@@ -1,6 +1,9 @@
 <script lang="ts">
 // modified version of default NuxtErrorBoundary component - https://github.com/nuxt/nuxt/blob/main/packages/nuxt/src/app/components/nuxt-error-boundary.ts
 import { message } from 'ant-design-vue'
+import * as Sentry from '@sentry/vue'
+
+const MESSAGE_KEY = 'ErrorMessageKey'
 
 export default {
   emits: {
@@ -17,24 +20,6 @@ export default {
     const isErrorExpanded = ref(false)
     const { copy } = useCopy()
 
-    onErrorCaptured((err) => {
-      if (import.meta.client && (!nuxtApp.isHydrating || !nuxtApp.payload.serverRendered)) {
-        console.error('UI Error :', err)
-        emit('error', err)
-        error.value = err
-        return false
-      }
-    })
-
-    const copyError = async () => {
-      try {
-        if (error.value) await copy(`message: ${error.value.message}\n\n${error.value.stack}`)
-        message.info('Error message copied to clipboard.')
-      } catch (e) {
-        message.error('Something went wrong while copying to clipboard, please copy from browser console.')
-      }
-    }
-
     const reload = () => {
       prevError.value = error.value
       error.value = null
@@ -46,6 +31,86 @@ export default {
       error.value = null
       location.hash = '/'
       location.reload()
+    }
+
+    onErrorCaptured((err) => {
+      if (import.meta.client && (!nuxtApp.isHydrating || !nuxtApp.payload.serverRendered)) {
+        console.error('UI Error :', err)
+        emit('error', err)
+        error.value = err
+
+        try {
+          Sentry.captureException(err)
+        } catch {
+          // ignore
+        }
+        // destroy any previous toast message to avoid duplicate messages
+        message.destroy(MESSAGE_KEY)
+
+        message.open({
+          key: MESSAGE_KEY,
+          content: h('div', [
+            h(
+              'div',
+              {
+                class: 'flex items-center gap-2',
+              },
+              [
+                h(resolveComponent('GeneralIcon'), { icon: 'error', class: 'text-2xl text-red-500' }),
+                h('div', { class: 'text-left' }, [
+                  h('div', { class: 'font-weight-bold' }, 'Error'),
+                  h('div', [h('span', 'Some error occurred while loading the page. Please reload.')]),
+                ]),
+                h(
+                  'div',
+                  {
+                    class: 'flex items-center gap-1 justify-end',
+                  },
+                  [
+                    h(
+                      resolveComponent('NcButton'),
+                      {
+                        onClick: reload,
+                        type: 'secondary',
+                        size: 'small',
+                      },
+                      'Reload',
+                    ),
+                    h(
+                      resolveComponent('NcButton'),
+                      {
+                        onClick: navigateToHome,
+                        type: 'secondary',
+                        size: 'small',
+                        class: 'flex items-center gap-1',
+                      },
+                      'Home',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ]),
+          duration: 10,
+          // type: 'error',
+          // prefixCls?: string;
+          // rootPrefixCls?: string;
+          // getPopupContainer?: (triggerNode: HTMLElement) => HTMLElement;
+          // style?: CSSProperties;
+          // class?: string;
+          // onClick?: (e: MouseEvent) => void;
+        })
+        return false
+      }
+    })
+
+    const copyError = async () => {
+      try {
+        if (error.value) await copy(`message: ${error.value.message}\n\n${error.value.stack}`)
+        message.info('Error message copied to clipboard.')
+      } catch (e) {
+        message.error('Something went wrong while copying to clipboard, please copy from browser console.')
+      }
     }
 
     return {
@@ -64,89 +129,4 @@ export default {
 
 <template>
   <slot :key="key"></slot>
-  <slot name="error">
-    <NcModal
-      v-if="error"
-      v-model:visible="errModal"
-      :class="{ active: errModal }"
-      :centered="true"
-      :closable="false"
-      :footer="null"
-    >
-      <div class="w-full flex flex-col gap-1">
-        <h2 class="text-xl font-semibold">Oops! Something unexpected happened :/</h2>
-
-        <p class="mb-0">
-          <span
-            >Please report this error in our
-            <a href="https://discord.gg/5RgZmkW" target="_blank" rel="noopener noreferrer">Discord channel</a>. You can copy the
-            error message by clicking the "Copy" button below.</span
-          >
-        </p>
-
-        <span class="cursor-pointer" @click="isErrorExpanded = !isErrorExpanded"
-          >{{ isErrorExpanded ? 'Hide' : 'Show' }} details
-          <GeneralIcon
-            icon="arrowDown"
-            class="transition-transform transform duration-300"
-            :class="{
-              'rotate-180': isErrorExpanded,
-            }"
-        /></span>
-        <div
-          class="nc-error"
-          :class="{
-            active: isErrorExpanded,
-          }"
-        >
-          <div class="nc-left-vertical-bar"></div>
-          <div class="nc-error-content">
-            <span class="font-weight-bold">Message: {{ error.message }}</span>
-            <br />
-            <div class="text-gray-500 mt-2">{{ error.stack }}</div>
-          </div>
-        </div>
-        <div class="flex justify-end gap-2">
-          <NcButton size="small" type="secondary" @click="copyError">
-            <div class="flex items-center gap-1">
-              <GeneralIcon icon="copy" />
-              Copy Error
-            </div>
-          </NcButton>
-          <NcButton v-if="!prevError || error.message !== prevError.message" size="small" @click="reload">
-            <div class="flex items-center gap-1">
-              <GeneralIcon icon="reload" />
-              Reload
-            </div>
-          </NcButton>
-          <NcButton v-else size="small" @click="navigateToHome">
-            <div class="flex items-center gap-1">
-              <GeneralIcon icon="link" />
-              Home
-            </div>
-          </NcButton>
-        </div>
-      </div>
-    </NcModal>
-  </slot>
 </template>
-
-<style scoped lang="scss">
-.nc-error {
-  @apply flex gap-2 mb-2 max-h-0;
-  white-space: pre;
-  transition: max-height 300ms linear;
-
-  &.active {
-    max-height: 250px;
-  }
-
-  .nc-left-vertical-bar {
-    @apply w-6px min-w-6px rounded min-h-full bg-gray-300;
-  }
-
-  .nc-error-content {
-    @apply min-w-0 overflow-auto pl-2 flex-shrink;
-  }
-}
-</style>

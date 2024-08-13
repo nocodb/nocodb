dad22fb4f6435711211faa589fe92f6ae2b1d6ae
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-08-13 20:55:26 +0530

    fix: refactor min/max with limit for columns fix: disable filter, groupby for Button Field fix: disable aggregation for Buttons

--- packages/nocodb/src/db/sortV2.ts
+++ packages/nocodb/src/db/sortV2.ts
@@ -2,11 +2,11 @@ import { UITypes } from 'nocodb-sdk';
 import type { BaseModelSqlv2 } from '~/db/BaseModelSqlv2';
 import type { Knex } from 'knex';
 import type { FormulaColumn, RollupColumn } from '~/models';
+import { Base, BaseUser, Sort } from '~/models';
 import { NcError } from '~/helpers/catchError';
 import formulaQueryBuilderv2 from '~/db/formulav2/formulaQueryBuilderv2';
 import genRollupSelectv2 from '~/db/genRollupSelectv2';
 import { sanitize } from '~/helpers/sqlSanitize';
-import { Base, BaseUser, Sort } from '~/models';
 import generateLookupSelectQuery from '~/db/generateLookupSelectQuery';
 import { getRefColumnIfAlias } from '~/helpers';
 
@@ -187,6 +187,8 @@ export default async function sortV2(
 
         break;
       }
+      case UITypes.Button:
+        break;
       default:
         qb.orderBy(
           sanitize(column.column_name),

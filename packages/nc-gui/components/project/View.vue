c23d62544661a546f48d549dc01925fd2dc4dcec
Pranav C <pranavxc@gmail.com>
2024-08-14 10:19:29 +0000

    refactor: hide user list in shared base

--- packages/nc-gui/components/project/View.vue
+++ packages/nc-gui/components/project/View.vue
@@ -14,7 +14,7 @@ const { openedProject, activeProjectId, basesUser, bases } = storeToRefs(basesSt
 const { activeTables, activeTable } = storeToRefs(useTablesStore())
 const { activeWorkspace } = storeToRefs(useWorkspace())
 
-const { navigateToProjectPage } = useBase()
+const { navigateToProjectPage, isSharedBase } = useBase()
 
 const isAdminPanel = inject(IsAdminPanelInj, ref(false))
 
@@ -160,7 +160,7 @@ watch(
         <!-- <a-tab-pane v-if="defaultBase" key="erd" tab="Base ERD" force-render class="pt-4 pb-12">
           <ErdView :source-id="defaultBase!.id" class="!h-full" />
         </a-tab-pane> -->
-        <a-tab-pane v-if="isUIAllowed('newUser', { roles: baseRoles })" key="collaborator">
+        <a-tab-pane v-if="isUIAllowed('newUser', { roles: baseRoles }) && !isSharedBase" key="collaborator">
           <template #tab>
             <div class="tab-title" data-testid="proj-view-tab__access-settings">
               <GeneralIcon icon="users" class="!h-3.5 !w-3.5" />

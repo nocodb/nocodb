91b957592e0470845eb842337638ad0e84d69302
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-08-14 09:48:55 +0000

    feat(nc-gui): custom list component setup

---
+++ packages/nc-gui/components/nc/List/index.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+export type Placement = 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topRight'
+
+export type RawValueType = string | number
+
+
+interface Props {
+  value: RawValueType
+  list: {
+    value: RawValueType
+    label: string
+  }[]
+}
+
+interface Emits {
+  (e: 'update:value', value: RawValueType): void
+  (e: 'change', value: RawValueType): void
+}
+
+
+const emits = defineEmits<Emits>()
+
+const props = withDefaults(defineProps<Props>(), {})
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>

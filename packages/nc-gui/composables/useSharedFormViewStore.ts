b03062c34db85ef428310d29aa1bb2493cde4f82
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-08-20 12:40:46 +0000

    fix(nc-gui): add support for redirect url in shared form

--- packages/nc-gui/composables/useSharedFormViewStore.ts
+++ packages/nc-gui/composables/useSharedFormViewStore.ts
@@ -333,7 +333,7 @@ const [useProvideSharedFormStore, useSharedFormStore] = useInjectionState((share
 
       const pk = extractPkFromRow(newRecord, meta.value?.columns as ColumnType[])
 
-      if (isEeUI && pk && isValidRedirectUrl.value) {
+      if (pk && isValidRedirectUrl.value) {
         const url = sharedFormView.value!.redirect_url!.replace('{record_id}', pk)
         window.location.href = url
       } else {
@@ -609,7 +609,7 @@ const [useProvideSharedFormStore, useSharedFormStore] = useInjectionState((share
   /** reset form if show_blank_form is true */
   watch(submitted, (nextVal) => {
     if (nextVal && sharedFormView.value?.show_blank_form) {
-      if (isEeUI && typeof sharedFormView.value?.redirect_url === 'string') {
+      if (typeof sharedFormView.value?.redirect_url === 'string') {
         return
       }
 

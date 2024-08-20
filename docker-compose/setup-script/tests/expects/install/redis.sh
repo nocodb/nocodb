fac610c05dfbf85a2559153581553411cfe7b56e
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-08-20 11:25:50 +0530

    fix: tests

--- docker-compose/setup-script/tests/expects/install/redis.sh
+++ docker-compose/setup-script/tests/expects/install/redis.sh
@@ -21,6 +21,9 @@ send "\r"
 expect "Do you want to enabled Redis for caching*"
 send "Y\r"
 
+expect "Do you want to enable Minio for file storage*"
+send "N\r"
+
 expect "Do you want to enabled Watchtower for automatic updates*"
 send "\r"
 

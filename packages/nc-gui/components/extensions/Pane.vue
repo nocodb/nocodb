e3487a2e1540ce690525dc3155beb88356f8da8f
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-08-22 14:15:03 +0000

    fix(nc-gui): update extension header

--- packages/nc-gui/components/extensions/Pane.vue
+++ packages/nc-gui/components/extensions/Pane.vue
@@ -15,7 +15,25 @@ const {
 
 const isReady = ref(false)
 
+const searchExtensionRef = ref<HTMLInputElement>()
+
+const searchExtensionWrapperRef = ref<HTMLDivElement>()
+
 const searchQuery = ref<string>('')
+
+const showSearchBox = ref(false)
+
+const isOpenSearchBox = computed(() => {
+  return searchQuery.value || showSearchBox.value
+})
+
+const handleShowSearchInput = () => {
+  showSearchBox.value = true
+
+  nextTick(() => {
+    searchExtensionRef.value?.focus()
+  })
+}
 
 const filteredExtensionList = computed(() =>
   (extensionList.value || []).filter((ext) => ext.title.toLowerCase().includes(searchQuery.value.toLowerCase())),
@@ -46,6 +64,20 @@ watch(isPanelExpanded, (newValue) => {
     }, 300)
   }
 })
+
+onClickOutside(searchExtensionWrapperRef, () => {
+  if (searchQuery.value) {
+    return
+  }
+
+  showSearchBox.value = false
+})
+
+onMounted(() => {
+  if (searchQuery.value && !showSearchBox.value) {
+    showSearchBox.value = true
+  }
+})
 </script>
 
 <template>
@@ -60,14 +92,11 @@ watch(isPanelExpanded, (newValue) => {
       maxWidth: `${normalizePaneMaxWidth}%`,
     }"
   >
-    <div class="flex justify-between items-center px-4 pt-3">
-      <div class="flex items-center gap-3 font-weight-700 text-brand-500 text-base">
-        <GeneralIcon icon="puzzle" class="h-5 w-5" /> Extensions
-      </div>
-      <NcTooltip class="flex" hide-on-click placement="topRight">
+    <div class="flex items-center gap-3 p-4 border-b-1 border-gray-200 bg-white">
+      <NcTooltip v-if="false" class="flex" hide-on-click placement="topRight">
         <template #title> Hide extensions </template>
         <NcButton
-          size="xxsmall"
+          size="small"
           type="text"
           class="!text-gray-700 !hover:text-gray-800 !hover:bg-gray-200"
           @click="toggleExtensionPanel"
@@ -77,6 +106,45 @@ watch(isPanelExpanded, (newValue) => {
           </div>
         </NcButton>
       </NcTooltip>
+      <div
+        class="flex items-center gap-3 font-weight-700 text-gray-700 text-base"
+        :class="{
+          'flex-1': !isOpenSearchBox,
+        }"
+      >
+        <GeneralIcon icon="ncPuzzleSolid" class="h-7 w-7" />
+        <span v-if="!isOpenSearchBox">Extensions</span>
+      </div>
+      <div
+        ref="searchExtensionWrapperRef"
+        :class="{
+          'flex-1': isOpenSearchBox,
+        }"
+      >
+        <NcButton v-if="!isOpenSearchBox" size="small" type="text" @click="handleShowSearchInput">
+          <GeneralIcon icon="search" class="flex-none !text-gray-500" />
+        </NcButton>
+        <div v-else class="flex flex-grow items-center justify-end">
+          <a-input
+            ref="searchExtensionRef"
+            v-model:value="searchQuery"
+            type="text"
+            class="!h-8 !px-3 !py-1 !rounded-lg !max-w-[350px]"
+            placeholder="Search Extension"
+            allow-clear
+          >
+            <template #prefix>
+              <GeneralIcon icon="search" class="mr-2 h-4 w-4 text-gray-500 group-hover:text-black" />
+            </template>
+          </a-input>
+        </div>
+      </div>
+      <NcButton type="secondary" size="small" @click="toggleMarket">
+        <div class="flex items-center gap-1 text-xs max-w-full">
+          <GeneralIcon icon="plus" />
+          Add
+        </div>
+      </NcButton>
     </div>
     <template v-if="extensionList.length === 0">
       <div class="flex items-center flex-col gap-4 w-full nc-scrollbar-md text-center px-4">
@@ -92,35 +160,6 @@ watch(isPanelExpanded, (newValue) => {
       </div>
     </template>
     <template v-else>
-      <div class="flex w-full items-center justify-between px-4">
-        <div class="flex flex-grow items-center mr-2">
-          <a-input
-            v-model:value="searchQuery"
-            type="text"
-            class="!h-8 !px-3 !py-1 !rounded-lg"
-            placeholder="Search Extension"
-            allow-clear
-          >
-            <template #prefix>
-              <GeneralIcon icon="search" class="mr-2 h-4 w-4 text-gray-500 group-hover:text-black" />
-            </template>
-          </a-input>
-        </div>
-        <NcButton type="ghost" size="small" class="!text-primary !bg-white children:children:max-w-full" @click="toggleMarket">
-          <div class="flex items-center gap-1 text-xs max-w-full">
-            <GeneralIcon icon="plus" />
-            <NcTooltip
-              class="max-w-[calc(100%_-_16px)] truncate"
-              show-on-truncate-only
-              overlay-class-name="children:-ml-2"
-              modifier-key=""
-            >
-              <template #title> Add Extension </template>
-              Add Extension
-            </NcTooltip>
-          </div>
-        </NcButton>
-      </div>
       <div
         class="nc-extension-list-wrapper flex items-center flex-col gap-3 w-full nc-scrollbar-md"
         :class="{
@@ -162,7 +201,7 @@ watch(isPanelExpanded, (newValue) => {
   }
 }
 
-.nc-extension-pane{
+.nc-extension-pane {
   @apply flex flex-col gap-3 bg-gray-50 rounded-l-xl border-1 border-gray-200 z-30;
 
   box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.16), 0px 8px 8px -4px rgba(0, 0, 0, 0.04);

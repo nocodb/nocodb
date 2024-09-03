f8b4faf85e6eaabdfd17c803e541103850d81a85
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-09-03 06:20:38 +0000

    fix(nc-gui): udpate extension marketplace modal

--- packages/nc-gui/components/extensions/Market.vue
+++ packages/nc-gui/components/extensions/Market.vue
@@ -27,13 +27,13 @@ const tabs = [
   {
     title: 'Scripts',
     tabKey: 'scripts',
-    icon: 'ncScript',
+    icon: 'ncPuzzleOutline',
     isDisabled: true,
   },
   {
     title: 'Build an extension',
     tabKey: 'build-an-extension',
-    icon: 'ncSpanner',
+    icon: 'ncSettings2',
     isDisabled: true,
   },
 ] as TabItem[]
@@ -81,117 +81,118 @@ const handleSetActiveTab = (tab: TabItem) => {
     v-model:visible="vModel"
     :class="{ active: vModel }"
     :footer="null"
-    :width="1024"
+    :width="1280"
     size="medium"
     wrap-class-name="nc-modal-extension-market"
   >
     <div class="h-full">
-      <div class="flex items-center gap-3 p-4 border-b-1 border-gray-200">
-        <GeneralIcon icon="ncPuzzleSolid" class="h-6 w-6 flex-none text-gray-700" />
-        <div class="flex-1 font-semibold text-xl">Extensions Marketplace</div>
-        <NcButton size="small" type="text" @click="vModel = false">
-          <GeneralIcon icon="close" class="text-gray-600" />
-        </NcButton>
+      <div class="nc-extension-market-header flex items-center gap-3 p-4 border-b-1 border-gray-200">
+        <div
+          class="flex items-center gap-3 flex-none"
+          :style="{
+            width: 'calc(\(100% - 358px - 24px\) / 2)',
+          }"
+        >
+          <GeneralIcon icon="ncPuzzleSolid" class="h-6 w-6 flex-none text-gray-700" />
+          <div class="flex-1 font-semibold text-xl">Marketplace</div>
+        </div>
+        <div class="flex bg-nc-gray-medium rounded-lg p-1">
+          <div class="flex items-center">
+            <NcTooltip v-for="(tab, idx) of tabs" :key="idx" :disabled="tab.tabKey === 'extensions'">
+              <template #title> {{ $t('msg.toast.futureRelease') }}</template>
+              <div
+                class="px-3 py-1 flex items-center gap-2 text-xs rounded-md select-none"
+                :class="{
+                  'active-tab bg-white text-nc-gray-emphasis': activeTab === tab.tabKey,
+                  'text-nc-gray-subtle2': activeTab !== tab.tabKey,
+                  'cursor-not-allowed opacity-70': tab.isDisabled,
+                  'cursor-pointer': !tab.isDisabled,
+                }"
+                @click="handleSetActiveTab(tab)"
+              >
+                <GeneralIcon :icon="tab.icon" class="h-4 w-4 flex-none stroke-transparent opacity-75" />
+                {{ tab.title }}
+              </div>
+            </NcTooltip>
+          </div>
+        </div>
+        <div class="flex-1 flex gap-3 justify-end">
+          <div class="flex-1 flex max-w-[290px]">
+            <a-input
+              v-model:value="searchQuery"
+              type="text"
+              class="nc-input-border-on-value !h-8 !px-3 !py-1 !rounded-lg"
+              placeholder="Search for an extension or script"
+              allow-clear
+            >
+              <template #prefix>
+                <GeneralIcon icon="search" class="mr-2 h-4 w-4 text-gray-500 group-hover:text-black" />
+              </template>
+            </a-input>
+          </div>
+          <NcButton size="small" type="text" @click="vModel = false">
+            <GeneralIcon icon="close" class="text-gray-600" />
+          </NcButton>
+        </div>
       </div>
 
       <div class="flex flex-col h-[calc(100%_-_65px)]">
-        <NcTabs class="h-full">
-          <template #leftExtra>
-            <div class="w-2"></div>
-          </template>
-
-          <a-tab-pane key="integrations" class="w-full h-full">
-            <template #tab>
-              <div class="tab-title">
-                {{ $t('general.extensions') }}
-              </div>
-            </template>
-            <div class="h-full py-6">
-              <div class="h-full flex flex-col gap-6 flex-1">
-                <div class="flex flex max-w-[470px] px-6">
-                  <a-input
-                    v-model:value="searchQuery"
-                    type="text"
-                    class="nc-input-border-on-value !h-8 !px-3 !py-1 !rounded-lg"
-                    placeholder="Search for an extension..."
-                    allow-clear
-                  >
-                    <template #prefix>
-                      <GeneralIcon icon="search" class="mr-2 h-4 w-4 text-gray-500 group-hover:text-black" />
-                    </template>
-                  </a-input>
-                </div>
+        <div v-if="activeTab === 'extensions'" class="h-full py-4">
+          <div class="h-full flex flex-col gap-6 flex-1">
+            <div
+              class="flex flex-wrap gap-4 nc-scrollbar-thin px-6 max-w-[900px] mx-auto py-2"
+              :class="{
+                'h-full': searchQuery && !filteredAvailableExtensions.length && availableExtensions.length,
+              }"
+            >
+              <template v-for="ext of filteredAvailableExtensions" :key="ext.id">
                 <div
-                  class="max-h-[calc(100%_-_40px)] flex flex-wrap gap-6 nc-scrollbar-thin px-6"
-                  :class="{
-                    'h-full': searchQuery && !filteredAvailableExtensions.length && availableExtensions.length,
-                  }"
+                  class="nc-market-extension-item w-full md:w-[calc(50%_-_8px)] flex items-center gap-3 border-1 rounded-xl p-3 cursor-pointer hover:bg-gray-50 transition-all"
+                  @click="onExtensionClick(ext.id)"
                 >
-                  <template v-for="ext of filteredAvailableExtensions" :key="ext.id">
-                    <div
-                      class="nc-market-extension-item w-full md:w-[calc(50%_-_12px)] flex items-center gap-3 border-1 rounded-xl p-3 cursor-pointer hover:bg-gray-50 transition-all"
-                      @click="onExtensionClick(ext.id)"
-                    >
-                      <div class="h-[60px] w-[60px] overflow-hidden m-auto flex-none">
-                        <img :src="getExtensionAssetsUrl(ext.iconUrl)" alt="icon" class="w-full h-full object-contain" />
-                      </div>
-                      <div class="flex-1 flex flex-grow flex-col gap-1">
-                        <div class="font-weight-600 text-base line-clamp-1">
-                          {{ ext.title }}
-                        </div>
-
-                        <div class="max-h-[32px] text-xs text-gray-500 line-clamp-2">{{ ext.subTitle }}</div>
-                      </div>
-                      <NcButton size="small" type="secondary" class="flex-none !px-7px" @click.stop="onAddExtension(ext)">
-                        <div class="flex items-center gap-1 -ml-3px text-small">
-                          <GeneralIcon icon="plus" />
-                          {{ $t('general.add') }}
-                        </div>
-                      </NcButton>
-                    </div>
-                  </template>
-
-                  <div
-                    v-if="searchQuery && !filteredAvailableExtensions.length && availableExtensions.length"
-                    class="w-full h-full flex items-center justify-center"
-                  >
-                    <div class="pb-6 text-gray-500 flex flex-col items-center gap-6 text-center">
-                      <img
-                        src="~assets/img/placeholder/no-search-result-found.png"
-                        class="!w-[164px] flex-none"
-                        alt="No search results found"
-                      />
-
-                      {{ $t('title.noResultsMatchedYourSearch') }}
-                    </div>
+                  <div class="h-[60px] w-[60px] overflow-hidden m-auto flex-none">
+                    <img :src="getExtensionAssetsUrl(ext.iconUrl)" alt="icon" class="w-full h-full object-contain" />
                   </div>
+                  <div class="flex-1 flex flex-grow flex-col gap-1">
+                    <div class="font-weight-600 text-base line-clamp-1">
+                      {{ ext.title }}
+                    </div>
+
+                    <div class="max-h-[32px] text-xs text-gray-500 line-clamp-2">{{ ext.subTitle }}</div>
+                  </div>
+                  <NcButton size="small" type="secondary" class="flex-none !px-7px" @click.stop="onAddExtension(ext)">
+                    <div class="flex items-center gap-1 -ml-3px text-small">
+                      <GeneralIcon icon="plus" />
+                      {{ $t('general.add') }}
+                    </div>
+                  </NcButton>
+                </div>
+              </template>
+
+              <div
+                v-if="searchQuery && !filteredAvailableExtensions.length && availableExtensions.length"
+                class="w-full h-full flex items-center justify-center"
+              >
+                <div class="pb-6 text-gray-500 flex flex-col items-center gap-6 text-center">
+                  <img
+                    src="~assets/img/placeholder/no-search-result-found.png"
+                    class="!w-[164px] flex-none"
+                    alt="No search results found"
+                  />
+
+                  {{ $t('title.noResultsMatchedYourSearch') }}
                 </div>
               </div>
             </div>
-          </a-tab-pane>
-          <a-tab-pane key="scripts" class="w-full h-full" disabled>
-            <template #tab>
-              <NcTooltip>
-                <template #title>
-                  {{ $t('msg.toast.futureRelease') }}
-                </template>
-                <div class="tab-title text-gray-400">Scripts</div>
-              </NcTooltip>
-            </template>
-            <div class="h-full p-6"></div>
-          </a-tab-pane>
-          <a-tab-pane key="build-extension" class="w-full h-full" disabled>
-            <template #tab>
-              <NcTooltip>
-                <template #title>
-                  {{ $t('msg.toast.futureRelease') }}
-                </template>
-                <div class="tab-title text-gray-400">Build an extension</div>
-              </NcTooltip>
-            </template>
-            <div class="h-full p-6"></div>
-          </a-tab-pane>
-        </NcTabs>
+          </div>
+        </div>
+
+        <template v-else-if="activeTab === 'scripts'">
+          <!-- Coming soon  -->
+        </template>
+        <template v-else-if="activeTab === 'build-an-extension'">
+          <!-- Coming soon  -->
+        </template>
       </div>
     </div>
   </NcModal>
@@ -261,6 +262,12 @@ const handleSetActiveTab = (tab: TabItem) => {
       &:has(+ .selected)::after {
         transform: scaleY(0); // Hide the line on the right of the selected item
       }
+    }
+  }
+
+  .nc-extension-market-header {
+    .active-tab {
+      box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.06), 0px 5px 3px -2px rgba(0, 0, 0, 0.02);
     }
   }
 }

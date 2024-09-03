43bf057b498c293da8e4c87dd030030224d516cb
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-09-03 06:20:36 +0000

    fix(nc-gui): extension pane scroll issue

--- packages/nc-gui/components/extensions/Pane.vue
+++ packages/nc-gui/components/extensions/Pane.vue
@@ -142,7 +142,7 @@ onMounted(() => {
     "
   >
     <Transition name="layout" :duration="150">
-      <div v-if="isPanelExpanded" class="flex flex-col">
+      <div v-if="isPanelExpanded" class="flex flex-col h-full">
         <div
           ref="extensionHeaderRef"
           class="h-[var(--toolbar-height)] flex items-center gap-3 px-4 py-2 border-b-1 border-gray-200 bg-white"

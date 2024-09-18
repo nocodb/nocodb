d2b2c7b3b14a4adaceb87b708d32f51d72d13f44
mertmit <mertmit99@gmail.com>
2024-09-18 10:30:15 +0000

    feat: restrict concurrent operations

--- packages/nocodb/src/utils/NcHelp.ts
+++ packages/nocodb/src/utils/NcHelp.ts
@@ -1,20 +1,41 @@
 import debug from 'debug';
+import PQueue from 'p-queue';
+import { Logger } from '@nestjs/common';
+
+const NC_EXECUTE_OPERATIONS_CONCURRENCY =
+  +process.env.NC_EXECUTE_OPERATIONS_CONCURRENCY || 5;
 
 export default class NcHelp {
+  public static logger = new Logger('NcHelp');
+
   public static async executeOperations(
     fns: Array<() => Promise<any>>,
     dbType: string,
   ): Promise<any> {
-    if (dbType === 'oracledb' || dbType === 'mssql') {
-      for (const fn of fns) {
-        await fn();
-      }
-    } else {
-      await Promise.all(
-        fns.map(async (f) => {
-          await f();
-        }),
-      );
+    const queue = new PQueue({
+      concurrency:
+        dbType === 'oracledb' || dbType === 'mssql'
+          ? 1
+          : NC_EXECUTE_OPERATIONS_CONCURRENCY,
+    });
+
+    let error = null;
+
+    for (const fn of fns) {
+      queue.add(async () => {
+        try {
+          await fn();
+        } catch (e) {
+          this.logger.error(e);
+          error = e;
+        }
+      });
+    }
+
+    await queue.onIdle();
+
+    if (error) {
+      throw error;
     }
   }
 

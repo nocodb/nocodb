dfe894ed0e3c5e5588edb8ebb03431a9df723008
Mert E. <mertmit99@gmail.com>
2024-09-23 12:43:44 +0300

    fix: show other workspaces properly in cmdk (#3802)
    * fix: shift modifier ref
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * fix: use state workspaces for cmdk
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    ---------
    
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/ee/services/command-palette.service.ts
+++ packages/nocodb/src/ee/services/command-palette.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { type UserType, ViewTypes } from 'nocodb-sdk';
-import WorkspaceUser from '~/models/WorkspaceUser';
 import { TablesService } from '~/services/tables.service';
 import { deserializeJSON } from '~/utils/serialize';
 import { getCommandPaletteForUserWorkspace } from '~/helpers/commandPaletteHelpers';
@@ -23,27 +22,7 @@ export class CommandPaletteService {
     try {
       const { scope, data } = param.body;
 
-      if (scope === 'root') {
-        const workspaces = await WorkspaceUser.workspaceList({
-          fk_user_id: param.user?.id,
-        });
-
-        for (const workspace of workspaces) {
-          cmdData.push({
-            id: `ws-nav-${workspace.id}`,
-            title: workspace.title,
-            icon: 'workspace',
-            iconColor: deserializeJSON(workspace.meta)?.color,
-            section: 'Workspaces',
-            scopePayload: {
-              scope: `ws-${workspace.id}`,
-              data: {
-                workspace_id: workspace.id,
-              },
-            },
-          });
-        }
-      } else if (scope.startsWith('ws-')) {
+      if (scope.startsWith('ws-')) {
         const list: {
           workspace_id: string;
           workspace_title: string;
@@ -148,25 +127,28 @@ export class CommandPaletteService {
           }
         }
 
-        for (const [id, workspace] of workspaces) {
-          cmdData.push({
-            id: `ws-nav-${id}`,
-            title: workspace.title,
-            icon: 'workspace',
-            iconColor: deserializeJSON(workspace.meta)?.color,
-            section: 'Workspaces',
-            scopePayload: {
-              scope: `ws-${id}`,
-              data: {
-                workspace_id: id,
+        /*
+          Avoid returning workspaces as we use them from state
+          for (const [id, workspace] of workspaces) {
+            cmdData.push({
+              id: `ws-nav-${id}`,
+              title: workspace.title,
+              icon: 'workspace',
+              iconColor: deserializeJSON(workspace.meta)?.color,
+              section: 'Workspaces',
+              scopePayload: {
+                scope: `ws-${id}`,
+                data: {
+                  workspace_id: id,
+                },
               },
-            },
-            handler: {
-              type: 'navigate',
-              payload: `/${id}/settings`,
-            },
-          });
-        }
+              handler: {
+                type: 'navigate',
+                payload: `/${id}/settings`,
+              },
+            });
+          }
+        */
 
         for (const [id, base] of bases) {
           cmdData.push({
@@ -207,110 +189,6 @@ export class CommandPaletteService {
             },
           });
         }
-
-        /*
-        const workspaces = await WorkspaceUser.workspaceList({
-          fk_user_id: param.user?.id,
-        });
-
-        const workspace = workspaces.find((el) => el.id === data.workspace_id);
-
-        for (const _workspace of workspaces) {
-          cmdData.push({
-            id: `ws-nav-${_workspace.id}`,
-            title: _workspace.title,
-            icon: 'workspace',
-            iconColor: deserializeJSON(_workspace.meta)?.color,
-            section: 'Workspaces',
-            scopePayload: {
-              scope: `ws-${_workspace.id}`,
-              data: {
-                workspace_id: _workspace.id,
-              },
-            },
-            handler: {
-              type: 'navigate',
-              payload: `/${_workspace.id}/settings`,
-            },
-          });
-        }
-
-        if (!workspace) {
-          NcError.workspaceNotFound(data.workspace_id);
-        }
-        const allBases = [];
-
-        const bases = await Base.listByWorkspaceAndUser(
-          data.workspace_id,
-          param.user?.id,
-        );
-
-        allBases.push(...bases);
-
-        const viewList = [];
-
-        for (const base of bases) {
-          viewList.push(
-            ...(
-              (await this.tablesService.xcVisibilityMetaGet(
-                { workspace_id: base.fk_workspace_id, base_id: base.id },
-                base.id,
-                null,
-                false,
-              )) as any[]
-            ).filter((v) => {
-              return Object.keys(param.user.roles).some(
-                (role) => param.user.roles[role] && !v.disabled[role],
-              );
-            }),
-          );
-        }
-
-        const tableList = [];
-        const vwList = [];
-
-        for (const b of allBases) {
-          cmdData.push({
-            id: `p-${b.id}`,
-            title: b.title,
-            parent: `ws-${workspace.id}`,
-            icon: 'project',
-            iconColor: deserializeJSON(b.meta)?.iconColor,
-            section: 'Bases',
-          });
-        }
-
-        for (const v of viewList) {
-          if (!tableList.find((el) => el.id === `tbl-${v.fk_model_id}`)) {
-            tableList.push({
-              id: `tbl-${v.fk_model_id}`,
-              title: v._ptn,
-              parent: `p-${v.base_id}`,
-              icon: v?.table_meta?.icon || v.ptype,
-              projectName: bases.find((el) => el.id === v.base_id)?.title,
-              section: 'Tables',
-            });
-          }
-          vwList.push({
-            id: `vw-${v.id}`,
-            title: `${v.title}`,
-            parent: `tbl-${v.fk_model_id}`,
-            icon: v?.meta?.icon || viewTypeAlias[v.type] || 'table',
-            projectName: bases.find((el) => el.id === v.base_id)?.title,
-            section: 'Views',
-            is_default: v?.is_default,
-            handler: {
-              type: 'navigate',
-              payload: `/${data.workspace_id}/${v.base_id}/${
-                v.fk_model_id
-              }/${encodeURIComponent(v.id)}`,
-            },
-          });
-        }
-
-        cmdData.push(...tableList);
-        cmdData.push(...vwList);
-        */
       } else if (scope.startsWith('p-')) {
         return [];
       }

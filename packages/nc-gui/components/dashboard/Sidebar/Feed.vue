b420d8861611246b352ad49881c269536958de7f
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-10-02 05:54:21 +0000

    feat: feed wip

---
+++ packages/nc-gui/components/dashboard/Sidebar/Feed.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { navigateToFeed } = useWorkspace()
+
+const gotoFeed = async () => {
+  await navigateToFeed()
+}
+</script>
+
+<template>
+  <div class="px-2 py-2">
+    <div class="flex items-center py-1.5 cursor-pointer px-3 hover:bg-gray-100 bg-[#F0F3FF] rounded-md gap-3" @click="gotoFeed">
+      <GeneralIcon class="text-yellow-500" icon="magic" />
+      <span class="text-[#2952CC] font-semibold"> What’s New! </span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ packages/nc-gui/components/feed/View.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const activeKey = ref('recents')
+</script>
+
+<template>
+  <FeedHeader />
+
+  <FeedNavigation v-model:active-key="activeKey" />
+</template>
+
+<style scoped lang="scss"></style>

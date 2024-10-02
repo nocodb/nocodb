8df61267f8ec2eeb5c24e265ad9fd16a8b3a028c
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-10-02 05:54:21 +0000

    feat: github release feed

--- packages/nc-gui/components/feed/Changelog/index.vue
+++ packages/nc-gui/components/feed/Changelog/index.vue
@@ -1,26 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const releases = ref<
+  {
+    body: string
+    id: string
+    published_at: string
+  }[]
+>([])
+
+const fetchReleaseNotes = async () => {
+  const response = await fetch('https://api.github.com/repos/nocodb/nocodb/releases')
+  const data = await response.json()
+  return data
+}
+
+onMounted(async () => {
+  releases.value = await fetchReleaseNotes()
+})
+</script>
 
 <template>
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
-  <FeedChangelogItem />
+  <FeedChangelogItem v-for="feed in releases" :key="feed.id" :date="feed.published_at" :body="feed?.body" />
 </template>
 
 <style scoped lang="scss"></style>

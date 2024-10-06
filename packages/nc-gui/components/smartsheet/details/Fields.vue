c4997c9e5dc84cbb5e2d53a8207a725a8677f412
Pranav C <pranavxc@gmail.com>
2024-10-06 08:19:51 +0000

    fix: avoid parsing twice

--- packages/nc-gui/components/smartsheet/details/Fields.vue
+++ packages/nc-gui/components/smartsheet/details/Fields.vue
@@ -442,10 +442,10 @@ const onFieldAdd = (state: TableExplorerColumn) => {
 
   state.temp_id = `temp_${++temporaryAddCount.value}`
   state.view_id = view.value?.id as string
-  ops.value.push({
+  ops.value = [...ops.value, {
     op: 'add',
     column: state,
-  })
+  }]
   newFields.value = [...newFields.value, state]
 
   if (addFieldMoveHook.value) {

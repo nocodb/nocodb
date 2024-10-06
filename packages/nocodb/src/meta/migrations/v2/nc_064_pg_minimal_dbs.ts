49c41564e014ec75bc41dae478f53c10393548fd
Pranav C <pranavxc@gmail.com>
2024-10-06 07:21:36 +0000

    refactor: enum alias correction

--- packages/nocodb/src/meta/migrations/v2/nc_064_pg_minimal_dbs.ts
+++ packages/nocodb/src/meta/migrations/v2/nc_064_pg_minimal_dbs.ts
@@ -2,13 +2,13 @@ import type { Knex } from 'knex';
 import { MetaTable } from '~/utils/globals';
 
 const up = async (knex: Knex) => {
-  await knex.schema.alterTable(MetaTable.BASES, (table) => {
+  await knex.schema.alterTable(MetaTable.SOURCES, (table) => {
     table.boolean('is_local').defaultTo(false);
   });
 };
 
 const down = async (knex: Knex) => {
-  await knex.schema.alterTable(MetaTable.BASES, (table) => {
+  await knex.schema.alterTable(MetaTable.SOURCES, (table) => {
     table.dropColumn('is_local');
   });
 };

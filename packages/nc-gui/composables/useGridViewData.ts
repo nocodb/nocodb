ad740c52e12d0805450c66661e11d8757e441218
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-10-18 09:28:15 +0000

    fix: improve scroll performance

--- packages/nc-gui/composables/useGridViewData.ts
+++ packages/nc-gui/composables/useGridViewData.ts
@@ -193,7 +193,7 @@ export function useGridViewData(
         totalRows.value = response.pageInfo.totalRows
       }
 
-      await loadAggCommentsCount(data)
+      loadAggCommentsCount(data)
 
       return data
     } catch (error: any) {

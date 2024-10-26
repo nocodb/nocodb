4997b726f50c0f25b7ecd36dd2530dae402f2e0c
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-10-26 10:32:29 +0530

    fix(nc-gui): decode only prefill keys

--- packages/nc-gui/composables/useSharedFormViewStore.ts
+++ packages/nc-gui/composables/useSharedFormViewStore.ts
@@ -441,25 +441,19 @@ const [useProvideSharedFormStore, useSharedFormStore] = useInjectionState((share
 
   async function handlePreFillForm() {
     if (Object.keys(route.query || {}).length) {
-      // Decode both query parameter keys and values if values are present
-      const decodedQuery = Object.fromEntries(
-        Object.entries(route.query).map(([key, value]) => [
-          decodeURIComponent(key),
-          value
-            ? Array.isArray(value)
-              ? value.map((qp) => decodeURIComponent(qp as string)?.trim())
-              : decodeURIComponent(value as string)?.trim()
-            : value,
-        ]),
-      )
+      // Decode query parameter keys
+      const decodedQuery = Object.fromEntries(Object.entries(route.query).map(([key, value]) => [decodeURIComponent(key), value]))
 
       columns.value = await Promise.all(
         (columns.value || []).map(async (c) => {
-          const decodedQueryParam = decodedQuery[c.title as string]
+          const queryParam =
+            route.query[c.title as string] ||
+            route.query[encodeURIComponent(c.title as string)] ||
+            decodedQuery[c.title as string]
 
           if (
             !c.title ||
-            !decodedQueryParam ||
+            !queryParam ||
             isSystemColumn(c) ||
             (isVirtualCol(c) && !isLinksOrLTAR(c)) ||
             (!sharedViewMeta.value.preFillEnabled && !isVirtualCol(c) && !isLinksOrLTAR(c)) ||
@@ -468,6 +462,10 @@ const [useProvideSharedFormStore, useSharedFormStore] = useInjectionState((share
           ) {
             return c
           }
+
+          const decodedQueryParam = Array.isArray(queryParam)
+            ? queryParam.map((qp) => decodeURIComponent(qp as string).trim())
+            : decodeURIComponent(queryParam as string).trim()
 
           const preFillValue = await getPreFillValue(c, decodedQueryParam)
           if (preFillValue !== undefined) {

d9f9862b0d7ba711cc34b5b428626f6afca77c86
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-10-31 06:04:58 +0000

    fix(nc-gui): extension pane new design changes

--- packages/nc-gui/components/extensions/Pane.vue
+++ packages/nc-gui/components/extensions/Pane.vue
@@ -190,11 +190,12 @@ onMounted(() => {
           </NcButton>
         </div>
         <template v-if="extensionList.length === 0">
-          <div class="flex items-center flex-col gap-4 w-full nc-scrollbar-md text-center p-4">
-            <GeneralIcon icon="ncPuzzleSolid" class="h-12 w-12 flex-none mt-[120px] text-gray-500 !stroke-transparent" />
-
-            <div class="font-weight-700 text-base">No extensions added</div>
-            <div class="text-sm text-gray-700">Add Extensions from the community extensions marketplace</div>
+          <div class="flex-1 flex items-center justify-center flex-col gap-4 w-full nc-scrollbar-md text-center p-4">
+            <div class="text-base font-bold text-nc-content-gray">Supercharge Your Workflow with Extensions</div>
+            <div class="text-sm text-nc-content-gray-subtle2">
+              Unlock powerful scripts and tools to enhance how you work with your databases. Get started by exploring available
+              extensions.
+            </div>
             <NcButton size="small" @click="toggleMarket">
               <div class="flex items-center gap-1 -ml-3px">
                 <GeneralIcon icon="plus" />
@@ -208,6 +209,8 @@ onMounted(() => {
                 {{ $t('activity.goToDocs') }}
               </div>
             </NcButton>
+
+            <img src="~assets/img/placeholder/extension.png" class="!w-full min-w-[250px] max-w-[432px] flex-none" />
           </div>
         </template>
         <template v-else>

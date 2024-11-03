58e6282f6852a829e9b65c704793ac3e386e7c3c
Quinn Jarrell <quinnjarr@gmail.com>
2024-11-03 14:27:08 -0600

    Revert "Filter using LinkToAnotherRecord path as it works"
    This reverts commit 1103005f38843e659dbd8da3f7f57737ff3fac58.

--- packages/nocodb/src/db/conditionV2.ts
+++ packages/nocodb/src/db/conditionV2.ts
@@ -214,7 +214,7 @@ const parseConditionV2 = async (
         NcError.fieldNotFound(filter.fk_column_id);
       }
     }
-    if (column.uidt === UITypes.LinkToAnotherRecord || column.uidt == UITypes.Links) {
+    if (column.uidt === UITypes.LinkToAnotherRecord) {
       const colOptions = (await column.getColOptions(
         context,
       )) as LinkToAnotherRecordColumn;

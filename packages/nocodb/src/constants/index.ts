c631dee8cbae57e92072cf08b2d5a9c757cb7f02
Pranav C <pranavxc@gmail.com>
2024-11-13 07:30:08 +0000

    refactor: apply refresh token expiry for cookie as well

--- packages/nocodb/src/constants/index.ts
+++ packages/nocodb/src/constants/index.ts
@@ -6,3 +6,10 @@ export const NC_ATTACHMENT_FIELD_SIZE =
   +process.env['NC_ATTACHMENT_FIELD_SIZE'] || 20 * 1024 * 1024; // 20 MB
 export const NC_MAX_ATTACHMENTS_ALLOWED =
   +process.env['NC_MAX_ATTACHMENTS_ALLOWED'] || 10;
+export const NC_REFRESH_TOKEN_EXP_IN_DAYS =
+  parseInt(process.env.NC_REFRESH_TOKEN_EXP_IN_DAYS, 10) || 90;
+
+// throw error if user provided invalid value
+if (!NC_REFRESH_TOKEN_EXP_IN_DAYS || NC_REFRESH_TOKEN_EXP_IN_DAYS <= 0) {
+  throw new Error('NC_REFRESH_TOKEN_EXP_IN_DAYS must be a positive number');
+}

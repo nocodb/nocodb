4027fd2a4cab35445a7113b6c799564b584da82e
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-11-18 12:52:39 +0530

    fix: rspack sync

--- packages/nocodb/src/controllers/org-users.controller.ts
+++ packages/nocodb/src/controllers/org-users.controller.ts
@@ -11,9 +11,11 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { OrgUserRoles } from 'nocodb-sdk';
+// This service is overwritten entirely in the cloud and does not extend there.
+// As a result, it refers to services from OSS to avoid type mismatches.
+import { OrgUsersService } from 'src/services/org-users.service';
 import { GlobalGuard } from '~/guards/global/global.guard';
 import { PagedResponseImpl } from '~/helpers/PagedResponse';
-import { OrgUsersService } from '~/services/org-users.service';
 import { User } from '~/models';
 import { Acl } from '~/middlewares/extract-ids/extract-ids.middleware';
 import { MetaApiLimiterGuard } from '~/guards/meta-api-limiter.guard';
--- packages/nocodb/src/services/command-palette.service.ts
+++ packages/nocodb/src/services/command-palette.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { type UserType, viewTypeAlias } from 'nocodb-sdk';
+// This service is overwritten entirely in the cloud and does not extend there.
+// As a result, it refers to services from OSS to avoid type mismatches.
+import { getCommandPaletteForUserWorkspace } from 'src/helpers/commandPaletteHelpers';
 import { deserializeJSON } from '~/utils/serialize';
-import { getCommandPaletteForUserWorkspace } from '~/helpers/commandPaletteHelpers';
 
 @Injectable()
 export class CommandPaletteService {

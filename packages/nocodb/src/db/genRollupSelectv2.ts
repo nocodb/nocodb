77aa1a94499e3811d2401c3d65c798db72eff49e
Quinn Jarrell <quinnjarr@gmail.com>
2024-11-18 02:56:28 +0000

    Speed up all genRollupSelectv2 by dropping them during links queries and using a CTE for rollups

--- packages/nocodb/src/db/genRollupSelectv2.ts
+++ packages/nocodb/src/db/genRollupSelectv2.ts
@@ -1,9 +1,7 @@
 import { NcDataErrorCodes, RelationTypes, UITypes } from 'nocodb-sdk';
-import type { IBaseModelSqlV2 } from './IBaseModelSqlV2';
-import type { Knex } from 'knex';
-import type {
-  ButtonColumn,
-  FormulaColumn,
+import type { BaseModelSqlv2 } from '~/db/BaseModelSqlv2';
+import type { IBaseModelSqlV2 } from '~/db/IBaseModelSqlV2';
+import {
   LinksColumn,
   LinkToAnotherRecordColumn,
   RollupColumn,
@@ -166,8 +164,7 @@ export default async function ({
         ]),
       ).where(
         knex.ref(
-          `${alias || parentBaseModel.getTnPath(parentModel.table_name)}.${
-            parentCol.column_name
+          `${alias || parentBaseModel.getTnPath(parentModel.table_name)}.${parentCol.column_name
           }`,
         ),
         '=',
@@ -197,8 +194,7 @@ export default async function ({
         ]),
       ).where(
         knex.ref(
-          `${alias || parentBaseModel.getTnPath(parentModel.table_name)}.${
-            parentCol.column_name
+          `${alias || parentBaseModel.getTnPath(parentModel.table_name)}.${parentCol.column_name
           }`,
         ),
         '=',
@@ -221,12 +217,21 @@ export default async function ({
     }
 
     case RelationTypes.MANY_TO_MANY: {
-      const mmModel = await relationColumnOption.getMMModel(mmContext);
-      const mmChildCol = await relationColumnOption.getMMChildColumn(mmContext);
-      const mmParentCol = await relationColumnOption.getMMParentColumn(
-        mmContext,
-      );
-      const assocBaseModel = await Model.getBaseModelSQL(mmContext, {
+      if (columnOptions instanceof LinksColumn) {
+        try {
+          const qb = knex("nc_tdo8___Obstructions").select(1).first()
+          return {
+            builder: qb,
+          };
+        } catch (error) {
+          console.trace(error)
+          throw error
+        }
+      }
+
+      const mmModel = await relationColumnOption.getMMModel(context);
+      const mmChildCol = await relationColumnOption.getMMChildColumn(context);
+      const assocBaseModel = await Model.getBaseModelSQL(context, {
         id: mmModel.id,
         dbDriver: knex,
       });
@@ -235,47 +240,40 @@ export default async function ({
           NcDataErrorCodes.NC_ERR_MM_MODEL_NOT_FOUND,
         ]);
       }
-
-      const qb = knex(
-        knex.raw(`?? as ??`, [
-          parentBaseModel.getTnPath(parentModel?.table_name),
-          refTableAlias,
-        ]),
-      )
+      const prejoined = "prejoined"
+      const qb = knex(refTableAlias
+      ).select("total").withMaterialized(refTableAlias, knex(knex.raw(`?? as ??`, [
+        parentBaseModel.getTnPath(parentModel?.table_name),
+        prejoined,
+      ]))
+        .select(knex.ref(
+          `${assocBaseModel.getTnPath(mmModel.table_name)}.${mmChildCol.column_name
+          }`,
+        ))
+        [columnOptions.rollup_function as string](`${prejoined}.${childCol.column_name} as total`)
         .innerJoin(
           assocBaseModel.getTnPath(mmModel.table_name) as any,
           knex.ref(
-            `${assocBaseModel.getTnPath(mmModel.table_name)}.${
-              mmParentCol.column_name
+            `${assocBaseModel.getTnPath(mmModel.table_name)}.${mmChildCol.column_name
             }`,
           ) as any,
           '=',
-          knex.ref(`${refTableAlias}.${parentCol.column_name}`) as any,
-        )
+          knex.ref(`${prejoined}.${parentCol.column_name}`) as any,
+        ).groupBy(knex.ref(
+          `${assocBaseModel.getTnPath(mmModel.table_name)}.${mmChildCol.column_name
+          }`,
+        )))
         .where(
           knex.ref(
-            `${assocBaseModel.getTnPath(mmModel.table_name)}.${
-              mmChildCol.column_name
+            `${refTableAlias}.${mmChildCol.column_name
             }`,
           ),
           '=',
           knex.ref(
-            `${alias || childBaseModel.getTnPath(childModel.table_name)}.${
-              childCol.column_name
+            `${alias || childBaseModel.getTnPath(childModel.table_name)}.${childCol.column_name
             }`,
           ),
         );
-
-      await extractLinkRelFiltersAndApply({
-        qb: qb,
-        column,
-        alias: refTableAlias,
-        table: parentBaseModel.model,
-        baseModel: parentBaseModel,
-        context: parentBaseModel.context,
-      });
-
-      await applyFunction(qb);
 
       return {
         builder: qb,

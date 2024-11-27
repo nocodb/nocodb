49b348ed8d2e6e8176acc7503888a6a356a5cd36
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-11-27 11:12:44 +0530

    fix(nc-gui): data exporter extension review changes

--- packages/nc-gui/extensions/data-exporter/index.vue
+++ packages/nc-gui/extensions/data-exporter/index.vue
@@ -395,7 +395,10 @@ onMounted(async () => {
           'flex-col': !fullscreen,
         }"
       >
-        <div v-if="fullscreen" class="w-[320px] border-r-1 border-r-nc-border-gray-medium bg-white p-4 pt-t flex flex-col gap-5 nc-scrollbar-thin">
+        <div
+          v-if="fullscreen"
+          class="w-[320px] border-r-1 border-r-nc-border-gray-medium bg-white p-4 pt-t flex flex-col gap-5 nc-scrollbar-thin"
+        >
           <div class="text-base font-bold text-nc-content-gray-extreme">Settings</div>
           <div class="flex flex-col gap-2">
             <div class="text-nc-content-gray font-medium">Table</div>
@@ -522,9 +525,7 @@ onMounted(async () => {
             </a-form-item>
           </div>
         </div>
-        <div class="flex flex-col flex-1 nc-scrollbar-thin" :class="{
-          'm-3 rounded-lg border-1 border-nc-border-gray-medium': fullscreen
-        }">
+        <div class="flex flex-col flex-1 nc-scrollbar-thin">
           <div class="data-exporter-header sticky top-0 z-100">Recent Exports</div>
           <div v-if="exportedFiles.length" class="flex-1 flex flex-col max-h-[calc(100%_-_25px)]">
             <template v-for="exp of exportedFiles">

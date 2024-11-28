7ea61dbf5b720b12232384d78a90b434c662be3d
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-11-28 02:06:59 +0000

    fix:missing col options

--- packages/nc-gui/components/cell/SingleSelect.vue
+++ packages/nc-gui/components/cell/SingleSelect.vue
@@ -204,12 +204,14 @@ async function addIfMissingAndSave() {
         }
       }
 
-      await $api.dbTableColumn.update(
+      const data = await $api.dbTableColumn.update(
         (column.value as { fk_column_id?: string })?.fk_column_id || (column.value?.id as string),
         updatedColMeta,
       )
+
+      column.value.colOptions = data.columns.find((c) => c.id === column.value.id).colOptions
+
       vModel.value = newOptValue
-      await getMeta(column.value.fk_model_id!, true)
     } catch (e: any) {
       console.log(e)
       message.error(await extractSdkResponseErrorMsg(e))

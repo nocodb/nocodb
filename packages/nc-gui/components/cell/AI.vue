5e12d243560ecbf5abd14b896b1673d05b5654fa
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-12-03 20:37:27 +0530

    fix(nc-gui): disable ai prompt button if user does not have purmission to edit data

--- packages/nc-gui/components/cell/AI.vue
+++ packages/nc-gui/components/cell/AI.vue
@@ -13,6 +13,10 @@ const { generateRows, generatingRows, generatingColumnRows, aiIntegrations } = u
 
 const { row } = useSmartsheetRowStoreOrThrow()
 
+const { isUIAllowed } = useRoles()
+
+const isPublic = inject(IsPublicInj, ref(false))
+
 const meta = inject(MetaInj, ref())
 
 const column = inject(ColumnInj) as Ref<
@@ -40,9 +44,7 @@ const isAiEdited = ref(false)
 const isFieldAiIntegrationAvailable = computed(() => {
   const fkIntegrationId = column.value?.colOptions?.fk_integration_id
 
-  if (!fkIntegrationId) return false
-
-  return ncIsArrayIncludes(aiIntegrations.value, fkIntegrationId, 'id')
+  return !!fkIntegrationId
 })
 
 const pk = computed(() => {
@@ -112,6 +114,10 @@ const handleSave = () => {
 }
 
 const debouncedSave = useDebounceFn(handleSave, 1000)
+
+const isDisabledAiButton = computed(() => {
+  return !isFieldAiIntegrationAvailable.value || isLoading.value || isPublic.value || !isUIAllowed('dataEdit')
+})
 </script>
 
 <template>
@@ -122,16 +128,11 @@ const debouncedSave = useDebounceFn(handleSave, 1000)
       'justify-center': isGrid && !isExpandedForm,
     }"
   >
-    <NcTooltip :disabled="isFieldAiIntegrationAvailable" class="flex">
+    <NcTooltip :disabled="isFieldAiIntegrationAvailable || isPublic || isUIAllowed('dataEdit')" class="flex">
       <template #title>
         {{ aiIntegrations.length ? $t('tooltip.aiIntegrationReConfigure') : $t('tooltip.aiIntegrationAddAndReConfigure') }}
       </template>
-      <button
-        class="nc-cell-ai-button nc-cell-button h-6"
-        size="small"
-        :disabled="!isFieldAiIntegrationAvailable || isLoading"
-        @click.stop="generate"
-      >
+      <button class="nc-cell-ai-button nc-cell-button h-6" size="small" :disabled="isDisabledAiButton" @click.stop="generate">
         <div class="flex items-center gap-1">
           <GeneralLoader v-if="isLoading" size="regular" />
           <GeneralIcon v-else icon="ncAutoAwesome" class="h-4 w-4" />

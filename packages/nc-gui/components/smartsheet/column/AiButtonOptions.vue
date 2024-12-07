5abc5cf27e8f4d53998ca6d996893fcb479942b7
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-12-07 19:31:17 +0530

    fix(nc-gui): ai button related changes

--- packages/nc-gui/components/smartsheet/column/AiButtonOptions.vue
+++ packages/nc-gui/components/smartsheet/column/AiButtonOptions.vue
@@ -32,9 +32,12 @@ const {
   formattedData,
   disableSubmitBtn,
   tableExplorerColumns,
+  fromTableExplorer,
 } = useColumnCreateStoreOrThrow()
 
 const { aiIntegrationAvailable, aiLoading, aiError, generateRows } = useNocoAi()
+
+const { isFeatureEnabled } = useBetaFeatureToggle()
 
 const isOpenConfigModal = ref<boolean>(false)
 
@@ -243,6 +246,14 @@ watch(isOpenSelectRecordDropdown, (newValue) => {
   }
 })
 
+const isAiButtonEnabled = computed(() => {
+  if (isEdit.value) {
+    return true
+  }
+
+  return isFeatureEnabled(FEATURE_FLAG.AI_FEATURES)
+})
+
 const previewPanelDom = ref<HTMLElement>()
 
 const isPreviewPanelOnScrollTop = ref(false)
@@ -294,7 +305,7 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div class="relative flex flex-col gap-4">
+  <div v-if="isAiButtonEnabled" class="relative flex flex-col gap-4">
     <AiIntegrationNotFound v-if="!aiIntegrationAvailable" />
     <template v-else-if="!!aiError"> </template>
     <template v-else>
@@ -332,6 +343,7 @@ onBeforeUnmount(() => {
           </NcButton>
 
           <NcButton
+            v-if="!fromTableExplorer"
             size="small"
             type="primary"
             :disabled="disableSubmitBtn || saving"
@@ -604,7 +616,7 @@ onBeforeUnmount(() => {
                                     show-search-always
                                     search-input-placeholder="Search records"
                                     :item-height="60"
-                                    class="!w-auto min-w-[500px] max-w-[576px]"
+                                    class="!w-auto min-w-[550px] max-w-[550px]"
                                     container-class-name="!px-0 !pb-0"
                                     item-class-name="!rounded-none !p-0 !bg-none !hover:bg-none"
                                     @update:value="handleResetOutput"
@@ -691,8 +703,9 @@ onBeforeUnmount(() => {
                   <div class="h-2.5 w-2.5 flex-none absolute -top-[30px] border-1 border-current rounded-full bg-current"></div>
                   <NcTooltip :disabled="!!(selectedRecordPk && outputColumnIds.length && inputColumns.length)">
                     <template #title>
-                      <div class="flex flex-col gap-2">
-                        <div>Preview checklist</div>
+                      <div class="flex flex-col gap-2 py-1 px-0.5">
+                        <div class="text-[10px] leading-[14px] text-gray-300 uppercase mb-1">Preview checklist</div>
+
                         <div class="flex gap-2">
                           <div
                             class="h-4 w-4 rounded-full grid place-items-center children:(h-3.5 w-3.5 flex-none)"
@@ -859,6 +872,7 @@ onBeforeUnmount(() => {
       </div>
     </NcModal>
   </div>
+  <div v-else></div>
 </template>
 
 <style lang="scss">

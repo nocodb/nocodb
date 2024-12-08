a7ceb2a2884ec9c38ea40384d2282df1bd234a0f
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-12-08 04:36:14 +0000

    fix: wip

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -91,6 +91,8 @@ const isPrimitiveType = (val) =>
 
 const JSON_COLUMN_TYPES = [UITypes.Button];
 
+const ORDER_STEP_INCREMENT = 1;
+
 export async function populatePk(
   context: NcContext,
   model: Model,
@@ -6874,17 +6876,13 @@ class BaseModelSqlv2 {
       return null;
     }
 
-    const step = '1.00000000000000000000';
-
     const orderQuery = await this.dbDriver(this.tnPath)
       .max(`${orderColumn.column_name} as max_order`)
       .first();
 
     const order = orderQuery ? orderQuery['max_order'] || '0' : '0';
 
-    const nextOrder = (parseFloat(order) + parseFloat(step)).toFixed(20);
-
-    return nextOrder;
+    return order + ORDER_STEP_INCREMENT;
   }
 
   // method for validating otpions if column is single/multi select
@@ -9797,7 +9795,7 @@ class BaseModelSqlv2 {
     cookie?: { user?: any; system?: boolean },
     // oldData uses title as key where as data uses column_name as key
     oldData?,
-    extra?: { raw?: boolean },
+    extra?: { raw?: boolean; ncOrder?: number },
   ): Promise<void> {
     for (const column of this.model.columns) {
       if (
@@ -9824,7 +9822,8 @@ class BaseModelSqlv2 {
           } else if (column.uidt === UITypes.CreatedBy) {
             data[column.column_name] = cookie?.user?.id;
           } else if (column.uidt === UITypes.Order) {
-            const order = await this.getHighestOrderInTable();
+            const order =
+              extra?.ncOrder ?? (await this.getHighestOrderInTable());
             data[column.column_name] = order;
           }
         }

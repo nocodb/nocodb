2c4e06a19d4e1a5752a75894841b92f6d3e04f0a
mertmit <mertmit99@gmail.com>
2024-12-09 07:35:36 +0000

    refactor: use feature flags

--- packages/nc-gui/components/workspace/integrations/IntegrationsTab.vue
+++ packages/nc-gui/components/workspace/integrations/IntegrationsTab.vue
@@ -19,8 +19,6 @@ const props = withDefaults(
     showFilter: false,
   },
 )
-const easterEggCount = ref(0)
-const easterEggToggle = computed(() => easterEggCount.value > 2)
 
 const { isModal, filterCategory, filterIntegration } = props
 
@@ -254,7 +252,7 @@ watch(activeViewTab, (value) => {
             <GeneralIcon icon="arrowLeft" />
           </NcButton>
           <GeneralIcon icon="gitCommit" class="flex-none h-5 w-5" />
-          <div class="flex-1 text-base font-weight-700" @dblclick="easterEggCount++">New Connection</div>
+          <div class="flex-1 text-base font-weight-700">New Connection</div>
           <div class="flex items-center gap-3">
             <NcButton size="small" type="text" @click="isAddNewIntegrationModalOpen = false">
               <GeneralIcon icon="close" class="text-gray-600" />
@@ -276,7 +274,7 @@ watch(activeViewTab, (value) => {
               }"
             >
               <div class="flex-1">
-                <div class="text-sm font-normal text-gray-600 mb-2" @dblclick="easterEggCount++">
+                <div class="text-sm font-normal text-gray-600 mb-2">
                   <div>
                     Connect integrations with NocoDB.
                     <a href="https://docs.nocodb.com/category/integrations" target="_blank" rel="noopener noreferrer"

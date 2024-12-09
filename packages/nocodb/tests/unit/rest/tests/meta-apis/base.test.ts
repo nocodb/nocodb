775c10d97eca47b659b65871bff1dfeb51c05f57
amandesai01 <amandesai01@gmail.com>
2024-12-09 20:27:36 +0530

    tests(nocodb): meta: user apis

--- packages/nocodb/tests/unit/rest/tests/meta-apis/base.test.ts
+++ packages/nocodb/tests/unit/rest/tests/meta-apis/base.test.ts
@@ -1,10 +1,13 @@
 import 'mocha';
-
+import { expect } from 'chai';
 import request from 'supertest';
+import { v4 as uuidv4 } from 'uuid';
+
+import { OrgUserRoles, ProjectRoles, WorkspacePlan } from 'nocodb-sdk';
 
 import init from '../../../init';
-import { Base, Source } from '../../../../../src/models';
-import { expect } from 'chai';
+import { Base, Source, User, BaseUser } from '../../../../../src/models';
+import { randomTokenString } from '../../../../../src/helpers/stringHelpers';
 
 interface CreateBaseArgs {
   title?: string;
@@ -26,7 +29,7 @@ export default function (API_VERSION: 'v3' | 'v2') {
       context = await init();
     });
 
-    it('Creat & Retrieve Base', async () => {
+    it('Create & Retrieve Base', async () => {
       const base: Base = await createBase({
         title: 'Test Base',
         description: 'Test Description',
@@ -123,6 +126,264 @@ export default function (API_VERSION: 'v3' | 'v2') {
         .set('xc-auth', context.token)
         .send()
         .expect(404);
+    });
+
+    it('List Base Users', async () => {
+      const base: Base = await createBase({
+        title: 'Test Base',
+        description: 'Test Description',
+        meta: {
+          iconColor: '#000000',
+        },
+      });
+
+      const testData = [...Array(30).keys()].map((i) => ({
+        displayName: `Test User${i}`,
+        email: `test-user${i}@nocotest.com`,
+        invite_token: uuidv4(),
+      }));
+
+      const users = await Promise.all(
+        testData.map((testData) =>
+          User.insert({
+            invite_token: testData.invite_token,
+            invite_token_expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+            email: testData.email,
+            display_name: testData.displayName,
+            roles: OrgUserRoles.VIEWER,
+            token_version: randomTokenString(),
+          }),
+        ),
+      );
+
+      if (isEE) {
+        const WorkspaceUser = (
+          await import('../../../../../src/ee/models/WorkspaceUser')
+        ).default;
+        await Promise.all(
+          users.map((user) =>
+            WorkspaceUser.insert({
+              fk_workspace_id: context.fk_workspace_id,
+              fk_user_id: user.id,
+              roles: 'workspace-level-viewer',
+              invited_by: context.user!.id,
+            }),
+          ),
+        );
+      }
+
+      const baseUsers = await Promise.all(
+        users.map((user) =>
+          BaseUser.insert(
+            {
+              workspace_id: context.fk_workspace_id,
+              base_id: base.id,
+            },
+            {
+              fk_workspace_id: context.fk_workspace_id,
+              base_id: base.id,
+              fk_user_id: user.id,
+              roles: ProjectRoles.COMMENTER,
+              invited_by: context.user!.id,
+            },
+          ),
+        ),
+      );
+
+      const response = await request(context.app)
+        .get(`/api/${API_VERSION}/meta/bases/${base.id}/users`)
+        .set('xc-auth', context.token)
+        .send()
+        .expect(200);
+
+      let usersResponseList: any[] = [];
+      if (isV2) {
+        usersResponseList = response.body.users.list;
+        // expect(usersResponseList.length).to.eq(25);
+      } else if (isV3) {
+        usersResponseList = response.body.users;
+        expect(usersResponseList.length).to.eq(31);
+      }
+
+      if (isV2) {
+        expect(response.body.users).to.haveOwnProperty('pageInfo');
+        expect(response.body.users.pageInfo).to.deep.eq({
+          totalRows: 31,
+          page: 1,
+          pageSize: 25,
+          isFirstPage: true,
+          isLastPage: false,
+        });
+      } else {
+        expect(response.body).to.not.haveOwnProperty('pageInfo');
+        expect(response.body.users).to.not.haveOwnProperty('pageInfo');
+      }
+
+      usersResponseList.forEach((responseBaseUser) => {
+        const baseUser = baseUsers.find(
+          // @ts-expect-error type not present, but data present
+          (bu) => bu.id === responseBaseUser.id,
+        )!;
+        if (responseBaseUser.roles === 'owner') return; // skip testing for org level user;
+        const underlyingUser = users.find((u) => u.id === responseBaseUser.id)!;
+
+        matchBaseUser(responseBaseUser, baseUser, underlyingUser);
+      });
+    });
+
+    it.only('Create, Update and Delete Base Users', async () => {
+      const base: Base = await createBase({
+        title: 'Test Base',
+        description: 'Test Description',
+        meta: {
+          iconColor: '#000000',
+        },
+      });
+
+      const TEST_EMAILS = [...Array(5).keys()].map(
+        (i) => `test-crud${i}@nocodb.com`,
+      );
+      const createUserArgsList: any[] = [];
+
+      TEST_EMAILS.forEach((TEST_EMAIL) => {
+        const createUserArgs: any = {
+          email: TEST_EMAIL,
+        };
+
+        if (isV2) {
+          createUserArgs.roles = ProjectRoles.COMMENTER;
+        } else if (isV3) {
+          createUserArgs.base_role = ProjectRoles.COMMENTER;
+        }
+
+        if (isEE) {
+          if (isV3) {
+            createUserArgs.workspace_role = `workspace-level-${ProjectRoles.COMMENTER}`;
+          }
+        }
+
+        createUserArgsList.push(createUserArgs);
+      });
+
+      await request(context.app)
+        .post(`/api/${API_VERSION}/meta/bases/${base.id}/users`)
+        .set('xc-auth', context.token)
+        .send(isV2 ? createUserArgsList[0] : createUserArgsList)
+        .expect(isV3 ? 201 : 200);
+
+      let users: any[] = (await fetchUsersFromBase(base.id)).filter((u) =>
+        (u.email as string).startsWith('test-crud'),
+      );
+
+      if (isV2) {
+        expect(users.length).to.eq(1);
+        const addedUser = users.find((u) => u.email === TEST_EMAILS[0]);
+
+        expect(!!addedUser).to.eq(true);
+        expect(addedUser.email).to.eq(TEST_EMAILS[0]);
+        expect(addedUser.roles).to.eq(ProjectRoles.COMMENTER);
+
+        if (isEE) {
+          // No access is given by default in v2;
+          expect(addedUser.workspace_roles).to.eq(
+            `workspace-level-${ProjectRoles.NO_ACCESS}`,
+          );
+        }
+      } else if (isV3) {
+        expect(users.length).to.eq(5);
+        for (let index = 0; index < TEST_EMAILS.length; index++) {
+          const TEST_EMAIL = TEST_EMAILS[index];
+          const addedUser = users.find((u) => u.email === TEST_EMAIL);
+
+          expect(!!addedUser).to.eq(true);
+          expect(addedUser.email).to.eq(TEST_EMAILS[0]);
+          expect(addedUser.base_role).to.eq(ProjectRoles.COMMENTER);
+
+          if (isEE) {
+            expect(addedUser.workspace_role).to.eq(
+              `workspace-level-${ProjectRoles.COMMENTER}`,
+            );
+          }
+        }
+      }
+
+      const updateQueries = [
+        {
+          id: users[0].id,
+          email: 'test-crud-new@nocodb.com',
+          workspace_role: `workspace-level-${ProjectRoles.CREATOR}`,
+          [isV3 ? 'base_role' : 'roles']: ProjectRoles.EDITOR,
+        },
+      ];
+
+      if (isV2) {
+        await request(context.app)
+          .patch(`/api/${API_VERSION}/meta/bases/${base.id}/users/${updateQueries[0].id}`)
+          .set('xc-auth', context.token)
+          .send(updateQueries[0])
+          .expect(200);
+      } else if (isV3) {
+        await request(context.app)
+          .patch(`/api/${API_VERSION}/meta/bases/${base.id}/users`)
+          .set('xc-auth', context.token)
+          .send(updateQueries)
+          .expect(200);
+      }
+
+      const usersUpdated = await fetchUsersFromBase(base.id);
+      const updatedUser = usersUpdated.find(
+        (u) => u.id === updateQueries[0].id,
+      );
+
+      expect(!!updatedUser).to.eq(true);
+      expect(updatedUser.email).to.eq('test-crud0@nocodb.com'); // Check that email should NOT have been updated
+
+      if (isV2) {
+        expect(updatedUser.roles).to.eq(ProjectRoles.EDITOR);
+      } else if (isV3) {
+        expect(updatedUser.base_role).to.eq(ProjectRoles.EDITOR);
+      }
+
+      if (isEE && isV3) {
+        expect(updatedUser.workspace_roles).to.eq(
+          `workspace-level-${ProjectRoles.CREATOR}`,
+        );
+      }
+
+      const deleteQueries = [
+        {
+          id: updatedUser.id,
+        },
+        {
+          email: `test-crud1@nocodb.com`,
+        },
+      ];
+
+      if (isV2) {
+        // Need to iterate for v2, but v3 should support bulk delete by default
+        await request(context.app)
+          .delete(
+            `/api/${API_VERSION}/meta/bases/${base.id}/users/${updatedUser.id}`,
+          )
+          .set('xc-auth', context.token)
+          .expect(200);
+      } else if (isV3) {
+        await request(context.app)
+          .delete(`/api/${API_VERSION}/meta/bases/${base.id}/users`)
+          .set('xc-auth', context.token)
+          .send(deleteQueries)
+          .expect(200);
+      }
+
+      const availableUsers = (await fetchUsersFromBase(base.id)).filter((u) =>
+        (u.email as string).startsWith('test-crud'),
+      );
+
+      if (isV3) {
+        expect(availableUsers.length).to.eq(3); // 2 deleted out of 5
+      } else if (isV2) {
+        expect(availableUsers.length).to.eq(0); // 1 deleted out of 1
+      }
     });
 
     function matchBase(responseBase: any, base: any) {
@@ -358,6 +619,90 @@ export default function (API_VERSION: 'v3' | 'v2') {
       }
     }
 
+    function matchBaseUser(
+      responseBaseUser: any,
+      baseUser: BaseUser,
+      underlyingUser: User,
+      isDeleted: boolean = false,
+    ) {
+      expect(responseBaseUser).to.haveOwnProperty('id');
+      expect(responseBaseUser.id).to.eq(underlyingUser.id);
+
+      expect(responseBaseUser).to.haveOwnProperty('email');
+      expect(responseBaseUser.email).to.eq(underlyingUser.email);
+
+      expect(responseBaseUser).to.haveOwnProperty('display_name');
+      expect(responseBaseUser.display_name).to.eq(underlyingUser.display_name);
+
+      expect(responseBaseUser).to.haveOwnProperty('created_at');
+
+      if (isV2) {
+        expect(responseBaseUser).to.haveOwnProperty('invite_token');
+        expect(responseBaseUser.invite_token).to.eq(
+          underlyingUser.invite_token,
+        );
+
+        expect(responseBaseUser).to.haveOwnProperty('main_roles');
+        expect(responseBaseUser.main_roles).to.eq(underlyingUser.roles);
+
+        expect(responseBaseUser).to.haveOwnProperty('base_id');
+        expect(responseBaseUser.base_id).to.eq(baseUser.base_id);
+
+        expect(responseBaseUser).to.haveOwnProperty('roles');
+        expect(responseBaseUser.roles).to.eq(baseUser.roles);
+      } else if (isV3) {
+        expect(responseBaseUser).to.not.haveOwnProperty('invite_token');
+        expect(responseBaseUser).to.not.haveOwnProperty('main_roles');
+        expect(responseBaseUser).to.not.haveOwnProperty('base_id');
+        expect(responseBaseUser).to.not.haveOwnProperty('roles');
+
+        expect(responseBaseUser).to.haveOwnProperty('base_role');
+        expect(responseBaseUser.base_role).to.eq(baseUser.roles);
+      }
+
+      if (isEE) {
+        expect(responseBaseUser).to.haveOwnProperty('workspace_id');
+        expect(responseBaseUser.workspace_id).to.eq(context.fk_workspace_id);
+
+        if (isV2) {
+          expect(responseBaseUser).to.haveOwnProperty('workspace_roles');
+          expect(responseBaseUser.workspace_roles).to.eq(
+            'workspace-level-viewer',
+          );
+
+          expect(responseBaseUser).to.haveOwnProperty('deleted');
+          expect(responseBaseUser.deleted).to.eq(isDeleted);
+        } else if (isV3) {
+          expect(responseBaseUser).to.not.haveOwnProperty('workspace_roles');
+          expect(responseBaseUser).to.not.haveOwnProperty('deleted');
+
+          expect(responseBaseUser).to.haveOwnProperty('workspace_role');
+          expect(responseBaseUser.workspace_role).to.eq(
+            'workspace-level-viewer',
+          );
+        }
+      }
+    }
+
+    async function fetchUsersFromBase(baseId: string): Promise<any[]> {
+      if (isEE) {
+        const BaseUser = (await import('../../../../../src/ee/models/BaseUser'))
+          .default;
+        return BaseUser.getUsersList(
+          { workspace_id: context.fk_workspace_id },
+          {
+            base_id: baseId,
+            mode: 'viewer',
+          },
+        );
+      } else {
+        return BaseUser.getUsersList(context, {
+          base_id: baseId,
+          mode: 'viewer',
+        });
+      }
+    }
+
     // ===========================
     // ---------- UTILS ----------
     // ===========================
@@ -378,7 +723,7 @@ export default function (API_VERSION: 'v3' | 'v2') {
           },
           ...(isEE ? { fk_workspace_id: context.fk_workspace_id } : {}),
         })
-        .expect(201);
+        .expect(isEE ? 201 : isV3 ? 201 : 200);
       return response.body;
     }
 

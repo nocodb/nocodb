f3cbf7528886131e421050f4bb6b9f99e5b51a82
Pranav C <pranavxc@gmail.com>
2024-12-09 09:54:21 +0000

    fix: shared url and dashboard url corrections

--- packages/nocodb/src/run/cloud.ts
+++ packages/nocodb/src/run/cloud.ts
@@ -15,6 +15,16 @@ server.use(
   process.env.NC_DASHBOARD_URL ?? '/dashboard',
   express.static(path.join(__dirname, 'nc-gui')),
 );
+
+// if NC_DASHBOARD_URL is not set to /dashboard, then redirect '/dashboard'
+// to the path set in NC_DASHBOARD_URL
+if (!/^\/?dashboard\/?$/.test(process.env.NC_DASHBOARD_URL)) {
+  server.use('/dashboard', (_, res) => {
+    // redirect to NC_DASHBOARD_URL path
+    res.redirect(process.env.NC_DASHBOARD_URL);
+  });
+}
+
 server.set('view engine', 'ejs');
 
 (async () => {

22724c20c3459f902d1f592efca60b51fda22a08
amandesai01 <amandesai01@gmail.com>
2024-12-18 20:43:17 +0530

    feat(nc-gui): number format options for rollup column

--- packages/nc-gui/components/smartsheet/column/DecimalOptions.vue
+++ packages/nc-gui/components/smartsheet/column/DecimalOptions.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { UITypes } from 'nocodb-sdk'
+import { makePrecisionFormatsDiplay, precisionFormats } from '../../../helpers/columnDefaultMeta'
 
 const props = defineProps<{
   value: any
@@ -7,22 +8,11 @@ const props = defineProps<{
 
 const emit = defineEmits(['update:value'])
 
-const precisionFormats = [1, 2, 3, 4, 5, 6, 7, 8]
-
 const { t } = useI18n()
 
-const precisionFormatsDisplay = {
-  1: t('placeholder.decimal1'),
-  2: t('placeholder.decimal2'),
-  3: t('placeholder.decimal3'),
-  4: t('placeholder.decimal4'),
-  5: t('placeholder.decimal5'),
-  6: t('placeholder.decimal6'),
-  7: t('placeholder.decimal7'),
-  8: t('placeholder.decimal8'),
-}
-
 const vModel = useVModel(props, 'value', emit)
+
+const precisionFormatsDisplay = makePrecisionFormatsDiplay(t)
 
 // set default value
 vModel.value.meta = {

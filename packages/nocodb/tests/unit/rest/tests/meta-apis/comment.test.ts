c1f95247604f9c8623d84974db456f045e0383ac
amandesai01 <amandesai01@gmail.com>
2024-12-24 04:45:01 +0000

    tests(nocodb): meta: comment p2

--- packages/nocodb/tests/unit/rest/tests/meta-apis/comment.test.ts
+++ packages/nocodb/tests/unit/rest/tests/meta-apis/comment.test.ts
@@ -55,17 +55,18 @@ export default async function (API_VERSION: 'v1' | 'v2' | 'v3') {
 
       await request(context.app)
         .post(`${META_API_COMMENTS_ROUTE}`)
+        .set('xc-auth', context.token)
         .send({
           comment: TEST_COMMENT_1,
-          row_id: firstRowId,
+          row_id: `${firstRowId}`,
           [isV3 ? 'table_id' : 'fk_model_id']: table.id,
         })
-        .expect(201);
+        .expect(isV3 ? 201 : 200);
 
       response = await request(context.app)
         .get(`${META_API_COMMENTS_ROUTE}`)
         .query({
-          row_id: firstRowId,
+          row_id: `${firstRowId}`,
           [isV3 ? 'table_id' : 'fk_model_id']: table.id,
         })
         .set('xc-auth', context.token)
@@ -75,34 +76,99 @@ export default async function (API_VERSION: 'v1' | 'v2' | 'v3') {
       expect(commentsResponse.length).to.eq(1);
 
       let commentResponse = commentsResponse[0];
-      
-      expect(commentResponse).to.haveOwnProperty('');
-      expect(commentResponse).to.haveOwnProperty('');
-      expect(commentResponse).to.haveOwnProperty('');
-      expect(commentResponse).to.haveOwnProperty('');
-      expect(commentResponse).to.haveOwnProperty('');
-      expect(commentResponse).to.haveOwnProperty('');
-      expect(commentResponse).to.haveOwnProperty('');
-      expect(commentResponse).to.haveOwnProperty('');
+
+      expect(commentResponse).to.haveOwnProperty('id');
+
+      expect(commentResponse).to.haveOwnProperty('comment');
+      expect(commentResponse.comment).to.eq(TEST_COMMENT_1);
+
+      expect(commentResponse).to.haveOwnProperty('created_at');
+      expect(commentResponse).to.haveOwnProperty('updated_at');
+
+      if (isV1 || isV2) {
+        expect(commentResponse).to.haveOwnProperty('source_id');
+
+        expect(commentResponse).to.haveOwnProperty('base_id');
+        expect(commentResponse.base_id).to.eq(base.id);
+
+        expect(commentResponse).to.haveOwnProperty('fk_model_id');
+        expect(commentResponse.fk_model_id).to.eq(table.id);
+
+        expect(commentResponse).to.haveOwnProperty('row_id');
+        expect(commentResponse.row_id).to.eq(`${firstRowId}`);
+
+        expect(commentResponse).to.haveOwnProperty('created_by');
+        expect(commentResponse).to.haveOwnProperty('created_by_email');
+        expect(commentResponse).to.haveOwnProperty('resolved_by');
+        expect(commentResponse).to.haveOwnProperty('resolved_by_email');
+
+        expect(commentResponse).to.haveOwnProperty('parent_comment_id');
+        expect(commentResponse.parent_comment_id).to.eq(null);
+
+        // expect(commentResponse).to.haveOwnProperty('status');
+      } else if (isV3) {
+        expect(commentResponse).to.haveOwnProperty('created_by');
+        expect(commentResponse.created_by).to.have.keys([
+          'id',
+          'name',
+          'email',
+        ]);
+
+        expect(commentResponse).to.haveOwnProperty('resolved_by');
+        expect(commentResponse.resolved_by).to.have.keys([
+          'id',
+          'name',
+          'email',
+        ]);
+      }
+
+      // For V1 and V2, path doesn't end with plural
+      const patchOrDelBasePath = isV3 ? META_API_COMMENTS_ROUTE : META_API_COMMENTS_ROUTE.replace("comments", "comment");
+      await request(context.app)
+        .patch(`${patchOrDelBasePath}/${commentResponse.id}`)
+        .set('xc-auth', context.token)
+        .send({
+          comment: TEST_COMMENT_2,
+          row_id: `${firstRowId}`,
+          [isV3 ? 'table_id' : 'fk_model_id']: table.id,
+        })
+        .expect(isV3 ? 201 : 200);
+
+      response = await request(context.app)
+        .get(`${META_API_COMMENTS_ROUTE}`)
+        .query({
+          row_id: `${firstRowId}`,
+          [isV3 ? 'table_id' : 'fk_model_id']: table.id,
+        })
+        .set('xc-auth', context.token)
+        .expect(200);
+      commentsResponse = isV3 ? response.body : response.body.list;
+      expect(commentsResponse).to.be.not.be.undefined;
+      expect(commentsResponse.length).to.eq(1);
+
+      commentResponse = commentsResponse[0];
+      expect(commentResponse).to.haveOwnProperty('comment');
+      expect(commentResponse.comment).to.eq(TEST_COMMENT_2);
+
+      await request(context.app)
+        .delete(`${patchOrDelBasePath}/${commentResponse.id}`)
+        .set('xc-auth', context.token)
+        .expect(200);
+
+      response = await request(context.app)
+        .get(`${META_API_COMMENTS_ROUTE}`)
+        .query({
+          row_id: `${firstRowId}`,
+          [isV3 ? 'table_id' : 'fk_model_id']: table.id,
+        })
+        .set('xc-auth', context.token)
+        .expect(200);
+
+      commentsResponse = isV3 ? response.body : response.body.list;
+      expect(commentsResponse).to.be.not.be.undefined;
+      expect(commentsResponse.length).to.eq(0);
     });
   }
 
   describe(`Comments Meta Tests ${API_VERSION}`, commentsTests);
 }
-
-/**
- * {
-"id": "adt_3sii7erfwrlegb",
-"source_id": null,
-"base_id": "p_63b4q0qengen1x",
-"fk_model_id": "md_5mipbdg6ketmv8",
-"row_id": "1",
-"created_by": "",
-"resolved_by": "",
-"parent_comment_id": null,
-"status": null,
-"comment": "bar",
-"created_at": "2023-03-13T09:39:14.225Z",
-"updated_at": "2023-03-13T09:39:14.225Z"
-}
- */

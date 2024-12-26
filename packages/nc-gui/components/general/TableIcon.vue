59ff4c4001335b4d1e00b48e03eb73285801f390
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-12-26 12:29:12 +0000

    fix(nc-gui): update table icon related ui changes

--- packages/nc-gui/components/general/TableIcon.vue
+++ packages/nc-gui/components/general/TableIcon.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
 import type { TableType } from 'nocodb-sdk'
 
-const { meta: tableMeta } = defineProps<{
-  meta: TableType
-}>()
+const { meta: tableMeta } = withDefaults(
+  defineProps<{
+    meta?: TableType
+  }>(),
+  {
+    meta: () => ({} as TableType),
+  },
+)
 </script>
 
 <template>
@@ -17,5 +22,5 @@ const { meta: tableMeta } = defineProps<{
   />
 
   <component :is="iconMap.eye" v-else-if="tableMeta?.type === 'view'" class="w-4 mx-0.75" />
-  <component :is="iconMap.table" v-else class="w-4 mx-0.5 !text-gray-600/75" />
+  <component :is="iconMap.table" v-else class="w-4 mx-0.5 text-gray-600/75" />
 </template>

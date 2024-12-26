b2dcf306325748ee00dfd28e0621d0af61effce6
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-12-26 08:01:47 +0100

    fix(nc-gui): MFE test cases

--- packages/nc-gui/components/smartsheet/details/Fields.vue
+++ packages/nc-gui/components/smartsheet/details/Fields.vue
@@ -1936,7 +1936,7 @@ watch(activeAiTab, (newValue) => {
                           <NcMenu variant="small">
                             <NcMenuItemCopyId
                               :id="displayColumn.id!"
-                              data-testid="nc-field-item-id"
+                              data-testid="nc-field-item-action-copy-id"
                               :tooltip="$t('msg.clickToCopyFieldId')"
                               :label="
                                 $t('labels.idColon', {
@@ -2006,10 +2006,6 @@ watch(activeAiTab, (newValue) => {
 
 .nc-dropdown-table-explorer-display-column {
   @apply !overflow-hidden;
-}
-
-.nc-dropdown-table-explorer-display-column > div > ul.ant-dropdown-menu.nc-menu {
-  @apply !py-1.5;
 }
 </style>
 

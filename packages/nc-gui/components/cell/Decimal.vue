e5dae6206260f99c7190dd09fb82382cc2ebe38f
amandesai01 <amandesai01@gmail.com>
2024-12-27 15:33:49 +0530

    Revert "fix(nc-gui): digit only input"
    This reverts commit 9d7ef53fb954bc823c6049e0e2976d9082412278.

--- packages/nc-gui/components/cell/Decimal.vue
+++ packages/nc-gui/components/cell/Decimal.vue
@@ -111,7 +111,6 @@ watch(isExpandedFormOpen, () => {
     class="nc-cell-field outline-none py-1 border-none rounded-md w-full h-full"
     type="number"
     :step="precision"
-    pattern="\d*"
     :placeholder="placeholder"
     style="letter-spacing: 0.06rem"
     @blur="editEnabled = false"

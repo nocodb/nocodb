f687eeefa3cd85e7ec4a40da8b928ccf80d2cf1e
mertmit <mertmit99@gmail.com>
2024-12-30 08:46:30 +0000

    chore: add optimize deps

--- packages/nc-gui/nuxt.config.ts
+++ packages/nc-gui/nuxt.config.ts
@@ -213,6 +213,7 @@ export default defineNuxtConfig({
     },
     optimizeDeps: {
       include: [
+        '@ckpack/vue-color',
         '@tiptap/core',
         '@tiptap/extension-link',
         '@tiptap/extension-mention',
@@ -226,6 +227,7 @@ export default defineNuxtConfig({
         '@vue-flow/core',
         '@vuelidate/core',
         '@vuelidate/validators',
+        '@vueuse/integrations/useQRCode',
         'company-email-validator',
         'crossoriginworker',
         'dayjs/plugin/utc',

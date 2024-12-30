0bf9e38f250c2b3fa3194e9ae56b29827741ffa6
mertmit <mertmit99@gmail.com>
2024-12-30 15:35:10 +0000

    fix: gracefully terminate connections

--- packages/nocodb/src/modules/jobs/jobs.controller.ts
+++ packages/nocodb/src/modules/jobs/jobs.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { customAlphabet } from 'nanoid';
+import type { OnApplicationShutdown } from '@nestjs/common';
 import type { Response } from 'express';
 import { JobStatus } from '~/interface/Jobs';
 import { JobEvents } from '~/interface/Jobs';
@@ -26,7 +27,7 @@ const POLLING_INTERVAL = 30000;
 
 @Controller()
 @UseGuards(MetaApiLimiterGuard, GlobalGuard)
-export class JobsController {
+export class JobsController implements OnApplicationShutdown {
   constructor(
     @Inject('JobsService') private readonly jobsService: IJobsService,
   ) {}
@@ -294,6 +295,21 @@ export class JobsController {
       await JobsRedis.publish(jobId, {
         cmd: JobEvents.LOG,
         ...data,
+      });
+    }
+  }
+
+  async onApplicationShutdown() {
+    /*
+     * Close all long polling connections
+     */
+    for (const jobId in this.jobRooms) {
+      this.jobRooms[jobId].listeners.forEach((res) => {
+        if (!res.headersSent) {
+          res.send({
+            status: 'close',
+          });
+        }
       });
     }
   }

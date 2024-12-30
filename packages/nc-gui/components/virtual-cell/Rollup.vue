a117abee5d9a23fbed65b6246daeb53d6a37cc88
amandesai01 <amandesai01@gmail.com>
2024-12-30 17:34:04 +0530

    fix(nc-gui): nf for rollup column: typings

--- packages/nc-gui/components/virtual-cell/Rollup.vue
+++ packages/nc-gui/components/virtual-cell/Rollup.vue
@@ -46,7 +46,7 @@ const renderAsTextFun = computed(() => {
 
 <template>
   <div class="nc-cell-field" @dblclick="activateShowEditNonEditableFieldWarning">
-    <div v-if="renderAsTextFun.includes((colOptions as any).rollup_function)">
+    <div v-if="renderAsTextFun.includes((colOptions as RollupType).rollup_function!)">
       <LazyCellDecimal :model-value="value"/>
     </div>
     <LazySmartsheetCell v-else v-model="value" :column="childColumn" :edit-enabled="false" :read-only="true" />

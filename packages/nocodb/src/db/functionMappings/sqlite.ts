5ccc602ab8ef767037b5a70323ec3561e9cf9ddd
Mert E <mertmit99@gmail.com>
2025-01-02 15:19:38 +0000

    feat: year fn for formula (#10171)

--- packages/nocodb/src/db/functionMappings/sqlite.ts
+++ packages/nocodb/src/db/functionMappings/sqlite.ts
@@ -208,6 +208,42 @@ const sqlite3 = {
       ),
     };
   },
+  DAY: async ({ fn, knex, pt, colAlias }: MapFnArgs) => {
+    return {
+      builder: knex.raw(
+        `CAST(strftime('%d', (${
+          (await fn(pt?.arguments[0])).builder
+        })) AS INTEGER) ${colAlias}`,
+      ),
+    };
+  },
+  MONTH: async ({ fn, knex, pt, colAlias }: MapFnArgs) => {
+    return {
+      builder: knex.raw(
+        `CAST(strftime('%m', (${
+          (await fn(pt?.arguments[0])).builder
+        })) AS INTEGER) ${colAlias}`,
+      ),
+    };
+  },
+  YEAR: async ({ fn, knex, pt, colAlias }: MapFnArgs) => {
+    return {
+      builder: knex.raw(
+        `CAST(strftime('%Y', (${
+          (await fn(pt?.arguments[0])).builder
+        })) AS INTEGER) ${colAlias}`,
+      ),
+    };
+  },
+  HOUR: async ({ fn, knex, pt, colAlias }: MapFnArgs) => {
+    return {
+      builder: knex.raw(
+        `CAST(strftime('%H', (${
+          (await fn(pt?.arguments[0])).builder
+        })) AS INTEGER) ${colAlias}`,
+      ),
+    };
+  },
   AND: async (args: MapFnArgs) => {
     return {
       builder: args.knex.raw(

e663dbaf1b29dfe5d80b2a89350f722583f6b389
amandesai01 <amandesai01@gmail.com>
2025-01-04 17:37:42 +0530

    fix(nc-gui): record editor: field border and height

--- packages/nc-gui/components/smartsheet/DivDataCell.vue
+++ packages/nc-gui/components/smartsheet/DivDataCell.vue
@@ -34,8 +34,14 @@ const onTabPress = () => {
 <style lang="scss" scoped>
 .nc-data-cell:focus-within {
   @apply !border-1 !border-brand-500 !rounded-lg !shadow-none !ring-0;
+  // Mimic ant's input box
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 150ms;
+  box-shadow: 0px 0px 0px 2px rgba(51, 102, 255, 0.24) !important;
 }
 .nc-data-cell {
-  @apply border-1 border-gray-200 overflow-hidden rounded-lg;
+  @apply border-1 border-gray-200 overflow-hidden rounded-lg shadow;
+  box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.08);
 }
 </style>

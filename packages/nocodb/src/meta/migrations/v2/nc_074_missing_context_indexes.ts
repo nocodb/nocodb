c6526e6ab843d261799cdee22c6482ce817e69fd
mertmit <mertmit99@gmail.com>
2025-01-09 14:01:37 +0000

    fix: missing column

--- packages/nocodb/src/meta/migrations/v2/nc_074_missing_context_indexes.ts
+++ packages/nocodb/src/meta/migrations/v2/nc_074_missing_context_indexes.ts
@@ -1,8 +1,15 @@
 import type { Knex } from 'knex';
 import { MetaTable } from '~/utils/globals';
+import { isEE } from '~/utils';
 
 const up = async (knex: Knex) => {
   console.time('nc_074_missing_context_indexes');
+
+  if (!isEE) {
+    await knex.schema.alterTable(MetaTable.COL_BUTTON, (table) => {
+      table.string('fk_workspace_id', 20);
+    });
+  }
 
   await knex.schema.alterTable(MetaTable.COL_BUTTON, (table) => {
     table.index('fk_column_id');

f9585b319d52f240a475b3731c6c107d7f0d8a5f
mertmit <mertmit99@gmail.com>
2025-01-09 14:01:37 +0000

    fix: avoid migration errors

--- packages/nocodb/src/meta/migrations/v2/nc_074_missing_context_indexes.ts
+++ packages/nocodb/src/meta/migrations/v2/nc_074_missing_context_indexes.ts
@@ -20,21 +20,12 @@ const up = async (knex: Knex) => {
     table.index(['base_id', 'fk_workspace_id'], 'nc_col_long_text_context');
   });
 
-  await knex.schema.alterTable(MetaTable.CUSTOM_URLS, (table) => {
-    table.index('custom_path');
-    table.index(['base_id', 'fk_workspace_id'], 'nc_custom_urls_context');
-  });
-
   await knex.schema.alterTable(MetaTable.DATA_REFLECTION, (table) => {
     table.index('fk_workspace_id');
   });
 
   await knex.schema.alterTable(MetaTable.JOBS, (table) => {
     table.index(['base_id', 'fk_workspace_id'], 'nc_jobs_context');
-  });
-
-  await knex.schema.alterTable(MetaTable.SNAPSHOT, (table) => {
-    table.index(['base_id', 'fk_workspace_id'], 'nc_snapshot_context');
   });
 
   await knex.schema.alterTable(MetaTable.USERS, (table) => {
@@ -45,6 +36,12 @@ const up = async (knex: Knex) => {
 };
 
 const down = async (knex) => {
+  if (!isEE) {
+    await knex.schema.alterTable(MetaTable.COL_BUTTON, (table) => {
+      table.dropColumn('fk_workspace_id');
+    });
+  }
+
   await knex.schema.alterTable(MetaTable.COL_BUTTON, (table) => {
     table.dropIndex('fk_column_id');
     table.dropIndex(['base_id', 'fk_workspace_id'], 'nc_col_button_context');
@@ -54,21 +51,12 @@ const down = async (knex) => {
     table.dropIndex(['base_id', 'fk_workspace_id'], 'nc_col_long_text_context');
   });
 
-  await knex.schema.alterTable(MetaTable.CUSTOM_URLS, (table) => {
-    table.dropIndex('custom_path');
-    table.dropIndex(['base_id', 'fk_workspace_id'], 'nc_custom_urls_context');
-  });
-
   await knex.schema.alterTable(MetaTable.DATA_REFLECTION, (table) => {
     table.dropIndex('fk_workspace_id');
   });
 
   await knex.schema.alterTable(MetaTable.JOBS, (table) => {
     table.dropIndex(['base_id', 'fk_workspace_id'], 'nc_jobs_context');
-  });
-
-  await knex.schema.alterTable(MetaTable.SNAPSHOT, (table) => {
-    table.dropIndex(['base_id', 'fk_workspace_id'], 'nc_snapshot_context');
   });
 
   await knex.schema.alterTable(MetaTable.USERS, (table) => {

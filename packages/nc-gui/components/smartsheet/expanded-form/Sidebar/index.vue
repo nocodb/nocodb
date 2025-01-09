34e336fe397e30ac4f76fe5a70958289fe3cd5dd
Yoones Khoshghadam <yooneskh@gmail.com>
2025-01-09 10:56:44 +0330

    fix (nc-gui): move records audit under a feature flag

--- packages/nc-gui/components/smartsheet/expanded-form/Sidebar/index.vue
+++ packages/nc-gui/components/smartsheet/expanded-form/Sidebar/index.vue
@@ -4,7 +4,8 @@ const props = defineProps<{
   showFieldsTab?: boolean
 }>()
 
-const { appInfo } = useGlobal()
+const { isFeatureEnabled } = useBetaFeatureToggle()
+const isAuditsEnabled = computed(() => isFeatureEnabled(FEATURE_FLAG.EXPANDED_FORM_RECORD_AUDITS))
 
 const tab = ref<'fields' | 'comments' | 'audits'>(props.showFieldsTab ? 'fields' : 'comments')
 </script>
@@ -32,18 +33,18 @@ const tab = ref<'fields' | 'comments' | 'audits'>(props.showFieldsTab ? 'fields'
         <SmartsheetExpandedFormSidebarComments />
       </a-tab-pane>
 
-      <a-tab-pane key="audits" class="w-full">
+      <a-tab-pane key="audits" :disabled="!isAuditsEnabled" class="w-full">
         <template #tab>
-          <!-- <NcTooltip v-if="appInfo.ee" class="tab flex-1">
+          <NcTooltip v-if="!isAuditsEnabled" class="tab flex-1">
             <template #title>{{ $t('title.comingSoon') }}</template>
 
             <div v-e="['c:row-expand:audit']" class="flex items-center gap-2 text-gray-400">
               <GeneralIcon icon="audit" class="w-4 h-4" />
               <span class="<lg:hidden"> {{ $t('title.audits') }} </span>
             </div>
-          </NcTooltip> -->
+          </NcTooltip>
 
-          <div v-e="['c:row-expand:audit']" class="flex items-center gap-2">
+          <div v-else v-e="['c:row-expand:audit']" class="flex items-center gap-2">
             <GeneralIcon icon="audit" class="w-4 h-4" />
             <span class="<lg:hidden"> {{ $t('title.audits') }} </span>
           </div>

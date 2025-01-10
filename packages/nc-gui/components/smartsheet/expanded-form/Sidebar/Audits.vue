c5d7be5255127f23314644fc102e101485caab78
Yoones Khoshghadam <yooneskh@gmail.com>
2025-01-10 12:02:56 +0000

    fix (nc-gui): records audit text diff and link ui polish

--- packages/nc-gui/components/smartsheet/expanded-form/Sidebar/Audits.vue
+++ packages/nc-gui/components/smartsheet/expanded-form/Sidebar/Audits.vue
@@ -92,13 +92,15 @@ watch(
             <div v-if="audit?.op_type === 'DATA_INSERT'" class="pl-9">created the record.</div>
             <div v-else-if="audit?.op_type === 'DATA_LINK'" class="pl-9">
               <span class="!text-sm px-1 py-0.5 text-green-700 font-weight-500 border-1 border-green-200 rounded-md bg-green-50">
-                Record with ID: {{ JSON.parse(audit.details)?.ref_row_id }} was linked
+                {{ JSON.parse(audit.details)?.ref_display_value || 'Record' }}
               </span>
+              was linked
             </div>
             <div v-else-if="audit?.op_type === 'DATA_UNLINK'" class="pl-9">
               <span class="!text-sm px-1 py-0.5 text-red-700 font-weight-500 border-1 border-red-200 rounded-md bg-red-50">
-                Record with ID: {{ JSON.parse(audit.details)?.ref_row_id }} was unlinked
+                {{ JSON.parse(audit.details)?.ref_display_value || 'Record' }}
               </span>
+              was unlinked
             </div>
             <template v-else-if="audit?.op_type === 'DATA_UPDATE'">
               <div class="ml-9 rounded-lg border-1 border-gray-200 bg-gray-50 divide-y">

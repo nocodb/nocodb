aed8a87fb7c3e013fb5477ba50de9718ca992dd5
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-01-14 01:48:57 +0300

    fix(nc-gui): remove console

--- packages/nc-gui/helpers/tiptap/extensions/nodes/paragraph.ts
+++ packages/nc-gui/helpers/tiptap/extensions/nodes/paragraph.ts
@@ -15,16 +15,16 @@ export const Paragraph = Node.create<any, { markdown: MarkdownNodeSpec }>({
 
           // Check if it's the last block in the document
           const isLastNode = parent && parent.child(parent.childCount - 1) === node
-          console.log('isEmpty', isEmpty, isLastNode)
-          // // Handle empty paragraphs
+
+          // Handle empty paragraphs
           if (isEmpty && !isLastNode) {
-            // Add `<br>` with a newline if the next node is a block
+            // Add `</br>` with a newline if the next node is a block
             const nextNode = parent.child(parent.children.indexOf(node) + 1)
 
             if (nextNode?.isBlock && !['hardBreak', 'paragraph'].includes(nextNode.type.name)) {
-              state.write('<br>\n\n') // Ensure block starts correctly
+              state.write('</br>\n\n') // Ensure block starts correctly
             } else {
-              state.write('<br>') // Inline `<br>` for non-block contexts
+              state.write('</br>') // Inline `</br>` for non-block contexts
             }
             return
           }

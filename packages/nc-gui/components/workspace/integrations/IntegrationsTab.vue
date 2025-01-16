174a138769c06803c42b8d8c51fb760b2e3034a3
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-01-16 06:17:27 +0000

    fix: hide data reflection is prod

--- packages/nc-gui/components/workspace/integrations/IntegrationsTab.vue
+++ packages/nc-gui/components/workspace/integrations/IntegrationsTab.vue
@@ -122,9 +122,15 @@ const categoriesQuery = computed({
   },
 })
 
+const isDataReflectionEnabled = computed(() => {
+  return isFeatureEnabled(FEATURE_FLAG.DATA_REFLECTION)
+})
+
 const getIntegrationsByCategory = (category: IntegrationCategoryType, query: string) => {
   return allIntegrations.filter((i) => {
     const isOssOnly = isEeUI ? !i?.isOssOnly : true
+
+    if (!isDataReflectionEnabled.value && i.sub_type === SyncDataType.NOCODB) return false
 
     if (i.hidden) return false
 
@@ -209,7 +215,7 @@ const handleAddIntegration = async (category: IntegrationCategoryType, integrati
     return
   }
 
-  if (!isFeatureEnabled(FEATURE_FLAG.DATA_REFLECTION) && integration.sub_type === SyncDataType.NOCODB) {
+  if (!isDataReflectionEnabled.value && integration.sub_type === SyncDataType.NOCODB) {
     return
   }
 
@@ -420,11 +426,7 @@ watch(activeViewTab, (value) => {
                               <div class="name">{{ $t(integration.title) }}</div>
                               <div v-if="integration.subtitle" class="subtitle flex-1">{{ $t(integration.subtitle) }}</div>
                             </div>
-                            <div
-                              v-if="
-                                !isFeatureEnabled(FEATURE_FLAG.DATA_REFLECTION) && integration?.sub_type === SyncDataType.NOCODB
-                              "
-                            ></div>
+                            <div v-if="!isDataReflectionEnabled && integration?.sub_type === SyncDataType.NOCODB"></div>
                             <div v-else-if="integration?.sub_type === SyncDataType.NOCODB" class="flex items-center">
                               <template v-if="dataReflectionEnabled">
                                 <GeneralIcon icon="check" class="text-primary text-lg" />

c19d0627ee3738c531c9bfeaaf2f1025c1b05b38
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-01-17 22:45:03 +0530

    feat: migrate DatePicker to readonly

---
+++ packages/nc-gui/components/cell/Date/Readonly.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import dayjs from 'dayjs'
+
+interface Props {
+  modelValue?: string | null
+}
+
+const { modelValue } = defineProps<Props>()
+
+const columnMeta = inject(ColumnInj, null)!
+
+const dateFormat = computed(() => parseProp(columnMeta?.value?.meta)?.date_format ?? 'YYYY-MM-DD')
+
+const localState = computed(() => {
+  if (!modelValue) {
+    return undefined
+  }
+
+  if (!dayjs(modelValue).isValid()) {
+    return undefined
+  }
+
+  return dayjs(/^\d+$/.test(modelValue) ? +modelValue : modelValue, dateFormat.value)
+})
+</script>
+
+<template>
+  <span>
+    {{ localState?.format(dateFormat) ?? '' }}
+  </span>
+</template>
+
+<style scoped lang="scss"></style>

e0e2e23cdb2160e554bce66941950c44f9fd1be3
Fendy Heryanto <fendy@nocodb.com>
2025-01-21 08:24:33 +0700

    fix: webhook not firing during add child

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -7649,6 +7649,19 @@ class BaseModelSqlv2 {
       model: childTable,
     });
 
+    const prevParentDataForWebhook = await parentBaseModel.readByPk(
+      rowId,
+      false,
+      {},
+      { ignoreView: true, getHiddenColumn: true },
+    );
+    const prevChildDataForWebhook = await childBaseModel.readByPk(
+      childId,
+      false,
+      {},
+      { ignoreView: true, getHiddenColumn: true },
+    );
+
     const childTn = childBaseModel.getTnPath(childTable);
     const parentTn = parentBaseModel.getTnPath(parentTable);
 
@@ -8354,6 +8367,34 @@ class BaseModelSqlv2 {
           });
         }
       }),
+    );
+
+    // FIXME : a first effort to send webhook when child added
+    const nextParentDataForWebhook = await parentBaseModel.readByPk(
+      rowId,
+      false,
+      {},
+      { ignoreView: true, getHiddenColumn: true },
+    );
+
+    await parentBaseModel.handleHooks(
+      'after.update',
+      prevParentDataForWebhook,
+      nextParentDataForWebhook,
+      cookie,
+    );
+    const nextChildDataForWebhook = await childBaseModel.readByPk(
+      childId,
+      false,
+      {},
+      { ignoreView: true, getHiddenColumn: true },
+    );
+
+    await childBaseModel.handleHooks(
+      'after.update',
+      prevChildDataForWebhook,
+      nextChildDataForWebhook,
+      cookie,
     );
   }
 

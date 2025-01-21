59e030ecaf5e5bb1d9ad6392e92a0fc77f87e3b0
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-01-21 12:22:12 +0530

    fix(nc-gui): integer cell index file refactor issue

--- packages/nc-gui/components/cell/Integer/index.vue
+++ packages/nc-gui/components/cell/Integer/index.vue
@@ -2,68 +2,135 @@
 import type { VNodeRef } from '@vue/runtime-core'
 
 interface Props {
-  modelValue?: string | number | null
+  // when we set a number, then it is number type
+  // for sqlite, when we clear a cell or empty the cell, it returns ""
+  // otherwise, it is null type
+  modelValue?: number | null | string
+}
+
+interface Emits {
+  (event: 'update:modelValue', model: number): void
 }
 
 const props = defineProps<Props>()
 
-const emits = defineEmits(['update:modelValue'])
+const emits = defineEmits<Emits>()
 
 const { showNull } = useGlobal()
 
-const editEnabled = inject(EditModeInj, ref(false))
+const editEnabled = inject(EditModeInj)
 
 const isEditColumn = inject(EditColumnInj, ref(false))
 
-const rowHeight = inject(RowHeightInj, ref(undefined))
-
 const readOnly = inject(ReadonlyInj, ref(false))
-
-const vModel = useVModel(props, 'modelValue', emits)
 
 const isExpandedFormOpen = inject(IsExpandedFormOpenInj, ref(false))!
 
 const isForm = inject(IsFormInj)!
 
+const column = inject(ColumnInj, null)!
+
+const _vModel = useVModel(props, 'modelValue', emits)
+
+const displayValue = computed(() => {
+  if (_vModel.value === null) return null
+
+  if (isNaN(Number(_vModel.value))) return null
+
+  if (parseProp(column.value.meta).isLocaleString) return Number(_vModel.value).toLocaleString()
+
+  return Number(_vModel.value)
+})
+
+const vModel = computed({
+  get: () => _vModel.value,
+  set: (value) => {
+    if (value === '') {
+      // if we clear / empty a cell in sqlite,
+      // the value is considered as ''
+      _vModel.value = null
+    } else if (isForm.value && !isEditColumn.value) {
+      _vModel.value = isNaN(Number(value)) ? value : Number(value)
+    } else {
+      _vModel.value = value
+    }
+  },
+})
+
+const inputType = computed(() => (isForm.value && !isEditColumn.value ? 'text' : 'number'))
+
 const focus: VNodeRef = (el) =>
   !isExpandedFormOpen.value && !isEditColumn.value && !isForm.value && (el as HTMLInputElement)?.focus()
 
-const textareaValue = computed({
-  get: () => vModel.value ?? '',
-  set: (val) => (vModel.value = val),
-})
+function onKeyDown(e: any) {
+  const cmdOrCtrl = isMac() ? e.metaKey : e.ctrlKey
+  if (cmdOrCtrl && !e.altKey) {
+    switch (e.keyCode) {
+      case 90: {
+        e.stopPropagation()
+        break
+      }
+    }
+  }
+  if (e.key === '.') {
+    return e.preventDefault()
+  }
+
+  if (e.key === 'ArrowDown') {
+    e.preventDefault()
+    // Move the cursor to the end of the input
+    e.target.type = 'text'
+    e.target?.setSelectionRange(e.target.value.length, e.target.value.length)
+    e.target.type = 'number'
+  } else if (e.key === 'ArrowUp') {
+    e.preventDefault()
+
+    e.target.type = 'text'
+    e.target?.setSelectionRange(0, 0)
+    e.target.type = 'number'
+  }
+}
 </script>
 
 <template>
-  <template v-if="!readOnly && editEnabled">
-    <!-- eslint-disable vue/use-v-on-exact -->
-    <input
-      v-if="!isExpandedFormOpen"
-      :ref="focus"
-      v-model="vModel"
-      class="nc-cell-field h-full w-full outline-none py-1 bg-transparent"
-      @blur="editEnabled = false"
-      @keydown.down.stop
-      @keydown.left.stop
-      @keydown.right.stop
-      @keydown.up.stop
-      @keydown.delete.stop
-      @keydown.alt.stop
-      @selectstart.capture.stop
-      @mousedown.stop
-    />
-    <a-textarea
-      v-else
-      :ref="focus"
-      v-model:value="textareaValue"
-      :auto-size="{ minRows: 1, maxRows: 6 }"
-      class="!border-0 !border-none !outline-0 !ring-0 focus:!border-0 focus:!outline-0 focus:!ring-0 !p-2 nc-scrollbar-thin"
-      style="color: inherit; resize: auto !important"
-      @keydown.enter.prevent
-    />
-  </template>
-
+  <!-- eslint-disable vue/use-v-on-exact -->
+  <input
+    v-if="!readOnly && editEnabled"
+    :ref="focus"
+    v-model="vModel"
+    class="nc-cell-field outline-none py-1 border-none w-full h-full"
+    :type="inputType"
+    style="letter-spacing: 0.06rem"
+    @blur="editEnabled = false"
+    @keydown="onKeyDown"
+    @keydown.down.stop
+    @keydown.left.stop
+    @keydown.right.stop
+    @keydown.up.stop
+    @keydown.delete.stop
+    @keydown.alt.stop
+    @selectstart.capture.stop
+    @mousedown.stop
+  />
   <span v-else-if="vModel === null && showNull" class="nc-cell-field nc-null uppercase">{{ $t('general.null') }}</span>
-
-  <LazyCellClampedText v-else class="nc-cell-field" :value="vModel" :lines="rowHeight" :style="{ 'word-break': 'break-word' }" />
+  <span v-else class="nc-cell-field">{{ displayValue }}</span>
 </template>
+
+<style scoped lang="scss">
+input[type='number']:focus,
+input[type='text']:focus {
+  @apply ring-transparent;
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type='number'] {
+  -moz-appearance: textfield;
+}
+</style>

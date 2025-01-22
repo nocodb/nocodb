65075926f7238e7ba762449d760bfddfc6460d1d
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-01-22 17:29:27 +0530

    fix(nc-gui): time cell import issue in formula wrapper cell

--- packages/nc-gui/components/smartsheet/FormulaWrapperCell.vue
+++ packages/nc-gui/components/smartsheet/FormulaWrapperCell.vue
@@ -36,7 +36,7 @@ provide(ColumnInj, column)
     <LazyCellRating v-else-if="isRating(column)" :model-value="modelValue" />
     <LazyCellDateReadonly v-else-if="isDate(column, '')" :model-value="modelValue" />
     <LazyCellDateTimeReadonly v-else-if="isDateTime(column, '')" :model-value="modelValue" />
-    <LazyCellTimePicker v-else-if="isTime(column, '')" :model-value="modelValue" />
+    <LazyCellTime v-else-if="isTime(column, '')" :model-value="modelValue" />
     <LazyCellEmail v-else-if="isEmail(column)" :model-value="modelValue" />
     <LazyCellUrl v-else-if="isURL(column)" :model-value="modelValue" />
     <LazyCellPhoneNumber v-else-if="isPhoneNumber(column)" :model-value="modelValue" />

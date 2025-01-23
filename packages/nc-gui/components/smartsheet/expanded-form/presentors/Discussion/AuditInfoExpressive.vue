64ddd611be7ca962cf30d9d688686145571f5897
Yoones Khoshghadam <yooneskh@gmail.com>
2025-01-23 23:46:39 +0300

    fix (nc-gui): finalize polishes on audits elements

--- packages/nc-gui/components/smartsheet/expanded-form/presentors/Discussion/AuditInfoExpressive.vue
+++ packages/nc-gui/components/smartsheet/expanded-form/presentors/Discussion/AuditInfoExpressive.vue
@@ -102,11 +102,11 @@ function processNewDataFor(key: string) {
 }
 
 function shouldUseNormalizedPadding(key: string) {
-  return !['Checkbox', 'SingleSelect', 'MultiSelect'].includes(meta.value?.[key]?.type) && !normalizeMeta(key).is_progress
+  return !['Checkbox', 'SingleSelect', 'MultiSelect', 'User'].includes(meta.value?.[key]?.type) && !normalizeMeta(key).is_progress
 }
 
 function shouldUseUniformPadding(key: string) {
-  return ['SingleSelect', 'MultiSelect'].includes(meta.value?.[key]?.type)
+  return ['SingleSelect', 'MultiSelect', 'User'].includes(meta.value?.[key]?.type)
 }
 
 /* visibility */
@@ -118,97 +118,135 @@ function isShowableValue(value: any) {
 
 <template>
   <div v-for="columnKey in columnKeys" :key="columnKey" class="relative mb-2">
-    <GeneralIcon icon="ncNode" class="w-[16px] h-[16px] text-gray-500 bg-white absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-1/2" />
-    <div class="mb-1 ml-6.5">
+    <GeneralIcon
+      icon="ncNode"
+      class="w-[16px] h-[16px] text-gray-500 bg-white absolute left-0 transform -translate-x-1/2"
+      :class="[
+        ['JSON', 'Attachment'].includes(meta[columnKey]?.type) ? 'top-1' : 'top-1/2 -translate-y-2/5',
+      ]"
+    />
+    <div class="ml-6.5">
       <div
         class="text-sm font-weight-500"
         :class="{
-          'inline-flex items-center flex-wrap': meta[columnKey]?.type !== 'LongText',
+          'inline-flex items-center flex-wrap': true,
         }"
       >
-        <span class="text-gray-600">
+        <span class="text-gray-600 text-xs">
           changed
         </span>
         <span
-          class="border-1 border-gray-300 rounded-md px-1 !h-[20px] bg-gray-200 inline-flex items-center gap-1 mx-3"
+          class="rounded-md px-1 !h-[20px] bg-gray-200 inline-flex items-center gap-1 mx-1"
         >
           <SmartsheetHeaderCellIcon
             :column-meta="{ uidt: meta[columnKey]?.type, dt: meta[columnKey]?.type === 'Number' ? 'bigint' : undefined }"
-            class="!w-[16px] !h-[16px] !m-0"
+            class="!w-[16px] !h-[16px] !m-0 !text-gray-600"
           />
           <span class="text-sm font-weight-500 text-gray-600">
             {{ columnKey }}
           </span>
         </span>
         <template v-if="meta[columnKey]?.type === 'Attachment'">
-          <span class="text-xs border-1 border-gray-300 rounded-md px-1 py-0.25 inline-flex items-center gap-1 line-through">
-            <template v-if="!oldData[columnKey]?.length"> 0 files </template>
-            <template v-else>
-              <div class="flex items-center gap-1">
-                <template v-for="(item, i) of oldData[columnKey]" :key="item.url || item.title">
-                  <div class="w-8 aspect-square">
+          <div v-if="processOldDataFor(columnKey)?.length > 0" class="w-full mt-1">
+            <div class="border-1 border-red-500 rounded-md bg-red-50 p-0.5 flex flex-col items-start gap-0.5 w-[284px]">
+              <div
+                v-for="(item, i) of processOldDataFor(columnKey)"
+                :key="item.url || item.title"
+                class="border-1 border-gray-200 rounded-md bg-white w-full"
+              >
+                <div class="flex items-center gap-2 w-full">
+                  <div class="flex items-center justify-center w-8 aspect-square">
                     <LazyCellAttachmentPreviewImage
                       v-if="isImage(item.title, item.mimetype ?? item.type)"
                       :alt="item.title || `#${i}`"
-                      class="nc-attachment rounded-lg w-full h-full object-cover overflow-hidden"
+                      class="nc-attachment rounded !w-5.5 !h-5.5 object-cover overflow-hidden"
                       :srcs="getPossibleAttachmentSrc(item, 'small')"
                     />
-                    <div v-else class="nc-attachment h-full w-full flex items-center justify-center">
-                      <CellAttachmentIconView :item="item" class="max-h-full max-w-full" />
+                    <div v-else class="nc-attachment flex items-center justify-center">
+                      <CellAttachmentIconView :item="item" class="!w-8 !h-8" />
                     </div>
                   </div>
-                </template>
+                  <span class="w-0 flex-1 truncate text-sm font-weight-500 text-gray-600">
+                    {{ item.title }}
+                  </span>
+                  <span class="text-xs font-weight-500 p-2 text-gray-500">
+                    {{ getReadableFileSize(item.size) }}
+                  </span>
+                </div>
               </div>
-            </template>
-          </span>
-          <span class="ml-2 text-xs"> to </span>
-          <span class="ml-2 text-xs border-1 border-gray-300 rounded-md px-1 py-0.25 inline-flex items-center gap-1">
-            <template v-if="!newData[columnKey]?.length"> 0 files </template>
-            <template v-else>
-              <div class="flex items-center gap-1">
-                <template v-for="(item, i) of newData[columnKey]" :key="item.url || item.title">
-                  <div class="w-8 aspect-square">
+            </div>
+          </div>
+          <div
+            v-if="processNewDataFor(columnKey)?.length > 0"
+            class="w-full mt-1"
+          >
+            <div class="border-1 border-green-500 rounded-md bg-green-50 p-0.5 flex flex-col items-start gap-0.5 w-[284px]">
+              <div
+                v-for="(item, i) of processNewDataFor(columnKey)"
+                :key="item.url || item.title"
+                class="border-1 border-gray-200 rounded-md bg-white w-full"
+              >
+                <div class="flex items-center gap-2 w-full">
+                  <div class="flex items-center justify-center w-8 aspect-square">
                     <LazyCellAttachmentPreviewImage
                       v-if="isImage(item.title, item.mimetype ?? item.type)"
                       :alt="item.title || `#${i}`"
-                      class="nc-attachment rounded-lg w-full h-full object-cover overflow-hidden"
+                      class="nc-attachment rounded !w-5.5 !h-5.5 object-cover overflow-hidden"
                       :srcs="getPossibleAttachmentSrc(item, 'small')"
                     />
-                    <div v-else class="nc-attachment h-full w-full flex items-center justify-center">
-                      <CellAttachmentIconView :item="item" class="max-h-full max-w-full" />
+                    <div v-else class="nc-attachment flex items-center justify-center">
+                      <CellAttachmentIconView :item="item" class="!w-8 !h-8" />
                     </div>
                   </div>
-                </template>
+                  <span class="w-0 flex-1 truncate text-sm font-weight-500 text-gray-600">
+                    {{ item.title }}
+                  </span>
+                  <span class="text-xs font-weight-500 p-2 text-gray-500">
+                    {{ getReadableFileSize(item.size) }}
+                  </span>
+                </div>
               </div>
-            </template>
-          </span>
+            </div>
+          </div>
         </template>
         <template v-else-if="['SingleLineText', 'LongText'].includes(meta[columnKey]?.type)">
           <template v-for="(block, i) of diffTextBlocks(oldData[columnKey] || '', newData[columnKey] || '')" :key="i">
             <span
               v-if="block.op === 'removed'"
-              class="text-red-700 border-1 border-red-200 rounded-md px-1 bg-red-50 line-through decoration-clone"
+              class="text-red-700 border-1 border-red-200 rounded-md px-1 mr-1 bg-red-50 line-through decoration-clone"
             >
               {{ block.text }}
             </span>
             <span
               v-else-if="block.op === 'added'"
-              class="text-green-700 border-1 border-green-200 rounded-md px-1 bg-green-50 decoration-clone"
+              class="text-green-700 border-1 border-green-200 rounded-md px-1 mr-1 bg-green-50 decoration-clone"
             >
               {{ block.text }}
             </span>
-            <span v-else>
+            <span v-else class="mr-1">
               {{ block.text }}
             </span>
           </template>
         </template>
+        <template v-else-if="meta[columnKey]?.type === 'JSON'">
+          <div class="w-full">
+            <pre
+              v-if="isShowableValue(processOldDataFor(columnKey))"
+              class="!text-red-700 border-1 border-red-200 rounded-md bg-red-50 line-through !mb-0 mt-1">{{ processOldDataFor(columnKey) }}</pre>
+          </div>
+          <div class="w-full">
+            <pre
+              v-if="isShowableValue(processNewDataFor(columnKey))"
+              class="!text-green-700 border-1 border-green-200 rounded-md bg-green-50 !mb-0 mt-1">{{ processNewDataFor(columnKey) }}</pre>
+          </div>
+        </template>
         <template v-else>
           <span
             v-if="isShowableValue(processOldDataFor(columnKey))"
-            class="nc-expressive-mini-item-cell nc-audit-removal !text-red-700 border-1 border-red-200 rounded-md bg-red-50 line-through"
+            class="nc-expressive-mini-item-cell nc-audit-removal !text-red-700 border-1 mr-1 border-red-200 rounded-md bg-red-50 line-through"
             :class="{
-              'px-1 py-0.25': shouldUseNormalizedPadding(columnKey),
-              '!p-0.25': shouldUseUniformPadding(columnKey),
+              'px-1 py-0': shouldUseNormalizedPadding(columnKey),
+              '!px-0.25 !py-0.25': shouldUseUniformPadding(columnKey),
             }">
             <SmartsheetCell
               :column="{
@@ -228,10 +266,10 @@ function isShowableValue(value: any) {
           </span>
           <span
             v-if="isShowableValue(processNewDataFor(columnKey))"
-            class="nc-expressive-mini-item-cell ml-2 nc-audit-addition border-1 border-green-200 rounded-md bg-green-50"
+            class="nc-expressive-mini-item-cell nc-audit-addition border-1 border-green-200 rounded-md bg-green-50"
             :class="{
-              'px-1 py-0.25': shouldUseNormalizedPadding(columnKey),
-              '!p-0.25': shouldUseUniformPadding(columnKey),
+              'px-1 py-0': shouldUseNormalizedPadding(columnKey),
+              '!px-0.25 !py-0.25': shouldUseUniformPadding(columnKey),
             }">
             <SmartsheetCell
               :column="{
@@ -262,12 +300,13 @@ function isShowableValue(value: any) {
 .nc-expressive-mini-item-cell :deep(.nc-cell-field.nc-multi-select > div) {
   @apply !gap-1 !flex;
   & > span {
-    @apply !m-0 flex items-center h-[22px];
+    @apply !m-0 flex items-center h-[18px];
   }
 }
 .nc-expressive-mini-item-cell :deep(.nc-cell-field.nc-single-select > div) {
   height: 20px !important;
-  & > span {
+  .ant-tag {
+    height: 20px !important;
     @apply !m-0;
   }
 }
@@ -326,9 +365,9 @@ function isShowableValue(value: any) {
   .nc-cell-field > div {
     display: flex !important;
     & > .ant-tag {
-      @apply !m-0 !p-0 !bg-transparent !text-inherit;
+      @apply !m-0 !text-inherit !border-1 !border-gray-300 !pr-1 !pl-0.5 !bg-gray-100 !rounded-[17px];
       & > span > div + div {
-        @apply flex items-center !text-sm font-weight-500;
+        @apply flex items-center !text-[13px] font-weight-500;
       }
       & > span {
         @apply gap-1;
@@ -336,14 +375,9 @@ function isShowableValue(value: any) {
     }
     .nc-user-avatar {
       @apply border-1 border-nc-gray-medium;
-      height: 24px !important;
-      width: 24px !important;
+      height: 16px !important;
+      width: 16px !important;
     }
-  }
-}
-.nc-expressive-mini-item-cell :deep(.nc-cell-user:has(.ant-tag + .ant-tag)) {
-  .ant-tag {
-    @apply !border-1 !border-gray-300 !py-0.5 !px-1 !bg-gray-100 !rounded-[6px];
   }
 }
 .nc-expressive-mini-item-cell.nc-audit-removal :deep(.nc-cell-user) {
@@ -364,8 +398,8 @@ function isShowableValue(value: any) {
     height: 12px;
   }
 }
-.nc-expressive-mini-item-cell:has(.nc-cell-user .ant-tag + .ant-tag) {
-  @apply !p-1;
+.nc-expressive-mini-item-cell:has(.nc-cell-user .ant-tag) {
+  @apply !p-0.25;
   .nc-cell-field > div {
     @apply gap-1;
   }

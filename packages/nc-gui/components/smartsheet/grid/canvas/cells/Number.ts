27a674dea30d611e5a9a20c1d73fd05260e323f1
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-01-23 13:44:00 +0530

    chore: refactor

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/Number.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/Number.ts
@@ -1,18 +1,19 @@
-import { truncateText } from '../canvasUtils'
-import type { CellRenderer } from '../useCellRenderer'
+import { truncateText } from '../utils/canvas'
+import type { CellRenderer } from '~/lib/types'
 
-export const SingleLineTextCellRenderer: CellRenderer = {
+export const FloatCellRenderer: CellRenderer = {
   render: (ctx, { value, x, y, width, height, selected, pv }) => {
     const padding = 10
     ctx.font = `${pv ? 600 : 500} 13px Manrope`
     ctx.textBaseline = 'middle'
-    ctx.textAlign = 'left'
+    ctx.textAlign = 'right'
 
     const emailText = value?.toString() ?? ''
     const maxWidth = width - padding * 2
     const truncatedText = truncateText(ctx, emailText, maxWidth)
     const textY = y + height / 2
+
     ctx.fillStyle = selected || pv ? '#4351e8' : '#4a5268'
-    ctx.fillText(truncatedText, x + padding, textY)
+    ctx.fillText(truncatedText, x + width - padding, textY)
   },
 }
--- packages/nc-gui/components/smartsheet/grid/canvas/cells/SingleLineText.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/SingleLineText.ts
@@ -1,22 +1,17 @@
-import { truncateText } from '../canvasUtils'
-import type { CellRenderer } from '../useCellRenderer'
+import { truncateText } from '../utils/canvas'
+import type { CellRenderer } from '~/lib/types'
 
-export const JsonCellRenderer: CellRenderer = {
+export const SingleLineTextCellRenderer: CellRenderer = {
   render: (ctx, { value, x, y, width, height, selected, pv }) => {
     const padding = 10
     ctx.font = `${pv ? 600 : 500} 13px Manrope`
     ctx.textBaseline = 'middle'
     ctx.textAlign = 'left'
 
-    let displayText = ''
-    if (value) {
-      displayText = typeof value === 'string' ? value : JSON.stringify(value)
-    }
-
+    const emailText = value?.toString() ?? ''
     const maxWidth = width - padding * 2
-    const truncatedText = truncateText(ctx, displayText, maxWidth)
+    const truncatedText = truncateText(ctx, emailText, maxWidth)
     const textY = y + height / 2
-
     ctx.fillStyle = selected || pv ? '#4351e8' : '#4a5268'
     ctx.fillText(truncatedText, x + padding, textY)
   },

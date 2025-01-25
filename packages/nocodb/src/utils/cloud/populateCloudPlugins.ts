d6aacbd3ffa18bed0f246cdb463f2bb4a174d3f3
Pranav C <pranavxc@gmail.com>
2025-01-25 09:31:39 +0000

    feat: scope correction

--- packages/nocodb/src/utils/cloud/populateCloudPlugins.ts
+++ packages/nocodb/src/utils/cloud/populateCloudPlugins.ts
@@ -14,9 +14,14 @@ export const populatePluginsForCloud = async ({ ncMeta = Noco.ncMeta }) => {
     throw new Error('S3 env variables not found');
   }
 
-  const s3PluginData = await ncMeta.metaGet2(null, null, MetaTable.PLUGIN, {
-    title: S3PluginConfig.title,
-  });
+  const s3PluginData = await ncMeta.metaGet2(
+    RootScopes.ROOT,
+    RootScopes.ROOT,
+    MetaTable.PLUGIN,
+    {
+      title: S3PluginConfig.title,
+    },
+  );
 
   if (!s3PluginData && !process.env.NC_LICENSE_KEY)
     throw new Error('S3 plugin not found');
@@ -61,9 +66,14 @@ export const populatePluginsForCloud = async ({ ncMeta = Noco.ncMeta }) => {
   //   throw new Error('SES env variables not found');
   // }
 
-  const sesPluginData = await ncMeta.metaGet2(null, null, MetaTable.PLUGIN, {
-    title: SESPluginConfig.title,
-  });
+  const sesPluginData = await ncMeta.metaGet2(
+    RootScopes.ROOT,
+    RootScopes.ROOT,
+    MetaTable.PLUGIN,
+    {
+      title: SESPluginConfig.title,
+    },
+  );
 
   if (!sesPluginData && !process.env.NC_LICENSE_KEY) {
     throw new Error('SES plugin not found');

6b60fc735809fad4d5db8a2d2db3f212f1cb3c8f
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-01-31 14:27:58 +0530

    fix(nc-gui): replace `<br />` with `<br>`

--- packages/nc-gui/helpers/tiptap-markdown/Markdown.ts
+++ packages/nc-gui/helpers/tiptap-markdown/Markdown.ts
@@ -40,7 +40,9 @@ export const Markdown = Extension.create<MarkdownOptions, MarkdownStorage>({
       parser: new MarkdownParser(this.editor, this.options),
       serializer: new MarkdownSerializer(this.editor),
       getMarkdown: () => {
-        return this.editor.storage.markdown.serializer.serialize(this.editor.state.doc)
+        const markdown = this.editor.storage.markdown.serializer.serialize(this.editor.state.doc)
+
+        return markdown.replace(/<br \/>/g, '<br>')
       },
       getHtmlFromMd: (md) => {
         if (!md) return ''

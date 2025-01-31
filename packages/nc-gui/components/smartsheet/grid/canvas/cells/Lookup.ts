d97d8ccdad10662edc14a9c064b946993ad5ad14
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-01-31 10:56:51 +0530

    fix(nc-gui): add tag render props in cell renderer

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/Lookup.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/Lookup.ts
@@ -2,12 +2,21 @@ import { isVirtualCol, RelationTypes, UITypes, type ColumnType, type LookupType,
 
 export const LookupCellRenderer: CellRenderer = {
   render: (ctx, props) => {
-    const { column, value, relatedColObj, relatedTableMeta, renderCell } = props
-
+    const { column, x: _x, y: _y, value, renderCell, metas } = props
+    let x = _x
+    let y = _y
     // If it is empty text then no need to render
-    if (!isValidValue(value)) return
+    if (!isValidValue(value) || !metas) return
 
     const colOptions = column.colOptions as LookupType
+
+    const relatedColObj = metas.value?.[column.fk_model_id!]?.columns?.find(
+      (c) => c.id === column?.colOptions?.fk_relation_column_id,
+    ) as ColumnType
+
+    if (!relatedColObj) return
+
+    const relatedTableMeta = metas.value?.[relatedColObj.colOptions?.fk_related_model_id]
 
     const lookupColumn = (relatedTableMeta?.columns || []).find((c: ColumnType) => c.id === colOptions?.fk_lookup_column_id)
 
@@ -39,7 +48,7 @@ export const LookupCellRenderer: CellRenderer = {
       height: isAttachment(lookupColumn) ? props.height : rowHeightInPx['1']!,
     }
 
-    // Todo: handle x and y value if we are rendering multiple chips
+    // Todo: handle x and y value if we are rendering multiple chips also we have to wrap each cell in chip
     if (isVirtualCol(lookupColumn) && lookupColumn.uidt !== UITypes.Rollup) {
       if (
         lookupColumn.uidt !== UITypes.LinkToAnotherRecord ||
@@ -47,7 +56,9 @@ export const LookupCellRenderer: CellRenderer = {
           [RelationTypes.BELONGS_TO, RelationTypes.ONE_TO_ONE].includes(lookupColumn.colOptions?.type))
       ) {
         arrValue.forEach((v) => {
-          renderCell(ctx, lookupColumn, { ...renderProps, value: v })
+          renderCell(ctx, lookupColumn, { ...renderProps, value: v, x, y })
+          x = _x
+          // y = y + 28
         })
       } else {
         renderCell(ctx, lookupColumn, renderProps)
@@ -56,10 +67,10 @@ export const LookupCellRenderer: CellRenderer = {
       if (isAttachment(lookupColumn) && arrValue[0] && !Array.isArray(arrValue[0]) && typeof arrValue[0] === 'object') {
         renderCell(ctx, lookupColumn, renderProps)
       } else {
-        let x = renderProps.x
         arrValue.forEach((v) => {
-          renderCell(ctx, lookupColumn, { ...renderProps, value: v, x })
-          x += 50
+          renderCell(ctx, lookupColumn, { ...renderProps, value: v, x, y })
+          x = _x
+          // y = y + 28
         })
       }
     }

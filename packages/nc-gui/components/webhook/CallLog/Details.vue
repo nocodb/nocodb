c01c999d6e53392c42710bf6a28ca14afcd7cf0e
Pranav C <pranavxc@gmail.com>
2025-02-03 23:15:03 +0530

    feat: scrollbar and height

--- packages/nc-gui/components/webhook/CallLog/Details.vue
+++ packages/nc-gui/components/webhook/CallLog/Details.vue
@@ -107,7 +107,7 @@ const parsedRespondePayload = computed(() => {
   }
 
   .request-response-wrapper {
-    @apply flex flex-row gap-3.5 w-full;
+    @apply flex flex-row gap-3.5 w-full items-stretch ;
     .request-wrapper,
     .response-wrapper {
       @apply flex flex-col flex-1 gap-2 min-w-10 flex-1 min-w-10;

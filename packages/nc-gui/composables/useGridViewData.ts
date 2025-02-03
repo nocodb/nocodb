2acd174588009096770ac7bcc8c48d627f8bfe38
Fendy Heryanto <fendy@nocodb.com>
2025-02-03 15:02:35 +0700

    rollback handling the issue from frontend since it'll be limited by pagination

--- packages/nc-gui/composables/useGridViewData.ts
+++ packages/nc-gui/composables/useGridViewData.ts
@@ -725,18 +725,10 @@ export function useGridViewData(
   async function bulkDeleteAll() {
     try {
       isBulkOperationInProgress.value = true
-      // if there is filter applied on view, do not delete all records
-      if (allFilters.value.length > 0) {
-        const rowsToRemove = Array.from(cachedRows.value.values()).map((k) => toRaw(k.row))
-        await $api.dbDataTableRow.delete(meta.value?.id as string, rowsToRemove.length === 1 ? rowsToRemove[0] : rowsToRemove, {
-          viewId: viewMeta.value?.id as string,
-        })
-      } else {
-        await $api.dbTableRow.bulkDeleteAll('noco', base.value.id!, meta.value.id!, {
-          where: where?.value,
-          viewId: viewMeta.value?.id,
-        })
-      }
+      await $api.dbTableRow.bulkDeleteAll('noco', base.value.id!, meta.value.id!, {
+        where: where?.value,
+        viewId: viewMeta.value?.id,
+      })
     } catch (error) {
     } finally {
       clearCache(Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY)

4ed83b2c74ef188d60546d7acb937cec93f60f5e
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-06 12:18:16 +0530

    refactor(nc-gui): cell tag render

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/Year.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/Year.ts
@@ -1,18 +1,9 @@
 import dayjs from 'dayjs'
-import { renderSingleLineText, renderTag } from '../utils/canvas'
+import { renderSingleLineText, renderTagLabel } from '../utils/canvas'
 
 export const YearCellRenderer: CellRenderer = {
   render: (ctx, props) => {
     const { selected, value, x, y, width, height, pv, padding, textColor = '#4a5268' } = props
-    const {
-      renderAsTag,
-      tagPaddingX = 8,
-      tagHeight = 20,
-      tagRadius = 6,
-      tagBgColor = '#f4f4f0',
-      tagBorderColor,
-      tagBorderWidth,
-    } = props.tag || {}
 
     let text = ''
 
@@ -30,42 +21,8 @@ export const YearCellRenderer: CellRenderer = {
       }
     }
 
-    if (renderAsTag) {
-      const maxWidth = width - padding * 2 - tagPaddingX * 2
-
-      const { text: truncatedText, width: textWidth } = renderSingleLineText(ctx, {
-        x: x + padding + tagPaddingX,
-        y: y + padding,
-        text,
-        maxWidth,
-        fontFamily: `${pv ? 600 : 500} 13px Manrope`,
-        render: false,
-      })
-
-      renderTag(ctx, {
-        x: x + padding,
-        y: y + padding - 4,
-        width: textWidth + tagPaddingX * 2,
-        height: tagHeight,
-        radius: tagRadius,
-        fillStyle: tagBgColor,
-        borderColor: tagBorderColor,
-        borderWidth: tagBorderWidth,
-      })
-
-      renderSingleLineText(ctx, {
-        x: x + padding + tagPaddingX,
-        y: y,
-        text: truncatedText,
-        maxWidth: maxWidth,
-        fontFamily: `${pv ? 600 : 500} 13px Manrope`,
-        fillStyle: textColor,
-      })
-
-      return {
-        x: x + padding + textWidth + tagPaddingX * 2,
-        y: y + padding - 4 + tagHeight,
-      }
+    if (props.tag?.renderAsTag) {
+      return renderTagLabel(ctx, { ...props, text })
     } else {
       const { x: xOffset, y: yOffset } = renderSingleLineText(ctx, {
         x: x + padding,

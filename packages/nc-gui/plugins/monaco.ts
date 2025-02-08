12aa2963d99f31bffd0bdfc5de115f68e8b42a99
mertmit <mertmit99@gmail.com>
2025-02-08 14:38:36 +0000

    fix: workaround for typescript worker

--- packages/nc-gui/plugins/monaco.ts
+++ packages/nc-gui/plugins/monaco.ts
@@ -1,26 +1,29 @@
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker&inline'
 import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker&inline'
-import TypeScriptWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker&inline'
 
 export default defineNuxtPlugin(() => {
-  /**
-   * Adding monaco editor to Vite
-   *
-   **/
-  self.MonacoEnvironment = window.MonacoEnvironment = {
-    async getWorker(_: any, label: string) {
-      switch (label) {
-        case 'typescript':
-        case 'javascript': {
-          return new TypeScriptWorker()
+  import('monaco-editor/esm/vs/language/typescript/ts.worker?worker&inline').then((md) => {
+    const TypeScriptWorker = md.default
+
+    /**
+     * Adding monaco editor to Vite
+     *
+     **/
+    self.MonacoEnvironment = window.MonacoEnvironment = {
+      async getWorker(_: any, label: string) {
+        switch (label) {
+          case 'typescript':
+          case 'javascript': {
+            return new TypeScriptWorker()
+          }
+          case 'json': {
+            return new JsonWorker()
+          }
+          default: {
+            return new EditorWorker()
+          }
         }
-        case 'json': {
-          return new JsonWorker()
-        }
-        default: {
-          return new EditorWorker()
-        }
-      }
-    },
-  }
+      },
+    }
+  })
 })

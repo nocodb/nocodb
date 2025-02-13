c88e5955317786024edfe1333adfa0afda4c4d9f
Amit Joki <amitjoki@gmail.com>
2025-02-13 20:04:45 +0000

    feat: single select tooltip render

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/SingleSelect.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/SingleSelect.ts
@@ -1,4 +1,4 @@
-import { renderSingleLineText, renderTag } from '../utils/canvas'
+import { isBoxHovered, renderSingleLineText, renderTag, truncateText } from '../utils/canvas'
 import type { getSingleMultiselectColOptions } from '../utils/cell'
 
 const tagPadding = 8
@@ -6,7 +6,7 @@ const tagHeight = 20
 const topPadding = 6
 
 export const SingleSelectCellRenderer: CellRenderer = {
-  render: (ctx, { column, value, x, y, width, height, pv, padding }) => {
+  render: (ctx, { column, value, x, y, width, pv, padding }) => {
     const text = value?.toString()?.trim() ?? ''
 
     // If it is empty text then no need to render
@@ -14,7 +14,7 @@ export const SingleSelectCellRenderer: CellRenderer = {
 
     const { text: truncatedText, width: optionWidth } = renderSingleLineText(ctx, {
       x: x + padding + tagPadding,
-      y: y,
+      y,
       text,
       maxWidth: width - padding * 2 - tagPadding * 2,
       textAlign: 'left',
@@ -36,7 +36,7 @@ export const SingleSelectCellRenderer: CellRenderer = {
 
     renderSingleLineText(ctx, {
       x: x + padding + tagPadding,
-      y: y,
+      y,
       text: truncatedText,
       maxWidth: width - padding * 2 - tagPadding * 2,
       textAlign: 'left',
@@ -44,5 +44,31 @@ export const SingleSelectCellRenderer: CellRenderer = {
       fontFamily: `${pv ? 600 : 500} 13px Manrope`,
       fillStyle: getSelectTypeOptionTextColor(opColor),
     })
+  },
+  async handleHover({ getCellPosition, column, row, value, mousePosition }) {
+    const { x, y, width } = getCellPosition(column, row.rowMeta.rowIndex!)
+    const { showTooltip, hideTooltip } = useTooltipStore()
+    hideTooltip()
+    const padding = 10
+    const maxWidth = width - padding * 2 - tagPadding * 2
+    const ctx = document.createElement('canvas').getContext('2d')
+    if (!ctx) return
+    ctx.font = `${column.pv ? 600 : 500} 13px Manrope`
+    const text = value?.toString()?.trim() ?? ''
+    // If it is empty text then no need to render
+    if (!text) return
+    const truncatedText = truncateText(ctx, text, maxWidth, true)
+    console.log({ text, truncatedText: truncatedText.text })
+    if (text === truncatedText.text) return
+    const box = { x: x + padding, y: y + topPadding, width: truncatedText.width + tagPadding * 2, height: tagHeight }
+    if (isBoxHovered(box, mousePosition)) {
+      showTooltip({
+        position: {
+          x: mousePosition.x,
+          y: mousePosition.y,
+        },
+        text,
+      })
+    }
   },
 }

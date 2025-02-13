754b98c7e22552d965220c77578a9f59edac3043
Amit Joki <amitjoki@gmail.com>
2025-02-13 20:04:59 +0000

    fix: make the attachment icons similar

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/Attachment.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/Attachment.ts
@@ -203,7 +203,7 @@ export const AttachmentCellRenderer: CellRenderer = {
         },
         mousePosition,
         spriteLoader,
-        icon: 'attachFile',
+        icon: 'ncPaperclip',
       })
     }
   },

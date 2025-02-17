9f1b9062263c2905f49f461463c24cdc62058662
Amit Joki <amitjoki@gmail.com>
2025-02-17 11:02:48 +0000

    feat: hover state ui for canvas render of LTAR belongs to, one to one, has many, many to many

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/LTAR/BelongsTo.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/LTAR/BelongsTo.ts
@@ -1,4 +1,18 @@
+import { isBoxHovered } from '../../utils/canvas'
+
 export const BelongsToCellRenderer: CellRenderer = {
-  render: (ctx, props) => {},
+  render: (ctx, props) => {
+    const { x, y, width, height, spriteLoader, mousePosition } = props
+
+    if (isBoxHovered({ x, y, width, height }, mousePosition)) {
+      spriteLoader.renderIcon(ctx, {
+        x: x + width - 26,
+        y: y + 8,
+        icon: 'ncPlus',
+        size: 14,
+        color: '#374151',
+      })
+    }
+  },
   handleClick: async () => {},
 }
--- packages/nc-gui/components/smartsheet/grid/canvas/cells/LTAR/HasMany.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/LTAR/HasMany.ts
@@ -1,5 +1,38 @@
+import { isBoxHovered, renderIconButton } from '../../utils/canvas'
+
 export const HasManyCellRenderer: CellRenderer = {
-    render: (ctx, props) => {},
-    handleClick: async () => {},
-  }
-  
+  render: (ctx, props) => {
+    const { x, y, width, height, spriteLoader, mousePosition } = props
+
+    const buttonSize = 24
+    const borderRadius = 6
+
+    if (isBoxHovered({ x, y, width, height }, mousePosition)) {
+      renderIconButton(ctx, {
+        buttonX: x + width - 57,
+        buttonY: y + 4,
+        borderRadius,
+        buttonSize,
+        spriteLoader,
+        mousePosition,
+        icon: 'ncPlus',
+        iconData: {
+          size: 14,
+          xOffset: 5,
+          yOffset: 5,
+        },
+      })
+
+      renderIconButton(ctx, {
+        buttonX: x + width - 30,
+        buttonY: y + 4,
+        borderRadius,
+        buttonSize,
+        spriteLoader,
+        mousePosition,
+        icon: 'maximize',
+      })
+    }
+  },
+  handleClick: async () => {},
+}

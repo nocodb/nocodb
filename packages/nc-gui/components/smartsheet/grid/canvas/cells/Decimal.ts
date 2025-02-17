683f05b92f2e28331f4e31e2c00d3833511b9c25
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-17 11:02:50 +0000

    fix(nc-gui): rollup and decimal cell alignment issue

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/Decimal.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/Decimal.ts
@@ -1,12 +1,18 @@
 import { roundUpToPrecision } from 'nocodb-sdk'
-import { truncateText } from '../utils/canvas'
+import { renderSingleLineText, renderTag } from '../utils/canvas'
 
 export const DecimalCellRenderer: CellRenderer = {
-  render: (ctx, { value, x, y, width, height, selected, pv, column, padding }) => {
-    ctx.fillStyle = '#4a5268'
-    ctx.font = `${pv ? 600 : 500} 13px Manrope`
-    ctx.textBaseline = 'middle'
-    ctx.textAlign = 'right'
+  render: (ctx, props) => {
+    const { column, selected, value, x, y, width, height, pv, padding, textColor = '#4a5268' } = props
+    const {
+      renderAsTag,
+      tagPaddingX = 8,
+      tagHeight = 20,
+      tagRadius = 6,
+      tagBgColor = '#f4f4f0',
+      tagBorderColor,
+      tagBorderWidth,
+    } = props.tag || {}
 
     let displayValue = null
     const meta = parseProp(column?.meta)
@@ -22,11 +28,66 @@ export const DecimalCellRenderer: CellRenderer = {
       }
     }
 
-    const maxWidth = width - padding * 2
-    const truncatedText = truncateText(ctx, displayValue?.toString() ?? '', maxWidth)
-    const textY = y + height / 2
+    const text = displayValue?.toString() ?? ''
 
-    ctx.fillStyle = selected || pv ? '#4351e8' : '#4a5268'
-    ctx.fillText(truncatedText, x + width - padding, textY)
+    if (!isValidValue(text)) {
+      return {
+        x,
+        y,
+      }
+    }
+
+    if (renderAsTag) {
+      const maxWidth = width - padding * 2 - tagPaddingX * 2
+
+      const { text: truncatedText, width: textWidth } = renderSingleLineText(ctx, {
+        x: x + padding + tagPaddingX,
+        y: y + padding,
+        text,
+        maxWidth,
+        fontFamily: `${pv ? 600 : 500} 13px Manrope`,
+        render: false,
+      })
+
+      renderTag(ctx, {
+        x: x + padding,
+        y: y + padding - 4,
+        width: textWidth + tagPaddingX * 2,
+        height: tagHeight,
+        radius: tagRadius,
+        fillStyle: tagBgColor,
+        borderColor: tagBorderColor,
+        borderWidth: tagBorderWidth,
+      })
+
+      renderSingleLineText(ctx, {
+        x: x + padding + tagPaddingX,
+        y: y,
+        text: truncatedText,
+        maxWidth: maxWidth,
+        fontFamily: `${pv ? 600 : 500} 13px Manrope`,
+        fillStyle: textColor,
+      })
+
+      return {
+        x: x + padding + textWidth + tagPaddingX * 2,
+        y: y + padding - 4 + tagHeight,
+      }
+    } else {
+      const { x: xOffset, y: yOffset } = renderSingleLineText(ctx, {
+        x: x + padding,
+        y,
+        text,
+        maxWidth: width - padding * 2,
+        fontFamily: `${pv ? 600 : 500} 13px Manrope`,
+        fillStyle: selected || pv ? '#4351e8' : textColor,
+        height,
+      })
+
+      return {
+        x: xOffset,
+        y: yOffset,
+      }
+    }
   },
 }

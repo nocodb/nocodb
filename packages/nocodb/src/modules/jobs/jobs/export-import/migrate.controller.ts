f584089e1c90d90a8e81b80c8a8fe6e78178b501
mertmit <mertmit99@gmail.com>
2025-02-18 02:22:39 +0300

    feat: ws migrate
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/modules/jobs/jobs/export-import/migrate.controller.ts
+++ packages/nocodb/src/modules/jobs/jobs/export-import/migrate.controller.ts
@@ -9,6 +9,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { MigrateService } from 'src/modules/jobs/jobs/export-import/migrate.service';
+import { OrgUserRoles } from 'nocodb-sdk';
 import { GlobalGuard } from '~/guards/global/global.guard';
 import { Acl } from '~/middlewares/extract-ids/extract-ids.middleware';
 import { BasesService } from '~/services/bases.service';
@@ -70,5 +71,70 @@ export class MigrateController {
       instanceUrl: instanceUrl,
       req,
     });
+  }
+
+  @Post(['/api/v2/meta/migrate/:workspaceId/workspace'])
+  @HttpCode(200)
+  @Acl('migrateWorkspace', {
+    scope: 'org',
+    allowedRoles: [OrgUserRoles.SUPER_ADMIN],
+    blockApiTokenAccess: true,
+  })
+  async migrateWorkspace(
+    @TenantContext() context: NcContext,
+    @Req() req: NcRequest,
+    @Body()
+    body: {
+      migrationUrl: string;
+    },
+  ) {
+    const url = new URL(body.migrationUrl);
+
+    const instanceUrl = url.origin;
+
+    // secret query param
+    const secret = url.searchParams.get('secret');
+
+    if (!instanceUrl || !secret) {
+      throw new Error(`Invalid migration url`);
+    }
+
+    const bases = await Base.list(context.workspace_id);
+
+    const responses = [];
+
+    const workspaceProgress = {
+      total: bases.length,
+      current: 0,
+    };
+
+    for (const base of bases) {
+      const source = (await base.getSources())[0];
+
+      if (!source) {
+        continue;
+      }
+
+      workspaceProgress.current++;
+
+      context = {
+        workspace_id: base.fk_workspace_id,
+        base_id: base.id,
+      };
+
+      responses.push(
+        await this.migrateService.migrateBase({
+          context,
+          base,
+          source,
+          secret: secret,
+          instanceUrl: instanceUrl,
+          req,
+          workspaceProgress,
+        }),
+      );
+    }
+
+    return responses;
   }
 }

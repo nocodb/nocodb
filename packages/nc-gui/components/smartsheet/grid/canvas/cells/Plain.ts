fd539557de72c51b1eca6918515a3c32a4bd189a
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-17 11:02:51 +0000

    feat(nc-gui): add plain cell renderer

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/Plain.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/Plain.ts
@@ -1,3 +1,64 @@
+import { getAbstractType, renderSingleLineText, renderTagLabel } from '../utils/canvas'
+
 export const PlainCellRenderer: CellRenderer = {
-  render: (ctx, props) => {},
+  render: (ctx, props) => {
+    const {
+      column,
+      value,
+      x,
+      y,
+      width,
+      height,
+      padding,
+      textColor = '#4a5268',
+      meta,
+      metas,
+      sqlUis,
+      isMssql,
+      isMysql,
+      isXcdbBase,
+      t,
+    } = props
+
+    if (!meta || !metas) return
+
+    const abstractType = getAbstractType(column, sqlUis)
+
+    const text = parsePlainCellValue(value, {
+      col: column,
+      abstractType,
+      meta,
+      metas,
+      isMysql,
+      isMssql,
+      isXcdbBase,
+      t,
+    })
+
+    if (!text) {
+      return {
+        x,
+        y,
+      }
+    }
+
+    if (props.tag?.renderAsTag) {
+      return renderTagLabel(ctx, { ...props, text })
+    } else {
+      const { x: xOffset, y: yOffset } = renderSingleLineText(ctx, {
+        x: x + padding,
+        y,
+        text,
+        maxWidth: width - padding * 2,
+        fontFamily: '500 13px Manrope',
+        fillStyle: textColor,
+        height,
+      })
+
+      return {
+        x: xOffset,
+        y: yOffset,
+      }
+    }
+  },
 }

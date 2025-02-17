2ffeda84384a74e2aacf0abb746fd5ab8b9b8bf4
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-02-17 11:03:20 +0000

    fix: input fields are broken

--- packages/nc-gui/components/smartsheet/grid/canvas/composables/useKeyboardNavigation.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/composables/useKeyboardNavigation.ts
@@ -55,7 +55,7 @@ export function useKeyboardNavigation({
   const meta = inject(MetaInj, ref())
 
   const handleKeyDown = async (e: KeyboardEvent) => {
-    if (isActiveInputElementExist()) return
+    if (isViewSearchActive()) return
     const activeDropdownEl = document.querySelector(
       '.nc-dropdown-single-select-cell.active,.nc-dropdown-multi-select-cell.active',
     )

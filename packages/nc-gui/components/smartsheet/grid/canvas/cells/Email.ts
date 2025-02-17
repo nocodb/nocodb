18452bd2904ef01c118fcb5f703158c348338564
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-17 13:11:01 +0000

    refactor(nc-gui): cell tag render

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/Email.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/Email.ts
@@ -1,17 +1,8 @@
-import { renderMultiLineText, renderSingleLineText, renderTag } from '../utils/canvas'
+import { renderMultiLineText, renderTagLabel } from '../utils/canvas'
 
 export const EmailCellRenderer: CellRenderer = {
   render: (ctx, props) => {
-    const { value, x, y, width, height, selected, pv, padding, textColor = '#4a5268', tag = {} } = props
-    const {
-      renderAsTag,
-      tagPaddingX = 8,
-      tagHeight = 20,
-      tagRadius = 6,
-      tagBgColor = '#f4f4f0',
-      tagBorderColor,
-      tagBorderWidth,
-    } = props.tag || {}
+    const { value, x, y, width, height, selected, pv, padding, textColor = '#4a5268' } = props
 
     const text = value?.toString() ?? ''
 
@@ -24,43 +15,8 @@ export const EmailCellRenderer: CellRenderer = {
 
     const isValidEmail = text && validateEmail(text)
 
-    if (renderAsTag) {
-      const maxWidth = width - padding * 2 - tagPaddingX * 2
-
-      const { text: truncatedText, width: textWidth } = renderSingleLineText(ctx, {
-        x: x + padding + tagPaddingX,
-        y: y + padding,
-        text,
-        maxWidth,
-        fontFamily: `${pv ? 600 : 500} 13px Manrope`,
-        render: false,
-      })
-
-      renderTag(ctx, {
-        x: x + padding,
-        y: y + padding - 4,
-        width: textWidth + tagPaddingX * 2,
-        height: tagHeight,
-        radius: tagRadius,
-        fillStyle: tagBgColor,
-        borderColor: tagBorderColor,
-        borderWidth: tagBorderWidth,
-      })
-
-      renderSingleLineText(ctx, {
-        x: x + padding + tagPaddingX,
-        y: y,
-        text: truncatedText,
-        maxWidth: maxWidth,
-        fontFamily: `${pv ? 600 : 500} 13px Manrope`,
-        fillStyle: textColor,
-        underline: isValidEmail,
-      })
-
-      return {
-        x: x + padding + textWidth + tagPaddingX * 2,
-        y: y + padding - 4 + tagHeight,
-      }
+    if (props.tag?.renderAsTag) {
+      return renderTagLabel(ctx, { ...props, text })
     } else {
       const { x: xOffset, y: yOffset } = renderMultiLineText(ctx, {
         x: x + padding,

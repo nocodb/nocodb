bc58f3fbea8e4733217e5070821104be3f079f4b
Amit Joki <amitjoki@gmail.com>
2025-02-17 13:10:56 +0000

    feat: make qrcode clickable

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/QRCode.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/QRCode.ts
@@ -1,5 +1,33 @@
+interface Position {
+  x: number
+  y: number
+}
+
+interface QRPayload {
+  size: number
+  position: Position
+}
+
+class QRPayloadRegistry {
+  static #map = new Map<string, QRPayload>()
+  static #getKey({ rowId, columnId }: { rowId: string; columnId: string }) {
+    return `${rowId}-${columnId}`
+  }
+
+  static set({ rowId, columnId }: { rowId: string; columnId: string }, payload: QRPayload) {
+    const key = this.#getKey({ rowId, columnId })
+    this.#map.set(key, payload)
+  }
+
+  static get({ rowId, columnId }: { rowId: string; columnId: string }) {
+    const key = this.#getKey({ rowId, columnId })
+    return this.#map.get(key)
+  }
+}
+
 export const QRCodeCellRenderer: CellRenderer = {
-  render: (ctx, { value, x, y, width, height, column, imageLoader, padding }) => {
+  render: (ctx, { value, x, y, width, height, column, imageLoader, padding, row }) => {
+    padding = 4
     if (!value || value === 'ERR!') {
       if (value === 'ERR!') {
         imageLoader.renderError(ctx, 'ERR!', x, y, width, height)
@@ -27,8 +55,28 @@ export const QRCodeCellRenderer: CellRenderer = {
       const xPos = x + (width - size) / 2
       const yPos = y + (height - size) / 2
       imageLoader.renderQRCode(ctx, qrCanvas, xPos, yPos, size)
+      QRPayloadRegistry.set(
+        { rowId: row.Id, columnId: column.id! },
+        {
+          position: { x: xPos, y: yPos },
+          size,
+        },
+      )
     } else {
       imageLoader.renderPlaceholder(ctx, x + padding, y + padding, size, 'qr_code')
+    }
+  },
+
+  async handleClick({ mousePosition, column, row }) {
+    if (!row || !column) return
+    const data = QRPayloadRegistry.get({ rowId: row.row.Id, columnId: column.id! })
+    if (!data) return
+    const { size, position } = data
+    const { x: mouseX, y: mouseY } = mousePosition
+    if (mouseX >= position.x && mouseX <= position.x + size && mouseY >= position.y && mouseY <= position.y + size) {
+      setTimeout(() => {
+        document.querySelector<HTMLElement>('.nc-canvas-table-editable-cell-wrapper .nc-qrcode-container > img')?.click()
+      }, 100)
     }
   },
 }

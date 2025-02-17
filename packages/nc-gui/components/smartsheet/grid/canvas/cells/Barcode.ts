2a612b4a96fef5e2151bd77f8d5b4f0002b8f68f
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-17 13:11:05 +0000

    fix(client): show barcode cell properly in lookup cell and show truncated if required

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/Barcode.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/Barcode.ts
@@ -3,7 +3,8 @@ import { truncateText } from '../utils/canvas'
 
 export const BarcodeCellRenderer: CellRenderer = {
   render: (ctx, props) => {
-    const { value, x, y, width, height, column } = props
+    const { value, x, y, width, height, column, tag = {} } = props
+    const { renderAsTag } = tag
 
     if (!value) return
     const padding = 4
@@ -25,20 +26,31 @@ export const BarcodeCellRenderer: CellRenderer = {
         font: 'Manrope',
       })
 
-      const availableWidth = width - padding * 2
+      const availableWidth = renderAsTag ? Math.min(75, width - padding * 2) : width - padding * 2
       const xPos = x + padding
 
       ctx.drawImage(tempCanvas, xPos, y + padding, availableWidth, height - padding * 2)
+
+      return {
+        x: xPos + availableWidth + 8,
+        y: y + height - padding * 2,
+      }
     } catch (error) {
       ctx.font = `500 13px Manrope`
       ctx.textBaseline = 'middle'
       ctx.textAlign = 'left'
+      ctx.fillStyle = '#4a5268'
 
-      const truncatedText = truncateText(ctx, value.toString(), width - padding * 2)
-      ctx.fillText(truncatedText, x + padding, y + height / 2)
+      const { text, width: textWidth } = truncateText(ctx, value.toString(), width - padding * 2, true)
+      ctx.fillText(text, x + padding, y + height / 2)
 
       ctx.fillStyle = '#ff0000'
-      ctx.fillRect(x + width - 4, y + 4, 2, 2)
+      ctx.fillRect(x + (renderAsTag ? textWidth + 4 : width - 4), y + 4, 2, 2)
+
+      return {
+        x: x + padding + textWidth + 4,
+        y: y + height / 2 + 13,
+      }
     }
   },
 }

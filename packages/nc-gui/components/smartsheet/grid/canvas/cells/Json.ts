a4a87815a76dc00de2f3c31b9fb01bf10d595410
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-17 11:02:47 +0000

    fix(nc-gui): show json cell as multiline text cell and add tag render support

--- packages/nc-gui/components/smartsheet/grid/canvas/cells/Json.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/Json.ts
@@ -1,21 +1,78 @@
-import { truncateText } from '../utils/canvas'
+import { renderMultiLineText, renderSingleLineText, renderTag } from '../utils/canvas'
 
 export const JsonCellRenderer: CellRenderer = {
-  render: (ctx, { value, x, y, width, height, selected, pv, padding }) => {
-    ctx.font = `${pv ? 600 : 500} 13px Manrope`
-    ctx.textBaseline = 'middle'
-    ctx.textAlign = 'left'
+  render: (ctx, props) => {
+    const { selected, value, x, y, width, height, pv, padding, textColor = '#4a5268' } = props
+    const {
+      renderAsTag,
+      tagPaddingX = 8,
+      tagHeight = 20,
+      tagRadius = 6,
+      tagBgColor = '#f4f4f0',
+      tagBorderColor,
+      tagBorderWidth,
+    } = props.tag || {}
 
-    let displayText = ''
-    if (value) {
-      displayText = typeof value === 'string' ? value : JSON.stringify(value)
+    if (!value) {
+      return {
+        x,
+        y,
+      }
     }
 
-    const maxWidth = width - padding * 2
-    const truncatedText = truncateText(ctx, displayText, maxWidth)
-    const textY = y + height / 2
+    const text = typeof value === 'string' ? value : JSON.stringify(value)
 
-    ctx.fillStyle = selected || pv ? '#4351e8' : '#4a5268'
-    ctx.fillText(truncatedText, x + padding, textY)
+    if (renderAsTag) {
+      const maxWidth = width - padding * 2 - tagPaddingX * 2
+
+      const { text: truncatedText, width: textWidth } = renderSingleLineText(ctx, {
+        x: x + padding + tagPaddingX,
+        y: y + padding,
+        text,
+        maxWidth,
+        fontFamily: `${pv ? 600 : 500} 13px Manrope`,
+        render: false,
+      })
+
+      renderTag(ctx, {
+        x: x + padding,
+        y: y + padding - 4,
+        width: textWidth + tagPaddingX * 2,
+        height: tagHeight,
+        radius: tagRadius,
+        fillStyle: tagBgColor,
+        borderColor: tagBorderColor,
+        borderWidth: tagBorderWidth,
+      })
+
+      renderSingleLineText(ctx, {
+        x: x + padding + tagPaddingX,
+        y: y,
+        text: truncatedText,
+        maxWidth: maxWidth,
+        fontFamily: `${pv ? 600 : 500} 13px Manrope`,
+        fillStyle: textColor,
+      })
+
+      return {
+        x: x + padding + textWidth + tagPaddingX * 2,
+        y: y + padding - 4 + tagHeight,
+      }
+    } else {
+      const { x: xOffset, y: yOffset } = renderMultiLineText(ctx, {
+        x: x + padding,
+        y,
+        text,
+        maxWidth: width - padding * 2,
+        fontFamily: `${pv ? 600 : 500} 13px Manrope`,
+        fillStyle: selected || pv ? '#4351e8' : textColor,
+        height,
+      })
+
+      return {
+        x: xOffset,
+        y: yOffset,
+      }
+    }
   },
 }

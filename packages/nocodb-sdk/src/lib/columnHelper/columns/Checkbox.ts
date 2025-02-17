bd44a6ea5c755b6c396c57fe27304161274cf1b2
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-17 20:23:58 +0530

    fix(nocodb-sdk): add checkbox helper

---
+++ packages/nocodb-sdk/src/lib/columnHelper/columns/Checkbox.ts
@@ -0,0 +1,26 @@
+import { AbstractColumnHelper } from '..';
+import { parseCheckboxValue } from '../utils/parser';
+import { serializeCheckboxValue } from '../utils/serializer';
+
+export class CheckboxHelper extends AbstractColumnHelper {
+  columnDefaultMeta = {
+    iconIdx: 0,
+    icon: {
+      checked: 'mdi-check-bold',
+      unchecked: 'mdi-crop-square',
+    },
+    color: '#777',
+  };
+
+  serializeValue(value: any): boolean {
+    return serializeCheckboxValue(value);
+  }
+
+  parseValue(value: any): boolean {
+    return parseCheckboxValue(value);
+  }
+
+  parsePlainCellValue(value: any): string {
+    return parseCheckboxValue(value) ? 'Checked' : 'Unchecked';
+  }
+}

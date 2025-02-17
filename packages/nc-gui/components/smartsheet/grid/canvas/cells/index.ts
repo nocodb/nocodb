d1cced2608f1c72d8517ffcb9d10a27a59eea7f7
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-02-17 13:10:47 +0000

    chore: refactor

---
+++ packages/nc-gui/components/smartsheet/grid/canvas/cells/index.ts
@@ -0,0 +1,96 @@
+import { UITypes } from 'nocodb-sdk'
+import { EmailCellRenderer } from './Email'
+import { SingleLineTextCellRenderer } from './SingleLineText'
+import { LongTextCellRenderer } from './LongText'
+import { FloatCellRenderer } from './Number'
+import { DecimalCellRenderer } from './Decimal'
+import { AttachmentCellRenderer } from './Attachment'
+import { CheckboxCellRenderer } from './Checkbox'
+import { DateCellRenderer } from './Date'
+import { DateTimeCellRenderer } from './DateTime'
+import { YearCellRenderer } from './Year'
+import { TimeCellRenderer } from './Time'
+import { CurrencyRenderer } from './Currency'
+import { PercentCellRenderer } from './Percent'
+import { UrlCellRenderer } from './Url'
+import { GeoDataCellRenderer } from './GeoData'
+import { PhoneNumberCellRenderer } from './PhoneNumber'
+import { DurationCellRenderer } from './Duration'
+import { JsonCellRenderer } from './Json'
+export function useCellRenderer() {
+  const cellTypesRegistry = new Map<string, CellRenderer>()
+
+  cellTypesRegistry.set(UITypes.Email, EmailCellRenderer)
+  cellTypesRegistry.set(UITypes.SingleLineText, SingleLineTextCellRenderer)
+  cellTypesRegistry.set(UITypes.LongText, LongTextCellRenderer)
+  cellTypesRegistry.set(UITypes.Number, FloatCellRenderer)
+  cellTypesRegistry.set(UITypes.Decimal, DecimalCellRenderer)
+  cellTypesRegistry.set(UITypes.Attachment, AttachmentCellRenderer)
+  cellTypesRegistry.set(UITypes.Checkbox, CheckboxCellRenderer)
+  cellTypesRegistry.set(UITypes.Date, DateCellRenderer)
+  cellTypesRegistry.set(UITypes.DateTime, DateTimeCellRenderer)
+  cellTypesRegistry.set(UITypes.Year, YearCellRenderer)
+  cellTypesRegistry.set(UITypes.Time, TimeCellRenderer)
+  cellTypesRegistry.set(UITypes.Currency, CurrencyRenderer)
+  cellTypesRegistry.set(UITypes.Percent, PercentCellRenderer)
+  cellTypesRegistry.set(UITypes.URL, UrlCellRenderer)
+  cellTypesRegistry.set(UITypes.GeoData, GeoDataCellRenderer)
+  cellTypesRegistry.set(UITypes.PhoneNumber, PhoneNumberCellRenderer)
+  cellTypesRegistry.set(UITypes.Duration, DurationCellRenderer)
+  cellTypesRegistry.set(UITypes.CreatedTime, DateTimeCellRenderer)
+  cellTypesRegistry.set(UITypes.LastModifiedTime, DateTimeCellRenderer)
+  cellTypesRegistry.set(UITypes.JSON, JsonCellRenderer)
+
+  const renderCell = (
+    ctx: CanvasRenderingContext2D,
+    column: any,
+    {
+      value,
+      row,
+      x,
+      y,
+      width,
+      height,
+      selected = false,
+      pv = false,
+      readonly = false,
+    }: {
+      value: any
+      row: any
+      x: number
+      y: number
+      width: number
+      height: number
+      selected?: boolean
+      pv?: boolean
+      readonly?: boolean
+    },
+  ) => {
+    const cellType = cellTypesRegistry.get(column.uidt)
+
+    if (cellType) {
+      cellType.render(ctx, {
+        value,
+        row,
+        column,
+        x,
+        y,
+        width,
+        height,
+        selected,
+        pv,
+        readonly,
+      })
+    } else {
+      ctx.fillStyle = pv ? '#4351e8' : '#4a5268'
+      ctx.font = `${pv ? 600 : 500} 13px Manrope`
+      ctx.textBaseline = 'middle'
+      ctx.fillText(value?.toString() ?? '', x + 10, y + height / 2)
+    }
+  }
+
+  return {
+    cellTypesRegistry,
+    renderCell,
+  }
+}

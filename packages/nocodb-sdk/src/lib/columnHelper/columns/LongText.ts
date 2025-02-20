b8728efc92b7f6bf27ca7f291e79e44e81cf9917
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-20 12:56:46 +0000

    fix(sdk): long text parse issue

--- packages/nocodb-sdk/src/lib/columnHelper/columns/LongText.ts
+++ packages/nocodb-sdk/src/lib/columnHelper/columns/LongText.ts
@@ -1,5 +1,20 @@
+import { ColumnType } from '~/lib/Api';
 import { SingleLineTextHelper } from './SingleLineText';
+import { parseProp } from '~/lib/helperFunctions';
+import { LongTextAiMetaProp } from '~/lib/globals';
+import { ncIsObject } from '~/lib/is';
 
 export class LongTextHelper extends SingleLineTextHelper {
   columnDefaultMeta = {};
+  parseValue(value: any, col: ColumnType): string | null {
+    const columnMeta = parseProp(col.meta);
+
+    if (columnMeta?.[LongTextAiMetaProp]) {
+      if (ncIsObject(value)) {
+        return value?.value ?? null;
+      }
+    } else {
+      return (value ?? '').replace(/"/g, '\\"');
+    }
+  }
 }

55540e63477b2076502f1741b4a487a97a44ac2c
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-20 12:56:48 +0000

    feat(sdk): time column helper

--- packages/nocodb-sdk/src/lib/columnHelper/columns/CreatedTime.ts
+++ packages/nocodb-sdk/src/lib/columnHelper/columns/CreatedTime.ts
@@ -1,25 +1,11 @@
 import { SerializerOrParserFnProps } from '../column.interface';
-import { DateHelper } from './Date';
+import { DateTimeHelper } from './DateTime';
 
-export class CreatedTimeHelper extends DateHelper {
+export class CreatedTimeHelper extends DateTimeHelper {
   serializeValue(
     _value: any,
     _params: SerializerOrParserFnProps['params']
   ): string | null {
     return null;
-  }
-
-  parseValue(
-    value: any,
-    params: SerializerOrParserFnProps['params']
-  ): string | null {
-    return super.parseValue(value, { ...params, isSystemCol: true });
-  }
-
-  parsePlainCellValue(
-    value: any,
-    params: SerializerOrParserFnProps['params']
-  ): string | null {
-    return this.parseValue(value, params) ?? '';
   }
 }

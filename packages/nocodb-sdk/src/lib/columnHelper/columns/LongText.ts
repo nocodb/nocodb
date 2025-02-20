712bcba45bf6426fa1263314ed519e63b3205fd3
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-20 12:56:46 +0000

    fix(sdk): longtext copy text format handling and ai field

--- packages/nocodb-sdk/src/lib/columnHelper/columns/LongText.ts
+++ packages/nocodb-sdk/src/lib/columnHelper/columns/LongText.ts
@@ -1,20 +1,31 @@
-import { ColumnType } from '~/lib/Api';
-import { SingleLineTextHelper } from './SingleLineText';
+import { AIRecordType, ColumnType } from '~/lib/Api';
 import { parseProp } from '~/lib/helperFunctions';
 import { LongTextAiMetaProp } from '~/lib/globals';
 import { ncIsObject } from '~/lib/is';
+import AbstractColumnHelper from '../column.interface';
 
-export class LongTextHelper extends SingleLineTextHelper {
+export class LongTextHelper extends AbstractColumnHelper {
   columnDefaultMeta = {};
+
+  public serializeValue(value: any) {
+    return value?.toString()?.trim() ?? null;
+  }
+
   parseValue(value: any, col: ColumnType): string | null {
     const columnMeta = parseProp(col.meta);
 
     if (columnMeta?.[LongTextAiMetaProp]) {
       if (ncIsObject(value)) {
-        return value?.value ?? null;
+        return (value as AIRecordType)?.value ?? null;
       }
     } else {
-      return (value ?? '').replace(/"/g, '\\"');
+      return `"${(value ?? '').replace(/"/g, '\\"')}"`;
     }
+
+    return null;
+  }
+
+  parsePlainCellValue(value: any): string {
+    return value?.toString()?.trim() ?? '';
   }
 }

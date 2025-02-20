e4b1ffbb1a8b516fe8392198133cb1d6cbdda98f
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-20 09:22:10 +0530

    fix(sdk): duration serializer fn

---
+++ packages/nocodb-sdk/src/lib/columnHelper/columns/Attachment.ts
@@ -0,0 +1,44 @@
+import { parseProp } from '~/lib/helperFunctions';
+import { parseDefault } from '..';
+import AbstractColumnHelper from '../column.interface';
+import { ncIsArray, ncIsEmptyObject, ncIsObject } from '~/lib/is';
+
+export class AttachmentHelper extends AbstractColumnHelper {
+  columnDefaultMeta = {};
+
+  // Incomplete
+  serializeValue(value: any) {
+    if (!value) return null;
+
+    let parsedVal = [];
+    if (value) {
+      try {
+        parsedVal = parseProp(value);
+
+        if (ncIsArray(parsedVal)) {
+          parsedVal = parsedVal;
+        } else if (ncIsObject(parsedVal) && !ncIsEmptyObject(parsedVal)) {
+          parsedVal = [parsedVal];
+        } else {
+          parsedVal = [];
+        }
+      } catch (e) {
+        return null;
+      }
+
+      if (
+        parsedVal.some((v: any) => !v || (v && !(v.url || v.data || v.path)))
+      ) {
+        return null;
+      }
+    }
+  }
+
+  parseValue(value: any): boolean {
+    return parseDefault(value);
+  }
+
+  parsePlainCellValue(value: any): string {
+    return parseDefault(value) ?? '';
+  }
+}

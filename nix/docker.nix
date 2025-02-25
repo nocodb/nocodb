41e1eeaf4ac4222885188d6133b97d23e354d3a3
sinanmohd <sinan@sinanmohd.com>
2025-02-25 09:51:28 +0000

    fix(nix/docker): link /bin/sh to bash

--- nix/docker.nix
+++ nix/docker.nix
@@ -5,16 +5,21 @@
 }:
 let
   nocodb = callPackage ./package.nix { };
+  port = 80;
 in
 dockerTools.buildLayeredImage {
   name = "nocodb";
+  contents = [ dockerTools.binSh ];
 
   config = {
+    Env = [
+      "PORT=${builtins.toString port}"
+    ];
+    ExposedPorts = {
+      "${builtins.toString port}/tcp" = { };
+    };
     Entrypoint = [
       (lib.getExe nocodb)
     ];
-    ExposedPorts = {
-      "8080/tcp" = { };
-    };
   };
 }

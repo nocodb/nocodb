098d5279fa505e894209e93c473490a196582329
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-02-27 09:16:32 +0000

    feat(nc-gui): add page leaving warning screen

---
+++ packages/nc-gui/components/general/PageLeavingWarning.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const getRedirectLink = () => {
+  return 'https://drive.google.com/drive/u/1/folders/1ilDWrbDjFXSqnDytwVl_8R5NVsZ7'
+}
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-3">
+    <div class="text-base font-bold text-nc-content-gray">{{ $t('title.youAreLeavingNocoDB') }}</div>
+    <div class="text-sm font-weight-500 text-nc-content-gray-subtle2">{{ $t('title.onlyProceedIfYouTrustThisLink') }}</div>
+    <div class="text-sm font-weight-500 text-nc-content-gray-subtle">{{ getRedirectLink() }}</div>
+    <div class="flex items-center gap-3">
+      <NcButton type="secondary">
+        {{ $t('general.cancel') }}
+      </NcButton>
+      <NcButton>
+        {{ $t('labels.proceedToLink') }}
+      </NcButton>
+    </div>
+  </div>
+</template>

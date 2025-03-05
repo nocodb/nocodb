b5434ee3bc0d5b636513dfe335a07fbaf9ddf48c
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-03-05 14:51:11 +0000

    feat(sdk): some column helpers

--- packages/nocodb-sdk/src/lib/columnHelper/column.interface.ts
+++ packages/nocodb-sdk/src/lib/columnHelper/column.interface.ts
@@ -1,7 +1,7 @@
 import { ColumnType } from '../Api';
 
 export default abstract class AbstractColumnHelper {
-  public static columnDefaultMeta?: Record<string, any> = {};
+  public columnDefaultMeta?: Record<string, any> = {};
   public abstract serializeValue(value: unknown, column: ColumnType): any;
   public abstract parseValue(value: unknown, column: ColumnType): any;
   public abstract parsePlainCellValue(

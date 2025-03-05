eaa9d74ae12c84801071f1100bbfb2d365a4b274
Anurag Tripathi <raganu1209@gmail.com>
2025-03-05 18:50:52 +0530

    Refactor R2Plugin: remove commented-out code and clean up constructor

--- packages/nocodb/src/plugins/r2/R2Plugin.ts
+++ packages/nocodb/src/plugins/r2/R2Plugin.ts
@@ -4,19 +4,9 @@ import type { IStorageAdapterV2 } from '~/types/nc-plugin';
 import { XcStoragePlugin } from '~/types/nc-plugin';
 
 class R2Plugin extends XcStoragePlugin {
-<<<<<<< HEAD
     private static storageAdapter: R2;
     private cloudflare: any;
     private config: any;
-=======
-  private static storageAdapter: R2;
-  private cloudflare: any;
-
-  constructor() {
-    super();
-    this.cloudflare = new Cloudflare({ token: 'your-api-key' });
-  }
->>>>>>> 0bb8e498c19e6ee31f0729e8dc322a6902423e18
 
     constructor(config: any) {
         super();
@@ -26,7 +16,6 @@ class R2Plugin extends XcStoragePlugin {
         });
     }
 
-<<<<<<< HEAD
     public getAdapter(): IStorageAdapterV2 {
         return R2Plugin.storageAdapter;
     }
@@ -64,33 +53,6 @@ class R2Plugin extends XcStoragePlugin {
 
         console.log("Custom domain set:", response.domain);
     }
-=======
-  public async init(config: any): Promise<any> {
-    const { custom_domain, bucket } = config;
-
-    R2Plugin.storageAdapter = new R2(config);
-    await R2Plugin.storageAdapter.init();
-
-    // Set the custom domain if provided
-    if (custom_domain) {
-      await this.setCustomDomain(bucket, custom_domain);
-    }
-  }
-
-  private async setCustomDomain(bucketName: string, domainName: string) {
-    const accountId = 'your-account-id';
-    const zoneId = 'your-zone-id';
-
-    const response = await this.cloudflare.r2.buckets.domains.custom.create({
-      bucketName,
-      domain: domainName,
-      enabled: true,
-      zoneId,
-    }, { accountId });
-
-    console.log('Custom domain set:', response.domain);
-  }
->>>>>>> 0bb8e498c19e6ee31f0729e8dc322a6902423e18
 }
 
 export default R2Plugin;

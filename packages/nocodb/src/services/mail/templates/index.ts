8a8cb88693add7e7d59a8721b435386602f3a52c
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-03-07 05:00:37 +0000

    fix: jest tests

--- packages/nocodb/src/services/mail/templates/index.ts
+++ packages/nocodb/src/services/mail/templates/index.ts
@@ -1,11 +1,11 @@
-import Welcome from './welcome';
-import BaseInvite from './base-invite';
-import PasswordReset from './password-reset';
-import VerifyEmail from './verify-your-email';
-import OrganizationInvite from './org-invite';
-import OrganizationRoleUpdate from './org-role-update';
-import BaseRoleUpdate from './base-role-update';
-import FormSubmission from './form-submission';
+import Welcome from '~/services/mail/templates/welcome';
+import BaseInvite from '~/services/mail/templates/base-invite';
+import PasswordReset from '~/services/mail/templates/password-reset';
+import VerifyEmail from '~/services/mail/templates/verify-your-email';
+import OrganizationInvite from '~/services/mail/templates/org-invite';
+import OrganizationRoleUpdate from '~/services/mail/templates/org-role-update';
+import BaseRoleUpdate from '~/services/mail/templates/base-role-update';
+import FormSubmission from '~/services/mail/templates/form-submission';
 
 export {
   Welcome,

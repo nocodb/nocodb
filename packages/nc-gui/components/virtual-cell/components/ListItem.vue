25399df5e599b54dd1a658d273a9c3c8c151c5ff
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-03-08 07:00:39 +0000

    fix(nc-gui): show link record extra fields in public view also

--- packages/nc-gui/components/virtual-cell/components/ListItem.vue
+++ packages/nc-gui/components/virtual-cell/components/ListItem.vue
@@ -119,10 +119,7 @@ const displayValue = computed(() => {
             </span>
           </div>
 
-          <div
-            v-if="fields.length > 0 && (!isPublic || isForm)"
-            class="flex ml-[-0.25rem] sm:flex-row xs:(flex-col mt-2) gap-4 min-h-5"
-          >
+          <div v-if="fields.length > 0" class="flex ml-[-0.25rem] sm:flex-row xs:(flex-col mt-2) gap-4 min-h-5">
             <div v-for="field in fields" :key="field.id" class="sm:(w-1/3 max-w-1/3 overflow-hidden)">
               <div v-if="!isRowEmpty({ row }, field)" class="flex flex-col gap-[-1]">
                 <NcTooltip class="z-10 flex" placement="bottomLeft" :arrow-point-at-center="false">

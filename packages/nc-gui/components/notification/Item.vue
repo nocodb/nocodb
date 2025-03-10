453bd56254b7b027378ba508fb5c46cdbf307d4e
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-03-10 07:53:50 +0000

    fix: workspace user invite notification is not shown in ui

--- packages/nc-gui/components/notification/Item.vue
+++ packages/nc-gui/components/notification/Item.vue
@@ -17,7 +17,7 @@ const { toggleRead } = notificationStore
   <div class="select-none" @click="toggleRead(item, item.is_read)">
     <NotificationItemWelcome v-if="item.type === AppEvents.WELCOME" :item="item" />
     <NotificationItemProjectInvite v-else-if="item.type === AppEvents.PROJECT_INVITE" :item="item" />
-    <NotificationItemWorkspaceInvite v-else-if="item.type === AppEvents.WORKSPACE_INVITE" :item="item" />
+    <NotificationItemWorkspaceInvite v-else-if="item.type === AppEvents.WORKSPACE_USER_INVITE" :item="item" />
     <NotificationItemMentionEvent v-else-if="['mention'].includes(item.type)" :item="item" />
     <NotificationItemRowMentionEvent v-else-if="AppEvents.ROW_USER_MENTION === item.type" :item="item" />
     <span v-else />

9c42fb607d8a9c17cee906334b2b9e0bf596228c
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-03-10 06:10:11 +0000

    fix(nc-gui): make cell text slightly darker in expanded form if it is sql view

--- packages/nc-gui/components/smartsheet/expanded-form/presentors/Fields/ColumnList.vue
+++ packages/nc-gui/components/smartsheet/expanded-form/presentors/Fields/ColumnList.vue
@@ -84,6 +84,7 @@ const showCol = (col: ColumnType) => {
             '!select-text nc-system-field bg-nc-bg-gray-extralight !text-nc-content-inverted-primary-disabled cursor-pointer':
               isReadOnlyVirtualCell(col) && shouldApplyDataCell(col) && !isLinksOrLTAR(col),
             '!select-text nc-readonly-div-data-cell': readOnly,
+            'nc-sql-view': isSqlView,
           }"
         >
           <LazySmartsheetVirtualCell
@@ -145,7 +146,15 @@ const showCol = (col: ColumnType) => {
     .nc-virtual-cell {
       @apply text-gray-400;
     }
+
+    &.nc-sql-view {
+      .nc-cell,
+      .nc-virtual-cell {
+        @apply text-gray-500;
+      }
+    }
   }
+
   &.nc-readonly-div-data-cell:focus-within,
   &.nc-system-field:focus-within {
     @apply !border-gray-200;

6121ab3c1d595611b7b1a4b92b27c7650e306c34
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-03-10 06:00:35 +0000

    fix(nocodb): trim single select value on type conversion

--- packages/nocodb/src/services/columns.service.ts
+++ packages/nocodb/src/services/columns.service.ts
@@ -1411,6 +1411,40 @@ export class ColumnsService implements IColumnsService {
           }
         }
 
+        // handle trim single select value
+        if (
+          column.uidt !== UITypes.SingleSelect &&
+          colBody.uidt === UITypes.SingleSelect
+        ) {
+          if (driverType === 'mssql') {
+            await sqlClient.raw(
+              `UPDATE ??
+               SET ?? = LTRIM(RTRIM(??))
+               WHERE ?? <> LTRIM(RTRIM(??))`,
+              [
+                baseModel.getTnPath(table.table_name),
+                column.column_name,
+                column.column_name,
+                column.column_name,
+                column.column_name,
+              ],
+            );
+          } else {
+            await sqlClient.raw(
+              `UPDATE ??
+               SET ?? = TRIM(??)
+               WHERE ?? <> TRIM(??)`,
+              [
+                baseModel.getTnPath(table.table_name),
+                column.column_name,
+                column.column_name,
+                column.column_name,
+                column.column_name,
+              ],
+            );
+          }
+        }
+
         // Update value in filters that reference this column
         const filters = await Filter.getFiltersByColumn(context, column.id);
 

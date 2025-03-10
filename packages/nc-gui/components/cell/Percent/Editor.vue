68eb96fe27f0ea70ec01034c4aff18a8ddc8ab28
Fendy Heryanto <fendy@nocodb.com>
2025-03-10 11:30:17 +0700

    handle for form progress

--- packages/nc-gui/components/cell/Percent/Editor.vue
+++ packages/nc-gui/components/cell/Percent/Editor.vue
@@ -53,6 +53,15 @@ const vModel = computed({
     }
   },
 })
+const vModelNumber = computed<number>(() => {
+  if (_vModel.value && _vModel.value !== '' && !isNaN(Number(_vModel.value))) {
+    return Number(_vModel.value)
+  }
+  return 0
+})
+const labelMarginLeft = computed<number>(() => {
+  return Math.max(1, Math.min(vModelNumber.value / 2, 50))
+})
 
 const inputType = computed(() => (isForm.value && !isEditColumn.value ? 'text' : 'number'))
 
@@ -65,7 +74,9 @@ const onBlur = () => {
 const onFocus = () => {
   cellFocused.value = true
 }
-
+const onContainerFocus = ($event) => {
+  setTimeout(() => $event?.target?.querySelector('input').focus(), 100)
+}
 onMounted(() => {
   if (isCanvasInjected && (!isExpandedFormOpen.value || localEditEnabled.value) && !isEditColumn.value && !isForm.value) {
     inputRef.value?.focus()
@@ -77,25 +88,78 @@ onMounted(() => {
 </script>
 
 <template>
-  <!-- eslint-disable vue/use-v-on-exact -->
-  <input
-    :ref="focus"
-    v-model="vModel"
-    class="nc-cell-field w-full !border-none !outline-none focus:ring-0 py-1"
-    :type="inputType"
-    :placeholder="placeholder"
-    :disabled="readOnly"
-    @blur="onBlur"
-    @focus="onFocus"
-    @keydown.down.stop
-    @keydown.left.stop
-    @keydown.right.stop
-    @keydown.up.stop
-    @keydown.delete.stop
-    @keydown.alt.stop
-    @selectstart.capture.stop
-    @mousedown.stop
-  />
+  <div
+    v-if="isForm"
+    tabindex="0"
+    class="flex h-full w-full progress-container"
+    style="align-self: stretch; justify-self: stretch"
+    @focus="onContainerFocus"
+  >
+    <!-- eslint-disable vue/use-v-on-exact -->
+    <input
+      :ref="focus"
+      v-model="vModel"
+      class="nc-cell-field w-full !border-none !outline-none focus:ring-0 py-1"
+      :type="inputType"
+      :placeholder="placeholder"
+      :disabled="readOnly"
+      @blur="onBlur"
+      @focus="onFocus"
+      @keydown.down.stop
+      @keydown.left.stop
+      @keydown.right.stop
+      @keydown.up.stop
+      @keydown.delete.stop
+      @keydown.alt.stop
+      @selectstart.capture.stop
+      @mousedown.stop
+    />
+    <div class="h-full w-full progress-bar flex" style="align-self: stretch; border-radius: 9999px; overflow: hidden">
+      <div style="align-self: stretch; background-color: #3366ff" :style="{ width: `${vModelNumber}%` }"></div>
+      <div style="align-self: stretch; background-color: #e5e5e5" :style="{ width: `${100 - vModelNumber}%` }"></div>
+      <div style="position: absolute" :style="{ 'margin-left': `${labelMarginLeft}%` }">
+        <span
+          style="mix-blend-mode: difference; color: #ffffff"
+          :style="{
+            'margin-left': `${-Math.min(vModelNumber, 50)}%`,
+          }"
+        >
+          {{ `${vModelNumber}%` }}
+        </span>
+      </div>
+      <div style="position: absolute" :style="{ 'margin-left': `${labelMarginLeft}%` }">
+        <span
+          style="mix-blend-mode: overlay; color: #ffffff"
+          :style="{
+            'margin-left': `${-Math.min(vModelNumber, 50)}%`,
+          }"
+        >
+          {{ `${vModelNumber}%` }}
+        </span>
+      </div>
+    </div>
+  </div>
+  <div v-else>
+    <!-- eslint-disable vue/use-v-on-exact -->
+    <input
+      :ref="focus"
+      v-model="vModel"
+      class="nc-cell-field w-full !border-none !outline-none focus:ring-0 py-1"
+      :type="inputType"
+      :placeholder="placeholder"
+      :disabled="readOnly"
+      @blur="onBlur"
+      @focus="onFocus"
+      @keydown.down.stop
+      @keydown.left.stop
+      @keydown.right.stop
+      @keydown.up.stop
+      @keydown.delete.stop
+      @keydown.alt.stop
+      @selectstart.capture.stop
+      @mousedown.stop
+    />
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -109,5 +173,14 @@ input::-webkit-inner-spin-button {
 /* Firefox */
 input[type='number'] {
   -moz-appearance: textfield;
+}
+.progress-container:not(:focus-within) > input {
+  visibility: collapse;
+  display: none;
+}
+.progress-container:focus > div.progress-bar,
+.progress-container > input:focus ~ div.progress-bar {
+  visibility: collapse;
+  display: none;
 }
 </style>

eff45a2ed2b33539300459338673c9a27e317d8b
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-03-12 07:42:22 +0000

    feat: move rendering to runtime

---
+++ packages/nocodb/src/services/mail/templates/base-role-update.tsx
@@ -0,0 +1,83 @@
+import {
+  Body,
+  Button,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+} from '@react-email/components';
+import * as React from 'react';
+import {
+  ContentWrapper,
+  Footer,
+  RootWrapper,
+} from '~/services/mail/templates/components';
+import { NC_EMAIL_ASSETS_BASE_URL } from '~/constants';
+
+interface BaseRoleUpdateTemplateProps {
+  baseTitle: string;
+  role: string;
+  name: string;
+  email: string;
+  link: string;
+}
+
+export const BaseRoleUpdate = ({
+  baseTitle,
+  email,
+  link,
+  role,
+  name,
+}: BaseRoleUpdateTemplateProps) => (
+  <Html>
+    <RootWrapper>
+      <Head />
+      <Preview>Your base role has been updated</Preview>
+      <Body className="bg-white">
+        <ContentWrapper>
+          <Heading className="text-gray-900 text-center font-bold m-auto text-xl md:text-2xl">
+            Your base role has been updated
+          </Heading>
+          <Section className="py-6 text-center font-bold text-gray-900 text-base">
+            {baseTitle}
+          </Section>
+          <Section className="pb-6 text-center">
+            <Img
+              src={`${NC_EMAIL_ASSETS_BASE_URL}/badges/${role}.png`}
+              alt={role}
+              className="h-7 mx-auto"
+            />
+          </Section>
+          <Text className="text-gray-600 text-center text-sm !mt-0">
+            Your access in
+            <span className="font-bold text-gray-800">{baseTitle}</span>
+            has been updated to{' '}
+            <span className="font-bold text-gray-800 capitalize ">{role}</span>
+            by <span className="font-bold text-gray-800">{` ${name}`}</span> ({' '}
+            {email})
+          </Text>
+          <Button
+            className="text-center w-full text-base font-bold bg-brand-500 text-white rounded-lg h-10"
+            href={link}
+          >
+            <Text className="!my-[8px]">Go to Base</Text>
+          </Button>
+        </ContentWrapper>
+        <Footer />
+      </Body>
+    </RootWrapper>
+  </Html>
+);
+
+BaseRoleUpdate.PreviewProps = {
+  baseTitle: 'Base Title',
+  role: 'editor',
+  name: 'John Doe',
+  email: 'johndoe@nocodb.com',
+  link: 'www.nocodb.com',
+};
+
+export default BaseRoleUpdate;
--- packages/nocodb/src/services/mail/templates/components/ContentWrapper.tsx
+++ packages/nocodb/src/services/mail/templates/components/ContentWrapper.tsx
@@ -1,16 +1,14 @@
-import { Img, Section, Container } from '@react-email/components';
-import { baseUrl } from '../utils/constant';
+import { Container, Img, Section } from '@react-email/components';
 import * as React from 'react';
+import { NC_EMAIL_ASSETS_BASE_URL } from '~/constants';
 
-const ContentWrapper = ({ children }: {
-  children: React.ReactNode;
-}) => {
+export const ContentWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <Container className="px-3 my-16 max-w-[480px]">
       <Section className="py-6 m-auto bg-gray-50 border border-gray-200 border-solid rounded-t-xl">
         <Img
           alt="NocoDB"
-          src={`${baseUrl}/nocodb-logo.png`}
+          src={`${NC_EMAIL_ASSETS_BASE_URL}/nocodb-logo.png`}
           width={40}
           style={{ display: 'block', margin: 'auto auto' }}
           height={40}
@@ -19,9 +17,6 @@ const ContentWrapper = ({ children }: {
       <Section className="p-6 border border-gray-200 border-solid border-t-0 rounded-b-xl bg-white">
         {children}
       </Section>
-
     </Container>
-  )
-}
-
-export default ContentWrapper;
+  );
+};

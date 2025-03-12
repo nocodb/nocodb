05079d9ae61cc12dfef00edfb9ae8c263986aaeb
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-03-12 07:42:19 +0000

    feat: action to sync template assets

--- packages/nc-mail-template/components/ContentWrapper.tsx
+++ packages/nc-mail-template/components/ContentWrapper.tsx
@@ -1,14 +1,16 @@
-import { Button, Heading, Img, Section, Text, Container } from '@react-email/components';
+import { Img, Section, Container } from '@react-email/components';
 import { baseUrl } from '../utils/constant';
 import * as React from 'react';
 
-const ContentWrapper = ({ children }) => {
+const ContentWrapper = ({ children }: {
+  children: React.ReactNode;
+}) => {
   return (
     <Container className="px-3 my-16 max-w-[480px]">
       <Section className="py-6 m-auto bg-gray-50 border border-gray-200 border-solid rounded-t-xl">
         <Img
           alt="NocoDB"
-          src={`${baseUrl}/static/nocodb-logo.png`}
+          src={`${baseUrl}/nocodb-logo.png`}
           width={40}
           style={{ display: 'block', margin: 'auto auto' }}
           height={40}

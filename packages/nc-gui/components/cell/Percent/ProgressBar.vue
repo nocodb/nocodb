1f78f3f93d8eddb65c6ac57c513f4a81e4644e34
Fendy Heryanto <fendy@nocodb.com>
2025-03-14 03:21:35 +0000

    show plain input on expanded form add little bit of animation

--- packages/nc-gui/components/cell/Percent/ProgressBar.vue
+++ packages/nc-gui/components/cell/Percent/ProgressBar.vue
@@ -13,13 +13,13 @@ const labelMarginLeft = computed<number>(() => {
 
 <template>
   <div
-    class="flex w-full progress-container min-h-[4px]"
+    class="flex flex-col w-full progress-container min-h-[4px]"
     style="align-self: stretch; justify-self: stretch; height: 100%; border-radius: 9999px"
   >
     <div class="progress-bar-input">
       <slot></slot>
     </div>
-    <div class="w-full progress-bar flex" style="align-self: stretch; border-radius: 9999px; overflow: hidden">
+    <div class="w-full progress-bar flex" style="align-self: stretch; border-radius: 9999px; overflow: hidden; height: 100%">
       <div style="align-self: stretch; background-color: #3366ff" :style="{ width: `${cPercentage}%` }"></div>
       <div style="align-self: stretch; background-color: #e5e5e5" :style="{ width: `${100 - cPercentage}%` }"></div>
       <template v-if="isShowNumber">
@@ -50,20 +50,30 @@ const labelMarginLeft = computed<number>(() => {
 
 <style lang="scss" scoped>
 .progress-container:not(:focus-within):not(:hover) > div.progress-bar-input:not(:focus-within):not(:hover) {
-  // visibility: collapse;
-  // display: none;
   position: absolute;
-  left: -1000px;
+  top: 0px;
+  // left: -1000px;
+  max-height: 0px !important;
+  overflow-y: hidden;
 }
 .progress-container:focus-within > div.progress-bar-input,
 .progress-container:hover > div.progress-bar-input {
   position: relative;
   width: 100%;
+  max-height: 100%;
+  height: 100%;
+  overflow-y: hidden;
+  transition: max-height 0.1s ease-in;
 }
 
+.progress-container:not(:focus-within):not(:hover) > div.progress-bar {
+  
+}
 .progress-container:focus-within > div.progress-bar,
 .progress-container:hover > div.progress-bar {
   visibility: collapse;
+  opacity: 0;
   display: none;
+  transition: visibility 0.1s ease-out, opacity 0.1s ease-out, display 0.1s allow-discrete;
 }
 </style>

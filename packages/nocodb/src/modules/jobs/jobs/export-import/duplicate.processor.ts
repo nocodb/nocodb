6dfed437ef5c12fc1a2b7455facc76882c56645a
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-03-17 06:16:30 +0000

    fix: ui update

--- packages/nocodb/src/modules/jobs/jobs/export-import/duplicate.processor.ts
+++ packages/nocodb/src/modules/jobs/jobs/export-import/duplicate.processor.ts
@@ -83,7 +83,7 @@ export class DuplicateProcessor {
       excludeViews?: boolean;
       excludeComments?: boolean;
     };
-    operation: string;
+    operation: JobTypes;
   }) {
     const hrTime = initTime();
 

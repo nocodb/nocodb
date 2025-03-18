9b7001dd66463238c4ea3abcb74e3684f450d3ef
mertmit <mertmit99@gmail.com>
2025-03-18 10:49:28 +0300

    feat: illustration
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/ee/components/payment/Banner.vue
+++ packages/nc-gui/ee/components/payment/Banner.vue
@@ -9,9 +9,12 @@ const onUpgradePlan = async () => {
 </script>
 
 <template>
-  <div v-if="!isPaidPlan" class="m-6 rounded-lg nc-border-gray-medium p-4 shadow-default max-w-350">
+  <div
+    v-if="!isPaidPlan"
+    class="m-6 rounded-lg border-1 border-nc-border-gray-medium shadow-default max-w-350 bg-nc-bg-orange-light"
+  >
     <div class="flex">
-      <div class="flex flex-col max-w-[560px]">
+      <div class="flex flex-col max-w-[560px] p-4">
         <div class="text-xl font-semibold">Get more from NocoDB</div>
         <div class="my-4">
           Get access to additional features like more seats for your workspace, additional records for Bases, additional storage,
@@ -33,7 +36,9 @@ const onUpgradePlan = async () => {
           </NcButton>
         </div>
       </div>
-      <div></div>
+      <div class="w-full flex items-end justify-end">
+        <img class="rounded-br-lg" src="~assets/img/upgrade-illustration.svg" alt="Upgrade Your Workspace" />
+      </div>
     </div>
   </div>
 </template>

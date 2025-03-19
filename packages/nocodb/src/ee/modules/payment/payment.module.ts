6405684cb058d5ab252b96b2e266b286f57600ce
mertmit <mertmit99@gmail.com>
2025-03-19 11:16:56 +0300

    fix: build error
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/ee/modules/payment/payment.module.ts
+++ packages/nocodb/src/ee/modules/payment/payment.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 
 import { PaymentController } from '~/modules/payment/payment.controller';
 import { PaymentService } from '~/modules/payment/payment.service';
+import { NocoModule } from '~/modules/noco.module';
 
 export const PaymentModuleMetadata = {
-  imports: [],
+  imports: [forwardRef(() => NocoModule)],
   controllers: [PaymentController],
   providers: [PaymentService],
   exports: [PaymentService],

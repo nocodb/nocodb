d982f68273770744e409eae43ca0c22363f7786e
Fendy Heryanto <fendy@nocodb.com>
2025-03-20 04:24:05 +0000

    verify on checkbox and decimal

--- packages/nocodb/src/db/field-handler/handlers/checkbox/checkbox.general.handler.ts
+++ packages/nocodb/src/db/field-handler/handlers/checkbox/checkbox.general.handler.ts
@@ -3,7 +3,35 @@ import type { Column, Filter } from '~/models';
 import { GenericFieldHandler } from '~/db/field-handler/handlers/generic';
 
 export class CheckboxGeneralHandler extends GenericFieldHandler {
-  override async verifyFilter(_filter: Filter, _column: Column) {
+  override async verifyFilter(filter: Filter, column: Column) {
+    const supportedOperations = [
+      'eq',
+      'neq',
+      'blank',
+      'notblank',
+      'is',
+      'isnot',
+    ];
+    if (!supportedOperations.includes(filter.comparison_op)) {
+      return {
+        isValid: false,
+        errors: [
+          `Operation ${filter.comparison_op} is not supported for type ${column.uidt}`,
+        ],
+      } as FilterVeriicationResult;
+    }
+    if (
+      ![null, true, false, 'true', 'false', '', 1, 0, '1', '0'].includes(
+        filter.value,
+      )
+    ) {
+      return {
+        isValid: false,
+        errors: [
+          `Value ${filter.value} is not supported for type ${column.uidt}`,
+        ],
+      } as FilterVeriicationResult;
+    }
     return {
       isValid: true,
     } as FilterVeriicationResult;

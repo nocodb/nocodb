3404f45d3fafbc961e57b1f1712883dbcdd686d0
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-03-21 10:56:50 +0530

    fix(nc-gui): payment banner related ui changes

--- packages/nc-gui/ee/components/payment/Banner.vue
+++ packages/nc-gui/ee/components/payment/Banner.vue
@@ -13,22 +13,27 @@ const onUpgradePlan = async () => {
   >
     <div class="flex">
       <div class="flex flex-col p-6">
-        <div class="text-xl font-semibold">Get more from NocoDB</div>
-        <div class="my-4">
-          Unlock more seats, extra records, more storage, conditional webhooks, integrations, NocoAI, and more!
+        <div class="text-xl font-weight-700 text-nc-content-gray">{{ $t('title.getMoreFromNocodb') }}</div>
+        <div class="mt-2">
+          {{ $t('title.getMoreFromNocodbSubtitle') }}
         </div>
-        <div class="flex">
+        <div class="flex gap-2 mt-5">
           <NcButton
-            class="mt-2 !bg-nc-fill-maroon-dark"
+            class="!bg-nc-fill-maroon-dark"
             type="primary"
             size="small"
             data-testid="nc-workspace-settings-upgrade-button"
+            icon-position="right"
+            inner-class="!gap-2"
             @click="onUpgradePlan"
           >
-            <div class="flex items-center justify-center text-sm gap-1 font-weight-700">
-              <span>Upgrade Plan</span>
+            <template #icon>
               <GeneralIcon icon="ncArrowUpRight" class="h-4 w-4" />
-            </div>
+            </template>
+            {{ $t('labels.upgradePlan') }}
+          </NcButton>
+          <NcButton type="text" size="small" data-testid="nc-workspace-settings-view-all-plan-btn">
+            {{ $t('labels.viewAllPlanDetails') }}
           </NcButton>
         </div>
       </div>

4615b95d205dec3cbeff691b7fa7213ecc5dc99e
ssweber <57631333+ssweber@users.noreply.github.com>
2025-03-21 08:53:48 -0400

    nc fix: Compatibility flag to fix Backblaze S3
    Fixes nocodb#10942
    
    See:
    https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/core/checksums/ResponseChecksumValidation.html
    
    https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/core/checksums/RequestChecksumCalculation.html

--- packages/nocodb/src/plugins/s3/S3.ts
+++ packages/nocodb/src/plugins/s3/S3.ts
@@ -49,6 +49,8 @@ export default class S3 extends GenericS3 implements IStorageAdapterV2 {
     const s3Options: S3ClientConfig = {
       region: this.input.region,
       forcePathStyle: this.input.force_path_style ?? false,
+      requestChecksumCalculation: 'WHEN_REQUIRED',
+      responseChecksumValidation: 'WHEN_REQUIRED'
     };
 
     if (this.input.access_key && this.input.access_secret) {

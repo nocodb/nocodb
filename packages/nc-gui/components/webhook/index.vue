091b2180987fefb62fc12cbd113e5b43ade41ad3
Fendy Heryanto <fendy@nocodb.com>
2025-03-22 11:59:28 +0000

    disable duplicate, align left

--- packages/nc-gui/components/webhook/index.vue
+++ packages/nc-gui/components/webhook/index.vue
@@ -813,8 +813,8 @@ onMounted(async () => {
                           @click="toggleSendMeEverythingChecked"
                           @mousedown.prevent
                         >
-                          <div class="w-full flex gap-2">
-                            <div class="flex-grow">Send me everything</div>
+                          <div class="w-full flex justify-between gap-2">
+                            <div class="flex-grow text-left">Send me everything</div>
                             <div class="flex flex-shrink max-w-[18px]">
                               <a-checkbox v-model:checked="sendMeEverythingChecked" :readonly="true"></a-checkbox>
                             </div>
@@ -838,8 +838,8 @@ onMounted(async () => {
                             "
                             @mousedown.prevent
                           >
-                            <div class="w-full flex gap-2">
-                              <div class="flex-grow">
+                            <div class="w-full flex justify-between gap-2">
+                              <div class="flex-grow text-left">
                                 <span v-if="hookRef.event === 'after'">{{ $t('general.after') }}&nbsp;</span>
                                 {{ operation.text }}
                               </div>

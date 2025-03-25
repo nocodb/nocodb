966406a18d33b7f11ace2441d478498dc97c763d
Fendy Heryanto <fendy@nocodb.com>
2025-03-25 09:01:43 +0000

    skeleton and foundation for several data types

---
+++ packages/nocodb/src/db/field-handler/handlers/decimal/decimal.general.handler.ts
@@ -0,0 +1,11 @@
+import type { FilterVeriicationResult } from '~/db/field-handler/field-handler.interface';
+import type { Column, Filter } from '~/models';
+import { GenericFieldHandler } from '~/db/field-handler/handlers/generic';
+
+export class DecimalGeneralHandler extends GenericFieldHandler {
+  override async verifyFilter(_filter: Filter, _column: Column) {
+    return {
+      isValid: true,
+    } as FilterVeriicationResult;
+  }
+}

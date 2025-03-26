a5dc90b60f412a26e931bbdabb05ae42745b4e9c
mertmit <mertmit99@gmail.com>
2025-03-26 07:25:34 +0000

    fix: source table name mismatch

--- packages/nocodb/src/meta/migrations/v2/nc_037_rename_project_and_base.ts
+++ packages/nocodb/src/meta/migrations/v2/nc_037_rename_project_and_base.ts
@@ -7,7 +7,7 @@ const logger = new Logger('nc_036_rename_project_and_base');
 const up = async (knex: Knex) => {
   logger.log('Renaming base table');
   if (await knex.schema.hasTable(MetaTableOldV2.BASES))
-    await knex.schema.renameTable(MetaTableOldV2.BASES, MetaTable.SOURCES);
+    await knex.schema.renameTable(MetaTableOldV2.BASES, MetaTable.SOURCES_OLD);
 
   logger.log('Renaming `base_id` column to `source_id`');
   if (await knex.schema.hasColumn(MetaTable.MODELS, 'base_id'))
@@ -349,10 +349,10 @@ const up = async (knex: Knex) => {
     });
 
   logger.log(
-    `Renaming 'project_id' column to 'base_id' in '${MetaTable.SOURCES}' table`,
+    `Renaming 'project_id' column to 'base_id' in '${MetaTable.SOURCES_OLD}' table`,
   );
-  if (await knex.schema.hasColumn(MetaTable.SOURCES, 'project_id'))
-    await knex.schema.alterTable(MetaTable.SOURCES, (table) => {
+  if (await knex.schema.hasColumn(MetaTable.SOURCES_OLD, 'project_id'))
+    await knex.schema.alterTable(MetaTable.SOURCES_OLD, (table) => {
       table.renameColumn('project_id', 'base_id');
     });
 

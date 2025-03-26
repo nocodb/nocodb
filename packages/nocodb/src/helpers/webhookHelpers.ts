88795b41e3474dbb4ce570a9dd68b427f4602477
Fendy Heryanto <fendy@nocodb.com>
2025-03-26 07:01:04 +0000

    fix trigger fields update

--- packages/nocodb/src/helpers/webhookHelpers.ts
+++ packages/nocodb/src/helpers/webhookHelpers.ts
@@ -1102,7 +1102,7 @@ export async function getAffectedColumns(
     model: Model;
   },
 ) {
-  if (hookName !== 'after.update') {
+  if (hookName !== 'after.update' && hookName !== 'after.bulkUpdate') {
     return undefined;
   }
   let affectedCols = [];

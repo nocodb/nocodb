803fbcaa2048b42da65335e716b10c290ca25e9b
Fendy Heryanto <fendy@nocodb.com>
2025-03-26 07:01:01 +0000

    working field trigger

--- packages/nocodb/src/helpers/webhookHelpers.ts
+++ packages/nocodb/src/helpers/webhookHelpers.ts
@@ -1069,9 +1069,11 @@ export async function getAffectedColumns(
     return undefined;
   }
   const compareSingle = (prev, next) => {
+    console.log(prev, next);
     const updatePayload = populateUpdatePayloadDiff({
       prev,
       next,
+      keepUnderModified: true,
     }) as UpdatePayload;
     if (updatePayload) {
       affectedCols = affectedCols.concat(

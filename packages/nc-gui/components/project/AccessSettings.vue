3255cf870fffbc63d5f74902fe3ef0202d38b02c
Pranav C <pranavxc@gmail.com>
2025-03-28 21:32:42 +0000

    Revert "fix: add option to alter role inheriting from workspace to base"

--- packages/nc-gui/components/project/AccessSettings.vue
+++ packages/nc-gui/components/project/AccessSettings.vue
@@ -104,7 +104,6 @@ const isOwnerOrCreator = computed(() => {
 })
 
 const updateCollaborator = async (collab: any, roles: ProjectRoles) => {
-  debugger
   const currentCollaborator = collaborators.value.find((coll) => coll.id === collab.id)!
 
   try {

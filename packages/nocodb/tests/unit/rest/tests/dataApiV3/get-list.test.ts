2bbfee0f0fd281d19cd2fd8e063e2ce1ffdb2ab1
Fendy Heryanto <fendy@nocodb.com>
2025-04-01 21:07:13 +0700

    attempt to add number based test

--- packages/nocodb/tests/unit/rest/tests/dataApiV3/get-list.test.ts
+++ packages/nocodb/tests/unit/rest/tests/dataApiV3/get-list.test.ts
@@ -1,11 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { expect } from 'chai';
-import { isCreatedOrLastModifiedTimeCol, ViewTypes } from 'nocodb-sdk';
+import {
+  convertMS2Duration,
+  isCreatedOrLastModifiedTimeCol,
+  ViewTypes,
+} from 'nocodb-sdk';
 import {
   createLookupColumn,
   createRollupColumn,
 } from '../../../factory/column';
 import { createView, updateView } from '../../../factory/view';
 import {
+  beforeEachNumberBased,
   beforeEachTextBased,
   beforeEach as dataApiV3BeforeEach,
 } from './beforeEach';
@@ -24,7 +30,7 @@ interface ListResult {
 
 const API_VERSION = 'v3';
 
-describe('dataApiV3', () => {
+describe.only('dataApiV3', () => {
   describe('get-list', () => {
     let testContext: ITestContext;
     let testAxios: INcAxios;
@@ -730,6 +736,230 @@ describe('dataApiV3', () => {
         });
       });
       // #endregion error handling
+    });
+    describe('number-based', () => {
+      let table: Model;
+      let columns: Column[] = [];
+      let insertedRecords: any[];
+
+      beforeEach(async function () {
+        const initResult = await beforeEachNumberBased(testContext);
+        table = initResult.table;
+        columns = initResult.columns;
+        insertedRecords = initResult.insertedRecords;
+      });
+
+      const records: {
+        Id?: number | null;
+        Number?: number | null;
+        Decimal?: number | null;
+        Currency?: number | null;
+        Percent?: number | null;
+        Duration?: number | null;
+        Rating?: number | null;
+      }[] = [
+        {
+          Id: 1,
+          Number: 33,
+          Decimal: 33.3,
+          Currency: 33.3,
+          Percent: 33,
+          Duration: 10 * 60,
+          Rating: 0,
+        },
+        {
+          Id: 2,
+          Number: null,
+          Decimal: 456.34,
+          Currency: 456.34,
+          Percent: null,
+          Duration: 20 * 60,
+          Rating: 1,
+        },
+        {
+          Id: 3,
+          Number: 456,
+          Decimal: 333.3,
+          Currency: 333.3,
+          Percent: 456,
+          Duration: 30 * 60,
+          Rating: 2,
+        },
+        {
+          Id: 4,
+          Number: 333,
+          Decimal: null,
+          Currency: null,
+          Percent: 333,
+          Duration: 40 * 60,
+          Rating: 3,
+        },
+        {
+          Id: 5,
+          Number: 267,
+          Decimal: 267.5674,
+          Currency: 267.5674,
+          Percent: 267,
+          Duration: 50 * 60,
+          Rating: null,
+        },
+        {
+          Id: 6,
+          Number: 34,
+          Decimal: 34,
+          Currency: 34,
+          Percent: 34,
+          Duration: 60 * 60,
+          Rating: 0,
+        },
+        {
+          Id: 7,
+          Number: 8754,
+          Decimal: 8754,
+          Currency: 8754,
+          Percent: 8754,
+          Duration: null,
+          Rating: 4,
+        },
+        {
+          Id: 8,
+          Number: 3234,
+          Decimal: 3234.547,
+          Currency: 3234.547,
+          Percent: 3234,
+          Duration: 70 * 60,
+          Rating: 5,
+        },
+        {
+          Id: 9,
+          Number: 44,
+          Decimal: 44.2647,
+          Currency: 44.2647,
+          Percent: 44,
+          Duration: 80 * 60,
+          Rating: 0,
+        },
+        {
+          Id: 10,
+          Number: 33,
+          Decimal: 33.98,
+          Currency: 33.98,
+          Percent: 33,
+          Duration: 90 * 60,
+          Rating: 1,
+        },
+      ];
+
+      // TODO: skipped for now, v3 CRUD api is not ordered by db rows
+      it.skip('Number based- List & CRUD', async function () {
+        // list 10 records
+        let rsp = await testAxios.ncAxiosGet({
+          url: `${urlPrefix}/${table.id}`,
+          query: {
+            limit: 10,
+            fields: 'Id,Number,Decimal,Currency,Percent,Duration,Rating',
+          },
+        });
+
+        expect(rsp.body.pageInfo).to.have.property('next');
+        expect(rsp.body.pageInfo.next).to.include(
+          `${urlPrefix}/${table.id}?page=2`,
+        );
+        expect(rsp.body.list).to.deep.equal(records);
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        // insert 10 records
+        // remove Id's from record array
+        records.forEach((r) => delete r.Id);
+        rsp = await testAxios.ncAxiosPost({
+          url: `${urlPrefix}/${table.id}`,
+          body: records.map((k) => ({
+            ...k,
+            Duration: convertMS2Duration(k.Duration, 0),
+          })),
+        });
+
+        // prepare array with 10 Id's, from 401 to 410
+        const ids: { Id: number }[] = [];
+        for (let i = 401; i <= 410; i++) {
+          ids.push({ Id: i });
+        }
+        expect(rsp.body.sort((a, b) => a.Id - b.Id)).to.deep.equal(ids);
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        // read record with Id 401
+        rsp = await testAxios.ncAxiosGet({
+          url: `${urlPrefix}/${table.id}/401`,
+          query: {
+            fields: 'Id,Number,Decimal,Currency,Percent,Duration,Rating',
+          },
+        });
+
+        expect(rsp.body).to.deep.equal({ ...records[0], Id: 401 });
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        // update record with Id 401 to 404
+        const updatedRecord = {
+          Number: 55,
+          Decimal: 55.5,
+          Currency: 55.5,
+          Percent: 55,
+          Duration: 55,
+          Rating: 5,
+        };
+
+        const updatedRecords = [
+          {
+            Id: 401,
+            ...updatedRecord,
+          },
+          {
+            Id: 402,
+            ...updatedRecord,
+          },
+          {
+            Id: 403,
+            ...updatedRecord,
+          },
+          {
+            Id: 404,
+            ...updatedRecord,
+          },
+        ];
+        rsp = await testAxios.ncAxiosPatch({
+          url: `${urlPrefix}/${table.id}`,
+          body: updatedRecords,
+        });
+        expect(rsp.body).to.deep.equal(
+          updatedRecords.map((record) => ({ Id: record.Id })),
+        );
+
+        // verify updated records
+        rsp = await testAxios.ncAxiosGet({
+          url: `${urlPrefix}/${table.id}`,
+          query: {
+            limit: 10,
+            offset: 400,
+            fields: 'Id,Number,Decimal,Currency,Percent,Duration,Rating',
+          },
+        });
+
+        expect(rsp.body.list.slice(0, 4)).to.deep.equal(updatedRecords);
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        // delete record with ID 401 to 404
+        rsp = await testAxios.ncAxiosDelete({
+          url: `${urlPrefix}/${table.id}`,
+          body: updatedRecords.map((record) => ({ Id: record.Id })),
+        });
+        expect(rsp.body).to.deep.equal(
+          updatedRecords.map((record) => ({ Id: record.Id })),
+        );
+      });
     });
   });
 });

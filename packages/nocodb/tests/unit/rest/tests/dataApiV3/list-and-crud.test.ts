d2e3c7a4d129c1e87b4673fc35f6359151a70ef5
Fendy Heryanto <fendy@nocodb.com>
2025-04-01 21:30:36 +0700

    attempt to add select based

--- packages/nocodb/tests/unit/rest/tests/dataApiV3/list-and-crud.test.ts
+++ packages/nocodb/tests/unit/rest/tests/dataApiV3/list-and-crud.test.ts
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import { convertMS2Duration } from 'nocodb-sdk';
 import {
   beforeEachNumberBased,
+  beforeEachSelectBased,
   beforeEach as dataApiV3BeforeEach,
 } from './beforeEach';
 import { ncAxios } from './ncAxios';
@@ -12,7 +13,7 @@ import type { INcAxios } from './ncAxios';
 
 const API_VERSION = 'v3';
 
-describe.only('dataApiV3', () => {
+describe('dataApiV3', () => {
   describe('list-and-crud', () => {
     let testContext: ITestContext;
     let testAxios: INcAxios;
@@ -234,6 +235,193 @@ describe.only('dataApiV3', () => {
           },
         });
 
+        expect(rsp.body.list.slice(0, 4)).to.deep.equal(updatedRecords);
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        // delete record with ID 401 to 404
+        rsp = await testAxios.ncAxiosDelete({
+          url: `${urlPrefix}/${table.id}`,
+          body: updatedRecords.map((record) => ({ Id: record.Id })),
+        });
+        expect(rsp.body).to.deep.equal(
+          updatedRecords.map((record) => ({ Id: record.Id })),
+        );
+      });
+    });
+
+    describe('select-based', () => {
+      let table: Model;
+      let columns: Column[] = [];
+      let insertedRecords: any[];
+
+      beforeEach(async function () {
+        const initResult = await beforeEachSelectBased(testContext);
+        table = initResult.table;
+        columns = initResult.columns;
+        insertedRecords = initResult.insertedRecords;
+      });
+
+      const records: {
+        Id?: number | null;
+        SingleSelect?: string | null;
+        MultiSelect?: string | null;
+      }[] = [
+        {
+          Id: 1,
+          SingleSelect: 'jan',
+          MultiSelect: 'jan,feb,mar',
+        },
+        {
+          Id: 2,
+          SingleSelect: 'feb',
+          MultiSelect: 'apr,may,jun',
+        },
+        {
+          Id: 3,
+          SingleSelect: 'mar',
+          MultiSelect: 'jul,aug,sep',
+        },
+        {
+          Id: 4,
+          SingleSelect: 'apr',
+          MultiSelect: 'oct,nov,dec',
+        },
+        {
+          Id: 5,
+          SingleSelect: 'may',
+          MultiSelect: 'jan,feb,mar',
+        },
+        {
+          Id: 6,
+          SingleSelect: 'jun',
+          MultiSelect: null,
+        },
+        {
+          Id: 7,
+          SingleSelect: 'jul',
+          MultiSelect: 'jan,feb,mar',
+        },
+        {
+          Id: 8,
+          SingleSelect: 'aug',
+          MultiSelect: 'apr,may,jun',
+        },
+        {
+          Id: 9,
+          SingleSelect: 'sep',
+          MultiSelect: 'jul,aug,sep',
+        },
+        {
+          Id: 10,
+          SingleSelect: 'oct',
+          MultiSelect: 'oct,nov,dec',
+        },
+      ];
+
+      const recordsV3: {
+        Id?: number | null;
+        SingleSelect?: string | null;
+        MultiSelect?: string[] | null;
+      }[] = records.map((r) => ({
+        Id: r.Id,
+        SingleSelect: r.SingleSelect,
+        MultiSelect: r.MultiSelect?.split(','),
+      }));
+
+      // TODO: skipped for now, v3 return undefined instead of null
+      // need to investigate first
+      it.skip('Select based- List & CRUD', async function () {
+        // list 10 records
+        let rsp = await testAxios.ncAxiosGet({
+          url: `${urlPrefix}/${table.id}`,
+          query: {
+            limit: 10,
+            fields: 'Id,SingleSelect,MultiSelect',
+          },
+        });
+
+        expect(rsp.body.pageInfo).to.have.property('next');
+        expect(rsp.body.pageInfo.next).to.include(
+          `${urlPrefix}/${table.id}?page=2`,
+        );
+        expect(rsp.body.list).to.deep.equal(recordsV3);
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        // insert 10 records
+        // remove Id's from record array
+        records.forEach((r) => delete r.Id);
+        recordsV3.forEach((r) => delete r.Id);
+
+        rsp = await testAxios.ncAxiosPost({
+          url: `${urlPrefix}/${table.id}`,
+          body: recordsV3,
+        });
+
+        // prepare array with 10 Id's, from 401 to 410
+        const ids: { Id: number }[] = [];
+        for (let i = 401; i <= 410; i++) {
+          ids.push({ Id: i });
+        }
+        expect(rsp.body).to.deep.equal(ids);
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        // read record with Id 401
+        rsp = await testAxios.ncAxiosGet({
+          url: `${urlPrefix}/${table.id}/401`,
+          query: {
+            fields: 'Id,SingleSelect,MultiSelect',
+          },
+        });
+        expect(rsp.body).to.deep.equal({
+          Id: 401,
+          ...recordsV3[0],
+        });
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        // update record with Id 401 to 404
+        const updatedRecord = {
+          SingleSelect: 'jan',
+          MultiSelect: ['jan', 'feb', 'mar'],
+        };
+        const updatedRecords = [
+          {
+            Id: 401,
+            ...updatedRecord,
+          },
+          {
+            Id: 402,
+            ...updatedRecord,
+          },
+          {
+            Id: 403,
+            ...updatedRecord,
+          },
+          {
+            Id: 404,
+            ...updatedRecord,
+          },
+        ];
+        rsp = await testAxios.ncAxiosPatch({
+          url: `${urlPrefix}/${table.id}`,
+          body: updatedRecords,
+        });
+        expect(rsp.body).to.deep.equal(
+          updatedRecords.map((record) => ({ Id: record.Id })),
+        );
+
+        // verify updated records
+        rsp = await testAxios.ncAxiosGet({
+          url: `${urlPrefix}/${table.id}`,
+          query: {
+            limit: 10,
+            offset: 400,
+            fields: 'Id,SingleSelect,MultiSelect',
+          },
+        });
         expect(rsp.body.list.slice(0, 4)).to.deep.equal(updatedRecords);
 
         ///////////////////////////////////////////////////////////////////////////

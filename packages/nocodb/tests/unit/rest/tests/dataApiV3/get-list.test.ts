68c177cb117cc159db42e9ed7b2e0fe7ebcdb86e
Fendy Heryanto <fendy@nocodb.com>
2025-04-01 13:11:41 +0700

    add nested list case

--- packages/nocodb/tests/unit/rest/tests/dataApiV3/get-list.test.ts
+++ packages/nocodb/tests/unit/rest/tests/dataApiV3/get-list.test.ts
@@ -1,4 +1,8 @@
 import { expect } from 'chai';
+import {
+  createLookupColumn,
+  createRollupColumn,
+} from '../../../factory/column';
 import { beforeEach as dataApiV3BeforeEach } from './beforeEach';
 import { ncAxios } from './ncAxios';
 import type { Base, Model } from '../../../../../src/models';
@@ -72,6 +76,97 @@ describe('dataApiV3', () => {
       expect(
         result.list.some((k) => k.Country.toLowerCase().startsWith('ind')),
       ).to.eq(true);
+    });
+
+    it('Nested List - Link to another record', async function () {
+      const expectedRecords = [1, 3, 1, 2, 1, 13, 1, 1, 3, 2];
+      const records = await testAxios.ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          limit: 10,
+        },
+      });
+      expect(records.body.list.length).to.equal(expectedRecords.length);
+
+      const cityList = records.body.list.map((r: any) => r['Cities']);
+      expect(cityList).to.deep.equal(expectedRecords);
+    });
+
+    it('Nested List - Lookup', async function () {
+      await createLookupColumn(testContext.context, {
+        base: testContext.sakilaProject,
+        title: 'Lookup',
+        table: testContext.countryTable,
+        relatedTableName: testContext.cityTable.table_name,
+        relatedTableColumnTitle: 'City',
+      });
+
+      const expectedRecords = [
+        ['Kabul'],
+        ['Batna', 'Bchar', 'Skikda'],
+        ['Tafuna'],
+        ['Benguela', 'Namibe'],
+        ['South Hill'],
+        [
+          'Almirante Brown',
+          'Avellaneda',
+          'Baha Blanca',
+          'Crdoba',
+          'Escobar',
+          'Ezeiza',
+          'La Plata',
+          'Merlo',
+          'Quilmes',
+          'San Miguel de Tucumn',
+          'Santa F',
+          'Tandil',
+          'Vicente Lpez',
+        ],
+        ['Yerevan'],
+        ['Woodridge'],
+        ['Graz', 'Linz', 'Salzburg'],
+        ['Baku', 'Sumqayit'],
+      ];
+
+      // read first 10 records
+      const records = await testAxios.ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          limit: 10,
+        },
+      });
+      expect(records.body.list.length).to.equal(10);
+
+      // extract Lookup column
+      const lookupData = records.body.list.map(
+        (record: any) => record['Lookup'],
+      );
+      expect(lookupData).to.deep.equal(expectedRecords);
+    });
+
+    it('Nested List - Rollup', async function () {
+      await createRollupColumn(testContext.context, {
+        base: testContext.sakilaProject,
+        title: 'Rollup',
+        table: testContext.countryTable,
+        relatedTableName: testContext.cityTable.table_name,
+        relatedTableColumnTitle: 'City',
+        rollupFunction: 'count',
+      });
+
+      const expectedRecords = [1, 3, 1, 2, 1, 13, 1, 1, 3, 2];
+      const records = await testAxios.ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          limit: 10,
+        },
+      });
+      expect(records.body.list.length).to.equal(expectedRecords.length);
+      const rollupData = records.body.list.map(
+        (record: any) => record['Rollup'],
+      );
+
+      expect(rollupData).to.deep.equal(expectedRecords);
     });
   });
 });

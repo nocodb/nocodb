e2044225123fb42d13dfaad530046a50b39a6b63
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-04-03 17:07:12 +0530

    fix(nc-gui): remove finish screen and add banner

--- packages/nc-gui/ee/components/payment/BillingPage.vue
+++ packages/nc-gui/ee/components/payment/BillingPage.vue
@@ -11,6 +11,8 @@ const { workspaceId } = toRefs(props)
 
 const route = useRoute()
 
+const router = useRouter()
+
 const workspaceStore = useWorkspace()
 
 const { workspacesList, activeWorkspace: _activeWorkspace } = storeToRefs(workspaceStore)
@@ -43,15 +45,15 @@ const getPaymentIntent = async () => {
   checkoutSession.value = session
 }
 
-const onBack = () => {
+const onClosePaymentBanner = () => {
   afterPayment.value = false
   afterPaymentState.value = null
   checkoutSession.value = null
 
   if (workspaceId.value) {
-    navigateTo(`/account/workspace/${activeWorkspace.value.id}/settings`)
+    router.replace({ query: {} })
   } else {
-    // Todo:
+    router.replace({ query: { tab: route?.query?.tab || 'billing' } })
   }
 }
 
@@ -110,149 +112,52 @@ const handleScroll = (e) => {
     @scroll.passive="handleScroll"
   >
     <div
-      class="p-6 pb-16 flex flex-col gap-6 min-w-[740px]"
+      class="p-6 pb-16 flex flex-col gap-8 min-w-[740px] w-full"
       :class="{
-        'nc-content-max-w': paymentState !== PaymentState.PAYMENT,
+        'max-w-250 mx-auto': paymentState !== PaymentState.PAYMENT,
       }"
     >
-      <div v-if="afterPayment">
-        <div class="flex flex-col gap-6 mx-auto items-center justify-center w-[600px]">
-          <template v-if="checkoutSession && checkoutSession.payment_status === 'paid'">
-            <div class="border-1 border-nc-border-gray-medium rounded-xl p-4 flex gap-4 bg-nc-bg-green-light w-full">
-              <GeneralIcon icon="checkFill" class="text-white h-6 w-6" />
-              <div class="flex flex-col gap-1">
-                <div class="text-base font-bold text-nc-content-gray">Payment Successful</div>
-                <div class="text-nc-content-gray-muted">Your payment has been processed. You can now use your new plan.</div>
-              </div>
-            </div>
-
-            <div class="border-1 border-nc-border-gray-medium rounded-xl p-6 flex w-full">
-              <div class="flex flex-col gap-8 w-full">
-                <div>
-                  <div class="text-base font-bold text-nc-content-gray-emphasis">Invoice</div>
-
-                  <div class="flex gap-2 mt-2">
-                    <div class="text-sm flex-1">
-                      {{ checkoutSession?.customer_details?.address?.line1 }},
-                      {{ checkoutSession?.customer_details?.address?.city }},
-                      {{ checkoutSession?.customer_details?.address?.state }},
-                      {{ checkoutSession?.customer_details?.address?.postal_code }},
-                      {{ checkoutSession?.customer_details?.address?.country }}
-                    </div>
-
-                    <div
-                      v-if="ncIsArray(checkoutSession?.customer_details?.tax_ids)"
-                      class="text-sm text-nc-content-gray-subtle2"
-                    >
-                      {{ checkoutSession.customer_details.tax_ids.join(', ') }}
-                    </div>
-                  </div>
-                </div>
-
-                <div class="flex flex-col gap-4">
-                  <div class="flex flex-col">
-                    <div class="flex justify-between">
-                      <div class="flex flex-col gap-1">
-                        <div class="text-sm text-nc-content-gray font-semibold">
-                          {{
-                            activeWorkspace?.payment?.plan?.title
-                              ? $t(`objects.paymentPlan.${activeWorkspace?.payment?.plan?.title}`)
-                              : ''
-                          }}
-                          {{ $t('general.plan') }}
-                        </div>
-                        <div
-                          v-if="activeWorkspace?.payment?.subscription?.period"
-                          class="text-xs text-nc-content-gray-muted leading-[18px]"
-                        >
-                          {{ $t('general.paid') }}
-                          {{ $t(`general.${activeWorkspace?.payment?.subscription?.period === 'year' ? 'yearly' : 'monthly'}`) }}
-                        </div>
-                      </div>
-                      <div class="text-sm text-nc-content-gray">${{ (checkoutSession?.amount_total ?? 0) / 100 }}</div>
-                    </div>
-                  </div>
-
-                  <!-- Todo: annual_saving is missing in session  -->
-                  <div
-                    v-if="checkoutSession?.annual_saving"
-                    class="flex justify-between text-nc-content-gray text-sm font-semibold"
-                  >
-                    <div>{{ $t('labels.annualSavings') }}</div>
-                    <div class="px-3">
-                      <NcBadge :border="false" color="green" class="!text-nc-content-green-dark">
-                        -${{ checkoutSession?.applicable_tax ?? 0 }}
-                      </NcBadge>
-                    </div>
-                  </div>
-
-                  <NcDivider class="!my-0" />
-
-                  <div
-                    v-if="checkoutSession?.total_details?.amount_tax"
-                    class="flex justify-between text-nc-content-gray text-sm"
-                  >
-                    <div>{{ $t('labels.applicableTax') }}</div>
-                    <div>${{ checkoutSession?.total_details?.amount_tax ?? 0 }}</div>
-                  </div>
-                  <div class="flex justify-between text-nc-content-gray font-weight-700 text-base">
-                    <div>{{ $t('general.total') }}</div>
-                    <div>${{ (checkoutSession?.amount_total ?? 0) / 100 }}</div>
-                  </div>
-                </div>
-
-                <div class="w-full flex flex-col gap-3">
-                  <!-- Actions -->
-                  <NcButton type="primary" size="medium" class="w-full" @click="onBack">
-                    {{ $t('general.finish') }}
-                  </NcButton>
-
-                  <NcButton type="text" size="medium" class="w-full" inner-class="!gap-2" @click="downloadInvoice">
-                    <template #icon>
-                      <GeneralIcon icon="ncDownload" />
-                    </template>
-                    {{ $t('labels.downloadInvoice') }}
-                  </NcButton>
-                </div>
-              </div>
-            </div>
-          </template>
-
-          <template v-else-if="checkoutSession && checkoutSession.payment_status === 'failed'">
-            <div class="border-1 border-nc-border-gray-medium rounded-xl p-4 flex gap-4 bg-nc-bg-red-light">
-              <GeneralIcon icon="ncAlertCircleFilled" class="text-nc-content-red-dark h-6 w-6" />
-              <div class="flex flex-col gap-1">
-                <div class="text-xl font-bold text-nc-content-red-dark">Payment Failed</div>
-                <div class="text-nc-content-gray-muted">
-                  Something went wrong while processing your payment. Please try again or contact support.
-                </div>
-              </div>
-            </div>
-
-            <NcButton type="ghost" class="w-full" @click="onBack">
-              <div class="flex items-center justify-center gap-1">
-                <span>Go Back & Retry</span>
-              </div>
-            </NcButton>
-          </template>
-
-          <!-- Loader while fetching session -->
-          <div v-else class="min-h-[256px] grid place-items-center">
+      <template v-if="!paymentInitiated">
+        <template v-if="afterPayment">
+          <NcAlert
+            v-if="checkoutSession && checkoutSession.payment_status === 'paid'"
+            :visible="afterPayment"
+            closable
+            type="success"
+            show-icon
+            class="!rounded-xl bg-nc-bg-green-light"
+            :message="$t('Payment Successful')"
+            :description="$t('Your payment has been processed. You can now use your new plan.')"
+            @close="onClosePaymentBanner"
+          >
+            <template #action>
+              <NcButton size="xsmall" class="!px-2" inner-class="!gap-2" @click="downloadInvoice">
+                {{ $t('labels.downloadInvoice') }}
+              </NcButton>
+            </template>
+          </NcAlert>
+          <NcAlert
+            v-else-if="checkoutSession && checkoutSession.payment_status === 'failed'"
+            :visible="afterPayment"
+            closable
+            type="error"
+            show-icon
+            class="!rounded-xl bg-nc-bg-red-light"
+            :message="$t('Payment Failed')"
+            :description="$t('Something went wrong while processing your payment. Please try again or contact support.')"
+            @close="onClosePaymentBanner"
+          >
+          </NcAlert>
+          <div v-else class="min-h-[82px] grid place-items-center">
             <GeneralLoader size="xlarge" />
           </div>
-        </div>
-      </div>
-      <div
-        v-else
-        class="flex flex-col gap-8 w-full"
-        :class="{
-          'max-w-250 mx-auto': !(paymentState && paymentState === PaymentState.PAYMENT),
-        }"
-      >
-        <PaymentPlanUsage v-if="!paymentInitiated" />
-        <Payment v-if="paymentState" />
-        <GeneralLoader v-else />
-      </div>
+        </template>
+
+        <PaymentPlanUsage />
+      </template>
+
+      <Payment v-if="paymentState" />
+      <GeneralLoader v-else size="xlarge" />
     </div>
   </div>
 </template>

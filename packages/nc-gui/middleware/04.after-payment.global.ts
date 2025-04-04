121e967ec80ae69b1037dba6379a43c426605b44
mertmit <mertmit99@gmail.com>
2025-04-04 15:04:25 +0000

    feat: manage subscription

--- packages/nc-gui/middleware/04.after-payment.global.ts
+++ packages/nc-gui/middleware/04.after-payment.global.ts
@@ -2,8 +2,9 @@ export default defineNuxtRouteMiddleware(() => {
   // Get the query params from the URL
   const params = new URLSearchParams(window.location.search)
   const afterPayment = params.get('afterPayment')
+  const afterManage = params.get('afterManage')
 
-  if (afterPayment) {
+  if (afterPayment || afterManage) {
     const workspaceId = params.get('workspaceId')
 
     const url = `/#/account/workspace/${workspaceId}/settings?${params.toString()}`

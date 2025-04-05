44b2bae4d0f7274f21e82af32f19622dc3360544
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-04-05 20:26:19 +0530

    fix(nc-gui): sidebar banner on record limit reach

---
+++ packages/nc-gui/ee/components/payment/upgrade/SidebarBanner.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+const { isRecordLimitReached, gracePeriodDaysLeft, navigateToBilling } = useEeConfig()
+</script>
+
+<template>
+  <div v-if="isRecordLimitReached" class="-mx-1 px-1 pb-1 border-b border-nc-border-gray-medium">
+    <NcAlert type="warning" show-icon class="nc-upgrade-sidebar-banner" @click="navigateToBilling()">
+      <template #icon>
+        <GeneralIcon icon="alertTriangleSolid" class="text-nc-content-red-dark h-5 w-5" />
+      </template>
+      <template #message>
+        <div class="flex items-center justify-between gap-3">
+          <div class="text-sm">{{ $t('upgrade.planLimitReached') }}</div>
+          <div class="text-xs text-nc-content-gray-muted font-500">
+            {{ gracePeriodDaysLeft }} {{ gracePeriodDaysLeft === 1 ? $t('objects.day') : $t('objects.days') }}
+            {{ $t('general.left') }}
+          </div>
+        </div>
+      </template>
+      <template #description>
+        <div class="text-small leading-[18px]">{{ $t('upgrade.planLimitReachedSubtitle') }}</div>
+      </template>
+    </NcAlert>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.nc-upgrade-sidebar-banner {
+  @apply bg-white !p-3 cursor-pointer !gap-2;
+
+  box-shadow: 0px -4px 16px 0px rgba(0, 0, 0, 0.08);
+}
+</style>

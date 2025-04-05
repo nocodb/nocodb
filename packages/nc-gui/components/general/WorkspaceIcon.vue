a1f09b7ab80800c6d5469254d65f50fc0f15773b
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-04-05 22:26:03 +0000

    fix(nc-gui): sidebar font weight issue

--- packages/nc-gui/components/general/WorkspaceIcon.vue
+++ packages/nc-gui/components/general/WorkspaceIcon.vue
@@ -146,7 +146,7 @@ const size = computed(() => props.size || 'medium')
           'text-white': isColorDark(workspaceColor),
           'text-black': !isColorDark(workspaceColor),
           'text-[8px]': size === 'small',
-          'text-[10px]': size === 'account-sidebar',
+          'text-sm': size === 'account-sidebar',
         }"
       >
         {{ workspace?.title?.slice(0, size === 'account-sidebar' ? 1 : 2) }}

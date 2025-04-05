19873d4de92d9692e172613eac118d28d22e9024
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-04-05 22:26:03 +0000

    chore(nc-gui): lint

--- packages/nc-gui/nuxt.config.ts
+++ packages/nc-gui/nuxt.config.ts
@@ -288,7 +288,7 @@ export default defineNuxtConfig({
         'lru-cache',
         'qrcode',
         '@floating-ui/vue',
-        '@stripe/stripe-js'
+        '@stripe/stripe-js',
       ],
       esbuildOptions: {
         define: {

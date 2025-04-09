23f6b7437e124ece4815eb53e3412dd9f89600e8
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-04-09 17:11:24 +0530

    fix(nc-gui): sidebar banner ui changes

--- packages/nc-gui/ee/components/payment/upgrade/SidebarBanner.vue
+++ packages/nc-gui/ee/components/payment/upgrade/SidebarBanner.vue
@@ -3,7 +3,7 @@ const { isRecordLimitReached, gracePeriodDaysLeft, navigateToBilling } = useEeCo
 </script>
 
 <template>
-  <div v-if="isRecordLimitReached" class="-mx-1 px-2 pb-2 pt-1 border-b border-nc-border-gray-medium">
+  <div v-if="isRecordLimitReached" class="-mx-1 px-2 pb-2 pt-1.5 border-b border-nc-border-gray-medium pointer-events-none">
     <NcAlert type="warning" align="center" show-icon class="nc-upgrade-sidebar-banner" @click="navigateToBilling()">
       <template #icon>
         <GeneralIcon icon="alertTriangleSolid" class="text-nc-content-red-dark h-5 w-5" />
@@ -26,8 +26,8 @@ const { isRecordLimitReached, gracePeriodDaysLeft, navigateToBilling } = useEeCo
 
 <style lang="scss" scoped>
 .nc-upgrade-sidebar-banner {
-  @apply bg-white !p-3 cursor-pointer !gap-3;
+  @apply bg-white !p-3 cursor-pointer !gap-3 pointer-events-auto;
 
-  box-shadow: 0px -4px 16px 0px rgba(0, 0, 0, 0.08);
+  box-shadow: 0px -2px 12px 0px rgba(0, 0, 0, 0.08);
 }
 </style>

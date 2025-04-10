1382e6222f191ddca13449f23087947aa818ef48
Fendy Heryanto <fendy@nocodb.com>
2025-04-10 19:09:02 +0700

    add UsingSingleSelectPanel

---
+++ packages/nc-gui/pages/playground/components/row-color-picker-toolbar/using-single-select-panel/index.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const vModel = ref({
+  isSetAsBackground: false,
+  selectedColumnId: '',
+})
+const removeClicked = ref(0)
+const columns = [
+  { id: 'col_1', title: 'First Column' },
+  { id: 'col_2', title: 'Second Column' },
+]
+</script>
+
+<template>
+  <div class="bg-gray-100">
+    <a-card>
+      <h4>Simple</h4>
+      <div class="flex gap-2">
+        <div>
+          vModel:
+          {{ vModel }}
+        </div>
+        <div>Remove clicked: {{ removeClicked }}</div>
+      </div>
+    </a-card>
+    <div class="m-4">
+      <SmartsheetToolbarRowColorFilterUsingSingleSelectPanel v-model="vModel" :columns="columns" @remove="removeClicked++" />
+    </div>
+  </div>
+</template>

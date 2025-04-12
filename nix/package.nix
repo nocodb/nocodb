19d2bc98db895fbc06d15d0af2d390e6dad7d80e
auto walle <auto@sinanmohd.com>
2025-04-12 10:26:48 +0530

    chore(nix/package/pnpmDeps): bump hash

--- nix/package.nix
+++ nix/package.nix
@@ -110,7 +110,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-utjGiL8EuNpnlG2oGIQ9rJ0qeCvSwLyl49EHK44MVKo=";
+    hash = "sha256-vMUkIyr8okespidJq/jIQLpOByyrFER3M3ojuhIZGNs=";
   };
 
   meta = {

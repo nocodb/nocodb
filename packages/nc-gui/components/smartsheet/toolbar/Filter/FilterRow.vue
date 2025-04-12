07ddb18ce54d94cb2fd07a983f51ee7db97000a7
Fendy Heryanto <fendy@nocodb.com>
2025-04-12 14:47:51 +0700

    add field inaccessible slot

--- packages/nc-gui/components/smartsheet/toolbar/Filter/FilterRow.vue
+++ packages/nc-gui/components/smartsheet/toolbar/Filter/FilterRow.vue
@@ -203,11 +203,11 @@ const onDelete = () => {
     <div class="flex flex-row gap-x-0 w-full nc-filter-wrapper bg-white" v-bind="containerProps">
       <!-- #region logical op -->
       <template v-if="index === 0">
-        <div class="flex items-center !min-w-18 !max-w-18 pl-3 nc-filter-where-label">
+        <div class="flex items-center !min-w-18 !max-w-18 pl-3 nc-filter-where-label" v-bind="logicalOpsProps">
           {{ $t('labels.where') }}
         </div>
       </template>
-      <tempalte v-else>
+      <template v-else>
         <NcSelect
           v-e="['c:filter:logical-op:select', sentryProps]"
           v-bind="logicalOpsProps"
@@ -235,76 +235,43 @@ const onDelete = () => {
             </div>
           </a-select-option>
         </NcSelect>
-      </tempalte>
+      </template>
       <!-- #endregion logical op -->
-      <SmartsheetToolbarFilterFieldListDropdownLite
-        v-model="vModel.fk_column_id"
-        v-bind="columnSelectProps"
-        class="nc-filter-field-select min-w-32 max-h-8"
-        :columns="columns"
-        :disabled="isDisabled"
-        :db-client-type="dbClientType"
-        @click.stop
-        @change="onColumnChange($event)"
-      />
-      <NcSelect
-        v-if="comparisonOps && comparisonOps.length > 0"
-        v-e="['c:filter:comparison-op:select', sentryProps]"
-        v-bind="comparisonOpsProps"
-        :value="vModel.comparison_op"
-        :dropdown-match-select-width="false"
-        class="caption nc-filter-operation-select !min-w-26.75 max-h-8"
-        :placeholder="$t('labels.operation')"
-        density="compact"
-        variant="solo"
-        :disabled="isDisabled"
-        hide-details
-        dropdown-class-name="nc-dropdown-filter-comp-op !max-w-80"
-        @change="onComparisonOpChange($event)"
-      >
-        <template v-for="compOp of comparisonOps" :key="compOp.value">
-          <a-select-option :value="compOp.value">
-            <div class="flex items-center w-full justify-between w-full gap-2">
-              <div class="truncate flex-1">{{ compOp.text }}</div>
-              <component
-                :is="iconMap.check"
-                v-if="vModel.comparison_op === compOp.value"
-                id="nc-selected-item-icon"
-                class="text-primary w-4 h-4"
-              />
-            </div>
-          </a-select-option>
-        </template>
-      </NcSelect>
-      <template v-if="comparisonSubOps && comparisonSubOps.length > 0">
+
+      <slot name="fieldInaccessibleError"></slot>
+      <template v-if="!slotHasChildren('fieldInaccessibleError')">
+        <SmartsheetToolbarFilterFieldListDropdownLite
+          v-model="vModel.fk_column_id"
+          v-bind="columnSelectProps"
+          class="nc-filter-field-select min-w-32 max-h-8"
+          :columns="columns"
+          :disabled="isDisabled"
+          :db-client-type="dbClientType"
+          @click.stop
+          @change="onColumnChange($event)"
+        />
         <NcSelect
-          v-model:value="vModel.comparison_sub_op"
-          v-e="['c:filter:sub-comparison-op:select', sentryProps]"
-          v-bind="comparisonSubOpsProps"
+          v-if="comparisonOps && comparisonOps.length > 0"
+          v-e="['c:filter:comparison-op:select', sentryProps]"
+          v-bind="comparisonOpsProps"
+          :value="vModel.comparison_op"
           :dropdown-match-select-width="false"
-          class="caption nc-filter-sub_operation-select min-w-28"
-          :class="{
-            'flex-grow w-full': !showFilterInput,
-            'max-w-28': showFilterInput,
-          }"
-          :placeholder="$t('labels.operationSub')"
+          class="caption nc-filter-operation-select !min-w-26.75 max-h-8"
+          :placeholder="$t('labels.operation')"
           density="compact"
           variant="solo"
           :disabled="isDisabled"
           hide-details
-          dropdown-class-name="nc-dropdown-filter-comp-sub-op"
-          @change="onComparisonSubOpChange($event)"
+          dropdown-class-name="nc-dropdown-filter-comp-op !max-w-80"
+          @change="onComparisonOpChange($event)"
         >
-          <template v-for="compSubOp of comparisonSubOps" :key="compSubOp.value">
-            <a-select-option :value="compSubOp.value">
-              <div class="flex items-center w-full justify-between w-full gap-2 max-w-40">
-                <NcTooltip show-on-truncate-only class="truncate flex-1">
-                  <template #title>{{ compSubOp.text }}</template>
-                  {{ compSubOp.text }}
-                </NcTooltip>
+          <template v-for="compOp of comparisonOps" :key="compOp.value">
+            <a-select-option :value="compOp.value">
+              <div class="flex items-center w-full justify-between w-full gap-2">
+                <div class="truncate flex-1">{{ compOp.text }}</div>
                 <component
                   :is="iconMap.check"
-                  v-if="vModel.comparison_sub_op === compSubOp.value"
+                  v-if="vModel.comparison_op === compOp.value"
                   id="nc-selected-item-icon"
                   class="text-primary w-4 h-4"
                 />
@@ -312,36 +279,73 @@ const onDelete = () => {
             </a-select-option>
           </template>
         </NcSelect>
+        <template v-if="comparisonSubOps && comparisonSubOps.length > 0">
+          <NcSelect
+            v-model:value="vModel.comparison_sub_op"
+            v-e="['c:filter:sub-comparison-op:select', sentryProps]"
+            v-bind="comparisonSubOpsProps"
+            :dropdown-match-select-width="false"
+            class="caption nc-filter-sub_operation-select min-w-28"
+            :class="{
+              'flex-grow w-full': !showFilterInput,
+              'max-w-28': showFilterInput,
+            }"
+            :placeholder="$t('labels.operationSub')"
+            density="compact"
+            variant="solo"
+            :disabled="isDisabled"
+            hide-details
+            dropdown-class-name="nc-dropdown-filter-comp-sub-op"
+            @change="onComparisonSubOpChange($event)"
+          >
+            <template v-for="compSubOp of comparisonSubOps" :key="compSubOp.value">
+              <a-select-option :value="compSubOp.value">
+                <div class="flex items-center w-full justify-between w-full gap-2 max-w-40">
+                  <NcTooltip show-on-truncate-only class="truncate flex-1">
+                    <template #title>{{ compSubOp.text }}</template>
+                    {{ compSubOp.text }}
+                  </NcTooltip>
+                  <component
+                    :is="iconMap.check"
+                    v-if="vModel.comparison_sub_op === compSubOp.value"
+                    id="nc-selected-item-icon"
+                    class="text-primary w-4 h-4"
+                  />
+                </div>
+              </a-select-option>
+            </template>
+          </NcSelect>
+        </template>
+        <template v-if="showFilterInput">
+          <SmartsheetToolbarFilterInputLite
+            v-if="!slotHasChildren('valueInput')"
+            v-bind="inputValueProps"
+            class="nc-filter-value-select rounded-md min-w-34"
+            :column="column"
+            :filter="vModel"
+            :disabled="isDisabled"
+            :db-client-type="dbClientType"
+            @update-filter-value="(value) => onValueChange(value)"
+            @click.stop
+          />
+          <slot name="valueInput"></slot>
+        </template>
+        <div>
+          <!-- if locked view, do not hide the button -->
+          <NcButton
+            v-if="!vModel.readOnly && !disabled"
+            v-e="['c:filter:delete', sentryProps]"
+            v-bind="deleteButtonProps"
+            type="text"
+            size="small"
+            :disabled="isLockedView"
+            class="nc-filter-item-remove-btn self-center"
+            @click.stop="onDelete()"
+          >
+            <component :is="iconMap.deleteListItem" />
+          </NcButton>
+        </div>
       </template>
-      <template v-if="showFilterInput">
-        <SmartsheetToolbarFilterInputLite
-          v-if="!slotHasChildren('valueInput')"
-          v-bind="inputValueProps"
-          class="nc-filter-value-select rounded-md min-w-34"
-          :column="column"
-          :filter="vModel"
-          :disabled="isDisabled"
-          :db-client-type="dbClientType"
-          @update-filter-value="(value) => onValueChange(value)"
-          @click.stop
-        />
-        <slot name="valueInput"></slot>
-      </template>
-      <div>
-        <!-- if locked view, do not hide the button -->
-        <NcButton
-          v-if="!vModel.readOnly && !disabled"
-          v-e="['c:filter:delete', sentryProps]"
-          v-bind="deleteButtonProps"
-          type="text"
-          size="small"
-          :disabled="isLockedView"
-          class="nc-filter-item-remove-btn self-center"
-          @click.stop="onDelete()"
-        >
-          <component :is="iconMap.deleteListItem" />
-        </NcButton>
-      </div>
     </div>
   </div>
 </template>

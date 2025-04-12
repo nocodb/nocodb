7ab6f9b6f72ec435630108eedd7897f95857c7b8
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-04-12 17:42:08 +0530

    fix(nocodb): add workspace info in shared base

--- packages/nocodb/src/ee/services/public-metas.service.ts
+++ packages/nocodb/src/ee/services/public-metas.service.ts
@@ -1,7 +1,8 @@
 import { PublicMetasService as PublicMetasServiceCE } from 'src/services/public-metas.service';
 import { Injectable } from '@nestjs/common';
 import type { NcContext } from '~/interface/config';
-import { Workspace } from '~/models';
+import { Base, Workspace } from '~/models';
+import { NcError } from '~/helpers/catchError';
 
 @Injectable()
 export class PublicMetasService extends PublicMetasServiceCE {
@@ -18,5 +19,23 @@ export class PublicMetasService extends PublicMetasServiceCE {
     });
 
     return view;
+  }
+
+  async publicSharedBaseGet(
+    context: NcContext,
+    param: { sharedBaseUuid: string },
+  ): Promise<any> {
+    const base = await Base.getByUuid(context, param.sharedBaseUuid);
+
+    if (!base) {
+      NcError.baseNotFound(param.sharedBaseUuid);
+    }
+
+    const workspace = await Workspace.get(base.fk_workspace_id, false);
+
+    return {
+      base_id: base.id,
+      workspace,
+    };
   }
 }

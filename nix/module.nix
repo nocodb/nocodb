4508c52c4cfa41358f31dc5c630b84a17073e710
sinanmohd <sinan@sinanmohd.com>
2025-04-15 17:00:12 +0530

    chore(nix/module): sync with nixpkgs

--- nix/module.nix
+++ nix/module.nix
@@ -12,6 +12,7 @@ let
 
   defaultEnvs = {
     DATABASE_URL = "sqlite:///%S/nocodb/sqlite.db";
+    PORT = builtins.toString cfg.port;
   };
 in
 {
@@ -25,14 +26,26 @@ in
       default = inputs.self.packages.${system}.nocodb;
     };
 
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 8080;
+      description = "The port nocodb should be reachable from.";
+    };
     environment = lib.mkOption {
       default = { };
       type = lib.types.attrsOf lib.types.str;
+      description = ''
+        Environment variables as defined in https://docs.nocodb.com/getting-started/self-hosted/environment-variables/.
+      '';
     };
     environmentFile = lib.mkOption {
       type = lib.types.nullOr lib.types.path;
       example = "/var/lib/nocodb/secrets";
       default = null;
+      description = ''
+        Environment variables as defined in https://docs.nocodb.com/getting-started/self-hosted/environment-variables/.
+        Secrets may be passed to the service without adding them to the world-readable Nix store using this option.
+      '';
     };
   };
 
@@ -40,7 +53,7 @@ in
     environment.systemPackages = [ cfg.package ];
 
     systemd.services.nocodb = {
-      description = "Open Source Airtable Alternative";
+      description = "NocoDB allows building no-code database solutions with ease of spreadsheets";
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
       wants = [ "network-online.target" ];

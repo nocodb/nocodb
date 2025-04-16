0614b14037ce470818f50753138d82a7e3979880
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-04-16 16:55:12 +0530

    fix(nc-gui): remove redundant css @apply directive

--- packages/nc-gui/components/smartsheet/grid/InfiniteTable.vue
+++ packages/nc-gui/components/smartsheet/grid/InfiniteTable.vue
@@ -3602,12 +3602,10 @@ const cellFilteredOrSortedClass = (colId: string) => {
         @apply !bg-gray-50 border-b-gray-200 border-r-gray-200;
       }
       td.nc-grid-cell.column-filtered:not(.active) {
-        @apply !bg-green-100;
-        @apply !border-b-gray-200;
+        @apply !bg-green-100 !border-b-gray-200;
       }
       td.nc-grid-cell.column-sorted:not(.active) {
-        @apply !bg-orange-100;
-        @apply !border-b-gray-300;
+        @apply !bg-orange-100 !border-b-gray-300;
       }
     }
   }
@@ -3618,12 +3616,10 @@ const cellFilteredOrSortedClass = (colId: string) => {
       @apply !bg-[#F0F3FF] border-b-gray-200 border-r-gray-200;
     }
     td.nc-grid-cell.column-filtered:not(.active) {
-      @apply !bg-green-100;
-      @apply !border-b-gray-200;
+      @apply !bg-green-100 !border-b-gray-200;
     }
     td.nc-grid-cell.column-sorted:not(.active) {
-      @apply !bg-orange-100;
-      @apply !border-b-gray-300;
+      @apply !bg-orange-100 !border-b-gray-300;
     }
   }
 

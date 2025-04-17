b3ff84d32e58dde6d9f9689ee035ab63c6c3f84c
mertmit <mertmit99@gmail.com>
2025-04-17 23:12:53 +0300

    fix: small issues
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/middlewares/url-encode.middleware.ts
+++ packages/nocodb/src/middlewares/url-encode.middleware.ts
@@ -7,6 +7,6 @@ export class UrlEncodeMiddleware implements NestMiddleware {
   constructor() {}
 
   use(req: any, res: any, next: () => void) {
-    express.urlencoded()(req, res, next);
+    express.urlencoded({ extended: true })(req, res, next);
   }
 }

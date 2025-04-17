8990ad64ee393dc2c75fe2c8919835d537ebf2ad
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-04-17 20:12:59 +0530

    fix(nocodb): restrict form validation feature if plan is expired or downgraded

--- packages/nocodb/src/ee/services/public-metas.service.ts
+++ packages/nocodb/src/ee/services/public-metas.service.ts
@@ -1,9 +1,21 @@
 import { PublicMetasService as PublicMetasServiceCE } from 'src/services/public-metas.service';
 import { Injectable } from '@nestjs/common';
 import type { NcContext } from '~/interface/config';
-import { Base, FormView, Workspace } from '~/models';
+import { Base, FormView, FormViewColumn, Workspace } from '~/models';
 import { NcError } from '~/helpers/catchError';
-import { parseProp, PlanFeatureTypes, ViewTypes } from 'nocodb-sdk';
+import {
+  AttachmentValidationType,
+  DateValidationType,
+  ncIsArray,
+  NumberValidationType,
+  parseProp,
+  PlanFeatureTypes,
+  SelectValidationType,
+  StringValidationType,
+  TimeValidationType,
+  ViewTypes,
+  YearValidationType,
+} from 'nocodb-sdk';
 import { getFeature } from '~/helpers/paymentHelpers';
 
 @Injectable()
@@ -77,8 +89,68 @@ export class PublicMetasService extends PublicMetasServiceCE {
 
     const formView = view.view as FormView;
 
+    const formColumns = (view.columns as FormViewColumn[]).map((c) => {
+      if (!ncIsArray((c.meta as Record<string, any>)?.validators)) return c;
+
+      const validators = (c.meta as Record<string, any>)?.validators.filter(
+        (val) => {
+          // Restrict number validation type
+          if (Object.values(NumberValidationType).includes(val.type)) {
+            return false;
+          }
+
+          // Restrict date, time, year ranage validation
+          if (
+            Object.values(DateValidationType).includes(val.type) ||
+            Object.values(TimeValidationType).includes(val.type) ||
+            Object.values(YearValidationType).includes(val.type)
+          ) {
+            return false;
+          }
+
+          // Restrict attachment valiation
+          if (Object.values(AttachmentValidationType).includes(val.type)) {
+            return false;
+          }
+
+          // From select validation restrict only range validation, limit options is by default enabled
+          if (
+            [
+              SelectValidationType.MinSelected,
+              SelectValidationType.MaxSelected,
+            ].includes(val.type)
+          ) {
+            return false;
+          }
+
+          // From string validation restrict only custom validation (email, url, phonenumber is default column validation)
+          if (
+            Object.values(StringValidationType)
+              .filter(
+                (v) =>
+                  ![
+                    StringValidationType.Email,
+                    StringValidationType.Url,
+                    StringValidationType.PhoneNumber,
+                  ].includes(v),
+              )
+              .includes(val.type)
+          ) {
+            return false;
+          }
+
+          return true;
+        },
+      );
+
+      c.meta = { ...(c.meta as Record<string, any>), validators };
+
+      return c;
+    });
+
     Object.assign(view, {
       ...view,
+      columns: formColumns,
       filter: isFormFieldOnConditionEnabled ? view.filter : {},
       view: {
         ...formView,

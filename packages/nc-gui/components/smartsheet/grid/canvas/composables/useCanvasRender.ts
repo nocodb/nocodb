aa37f49c844c8c7d5ecfd4863d496f4aac7ab71f
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-04-17 18:34:50 +0000

    fix: adjust min field width for display value

--- packages/nc-gui/components/smartsheet/grid/canvas/composables/useCanvasRender.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/composables/useCanvasRender.ts
@@ -2717,7 +2717,7 @@ export function useCanvasRender({
 
         ctx.restore()
 
-        renderGroupContent(ctx, group, contentX, contentY + 22, availableWidth - contentWidth - 20 - countWidth, i)
+        renderGroupContent(ctx, group, contentX, contentY + 22, availableWidth - contentWidth - countWidth, i)
 
         currentOffset = tempCurrentOffset
       }

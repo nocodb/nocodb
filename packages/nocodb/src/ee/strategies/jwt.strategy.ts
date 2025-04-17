774be46d6a5f290e3885735ff8dc6b1df5ad96da
Pranav C <pranavxc@gmail.com>
2025-04-17 14:29:48 +0530

    fix: include workspace id in extra

--- packages/nocodb/src/ee/strategies/jwt.strategy.ts
+++ packages/nocodb/src/ee/strategies/jwt.strategy.ts
@@ -44,6 +44,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
       extra: {
         org_id: jwtPayload.org_id,
+        workspace_d: jwtPayload.workspace_d,
         sso_client_id: jwtPayload.sso_client_id,
         sso_client_type: jwtPayload.sso_client_type,
       },

43cf33ae2ccabfc0b8e370ca28ceef8c3c59bd0b
mertmit <mertmit99@gmail.com>
2025-04-17 16:19:25 +0300

    fix: plans part
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/ee/components/payment/index.vue
+++ packages/nc-gui/ee/components/payment/index.vue
@@ -9,6 +9,7 @@ onMounted(async () => {
 <template>
   <div>
     <div v-if="paymentState === PaymentState.SELECT_PLAN" class="flex flex-col gap-8">
+      <PaymentPlans />
       <div class="w-[70%] max-w-[640px] mx-auto">
         <NcDivider class="!my-0" />
       </div>

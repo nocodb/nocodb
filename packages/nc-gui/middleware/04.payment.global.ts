7766d9d5c81dce1e2d605d4fb94f4a2a468bfa8f
mertmit <mertmit99@gmail.com>
2025-04-17 14:16:22 +0000

    feat: upgrade page

--- packages/nc-gui/middleware/04.payment.global.ts
+++ packages/nc-gui/middleware/04.payment.global.ts
@@ -1,8 +1,17 @@
 export default defineNuxtRouteMiddleware(() => {
-  // Get the query params from the URL
   const params = new URLSearchParams(window.location.search)
   const afterPayment = params.get('afterPayment')
   const afterManage = params.get('afterManage')
+
+  const upgrade = params.get('upgrade')
+
+  if (upgrade) {
+    const url = `/#/upgrade?${params.toString()}`
+
+    window.location.href = url
+
+    return
+  }
 
   if (afterPayment || afterManage) {
     const workspaceId = params.get('workspaceId')

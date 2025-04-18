b1002b410323fa52db5bdad8dd48cd12cd01c965
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-04-18 11:56:21 +0530

    fix(nc-gui): wireup navigate to workspace

--- packages/nc-gui/components/dashboard/topbar/index.vue
+++ packages/nc-gui/components/dashboard/topbar/index.vue
@@ -1,8 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const props = defineProps<{
+  workspaceId?: string
+}>()
+
+const navigateToWorkspace = () => {
+  if (props.workspaceId) {
+    navigateTo(`/${props.workspaceId}/settings`)
+  }
+}
+</script>
 
 <template>
   <div class="flex items-center justify-between w-full border-b-1 border-nc-gray-medium py-2 px-4 h-[56px]">
-    <img class="cursor-pointer" width="32" alt="NocoDB" src="~/assets/img/icons/512x512.png" />
+    <img class="cursor-pointer" width="32" alt="NocoDB" src="~/assets/img/icons/512x512.png" @click.stop="navigateToWorkspace" />
     <DashboardTopbarUserInfo />
   </div>
 </template>

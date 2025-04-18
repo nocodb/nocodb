2576d7e2be7546e12307b2afb4f8479b491aa985
Pranav C <pranavxc@gmail.com>
2025-04-18 18:11:18 +0530

    test: enable sso test

--- tests/playwright/tests/ee/ssoAuth.spec.ts
+++ tests/playwright/tests/ee/ssoAuth.spec.ts
@@ -12,8 +12,8 @@ import { CloudSAMLLoginPage } from '../../pages/OrgAdmin/SAMLLoginPage';
 const getDomain = () => `test-${Math.random().toString(36).substring(7)}.nocodb.com`;
 
 // TODO: ENABLE - FAILS ON CI ONLY
-test.describe.serial('SSO', () => {
-  test.skip();
+test.describe.serial.only('SSO', () => {
+  // test.skip();
 
   test.describe.serial('CRUD', () => {
     let accountsPage: AccountPage;

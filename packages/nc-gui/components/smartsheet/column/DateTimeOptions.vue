c83b9e7b39bd0b0dca4fcd0fb615f19404bb22b7
Amit Joki <amitjoki@gmail.com>
2025-04-22 14:15:55 +0000

    feat: track older aliases and toggleable all member timezone with default values and priority timezones

--- packages/nc-gui/components/smartsheet/column/DateTimeOptions.vue
+++ packages/nc-gui/components/smartsheet/column/DateTimeOptions.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ColumnHelper, UITypes, dateFormats, timeFormats } from 'nocodb-sdk'
-import { getTimeZones } from '@vvo/tzdb'
+import { type TimeZone, getTimeZones } from '@vvo/tzdb'
 
 const props = defineProps<{
   value: any
@@ -10,25 +10,23 @@ const emit = defineEmits(['update:value'])
 
 const vModel = useVModel(props, 'value', emit)
 
-const timezoneList = computed(() => {
-  const timezones = getTimeZones({ includeUtc: true })
-  const browserTzName = Intl.DateTimeFormat().resolvedOptions().timeZone
-  const browserTz = timezones.find((tz) => tz.name === browserTzName)
-  const utcTz = timezones.find((tz) => tz.name === 'Etc/UTC')
-  const priorityTz = [utcTz, browserTz].filter((k) => k)
-  const restOfTz = []
-  for (const tz of timezones) {
-    if (['Etc/UTC', browserTzName].includes(tz.name)) {
-      continue
-    }
-    if (browserTz?.countryCode === tz.countryCode) {
-      priorityTz.push(tz)
-      continue
-    }
-    restOfTz.push(tz)
-  }
+const timezones = getTimeZones({ includeUtc: true }).sort((a, b) => a.name.localeCompare(b.name))
+const browserTzName = Intl.DateTimeFormat().resolvedOptions().timeZone
+const browserTz = timezones.find((tz) => isSameTimezone(tz.name, browserTzName))
+const utcTz = timezones.find((tz) => tz.name === 'Etc/UTC')
+const defaultSuggestedTzs = [browserTz, utcTz].filter((k) => k) as TimeZone[]
 
-  return [...priorityTz, ...restOfTz.sort((a, b) => a.name.localeCompare(b.name))]
+const priorityTzs = computed(() => {
+  const otherPriorityTzs = []
+  for (const tz of timezones) {
+    if (
+      browserTz?.countryCode === tz.countryCode &&
+      !defaultSuggestedTzs.find((suggestedTz) => isSameTimezone(suggestedTz?.name, tz.name))
+    ) {
+      otherPriorityTzs.push(tz)
+    }
+  }
+  return [...defaultSuggestedTzs, ...otherPriorityTzs]
 })
 
 // set default value
@@ -45,6 +43,20 @@ const isDisplayTimezone = computed({
     vModel.value.meta.isDisplayTimezone = value
   },
 })
+
+const useSameTimezoneForAll = computed({
+  get: () => !!vModel.value.meta?.useSameTimezoneForAll,
+  set: (value) => {
+    if (!vModel.value.meta) vModel.value.meta = {}
+    vModel.value.meta.useSameTimezoneForAll = value
+    if (!value) vModel.value.meta.timezone = undefined
+    else if (!vModel.value.meta.timezone) {
+      vModel.value.meta.timezone = priorityTzs.value[0]?.name
+    }
+  },
+})
+
+watch(vModel, (a) => console.log(a.meta), { deep: true })
 </script>
 
 <template>
@@ -107,37 +119,6 @@ const isDisplayTimezone = computed({
     </a-form-item>
 
     <template v-if="isEeUI">
-      <a-form-item :label="$t('title.selectTimezone')">
-        <a-select
-          v-model:value="vModel.meta.timezone"
-          show-search
-          allow-clear
-          dropdown-class-name="nc-dropdown-date-format"
-          placeholder="Use same timezone for all collaborator"
-          :disabled="isSystem"
-        >
-          <template #suffixIcon>
-            <GeneralIcon icon="arrowDown" class="text-gray-700" />
-          </template>
-
-          <a-select-option v-for="(timezone, i) of timezoneList" :key="i" :value="timezone.name">
-            <div class="flex gap-2 w-full justify-between items-center">
-              {{ timezone.name }}
-              <div>
-                <span class="text-gray-400 mr-2">
-                  {{ timezone.abbreviation }}
-                </span>
-                <component
-                  :is="iconMap.check"
-                  id="nc-selected-item-icon"
-                  class="text-primary w-4 h-4"
-                  :class="{ invisible: vModel.meta.timezone !== timezone.name }"
-                />
-              </div>
-            </div>
-          </a-select-option>
-        </a-select>
-      </a-form-item>
       <a-form-item>
         <NcTooltip :disabled="true">
           <div class="flex items-center gap-1">
@@ -148,6 +129,69 @@ const isDisplayTimezone = computed({
             </NcSwitch>
           </div>
         </NcTooltip>
+      </a-form-item>
+      <a-form-item>
+        <NcTooltip :disabled="true">
+          <div class="flex items-center gap-1">
+            <NcSwitch v-model:checked="useSameTimezoneForAll">
+              <div class="text-sm text-gray-800 select-none font-semibold">
+                {{ $t('labels.useSameTimezoneForAllMembers') }}
+              </div>
+            </NcSwitch>
+          </div>
+        </NcTooltip>
+      </a-form-item>
+      <a-form-item v-if="useSameTimezoneForAll">
+        <a-select
+          v-model:value="vModel.meta.timezone"
+          show-search
+          allow-clear
+          dropdown-class-name="nc-dropdown-timezone"
+          placeholder="Use same timezone for all collaborator"
+          class="nc-search-timezone"
+          :disabled="isSystem"
+        >
+          <template #suffixIcon>
+            <GeneralIcon icon="arrowDown" class="text-gray-700" />
+          </template>
+
+          <a-select-opt-group label="Suggested">
+            <a-select-option v-for="timezone of priorityTzs" :key="timezone.name" :value="timezone.name">
+              <div class="flex gap-2 w-full justify-between items-center">
+                <span class="font-650 text-[13px]">{{ timezone.name }}</span>
+                <div>
+                  <span class="text-nc-content-gray-subtle2 text-[13px] font-500 mr-2">
+                    {{ timezone.abbreviation }}
+                  </span>
+                  <component
+                    :is="iconMap.check"
+                    id="nc-selected-item-icon"
+                    class="text-primary w-4 h-4"
+                    :class="{ invisible: vModel.meta.timezone !== timezone.name }"
+                  />
+                </div>
+              </div>
+            </a-select-option>
+          </a-select-opt-group>
+          <a-select-opt-group label="All">
+            <a-select-option v-for="timezone of timezones" :key="timezone.name" :value="timezone.name">
+              <div class="flex gap-2 w-full justify-between items-center">
+                <span class="font-650 text-[13px]">{{ timezone.name }}</span>
+                <div>
+                  <span class="text-nc-content-gray-subtle2 text-[13px] font-500 mr-2">
+                    {{ timezone.abbreviation }}
+                  </span>
+                  <component
+                    :is="iconMap.check"
+                    id="nc-selected-item-icon"
+                    class="text-primary w-4 h-4"
+                    :class="{ invisible: vModel.meta.timezone !== timezone.name }"
+                  />
+                </div>
+              </div>
+            </a-select-option>
+          </a-select-opt-group>
+        </a-select>
       </a-form-item>
     </template>
   </div>
@@ -163,5 +207,8 @@ const isDisplayTimezone = computed({
       @apply border-brand-500;
     }
   }
+}
+.nc-search-timezone :deep(.ant-select-clear) {
+  margin-top: -8px;
 }
 </style>

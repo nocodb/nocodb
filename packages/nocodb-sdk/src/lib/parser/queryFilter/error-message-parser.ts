fc4b594fb17aee6469a325bca0c268bd73fad3a1
Fendy Heryanto <fendy@nocodb.com>
2025-04-25 08:41:22 +0000

    handle missing closing parentheses

--- packages/nocodb-sdk/src/lib/parser/queryFilter/error-message-parser.ts
+++ packages/nocodb-sdk/src/lib/parser/queryFilter/error-message-parser.ts
@@ -8,15 +8,18 @@ export const parseParsingError = (e: IRecognitionException) => {
   try {
     if (e.message.toLowerCase().startsWith('expecting token of type --> ')) {
       const operatorRegex =
-        /^Expecting token of type --> ([^\s]+) <-- but found --> '?([^\s']+)'? <--.?/i;
+        /^Expecting token of type --> ([^\s]+) <-- but found --> '?([^\s']*)'? <--.?/i;
       const tokens = e.message.match(operatorRegex);
       const [expectation, found] = tokens.slice(1, 3);
       switch (expectation) {
         case 'OPERATOR': {
           return `Invalid filter expression: '${found}' is not a recognized operator. Please use a valid comparison or logical operator.`;
         }
+        case 'PAREN_END': {
+          return `Invalid filter syntax: expected a closing parentheses ')', but found '${found}'.`;
+        }
         default: {
-          return `Invalid filter expression: '${found}' is not a valid token`;
+          return `Invalid filter expression: '${found}' is not a valid token.`;
         }
       }
     } else if (

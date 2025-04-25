e07783fe40dd3b09a7b7dd9d388f5db6482a8730
Fendy Heryanto <fendy@nocodb.com>
2025-04-25 08:41:24 +0000

    fix rebase

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -33,7 +33,6 @@ import {
 import { v4 as uuidv4 } from 'uuid';
 import Validator from 'validator';
 import { addOrRemoveLinks } from './BaseModelSqlv2/add-remove-links';
-import { apiVersion } from 'airtable';
 import { NestedLinkPreparator } from './BaseModelSqlv2/nested-link-preparator';
 import { relationDataFetcher } from './BaseModelSqlv2/relation-data-fetcher';
 import { baseModelInsert } from './BaseModelSqlv2/insert';

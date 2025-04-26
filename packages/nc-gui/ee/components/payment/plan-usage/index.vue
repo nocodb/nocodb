4fb9df096c3de07add5264c8b836a9e35bd74854
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-04-26 16:27:00 +0530

    fix(nc-gui): gray badge background

--- packages/nc-gui/ee/components/payment/plan-usage/index.vue
+++ packages/nc-gui/ee/components/payment/plan-usage/index.vue
@@ -241,7 +241,7 @@ const onUpdateSubscription = async (planId: string, stripePriceId: string) => {
         <NcBadge
           v-if="activeSubscription?.period"
           :border="false"
-          class="text-nc-content-gray-subtle2 !bg-nc-bg-gray-dark text-[10px] leading-[14px] !h-[18px] font-semibold mt-1"
+          class="text-nc-content-gray-subtle2 !bg-nc-bg-gray-medium text-[10px] leading-[14px] !h-[18px] font-semibold mt-1"
         >
           {{ activeSubscription?.period === 'year' ? 'Annual' : 'Monthly' }}
         </NcBadge>

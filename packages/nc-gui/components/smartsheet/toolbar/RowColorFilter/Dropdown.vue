358fe9edb214170d4d417d577f3a7fc34f147c8d
Fendy Heryanto <fendy@nocodb.com>
2025-05-06 10:47:50 +0700

    move to ee-only gui

--- packages/nc-gui/components/smartsheet/toolbar/RowColorFilter/Dropdown.vue
+++ packages/nc-gui/components/smartsheet/toolbar/RowColorFilter/Dropdown.vue
@@ -1,95 +1,33 @@
 <script setup lang="ts">
-import { PlanLimitTypes, UITypes } from 'nocodb-sdk'
-import { SmartsheetToolbarRowColorFilterUsingFilterPanel } from '#components'
-
-const meta = inject(MetaInj, ref())
-const activeView = inject(ActiveViewInj, ref())
-const { getPlanLimit } = useWorkspace()
-
 const { isMobileMode } = useGlobal()
-
-const baseStore = useBase()
-const { getBaseType, baseMeta } = baseStore
-
 const isToolbarIconMode = inject(
   IsToolbarIconMode,
   computed(() => false),
 )
-
-const {
-  rowColorInfo,
-  filterPerViewLimit,
-  onDropdownOpen,
-  onRemoveRowColoringMode,
-  onRowColorSelectChange,
-  onRowColorConditionAdd,
-  onRowColorConditionDelete,
-  onRowColorConditionUpdate,
-  onRowColorConditionFilterAdd,
-  onRowColorConditionFilterUpdate,
-  onRowColorConditionFilterAddGroup,
-  onRowColorConditionFilterDelete,
-  filterColumns,
-} = useViewRowColorOption({
-  meta,
-  view: activeView,
-})
-const rowColoringMode = computed({
-  set: (value) => {
-    rowColorInfo.value.mode = value
-  },
-  get: () => {
-    return rowColorInfo.value.mode
-  },
-})
+const isShowColoringButton = ref(false)
 </script>
 
 <template>
-  <NcDropdown @update:visible="onDropdownOpen">
-    <NcButton type="text" size="small" class="nc-toolbar-btn !border-0 !h-7">
-      <div class="flex items-center gap-1 min-h-5">
-        <div class="flex items-center gap-2">
-          <component :is="iconMap.ncPaintRoller" class="h-4 w-4" />
+  <!-- TODO: show "ONLY IN EE feature" -->
+  <template v-if="isShowColoringButton">
+    <NcDropdown>
+      <NcButton type="text" size="small" class="nc-toolbar-btn !border-0 !h-7">
+        <div class="flex items-center gap-1 min-h-5">
+          <div class="flex items-center gap-2">
+            <component :is="iconMap.ncPaintRoller" class="h-4 w-4" />
 
-          <!-- Group By -->
-          <span v-if="!isMobileMode && !isToolbarIconMode" class="text-capitalize !text-[13px] font-medium">Coloring</span>
+            <!-- Group By -->
+            <span v-if="!isMobileMode && !isToolbarIconMode" class="text-capitalize !text-[13px] font-medium">Coloring</span>
+          </div>
+          <!-- <span v-if="groupedByColumnIds?.length" class="bg-brand-50 text-brand-500 nc-toolbar-btn-chip">{{
+            groupedByColumnIds.length
+          }}</span> -->
         </div>
-        <!-- <span v-if="groupedByColumnIds?.length" class="bg-brand-50 text-brand-500 nc-toolbar-btn-chip">{{
-          groupedByColumnIds.length
-        }}</span> -->
-      </div>
-    </NcButton>
-    <template #overlay>
-      <SmartsheetToolbarRowColorFilterTypeOption v-model:row-coloring-mode="rowColoringMode">
-        <template #select>
-          <SmartsheetToolbarRowColorFilterUsingSingleSelectPanel
-            v-model="rowColorInfo"
-            :columns="meta?.columns.filter((k) => k.uidt === UITypes.SingleSelect)"
-            :filter-per-view-limit="filterPerViewLimit"
-            @change="onRowColorSelectChange"
-            @remove="onRemoveRowColoringMode"
-          />
-        </template>
-        <template #filter>
-          <SmartsheetToolbarRowColorFilterUsingFilterPanel
-            v-model="rowColorInfo"
-            :columns="filterColumns"
-            :filter-per-view-limit="filterPerViewLimit"
-            :handler="{
-              conditionAdd: onRowColorConditionAdd,
-              conditionDelete: onRowColorConditionDelete,
-              conditionUpdate: onRowColorConditionUpdate,
-              allConditionDeleted: onRemoveRowColoringMode,
-              filters: {
-                addFilter: onRowColorConditionFilterAdd,
-                addFilterGroup: onRowColorConditionFilterAddGroup,
-                deleteFilter: onRowColorConditionFilterDelete,
-                rowChange: onRowColorConditionFilterUpdate,
-              },
-            }"
-          />
-        </template>
-      </SmartsheetToolbarRowColorFilterTypeOption>
-    </template>
-  </NcDropdown>
+      </NcButton>
+      <template #overlay>
+        <SmartsheetToolbarRowColorFilterTypeOptionOSS />
+      </template>
+    </NcDropdown>
+  </template>
+  <template v-else></template>
 </template>

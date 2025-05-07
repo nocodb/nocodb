48261c0bafafe042ffc8e15e0d18aae700acb913
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-05-07 04:53:39 +0000

    fix: tab colors and design

--- packages/nc-gui/components/smartsheet/toolbar/Calendar/Mode.vue
+++ packages/nc-gui/components/smartsheet/toolbar/Calendar/Mode.vue
@@ -43,16 +43,25 @@ watch(activeCalendarView, () => {
     data-testid="nc-calendar-view-mode"
   >
     <div class="flex flex-row">
-      <div :style="highlightStyle" class="highlight h-0.5 rounded-t-md absolute transition-all -bottom-2.5 bg-brand-500"></div>
+      <div
+        :style="highlightStyle"
+        class="highlight h-0.5 rounded-t-md absolute transition-all -bottom-2.8 bg-nc-content-brand"
+      ></div>
+
       <div
         v-for="mode in ['day', 'week', 'month', 'year']"
         :key="mode"
-        :class="{ '!text-brand-500 !font-bold  bg-transparent active': activeCalendarView === mode }"
         :data-testid="`nc-calendar-view-mode-${mode}`"
-        class="tab flex items-center h-7 w-14 z-10 justify-center px-2 py-1 rounded-lg gap-x-1.5 text-gray-600 hover:text-black cursor-pointer transition-all duration-300 select-none"
+        class="cursor-pointer tab transition-all px-2 duration-300 flex items-center w-12 h-7 z-10 justify-center"
+        :class="{
+          'text-nc-content-brand font-bold  bg-transparent active': activeCalendarView === mode,
+          'text-nc-content-gray-subtle2 font-[500] hover:text-nc-content-gray-extreme ': activeCalendarView !== mode,
+        }"
         @click="setActiveCalendarMode(mode, $event)"
       >
-        <div class="min-w-0 pointer-events-none !text-[13px]]">{{ $t(`objects.${mode}`) }}</div>
+        <div class="min-w-0 pointer-events-none leading-[18px] text-[12px] transition-all duration-300">
+          {{ $t(`objects.${mode}`) }}
+        </div>
       </div>
     </div>
   </div>

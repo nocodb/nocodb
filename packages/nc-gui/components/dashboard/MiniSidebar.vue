b9147bc0b071e9b4ebef6d4c69e877881755cb2f
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-05-08 20:37:19 +0530

    fix(nc-gui): closed sidebar hover area

--- packages/nc-gui/components/dashboard/MiniSidebar.vue
+++ packages/nc-gui/components/dashboard/MiniSidebar.vue
@@ -12,7 +12,7 @@ provide(IsMiniSidebarInj, ref(true))
 
 <template>
   <div
-    class="nc-mini-sidebar w-[var(--mini-sidebar-width)] flex-none bg-nc-bg-gray-light flex flex-col justify-between items-center px-2 py-3 outline-r-1 outline-gray-100 z-3"
+    class="nc-mini-sidebar"
     :style="{
       outlineWidth: '1px',
     }"
@@ -29,29 +29,7 @@ provide(IsMiniSidebarInj, ref(true))
 </template>
 
 <style lang="scss" scoped>
-.nc-sidebar-top-button {
-  @apply flex flex-row mx-1 px-3.5 rounded-md items-center py-0.75 my-0.5 gap-x-2 hover:bg-gray-200 cursor-pointer;
-}
-
-.nc-sidebar-bottom-section {
-  @apply flex-none overflow-auto p-1;
-
-  &:not(:has(.nc-upgrade-sidebar-banner)) {
-    @apply border-t-1;
-  }
-  &:has(.nc-upgrade-sidebar-banner) {
-    @apply -mt-2.5 pointer-events-none;
-  }
-
-  & > * {
-    @apply my-0.5 pointer-events-auto;
-  }
-
-  & > :first-child {
-    @apply mt-0;
-  }
-  & > :last-child {
-    @apply mb-0;
-  }
+.nc-mini-sidebar {
+  @apply w-[var(--mini-sidebar-width)] flex-none bg-nc-bg-gray-light flex flex-col justify-between items-center px-2 py-3 border-r-1 border-nc-border-gray-medium z-12;
 }
 </style>

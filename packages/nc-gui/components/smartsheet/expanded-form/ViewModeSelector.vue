68ad70b79905ad3ae89d6087687210ed29137db1
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-05-09 10:40:22 +0000

    fix(nc-gui): update expanded form view mode tabs

---
+++ packages/nc-gui/components/smartsheet/expanded-form/ViewModeSelector.vue
@@ -0,0 +1,106 @@
+<script lang="ts" setup>
+interface ItemType {
+  icon: IconMapKey
+  title?: string
+  tooltip?: string
+  value: string
+  hidden?: boolean
+}
+
+interface Props {}
+
+const props = defineProps<Props>()
+
+const modelValue = defineModel<string>()
+
+const { isUIAllowed } = useRoles()
+
+const { isFeatureEnabled } = useBetaFeatureToggle()
+
+const { isSqlView } = useSmartsheetStoreOrThrow()
+
+const disabled = computed(() => {
+  return !isUIAllowed('viewCreateOrEdit')
+})
+
+const items = computed<ItemType[]>(() => {
+  return [
+    { icon: 'menu', value: 'field', tooltip: 'Fields' },
+    {
+      icon: modelValue.value === 'attachment' ? 'ncFileTextSolid' : 'ncFileText',
+      value: 'attachment',
+      tooltip: 'File Preview',
+      hidden: !isFeatureEnabled(FEATURE_FLAG.EXPANDED_FORM_FILE_PREVIEW_MODE),
+    },
+    {
+      icon: modelValue.value === 'attachment' ? 'ncMessageSquare1Outline' : 'ncMessageSquare1Solid',
+      value: 'discussion',
+      tooltip: 'Discussion',
+      hidden: !isFeatureEnabled(FEATURE_FLAG.EXPANDED_FORM_DISCUSSION_MODE) || isSqlView.value,
+    },
+  ]
+})
+</script>
+
+<template>
+  <NcTooltip :disabled="!disabled">
+    <template #title> You do not have permission to change view mode. </template>
+    <div
+      class="tab-wrapper flex flex-row rounded-lg border-1 border-nc-border-gray-medium bg-white h-7 overflow-hidden"
+      :class="{
+        '!cursor-not-allowed opacity-50': disabled,
+      }"
+    >
+      <NcTooltip v-for="(item, idx) of items" :key="item.value" :disabled="!item.tooltip || disabled">
+        <template #title>{{ item.tooltip }}</template>
+        <div
+          v-e="[`c:project:mode:${item.value}`]"
+          class="tab"
+          :class="{
+            'pointer-events-none': disabled,
+            'active': modelValue === item.value,
+            'first-tab': idx === 0,
+            'last-tab': idx === items.length - 1,
+          }"
+          @click="modelValue = item.value"
+        >
+          <GeneralIcon :icon="item.icon" class="tab-icon" />
+          <div v-if="item.title" class="tab-title nc-tab">
+            {{ $t(item.title) }}
+          </div>
+        </div>
+      </NcTooltip>
+    </div>
+  </NcTooltip>
+</template>
+
+<style lang="scss" scoped>
+.tab {
+  @apply flex flex-row items-center h-full justify-center px-2 border-1 border-t-0 border-b-0 border-nc-border-gray-medium text-gray-600 hover:text-black cursor-pointer transition-all duration-300 select-none;
+
+  &.first-tab {
+    @apply border-0;
+  }
+  &.last-tab {
+    @apply border-0;
+  }
+}
+
+.tab-icon {
+  font-size: 1rem !important;
+  @apply w-4;
+}
+.tab .tab-title {
+  @apply min-w-0;
+  word-break: keep-all;
+  white-space: nowrap;
+  display: inline;
+  line-height: 0.95;
+}
+
+.active {
+  @apply bg-nc-bg-brand text-brand-600 hover:text-brand-600;
+
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.06), 0px 5px 3px -2px rgba(0, 0, 0, 0.02);
+}
+</style>

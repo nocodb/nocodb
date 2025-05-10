58ddf9fcca4370464b31400c71deaab8738f35ce
Fendy Heryanto <fendy@nocodb.com>
2025-05-10 09:06:45 +0000

    add computed field handler

---
+++ packages/nocodb/src/db/field-handler/handlers/computed.ts
@@ -0,0 +1,19 @@
+import { GenericFieldHandler } from './generic';
+import type { NcContext } from 'nocodb-sdk';
+import type { IBaseModelSqlV2 } from 'src/db/IBaseModelSqlV2';
+import type { MetaService } from 'src/meta/meta.service';
+import type { Column } from 'src/models';
+
+export class ComputedFieldHandler extends GenericFieldHandler {
+  override async parseUserInput(_params: {
+    value: any;
+    row: any;
+    column: Column;
+    baseModel: IBaseModelSqlV2;
+    options?: { context?: NcContext; metaService?: MetaService };
+  }): Promise<{ value: any }> {
+    return {
+      value: undefined,
+    };
+  }
+}

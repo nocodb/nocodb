bfde523df9f09e96ddb829899ac17ad024b9f28e
Fendy Heryanto <fendy@nocodb.com>
2025-05-10 12:11:50 +0000

    Add selected border

---
+++ packages/nc-gui/components/nc/RowColorPicker/Dropdown.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+interface Props {
+  modelValue?: string | null
+}
+
+interface Emits {
+  (event: 'update:modelValue', model: number): void
+  (event: 'click', model: ClickEvent): void
+}
+
+const props = defineProps<Props>()
+const emits = defineEmits<Emits>()
+const vModel = useVModel(props, 'modelValue', emits)
+
+const iconColor = computed(() => {
+  if (!vModel.value) {
+    return 'text-gray-800'
+  }
+  const selectedColorArray = vModel.value?.split('-')
+  const selectedColorWeight = selectedColorArray[selectedColorArray.length - 1]
+  if (Number(selectedColorWeight) < 500) {
+    return 'text-gray-800'
+  } else {
+    return 'text-gray-300'
+  }
+})
+</script>
+
+<template>
+  <NcDropdown>
+    <button
+      type="button"
+      class="p-1 inline-flex content-center items-center justify-center aspect-square rounded-md min-w-[30px] min-h-[30px] hover:border-gray-200 border-2"
+      :class="[`bg-${vModel}`]"
+    >
+      <GeneralIcon class="w-[16px] h-[16px]" :class="[iconColor]" icon="ncChevronDown" />
+    </button>
+    <template #overlay>
+      <NcRowColorPicker v-model="vModel" />
+    </template>
+  </NcDropdown>
+</template>

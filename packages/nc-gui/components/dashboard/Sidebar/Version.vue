d2816f5c653523bdb6fff6c7ef9a33b22f4348e3
Pranav C <pranavxc@gmail.com>
2025-05-10 09:11:21 +0000

    feat: add version in sidebar

---
+++ packages/nc-gui/components/dashboard/Sidebar/Version.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { appInfo } = useGlobal()
+</script>
+
+<template>
+  <div>
+    <div class="flex items-center text-xs text-gray-400 px-3 py-1">
+      <span>Version {{ appInfo.version }}</span>
+    </div>
+  </div>
+</template>

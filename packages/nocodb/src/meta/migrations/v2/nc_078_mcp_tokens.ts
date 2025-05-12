03e3da07b9d4f23578178afd92696c4cfac86af8
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-05-12 13:06:00 +0000

    fix: review changes

--- packages/nocodb/src/meta/migrations/v2/nc_078_mcp_tokens.ts
+++ packages/nocodb/src/meta/migrations/v2/nc_078_mcp_tokens.ts
@@ -9,21 +9,14 @@ const up = async (knex: Knex) => {
     table.string('base_id', 20);
     table.string('expires_at');
 
+    table.string('fk_workspace_id', 20);
     table.float('order');
-
-    if (isEE) {
-      table.string('fk_workspace_id', 20);
-    }
 
     table.string('fk_user_id', 20);
 
-    table.string('status', 20);
-
     table.timestamps(true, true);
 
-    if (isEE) {
-      table.index(['fk_workspace_id', 'base_id'], 'nc_mc_tokens_context');
-    }
+    table.index(['base_id', 'fk_workspace_id'], 'nc_mc_tokens_context');
   });
 };
 

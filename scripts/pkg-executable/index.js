81a423cc1dbd8ff10a14d48d7e1500904f98f252
Dev Dalia <devdalia9@gmail.com>
2025-05-13 22:23:54 +0530

    fix: args added to executable

--- scripts/pkg-executable/index.js
+++ scripts/pkg-executable/index.js
@@ -1,4 +1,33 @@
 process.env.NC_BINARY_BUILD = 'true';
+
+const args = process.argv.slice(2);
+
+const knownFlags = ['-h', '--help'];
+const unknownFlags = args.filter(arg => arg.startsWith('-') && !knownFlags.includes(arg));
+const helpText = `
+Usage:
+  nocodb [options]
+
+Options:
+  -h, --help     Show this help message and exit
+
+Description:
+  NocoDB turns your database into a smart spreadsheet.
+
+For more information, visit: https://github.com/nocodb/nocodb
+  `
+
+if (unknownFlags.length > 0) {
+  console.log(`Unknown option(s): ${unknownFlags.join(', ')}`);
+  console.log(helpText);
+  process.exit(1);
+}
+
+if (args.includes('-h') || args.includes('--help')) {
+  console.log(helpText);
+  process.exit(0);
+}
+
 (async () => {
   try {
     const app = require('express')();

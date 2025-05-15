0680e4216b1d90598997b8862d838c7cf7d7d9ef
Pranav C <pranavxc@gmail.com>
2025-05-15 20:11:37 +0000

    refactor: correction

--- packages/nocodb/src/db/formulav2/lookup-or-ltar-builder.ts
+++ packages/nocodb/src/db/formulav2/lookup-or-ltar-builder.ts
@@ -77,7 +77,6 @@ export const lookupOrLtarBuilder =
           {
             const parentBaseModel = await Model.getBaseModelSQL(parentContext, {
               model: parentModel,
-              tableName: parentModel.table_name,
               dbDriver: baseModelSqlv2.dbDriver,
             });
 
@@ -101,7 +100,6 @@ export const lookupOrLtarBuilder =
           {
             const childBaseModel = await Model.getBaseModelSQL(childContext, {
               model: childModel,
-              tableName: childModel.table_name,
               dbDriver: baseModelSqlv2.dbDriver,
             });
             isArray = relation.type !== RelationTypes.ONE_TO_ONE;
@@ -125,7 +123,6 @@ export const lookupOrLtarBuilder =
           {
             const parentBaseModel = await Model.getBaseModelSQL(parentContext, {
               model: parentModel,
-              tableName: parentModel.table_name,
               dbDriver: baseModelSqlv2.dbDriver,
             });
             isArray = true;

34a5c167bb76322a0d0b3a1bab830d1be26c1708
mertmit <mertmit99@gmail.com>
2025-05-16 19:40:17 +0300

    Add Asana Auth Integration

---
+++ packages/noco-integrations/packages/asana-auth/src/form.ts
@@ -0,0 +1,96 @@
+import { FormBuilderInputType } from '@noco-integrations/core';
+import { AuthType } from '@noco-integrations/core';
+import { authUri, clientId, redirectUri, scopes } from './config';
+import type { FormDefinition } from '@noco-integrations/core';
+
+export const form: FormDefinition = [
+  {
+    type: FormBuilderInputType.Input,
+    label: 'Integration name',
+    width: 100,
+    model: 'title',
+    placeholder: 'Integration name',
+    category: 'General',
+    validators: [
+      {
+        type: 'required' as const,
+        message: 'Integration name is required',
+      },
+    ],
+  },
+  {
+    type: FormBuilderInputType.Select,
+    label: 'Auth Type',
+    width: 48,
+    model: 'config.type',
+    category: 'Authentication',
+    placeholder: 'Select auth type',
+    defaultValue: AuthType.ApiKey,
+    options: [
+      {
+        label: 'API Key',
+        value: AuthType.ApiKey,
+      },
+      ...(redirectUri && clientId
+        ? [
+            {
+              label: 'OAuth2',
+              value: AuthType.OAuth,
+            },
+          ]
+        : []),
+    ],
+    validators: [
+      {
+        type: 'required' as const,
+        message: 'Auth type is required',
+      },
+    ],
+  },
+  {
+    type: FormBuilderInputType.Input,
+    label: 'API Token',
+    width: 100,
+    model: 'config.token',
+    category: 'Authentication',
+    placeholder: 'Enter your Asana Personal Access Token',
+    validators: [
+      {
+        type: 'required' as const,
+        message: 'API Token is required',
+      },
+    ],
+    condition: {
+      model: 'config.type',
+      value: AuthType.ApiKey,
+    },
+  },
+  ...(redirectUri && clientId
+    ? [
+        {
+          type: FormBuilderInputType.OAuth,
+          label: 'OAuth Configuration',
+          width: 100,
+          model: 'config.oauth.code',
+          category: 'Authentication',
+          validators: [
+            {
+              type: 'required' as const,
+              message: 'OAuth Configuration is required',
+            },
+          ],
+          condition: {
+            model: 'config.type',
+            value: AuthType.OAuth,
+          },
+          oauthMeta: {
+            provider: 'Asana',
+            authUri,
+            redirectUri,
+            clientId,
+            scopes,
+          },
+        },
+      ]
+    : []),
+]; 
---
+++ packages/noco-integrations/packages/asana-auth/src/integration.ts
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import * as asana from 'asana';
+import { AuthIntegration, AuthType } from '@noco-integrations/core';
+import { clientId, clientSecret, tokenUri } from './config';
+import type { AuthResponse } from '@noco-integrations/core';
+
+export class AsanaAuthIntegration extends AuthIntegration {
+  public async authenticate(): Promise<AuthResponse<asana.Client>> {
+    switch (this.config.type) {
+      case AuthType.ApiKey:
+        if (!this.config.token) {
+          throw new Error('Missing required Asana API token');
+        }
+        
+        const client = asana.Client.create();
+        client.useAccessToken(this.config.token);
+        
+        return {
+          custom: client,
+        };
+      case AuthType.OAuth:
+        if (!this.config.oauth_token) {
+          throw new Error('Missing required Asana OAuth token');
+        }
+        
+        const oauthClient = asana.Client.create();
+        oauthClient.useAccessToken(this.config.oauth_token);
+        
+        return {
+          custom: oauthClient,
+        };
+      default:
+        throw new Error('Not implemented');
+    }
+  }
+
+  public async exchangeToken(payload: {
+    code: string;
+  }): Promise<{ oauth_token: string }> {
+    const response = await axios.post(
+      tokenUri,
+      {
+        grant_type: 'authorization_code',
+        client_id: clientId,
+        client_secret: clientSecret,
+        code: payload.code,
+        redirect_uri: this.config.redirect_uri,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Accept: 'application/json',
+        },
+      },
+    );
+
+    return {
+      oauth_token: response.data.access_token,
+    };
+  }
+} 

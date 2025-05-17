97e555734e9557c6ad2e6857be31682af64115eb
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-05-17 06:32:01 +0000

    chore(nc-gui): lint

--- packages/nc-gui/components/dashboard/TreeView/Project/SearchInput.vue
+++ packages/nc-gui/components/dashboard/TreeView/Project/SearchInput.vue
@@ -26,11 +26,11 @@ const onKeydown = (e: KeyboardEvent) => {
   <a-input
     ref="inputRef"
     v-model:value="vModel"
-    @keydown="onKeydown"
     type="text"
     class="nc-base-search-input nc-input-border-on-value nc-input-shadow !h-8 !px-2.5 !py-1 !rounded-lg"
     :placeholder="`${$t('activity.searchProject').charAt(0).toUpperCase()}${$t('activity.searchProject').slice(1).toLowerCase()}`"
     allow-clear
+    @keydown="onKeydown"
   >
     <template #prefix>
       <GeneralIcon icon="search" class="mr-1 h-4 w-4 text-gray-500 group-hover:text-black" />

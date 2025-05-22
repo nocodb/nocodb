90de66eba762fa19af0ecd6b30a2776fd60057a7
Fendy Heryanto <fendy@nocodb.com>
2025-05-22 08:37:05 +0000

    reenable not found module tests

--- packages/nocodb/tests/unit/rest/tests/dataApiV3/error-handling.test.ts
+++ packages/nocodb/tests/unit/rest/tests/dataApiV3/error-handling.test.ts
@@ -99,13 +99,256 @@ describe('dataApiV3', () => {
         );
       });
 
-      // we revert to default limit if provided limit is outside of allowed range
-      // or incorrect format
-      it.skip('Invalid Page Size', async () => {
-        let response = await ncAxiosGet({
-          url: `${urlPrefix}/${testContext.countryTable.id}`,
-          query: {
-            limit: 0,
+    // we revert to default limit if provided limit is outside of allowed range
+    // or incorrect format
+    it.skip('Invalid Page Size', async () => {
+      let response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          limit: 0,
+        },
+      });
+      console.log(response.status);
+      response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          limit: 1000,
+        },
+      });
+      console.log(response.status);
+      response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          limit: -1,
+        },
+      });
+      console.log(response.status);
+      response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          limit: 'Hello',
+        },
+      });
+      console.log(response.status);
+    });
+
+    it('tableId not found', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/123456789`,
+        status: 422,
+      });
+      expect(response.body.error).to.eq('TABLE_NOT_FOUND');
+      expect(response.body.message).to.eq(`Table '123456789' not found`);
+    });
+    it('baseId not found', async () => {
+      const response = await ncAxiosGet({
+        url: `/api/v3/234567890/123456789`,
+        status: 422,
+      });
+      expect(response.body.error).to.equal('BASE_NOT_FOUND');
+      expect(response.body.message).to.eq(`Base '234567890' not found`);
+    });
+    it('invalid api version', async () => {
+      const response = await ncAxiosGet({
+        url: `/api/v4/1234567890/2134567890`,
+        status: 404,
+      });
+      expect(response.body.error).to.eq(`INVALID_API_VERSION`);
+      expect(response.body.message).to.eq(`API version unsupported`);
+    });
+    it('backward compatibility for previous api version', async () => {
+      const response = await ncAxiosGet({
+        url: `/api/v1/abcasdasda`,
+        status: 404,
+      });
+      expect(response.body.msg).to.eq(`Cannot GET /api/v1/abcasdasda`);
+      const responsev2 = await ncAxiosGet({
+        url: `/api/v2/abcasdasda`,
+        status: 404,
+      });
+      expect(responsev2.body.msg).to.eq(`Cannot GET /api/v2/abcasdasda`);
+    });
+    it('invalid view param', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}?viewId=123456890`,
+        status: 422,
+      });
+      expect(response.body.error).to.eq(`VIEW_NOT_FOUND`);
+      expect(response.body.message).to.eq(`View '123456890' not found`);
+    });
+    it('invalid page', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          page: 500,
+        },
+        status: 422,
+      });
+      expect(response.body.message).to.eq(`Offset value '12475' is invalid`);
+    });
+    it('invalid page (minus)', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          page: -1,
+        },
+        status: 422,
+      });
+      expect(response.body.message).to.eq(
+        `Offset must be a non-negative integer`,
+      );
+    });
+    it('invalid page (string)', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          page: 'hello',
+        },
+        status: 422,
+      });
+      expect(response.body.message).to.eq(
+        `Offset must be a non-negative integer`,
+      );
+    });
+    it('invalid sort field', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          sort: 'NotFoundField',
+        },
+        status: 422,
+      });
+      expect(response.body.error).to.eq('FIELD_NOT_FOUND');
+      expect(response.body.message).to.eq(`Field 'NotFoundField' not found`);
+    });
+    // skip, our sort direction is either {field} (asc) or -{field} (desc) so no validation required
+    it.skip('invalid sort direction', async () => {});
+
+    it('invalid filter field (column not found)', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          where: '(NotFoundField,eq,1)',
+        },
+        status: 422,
+      });
+      expect(response.body.message).to.eq(
+        `Invalid filter field 'NotFoundField' not found`,
+      );
+    });
+
+    it('invalid filter (invalid parsing)', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          where: '(NotFoundField,eq',
+        },
+        status: 422,
+      });
+      expect(response.body.message).to.eq(
+        `Invalid filter syntax: expected a closing parentheses ')', but found ''`,
+      );
+    });
+
+    it('invalid filter (missing comma)', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          where: '(NotFoundField',
+        },
+        status: 422,
+      });
+      expect(response.body.message).to.eq(
+        `Invalid filter syntax: expected comma ',' followed with operator (and value) after field`,
+      );
+    });
+
+    it('invalid filter value format', async () => {
+      const paymentTable = await getTable({
+        base: testContext.sakilaProject,
+        name: 'payment',
+      });
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${paymentTable.id}`,
+        query: {
+          where: '(Amount,eq,HELLO)',
+        },
+        status: 422,
+      });
+      expect(response.body.error).to.eq(`INVALID_FILTER`);
+      expect(response.body.message).to.eq(
+        `Invalid filter expression: Value HELLO is not supported for type Decimal on column Amount`,
+      );
+    });
+
+    it('invalid filter operator', async () => {
+      const paymentTable = await getTable({
+        base: testContext.sakilaProject,
+        name: 'payment',
+      });
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${paymentTable.id}`,
+        query: {
+          where: '(Amount,notInOperator,HELLO)',
+        },
+        status: 422,
+      });
+      expect(response.body.error).to.eq(`INVALID_FILTER`);
+      expect(response.body.message).to.eq(
+        `Invalid filter expression: 'notInOperator' is not a recognized operator. Please use a valid comparison or logical operator`,
+      );
+    });
+
+    it('invalid select field', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          fields: ['Country', 'NotFoundField'],
+        },
+        status: 422,
+      });
+      expect(response.body.message).to.eq(`Field 'NotFoundField' not found`);
+    });
+    // our api can accept array or not array
+    it.skip('field parameter malformed', async () => {
+      await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}`,
+        query: {
+          fields: 'Country',
+        },
+        status: 404,
+      });
+    });
+
+    it('record id not found', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}/1032`,
+        status: 404,
+      });
+      expect(response.body.message).to.eq("Record '1032' not found");
+    });
+
+    it('primary key not in correct data type (CountryId = number)', async () => {
+      const response = await ncAxiosGet({
+        url: `${urlPrefix}/${testContext.countryTable.id}/text-primary-key`,
+        status: 422,
+      });
+      expect(response.body.error).to.eq('INVALID_PK_VALUE');
+      expect(response.body.message).to.eq(
+        `Primary key value 'text-primary-key' is invalid for column 'CountryId'`,
+      );
+    });
+
+    it('primary key not in correct data type (Id)', async () => {
+      const table = await createTable(testContext.context, testContext.base, {
+        table_name: 'testTable',
+        title: 'TestTable',
+        columns: [
+          {
+            column_name: 'id',
+            title: 'Id',
+            uidt: UITypes.ID,
+            pk: true,
           },
         });
         console.log(response.status);
@@ -225,176 +468,10 @@ describe('dataApiV3', () => {
       // skip, our sort direction is either {field} (asc) or -{field} (desc) so no validation required
       it.skip('invalid sort direction', async () => {});
 
-      it('invalid filter field (column not found)', async () => {
-        const response = await ncAxiosGet({
-          url: `${urlPrefix}/${testContext.countryTable.id}`,
-          query: {
-            where: '(NotFoundField,eq,1)',
-          },
-          status: 422,
-        });
-        expect(response.body.message).to.eq(
-          `Invalid filter field 'NotFoundField' not found`,
-        );
-      });
-
-      it('invalid filter (invalid parsing)', async () => {
-        const response = await ncAxiosGet({
-          url: `${urlPrefix}/${testContext.countryTable.id}`,
-          query: {
-            where: '(NotFoundField,eq',
-          },
-          status: 422,
-        });
-        expect(response.body.message).to.eq(
-          `Invalid filter syntax: expected a closing parentheses ')', but found ''`,
-        );
-      });
-
-      it('invalid filter (missing comma)', async () => {
-        const response = await ncAxiosGet({
-          url: `${urlPrefix}/${testContext.countryTable.id}`,
-          query: {
-            where: '(NotFoundField',
-          },
-          status: 422,
-        });
-        expect(response.body.message).to.eq(
-          `Invalid filter syntax: expected comma ',' followed with operator (and value) after field`,
-        );
-      });
-
-      it('invalid filter value format', async () => {
-        const paymentTable = await getTable({
-          base: testContext.sakilaProject,
-          name: 'payment',
-        });
-        const response = await ncAxiosGet({
-          url: `${urlPrefix}/${paymentTable.id}`,
-          query: {
-            where: '(Amount,eq,HELLO)',
-          },
-          status: 422,
-        });
-        expect(response.body.error).to.eq(`INVALID_FILTER`);
-        expect(response.body.message).to.eq(
-          `Invalid filter expression: Value HELLO is not supported for type Decimal on column Amount`,
-        );
-      });
-
-      it('invalid filter operator', async () => {
-        const paymentTable = await getTable({
-          base: testContext.sakilaProject,
-          name: 'payment',
-        });
-        const response = await ncAxiosGet({
-          url: `${urlPrefix}/${paymentTable.id}`,
-          query: {
-            where: '(Amount,notInOperator,HELLO)',
-          },
-          status: 422,
-        });
-        expect(response.body.error).to.eq(`INVALID_FILTER`);
-        expect(response.body.message).to.eq(
-          `Invalid filter expression: 'notInOperator' is not a recognized operator. Please use a valid comparison or logical operator`,
-        );
-      });
-
-      it('invalid select field', async () => {
-        const response = await ncAxiosGet({
-          url: `${urlPrefix}/${testContext.countryTable.id}`,
-          query: {
-            fields: ['Country', 'NotFoundField'],
-          },
-          status: 422,
-        });
-        expect(response.body.message).to.eq(`Field 'NotFoundField' not found`);
-      });
-      // our api can accept array or not array
-      it.skip('field parameter malformed', async () => {
-        await ncAxiosGet({
-          url: `${urlPrefix}/${testContext.countryTable.id}`,
-          query: {
-            fields: 'Country',
-          },
-          status: 404,
-        });
-      });
-
-      it('record id not found', async () => {
-        const response = await ncAxiosGet({
-          url: `${urlPrefix}/${testContext.countryTable.id}/1032`,
-          status: 404,
-        });
-        expect(response.body.message).to.eq("Record '1032' not found");
-      });
-
-      it('primary key not in correct data type (CountryId = number)', async () => {
-        const response = await ncAxiosGet({
-          url: `${urlPrefix}/${testContext.countryTable.id}/text-primary-key`,
-          status: 422,
-        });
-        expect(response.body.error).to.eq('INVALID_PK_VALUE');
-        expect(response.body.message).to.eq(
-          `Primary key value 'text-primary-key' is invalid for column 'CountryId'`,
-        );
-      });
-
-      it('primary key not in correct data type (Id)', async () => {
-        const table = await createTable(
-          testContext.context,
-          testContext.sakilaProject,
-          {
-            table_name: 'testTable',
-            title: 'TestTable',
-            columns: [
-              {
-                column_name: 'id',
-                title: 'Id',
-                uidt: UITypes.ID,
-                pk: true,
-              },
-              {
-                column_name: 'text',
-                title: 'Text',
-                uidt: UITypes.SingleLineText,
-                pk: false,
-                pv: true,
-              },
-            ],
-          },
-        );
-        await createBulkRows(testContext.context, {
-          base: testContext.sakilaProject,
-          table,
-          values: [
-            {
-              Id: 1,
-              SingleLineText: 'A',
-            },
-          ],
-        });
-
-        const response = await ncAxiosGet({
-          url: `${urlPrefix}/${table.id}/text-primary-key`,
-          status: 422,
-        });
-        expect(response.body.error).to.eq('INVALID_PK_VALUE');
-        expect(response.body.message).to.eq(
-          `Primary key value 'text-primary-key' is invalid for column 'Id'`,
-        );
-      });
-
-      // TODO: reenable after fix/not-found-module
-      it.skip('url path not found', async () => {
-        const response = await ncAxiosGet({
-          url: `/api/v3/mybase/mytable/unknown-path/1234`,
-          status: 404,
-        });
-        expect(response.body.error).to.eq('NOT_FOUND');
-        expect(response.body.message).to.eq(
-          'Cannot GET /api/v3/mybase/mytable/unknown-path/1234',
-        );
+    it('url path not found', async () => {
+      const response = await ncAxiosGet({
+        url: `/api/v3/mybase/mytable/unknown-path/1234`,
+        status: 404,
       });
     });
 

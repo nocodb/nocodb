bcfde56dfb2b08c6731096be0ecb2502eb2ba770
Fendy Heryanto <fendy@nocodb.com>
2025-05-23 03:03:59 +0000

    Add playground and row color picker

---
+++ packages/nc-gui/pages/playground/component/row-color-picker/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const color1 = ref('')
+</script>
+
+<template>
+  <div class="bg-gray-100">
+    <a-card>
+      <h4>Simple</h4>
+
+      Selected color: {{ color1 }}
+      <div class="inline-block min-h-[24px] min-w-[24px] h-[24px] w-[24px] rounded-md" :class="[`bg-${color1}`]"></div>
+    </a-card>
+    <NcRowColorPicker v-model="color1" />
+  </div>
+</template>

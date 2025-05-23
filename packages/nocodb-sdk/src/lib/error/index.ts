67eb676504f28d78901e754d2810faf600e6579e
Fendy Heryanto <fendy@nocodb.com>
2025-05-23 02:37:51 +0000

    refactor catchError to take out NcError and move most of the logic into sdk

--- packages/nocodb-sdk/src/lib/error/index.ts
+++ packages/nocodb-sdk/src/lib/error/index.ts
@@ -1,5 +1,6 @@
 export * from './computed-type-paste.error';
 export * from './select-type-conversion.error';
 export * from './silent-type-conversion.error';
+export * from './nc-base.error';
 export * from './suppressed.error';
 export * from './type-conversion.error';

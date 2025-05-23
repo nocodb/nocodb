cbc10c259295822a6b32386f676c6ab46ecd7401
mertmit <mertmit99@gmail.com>
2025-05-23 08:29:23 +0000

    chore: ai template

--- packages/noco-integrations/old/clickhouse-table-sync/src/manifest.ts
+++
@@ -1,9 +0,0 @@
-import type { IntegrationManifest } from '@noco-integrations/core';
-
-export const manifest: IntegrationManifest = {
-  title: 'Clickhouse Table',
-  icon: 'ncDatabase',
-  version: '0.1.0',
-  description: 'Sync data from Clickhouse tables',
-  author: 'NocoDB',
-};

b5a08857f086a87b3b364f1f7ac3b24832cd77c8
sinanmohd <sinan@sinanmohd.com>
2025-05-25 22:37:50 +0530

    refactor(nix/workflows/aiopusher): add version, only push multiarch

--- nix/workflows/aiopusher/default.nix
+++ nix/workflows/aiopusher/default.nix
@@ -20,50 +20,53 @@ writeShellApplication {
     image="nocodb/nocodb"
 
     note () {
-      echo "$@"
+      echo aiopusher: "$@"
     }
 
     secret_get() {
       sops decrypt --extract "[\"app.docker.com\"][\"$1\"]"  ${./secrets.yaml}
     }
 
-    image_push() {
+    image_build() {
       # $1: nix arch
-      # $2: tag
       package="packages.$1.docker_aio"
-      note "updating $image:$2 with $package"
+      note "building $image:$1 with $package"
 
       nix build "${self}#$package" -L
       tag="$(docker image load -i result | tail -n1 | cut -d: -f3)"
       id="$(docker image ls --format "{{.ID}}:{{.Tag}}" | grep "$tag" | cut -d: -f1)"
 
-      docker image tag "$id" "$image:$2"
-      docker push "$image:$2"
+      docker image tag "$id" "$image:$1"
     }
 
-    multiarch_image_push() {
-      # $1: multiarch tag
+    image_push() {
+      # $1: version tag
       # $2: arch tag 1
       # $3: arch tag 1
 
       docker manifest \
-        create "$image:$1" \
+        create "$image:latest" \
         --amend "$image:$2" \
         --amend "$image:$3"
-
       docker manifest push "$image:$1"
+
+      if [ -n "$1" ]; then
+        docker image tag "$image:latest" "$image:$1"
+        docker push "$image:$1"
+      fi
     }
 
     ########
     # MAIN #
     ########
+    # $1?: version
 
     docker login \
       --username "$(secret_get username)" \
       --password "$(secret_get password)"
 
-    image_push x86_64-linux amd64_latest
-    image_push aarch64-linux arm64_latest
-    multiarch_image_push latest amd64_latest arm64_latest
+    image_build x86_64-linux
+    image_build aarch64-linux
+    image_push "$1" x86_64-linux aarch64-linux
   '';
 }

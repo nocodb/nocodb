dae29080c022ca7f1f7c676c6e827b242c4a583b
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-05-28 09:42:17 +0000

    fix(nc-gui): base home page loading skeleton

--- packages/nc-gui/components/dashboard/TreeView/Project/List.vue
+++ packages/nc-gui/components/dashboard/TreeView/Project/List.vue
@@ -488,15 +488,22 @@ watch(
       </Transition>
       <!-- Slide in Project Home -->
       <Transition name="layout" mode="out-in" :duration="400" appear>
-        <div v-if="!showProjectList" key="project-home" class="absolute w-full h-full top-0 left-0 z-5 flex flex-col">
-          <ProjectWrapper v-if="activeProjectId && openedBase?.id" :base-role="openedBase?.project_role" :base="openedBase">
-            <DashboardTreeViewProjectHome>
-              <template #footer>
-                <slot name="footer"></slot>
-              </template>
-            </DashboardTreeViewProjectHome>
-          </ProjectWrapper>
-        </div>
+        <template v-if="!showProjectList">
+          <div
+            v-if="activeProjectId && openedBase?.id && !openedBase.isLoading"
+            key="project-home"
+            class="absolute w-full h-full top-0 left-0 z-5 flex flex-col"
+          >
+            <ProjectWrapper :base-role="openedBase?.project_role" :base="openedBase">
+              <DashboardTreeViewProjectHome>
+                <template #footer>
+                  <slot name="footer"></slot>
+                </template>
+              </DashboardTreeViewProjectHome>
+            </ProjectWrapper>
+          </div>
+          <DashboardTreeViewProjectListSkeleton v-else />
+        </template>
       </Transition>
       <WorkspaceCreateProjectDlg v-model="baseCreateDlg" />
     </template>

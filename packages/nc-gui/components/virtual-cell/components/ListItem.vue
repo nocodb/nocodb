3429c53738fd2e28fc16564b95b8703b03cf3eb4
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-05-31 11:54:52 +0000

    feat(nc-gui): show extra fields in link record dropdown based on form field settings

--- packages/nc-gui/components/virtual-cell/components/ListItem.vue
+++ packages/nc-gui/components/virtual-cell/components/ListItem.vue
@@ -40,7 +40,7 @@ const readOnly = inject(ReadonlyInj, ref(false))
 
 const { getPossibleAttachmentSrc } = useAttachment()
 
-const { externalBaseUserRoles } = useLTARStoreOrThrow()!
+const { externalBaseUserRoles, showExtraFields } = useLTARStoreOrThrow()!
 
 const { isUIAllowed } = useRoles()
 
@@ -123,7 +123,10 @@ const displayValue = computed(() => {
             </span>
           </div>
 
-          <div v-if="fields.length > 0" class="flex ml-[-0.25rem] sm:flex-row xs:(flex-col mt-2) gap-4 min-h-5">
+          <div
+            v-if="fields.length > 0 && showExtraFields"
+            class="flex ml-[-0.25rem] sm:flex-row xs:(flex-col mt-2) gap-4 min-h-5"
+          >
             <div v-for="field in fields" :key="field.id" class="sm:(w-1/3 max-w-1/3 overflow-hidden)">
               <div v-if="!isRowEmpty({ row }, field)" class="flex flex-col gap-[-1]">
                 <NcTooltip class="z-10 flex" placement="bottomLeft" :arrow-point-at-center="false">

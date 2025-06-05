f356eb62a4eadb41763a78607ef373a65ad9728f
Pranav C <pranavxc@gmail.com>
2025-06-05 06:24:13 +0000

    feat: increment extension feature toggle version

--- packages/nc-gui/composables/useBetaFeatureToggle.ts
+++ packages/nc-gui/composables/useBetaFeatureToggle.ts
@@ -130,7 +130,7 @@ const FEATURES = [
     title: 'Extensions',
     description: 'Extensions allows you to add new features or functionalities to the NocoDB platform.',
     enabled: ncIsPlaywright(),
-    version: 1,
+    version: 2,
     isEngineering: true,
   },
   {

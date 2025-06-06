0bb7fb2053e8dfc70cba259658317035a8ab0ad9
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-06-06 09:03:12 +0000

    fix(nc-gui): on esc focus at the end of selection

--- packages/nc-gui/components/cell/RichText.vue
+++ packages/nc-gui/components/cell/RichText.vue
@@ -345,7 +345,7 @@ onClickOutside(editorDom, (e) => {
         @keydown.alt.stop
         @keydown.alt.enter.stop
         @keydown.shift.enter.stop
-        @keydown.esc="handleOnEscRichTextEditor"
+        @keydown.esc="handleOnEscRichTextEditor($event, editor)"
       />
       <div v-if="isFormField && !readOnly" class="nc-form-field-bubble-menu-wrapper overflow-hidden">
         <div

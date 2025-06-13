e2d5a41b31c513bcf5b50db228ba0e9b76bba263
Fendy Heryanto <fendy@nocodb.com>
2025-06-13 22:46:49 +0000

    initial effort for stateful filter

--- packages/nc-gui/pages/playground/-helper/mock-setup.ts
+++ packages/nc-gui/pages/playground/-helper/mock-setup.ts
@@ -1,14 +1,18 @@
+import { defaultBases } from './bases'
 import { defaultColumns } from './columns'
 
+const defaultBaseId = 'pRdVnZXPZgA'
 export const MOCK_TABLES_RAW = [
   {
     id: 'mtWA9ZXvsuh',
     name: 'table1',
     columns: defaultColumns,
+    base_id: defaultBaseId,
   },
   {
     id: 'mehpRLA42Cz',
     name: 'table2',
+    base_id: 'pRdVnZXPZgA',
   },
 ]
 
@@ -17,4 +21,11 @@ export const mockSetupInit = async () => {
   for (const table of MOCK_TABLES_RAW) {
     metas.value[table.id] = table
   }
+  const basesStore = useBases()
+  for (const baseId of Object.keys(defaultBases)) {
+    basesStore.bases.set(baseId, defaultBases[baseId])
+  }
+
+  const baseStore = useBase()
+  baseStore.forcedProjectId = defaultBaseId
 }

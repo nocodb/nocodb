bdff9234c2974db23b33314e7e0588232e9dfa4c
Fendy Heryanto <fendy@nocodb.com>
2025-06-13 15:09:47 +0700

    also handle currency string

--- packages/nocodb-sdk/src/lib/currencyHelpers.spec.ts
+++ packages/nocodb-sdk/src/lib/currencyHelpers.spec.ts
@@ -16,6 +16,20 @@ describe('currencyHelpers', () => {
       expect(result.isValid).toBe(true);
       expect(result.pointDecimalValue).toBe('1234.5');
     });
+    it('will parse a correct number for german locale with currency', () => {
+      const strVal = '€ 1.234,5';
+      const locale = 'de-DE';
+      const result = getNumericValue(strVal, locale);
+      expect(result.isValid).toBe(true);
+      expect(result.pointDecimalValue).toBe('1234.5');
+    });
+    it('will parse a correct number for USD with currency', () => {
+      const strVal = '$ 1,234.5';
+      const locale = 'us-US';
+      const result = getNumericValue(strVal, locale);
+      expect(result.isValid).toBe(true);
+      expect(result.pointDecimalValue).toBe('1234.5');
+    });
     it('will parse a incorrect correct number', () => {
       const strVal = '1.234,5';
       const locale = 'us-US';
@@ -23,14 +37,14 @@ describe('currencyHelpers', () => {
       expect(result.isValid).toBe(false);
       expect(result.pointDecimalValue).toBe(undefined);
     });
-    it('will parse a incorrect correct number due to misplaced thousand separator', () => {
+    it('will parse an incorrect correct number due to misplaced thousand separator', () => {
       const strVal = '1,2';
       const locale = 'us-US';
       const result = getNumericValue(strVal, locale);
       expect(result.isValid).toBe(false);
       expect(result.pointDecimalValue).toBe(undefined);
     });
-    it('will parse a correct number for german locale', () => {
+    it('will parse an incorrect number for german locale', () => {
       const strVal = '1,234.5';
       const locale = 'de-DE';
       const result = getNumericValue(strVal, locale);

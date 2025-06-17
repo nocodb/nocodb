5c537fd67f2301368217397e06fe62ae63d49517
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-06-17 05:42:30 +0000

    fix: v3 sorts fixes

--- packages/nocodb/src/helpers/dbHelpers.ts
+++ packages/nocodb/src/helpers/dbHelpers.ts
@@ -354,9 +354,7 @@ export function extractSortsObject(
   if (apiVersion === NcApiVersion.V3) {
     try {
       _sorts = JSON.parse(_sorts as string);
-    } catch (_e) {
-      _sorts = [];
-    }
+    } catch (_e) {}
     if (!Array.isArray(_sorts)) _sorts = [_sorts];
     return (_sorts as { direction: string; field: string }[]).map((s) => {
       const sort: SortType = {

994075582c8ff0e6745cb1d8c16d66817cfa581e
Fendy Heryanto <fendy@nocodb.com>
2025-06-19 03:30:52 +0000

    add sub op select

---
+++ packages/nc-gui/components/smartsheet/toolbar/Filter/ComparisonSubOpSelect.vue
@@ -0,0 +1,85 @@
+<script lang="ts" setup>
+import type { ColumnType, UITypes } from 'nocodb-sdk'
+
+interface Props {
+  modelValue: string
+  filter: ColumnFilterType
+  column: ColumnType
+  uidt: UITypes
+  link: boolean
+  webHook: boolean
+  isLockedView: boolean
+  readOnly: boolean
+}
+interface Emits {
+  (event: 'update:modelValue', model: string): void
+  (event: 'change', model: { filter: ColumnFilterType; comparison_op: string }): void
+}
+const props = defineProps<Props>()
+const emits = defineEmits<Emits>()
+const vModel = useVModel(props, 'modelValue', emits)
+
+const showFilterInput = (filter: Filter) => {
+  if (!filter.comparison_op) return false
+
+  if (filter.comparison_sub_op) {
+    return !comparisonSubOpList(filter.comparison_op, parseProp(props.column?.meta)?.date_format).find(
+      (op) => op.value === filter.comparison_sub_op,
+    )?.ignoreVal
+  } else {
+    return !comparisonOpList(props.uidt as UITypes, parseProp(props.column?.meta)?.date_format).find(
+      (op) => op.value === filter.comparison_op,
+    )?.ignoreVal
+  }
+}
+const allowedComparisonSubOp = computed(() => {
+  const { filter, uidt } = props
+  return comparisonSubOpList(filter.comparison_op!, parseProp(props.column?.meta)?.date_format).filter((compSubOp) =>
+    isComparisonSubOpAllowed(filter, compSubOp, uidt),
+  )
+})
+
+const onChange = (filter: ColumnFilterType) => {
+  emits('change', {
+    filter,
+    comparison_op: filter.comparison_op,
+  })
+}
+</script>
+
+<template>
+  <NcSelect
+    v-model:value="vModel"
+    v-e="['c:filter:sub-comparison-op:select', { link: !!link, webHook: !!webHook }]"
+    :dropdown-match-select-width="false"
+    class="caption nc-filter-sub_operation-select min-w-28"
+    :class="{
+      'flex-grow w-full': !showFilterInput(filter),
+      'max-w-28': showFilterInput(filter) && !webHook,
+    }"
+    :placeholder="$t('labels.operationSub')"
+    density="compact"
+    variant="solo"
+    :disabled="filter.readOnly || isLockedView || readOnly"
+    hide-details
+    dropdown-class-name="nc-dropdown-filter-comp-sub-op"
+    @change="onChange(filter)"
+  >
+    <template v-for="compSubOp of allowedComparisonSubOp" :key="compSubOp.value">
+      <a-select-option :value="compSubOp.value">
+        <div class="flex items-center w-full justify-between w-full gap-2 max-w-40">
+          <NcTooltip show-on-truncate-only class="truncate flex-1">
+            <template #title>{{ compSubOp.text }}</template>
+            {{ compSubOp.text }}
+          </NcTooltip>
+          <component
+            :is="iconMap.check"
+            v-if="filter.comparison_sub_op === compSubOp.value"
+            id="nc-selected-item-icon"
+            class="text-primary w-4 h-4"
+          />
+        </div>
+      </a-select-option>
+    </template>
+  </NcSelect>
+</template>
--- packages/nc-gui/pages/playground/components/filter/comparison-op-select/index.vue
+++ packages/nc-gui/pages/playground/components/filter/comparison-op-select/index.vue
@@ -2,25 +2,55 @@
 import { type ColumnType, UITypes } from 'nocodb-sdk'
 import { SmartsheetToolbarFilterComparisonOpSelect } from '#components'
 
+const columns: ColumnType[] = [
+  {
+    id: 'col1',
+    uidt: UITypes.SingleLineText,
+  },
+  {
+    id: 'col2',
+    uidt: UITypes.Decimal,
+  },
+  {
+    id: 'col3',
+    uidt: UITypes.Number,
+  },
+  {
+    id: 'col4',
+    uidt: UITypes.Links,
+  },
+  {
+    id: 'col5',
+    uidt: UITypes.DateTime,
+    meta: {
+      dateFormat: 'YY/MM/DD',
+    },
+  },
+  {
+    id: 'col-date-monthonly',
+    uidt: UITypes.Date,
+    meta: {
+      dateFormat: 'YY-MM',
+    },
+  },
+]
+
 const filter1 = ref({
   fk_column_id: 'col1',
   comparison_op: 'eq',
 })
-const columnSingleLineText = {
-  id: 'col1',
-  uidt: UITypes.SingleLineText,
-} as ColumnType
 
 const changeTimes1 = ref(0)
 
 const filter2 = ref({
   fk_column_id: 'col2',
   comparison_op: 'eq',
+  comparison_sub_op: undefined,
 })
-const columnSingleLineText2 = {
-  id: 'col2',
-  uidt: UITypes.SingleLineText,
-} as ColumnType
+const column2Id = ref(columns[0]!.id)
+const column2 = computed(() => {
+  return columns.find((col) => col.id === column2Id.value)
+})
 
 const changeTimes2 = ref(0)
 const options2 = ref({
@@ -44,8 +74,8 @@ const options2 = ref({
       <SmartsheetToolbarFilterComparisonOpSelect
         v-model="filter1.comparison_op"
         :filter="filter1"
-        :column="columnSingleLineText"
-        :uidt="columnSingleLineText.uidt"
+        :column="columns[0]!"
+        :uidt="columns[0]!.uidt"
         :link="false"
         :web-hook="false"
         :is-locked-view="false"
@@ -53,32 +83,66 @@ const options2 = ref({
         :show-null-and-empty-in-filter="true"
         @change="() => changeTimes1++"
       />
+      <SmartsheetToolbarFilterComparisonSubOpSelect
+        v-model="filter1.comparison_sub_op"
+        :filter="filter1"
+        :column="columns[0]!"
+        :uidt="columns[0]!.uidt"
+        :link="false"
+        :web-hook="false"
+        :is-locked-view="false"
+        :read-only="false"
+        @change="() => changeTimes1++"
+      />
     </div>
 
     <a-card>
       <h4>Options</h4>
 
-      <div>Selected comparisonOp: {{ filter1.comparison_op }}</div>
-      <div>Times changed: {{ changeTimes1 }}</div>
-      <div>
-        <NcSwitch v-model:checked="options2.link">link</NcSwitch><br />
-        <NcSwitch v-model:checked="options2.webHook">webHook</NcSwitch><br />
-        <NcSwitch v-model:checked="options2.isLockedView">isLockedView</NcSwitch><br />
-        <NcSwitch v-model:checked="options2.isReadOnly">isReadOnly</NcSwitch><br />
-        <NcSwitch v-model:checked="options2.isShowNullAndEmptyInFilter">isShowNullAndEmptyInFilter</NcSwitch>
+      <div>Selected comparisonOp: {{ filter2.comparison_op }}</div>
+      <div>Times changed: {{ changeTimes2 }}</div>
+      <div class="flex">
+        <div>
+          <NcSwitch v-model:checked="options2.link">link</NcSwitch><br />
+          <NcSwitch v-model:checked="options2.webHook">webHook</NcSwitch><br />
+          <NcSwitch v-model:checked="options2.isLockedView">isLockedView</NcSwitch><br />
+          <NcSwitch v-model:checked="options2.isReadOnly">isReadOnly</NcSwitch><br />
+          <NcSwitch v-model:checked="options2.isShowNullAndEmptyInFilter">isShowNullAndEmptyInFilter</NcSwitch>
+        </div>
+        <div class="flex">
+          <NcSelect v-model:value="column2Id" @change="filter2.fk_column_id = column2Id">
+            <a-select-option v-for="col of columns" :key="col.id" :value="col.id">
+              {{ col.id }} - {{ col.uidt }}
+            </a-select-option>
+          </NcSelect>
+          <div class="w-[300px] overflow-wrap">
+            {{ column2 }}
+          </div>
+        </div>
       </div>
     </a-card>
     <div class="p-4">
       <SmartsheetToolbarFilterComparisonOpSelect
         v-model="filter2.comparison_op"
         :filter="filter2"
-        :column="columnSingleLineText2"
-        :uidt="columnSingleLineText2.uidt"
-        :link="options2.link"
+        :column="column2!"
+        :uidt="column2?.uidt"
+        :link="options2?.link"
         :web-hook="options2.webHook"
         :is-locked-view="options2.isLockedView"
         :read-only="options2.isReadOnly"
         :show-null-and-empty-in-filter="options2.isShowNullAndEmptyInFilter"
+        @change="() => changeTimes2++"
+      />
+      <SmartsheetToolbarFilterComparisonSubOpSelect
+        v-model="filter2.comparison_sub_op"
+        :filter="filter2"
+        :column="column2!"
+        :uidt="column2?.uidt"
+        :link="options2?.link"
+        :web-hook="options2.webHook"
+        :is-locked-view="options2.isLockedView"
+        :read-only="options2.isReadOnly"
         @change="() => changeTimes2++"
       />
     </div>

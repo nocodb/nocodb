c8c13fbc3b452a06489540b20e2b9d4e9107cd03
Fendy Heryanto <fendy@nocodb.com>
2025-06-19 03:30:52 +0000

    prototype playground filter input

---
+++ packages/nc-gui/pages/playground/components/row-color-picker-toolbar/using-filter-panel/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const vModel = ref({})
+</script>
+
+<template>
+  <div class="bg-gray-100">
+    <a-card>
+      <h4>Simple</h4>
+      <div class="flex gap-2">
+        <div>
+          vModel:
+          {{ vModel }}
+        </div>
+      </div>
+    </a-card>
+    <div class="m-4">
+      <SmartsheetToolbarRowColorFilterUsingFilterPanel v-model="vModel" />
+    </div>
+  </div>
+</template>

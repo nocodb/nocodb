df9f9bc75d4d0a6bd986b2b3ddde353d4339a618
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-06-19 03:30:59 +0000

    fix(nc-gui): canvas re render issue on row color select field change

--- packages/nc-gui/components/smartsheet/grid/canvas/composables/useCanvasTable.ts
+++ packages/nc-gui/components/smartsheet/grid/canvas/composables/useCanvasTable.ts
@@ -1250,8 +1250,10 @@ export function useCanvasTable({
 
   eventBus.on((event) => {
     if (event === SmartsheetStoreEvents.TRIGGER_RE_RENDER) {
-      clearRowColouringCache()
-      triggerRefreshCanvas()
+      forcedNextTick(() => {
+        clearRowColouringCache()
+        triggerRefreshCanvas()
+      })
     }
   })
 

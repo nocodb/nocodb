d3c2ed036b6b47c2530a09aa2e27db03c1f89426
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-06-19 16:19:15 +0530

    fix: auto import

--- packages/nc-gui/components/n/Select/Field.vue
+++ packages/nc-gui/components/n/Select/Field.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { type ColumnType, isSystemColumn } from 'nocodb-sdk'
-import NIconField from '../Icon/Field.vue'
 import type { NSelectProps } from './types'
 const props = withDefaults(
   defineProps<NSelectProps & { tableId?: string; forceLoadTableFields?: boolean; filterColumn?: (t: ColumnType) => boolean }>(),

e89c532af6bf38cc8bba8ef9f39225069b585ce6
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-06-20 13:36:38 +0000

    feat: base schema cache

---
+++ packages/nocodb/src/helpers/scriptHelper.ts
@@ -0,0 +1,17 @@
+import Noco from '~/Noco';
+
+export async function getBaseSchema(
+  _baseId: string,
+  _workspaceId: string,
+  _ncMeta = Noco.ncMeta,
+) {
+  // Not Implemented
+}
+
+export async function cleanBaseSchemaCacheForBase(_baseId: string) {
+  // Not Implemented
+}
+
+export async function cleanBaseSchemaCacheForWorkspace(_workspaceId: string) {
+  // Not Implemented
+}

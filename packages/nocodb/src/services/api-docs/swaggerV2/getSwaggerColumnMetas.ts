051da3bbed074f6e740cebc106377f832c52c72c
Pranav C <pranavxc@gmail.com>
2025-06-27 01:12:49 +0530

    fix: add missing type extraction for number, boolean and json types
    re #11718

--- packages/nocodb/src/services/api-docs/swaggerV2/getSwaggerColumnMetas.ts
+++ packages/nocodb/src/services/api-docs/swaggerV2/getSwaggerColumnMetas.ts
@@ -34,8 +34,8 @@ export default async (
             }
           }
           break;
-        case UITypes.Formula:
         case UITypes.Lookup:
+        case UITypes.Formula:
           field.type = 'object';
           break;
         case UITypes.Rollup:
@@ -48,12 +48,44 @@ export default async (
             $ref: `#/components/schemas/Attachment`,
           };
           break;
-        case UITypes.LastModifiedTime:
+        case UITypes.User:
+        case UITypes.Collaborator:
+        case UITypes.CreatedBy:
+        case UITypes.LastModifiedBy:
+          field.type = 'object';
+          break;
+        case UITypes.Date:
+        case UITypes.DateTime:
+        case UITypes.Time:
         case UITypes.CreatedTime:
+        case UITypes.LastModifiedTime:
+        case UITypes.SingleLineText:
+        case UITypes.LongText:
+        case UITypes.Email:
+        case UITypes.PhoneNumber:
+        case UITypes.URL:
+          field.virtual = false;
           field.type = 'string';
           break;
-        case UITypes.LastModifiedBy:
-        case UITypes.CreatedBy:
+        case UITypes.Checkbox:
+          field.virtual = false;
+          field.type = 'boolean';
+          break;
+        case UITypes.Number:
+        case UITypes.Decimal:
+        case UITypes.Currency:
+        case UITypes.Percent:
+        case UITypes.Duration:
+        case UITypes.Rating:
+        case UITypes.Year:
+        case UITypes.Count:
+        case UITypes.ID:
+        case UITypes.Order:
+          field.virtual = false;
+          field.type = 'number';
+          break;
+        case UITypes.JSON:
+          field.virtual = false;
           field.type = 'object';
           break;
         default:

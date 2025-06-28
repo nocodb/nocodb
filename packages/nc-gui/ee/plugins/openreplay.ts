0d9d67fe7d7778ac1e6e1ac8119abc1609d80ed2
Pranav C <pranavxc@gmail.com>
2025-06-29 04:07:21 +0530

    refactor: remove posthog session capturing and start session capturing only after login

--- packages/nc-gui/ee/plugins/openreplay.ts
+++ packages/nc-gui/ee/plugins/openreplay.ts
@@ -6,40 +6,43 @@ export default defineNuxtPlugin(() => {
   const { appInfo, user } = useGlobal()
 
   watch(
-    [() => appInfo.value?.isCloud, () => appInfo.value?.isOnPrem],
-    ([isCloud, isOnPrem]) => {
-      if (isCloud && !isOnPrem) {
-        const script = document.createElement('script')
-        script.src = 'https://cdn.nocodb.com/lib/or.min.js'
-        script.async = true
-        script.onload = () => {
-          tracker = new (window as any).OpenReplay({
-            // TODO: make these part of appInfo
-            projectKey: 'WX6JlrfCDKS1uuuzhbYm',
-            ingestPoint: 'https://opr.nocodb.com/ingest',
-            resourceBaseHref: 'https://cdn.nocodb.com',
-            inlineCss: 3,
-          })
+    [() => appInfo.value?.isCloud, () => appInfo.value?.isOnPrem, () => user.value?.email],
+    ([isCloud, isOnPrem, email]) => {
+      // skip if on-prem
+      if (isOnPrem) {
+        return
+      }
 
-          tracker.start()
+      // skip if not cloud
+      if (!isCloud) {
+        return
+      }
 
-          if (user.value?.email) {
-            tracker.setUserID(user.value?.email)
-          }
+      // skip if user email is not available(not logged in)
+      if (!email) {
+        return
+      }
+
+      const script = document.createElement('script')
+      script.src = 'https://cdn.nocodb.com/lib/or.min.js'
+      script.async = true
+      script.onload = () => {
+        tracker = new (window as any).OpenReplay({
+          // TODO: make these part of appInfo
+          projectKey: 'WX6JlrfCDKS1uuuzhbYm',
+          ingestPoint: 'https://opr.nocodb.com/ingest',
+          resourceBaseHref: 'https://cdn.nocodb.com',
+          inlineCss: 3,
+        })
+
+        tracker.start()
+        if ((window as any).ncClientId) {
+          tracker.setUserID((window as any).ncClientId)
         }
+        ;(window as any).orTracker = tracker
+
         document.head.appendChild(script)
       }
-    },
-    {
-      immediate: true,
-    },
-  )
-
-  watch(
-    () => user.value?.email,
-    (email) => {
-      if (!tracker) return
-      tracker?.setUserID(email || (window as any).ncClientId)
     },
     {
       immediate: true,

5370b62a770492c5f56968389954b26d3f77c4de
Pranav C <pranavxc@gmail.com>
2025-06-29 03:20:29 +0530

    feat: set email id

--- packages/nc-gui/ee/plugins/openreplay.ts
+++ packages/nc-gui/ee/plugins/openreplay.ts
@@ -5,35 +5,44 @@ export default defineNuxtPlugin(() => {
   let tracker: any
   const { appInfo, user } = useGlobal()
 
-  watch([() => appInfo.value?.isCloud, () => appInfo.value?.isOnPrem], ([isCloud, isOnPrem]) => {
-    if (isCloud && !isOnPrem) {
-      const script = document.createElement('script')
-      script.src = 'https://cdn.nocodb.com/lib/or.min.js'
-      script.async = true
-      script.onload = () => {
-        tracker = new (window as any).OpenReplay({
-          // TODO: make these part of appInfo
-          projectKey: 'WX6JlrfCDKS1uuuzhbYm',
-          ingestPoint: 'https://opr.nocodb.com/ingest',
-          resourceBaseHref: 'https://cdn.nocodb.com',
-          inlineCss: 3,
-        })
+  watch(
+    [() => appInfo.value?.isCloud, () => appInfo.value?.isOnPrem],
+    ([isCloud, isOnPrem]) => {
+      if (isCloud && !isOnPrem) {
+        const script = document.createElement('script')
+        script.src = 'https://cdn.nocodb.com/lib/or.min.js'
+        script.async = true
+        script.onload = () => {
+          tracker = new (window as any).OpenReplay({
+            // TODO: make these part of appInfo
+            projectKey: 'WX6JlrfCDKS1uuuzhbYm',
+            ingestPoint: 'https://opr.nocodb.com/ingest',
+            resourceBaseHref: 'https://cdn.nocodb.com',
+            inlineCss: 3,
+          })
 
-        tracker.start()
+          tracker.start()
 
-        // if ((window as any).ncClientId) {
-        //   tracker.setUserID((window as any).ncClientId)
-        // }
+          if (user.value?.email) {
+            tracker.setUserID(user.value?.email)
+          }
+        }
+        document.head.appendChild(script)
       }
-      document.head.appendChild(script)
-    }
-  })
+    },
+    {
+      immediate: true,
+    },
+  )
 
   watch(
     () => user.value?.email,
     (email) => {
       if (!tracker) return
       tracker?.setUserID(email || (window as any).ncClientId)
+    },
+    {
+      immediate: true,
     },
   )
 })

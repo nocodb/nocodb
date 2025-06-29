0e8548a92e6284dd7550a39f70ba4720d9385e0d
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-06-29 17:17:58 +0530

    fix(nc-gui): role badge width issue

--- packages/nc-gui/components/roles/Badge.vue
+++ packages/nc-gui/components/roles/Badge.vue
@@ -46,45 +46,45 @@ const roleProperties = computed(() => {
 </script>
 
 <template>
-  <div
+  <NcTooltip
+    :disabled="!showTooltip"
     class="flex items-start rounded-md w-[fit-content] nc-role-badge"
     :class="{
       'cursor-pointer': clickableRef,
     }"
   >
-    <NcTooltip :disabled="!showTooltip">
-      <template #title> {{ $t(`objects.roleType.${roleProperties.label}`) }}</template>
+    <template #title> {{ $t(`objects.roleType.${roleProperties.label}`) }}</template>
 
-      <NcBadge class="!px-2 w-full" :class="ncBadgeClass" :color="roleProperties.color" :border="borderRef" :size="sizeSelect">
-        <div
-          class="badge-text w-full flex items-center justify-between gap-2"
-          :class="{
-            'text-purple-700': roleProperties.color === 'purple',
-            'text-blue-700': roleProperties.color === 'blue',
-            'text-green-700': roleProperties.color === 'green',
-            'text-orange-700': roleProperties.color === 'orange',
-            'text-yellow-700': roleProperties.color === 'yellow',
-            'text-red-700': roleProperties.color === 'red',
-            'text-maroon-700': roleProperties.color === 'maroon',
-            'text-gray-400': !roleProperties.color === 'grey',
-            'text-gray-300': !roleProperties.color,
-            sizeSelect,
-          }"
-        >
-          <div class="flex items-center gap-2">
-            <GeneralIcon v-if="showIcon" :icon="roleProperties.icon" />
-            <span v-if="!iconOnly" class="flex whitespace-nowrap">
-              {{ $t(`objects.roleType.${roleProperties.label}`) }}
-            </span>
-          </div>
-          <GeneralIcon v-if="clickableRef" icon="arrowDown" class="flex-none" />
+    <NcBadge class="!px-2 w-full" :class="ncBadgeClass" :color="roleProperties.color" :border="borderRef" :size="sizeSelect">
+      <div
+        class="badge-text w-full flex items-center justify-between gap-2"
+        :class="{
+          'text-purple-700': roleProperties.color === 'purple',
+          'text-blue-700': roleProperties.color === 'blue',
+          'text-green-700': roleProperties.color === 'green',
+          'text-orange-700': roleProperties.color === 'orange',
+          'text-yellow-700': roleProperties.color === 'yellow',
+          'text-red-700': roleProperties.color === 'red',
+          'text-maroon-700': roleProperties.color === 'maroon',
+          'text-gray-400': !roleProperties.color === 'grey',
+          'text-gray-300': !roleProperties.color,
+          sizeSelect,
+        }"
+      >
+        <div class="flex items-center gap-2">
+          <GeneralIcon v-if="showIcon" :icon="roleProperties.icon" />
+          <span v-if="!iconOnly" class="flex whitespace-nowrap">
+            {{ $t(`objects.roleType.${roleProperties.label}`) }}
+          </span>
         </div>
-      </NcBadge>
-    </NcTooltip>
+        <GeneralIcon v-if="clickableRef" icon="arrowDown" class="flex-none" />
+      </div>
+    </NcBadge>
+
     <!--
     <a-tooltip v-if="inheritRef" placement="bottom">
       <div class="text-gray-400 text-xs p-1 rounded-md">Workspace Role</div>
     </a-tooltip>
     -->
-  </div>
+  </NcTooltip>
 </template>

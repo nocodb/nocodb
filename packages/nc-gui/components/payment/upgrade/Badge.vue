a9e4d62344e0e018606d89ea31f406a6a9704c18
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-06-30 13:58:02 +0000

    fix(nc-gui): add missing limitOrFeature

--- packages/nc-gui/components/payment/upgrade/Badge.vue
+++ packages/nc-gui/components/payment/upgrade/Badge.vue
@@ -7,18 +7,29 @@ import { PlanTitles } from 'nocodb-sdk'
 interface Props {
   /** Required plan to access new feature */
   planTitle?: PlanTitles
-  /** Feature to check */
-  feature: PlanFeatureTypes
+  /** Feature to check and show upgrade badge if not available */
+  feature?: PlanFeatureTypes
+  /**
+   * If feature is not provided, then it's important to provide limitOrFeature to send this info to backend for upgrade request
+   */
+  limitOrFeature?: PlanFeatureTypes | PlanLimitTypes
   /** Title to show in upgrade modal */
-  title: string
+  title?: string
   /** Content to show in upgrade modal */
   content: string
   /** Callback will be triggered on click upgrade plan modal buttons or close modal */
   callback?: (type: 'ok' | 'cancel') => void
+
+  disabled?: boolean
+  removeClick?: boolean
+  featureEnabledCallback?: () => boolean
+  onClickCallback?: () => void
+  size?: 'xs' | 'sm' | 'md' | 'lg'
 }
 
 withDefaults(defineProps<Props>(), {
   planTitle: PlanTitles.PLUS,
+  size: 'sm',
 })
 </script>
 

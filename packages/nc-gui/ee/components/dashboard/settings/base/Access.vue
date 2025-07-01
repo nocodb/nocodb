6a351bd4e7c10d92c3676d7dd155bcbcae735e58
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-07-01 17:01:13 +0530

    fix(nc-gui): add base access tab in base settings

---
+++ packages/nc-gui/ee/components/dashboard/settings/base/Access.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const { t } = useI18n()
+
+const { blockPrivateBases, showUpgradeToUsePrivateBases } = useEeConfig()
+</script>
+
+<template>
+  <div data-testid="nc-settings-subtab-visibility" class="item-card flex flex-col w-full">
+    <div class="flex items-center justify-between">
+      <div class="text-nc-content-gray-emphasis font-semibold text-lg">
+        {{ $t('general.baseAccess') }}
+      </div>
+      <div>
+        <!-- Todo: @rameshmane7218 update link  -->
+        <a
+          href="https://nocodb.com/docs/product-docs/bases/base-access"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="!text-gray-700 text-sm !no-underline !hover:underline"
+        >
+          {{ $t('title.docs') }}
+        </a>
+      </div>
+    </div>
+
+    <div class="text-nc-content-gray-subtle2 mt-2 leading-5">
+      {{ $t('title.baseAccessTabSubtext') }}
+    </div>
+  </div>
+</template>

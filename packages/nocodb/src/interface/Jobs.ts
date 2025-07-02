08242dd7272ed3d212a628dd9b679a7356dd6293
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-07-02 17:15:39 +0530

    fix(nocodb): remove unwanted code

--- packages/nocodb/src/interface/Jobs.ts
+++ packages/nocodb/src/interface/Jobs.ts
@@ -184,7 +184,6 @@ export interface DataExportJobData extends JobData {
   viewId: string;
   exportAs: 'csv' | 'json' | 'xlsx';
   ncSiteUrl: string;
-  password?: string;
 }
 
 export interface ThumbnailGeneratorJobData extends JobData {

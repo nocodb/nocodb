9b4bf312d0097bb0951002bb048a4455ff652b1f
Pranav C <pranavxc@gmail.com>
2025-07-03 23:54:40 +0530

    refactor: icon spacing

--- packages/nc-gui/ee/components/project/PrivateOverlay.vue
+++ packages/nc-gui/ee/components/project/PrivateOverlay.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nc-smartsheet-access-restrict-overlay">
     <div class="flex flex-col gap-2 flex-col items-center">
-      <GeneralIcon icon="ncLock" class="mb-4 w-5 h-5 text-gray-700" />
+      <GeneralIcon icon="ncLock" class="mb-1 w-5 h-5 text-gray-700" />
       <h3 class="my-0 text-heading3">{{ $t('title.accessRestricted') }}</h3>
       <span class="text-gray-800 font-weight-regular">{{ $t('msg.privateBaseAccessRestricted') }}</span>
     </div>

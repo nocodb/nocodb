40bff8868f61216d1e32a99f003bd813fecfe793
mertmit <mertmit99@gmail.com>
2025-07-08 14:15:30 +0000

    fix: avoid unnecessary step

--- tests/playwright/pages/Dashboard/common/LeftSidebar/index.ts
+++ tests/playwright/pages/Dashboard/common/LeftSidebar/index.ts
@@ -62,8 +62,10 @@ export class LeftSidebarPage extends BasePage {
     title = isEE() ? title : `nc-${context.workerId}-${title}`;
     await this.btn_newProject.click();
 
-    await this.rootPage.locator('.nc-create-base').waitFor();
-    await this.rootPage.locator('.nc-create-base').click();
+    if (isEE()) {
+      await this.rootPage.locator('.nc-create-base').waitFor();
+      await this.rootPage.locator('.nc-create-base').click();
+    }
 
     await this.rootPage.locator('.ant-modal-content:has-text(" Create Base")').waitFor();
     await this.rootPage.locator('.ant-modal-content:has-text(" Create Base")').locator('input').fill(title);

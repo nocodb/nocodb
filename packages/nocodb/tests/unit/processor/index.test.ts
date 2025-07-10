2ec196f5484b761bac18cde93a2868273b6694a4
Fendy Heryanto <fendy@nocodb.com>
2025-07-10 09:52:14 +0700

    add duplicate model test

--- packages/nocodb/tests/unit/processor/index.test.ts
+++ packages/nocodb/tests/unit/processor/index.test.ts
@@ -1,8 +1,10 @@
 import 'mocha';
 import duplicateColumnTest from './duplicate/duplicate-column.test';
+import duplicateModelTest from './duplicate/duplicate-model.test';
 
 function _processorTests() {
   duplicateColumnTest();
+  duplicateModelTest();
 }
 
 export function processorTests() {

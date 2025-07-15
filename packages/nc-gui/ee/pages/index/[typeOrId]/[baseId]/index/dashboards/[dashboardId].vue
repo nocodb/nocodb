d72d905a93b5dfc987fbeb2dbc7924cf75f0ddb9
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-07-15 12:39:09 +0530

    fix: warning interaction on dashboard leave while editing

--- packages/nc-gui/ee/pages/index/[typeOrId]/[baseId]/index/dashboards/[dashboardId].vue
+++ packages/nc-gui/ee/pages/index/[typeOrId]/[baseId]/index/dashboards/[dashboardId].vue
@@ -1,4 +1,57 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { NavigationGuardNext, RouteLocationNormalizedLoadedGeneric } from 'vue-router'
+
+const { t } = useI18n()
+
+const dashboardStore = useDashboardStore()
+
+const { isEditingDashboard } = storeToRefs(dashboardStore)
+
+const confirmUnsavedChangesBeforeLeaving = (from: RouteLocationNormalizedLoadedGeneric, next: NavigationGuardNext) => {
+  if (!isEditingDashboard.value) {
+    next()
+    return
+  }
+
+  const isOpen = ref(true)
+
+  const { close } = useDialog(resolveComponent('NcModalConfirm'), {
+    'visible': isOpen,
+    'title': t('msg.info.leaveWithoutFinishing'),
+    'content': t('msg.info.leaveWithoutFinishingConfirmation'),
+    'okText': t('labels.keepEditing'),
+    'cancelText': t('labels.leave'),
+    'onCancel': () => {
+      isEditingDashboard.value = false
+      isOpen.value = false
+      close(1000)
+      next()
+    },
+    'onOk': () => {
+      isOpen.value = false
+      close(1000)
+      next(false) // Cancel navigation
+    },
+    'update:visible': (visible: boolean) => {
+      if (!visible) {
+        close(1000)
+        next(false) // Cancel navigation if dialog is closed without action
+      }
+    },
+    'showIcon': false,
+    'keyboard': false,
+    'maskClosable': false,
+  })
+}
+
+onBeforeRouteLeave((_to, from, next) => {
+  confirmUnsavedChangesBeforeLeaving(from, next)
+})
+
+onBeforeRouteUpdate((_to, from, next) => {
+  confirmUnsavedChangesBeforeLeaving(from, next)
+})
+</script>
 
 <template>
   <div class="flex flex-col h-full">

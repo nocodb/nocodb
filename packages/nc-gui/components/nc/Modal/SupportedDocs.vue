18c889c82d1d8aecc05f424c35c00ef312ff201f
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-07-21 21:03:02 +0530

    feat(nc-gui): create supported docs component

---
+++ packages/nc-gui/components/nc/Modal/SupportedDocs.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+interface Props {
+  docs: {
+    title: string
+    href: string
+  }[]
+}
+
+withDefaults(defineProps<Props>(), {
+  docs: () => [],
+})
+</script>
+
+<template>
+  <div class="w-full flex flex-col gap-3">
+    <div class="text-sm text-gray-800 font-semibold">Support Docs</div>
+
+    <div>
+      <div v-for="(doc, idx) of docs" :key="idx" class="flex items-center gap-1">
+        <div class="h-7 w-7 flex items-center justify-center">
+          <GeneralIcon icon="bookOpen" class="flex-none w-4 h-4 text-gray-600" />
+        </div>
+        <a
+          :href="doc.href"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="!text-gray-700 text-sm !no-underline !hover:underline"
+        >
+          {{ doc.title }}
+        </a>
+      </div>
+    </div>
+  </div>
+</template>

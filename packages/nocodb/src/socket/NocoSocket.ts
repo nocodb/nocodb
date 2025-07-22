efa0e309995feb98337b29dc0b90058ad0955334
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-07-22 19:25:55 +0530

    fix: type errors

---
+++ packages/nocodb/src/socket/NocoSocket.ts
@@ -0,0 +1,7 @@
+export default class NocoSocket {
+  public static ioServer;
+
+  public static handleConnection(..._args: unknown[]) {}
+
+  public static broadcastDataEvent(..._args: unknown[]) {}
+}

72e20dda5984a167a3915e8462d25b25dcb9196f
Pranav C <pranavxc@gmail.com>
2025-07-24 21:31:17 +0530

    fix: type correction

--- packages/nocodb/src/ee/models/ApiToken.ts
+++ packages/nocodb/src/ee/models/ApiToken.ts
@@ -3,7 +3,9 @@ import SSOClient from './SSOClient';
 import Noco from '~/Noco';
 
 export default class ApiToken extends ApiTokenCE {
-  async getExtraForUserPayload(ncMeta = Noco.ncMeta) {
+  async getExtraForUserPayload(
+    ncMeta = Noco.ncMeta,
+  ): Promise<void | Record<string, any>> {
     if (!this.fk_sso_client_id) return;
 
     const ssoClient = await SSOClient.get(this.fk_sso_client_id, ncMeta);
@@ -17,6 +19,6 @@ export default class ApiToken extends ApiTokenCE {
       workspace_id: ssoClient.fk_workspace_id,
       sso_client_id: ssoClient.id,
       sso_client_type: ssoClient.type,
-    };
+    } as Record<string, any>;
   }
 }

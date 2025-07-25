cefb0cccec63f179e0c672b8138f6fbc51b1524d
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-07-25 06:21:00 +0000

    fix(nc-gui): add minAccessRole in extension manifest

--- packages/nc-gui/composables/usePlugin/interface.ts
+++ packages/nc-gui/composables/usePlugin/interface.ts
@@ -33,10 +33,15 @@ export interface ExtensionManifest {
   onPrem?: boolean
   /**
    * The minimum access role required to access the extension.
-   * @default ProjectRoles.CREATOR
+   * @default 'creator'
    * Todo: @rameshmane7218
    */
-  minAccessRole?: ProjectRoles
+  minAccessRole?: 'owner' | 'creator' | 'editor' | 'commenter' | 'viewer' | 'no-access'
+  /**
+   * `showAsBeta` released as a beta extension.
+   * We will add beta tab in market, details and extension places.
+   */
+  showAsBeta?: boolean
 }
 
 export interface ScriptManifest {

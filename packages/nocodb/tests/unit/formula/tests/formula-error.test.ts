3b30075486bede070442012331849bb994616943
Fendy Heryanto <fendy@nocodb.com>
2025-07-28 14:36:54 +0700

    skip another test if not pg

--- packages/nocodb/tests/unit/formula/tests/formula-error.test.ts
+++ packages/nocodb/tests/unit/formula/tests/formula-error.test.ts
@@ -1,8 +1,9 @@
 import 'mocha';
-import { UITypes } from 'nocodb-sdk';
+import { ClientType, UITypes } from 'nocodb-sdk';
 import { expect } from 'chai';
 import { initInitialModel } from '../initModel';
 import { createColumn } from '../../factory/column';
+import { Source } from '../../../../src/models';
 
 function formulaErrorTests() {
   let _setup;
@@ -15,6 +16,7 @@ function formulaErrorTests() {
   let _tables;
   let _view;
   let _baseModelSql;
+  let _source;
 
   beforeEach(async function () {
     const setup = await initInitialModel();
@@ -23,10 +25,16 @@ function formulaErrorTests() {
     _ctx = setup.ctx;
     _base = setup.base;
     _tables = setup.tables;
+    const source = await Source.get(setup.ctx, setup.tables.table1.source_id);
+    _source = source;
   });
 
   // issue #6075
   it('will create a formula referencing formula at table1', async () => {
+    // skip if not pg
+    if (_source.type !== ClientType.PG) {
+      return;
+    }
     try {
       await createColumn(_context, _tables.table1, {
         title: 'formulaTitle',

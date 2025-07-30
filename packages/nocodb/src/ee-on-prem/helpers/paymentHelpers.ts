869365b9c7b0eb78c009cf2ba94c2b5b6a43d185
Pranav C <pranavxc@gmail.com>
2025-07-30 18:19:05 +0530

    feat: override getFeature function as well

--- packages/nocodb/src/ee-on-prem/helpers/paymentHelpers.ts
+++ packages/nocodb/src/ee-on-prem/helpers/paymentHelpers.ts
@@ -1,7 +1,13 @@
 import { PlanTitles } from 'nocodb-sdk';
+import type { PlanFeatureTypes } from 'nocodb-sdk';
 import type { Workspace } from '~/models';
 import Noco from '~/Noco';
 import { LicenseService } from '~/services/license/license.service';
+import {
+  EnterprisePlan,
+  EnterpriseStarterPlan,
+  GenericFeatures,
+} from '~/models/Plan';
 
 export * from 'src/ee/helpers/paymentHelpers';
 
@@ -26,4 +32,18 @@ export async function getActivePlanAndSubscription(
   const plan = getOnPremPlan();
 
   return { plan };
+}
+
+export async function getFeature(
+  type: PlanFeatureTypes,
+  workspaceOrId?: string | Workspace,
+  ncMeta = Noco.ncMeta,
+) {
+  const plan = getOnPremPlan();
+
+  if (plan === PlanTitles.ENTERPRISE) {
+    return EnterprisePlan.meta?.[type] || GenericFeatures[type];
+  }
+
+  return EnterpriseStarterPlan.meta?.[type] || GenericFeatures[type];
 }

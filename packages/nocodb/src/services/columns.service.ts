3430b2044c9bff75d9f8f3fc9479f13fe2298743
Fendy Heryanto <fendy@nocodb.com>
2025-08-01 03:04:19 +0000

    better swagger validation for v3

--- packages/nocodb/src/services/columns.service.ts
+++ packages/nocodb/src/services/columns.service.ts
@@ -2099,7 +2099,7 @@ export class ColumnsService implements IColumnsService {
         break;
 
       case UITypes.QrCode:
-        validateParams(['fk_qr_value_column_id'], param.column);
+        validateParams(['fk_qr_value_column_id'], param.column, context);
 
         savedColumn = await Column.insert(context, {
           ...colBody,
@@ -2107,7 +2107,7 @@ export class ColumnsService implements IColumnsService {
         });
         break;
       case UITypes.Barcode:
-        validateParams(['fk_barcode_value_column_id'], param.column);
+        validateParams(['fk_barcode_value_column_id'], param.column, context);
 
         savedColumn = await Column.insert(context, {
           ...colBody,
@@ -3648,7 +3648,7 @@ export class ColumnsService implements IColumnsService {
   ) {
     let savedColumn: Column;
 
-    validateParams(['parentId', 'childId', 'type'], param.column);
+    validateParams(['parentId', 'childId', 'type'], param.column, context);
 
     const reuse = param.reuse ?? {};
 

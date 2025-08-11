9072eabab1d449086944796fbd6cd116f9264712
mertmit <mertmit99@gmail.com>
2025-08-11 12:59:39 +0000

    feat: base ops

--- packages/nocodb/src/services/bases.service.ts
+++ packages/nocodb/src/services/bases.service.ts
@@ -4,6 +4,7 @@ import * as DOMPurify from 'isomorphic-dompurify';
 import { customAlphabet } from 'nanoid';
 import {
   AppEvents,
+  EventType,
   extractRolesObj,
   IntegrationsType,
   OrgUserRoles,
@@ -29,6 +30,7 @@ import { MetaService } from '~/meta/meta.service';
 import { MetaTable, RootScopes } from '~/utils/globals';
 import { TablesService } from '~/services/tables.service';
 import { stringifyMetaProp } from '~/utils/modelUtils';
+import NocoSocket from '~/socket/NocoSocket';
 
 const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz_', 4);
 
@@ -128,6 +130,22 @@ export class BasesService {
       req: param.req,
       context,
     });
+
+    NocoSocket.broadcastEvent(
+      context,
+      {
+        event: EventType.META_EVENT,
+        payload: {
+          action: 'base_update',
+          payload: {
+            ...base,
+            ...data,
+          },
+        },
+        workspaceEvent: true,
+      },
+      context.socket_id,
+    );
 
     return result;
   }

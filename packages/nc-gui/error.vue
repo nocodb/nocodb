19e957dc59eb6756a784591ddfb056002750259f
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-08-12 06:38:42 +0000

    fix(nc-gui): use moscot icon in 404 page

--- packages/nc-gui/error.vue
+++ packages/nc-gui/error.vue
@@ -4,6 +4,12 @@ withDefaults(defineProps<{ error: Record<string, any> }>(), { error: () => ({}) 
 
 <template>
   <GeneralPageDoesNotExist action-button-class="mt-4">
+    <template v-if="error.statusCode === 404" #icon>
+      <div class="flex min-h-[205px]">
+        <img src="~assets/img/placeholder/no-search-result-found.png" class="!w-[264px] flex-none" alt="404 - Page Not Found" />
+      </div>
+    </template>
+
     <template #title>
       {{
         error.statusCode === 404 ? '404 - Page Not Found' : error.statusMessage ?? error.message ?? `Error ${error.statusCode}`

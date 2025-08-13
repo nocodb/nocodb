4fce06779fbfbdd8135c2bebbc6ce658e5971029
Pranav C <pranavxc@gmail.com>
2025-08-13 06:20:35 +0000

    feat: API v3 - workspace-user - keep only required props

--- packages/nocodb/src/models/User.ts
+++ packages/nocodb/src/models/User.ts
@@ -76,7 +76,7 @@ export default class User implements UserType {
       'meta',
     ]);
 
-    // // Todo: @rameshmane7218 uncomment this part in onboarding flow pr 
+    // // Todo: @rameshmane7218 uncomment this part in onboarding flow pr
     // // Set is_new_user to true for new users if not explicitly set
     // if (insertObj.is_new_user === undefined) {
     //   insertObj.is_new_user = true;

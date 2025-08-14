9756a49e09bfadfd8e086ade376a35a6a3c2137f
mertmit <mertmit99@gmail.com>
2025-08-14 23:08:09 +0000

    fix: skip base_id

--- packages/nocodb/src/modules/jobs/jobs/export-import/export.service.ts
+++ packages/nocodb/src/modules/jobs/jobs/export-import/export.service.ts
@@ -616,7 +616,6 @@ export class ExportService {
 
           serializedPermissions.push({
             id: idMap.get(permission.id),
-            base_id: idMap.get(permission.base_id),
             entity: permission.entity,
             entity_id: idMap.get(permission.entity_id),
             permission: permission.permission,

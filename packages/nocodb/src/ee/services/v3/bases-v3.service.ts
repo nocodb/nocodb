7b6bb82a432b95c2a242e610b97d36403d2e13c2
Fendy Heryanto <fendy@nocodb.com>
2025-08-14 12:24:15 +0700

    base type private / default

--- packages/nocodb/src/ee/services/v3/bases-v3.service.ts
+++ packages/nocodb/src/ee/services/v3/bases-v3.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { NcApiVersion } from 'nocodb-sdk';
 import { BasesV3Service as BasesV3ServiceCE } from 'src/services/v3/bases-v3.service';
 import type { NcContext } from '~/interface/config';
+import { getFeature, PlanFeatureTypes } from '~/ee/helpers/paymentHelpers';
+import { NcError } from '~/helpers/ncError';
+import { isOnPrem } from '~/utils';
 import { Base } from '~/models';
 import { BasesService } from '~/services/bases.service';
 
@@ -22,7 +26,36 @@ export class BasesV3Service extends BasesV3ServiceCE {
       param.workspaceId,
       param.user.id,
     );
-
     return bases;
+  }
+
+  override async parseBaseRequest(
+    context: { workspace_id: string },
+    base: any,
+  ) {
+    if (base.type === 'private') {
+      if (
+        !(await getFeature(
+          PlanFeatureTypes.FEATURE_PRIVATE_BASES,
+          context.workspace_id,
+        ))
+      ) {
+        NcError.get({
+          api_version: NcApiVersion.V3,
+        }).featureNotSupported({
+          feature: PlanFeatureTypes.FEATURE_PRIVATE_BASES,
+          isOnPrem: isOnPrem,
+        });
+      }
+    }
+    return (
+      base.type && base.type === 'private'
+        ? {
+            default_role: 'no-access',
+          }
+        : base.type === 'default'
+        ? { default_role: null }
+        : {}
+    ) as any;
   }
 }

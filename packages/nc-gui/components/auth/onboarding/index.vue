6d15b6b75433985f8e6fc2e558e816c4d2b84dec
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-08-16 12:25:25 +0000

    feat(nc-gui): add slide animation

--- packages/nc-gui/components/auth/onboarding/index.vue
+++ packages/nc-gui/components/auth/onboarding/index.vue
@@ -11,6 +11,17 @@ const onSelectRef: VNodeRef = (el) => {
   }
 }
 
+const transitionName = ref<'slide-left' | 'slide-right' | null>(null)
+
+watch(
+  () => stepper.index.value,
+  (newIndex, oldIndex) => {
+    if (oldIndex === undefined) return
+
+    transitionName.value = newIndex > (oldIndex ?? 0) ? 'slide-left' : 'slide-right'
+  },
+)
+
 let timer: any
 watch(
   () => visibleQuestion.value.id,
@@ -43,9 +54,59 @@ onMounted(() => {
 <template>
   <AuthOnboardingLayout>
     <template #content>
-      <AuthOnboardingQuestion :question="visibleQuestion" :question-index="stepper.index.value" :ref="onSelectRef" />
+      <div class="relative w-full">
+        <Transition :name="transitionName ?? ''" mode="out-in">
+          <AuthOnboardingQuestion
+            :key="stepper.index.value"
+            :question="visibleQuestion"
+            :question-index="stepper.index.value"
+            :ref="onSelectRef"
+          />
+        </Transition>
+      </div>
     </template>
   </AuthOnboardingLayout>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.slide-left-enter-active,
+.slide-right-enter-active,
+.slide-left-leave-active,
+.slide-right-leave-active {
+  transition: all 0.3s ease;
+}
+
+.slide-left-enter-from {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.slide-left-enter-to {
+  opacity: 1;
+  transform: translateX(0);
+}
+.slide-left-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+.slide-left-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+
+.slide-right-enter-from {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+.slide-right-enter-to {
+  opacity: 1;
+  transform: translateX(0);
+}
+.slide-right-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+.slide-right-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
+}
+</style>

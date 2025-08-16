1842bbf3bbe25bbd910f99454364b67f19f3dc1d
Fendy Heryanto <fendy@nocodb.com>
2025-08-16 08:44:38 +0000

    working create simple

--- packages/nocodb/tests/unit/rest/tests/metaApiV3/index.test.ts
+++ packages/nocodb/tests/unit/rest/tests/metaApiV3/index.test.ts
@@ -1,5 +1,6 @@
 import baseTestV3 from './base.test';
 import tableTestV3 from './table.test';
+import viewTestV3 from './view.test';
 import columnTestV3 from './column.test';
 import baseUsersTestV3 from './baseUsers.test';
 import errorHandlingMetaTestsV3 from './error-handling/index.test';
@@ -9,6 +10,7 @@ export default function () {
   baseTestV3();
   baseUsersTestV3();
   tableTestV3();
+  viewTestV3();
   columnTestV3();
   errorHandlingMetaTestsV3();
   workspaceUsersTest();

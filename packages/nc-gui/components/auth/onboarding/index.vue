ddbdfb5e7e3590ed06a04011265c981e9aed3b91
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-08-16 12:25:25 +0000

    fix(nc-gui): revert some local changes

--- packages/nc-gui/components/auth/onboarding/index.vue
+++ packages/nc-gui/components/auth/onboarding/index.vue
@@ -40,15 +40,15 @@ onMounted(() => {
   onInitOnboardingFlow()
 })
 
-// useEventListener('beforeunload', (event) => {
-//   if (!showOnboardingFlow.value) return
+useEventListener('beforeunload', (event) => {
+  if (!showOnboardingFlow.value) return
 
-//   // Recommended
-//   event.preventDefault()
+  // Recommended
+  event.preventDefault()
 
-//   // Included for legacy support, e.g. Chrome/Edge < 119
-//   event.returnValue = ''
-// })
+  // Included for legacy support, e.g. Chrome/Edge < 119
+  event.returnValue = ''
+})
 </script>
 
 <template>

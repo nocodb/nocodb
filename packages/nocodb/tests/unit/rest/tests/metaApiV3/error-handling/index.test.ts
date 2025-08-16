f7c284b2a29cdd832759125e804d775e0b0cd55c
Fendy Heryanto <fendy@nocodb.com>
2025-08-16 02:20:07 +0000

    add skeleton for column test

--- packages/nocodb/tests/unit/rest/tests/metaApiV3/error-handling/index.test.ts
+++ packages/nocodb/tests/unit/rest/tests/metaApiV3/error-handling/index.test.ts
@@ -1,7 +1,9 @@
 import baseTestV3 from './base.test';
 import tableTestV3 from './table.test';
+import columnTestV3 from './column.test';
 
 export default function () {
   baseTestV3();
   tableTestV3();
+  columnTestV3();
 }

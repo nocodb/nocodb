98f55ae33aa9994c54961739ac4a8603418ecc7d
Pranav C <pranavxc@gmail.com>
2025-08-18 04:07:48 +0530

    refactor: cache related fix

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -6069,11 +6069,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
           (c.column_name &&
             (
               c.colOptions as TrackModificationsColumnOptions
-            )?.triggerColumnIds?.some(
-              (id) => updatedColIds.includes(id)
-            )
-          )
-        ),
+            )?.triggerColumnIds?.some((id) => updatedColIds.includes(id)))),
     );
 
     for (const lastModifiedTimeColumn of lastModifiedTimeColumns) {

2adfb98f219bafd375173265ee575a8564a29930
Fendy Heryanto <fendy@nocodb.com>
2025-08-18 16:46:53 +0700

    check plan on BE

--- packages/nocodb/src/ee/utils/duplicate-model.utils.ts
+++ packages/nocodb/src/ee/utils/duplicate-model.utils.ts
@@ -1,6 +1,10 @@
 import { DuplicateModelUtils as DuplicateModelUtilsCE } from 'src/utils/duplicate-model.utils';
+import { PlanFeatureTypes } from 'nocodb-sdk';
+import { getFeature } from '../helpers/paymentHelpers';
 import type { NcContext } from 'nocodb-sdk';
 import type { DuplicateModelJobData } from '~/interface/Jobs';
+import { NcError } from '~/helpers/ncError';
+import { isOnPrem } from '~/utils';
 
 export class DuplicateModelUtils extends DuplicateModelUtilsCE {
   static get _() {
@@ -11,6 +15,25 @@ export class DuplicateModelUtils extends DuplicateModelUtilsCE {
     options?: DuplicateModelJobData['options'],
   ) {
     if (options.targetBaseId && options.targetBaseId != context.base_id) {
+      if (!getFeature(PlanFeatureTypes.FEATURE_DUPLICATE_TABLE_TO_OTHER_BASE)) {
+        NcError.get(context).featureNotSupported({
+          feature: PlanFeatureTypes.FEATURE_DUPLICATE_TABLE_TO_OTHER_BASE,
+          isOnPrem,
+        });
+      }
+      if (options.targetWorkspaceId !== context.workspace_id) {
+        if (
+          !getFeature(
+            PlanFeatureTypes.FEATURE_DUPLICATE_TABLE_TO_OTHER_WORKSPACE,
+          )
+        ) {
+          NcError.get(context).featureNotSupported({
+            feature:
+              PlanFeatureTypes.FEATURE_DUPLICATE_TABLE_TO_OTHER_WORKSPACE,
+            isOnPrem,
+          });
+        }
+      }
       return {
         context: {
           ...context,

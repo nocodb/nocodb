814f8865be1db968f213410a7a447549092fc074
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-08-19 18:42:32 +0530

    fix: remove email apis

--- packages/nc-gui/composables/useBetaFeatureToggle.ts
+++ packages/nc-gui/composables/useBetaFeatureToggle.ts
@@ -174,15 +174,6 @@ const FEATURES = [
     isEngineering: true,
     isEE: true,
   },
-  {
-    id: 'email_apis',
-    title: 'Email APIs',
-    description: 'Send Emails via Scripts',
-    enabled: true,
-    version: 1,
-    isEngineering: true,
-    isEE: true,
-  },
 ] as const
 
 export const FEATURE_FLAG = Object.fromEntries(FEATURES.map((feature) => [feature.id.toUpperCase(), feature.id])) as Record<

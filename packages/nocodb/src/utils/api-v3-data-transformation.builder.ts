a2aee438a6dfa80e347e2df692e4a3ebd19397c7
Fendy Heryanto <fendy@nocodb.com>
2025-08-20 06:14:57 +0000

    change thousand_separator back to locale_string

--- packages/nocodb/src/utils/api-v3-data-transformation.builder.ts
+++ packages/nocodb/src/utils/api-v3-data-transformation.builder.ts
@@ -354,7 +354,7 @@ export const columnBuilder = builderGenerator<Column | ColumnType, FieldV3Type>(
       metaProps: ['meta'],
       mappings: {
         is12hrFormat: '12hr_format',
-        isLocaleString: 'thousand_separator',
+        isLocaleString: 'locale_string',
         richMode: 'rich_text',
         [LongTextAiMetaProp]: 'generate_text_using_ai',
         isDisplayTimezone: 'display_timezone',
@@ -570,7 +570,7 @@ export const columnV3ToV2Builder = builderGenerator<FieldV3Type, ColumnType>({
     metaProps: ['options', 'meta'],
     mappings: {
       '12hr_format': 'is12hrFormat',
-      thousand_separator: 'isLocaleString',
+      locale_string: 'isLocaleString',
       rich_text: 'richMode',
       display_timezone: 'isDisplayTimezone',
 

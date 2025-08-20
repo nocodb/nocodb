7e99abe7b10d39dd17d2e542d08b27bdda48fb39
Fendy Heryanto <fendy@nocodb.com>
2025-08-20 13:49:56 +0700

    fix import

--- packages/nocodb/src/services/v3/view-row-color-v3.service.ts
+++ packages/nocodb/src/services/v3/view-row-color-v3.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import type { NcContext, NcRequest, ViewRowColour } from 'nocodb-sdk';
+import type { NcContext, NcRequest, ViewRowColourV3Type } from 'nocodb-sdk';
 import type { MetaService } from '~/meta/meta.service';
 
 @Injectable()
@@ -8,7 +8,7 @@ export class ViewRowColorV3Service {
     _context: NcContext,
     _params: {
       viewId: string;
-      body: ViewRowColour;
+      body: ViewRowColourV3Type;
       req: NcRequest;
     },
     _ncMeta?: MetaService,

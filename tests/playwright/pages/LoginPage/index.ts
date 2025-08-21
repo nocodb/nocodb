017ed34cdfbe43e6df9c2cc5c98cc41b3afb930f
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-08-21 07:57:25 +0000

    fix(pw): skip onboarding flow if not available in signin step

--- tests/playwright/pages/LoginPage/index.ts
+++ tests/playwright/pages/LoginPage/index.ts
@@ -1,9 +1,13 @@
 import { Page } from '@playwright/test';
 import BasePage from '../Base';
+import { OnboardingFlowPage } from '../OnboardingFlowPage';
 
 export class LoginPage extends BasePage {
+  readonly onboardingFlowPage: OnboardingFlowPage;
+
   constructor(rootPage: Page) {
     super(rootPage);
+    this.onboardingFlowPage = new OnboardingFlowPage(rootPage);
   }
 
   prefixEmail(email: string) {
@@ -40,11 +44,13 @@ export class LoginPage extends BasePage {
     password,
     withoutPrefix,
     skipReload = false,
+    skipOnboardingFlow = true,
   }: {
     email: string;
     password: string;
     withoutPrefix?: boolean;
     skipReload?: boolean;
+    skipOnboardingFlow?: boolean;
   }) {
     if (!skipReload) await this.goto();
 
@@ -53,5 +59,14 @@ export class LoginPage extends BasePage {
     await this.fillEmail({ email, withoutPrefix });
     await this.fillPassword(password);
     await this.submit();
+
+    await this.rootPage.waitForLoadState('networkidle');
+
+    // It's possible that user can see onboarding flow after login if they have not attempted that after signup
+    if (skipOnboardingFlow) {
+      await this.onboardingFlowPage.skipOnboardingFlow();
+    } else {
+      await this.onboardingFlowPage.completeOnboardingFlow();
+    }
   }
 }

fc7d0da1221b2ce152bb7979b771e710bc95b9a2
Pranav C <pranavxc@gmail.com>
2025-08-23 07:34:57 +0000

    chore: lint

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -155,7 +155,7 @@ function prepareMetaUpdateQuery({
   knex: XKnex;
   colIds: string[];
   props: Record<string, unknown>;
-  metaColumn: column;
+  metaColumn: Column;
 }) {
   const jsonObjQuery = knex.raw('?::jsonb', JSON.stringify(props)).toString();
 
@@ -6340,7 +6340,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
           modifiedBy: cookie?.user?.id,
           modifiedTime: this.now(),
         },
-        metaColumn: column,
+        metaColumn,
       });
     }
 

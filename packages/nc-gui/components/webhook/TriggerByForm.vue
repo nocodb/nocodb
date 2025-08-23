39ef686bc2e094410ede35ada806f3599e6157ee
mertmit <mertmit99@gmail.com>
2025-08-23 17:09:51 +0300

    feat: webhook on specific form submitted
    Signed-off-by: mertmit <mertmit99@gmail.com>

---
+++ packages/nc-gui/components/webhook/TriggerByForm.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+interface Props {
+  tableId: string
+  triggerForm?: boolean
+  triggerFormId?: string
+}
+
+const props = defineProps<Props>()
+
+const viewStore = useViewsStore()
+
+const { viewsByTable } = storeToRefs(viewStore)
+
+const formOptions = computed(() => {
+  const views = viewsByTable.value.get(props.tableId) || []
+  return views
+    .filter((v) => v.type === 1)
+    .map((view) => ({
+      label: view.title,
+      value: view.id,
+    }))
+})
+
+const triggerForm = useVModel(props, 'triggerForm')
+const triggerFormId = useVModel(props, 'triggerFormId')
+
+const onChangeTriggerForm = (val: boolean) => {
+  if (!val) {
+    triggerFormId.value = undefined
+  }
+}
+
+onMounted(() => {
+  if (triggerFormId.value && formOptions.value.findIndex((o) => o.value === triggerFormId.value) === -1) {
+    triggerFormId.value = undefined
+  }
+  if (formOptions.value.length === 0) {
+    triggerForm.value = false
+  }
+})
+</script>
+
+<template>
+  <div class="w-full flex items-center justify-between h-[28px]">
+    <label class="cursor-pointer flex items-center" @click.prevent="triggerForm = !triggerForm">
+      <NcSwitch
+        :checked="triggerForm"
+        class="nc-check-box-trigger-field"
+        :disabled="formOptions.length === 0"
+        @change="onChangeTriggerForm"
+      >
+        <span class="!text-gray-700 font-semibold"> Trigger only when specific form submitted </span>
+      </NcSwitch>
+    </label>
+    <NcSelect
+      v-if="triggerForm"
+      v-model:value="triggerFormId"
+      :options="formOptions"
+      placeholder="Select a form view"
+      size="small"
+      class="w-50"
+    />
+  </div>
+</template>

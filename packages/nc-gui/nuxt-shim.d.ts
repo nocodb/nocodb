c052bf1c2bb2338c83fa412e3bb6076dd635f353
Pranav C <pranavxc@gmail.com>
2025-08-25 09:50:19 +0000

    refactor: onboarding

--- packages/nc-gui/nuxt-shim.d.ts
+++ packages/nc-gui/nuxt-shim.d.ts
@@ -12,7 +12,7 @@ declare module '#app' {
       emit: (event: string, data: any) => void
     }
     /** {@link import('./plugins/tele') Telemetry} Emit telemetry event */
-    $e: (event: string, data?: any) => void
+    $e: (event: string, data?: any, rootProps?: Record<string, any>) => void
     /** {@link import('./plugins/report') Error reporting} Error reporting */
     $report: (event: Error) => void
     $state: UseGlobalReturn

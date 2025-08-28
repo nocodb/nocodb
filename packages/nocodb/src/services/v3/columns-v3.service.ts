90fbd4d41353f1180c38a04f567deaf2b2fb192c
Fendy Heryanto <fendy@nocodb.com>
2025-08-28 16:44:23 +0700

    remove console.log

--- packages/nocodb/src/services/v3/columns-v3.service.ts
+++ packages/nocodb/src/services/v3/columns-v3.service.ts
@@ -106,7 +106,6 @@ export class ColumnsV3Service {
 
     // in payload id is required in existing implementation
     column.id = param.columnId;
-    console.log(processedColumnReq);
 
     await this.columnsService.columnUpdate(
       context,

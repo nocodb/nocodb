d97bf56b6d1cb4a6eb4ac8c645b660e9e45313ec
Fendy Heryanto <fendy@nocodb.com>
2025-09-04 17:13:15 +0700

    for safe operation, add an optional chain

--- packages/nocodb/src/modules/jobs/jobs/export-import/export.service.ts
+++ packages/nocodb/src/modules/jobs/jobs/export-import/export.service.ts
@@ -816,7 +816,7 @@ export class ExportService {
               (c) =>
                 isSystemColumn(c) &&
                 c.uidt === UITypes.LinkToAnotherRecord &&
-                c.colOptions.fk_related_model_id !== model.id,
+                c.colOptions?.fk_related_model_id !== model.id,
             )
             .map((c) => c.id)
         : model.columns.filter((c) => isSystemColumn(c)).map((c) => c.id);

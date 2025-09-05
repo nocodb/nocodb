7b627db76863e599a4cda719306bc8119f114a34
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-09-05 11:30:17 +0000

    fix(nc-gui): keep same ai text btn color as ai button

--- packages/nc-gui/components/smartsheet/column/LongTextOptions.vue
+++ packages/nc-gui/components/smartsheet/column/LongTextOptions.vue
@@ -239,8 +239,7 @@ watch(isPreviewEnabled, handleDisableSubmitBtn, {
         </NcTooltip>
         <div class="flex-1"></div>
 
-        <!-- Todo @rameshmane7218 remove hidden after enabling other integrations, hidden for now as we allow only nocoai -->
-        <div class="absolute right-0 hidden">
+        <div class="absolute right-0">
           <AiSettings
             v-model:fk-integration-id="vModel.fk_integration_id"
             v-model:model="vModel.model"

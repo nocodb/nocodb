373f9080b17d79e5c13f4141c8a7dbfe602c278d
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-09-06 08:11:54 +0000

    fix: gallery realtime

--- packages/nc-gui/components/smartsheet/Gallery.vue
+++ packages/nc-gui/components/smartsheet/Gallery.vue
@@ -352,7 +352,7 @@ const placeholderAboveHeight = computed(() => {
 const { width, height } = useWindowSize()
 
 watch(
-  [() => width.value, () => height.value, () => columnsPerRow.value, () => scrollContainerWidth.value],
+  [() => width.value, () => height.value, () => columnsPerRow.value, () => scrollContainerWidth.value, () => totalRows.value],
   () => {
     calculateSlices()
   },

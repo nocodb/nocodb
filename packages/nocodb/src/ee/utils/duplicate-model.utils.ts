74e2937bdbb3f65db88fe2ef496f2b685ca12cde
Fendy Heryanto <fendy@nocodb.com>
2025-09-06 09:09:49 +0700

    check for plan number of row before duplicate model

--- packages/nocodb/src/ee/utils/duplicate-model.utils.ts
+++ packages/nocodb/src/ee/utils/duplicate-model.utils.ts
@@ -1,10 +1,14 @@
 import { DuplicateModelUtils as DuplicateModelUtilsCE } from 'src/utils/duplicate-model.utils';
-import { PlanFeatureTypes } from 'nocodb-sdk';
-import { getFeature } from '../helpers/paymentHelpers';
+import { PlanFeatureTypes, PlanLimitTypes } from 'nocodb-sdk';
+import { UPDATE_WORKSPACE_STAT } from '../services/update-stats.service';
 import type { NcContext } from 'nocodb-sdk';
 import type { DuplicateModelJobData } from '~/interface/Jobs';
+import { getBaseModelSqlFromModelId } from '~/helpers/dbHelpers';
+import { checkLimit, getFeature } from '~/ee/helpers/paymentHelpers';
 import { NcError } from '~/helpers/ncError';
 import { isOnPrem } from '~/utils';
+import { ModelStat } from '~/models';
+import Noco from '~/Noco';
 
 export class DuplicateModelUtils extends DuplicateModelUtilsCE {
   static get _() {
@@ -49,5 +53,49 @@ export class DuplicateModelUtils extends DuplicateModelUtilsCE {
       };
     }
     return { context, isDifferent: false };
+  }
+
+  override async verifyTargetContext(
+    sourceContext: NcContext,
+    targetContext: NcContext,
+    modelId: string,
+    options?: DuplicateModelJobData['options'],
+  ): Promise<boolean> {
+    if (options.excludeData) {
+      return true;
+    }
+    const baseModel = await getBaseModelSqlFromModelId({
+      modelId,
+      context: sourceContext,
+    });
+    const count = await baseModel.count();
+    if (count === 0) {
+      return true;
+    }
+
+    const workspaceStats = await ModelStat.getWorkspaceSum(
+      targetContext.workspace_id,
+    );
+
+    let workspaceRowCount = workspaceStats ? workspaceStats.row_count : null;
+
+    // initial case
+    if (workspaceRowCount === null) {
+      Noco.eventEmitter.emit(UPDATE_WORKSPACE_STAT, {
+        context: targetContext,
+        fk_workspace_id: targetContext.workspace_id,
+        force: true,
+      });
+
+      workspaceRowCount = 0;
+    }
+
+    await checkLimit({
+      workspaceId: targetContext.workspace_id,
+      type: PlanLimitTypes.LIMIT_RECORD_PER_WORKSPACE,
+      count: workspaceRowCount + count,
+      message: ({ limit }) =>
+        `Only ${limit} records are allowed in target workspace, for more please upgrade your plan`,
+    });
   }
 }

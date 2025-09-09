ae55253e9ec59881fdaaed2139116896a33f757c
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-09-09 16:16:55 +0530

    fix(nc-gui): on trigger ai button data is directly getting saved in db #5042

--- packages/nc-gui/composables/useSmartsheetRowStore.ts
+++ packages/nc-gui/composables/useSmartsheetRowStore.ts
@@ -1,54 +1,58 @@
 import type { MaybeRef } from '@vueuse/core'
 
-const [useProvideSmartsheetRowStore, useSmartsheetRowStore] = useInjectionState((row: MaybeRef<Row>) => {
-  const currentRow = ref(row)
+const [useProvideSmartsheetRowStore, useSmartsheetRowStore] = useInjectionState(
+  (row: MaybeRef<Row>, changedColumns: Ref<Set<string>> = ref(new Set<string>())) => {
+    const currentRow = ref(row)
 
-  // state
-  const state = computed({
-    get: () => currentRow.value?.rowMeta?.ltarState ?? {},
-    set: (value) => {
-      if (currentRow.value) {
-        if (!currentRow.value.rowMeta) {
-          currentRow.value.rowMeta = {}
+    // state
+    const state = computed({
+      get: () => currentRow.value?.rowMeta?.ltarState ?? {},
+      set: (value) => {
+        if (currentRow.value) {
+          if (!currentRow.value.rowMeta) {
+            currentRow.value.rowMeta = {}
+          }
+          currentRow.value.rowMeta.ltarState = value
         }
-        currentRow.value.rowMeta.ltarState = value
-      }
-    },
-  })
+      },
+    })
 
-  const meta = inject(MetaInj, ref())
+    const meta = inject(MetaInj, ref())
 
-  const pk = computed(() => extractPkFromRow(currentRow.value.row, meta.value?.columns ?? []))
+    const pk = computed(() => extractPkFromRow(currentRow.value.row, meta.value?.columns ?? []))
 
-  // getters
-  const isNew = computed(() => unref(row).rowMeta?.new ?? false)
+    // getters
+    const isNew = computed(() => unref(row).rowMeta?.new ?? false)
 
-  const displayValue = computed(() => {
-    const row = unref(currentRow)
+    const displayValue = computed(() => {
+      const row = unref(currentRow)
 
-    const column = meta.value?.columns.find((col) => col.pv) || meta.value?.columns.find((col) => col.pk)
+      const column = meta.value?.columns.find((col) => col.pv) || meta.value?.columns.find((col) => col.pk)
 
-    return row.row[column?.title]
-  })
+      return row.row[column?.title]
+    })
 
-  const { addLTARRef, removeLTARRef, syncLTARRefs, loadRow, clearLTARCell, cleaMMCell } = useSmartsheetLtarHelpersOrThrow()
+    const { addLTARRef, removeLTARRef, syncLTARRefs, loadRow, clearLTARCell, cleaMMCell } = useSmartsheetLtarHelpersOrThrow()
 
-  return {
-    pk,
-    row,
-    state,
-    isNew,
-    displayValue,
-    // todo: use better name
-    addLTARRef: (...args: any) => addLTARRef(currentRow.value, ...args),
-    removeLTARRef: (...args: any) => removeLTARRef(currentRow.value, ...args),
-    syncLTARRefs: (...args: any) => syncLTARRefs(currentRow.value, ...args),
-    loadRow: (...args: any) => loadRow(currentRow.value, ...args),
-    currentRow,
-    clearLTARCell: (...args: any) => clearLTARCell(currentRow.value, ...args),
-    cleaMMCell: (...args: any) => cleaMMCell(currentRow.value, ...args),
-  }
-}, 'smartsheet-row-store')
+    return {
+      pk,
+      row,
+      changedColumns,
+      state,
+      isNew,
+      displayValue,
+      // todo: use better name
+      addLTARRef: (...args: any) => addLTARRef(currentRow.value, ...args),
+      removeLTARRef: (...args: any) => removeLTARRef(currentRow.value, ...args),
+      syncLTARRefs: (...args: any) => syncLTARRefs(currentRow.value, ...args),
+      loadRow: (...args: any) => loadRow(currentRow.value, ...args),
+      currentRow,
+      clearLTARCell: (...args: any) => clearLTARCell(currentRow.value, ...args),
+      cleaMMCell: (...args: any) => cleaMMCell(currentRow.value, ...args),
+    }
+  },
+  'smartsheet-row-store',
+)
 
 export { useProvideSmartsheetRowStore }
 

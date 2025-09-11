c49f314239bec3c53be789ea7f9dd8c4d9dad0c1
Fendy Heryanto <fendy@nocodb.com>
2025-09-11 06:19:12 +0000

    test set 1 or 2

--- packages/nocodb/tests/unit/error/index.test.ts
+++ packages/nocodb/tests/unit/error/index.test.ts
@@ -1,10 +1,11 @@
 import 'mocha';
+import { runOnSet } from '../utils/runOnSet';
 import { pgErrorExtractorTest } from './db-error-extractor/pg-error-extractor.test';
 
 function _errorTests() {
   pgErrorExtractorTest();
 }
 
-export function errorTests() {
+export const errorTests = runOnSet(2, function () {
   describe('Error', _errorTests);
-}
+});

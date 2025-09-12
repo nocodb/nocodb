a5e140c27676e5c4ca86b5374580514945ba7e9e
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-09-12 14:09:40 +0530

    fix(nc-gui): NcIconField component should behave same as cell and virtual cell icon

--- packages/nc-gui/components/nc/Icon/Field.vue
+++ packages/nc-gui/components/nc/Icon/Field.vue
@@ -1,17 +1,23 @@
 <script lang="ts" setup>
 import { type ColumnType, isVirtualCol, UITypes } from 'nocodb-sdk'
-defineProps<{
-  field: ColumnType
+const props = defineProps<{
+  field?: ColumnType
   /**
    * Windicss color class
    */
   color?: string
   defaultUidt?: UITypes
 }>()
+
+const { field } = toRefs(props)
+
+const injectedColumn = inject(ColumnInj, field)
+
+const column = computed(() => field.value ?? injectedColumn.value)
 </script>
 
 <template>
-  <SmartsheetHeaderVirtualCellIcon v-if="field && isVirtualCol(field)" :column-meta="field" :color="color" />
-  <SmartsheetHeaderCellIcon v-else-if="field" :column-meta="field" :color="color" />
+  <SmartsheetHeaderVirtualCellIcon v-if="column && isVirtualCol(column)" :column-meta="column" :color="color" />
+  <SmartsheetHeaderCellIcon v-else-if="column" :column-meta="column" :color="color" />
   <component v-else-if="defaultUidt" :is="getUIDTIcon(defaultUidt)" class="flex-none h-4 w-4" :class="color" />
 </template>

daabb5f5a4b353a3141272a4d68749fd00e04aa4
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-09-13 15:10:00 +0530

    fix: cannot reinitalize

--- packages/nc-gui/plugins/a.socket.ts
+++ packages/nc-gui/plugins/a.socket.ts
@@ -1,9 +1,10 @@
 export default defineNuxtPlugin(async (nuxtApp) => {
-  const ncSocket = {
-    id: () => null,
-    onMessage: (..._args: any[]) => null,
-    offMessage: (..._args: any[]) => null,
+  if (!isEeUI) {
+    const ncSocket = {
+      id: () => null,
+      onMessage: (..._args: any[]) => null,
+      offMessage: (..._args: any[]) => null,
+    }
+    nuxtApp.provide('ncSocket', ncSocket)
   }
-
-  nuxtApp.provide('ncSocket', ncSocket)
 })

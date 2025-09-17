da433a6976458ec5aff422c3b2d485bad0fe5caa
Fendy Heryanto <fendy@nocodb.com>
2025-09-17 11:13:14 +0000

    remove redundant addcolumn for webhook

--- packages/nocodb/src/services/columns.service.ts
+++ packages/nocodb/src/services/columns.service.ts
@@ -3271,10 +3271,6 @@ export class ColumnsService implements IColumnsService {
                     colOpt.fk_mm_child_column_id ===
                       relationColOpt.fk_mm_parent_column_id
                   ) {
-                    await columnWebhookManager?.addOldColumnById({
-                      columnId: c.id,
-                      action: WebhookActions.DELETE,
-                    });
                     await Column.delete2(
                       refContext,
                       {
@@ -3297,10 +3293,6 @@ export class ColumnsService implements IColumnsService {
                   }
                 }
 
-                await columnWebhookManager?.addOldColumnById({
-                  columnId: relationColOpt.fk_column_id,
-                  action: WebhookActions.DELETE,
-                });
                 await Column.delete2(
                   context,
                   {

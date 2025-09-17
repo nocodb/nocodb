c5dcd2ddd66809fe57c8a94711de96617b5da032
Fendy Heryanto <fendy@nocodb.com>
2025-09-17 11:13:06 +0000

    refactor webhook invoke to a class

--- packages/nocodb/src/services/v3/views-v3.service.ts
+++ packages/nocodb/src/services/v3/views-v3.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
+import type { MetaService } from '~/meta/meta.service';
 import type { NcContext, NcRequest } from '~/interface/config';
 import type { IViewsV3Service } from '~/services/v3/views-v3.types';
 
@@ -10,6 +11,7 @@ export class ViewsV3Service implements IViewsV3Service {
   async getView(
     _context: NcContext,
     _param: { viewId: string; req: NcRequest },
+    _ncMeta?: MetaService,
   ) {
     return undefined;
   }

ec168442f86dd727d4afbc41196911c3c918d050
pranavxc <pranavxc@users.noreply.github.com>
2025-09-18 09:23:41 +0000

    [create-pull-request] automated change
    Signed-off-by: GitHub <noreply@github.com>

--- packages/nocodb-sdk/src/lib/Api.ts
+++ packages/nocodb-sdk/src/lib/Api.ts
@@ -1002,23 +1002,14 @@ export interface FieldBaseV3Type {
 }
 
 export type ViewV3Type = {
-  /**
-   * Unique identifier for the view.
-   * @format uuid
-   */
+  /** Unique identifier for the view. */
   id: string;
   /** Indicates if this is the default view. Omitted if not the default view. */
   is_default?: boolean;
 } & ViewBaseV3Type & {
-    /**
-     * User ID of the creator.
-     * @format uuid
-     */
+    /** User ID of the creator. */
     created_by?: string;
-    /**
-     * User ID of the owner.
-     * @format uuid
-     */
+    /** User ID of the owner. */
     owned_by?: string;
     /**
      * Timestamp of creation.
@@ -1281,34 +1272,22 @@ export interface ViewOptionsFormV3Type {
 }
 
 export interface ViewOptionsGalleryV3Type {
-  /**
-   * Attachment field ID to be used as cover image in gallery view. Is optional, if not provided, the first attachment field will be used.
-   * @format uuid
-   */
+  /** Attachment field ID to be used as cover image in gallery view. Is optional, if not provided, the first attachment field will be used. */
   cover_field_id?: string;
 }
 
 export interface ViewOptionsCalendarV3Type {
   date_ranges: {
-    /**
-     * Date field ID to be used as start date in calendar view.
-     * @format uuid
-     */
+    /** Date field ID to be used as start date in calendar view. */
     start_date_field_id: string;
-    /**
-     * Date field ID to be used as end date in calendar view.
-     * @format uuid
-     */
+    /** Date field ID to be used as end date in calendar view. */
     end_date_field_id?: string;
   }[];
 }
 
 export interface ViewOptionsKanbanV3Type {
   stack_by: {
-    /**
-     * Single select field ID to be used for stacking cards in kanban view.
-     * @format uuid
-     */
+    /** Single select field ID to be used for stacking cards in kanban view. */
     field_id: string;
     /**
      * Order of the stacks in kanban view. If not provided, the order will be determined by options listed in associated field.
@@ -1317,20 +1296,14 @@ export interface ViewOptionsKanbanV3Type {
      */
     stack_order?: string[];
   };
-  /**
-   * Attachment field ID to be used as cover image in kanban view. If not provided, cover field configuration is skipped.
-   * @format uuid
-   */
+  /** Attachment field ID to be used as cover image in kanban view. If not provided, cover field configuration is skipped. */
   cover_field_id?: string;
 }
 
 export interface ViewOptionsGridV3Type {
   /** List of groups to be applied on the grid view. */
   groups?: {
-    /**
-     * Identifier for the field being sorted.
-     * @format uuid
-     */
+    /** Identifier for the field being sorted. */
     field_id: string;
     /** Direction of the group, either 'asc' (ascending) or 'desc' (descending). */
     direction?: 'asc' | 'desc';
@@ -1352,10 +1325,7 @@ export type ViewRowColourV3Type =
   | {
       /** Mode of row coloring. In this mode, the color is selected based on a single select field. */
       mode: 'select';
-      /**
-       * Single select field ID to be used for colouring rows in the view.
-       * @format uuid
-       */
+      /** Single select field ID to be used for colouring rows in the view. */
       field_id: string;
       /** Whether to additionally apply the color as row background. */
       apply_as_row_background?: boolean;
@@ -1369,10 +1339,7 @@ export type ViewRowColourV3Type =
 - In case of partial list, fields not included in the list will be excluded from the view.
 */
 export type ViewFieldsV3Type = {
-  /**
-   * Unique identifier for the field.
-   * @format uuid
-   */
+  /** Unique identifier for the field. */
   field_id: string;
   /** Indicates whether the field should be displayed in the view. */
   show: boolean;
@@ -1436,15 +1403,9 @@ export interface ViewListV3Type {
     lock_type: 'collaborative' | 'locked' | 'personal';
     /** Indicates if this is the default view. */
     is_default?: boolean;
-    /**
-     * User ID of the creator.
-     * @format uuid
-     */
+    /** User ID of the creator. */
     created_by: string;
-    /**
-     * User ID of the owner. Applicable only for personal views.
-     * @format uuid
-     */
+    /** User ID of the owner. Applicable only for personal views. */
     owned_by?: string;
     /**
      * Timestamp of creation.
@@ -1563,14 +1524,12 @@ export type BaseMemberUpdateV3Type = {
 /**
  * Array of members to be created.
  */
-export type BaseMemberCreateV3Type = (
+export type BaseMemberCreateV3Type = ((
   | {
       /** Unique identifier for the user (skip if email is provided) */
       user_id: string;
       /** Full name of the user. */
       user_name?: string;
-      /** Base roles for the user. */
-      base_role: BaseRolesV3Type;
     }
   | {
       /**
@@ -1580,10 +1539,11 @@ export type BaseMemberCreateV3Type = (
       email: string;
       /** Full name of the user. */
       user_name?: string;
-      /** Base roles for the user. */
-      base_role: BaseRolesV3Type;
     }
-)[];
+) & {
+  /** Base roles for the user. */
+  base_role: BaseRolesV3Type;
+})[];
 
 export interface BaseMemberListV3Type {
   list?: BaseMemberV3Type[];

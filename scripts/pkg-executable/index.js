4d3ae92662fb752c35686a6011bf243ef326343c
Pranav C <pranavxc@gmail.com>
2025-09-22 12:57:59 +0530

    chore: oss sync

--- scripts/pkg-executable/index.js
+++ scripts/pkg-executable/index.js
@@ -2,9 +2,11 @@ process.env.NC_BINARY_BUILD = 'true';
 (async () => {
   try {
     const app = require('express')();
-    const {Noco} = require("nocodb");
+    const { Noco } = require("nocodb");
+    const cors = require('cors')
     const port = process.env.PORT || 8080;
     const httpServer = app.listen(port);
+    app.use(cors())
     app.use(await Noco.init({}, httpServer, app));
     console.log(`Visit : localhost:${port}/dashboard`)
   } catch(e) {

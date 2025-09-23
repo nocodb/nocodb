07c753b4356d014c32d6d840ff1debb502c36734
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-09-23 08:42:25 +0000

    fix(nc-gui): review changes

--- packages/nc-gui/components/smartsheet/toolbar/FieldsMenu.vue
+++ packages/nc-gui/components/smartsheet/toolbar/FieldsMenu.vue
@@ -715,9 +715,9 @@ const onAddColumnDropdownVisibilityChange = () => {
               item-key="id"
               ghost-class="nc-fields-menu-items-ghost"
               :disabled="isLocked"
-              :scroll="true"
+              scroll
               :scroll-sensitivity="120"
-              :bubble-scroll="true"
+              bubble-scroll
               @change="onMove($event)"
               @start="isDragging = true"
               @end="isDragging = false"

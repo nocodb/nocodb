f99743b92dbb03ff2142d3ac849ae2b3893517e1
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-09-24 17:00:13 +0530

    fix(nc-gui): global search show proper keyboard based on UITypes in mobile mode

--- packages/nc-gui/components/smartsheet/toolbar/SearchData.vue
+++ packages/nc-gui/components/smartsheet/toolbar/SearchData.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import type { ColumnType, TableType } from 'nocodb-sdk'
+import type { ColumnType, TableType, UITypes } from 'nocodb-sdk'
 
 const reloadData = inject(ReloadViewDataHookInj)!
 
@@ -80,6 +80,12 @@ const displayColumn = computed(() => {
   const pvColumn = columns.value?.find((column) => column.pv)
   search.value.field = pvColumn?.id as string
   return pvColumn
+})
+
+const searchInputMode = computed(() => {
+  if (!displayColumn.value?.uidt) return
+
+  return getInputModeFromUITypes(displayColumn.value?.uidt as UITypes)
 })
 
 const displayColumnLabel = computed(() => {
@@ -242,6 +248,7 @@ watch(
               v-if="search.query || showSearchBox"
               ref="globalSearchRef"
               v-model:value="search.query"
+              :inputmode="searchInputMode"
               name="globalSearchQuery"
               size="small"
               class="!text-bodyDefaultSm !w-40 h-full nc-view-search-data !pl-0"

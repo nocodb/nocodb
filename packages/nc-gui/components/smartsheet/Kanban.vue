522bcde6ced237a6da2fcbb9d0b00af1e2013b2e
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-09-25 14:24:21 +0530

    fix: Kanban if stacked by field has default value, add new record in uncategorized stack doesnt work

--- packages/nc-gui/components/smartsheet/Kanban.vue
+++ packages/nc-gui/components/smartsheet/Kanban.vue
@@ -473,6 +473,7 @@ const draggableCardFilter = (event: Event, target: HTMLElement) => {
 
   return !!(
     eventTarget &&
+    target &&
     target.contains(eventTarget) &&
     closestNotDraggable &&
     (target.contains(closestNotDraggable) || closestNotDraggable === target)
@@ -1258,6 +1259,7 @@ const handleOpenNewRecordForm = (stackTitle?: string) => {
       :meta="meta"
       :load-row="!isPublic"
       :view="view"
+      :allow-null-field-ids="groupingFieldColumn?.id ? [groupingFieldColumn.id] : []"
       @cancel="removeRowFromUncategorizedStack"
     />
   </Suspense>
@@ -1272,6 +1274,7 @@ const handleOpenNewRecordForm = (stackTitle?: string) => {
       :expand-form="expandForm"
       :row-id="route.query.rowId"
       :view="view"
+      :allow-null-field-ids="groupingFieldColumn?.id ? [groupingFieldColumn.id] : []"
     />
   </Suspense>
 

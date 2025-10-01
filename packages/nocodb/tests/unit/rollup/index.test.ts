6f612e673ca090ab0633af6dabb021bee8848467
Fendy Heryanto <fendy@nocodb.com>
2025-10-01 06:02:38 +0000

    add test for circular ref rollup to rollup

--- packages/nocodb/tests/unit/rollup/index.test.ts
+++ packages/nocodb/tests/unit/rollup/index.test.ts
@@ -1,8 +1,10 @@
 import 'mocha';
 import { rollupRollupTest } from './tests/rollup-rollup.test';
+import { rollupErrorTest } from './tests/rollup-error.test';
 
 function _rollupTests() {
   rollupRollupTest();
+  rollupErrorTest();
 }
 
 // FIXME: run on set 2

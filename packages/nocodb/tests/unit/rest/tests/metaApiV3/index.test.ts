d0545a6790ad0715a06a738b7fbd3d14fd6b75b5
Pranav C <pranavxc@gmail.com>
2025-10-10 02:21:03 +0530

    test: teams api CRUD - unit tests

--- packages/nocodb/tests/unit/rest/tests/metaApiV3/index.test.ts
+++ packages/nocodb/tests/unit/rest/tests/metaApiV3/index.test.ts
@@ -6,6 +6,7 @@ import columnTestV3 from './column.test';
 import baseUsersTestV3 from './baseUsers.test';
 import errorHandlingMetaTestsV3 from './error-handling/index.test';
 import workspaceUsersTest from './workspaceUsers.test';
+// import teamsTestV3 from './teams.test';
 
 export default runOnSet(2, function () {
   baseTestV3();
@@ -15,4 +16,5 @@ export default runOnSet(2, function () {
   columnTestV3();
   errorHandlingMetaTestsV3();
   workspaceUsersTest();
+  // teamsTestV3();
 });

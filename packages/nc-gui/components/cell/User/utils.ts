cbe6be552bd13d37c1e4ef37b8ebbfec87edfae4
Fendy Heryanto <fendy@nocodb.com>
2025-10-15 20:58:43 +0700

    remove console.log

--- packages/nc-gui/components/cell/User/utils.ts
+++ packages/nc-gui/components/cell/User/utils.ts
@@ -117,7 +117,6 @@ export const getSelectedUsers = (
     selected = localModelValue
       ? (Array.isArray(localModelValue) ? localModelValue : [localModelValue]).reduce((acc, item) => {
           const label = item?.display_name || item?.email
-          console.log(optionsMap[item.id])
           if (label) {
             const user = optionsMap[item.id]
             acc.push({

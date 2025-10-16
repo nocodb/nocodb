4f8c9384632957306e848b8adb1baddae7abe878
Pranav C <pranavxc@gmail.com>
2025-10-16 18:32:11 +0530

    refactor: update teams migration file and references

--- packages/nocodb/src/meta/migrations/XcMigrationSourcev2.ts
+++ packages/nocodb/src/meta/migrations/XcMigrationSourcev2.ts
@@ -81,7 +81,7 @@ import * as nc_091_unify_model from '~/meta/migrations/v2/nc_091_unify_model';
 import * as nc_092_composite_pk from '~/meta/migrations/v2/nc_092_composite_pk';
 import * as nc_093_oauth_server from '~/meta/migrations/v2/nc_093_oauth_server';
 import * as nc_094_add_meta_to_filter_exp_v2 from '~/meta/migrations/v2/nc_094_add_meta_to_filter_exp_v2';
-import * as nc_093_teams from '~/meta/migrations/v2/nc_093_teams';
+import * as nc_095_teams from '~/meta/migrations/v2/nc_095_teams';
 
 // Create a custom migration source class
 export default class XcMigrationSourcev2 {
@@ -174,7 +174,7 @@ export default class XcMigrationSourcev2 {
       'nc_092_composite_pk',
       'nc_093_oauth_server',
       'nc_094_add_meta_to_filter_exp_v2',
-      'nc_093_teams',
+      'nc_095_teams',
     ]);
   }
 
@@ -350,8 +350,8 @@ export default class XcMigrationSourcev2 {
         return nc_093_oauth_server;
       case 'nc_094_add_meta_to_filter_exp_v2':
         return nc_094_add_meta_to_filter_exp_v2;
-      case 'nc_093_teams':
-        return nc_093_teams;
+      case 'nc_095_teams':
+        return nc_095_teams;
     }
   }
 }

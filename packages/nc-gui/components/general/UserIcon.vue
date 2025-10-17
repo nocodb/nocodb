688e5936acf7efdc61f84e29c3c3e24437e93e2d
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-10-17 18:03:49 +0530

    fix(nc-gui): keep 1 user initial char for personal view indicator icon

--- packages/nc-gui/components/general/UserIcon.vue
+++ packages/nc-gui/components/general/UserIcon.vue
@@ -12,6 +12,7 @@ const props = withDefaults(
     iconBgColor?: string
     showPlaceholderIcon?: boolean
     isDeleted?: boolean
+    initialLegth?: 1 | 2
   }>(),
   {
     user: () => ({}),
@@ -22,6 +23,7 @@ const props = withDefaults(
     iconBgColor: '#F4F4F5',
     showPlaceholderIcon: false,
     isDeleted: false,
+    initialLegth: 2,
   },
 )
 
@@ -109,10 +111,10 @@ const usernameInitials = computed(() => {
     if (displayNameSplit.length > 1) {
       return displayNameSplit[0][0] + displayNameSplit[1][0]
     } else {
-      return user.value.display_name.slice(0, 2)
+      return user.value.display_name.slice(0, props.initialLegth)
     }
   } else {
-    return user.value.email?.split('@')[0].slice(0, 2)
+    return user.value.email?.split('@')[0].slice(0, props.initialLegth)
   }
 })
 </script>

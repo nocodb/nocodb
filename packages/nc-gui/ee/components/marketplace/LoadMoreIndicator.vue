c4db7f7ff658167b6ad3b6cb8dcc778aea109fa0
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-10-17 16:14:00 +0530

    fix(nc-gui): dont show loading more indicator if initial data is not loaded

--- packages/nc-gui/ee/components/marketplace/LoadMoreIndicator.vue
+++ packages/nc-gui/ee/components/marketplace/LoadMoreIndicator.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
-const { isLoading, loadingTrigger, hasMore } = useMarketplaceTemplates('marketplace')
+const { isLoading, loadingTrigger, hasMore, templates } = useMarketplaceTemplates('marketplace')
 </script>
 <template>
-  <div v-if="hasMore" ref="loadingTrigger" class="py-4 flex justify-center">
+  <div v-if="hasMore && templates.length" ref="loadingTrigger" class="py-4 flex justify-center">
     <div v-if="isLoading" class="flex items-center gap-2">
       <GeneralLoader size="medium" />
       <span class="text-nc-content-gray-subtle2">{{ $t('objects.templates.loadingMoreTemplates') }}...</span>

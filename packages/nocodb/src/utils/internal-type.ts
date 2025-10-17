5347bcf40eb9804d112fa46596972432e52035ed
Pranav C <pranavxc@gmail.com>
2025-10-17 17:06:54 +0530

    refactor: team-related types in internal-type.ts

--- packages/nocodb/src/utils/internal-type.ts
+++ packages/nocodb/src/utils/internal-type.ts
@@ -1,10 +1,5 @@
 import type { PagedResponseImpl } from '~/helpers/PagedResponse';
 import type { MCPToken, OAuthClient } from '~/models';
-import type {
-  TeamDetailV3Type,
-  TeamMemberV3ResponseType,
-  TeamV3ResponseType,
-} from '~/services/v3/teams-v3.types';
 
 export type InternalGETResponseType = Promise<
   | void
@@ -13,17 +8,8 @@ export type InternalGETResponseType = Promise<
   | PagedResponseImpl<any>
   | OAuthClient
   | OAuthClient[]
-  | { list: TeamV3ResponseType[] }
-  | TeamDetailV3Type
 >;
 
 export type InternalPOSTResponseType = Promise<
-  | void
-  | boolean
-  | MCPToken
-  | OAuthClient
-  | OAuthClient[]
-  | TeamV3ResponseType
-  | TeamMemberV3ResponseType[]
-  | { msg: string }
+  void | boolean | MCPToken | OAuthClient | OAuthClient[] | { msg: string }
 >;

fec69d828f489972d8b5a39a0566b89dfacd933f
mertmit <mertmit99@gmail.com>
2025-10-21 10:08:17 +0300

    fix: remove unused type
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/ee/meta/meta.service.ts
+++ packages/nocodb/src/ee/meta/meta.service.ts
@@ -87,7 +87,6 @@ export class MetaService extends MetaServiceCE {
       [MetaTable.HOOK_LOGS]: 'hkl',
       [MetaTable.API_TOKENS]: 'tkn',
       [MetaTable.WORKSPACE]: 'w',
-      [MetaTable.COWRITER]: 'cw',
       [MetaTable.SSO_CLIENT]: 'sso',
       [MetaTable.ORG]: 'o',
       [MetaTable.EXTENSIONS]: 'ext',

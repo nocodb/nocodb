d0f8429eb0ee1ae616ecc87bb85b55d2ac95c3d0
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-10-21 18:24:42 +0530

    fix: show value below barcode and qr code

--- packages/nc-gui/components/virtual-cell/QrCode.vue
+++ packages/nc-gui/components/virtual-cell/QrCode.vue
@@ -107,7 +107,7 @@ onMounted(() => {
     v-model:visible="modalVisible"
     :class="{ active: modalVisible }"
     wrap-class-name="nc-qr-code-large qrcode-modal"
-    :body-style="{ padding: '0px', display: 'flex', justifyContent: 'center' }"
+    :body-style="{ padding: '0px', display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }"
     :closable="false"
     :centered="isMobileMode"
     @ok="handleModalOkClick"
@@ -169,6 +169,16 @@ onMounted(() => {
       </div>
     </template>
     <img v-if="showQrCode" :src="qrCodeLarge" :alt="$t('title.qrCode')" class="h-[156px] mt-8 mb-4" />
+
+    <div v-if="showQrCode" class="bg-nc-bg-gray-light mx-4 px-3 py-2 w-120 rounded-lg">
+      <NcTooltip show-on-truncate-only class="truncate">
+        <template #title>
+          {{ qrValue }}
+        </template>
+
+        {{ qrValue }}
+      </NcTooltip>
+    </div>
   </a-modal>
   <div
     v-if="showQrCode"

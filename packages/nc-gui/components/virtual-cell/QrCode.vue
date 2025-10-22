bb501e28f4d995fa16ab22e306702829ce402cdd
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-10-22 08:17:10 +0000

    fix: qr code fix

--- packages/nc-gui/components/virtual-cell/QrCode.vue
+++ packages/nc-gui/components/virtual-cell/QrCode.vue
@@ -168,9 +168,9 @@ onMounted(() => {
         </div>
       </div>
     </template>
-    <div v-if="showQrCode" class="w-full">
+    <div v-if="showQrCode" class="w-full px-4">
       <img :src="qrCodeLarge" :alt="$t('title.qrCode')" class="h-[156px] mx-auto mt-8 mb-4" />
-      <div class="bg-nc-bg-gray-light px-3 py-2 mx-4 rounded-lg">
+      <div class="bg-nc-bg-gray-light px-3 py-2 rounded-lg">
         <NcTooltip show-on-truncate-only class="truncate">
           <template #title>
             {{ qrValue }}

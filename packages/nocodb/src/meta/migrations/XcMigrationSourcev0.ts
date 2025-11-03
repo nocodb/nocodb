476d918a2498f69c1bc3c5d718fed4a6c9db74f4
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-11-03 07:38:07 +0000

    fix(nocodb): add alter color condition nc_order col migration

---
+++ packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
@@ -0,0 +1,33 @@
+import * as nc_001_init from './v0/nc_001_init';
+import * as nc_002_teams from './v0/nc_002_teams';
+import * as nc_003_alter_row_color_condition_nc_order_col from './v0/nc_003_alter_row_color_condition_nc_order_col';
+
+// Create a custom migration source class
+export default class XcMigrationSourcev0 {
+  // Must return a Promise containing a list of migrations.
+  // Migrations can be whatever you want, they will be passed as
+  // arguments to getMigrationName and getMigration
+  public getMigrations(): Promise<any> {
+    // In this run we are just returning migration names
+    return Promise.resolve([
+      'nc_001_init',
+      'nc_002_teams',
+      'nc_003_alter_row_color_condition_nc_order_col',
+    ]);
+  }
+
+  public getMigrationName(migration): string {
+    return migration;
+  }
+
+  public getMigration(migration): any {
+    switch (migration) {
+      case 'nc_001_init':
+        return nc_001_init;
+      case 'nc_002_teams':
+        return nc_002_teams;
+      case 'nc_003_alter_row_color_condition_nc_order_col':
+        return nc_003_alter_row_color_condition_nc_order_col;
+    }
+  }
+}

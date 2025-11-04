a8d79fdc3f4d4ada998e9bced70438466572c98d
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2025-11-04 09:06:03 +0000

    feat(nc-gui): add copy template route

---
+++ packages/nc-gui/pages/copy-template.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const route = useRoute()
+
+const { sharedBaseId, isUseThisTemplate, options } = useCopySharedBase()
+
+const { forcedProjectId } = storeToRefs(useBase())
+
+onMounted(() => {
+  isUseThisTemplate.value = true
+
+  options.value.includeData = true
+  options.value.includeViews = true
+
+  sharedBaseId.value = route.query.base as string
+
+  if (forcedProjectId?.value) forcedProjectId.value = undefined
+
+  navigateTo(`/`)
+})
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>

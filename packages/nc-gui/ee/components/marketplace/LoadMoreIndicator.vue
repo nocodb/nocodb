4683cc83185d59e44d616ad1e7a66697e93c2617
Pranav C <pranavxc@gmail.com>
2025-11-05 00:45:20 +0530

    refactor: background color correction

--- packages/nc-gui/ee/components/marketplace/LoadMoreIndicator.vue
+++ packages/nc-gui/ee/components/marketplace/LoadMoreIndicator.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 const { isLoading, loadingTrigger, hasMore, templates } = useMarketplaceTemplates('all-templates')
 </script>
+
 <template>
   <div v-if="hasMore && templates.length" ref="loadingTrigger" class="py-4 flex justify-center">
     <div v-if="isLoading" class="flex items-center gap-2">

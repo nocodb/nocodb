e487b7e72528e56c29700e004c705e1fff5a01cd
Fendy Heryanto <fendy@nocodb.com>
2025-11-08 16:49:41 +0700

    fix: last modified by / time is nullable on openapi

--- packages/nocodb/src/services/api-docs/swaggerV3/getSwaggerColumnMetasV3.ts
+++ packages/nocodb/src/services/api-docs/swaggerV3/getSwaggerColumnMetasV3.ts
@@ -235,11 +235,16 @@ async function processColumnToSwaggerField(
       break;
     }
     case UITypes.LastModifiedTime:
+      field.type = ['string', 'null'];
+      field.format = 'date-time';
+      break;
     case UITypes.CreatedTime:
       field.type = 'string';
       field.format = 'date-time';
       break;
     case UITypes.LastModifiedBy:
+      field.type = ['object', 'null'];
+      break;
     case UITypes.CreatedBy:
       field.type = 'object';
       break;

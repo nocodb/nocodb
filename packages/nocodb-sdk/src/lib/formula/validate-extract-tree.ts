fde8c66cc888784a6db7b3472ff88c110ed003d3
Pranav C <pranavxc@gmail.com>
2025-11-17 13:24:37 +0530

    fix: type correction

--- packages/nocodb-sdk/src/lib/formula/validate-extract-tree.ts
+++ packages/nocodb-sdk/src/lib/formula/validate-extract-tree.ts
@@ -697,7 +697,7 @@ export async function validateFormulaAndExtractTreeWithType({
 }): Promise<ParsedFormulaNode> {
   // extract column list from meta since columns array might not have all columns(system columns)
   const meta = await getMeta(
-    unifiedMeta.getContextFromObject(column ?? columns?.[0] ?? {}),
+    unifiedMeta.getContextFromObject(column ?? columns?.[0] ?? ({} as any)),
     { id: column?.fk_model_id || columns?.[0]?.fk_model_id || '' }
   );
   const allColumns = meta?.columns || columns;

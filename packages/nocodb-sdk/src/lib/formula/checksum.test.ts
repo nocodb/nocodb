a465885a8a555530e3b86ecdaa1e072c7e4d5dec
kunal-shidling <kunalshidling@gmail.com>
2025-11-23 17:25:41 +0530

    added the checksum formula feild

---
+++ packages/nocodb-sdk/src/lib/formula/checksum.test.ts
@@ -0,0 +1,27 @@
+import {
+  checksum_md5,
+  checksum_sha1,
+  checksum_sha256
+} from "./functions";
+
+describe("Checksum Formula Functions", () => {
+  test("MD5 checksum", () => {
+    expect(checksum_md5("hello")).toBe("5d41402abc4b2a76b9719d911017c592");
+  });
+
+  test("SHA-1 checksum", () => {
+    expect(checksum_sha1("hello")).toBe("f7ff9e8b7bb2e0fdb81c06f4e8d5b0d4f33196dd");
+  });
+
+  test("SHA-256 checksum", () => {
+    expect(checksum_sha256("hello")).toBe(
+      "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+    );
+  });
+
+  test("Null or undefined returns null", () => {
+    expect(checksum_md5(null)).toBeNull();
+    expect(checksum_sha1(undefined)).toBeNull();
+    expect(checksum_sha256(null)).toBeNull();
+  });
+});

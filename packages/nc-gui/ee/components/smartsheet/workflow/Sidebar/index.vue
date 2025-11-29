67bf508d40308a6682e1a2ad7d21ab44675f74af
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-11-29 19:34:10 +0530

    fix: exectuin logs ui
    # Conflicts:
    #	packages/nc-gui/ee/components/dlg/Workflow/ExecutionLogs/List.vue

--- packages/nc-gui/ee/components/smartsheet/workflow/Sidebar/index.vue
+++ packages/nc-gui/ee/components/smartsheet/workflow/Sidebar/index.vue
@@ -4,24 +4,28 @@ import NodeConfig from '~/components/smartsheet/workflow/Sidebar/Config/index.vu
 import TestStep from '~/components/smartsheet/workflow/Sidebar/Config/TestStep.vue'
 import Result from '~/components/smartsheet/workflow/Sidebar/Config/Result.vue'
 import NodeDetails from '~/components/smartsheet/workflow/Sidebar/NodeDetails.vue'
+import LogSidebar from '~/components/smartsheet/workflow/Sidebar/Logs/index.vue'
 
-const { selectedNode, selectedNodeId } = useWorkflowOrThrow()
+const { selectedNode, selectedNodeId, activeTab } = useWorkflowOrThrow()
 </script>
 
 <template>
-  <div class="border-l-1 node-sidebar w-100 sidebar-config border-nc-border-gray-medium bg-nc-bg-default">
-    <Details v-if="!selectedNode" />
-    <template v-else>
-      <div class="border-b-1 border-nc-border-gray-medium py-2 px-1" @click="selectedNodeId = null">
-        <NcButton type="text" size="small">
-          <GeneralIcon icon="ncChevronLeft" />
-        </NcButton>
-      </div>
-      <NodeDetails />
-      <NodeConfig />
-      <TestStep />
-      <Result v-if="selectedNode.data?.testResult" />
+  <div class="border-l-1 node-sidebar w-95 sidebar-config border-nc-border-gray-medium bg-nc-bg-default">
+    <template v-if="activeTab === 'editor'">
+      <Details v-if="!selectedNode" />
+      <template v-else>
+        <div class="border-b-1 border-nc-border-gray-medium py-2 px-1" @click="selectedNodeId = null">
+          <NcButton type="text" size="small">
+            <GeneralIcon icon="ncChevronLeft" />
+          </NcButton>
+        </div>
+        <NodeDetails />
+        <NodeConfig />
+        <TestStep />
+        <Result v-if="selectedNode.data?.testResult" />
+      </template>
     </template>
+    <LogSidebar />
   </div>
 </template>
 

e7945dc994ab451ea9b5525c991a3ca13165832e
Tiago Granelli Ribeiro <tiago.granelli@gmail.com>
2025-12-04 14:59:49 +0100

    fix(nc-gui): migrate calendar components to Pointer Events API
    - Update VRecordCard, RecordCard for touch resize support
    - Update MonthView drag/resize handlers
    - Update WeekView DateTimeField drag/resize handlers
    - Enables calendar record dragging and resizing on touch devices

--- packages/nc-gui/components/smartsheet/calendar/VRecordCard.vue
+++ packages/nc-gui/components/smartsheet/calendar/VRecordCard.vue
@@ -49,7 +49,7 @@ const rowColorInfo = computed(() => {
     <div
       v-if="resize"
       class="absolute w-full h-1 z-20 top-0 cursor-row-resize"
-      @mousedown.stop="emit('resizeStart', 'left', $event, record)"
+      @pointerdown.stop="emit('resizeStart', 'left', $event, record)"
     ></div>
     <div
       :class="{
@@ -84,7 +84,7 @@ const rowColorInfo = computed(() => {
     <div
       v-if="resize"
       class="absolute cursor-row-resize w-full bottom-0 w-full h-1"
-      @mousedown.stop="emit('resizeStart', 'right', $event, record)"
+      @pointerdown.stop="emit('resizeStart', 'right', $event, record)"
     ></div>
   </div>
 </template>

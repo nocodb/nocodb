0cb5bf787fee8e3c91a3d75ff72bec4266d47b23
Pranav C <pranavxc@gmail.com>
2025-12-05 01:57:18 +0530

    fix: extract mm table meta and get non-external models as well

--- packages/nocodb/src/utils/getRelatedModelMap.ts
+++ packages/nocodb/src/utils/getRelatedModelMap.ts
@@ -1,3 +1,4 @@
+import { isLinksOrLTAR } from 'nocodb-sdk';
 import type { NcContext } from 'nocodb-sdk';
 import { Model } from '~/models';
 
@@ -17,23 +18,39 @@ export async function getRelatedModelMap(
       relatedModels.set(col.fk_model_id, model);
     }
 
+    if (!col.colOptions || !isLinksOrLTAR(col)) {
+      continue;
+    }
+
+    // related model
+    if (!relatedModels.has(col.colOptions.fk_related_model_id)) {
+      const targetContext = {
+        ...context,
+        base_id: col.colOptions.fk_related_base_id || context.base_id,
+      };
+      const relatedModel = await Model.get(
+        targetContext,
+        col.colOptions.fk_related_model_id,
+      );
+      await relatedModel.getColumns(targetContext);
+      relatedModels.set(col.colOptions.fk_related_model_id, relatedModel);
+    }
+
+    // junction model for many-to-many
     if (
-      col.colOptions?.base_id &&
-      col.colOptions?.fk_related_base_id &&
-      col.colOptions?.base_id !== col.colOptions?.fk_related_base_id
+      col.colOptions.fk_mm_model_id &&
+      !relatedModels.has(col.colOptions.fk_mm_model_id)
     ) {
-      if (!relatedModels.has(col.colOptions.fk_related_model_id)) {
-        const targetContext = {
-          ...context,
-          base_id: col.colOptions.fk_related_base_id,
-        };
-        const relatedModel = await Model.get(
-          targetContext,
-          col.colOptions.fk_related_model_id,
-        );
-        await relatedModel.getColumns(targetContext);
-        relatedModels.set(col.colOptions.fk_related_model_id, relatedModel);
-      }
+      const targetContext = {
+        ...context,
+        base_id: col.colOptions.fk_mm_base_id || context.base_id,
+      };
+      const relatedModel = await Model.get(
+        targetContext,
+        col.colOptions.fk_mm_model_id,
+      );
+      await relatedModel.getColumns(targetContext);
+      relatedModels.set(col.colOptions.fk_mm_model_id, relatedModel);
     }
   }
 

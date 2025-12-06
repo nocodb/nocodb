392a7b7bb447aef2445d349cb54d4338cb2ece68
Fendy Heryanto <fendy@nocodb.com>
2025-12-06 22:30:51 +0700

    remove console.log

--- packages/nocodb/src/services/columns.service.ts
+++ packages/nocodb/src/services/columns.service.ts
@@ -3255,9 +3255,6 @@ export class ColumnsService implements IColumnsService {
             childContext,
             ncMeta,
           );
-          if (!childColumn) {
-            console.log(column.id, relationColOpt);
-          }
           const childTable = await childColumn.getModel(childContext, ncMeta);
 
           const parentColumn = await relationColOpt.getParentColumn(

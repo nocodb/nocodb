dc72fc36821c5bbb718f89a8c0563731028ff068
Pranav C <pranavxc@gmail.com>
2025-12-19 20:09:45 +0000

    fix: add missing context arg

--- packages/nocodb-sdk/src/lib/formula/validate-extract-tree.ts
+++ packages/nocodb-sdk/src/lib/formula/validate-extract-tree.ts
@@ -556,6 +556,7 @@ export async function validateFormulaAndExtractTreeWithType({
 }): Promise<ParsedFormulaNode> {
   // extract column list from meta since columns array might not have all columns(system columns)
   const meta = await getMeta(
+    unifiedMeta.getContextFromObject(column),
     column?.fk_model_id || columns?.[0]?.fk_model_id || ''
   );
   const allColumns = meta?.columns || columns;

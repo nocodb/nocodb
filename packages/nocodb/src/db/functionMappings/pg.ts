71a5ae40e5fc9e1c5834dd2b9b7d5617f8e1c868
Pranav C <pranavxc@gmail.com>
2025-12-19 20:09:42 +0000

    refactor: update the meta structure

--- packages/nocodb/src/db/functionMappings/pg.ts
+++ packages/nocodb/src/db/functionMappings/pg.ts
@@ -518,7 +518,7 @@ END`,
           if (!column) return;
 
           return knex.raw(
-            `(COALESCE(??::jsonb->'lastModifiedTime','{}'::jsonb)->> ?)::timestamp`,
+            `(COALESCE(??::jsonb-> ?,'{}'::jsonb)->>'modifiedTime')::timestamp`,
             [rowMetaColumn.column_name, column?.id],
           );
         })

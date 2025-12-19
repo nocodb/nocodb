72b95ae84486b4d607581d45440f93106d59ff04
Pranav C <pranavxc@gmail.com>
2025-12-19 20:09:43 +0000

    refactor: review changes

--- packages/nocodb-sdk/src/lib/formula/formulas.ts
+++ packages/nocodb-sdk/src/lib/formula/formulas.ts
@@ -1221,7 +1221,11 @@ export const formulas: Record<string, FormulaMeta> = {
             throw new FormulaError(
               FormulaErrorType.INVALID_ARG,
               {},
-              `Column "${arg.name}" does not support LAST_MODIFIED_TIME`
+              `LAST_MODIFIED_TIME() is not supported for ${
+                virtualOrSystemColRef.system
+                  ? 'System'
+                  : virtualOrSystemColRef.uidt
+              } field.`
             );
           }
         }
@@ -1229,8 +1233,11 @@ export const formulas: Record<string, FormulaMeta> = {
     },
     description:
       'Returns the last modified time of the current record or selection if it exists',
-    syntax: 'LAST_MODIFIED_TIME()',
-    examples: [' LAST_MODIFIED_TIME()'],
+    syntax: 'LAST_MODIFIED_TIME([field1, ...])',
+    examples: [
+      'LAST_MODIFIED_TIME()',
+      'LAST_MODIFIED_TIME({Status}, {Priority})',
+    ],
     returnType: FormulaDataTypes.DATE,
   },
 };

b084b2b54b340ba7966fcf7df0be2563a207decd
DarkPhoenix2704 <anbarasun123@gmail.com>
2025-12-20 16:03:51 +0530

    fix: disable manual trigger

--- packages/nc-gui/ee/components/smartsheet/workflow/canvas/ManualTrigger.vue
+++ packages/nc-gui/ee/components/smartsheet/workflow/canvas/ManualTrigger.vue
@@ -3,6 +3,8 @@ import { useWorkflowOrThrow } from '~/composables/useWorkflow'
 
 const workflowStore = useWorkflowStore()
 
+const { activeWorkflowHasDraftChanges } = storeToRefs(workflowStore)
+
 const { executeWorkflow: _executeWorkflow } = workflowStore
 
 const { nodes, workflow } = useWorkflowOrThrow()
@@ -23,12 +25,21 @@ const hasManualTrigger = computed(() => {
 
 <template>
   <div v-if="hasManualTrigger" class="absolute w-[calc(100%-379px)] bottom-2 flex items-center right-0 left-0">
-    <NcButton class="mx-auto" :disabled="isLoading" :loading="isLoading" size="small" @click="executeWorkflow">
-      <div class="flex items-center gap-2">
-        <GeneralIcon icon="ncSend" />
-        {{ $t('labels.triggerWorkflow') }}
-      </div>
-    </NcButton>
+    <NcTooltip :disabled="!activeWorkflowHasDraftChanges">
+      <template #title> Workflow has draft changes. Please publish the workflow before triggering. </template>
+      <NcButton
+        class="mx-auto"
+        :disabled="isLoading || activeWorkflowHasDraftChanges"
+        :loading="isLoading"
+        size="small"
+        @click="executeWorkflow"
+      >
+        <div class="flex items-center gap-2">
+          <GeneralIcon icon="ncSend" />
+          {{ $t('labels.triggerWorkflow') }}
+        </div>
+      </NcButton>
+    </NcTooltip>
   </div>
   <NcSpanHidden v-else />
 </template>

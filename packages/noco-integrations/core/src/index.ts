2948a9153904ad7429bf3aca9e8bf8bd48d8bf8c
Fendy Heryanto <fendy@nocodb.com>
2026-01-02 05:21:54 +0000

    WIP

--- packages/noco-integrations/core/src/index.ts
+++ packages/noco-integrations/core/src/index.ts
@@ -6,6 +6,10 @@ export * from './sync';
 export * from './auth';
 export * from './ai';
 export { SCHEMA_TICKETING } from './sync/schema-ticketing';
+export { SCHEMA_HRIS } from './sync/schema-hris';
+export { SCHEMA_CRM } from './sync/schema-crm';
+export { SCHEMA_FILE_STORAGE } from './sync/schema-filestorage';
+export { SCHEMA_ATS } from './sync/schema-ats';
 export type {
   TicketingTicketRecord,
   TicketingUserRecord,

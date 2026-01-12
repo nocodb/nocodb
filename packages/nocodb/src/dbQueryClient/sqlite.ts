a415618b0d0a2462ce6915cb03004d2e9834d178
mertmit <mertmit99@gmail.com>
2026-01-13 00:34:00 +0300

    chore: sync missing changes
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/dbQueryClient/sqlite.ts
+++ packages/nocodb/src/dbQueryClient/sqlite.ts
@@ -1,15 +1,14 @@
 import type { DBQueryClient } from '~/dbQueryClient/types';
 import { GenericDBQueryClient } from '~/dbQueryClient/generic';
 
-export class PGDBQueryClient
+export class SqliteDBQueryClient
   extends GenericDBQueryClient
   implements DBQueryClient
 {
   concat(fields: string[]) {
-    return `CONCAT(${fields.join(', ')})`;
+    return `${fields.join(' || ')}`;
   }
-
   simpleCast(field: string, asType: string) {
-    return `${field}::${asType}`;
+    return `CAST(${field} as ${asType})`;
   }
 }

865d3679114279810cacfc3242bd7a04a184413b
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2026-01-16 15:36:37 +0530

    chore(nc-gui): lint

--- packages/nc-gui/components/smartsheet/grid/InfiniteTable.vue
+++ packages/nc-gui/components/smartsheet/grid/InfiniteTable.vue
@@ -1228,7 +1228,7 @@ const bulkExecuteScript = async () => {
     runScript((field.colOptions as ButtonType).fk_script_id!, row.row, {
       pk,
       fieldId: field.id,
-    }).catch((e: any) => {
+    }).catch((_e: any) => {
       // ignore - console error is logged in runScript
     })
   }

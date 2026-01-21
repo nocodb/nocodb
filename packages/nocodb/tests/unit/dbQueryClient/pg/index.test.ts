efc1f68cd71058390ad8d24ad8a3dca0e49408e2
Fendy Heryanto <fendy@nocodb.com>
2026-01-21 13:10:09 +0700

    db query client not exclusive to ee

--- packages/nocodb/tests/unit/dbQueryClient/pg/index.test.ts
+++ packages/nocodb/tests/unit/dbQueryClient/pg/index.test.ts
@@ -1,14 +1,11 @@
 import 'mocha';
 import { runOnSet } from '../../utils/runOnSet';
 import { pgTemporaryTableTests } from './temporaryTable.test';
-import { isEE } from '../../utils/helpers';
 
 function _pgDBQueryClientTests() {
   pgTemporaryTableTests();
 }
 
 export const pgDBQueryClientTests = runOnSet(2, function () {
-  if (isEE()) {
-    describe('PGDBQueryClientTests', _pgDBQueryClientTests);
-  }
+  describe('PGDBQueryClientTests', _pgDBQueryClientTests);
 });

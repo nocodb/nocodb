a2d5c00c87dddf5e18ce7f61fc8af99ad2270ae4
mertmit <mertmit99@gmail.com>
2026-01-21 10:17:58 +0300

    fix: use req itself as we replace circular
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/modules/jobs/jobs/meta-sync/meta-sync.controller.ts
+++ packages/nocodb/src/modules/jobs/jobs/meta-sync/meta-sync.controller.ts
@@ -120,11 +120,7 @@ export class MetaSyncController {
       baseId,
       sourceId: 'all',
       user: req.user,
-      req: {
-        user: req.user,
-        clientIp: req.clientIp,
-        headers: req.headers,
-      },
+      req,
     });
 
     return { id: job.id };
@@ -160,11 +156,7 @@ export class MetaSyncController {
       baseId,
       sourceId,
       user: req.user,
-      req: {
-        user: req.user,
-        clientIp: req.clientIp,
-        headers: req.headers,
-      },
+      req,
     });
 
     return { id: job.id };

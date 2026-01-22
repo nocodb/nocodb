71315d59bf2c960866188b368b0fb0fc3d01d1b3
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2026-01-22 10:31:54 +0000

    fix: keep directly opened create base mode selection design as dropdown

--- packages/nc-gui/components/dashboard/TreeView/CreateProject/MenuItem.vue
+++ packages/nc-gui/components/dashboard/TreeView/CreateProject/MenuItem.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
 
+import { NcMenuItem } from "#components"
+
 interface Props {
     label: string
     subtext?: string
     icon?: IconMapKey
+    variant?: 'dropdown' | 'modal'
 }
 
 withDefaults(defineProps<Props>(), {
@@ -12,12 +15,16 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-    <NcMenuItem :inner-class="`w-full ${$slots.subtext || subtext ? '!items-start' : ''}`">
-        <slot name="icon">
-            <GeneralIcon v-if="icon" :icon="icon" class="h-4 w-4 flex-none" :class="{
-                'mt-0.5': $slots.subtext || subtext
-            }" />
-        </slot>
+    <component :is="variant === 'modal' ? 'div' : NcMenuItem"
+        :inner-class="`w-full ${$slots.subtext || subtext ? '!items-start' : ''}`"
+        :class="`nc-create-project-menu-item-${variant}`">
+        <div class="nc-icon-wrapper">
+            <slot name="icon">
+                <GeneralIcon v-if="icon" :icon="icon" class="h-4 w-4 flex-none" :class="{
+                    'mt-0.5': $slots.subtext || subtext
+                }" />
+            </slot>
+        </div>
 
         <div class="nc-content-wrapper">
             <div class="nc-content-label">
@@ -27,10 +34,23 @@ withDefaults(defineProps<Props>(), {
                 <slot name="subtext">{{ subtext }}</slot>
             </div>
         </div>
-    </NcMenuItem>
+    </component>
 </template>
 
 <style lang="scss" scoped>
+.nc-icon-wrapper {
+    @apply flex items-center justify-center h-4 w-4 children:flex-none;
+}
+
+.nc-create-project-menu-item-modal {
+    @apply font-normal text-sm flex items-start gap-3 mx-1 px-2 py-1 rounded-md hover:bg-nc-bg-gray-light transition-colors cursor-pointer;
+
+    .nc-content-wrapper {
+        .nc-content-label {}
+
+    }
+}
+
 .nc-content-wrapper {
     .nc-content-subtext {
         @apply text-tiny !leading-4 text-nc-content-gray-muted;

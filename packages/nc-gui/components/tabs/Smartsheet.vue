bd5bf7f7a8f0313d88264546825cb431fd33452d
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2026-01-23 06:21:10 +0000

    fix: remove lazy loading from views as it feels like stuck

--- packages/nc-gui/components/tabs/Smartsheet.vue
+++ packages/nc-gui/components/tabs/Smartsheet.vue
@@ -295,23 +295,23 @@ watch(isViewsLoading, async () => {
           @resized="onResized"
         >
           <Pane class="flex flex-col h-full min-w-0" :max-size="contentMaxSize" :size="contentSize">
-            <LazySmartsheetToolbar v-if="!isForm" show-full-screen-toggle />
+            <SmartsheetToolbar v-if="!isForm" show-full-screen-toggle />
             <div :style="{ height: isForm ? '100%' : 'calc(100% - var(--toolbar-height))' }" class="flex flex-row w-full">
               <Transition name="layout" mode="out-in">
                 <div v-if="openedViewsTab === 'view'" class="flex flex-1 min-h-0 w-3/4">
                   <div class="h-full flex-1 min-w-0 min-h-0 bg-nc-bg-default">
-                    <LazySmartsheetGrid v-if="isGrid || !meta || !activeView" ref="grid" />
+                    <SmartsheetGrid v-if="isGrid || !meta || !activeView" ref="grid" />
 
                     <template v-if="activeView && meta">
-                      <LazySmartsheetGallery v-if="isGallery" />
+                      <SmartsheetGallery v-if="isGallery" />
 
-                      <LazySmartsheetForm v-else-if="isForm && !$route.query.reload" />
+                      <SmartsheetForm v-else-if="isForm && !$route.query.reload" />
 
                       <SmartsheetKanbanWrapper v-else-if="isKanban" />
 
-                      <LazySmartsheetCalendar v-else-if="isCalendar" />
+                      <SmartsheetCalendar v-else-if="isCalendar" />
 
-                      <LazySmartsheetMap v-else-if="isMap" />
+                      <SmartsheetMap v-else-if="isMap" />
                     </template>
                   </div>
                 </div>

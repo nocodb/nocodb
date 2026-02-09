fca668d54a9662c3b4ffe22770f00d9ac4b655fc
Raju Udava <86527202+dstala@users.noreply.github.com>
2026-02-09 21:58:33 +0530

    fix: darkmode
    Signed-off-by: Raju Udava <86527202+dstala@users.noreply.github.com>

--- packages/nc-gui/components/smartsheet/toolbar/MappedBy.vue
+++ packages/nc-gui/components/smartsheet/toolbar/MappedBy.vue
@@ -61,42 +61,91 @@ const handleChange = () => {
 </script>
 
 <template>
-  <a-dropdown v-if="!IsPublic" v-model:visible="mappedByDropdown" :trigger="['click']" class="!xs:hidden">
+  <NcDropdown
+    v-if="!IsPublic"
+    v-model:visible="mappedByDropdown"
+    :trigger="['click']"
+    overlay-class-name="nc-dropdown-mapped-by-menu overflow-hidden"
+    class="!xs:hidden"
+  >
     <div class="nc-map-btn">
-      <a-button v-e="['c:map:change-grouping-field']" class="nc-map-stacked-by-menu-btn nc-toolbar-btn" :disabled="isLocked">
-        <div class="flex items-center gap-1">
-          <mdi-arrow-down-drop-circle-outline />
-          <span class="text-capitalize !text-sm font-weight-normal">
-            {{ $t('activity.map.mappedBy') }}
-            <span class="font-bold">{{ geoDataFieldColumn?.title }}</span>
-          </span>
-          <component :is="iconMap.arrowDown" class="text-nc-content-gray-muted" />
+      <NcButton
+        v-e="['c:map:change-grouping-field']"
+        class="nc-map-stacked-by-menu-btn nc-toolbar-btn !border-0 !h-7 group"
+        size="small"
+        type="secondary"
+        :show-as-disabled="isLocked"
+      >
+        <div class="flex items-center gap-2">
+          <GeneralIcon icon="settings" class="h-4 w-4" />
+          <div class="flex items-center gap-0.5">
+            <span class="text-capitalize !text-[13px] font-medium flex items-center gap-1">
+              {{ $t('activity.map.mappedBy') }}
+            </span>
+            <div
+              class="flex items-center rounded-md transition-colors duration-0.3s bg-nc-bg-gray-light px-1 min-h-5 max-w-[108px]"
+              :class="{
+                'group-hover:bg-nc-bg-gray-medium': !isLocked,
+              }"
+            >
+              <span class="!text-[13px] font-medium truncate !leading-5">{{ geoDataFieldColumn?.title }}</span>
+            </div>
+          </div>
         </div>
-      </a-button>
+      </NcButton>
     </div>
     <template #overlay>
       <div
         v-if="mappedByDropdown"
-        class="p-3 min-w-[280px] bg-nc-bg-gray-extralight shadow-lg nc-table-toolbar-menu max-h-[max(80vh,500px)] overflow-auto !border"
+        class="p-4 w-90 bg-nc-bg-default nc-table-toolbar-menu rounded-lg flex flex-col gap-5"
         @click.stop
       >
-        <div>
-          <span class="font-bold"> {{ $t('activity.map.chooseMappingField') }}</span>
-          <a-divider class="!my-2" />
-        </div>
-        <div class="nc-fields-list py-1">
-          <div class="grouping-field">
-            <a-select
-              v-model:value="geoDataMappingFieldColumnId"
-              class="w-full nc-msp-grouping-field-select"
-              :options="geoDataFieldOptions"
-              placeholder="Select a Mapping Field"
-              @change="handleChange"
-              @click.stop
-            />
+        <div class="flex flex-col gap-2">
+          <div>
+            {{ $t('activity.map.chooseMappingField') }}
+          </div>
+          <div class="nc-fields-list">
+            <div class="grouping-field">
+              <a-select
+                v-model:value="geoDataMappingFieldColumnId"
+                class="nc-select-shadow w-full nc-map-grouping-field-select !rounded-lg"
+                dropdown-class-name="!rounded-lg"
+                placeholder="Select a Mapping Field"
+                :disabled="isLocked"
+                @change="handleChange"
+                @click.stop
+              >
+                <template #suffixIcon><GeneralIcon icon="arrowDown" class="text-nc-content-gray-subtle" /></template>
+                <a-select-option v-for="option of geoDataFieldOptions" :key="option.value" :value="option.value">
+                  <div class="w-full flex gap-2 items-center justify-between" :title="option.label">
+                    <div class="flex items-center gap-1 max-w-[calc(100%_-_20px)]">
+                      <SmartsheetHeaderIcon
+                        v-if="option.value && metaColumnById[option.value]"
+                        :column="metaColumnById[option.value]"
+                        class="!w-3.5 !h-3.5 opacity-80 !ml-0"
+                        color="text-current"
+                      />
+                      <NcTooltip class="flex-1 max-w-[calc(100%_-_20px)] truncate" show-on-truncate-only>
+                        <template #title>
+                          {{ option.label }}
+                        </template>
+                        <template #default>{{ option.label }}</template>
+                      </NcTooltip>
+                    </div>
+                    <GeneralIcon
+                      v-if="geoDataMappingFieldColumnId === option.value"
+                      id="nc-selected-item-icon"
+                      icon="check"
+                      class="flex-none text-primary w-4 h-4"
+                    />
+                  </div>
+                </a-select-option>
+              </a-select>
+            </div>
           </div>
         </div>
+        <GeneralLockedViewFooter v-if="isLocked" class="-mb-4 -mx-4" @on-open="mappedByDropdown = false" />
       </div>
     </template>
-  </a-dropdown>
+  </NcDropdown>
 </template>

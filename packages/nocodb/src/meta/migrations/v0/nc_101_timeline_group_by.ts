d2eae3f6b86367aa9eba3f8d28919c03e109cfd5
Raju <raju@nocodb.com>
2026-02-18 10:42:54 +0000

    fix: migration idempotency

--- packages/nocodb/src/meta/migrations/v0/nc_101_timeline_group_by.ts
+++ packages/nocodb/src/meta/migrations/v0/nc_101_timeline_group_by.ts
@@ -2,12 +2,19 @@ import type { Knex } from 'knex';
 import { MetaTable } from '~/utils/globals';
 
 const up = async (knex: Knex) => {
-  await knex.schema.alterTable(MetaTable.TIMELINE_VIEW_COLUMNS, (table) => {
-    table.boolean('group_by');
-    table.float('group_by_order');
-    table.string('group_by_sort', 4);
-    table.string('aggregation', 20);
-  });
+  const hasGroupBy = await knex.schema.hasColumn(
+    MetaTable.TIMELINE_VIEW_COLUMNS,
+    'group_by',
+  );
+
+  if (!hasGroupBy) {
+    await knex.schema.alterTable(MetaTable.TIMELINE_VIEW_COLUMNS, (table) => {
+      table.boolean('group_by');
+      table.float('group_by_order');
+      table.string('group_by_sort', 4);
+      table.string('aggregation', 20);
+    });
+  }
 };
 
 const down = async (knex: Knex) => {

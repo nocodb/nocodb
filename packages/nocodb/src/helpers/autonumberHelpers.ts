c3793639df9573325a3d348220fc24dd0e6076fb
DarkPhoenix2704 <anbarasun123@gmail.com>
2026-02-26 18:19:16 +0530

    fix: clean up

--- packages/nocodb/src/helpers/autonumberHelpers.ts
+++ packages/nocodb/src/helpers/autonumberHelpers.ts
@@ -1,7 +1,1 @@
-async function resetPgSequence(..._args: any) {}
-
-export async function backfillAutoNumber(..._args: any){}
-
-async function backfillDefaultOrder(..._args: any){}
-
-async function backfillWithViewOrder(..._args: any){}
+export async function backfillAutoNumber(..._args: any) {}

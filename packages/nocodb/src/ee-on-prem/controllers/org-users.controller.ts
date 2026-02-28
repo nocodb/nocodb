40c2d9e6dd2e799b19f89ea691c9f19094b8f522
mertmit <mertmit99@gmail.com>
2026-02-28 14:50:26 +0530

    fix: build fix
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/ee-on-prem/controllers/org-users.controller.ts
+++ packages/nocodb/src/ee-on-prem/controllers/org-users.controller.ts
@@ -1,9 +1,9 @@
 import { Controller } from '@nestjs/common';
-import { OrgUsersController as OrgUsersControllerCE } from 'src/controllers/org-users.controller';
+import { OrgUsersController as OrgUsersControllerEE } from 'src/ee/controllers/org-users.controller';
 import { OrgUsersService } from '~/services/org-users.service';
 
 @Controller()
-export class OrgUsersController extends OrgUsersControllerCE {
+export class OrgUsersController extends OrgUsersControllerEE {
   constructor(protected readonly orgUsersService: OrgUsersService) {
     super(orgUsersService);
   }

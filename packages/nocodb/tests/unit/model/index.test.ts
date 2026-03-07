93c0fa8187f779bb84f3e280a5f090450ac11c07
mertmit <mertmit99@gmail.com>
2026-03-07 09:45:42 +0000

    chore: issues

--- packages/nocodb/tests/unit/model/index.test.ts
+++ packages/nocodb/tests/unit/model/index.test.ts
@@ -1,15 +1,24 @@
 import 'mocha';
 import { runOnSet } from '../utils/runOnSet';
 import baseModelSqlTest from './tests/baseModelSql.test';
-import documentTest from './tests/document.test';
-import documentsServiceTest from './tests/documentsService.test';
 import { tableHelpersTest } from './tableHelpers/tableHelpers.test';
 import { isEE } from '../utils/helpers';
 
 function modelTests() {
   baseModelSqlTest();
-  documentTest();
-  documentsServiceTest();
+
+  if (isEE()) {
+    // Document model + service tests require EE model implementations
+    try {
+      const documentTest = require('./tests/document.test').default;
+      const documentsServiceTest =
+        require('./tests/documentsService.test').default;
+      documentTest();
+      documentsServiceTest();
+    } catch (e) {
+      // EE test files not available in CE
+    }
+  }
 }
 
 export default runOnSet(1, function () {

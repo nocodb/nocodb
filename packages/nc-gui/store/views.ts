847d35506f49edeaba5f32d1bf24f7c9db613085
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2026-03-10 12:09:29 +0530

    fix: active table view load issue in shared base on reload

--- packages/nc-gui/store/views.ts
+++ packages/nc-gui/store/views.ts
@@ -1300,9 +1300,12 @@ export const useViewsStore = defineStore('viewsStore', () => {
   }
 
   watch(
-    () => tablesStore.activeTableId,
-    async (newId, oldId) => {
-      if (newId === oldId) return
+    [() => tablesStore.activeTableId, () => activeProjectId.value],
+    async ([newId, newProjectId], [oldId, oldProjectId]) => {
+      if (!newId || !newProjectId) return
+
+      if (newId === oldId && newProjectId === oldProjectId) return
+
       if (isPublic.value) {
         isViewsLoading.value = false
         return

9570eb1c78ddc2355cac6d741d0bf791ed4515c9
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2026-03-13 18:59:14 +0530

    chore: lint

--- packages/nc-gui/ee/components/workspace/CreateProjectBtn.vue
+++ packages/nc-gui/ee/components/workspace/CreateProjectBtn.vue
@@ -22,8 +22,6 @@ const { isUIAllowed } = useRoles()
 
 const { activeWorkspaceId, workspaces } = storeToRefs(useWorkspace())
 
-const { showEEFeatures } = useEeConfig()
-
 const { baseCreateMode } = storeToRefs(useBases())
 
 const baseStore = useBase()

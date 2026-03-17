0c55ea37938cc8ed67f490db6f5029f4fc12ecad
DarkPhoenix2704 <anbarasun123@gmail.com>
2026-03-17 17:58:29 +0530

    fix: workflow draft guard

--- packages/nc-gui/ee/pages/index/[typeOrId]/[baseId]/index/workflows/[workflowId].vue
+++ packages/nc-gui/ee/pages/index/[typeOrId]/[baseId]/index/workflows/[workflowId].vue
@@ -1,11 +1,62 @@
 <script setup lang="ts">
+import type { NavigationGuardNext, RouteLocationNormalizedLoadedGeneric } from 'vue-router'
 import { useProvideWorkflow } from '~/composables/useWorkflow'
+
+const { t } = useI18n()
 
 const workflowStore = useWorkflowStore()
 
-const { activeWorkflow } = storeToRefs(workflowStore)
+const { activeWorkflow, activeWorkflowHasDraftChanges } = storeToRefs(workflowStore)
 
 useProvideWorkflow(activeWorkflow)
+
+const confirmUnpublishedChangesBeforeLeaving = (
+  _to: RouteLocationNormalizedLoadedGeneric,
+  _from: RouteLocationNormalizedLoadedGeneric,
+  next: NavigationGuardNext,
+) => {
+  if (!activeWorkflowHasDraftChanges.value) {
+    next()
+    return
+  }
+
+  const isOpen = ref(true)
+
+  const { close } = useDialog(resolveComponent('NcModalConfirm'), {
+    'visible': isOpen,
+    'title': t('msg.info.leaveWithoutFinishing'),
+    'content': t('msg.info.workflowHasUnpublishedChanges'),
+    'okText': t('labels.keepEditing'),
+    'cancelText': t('labels.leave'),
+    'onCancel': () => {
+      isOpen.value = false
+      close(1000)
+      next()
+    },
+    'onOk': () => {
+      isOpen.value = false
+      close(1000)
+      next(false)
+    },
+    'update:visible': (visible: boolean) => {
+      if (!visible) {
+        close(1000)
+        next(false)
+      }
+    },
+    'showIcon': false,
+    'keyboard': false,
+    'maskClosable': false,
+  })
+}
+
+onBeforeRouteUpdate((to, from, next) => {
+  confirmUnpublishedChangesBeforeLeaving(to, from, next)
+})
+
+onBeforeRouteLeave((to, from, next) => {
+  confirmUnpublishedChangesBeforeLeaving(to, from, next)
+})
 </script>
 
 <template>

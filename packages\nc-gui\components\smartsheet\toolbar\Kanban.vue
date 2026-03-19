94061afa966be74c23842a553a3c17a697553e9f
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-19 18:20:24 +0400

    Update packages\nc-gui\components\smartsheet\toolbar\Kanban.vue

--- packages\nc-gui\components\smartsheet\toolbar\Kanban.vue
+++ packages\nc-gui\components\smartsheet\toolbar\Kanban.vue
@@ -1,22 +1,25 @@
-<script setup lang="ts">
-const { isCompactMode, toggleCompactMode } = useKanbanViewStore()
+<script lang="ts" setup>
+const { isCompactMode, toggleCompactMode } = useKanbanViewStore()!
 
 const { t } = useI18n()
 </script>
 
 <template>
-  <div class="flex items-center gap-2">
-    <NcTooltip :title="isCompactMode ? t('tooltip.disableCompactMode') : t('tooltip.enableCompactMode')">
+  <div class="flex items-center gap-2 nc-kanban-toolbar-extra">
+    <NcTooltip>
+      <template #title>
+        {{ isCompactMode ? t('tooltip.kanbanDisableCompact') : t('tooltip.kanbanEnableCompact') }}
+      </template>
       <NcButton
-        v-e="['c:kanban:compact-mode']"
-        :type="isCompactMode ? 'primary' : 'text'"
-        size="small"
-        class="nc-kanban-compact-mode-btn"
+        v-e="['c:kanban:compact-mode-toggle']"
+        :type="isCompactMode ? 'primary' : 'secondary'"
+        size="xs"
+        class="nc-kanban-compact-btn"
         @click="toggleCompactMode"
       >
         <div class="flex items-center gap-1">
-          <GeneralIcon icon="rows" class="h-4 w-4" />
-          <span class="text-xs">{{ t('general.compact') }}</span>
+          <GeneralIcon icon="list" class="h-3.5 w-3.5" />
+          <span class="text-xs font-medium">{{ t('general.compact') }}</span>
         </div>
       </NcButton>
     </NcTooltip>

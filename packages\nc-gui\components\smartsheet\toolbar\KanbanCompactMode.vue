7f6b37f08d1bafc1f4a86404b1665bbb2f510400
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-19 18:21:47 +0400

    Update packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue

--- packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue
+++ packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue
@@ -8,11 +8,9 @@ const { isMobileMode } = useGlobal()
 const isCompactMode = computed(() => !!(kanbanMetaData.value as any)?.compact_mode)
 
 async function toggleCompactMode() {
-  if (!isUIAllowed('dataEdit')) return
-
   await updateKanbanMeta({
     compact_mode: !isCompactMode.value,
-  })
+  } as any)
 }
 </script>
 
@@ -22,18 +20,18 @@ async function toggleCompactMode() {
       <span>{{ isCompactMode ? $t('tooltip.expandCardView') : $t('tooltip.compactCardView') }}</span>
     </template>
     <NcButton
-      v-e="['c:kanban:toggle-compact-mode']"
-      class="nc-kanban-compact-mode-btn nc-toolbar-btn"
+      v-e="['c:kanban:compact-mode']"
+      :class="{
+        '!text-brand-500 !bg-brand-50': isCompactMode,
+      }"
+      class="nc-kanban-compact-mode nc-toolbar-btn"
       size="small"
       type="text"
-      :class="{
-        '!text-brand-500 !bg-brand-50 !border-brand-200': isCompactMode,
-      }"
       @click="toggleCompactMode"
     >
-      <div class="flex items-center gap-1.5">
-        <GeneralIcon icon="ncAlignJustify" class="h-4 w-4" />
-        <span v-if="!isMobileMode" class="font-medium text-[13px]">
+      <div class="flex items-center gap-2">
+        <GeneralIcon icon="ncList" class="h-4 w-4" />
+        <span v-if="!isMobileMode" class="text-[13px] font-medium">
           {{ $t('title.compact') }}
         </span>
       </div>

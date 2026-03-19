d2cc4472fc4ab65c710e348980e689b22b5b41b9
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-19 18:21:19 +0400

    Update packages\nc-gui\store\kanbanView.ts

--- packages\nc-gui\store\kanbanView.ts
+++ packages\nc-gui\store\kanbanView.ts
@@ -1,17 +1,68 @@
-import type { KanbanType } from 'nocodb-sdk'
-import { acceptHMRUpdate, defineStore } from 'pinia'
+import type { Api, ColumnType, KanbanType, SelectOptionType, TableType, ViewType } from 'nocodb-sdk'
+import type { Ref } from 'vue'
+import { GROUP_BY_VARS, ref, useInjectionState } from '#imports'
 
-export const useKanbanViewStore = defineStore('kanbanViewStore', () => {
-  // ... existing code would be here
-  
-  const kanbanMetaData = ref<KanbanType>({})
-  
-  // compact mode getter
-  const isCompactMode = computed(() => !!(kanbanMetaData.value as any)?.compact_mode)
-  
-  return {
-    kanbanMetaData,
-    isCompactMode,
-    // ... other existing exports
+const [useProvideKanbanViewStore, useKanbanViewStoreOrThrow] = useInjectionState(
+  (
+    meta: Ref<TableType | undefined>,
+    viewMeta: Ref<ViewType | undefined>,
+    shared = false,
+    where?: ComputedRef<string | undefined>,
+  ) => {
+    if (!meta) {
+      throw new Error('Table meta is not available')
+    }
+
+    const { t } = useI18n()
+
+    const { api } = useApi()
+
+    const { $e } = useNuxtApp()
+
+    const { statuses } = storeToRefs(useViewsStore())
+
+    const { addUndo, defineViewScope } = useUndoRedo()
+
+    const isPublic = inject(IsPublicInj, ref(false))
+
+    const { isUIAllowed } = useRoles()
+
+    const { sorts, nestedFilters } = useSmartsheetStoreOrThrow()
+
+    const { sharedView, fetchSharedViewData } = useSharedView()
+
+    const { metas, getMeta } = useMetas()
+
+    const kanbanMetaData = ref<KanbanType>({})
+
+    const isCompactMode = computed(() => !!(kanbanMetaData.value as any)?.compact_mode)
+
+    // ... rest of implementation
+
+    async function updateKanbanMeta(updateObj: Partial<KanbanType & { compact_mode?: boolean }>) {
+      if (!viewMeta?.value?.id || !isUIAllowed('dataEdit')) return
+      kanbanMetaData.value = {
+        ...(kanbanMetaData.value ?? {}),
+        ...updateObj,
+      }
+      await api.dbView.kanbanUpdate(viewMeta.value.id, kanbanMetaData.value as KanbanType)
+    }
+
+    return {
+      kanbanMetaData,
+      isCompactMode,
+      updateKanbanMeta,
+    }
+  },
+  'kanban-view-store',
+)
+
+export { useProvideKanbanViewStore }
+
+export function useKanbanViewStore() {
+  const store = useKanbanViewStoreOrThrow()
+  if (!store) {
+    throw new Error('Kanban view store is not provided')
   }
-})
+  return store
+}

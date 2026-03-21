2e9a8b8626885b2fcefb31d7d3eddb5e7e0bd651
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-21 18:55:20 +0400

    Update packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue

--- packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue
+++ packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue
@@ -1,33 +1,29 @@
 <script lang="ts" setup>
 const { isCompact, toggleCompact } = useKanbanViewStore()
+
+const { isMobileMode } = useGlobal()
 </script>
 
 <template>
-  <NcTooltip>
+  <NcTooltip placement="bottom">
     <template #title>
-      {{ isCompact ? $t('activity.expandCards') : $t('activity.compactCards') }}
+      <span>{{ isCompact ? $t('activity.kanban.expandCards') : $t('activity.kanban.compactCards') }}</span>
     </template>
     <NcButton
-      v-e="['c:kanban:toggle-compact']"
+      v-e="['c:kanban:compact-mode-toggle']"
       size="small"
       type="text"
-      class="nc-kanban-compact-mode-btn !h-7 !px-2 !rounded-md"
-      :class="{ 'bg-gray-100 dark:bg-gray-800': isCompact }"
+      class="nc-toolbar-btn nc-kanban-compact-mode-btn !h-7 !px-1.5 !rounded"
+      :class="{ '!bg-gray-100 dark:!bg-gray-800 !text-primary': isCompact }"
       @click="toggleCompact"
     >
-      <div class="flex items-center gap-1">
-        <component
-          :is="iconMap.list"
-          v-if="isCompact"
+      <div class="flex items-center gap-1.5">
+        <GeneralIcon
+          :icon="isCompact ? 'kanbanCollapse' : 'kanbanExpand'"
           class="h-4 w-4"
         />
-        <component
-          :is="iconMap.expand"
-          v-else
-          class="h-4 w-4"
-        />
-        <span class="text-xs hidden md:inline">
-          {{ isCompact ? $t('activity.defaultView') : $t('activity.compactView') }}
+        <span v-if="!isMobileMode" class="text-xs font-medium">
+          {{ $t('activity.kanban.compactMode') }}
         </span>
       </div>
     </NcButton>

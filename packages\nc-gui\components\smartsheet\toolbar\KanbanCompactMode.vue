c1ae3ed0f2fdb534b86b24a2ad7193b366a4307b
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-21 18:56:20 +0400

    Update packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue

--- packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue
+++ packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue
@@ -3,23 +3,16 @@ const { isCompact, toggleCompact } = useKanbanViewStore()
 </script>
 
 <template>
-  <NcTooltip placement="bottom">
-    <template #title>
-      {{ isCompact ? $t('tooltip.kanbanExpandCards') : $t('tooltip.kanbanCompactCards') }}
-    </template>
-
+  <NcTooltip>
+    <template #title>{{ isCompact ? $t('tooltip.kanbanExpandCards') : $t('tooltip.kanbanCompactCards') }}</template>
     <NcButton
-      v-e="['c:kanban:compact-mode']"
-      type="text"
+      v-e="['c:toolbar:kanban-compact']"
       size="small"
-      class="nc-toolbar-btn nc-kanban-compact-mode-btn !h-7 !px-2"
-      :class="{ 'nc-active-btn': isCompact }"
+      type="text"
+      :class="{ 'text-brand-500 bg-brand-50': isCompact }"
       @click="toggleCompact"
     >
-      <div class="flex items-center gap-1.5">
-        <GeneralIcon icon="rowHeight" class="h-4 w-4" />
-        <span class="text-xs font-medium">{{ $t('title.compactMode') }}</span>
-      </div>
+      <GeneralIcon icon="minimize2" class="h-4 w-4" />
     </NcButton>
   </NcTooltip>
 </template>

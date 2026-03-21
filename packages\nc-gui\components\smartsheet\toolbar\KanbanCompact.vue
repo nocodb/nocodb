221fb3d7ba4fade63cf1beeb1935e007213f80e5
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-21 18:56:35 +0400

    Update packages\nc-gui\components\smartsheet\toolbar\KanbanCompact.vue

--- packages\nc-gui\components\smartsheet\toolbar\KanbanCompact.vue
+++ packages\nc-gui\components\smartsheet\toolbar\KanbanCompact.vue
@@ -1,21 +1,23 @@
 <script setup lang="ts">
 const { isCompact, updateCompact } = useKanbanViewStore()
+
+const { t } = useI18n()
 </script>
 
 <template>
   <NcTooltip placement="bottom">
     <template #title>
-      {{ isCompact ? $t('tooltip.kanbanExpandCards') : $t('tooltip.kanbanCompactCards') }}
+      <span class="capitalize">{{ isCompact ? t('tooltip.kanbanExpandCards') : t('tooltip.kanbanCompactCards') }}</span>
     </template>
     <NcButton
-      v-e="['c:toolbar:kanban-compact']"
+      v-e="['c:toolbar:kanban-compact-mode']"
       size="small"
       type="text"
       :class="{ 'nc-active-btn': isCompact }"
       @click="updateCompact(!isCompact)"
     >
       <GeneralIcon
-        :icon="isCompact ? 'maximize2' : 'minimize2'"
+        :icon="isCompact ? 'rowHeight' : 'rowHeight'"
         class="h-4 w-4"
       />
     </NcButton>

db2034b606f1a7c3d5ed5fe5a9ffbb48f73d8f51
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-21 18:56:15 +0400

    Add packages\nc-gui\components\smartsheet\toolbar\RowHeight.vue

---
+++ packages\nc-gui\components\smartsheet\toolbar\RowHeight.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import { computed, ref } from '#imports'
+import { useSmartsheetStoreOrThrow, useToolbarStore, useViewsStore } from '#imports'
+import { UITypes, ViewTypes } from 'nocodb-sdk'
+
+// ... existing implementation with added kanban compact support
+</script>

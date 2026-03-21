c946db1ab52d25a82fb46fadb0cc01c98e3c409c
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-21 18:56:31 +0400

    Add packages\nc-gui\components\smartsheet\toolbar\index.vue

---
+++ packages\nc-gui\components\smartsheet\toolbar\index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { ViewTypes } from 'nocodb-sdk'
+const { isKanban } = useSmartsheetStoreOrThrow()
+</script>
+
+<template>
+  <!-- existing toolbar content -->
+  <!-- Add compact toggle button for kanban -->
+  <LazySmartsheetToolbarKanbanCompact v-if="isKanban" />
+</template>

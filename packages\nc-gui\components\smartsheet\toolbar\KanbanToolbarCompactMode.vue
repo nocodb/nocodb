6b8562c678b41842ebe23ce305402a2039826385
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-21 18:54:59 +0400

    Add packages\nc-gui\components\smartsheet\toolbar\KanbanToolbarCompactMode.vue

---
+++ packages\nc-gui\components\smartsheet\toolbar\KanbanToolbarCompactMode.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { useKanbanViewStore } from '#imports'
+
+const { isCompact, toggleCompact } = useKanbanViewStore()
+</script>
+
+<template>
+  <NcTooltip>
+    <template #title>
+      {{ isCompact ? $t('tooltip.expandCards') : $t('tooltip.compactCards') }}
+    </template>
+    <NcButton
+      v-e="['c:kanban:compact-mode']"
+      size="small"
+      type="text"
+      class="nc-kanban-compact-btn !h-7 !w-7"
+      :class="{ 'active': isCompact }"
+      @click="toggleCompact"
+    >
+      <GeneralIcon :icon="isCompact ? 'expand' : 'compact'" class="h-4 w-4" />
+    </NcButton>
+  </NcTooltip>
+</template>

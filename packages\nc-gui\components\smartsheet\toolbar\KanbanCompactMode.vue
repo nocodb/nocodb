46aea987ca5f14d6cf5944608486c8f0d210bee3
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-23 21:07:57 +0400

    Update packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue

--- packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue
+++ packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue
@@ -3,30 +3,43 @@ const { isCompactMode, toggleCompactMode } = useKanbanViewStoreOrThrow()
 
 const { $e } = useNuxtApp()
 
-async function onClick() {
-  await toggleCompactMode()
-  $e('a:kanban:compact-mode', { compact: isCompactMode.value })
+const { t } = useI18n()
+
+async function handleToggle() {
+  try {
+    await toggleCompactMode()
+    $e('a:kanban:compact-mode-toggle', { compact: isCompactMode.value })
+  } catch (e) {
+    console.error(e)
+  }
 }
 </script>
 
 <template>
   <NcTooltip placement="bottom">
     <template #title>
-      <span>{{ isCompactMode ? $t('activity.kanban.disableCompactMode') : $t('activity.kanban.enableCompactMode') }}</span>
+      {{
+        isCompactMode
+          ? t('tooltip.kanbanDisableCompactMode')
+          : t('tooltip.kanbanEnableCompactMode')
+      }}
     </template>
 
     <NcButton
-      v-e="['c:kanban:toggle-compact-mode']"
-      :type="isCompactMode ? 'secondary' : 'text'"
+      v-e="['c:kanban:compact-mode']"
       size="small"
+      :type="isCompactMode ? 'secondary' : 'text'"
       :class="{
-        'text-brand-500 bg-brand-50 !border-brand-200': isCompactMode,
+        '!text-brand-500 !bg-brand-50 hover:!bg-brand-100': isCompactMode,
+        '!text-gray-600': !isCompactMode,
       }"
-      @click="onClick"
+      @click="handleToggle"
     >
       <div class="flex items-center gap-1.5">
-        <GeneralIcon icon="compact" class="h-4 w-4" />
-        <span class="text-xs font-medium">{{ $t('activity.kanban.compactMode') }}</span>
+        <GeneralIcon icon="rows01" class="h-3.5 w-3.5" />
+        <span class="text-xs font-medium leading-4">
+          {{ t('activity.kanbanCompactMode') }}
+        </span>
       </div>
     </NcButton>
   </NcTooltip>

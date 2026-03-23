7c40fa27141ab4f1798b36992436d117ed196e27
Mert E. <mertmit99@gmail.com>
2026-03-24 01:12:40 +0530

    fix: redis env mismatches (#8465)
    * fix: redis env mismatches
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    * fix: add back accidental removal
    
    Signed-off-by: mertmit <mertmit99@gmail.com>
    
    ---------
    
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/ee/helpers/redisHelpers.ts
+++ packages/nocodb/src/ee/helpers/redisHelpers.ts
@@ -10,16 +10,22 @@ export const throttlerEnabled = () =>
   !!(process.env.NC_THROTTLER_REDIS || process.env.NC_REDIS_URL);
 
 export const getRedisURL = (type?: NC_REDIS_TYPE) => {
-  if (!process.env.NC_REDIS_URL) return undefined;
-
   switch (type) {
     case NC_REDIS_TYPE.CACHE:
       return process.env.NC_REDIS_URL;
     case NC_REDIS_TYPE.JOB:
-      return process.env.NC_REDIS_JOB_URL || process.env.NC_REDIS_URL;
+      return (
+        process.env.NC_REDIS_JOB_URL ||
+        process.env.NC_JOBS_REDIS_URL ||
+        process.env.NC_REDIS_URL
+      );
     case NC_REDIS_TYPE.THROTTLER:
-      return process.env.NC_THROTTLER_REDIS || process.env.NC_REDIS_URL;
+      return (
+        process.env.NC_RATE_LIMIT_REDIS_URL ||
+        process.env.NC_THROTTLER_REDIS ||
+        process.env.NC_REDIS_URL
+      );
     default:
-      return process.env.NC_REDIS_URL;
+      return process.env.NC_CACHE_REDIS_URL || process.env.NC_REDIS_URL;
   }
 };

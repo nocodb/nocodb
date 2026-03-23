e7c7ef10cafe6d6c76350340c6e0bcd9276911f3
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-23 21:08:19 +0400

    Update packages\nc-gui\components\board\KanbanCard.vue

--- packages\nc-gui\components\board\KanbanCard.vue
+++ packages\nc-gui\components\board\KanbanCard.vue
@@ -13,9 +13,12 @@ const props = defineProps<{
 const emits = defineEmits(['expand', 'deleteRow', 'unGroupRow', 'reorder'])
 
 const { isMobileMode } = useGlobal()
-const { getPossibleAttachmentSrc, isImage } = useAttachment()
-const { isSynced } = useNocoEe()
+
 const { isUIAllowed } = useRoles()
+
+const { getPossibleAttachmentSrc, isImage } = useAttachment()
+
+const { isSynced } = useNocoEe()
 
 const isCompact = computed(() => !!props.compactMode)
 
@@ -50,16 +53,14 @@ function expandCard() {
 <template>
   <div
     :class="[
-      'nc-kanban-item group relative flex flex-col rounded-md border border-gray-200 bg-white shadow-sm transition-shadow cursor-pointer',
+      'nc-kanban-item group relative flex flex-col rounded-md border border-gray-200 bg-white shadow-sm transition-shadow',
       isCompact ? 'py-1 px-2 gap-0' : 'pt-2 pb-3 px-3 gap-2',
-      { 'hover:shadow-md': !readOnly },
     ]"
-    @click="expandCard"
   >
-    <!-- Compact view -->
+    <!-- Compact view: single row -->
     <template v-if="isCompact">
-      <div class="flex items-center gap-1 min-h-[22px]">
-        <div class="flex-1 min-w-0 text-[11px] text-gray-800 leading-snug truncate">
+      <div class="flex items-center gap-1">
+        <div class="flex-1 min-w-0">
           <template v-if="primaryField">
             <LazySmartsheetVirtualCell
               v-if="isVirtualCol(primaryField)"
@@ -67,7 +68,7 @@ function expandCard() {
               :column="primaryField"
               :row="row"
               :read-only="true"
-              class="!text-[11px] !leading-snug truncate"
+              class="!text-xs !leading-snug truncate"
             />
             <LazySmartsheetCell
               v-else
@@ -75,25 +76,17 @@ function expandCard() {
               :column="primaryField"
               :row="row"
               :read-only="true"
-              class="!text-[11px] !leading-snug truncate"
+              class="!text-xs !leading-snug truncate"
             />
           </template>
-          <span v-else class="text-gray-400 italic text-[11px]">
-            {{ $t('msg.noData') }}
-          </span>
         </div>
         <div
-          v-if="!readOnly && isUIAllowed('dataEdit')"
-          class="flex-shrink-0 opacity-0 group-hover:opacity-100 transition-opacity flex items-center"
+          v-if="!readOnly"
+          class="flex-shrink-0 opacity-0 group-hover:opacity-100 transition-opacity"
           @click.stop
         >
-          <NcButton
-            size="xsmall"
-            type="text"
-            class="!h-5 !w-5 !min-w-5 !p-0"
-            @click.stop="emits('expand')"
-          >
-            <component :is="iconMap.expand" class="h-2.5 w-2.5 text-gray-500" />
+          <NcButton size="xsmall" type="text" class="!h-5 !w-5 !min-w-5 !p-0" @click.stop="$emit('expand')">
+            <component :is="iconMap.expand" class="h-3 w-3" />
           </NcButton>
         </div>
       </div>
@@ -101,54 +94,38 @@ function expandCard() {
 
     <!-- Normal view -->
     <template v-else>
-      <!-- Cover image -->
-      <div
-        v-if="attachments.length"
-        class="-mt-2 -mx-3 mb-1 overflow-hidden rounded-t-md"
-      >
-        <img
-          :src="getPossibleAttachmentSrc(attachments[0])"
-          class="w-full h-40 object-cover"
-          alt=""
-        />
+      <div v-if="attachments.length" class="-mt-2 -mx-3 mb-0 overflow-hidden rounded-t-md">
+        <img :src="getPossibleAttachmentSrc(attachments[0])" class="w-full h-40 object-cover" alt="" />
       </div>
 
-      <div class="flex items-start gap-1.5 w-full">
-        <div class="flex-1 min-w-0 flex flex-col gap-1.5">
-          <template v-for="field in fieldsWithoutCover" :key="field.id">
-            <div class="flex items-start gap-1 text-sm min-w-0">
-              <LazySmartsheetVirtualCell
-                v-if="isVirtualCol(field)"
-                :model-value="row.row[field.title!]"
-                :column="field"
-                :row="row"
-                :read-only="true"
-                class="min-w-0 flex-1"
-              />
-              <LazySmartsheetCell
-                v-else
-                :model-value="row.row[field.title!]"
-                :column="field"
-                :row="row"
-                :read-only="true"
-                class="min-w-0 flex-1"
-              />
-            </div>
-          </template>
+      <div class="flex gap-1 items-start">
+        <div class="flex-1 min-w-0 flex flex-col gap-1">
+          <div v-for="field in fieldsWithoutCover" :key="field.id" class="flex items-start gap-1 min-w-0">
+            <LazySmartsheetVirtualCell
+              v-if="isVirtualCol(field)"
+              :model-value="row.row[field.title!]"
+              :column="field"
+              :row="row"
+              :read-only="true"
+              class="min-w-0 flex-1"
+            />
+            <LazySmartsheetCell
+              v-else
+              :model-value="row.row[field.title!]"
+              :column="field"
+              :row="row"
+              :read-only="true"
+              class="min-w-0 flex-1"
+            />
+          </div>
         </div>
-
         <div
-          v-if="!readOnly && isUIAllowed('dataEdit')"
+          v-if="!readOnly"
           class="flex-shrink-0 opacity-0 group-hover:opacity-100 transition-opacity"
           @click.stop
         >
-          <NcButton
-            size="xsmall"
-            type="text"
-            class="!h-6 !w-6 !min-w-6 !p-0"
-            @click.stop="emits('expand')"
-          >
-            <component :is="iconMap.expand" class="h-3.5 w-3.5 text-gray-500" />
+          <NcButton size="xsmall" type="text" class="!h-6 !w-6 !min-w-6 !p-0" @click.stop="$emit('expand')">
+            <component :is="iconMap.expand" class="h-3.5 w-3.5" />
           </NcButton>
         </div>
       </div>

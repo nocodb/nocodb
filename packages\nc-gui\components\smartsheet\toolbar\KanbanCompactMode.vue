2b36cb811f9cdf0ed972f22f3950300dcf1debeb
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-23 21:08:03 +0400

    Update packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue

--- packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue
+++ packages\nc-gui\components\smartsheet\toolbar\KanbanCompactMode.vue
@@ -1,45 +1,32 @@
 <script lang="ts" setup>
 const { isCompactMode, toggleCompactMode } = useKanbanViewStoreOrThrow()
-
 const { $e } = useNuxtApp()
 
-const { t } = useI18n()
-
-async function handleToggle() {
-  try {
-    await toggleCompactMode()
-    $e('a:kanban:compact-mode-toggle', { compact: isCompactMode.value })
-  } catch (e) {
-    console.error(e)
-  }
+async function onClick() {
+  await toggleCompactMode()
+  $e('a:kanban:compact-mode', { enabled: isCompactMode.value })
 }
 </script>
 
 <template>
   <NcTooltip placement="bottom">
     <template #title>
-      {{
-        isCompactMode
-          ? t('tooltip.kanbanDisableCompactMode')
-          : t('tooltip.kanbanEnableCompactMode')
-      }}
+      {{ isCompactMode ? $t('tooltip.kanbanCompactModeDisable') : $t('tooltip.kanbanCompactModeEnable') }}
     </template>
-
     <NcButton
       v-e="['c:kanban:compact-mode']"
       size="small"
       :type="isCompactMode ? 'secondary' : 'text'"
-      :class="{
-        '!text-brand-500 !bg-brand-50 hover:!bg-brand-100': isCompactMode,
-        '!text-gray-600': !isCompactMode,
-      }"
-      @click="handleToggle"
+      :class="[
+        isCompactMode
+          ? 'nc-active-btn !text-brand-500'
+          : 'nc-toolbar-btn',
+      ]"
+      @click="onClick"
     >
-      <div class="flex items-center gap-1.5">
-        <GeneralIcon icon="rows01" class="h-3.5 w-3.5" />
-        <span class="text-xs font-medium leading-4">
-          {{ t('activity.kanbanCompactMode') }}
-        </span>
+      <div class="flex items-center gap-1">
+        <component :is="iconMap.rowHeight" class="h-4 w-4" />
+        <span class="nc-toolbar-btn-txt hidden md:inline">{{ $t('activity.kanbanCompactMode') }}</span>
       </div>
     </NcButton>
   </NcTooltip>

4f109efd9a63970ccd404c611b770e09a53577c5
yuri <yuri@mac.lan>
2026-04-13 13:39:34 +0800

    fix: 移除对不存在的 gcp_marketplace 迁移文件的引用
    问题：
    - XcMigrationSourcev0.ts 引用了 nc_202604040000_gcp_marketplace 迁移
    - 该迁移文件从未被创建，导致后端启动时报 MODULE_NOT_FOUND 错误
    
    解决方案：
    - 从迁移源文件中移除对该缺失迁移的 import、注册和 switch case
    
    这修复了本地开发环境中后端无法启动的问题。

--- packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
+++ packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
@@ -45,7 +45,6 @@ import * as nc_202603230000_subscription_last_paid_seat_count from './v0/nc_2026
 import * as nc_202603301109_fine_grained_api_tokens from './v0/nc_202603301109_fine_grained_api_tokens';
 import * as nc_202603310000_integration_links from './v0/nc_202603310000_integration_links';
 import * as nc_202604030000_installations_add_fk_user_id from './v0/nc_202604030000_installations_add_fk_user_id';
-import * as nc_202604040000_gcp_marketplace from './v0/nc_202604040000_gcp_marketplace';
 import * as nc_202604071200_default_org from './v0/nc_202604071200_default_org';
 import * as nc_202604071201_scim_config_default_role from './v0/nc_202604071201_scim_config_default_role';
 
@@ -104,8 +103,6 @@ export default class XcMigrationSourcev0 {
       'nc_202603301109_fine_grained_api_tokens',
       'nc_202603310000_integration_links',
       'nc_202604030000_installations_add_fk_user_id',
-
-      'nc_202604040000_gcp_marketplace',
       'nc_202604071200_default_org',
       'nc_202604071201_scim_config_default_role',
     ]);
@@ -211,9 +208,6 @@ export default class XcMigrationSourcev0 {
         return nc_202603310000_integration_links;
       case 'nc_202604030000_installations_add_fk_user_id':
         return nc_202604030000_installations_add_fk_user_id;
-
-      case 'nc_202604040000_gcp_marketplace':
-        return nc_202604040000_gcp_marketplace;
       case 'nc_202604071200_default_org':
         return nc_202604071200_default_org;
       case 'nc_202604071201_scim_config_default_role':

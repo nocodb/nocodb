5d6f1976cc0aaff83bb622d8f1faf2075d039356
Fendy Heryanto <fendy@nocodb.com>
2026-04-21 14:47:19 +0700

    fix on getMigrations

--- packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
+++ packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
@@ -103,7 +103,6 @@ export default class XcMigrationSourcev0 {
       'nc_202603110001_chat_session_base_id',
       'nc_202603170000_form_view_expires_at',
       'nc_202603170001_workflow_draft_reminder',
-      'nc_202603180000_user_mfa',
       'nc_202603090002_date_dependency',
       'nc_202603230000_subscription_last_paid_seat_count',
       'nc_202603301109_fine_grained_api_tokens',
@@ -115,6 +114,7 @@ export default class XcMigrationSourcev0 {
       'nc_202604100000_audit_org_id',
       'nc_202604160000_docs_in_data',
       'nc_202604200002_trash_cleanup_due_at',
+      'nc_202603180000_user_mfa',
     ]);
   }
 

03198f2ec763839a4a421875f125ff537712fd24
DarkPhoenix2704 <anbarasun123@gmail.com>
2026-04-27 16:22:11 +0530

    fix: update migration date

--- packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
+++ packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
@@ -53,7 +53,7 @@ import * as nc_202604100000_audit_org_id from './v0/nc_202604100000_audit_org_id
 import * as nc_202604160000_docs_in_data from './v0/nc_202604160000_docs_in_data';
 import * as nc_202604200002_trash_cleanup_due_at from './v0/nc_202604200002_trash_cleanup_due_at';
 import * as nc_202604220000_uuid_readonly from './v0/nc_202604220000_uuid_readonly';
-import * as nc_202604200000_base_trash from './v0/nc_202604200000_base_trash';
+import * as nc_202604270000_base_trash from './v0/nc_202604270000_base_trash';
 
 // Create a custom migration source class
 export default class XcMigrationSourcev0 {
@@ -118,7 +118,7 @@ export default class XcMigrationSourcev0 {
       'nc_202604200002_trash_cleanup_due_at',
       'nc_202603180000_user_mfa',
       'nc_202604220000_uuid_readonly',
-      'nc_202604200000_base_trash',
+      'nc_202604270000_base_trash',
     ]);
   }
 
@@ -238,8 +238,8 @@ export default class XcMigrationSourcev0 {
         return nc_202603180000_user_mfa;
       case 'nc_202604220000_uuid_readonly':
         return nc_202604220000_uuid_readonly;
-      case 'nc_202604200000_base_trash':
-        return nc_202604200000_base_trash;
+      case 'nc_202604270000_base_trash':
+        return nc_202604270000_base_trash;
     }
   }
 }

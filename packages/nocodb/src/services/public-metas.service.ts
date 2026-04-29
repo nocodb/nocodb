b268a68b7c96c34599ba9715d6f1e16062fafa64
Fendy Heryanto <fendy@nocodb.com>
2026-04-29 14:00:16 +0700

    refactor to use extractDisplayNameFromEmail

--- packages/nocodb/src/services/public-metas.service.ts
+++ packages/nocodb/src/services/public-metas.service.ts
@@ -27,6 +27,7 @@ import {
 } from '~/models';
 import { NcError } from '~/helpers/catchError';
 import { extractProps } from '~/helpers/extractProps';
+import { extractDisplayNameFromEmail } from '~/utils/emailUtils';
 import { hasDefaultTableVisibility } from '~/helpers/tableHelpers';
 
 @Injectable()
@@ -155,14 +156,7 @@ export class PublicMetasService {
 
       view.users = baseUsers.map((u) => ({
         id: u.id,
-        display_name:
-          u.display_name ||
-          (u.email
-            ? u.email
-                .split('@')[0]
-                .replace(/[._-]+/g, ' ')
-                .replace(/\b\w/g, (c) => c.toUpperCase())
-            : null),
+        display_name: extractDisplayNameFromEmail(u.email, u.display_name),
         email: '',
         meta: ncIsObject(u.meta)
           ? extractProps(u.meta, ['icon', 'iconType'])

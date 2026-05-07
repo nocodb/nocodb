7e8c118b1518337dfa1914cd4a5d13b7f2b395c8
mertmit <mertmit99@gmail.com>
2026-05-07 18:36:22 +0300

    fix: meta service recreate issue
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/meta/docs-content.service.ts
+++ packages/nocodb/src/meta/docs-content.service.ts
@@ -1,10 +1,16 @@
 import { Injectable, Optional } from '@nestjs/common';
+import type { knex } from 'knex';
 import { MetaService } from '~/meta/meta.service';
 import { NcConfig } from '~/utils/nc-config';
 
 @Injectable()
 export class DocsContentService extends MetaService {
-  constructor(config: NcConfig, @Optional() trx = null) {
-    super(config, trx);
+  constructor(
+    config: NcConfig,
+    @Optional() trx = null,
+    @Optional() nested = 0,
+    @Optional() sharedKnex: knex.Knex | null = null,
+  ) {
+    super(config, trx, nested, sharedKnex);
   }
 }

698ed659dd8873deff1c63897878f78c00244fbb
Fendy Heryanto <fendy@nocodb.com>
2026-05-18 04:19:21 +0000

    fix(nocodb): move Docs v3 API routes under /api/v3/meta/bases/:baseId

--- packages/nocodb/src/constants/controllers.ts
+++ packages/nocodb/src/constants/controllers.ts
@@ -1,3 +1,3 @@
 export const PREFIX_APIV3_DATA = '/api/v3/data/:baseName';
 export const PREFIX_APIV3_METABASE = '/api/v3/meta/bases/:baseId';
-export const PREFIX_APIV3_DOCS = '/api/v3/docs/:baseId';
+export const PREFIX_APIV3_METADOCS = `${PREFIX_APIV3_METABASE}/docs`;

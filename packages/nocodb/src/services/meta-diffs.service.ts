1c7e0a38e86c79f0d743bac10c2afd24fc6f7bb8
Fendy H <fendy@nocodb.com>
2026-05-30 23:33:33 +0700

    Update packages/nocodb/src/services/meta-diffs.service.ts
    Co-authored-by: Mert E. <mertmit99@gmail.com>

--- packages/nocodb/src/services/meta-diffs.service.ts
+++ packages/nocodb/src/services/meta-diffs.service.ts
@@ -1445,10 +1445,10 @@ export class MetaDiffsService {
         }
         if (!isRelationAvailInB) {
           const fkChildColB = assocModel.columns.find(
-            (c) => c.id === belongsToCols[0].colOptions.fk_child_column_id,
+            (c) => c.id === belongsToCols[1].colOptions.fk_child_column_id,
           );
           const fkParentColB = assocModel.columns.find(
-            (c) => c.id === belongsToCols[1].colOptions.fk_child_column_id,
+            (c) => c.id === belongsToCols[0].colOptions.fk_child_column_id,
           );
 
           await Column.insert<LinksColumn>(context, {

c74dffa8b523e0775585b7e7bea78aa0fd8c0040
Raju Udava <86527202+dstala@users.noreply.github.com>
2026-07-09 07:03:19 +0000

    chore(comments): rename attachments migration after rebase onto develop

--- packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
+++ packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
@@ -80,9 +80,9 @@ import * as nc_202606240001_chat_ui_context_record from './v0/nc_202606240001_ch
 import * as nc_202606251200_form_save_draft_to_browser from './v0/nc_202606251200_form_save_draft_to_browser';
 import * as nc_202606260001_dedupe_user_emails from './v0/nc_202606260001_dedupe_user_emails';
 import * as nc_202606271200_hook_comment_config from './v0/nc_202606271200_hook_comment_config';
-import * as nc_202606270001_comment_attachments from './v0/nc_202606270001_comment_attachments';
 import * as nc_202606290001_sort_lookup_col from './v0/nc_202606290001_sort_lookup_col';
 import * as nc_202607030001_ltar_order_columns from './v0/nc_202607030001_ltar_order_columns';
+import * as nc_202607071200_comment_attachments from './v0/nc_202607071200_comment_attachments';
 
 // Create a custom migration source class
 export default class XcMigrationSourcev0 {
@@ -174,9 +174,9 @@ export default class XcMigrationSourcev0 {
       'nc_202606251200_form_save_draft_to_browser',
       'nc_202606260001_dedupe_user_emails',
       'nc_202606271200_hook_comment_config',
-      'nc_202606270001_comment_attachments',
       'nc_202606290001_sort_lookup_col',
       'nc_202607030001_ltar_order_columns',
+      'nc_202607071200_comment_attachments',
     ]);
   }
 
@@ -350,12 +350,12 @@ export default class XcMigrationSourcev0 {
         return nc_202606260001_dedupe_user_emails;
       case 'nc_202606271200_hook_comment_config':
         return nc_202606271200_hook_comment_config;
-      case 'nc_202606270001_comment_attachments':
-        return nc_202606270001_comment_attachments;
       case 'nc_202606290001_sort_lookup_col':
         return nc_202606290001_sort_lookup_col;
       case 'nc_202607030001_ltar_order_columns':
         return nc_202607030001_ltar_order_columns;
+      case 'nc_202607071200_comment_attachments':
+        return nc_202607071200_comment_attachments;
     }
   }
 }

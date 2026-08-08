9243e0973a0754685a737dc938fd57bee22f1164
Raju Udava <86527202+dstala@users.noreply.github.com>
2026-08-08 08:40:12 +0000

    feat(interface): seed a new page's config from a table view

--- packages/nc-gui/composables/useEeConfig.ts
+++ packages/nc-gui/composables/useEeConfig.ts
@@ -86,6 +86,7 @@ export const useEeConfig = createSharedComposable(() => {
   const blockInterfaceViewWidget = computed(() => true)
   const blockInterfaceIframeWidget = computed(() => true)
   const blockInterfaceUserFilters = computed(() => true)
+  const blockCopyViewSettingFromOther = computed(() => true)
   const blockInterfaceToolbarToggles = computed(() => true)
   const blockInterfaceDrafts = computed(() => true)
   const blockInterfacePreviewAs = computed(() => true)
@@ -517,6 +518,7 @@ export const useEeConfig = createSharedComposable(() => {
     blockAddNewInterface,
     isInterfacePageLimitReached,
     blockInterfaceMultiViz,
+    blockCopyViewSettingFromOther,
     blockInterfaceMetricColorConditions,
     blockInterfacePivotWidget,
     blockInterfaceViewWidget,

e04f6260e1b3295caa76643944fa98e6f7965e28
Raju Udava <86527202+dstala@users.noreply.github.com>
2026-08-28 08:58:48 +0000

    feat(enterprise): admin panel navigation polish

--- packages/nc-gui/components/workspace/ViewTopbar.vue
+++ packages/nc-gui/components/workspace/ViewTopbar.vue
@@ -37,7 +37,7 @@ const activeTabLabel = computed(() => {
     case 'integrations':
       return t('general.integrations')
     case 'admin':
-      return t('labels.admin')
+      return t('labels.settings')
     default:
       return t('objects.projects')
   }

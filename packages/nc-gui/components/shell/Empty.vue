ad20e67de387f1bb93996c632b8d7ec1961bfa9c
Raju Udava <86527202+dstala@users.noreply.github.com>
2026-09-22 17:31:40 +0000

    style(nc-gui): bigger mascot on empty settings panes

--- packages/nc-gui/components/shell/Empty.vue
+++ packages/nc-gui/components/shell/Empty.vue
@@ -11,7 +11,7 @@ defineProps<Props>()
 
 <template>
   <div class="nc-shell-empty px-2 py-6 flex flex-col items-center gap-6 text-center" data-testid="nc-shell-empty">
-    <img src="~assets/img/placeholder/no-search-result-found.png" class="!w-[164px] flex-none" :alt="title" />
+    <img src="~assets/img/placeholder/no-search-result-found.png" class="!w-[240px] flex-none" :alt="title" />
     <div class="max-w-md text-bodyDefaultSm text-nc-content-gray-muted">{{ title }}</div>
     <slot name="action" />
   </div>
